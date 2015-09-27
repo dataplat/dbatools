74491ce71ebcd5b61fc814844b1cb48de7922ae8
Chrissy LeMaire <clemaire@gmail.com>
2015-09-27 20:36:21 +0200

    updated warning message to be more informative

--- Functions/SharedFunctions.ps1
+++ Functions/SharedFunctions.ps1
@@ -94,7 +94,7 @@ Function Invoke-SMOCheck {
 		$serverversion = $SqlServer.version.major
 		
 		if ($serverversion-$smo -gt 1) {
-			Write-Warning "Your version of SMO is $smo, which is significantly older than the server's version $($SqlServer.version.major)."
+			Write-Warning "Your version of SMO is $smo, which is significantly older than the $($sqlserver.name) version $($SqlServer.version.major)."
 			Write-Warning "This may present an issue when migrating certain portions of SQL Server."
 			Write-Warning "If you encounter issues, consider upgrading SMO."
 		}
