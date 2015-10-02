dc18ef1eba86683b2acac1b3397805ea2192ac37
Chrissy LeMaire <clemaire@gmail.com>
2015-10-02 19:28:44 +0200

    moved all public/shared functions to SharedFunctions.ps1

--- dbatools.psm1
+++ dbatools.psm1
@@ -1,3 +1,7 @@
+# All supporting functions have been moved to Functions\SharedFunctions.ps1
+# If you're looking through the code, you pretty much have to work with two files
+# at any one time. The function you're working on, and SharedFunctions.ps1
+
 # Strictmode coming when I've got time.  Set-StrictMode -Version Latest
 
 foreach ($function in (Get-ChildItem "$PSScriptRoot\Functions\*.ps1")) { . $function  }
