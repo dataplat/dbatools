49b669913cffc78adcf3bd188db3f8eb379e1a0c
Chrissy LeMaire <clemaire@gmail.com>
2016-05-01 01:09:42 +0200

    cleaned up a little

--- dbatools.psm1
+++ dbatools.psm1
@@ -1,10 +1,13 @@
 # All supporting functions have been moved to Functions\SharedFunctions.ps1
 # If you're looking through the code, you pretty much have to work with two files
 # at any one time. The function you're working on, and SharedFunctions.ps1
-
-# Strictmode coming when I've got time.  Set-StrictMode -Version Latest
-
 foreach ($function in (Get-ChildItem "$PSScriptRoot\Functions\*.ps1")) { . $function  }
+
+# Not supporting the provider path at this time
+if (((Resolve-Path .\).Path).StartsWith("SQLSERVER:\")) { throw "Starting " }
 
 # I renamed this function to be more accurate
 Set-Alias -Name Reset-SqlSaPassword -Value Reset-SqlAdmin
+
+# Strictmode coming when I've got time.
+# Set-StrictMode -Version Latest
