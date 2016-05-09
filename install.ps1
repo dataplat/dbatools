6dd34005cd87b2efb192160484acffffafa51579
Chrissy LeMaire <clemaire@gmail.com>
2016-05-09 20:47:36 +0200

    updated to add optional path

--- install.ps1
+++ install.ps1
@@ -1,49 +1,55 @@
-Remove-Module dbatools -ErrorAction SilentlyContinue
-$url = 'https://github.com/ctrlbold/dbatools/archive/master.zip'
-$path = Join-Path -Path (Split-Path -Path $profile) -ChildPath '\Modules\dbatools'
-$temp = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
-$zipfile = "$temp\sqltools.zip"
+[CmdletBinding()] 
+param(
+	[string] $Path = $(Join-Path -Path (Split-Path -Path $profile) -ChildPath '\Modules\dbatools')
+)
 
-if (!(Test-Path -Path $path))
-{
-	Write-Output "Creating directory: $path"
-	New-Item -Path $path -ItemType Directory | Out-Null
-}
-else
-{
-	Write-Output "Deleting previously installed module"
-	Remove-Item -Path "$path\*" -Force -Recurse
-}
 
-Write-Output "Downloading archive from github"
-try
-{
-	Invoke-WebRequest $url -OutFile $zipfile
-}
-catch
-{
-	#try with default proxy and usersettings
-	Write-Output "Probably using a proxy for internet access, trying default proxy settings"
-	(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-	Invoke-WebRequest $url -OutFile $zipfile
-}
+	Remove-Module dbatools -ErrorAction SilentlyContinue
+	$url = 'https://github.com/ctrlbold/dbatools/archive/master.zip'
 
-# Unblock if there's a block
-Unblock-File $zipfile -ErrorAction SilentlyContinue
+	$temp = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
+	$zipfile = "$temp\sqltools.zip"
 
-Write-Output "Unzipping"
-# Keep it backwards compatible
-$shell = New-Object -COM Shell.Application
-$zipPackage = $shell.NameSpace($zipfile)
-$destinationFolder = $shell.NameSpace($temp)
-$destinationFolder.CopyHere($zipPackage.Items())
+	if (!(Test-Path -Path $path))
+	{
+		Write-Output "Creating directory: $path"
+		New-Item -Path $path -ItemType Directory | Out-Null
+	}
+	else
+	{
+		Write-Output "Deleting previously installed module"
+		Remove-Item -Path "$path\*" -Force -Recurse
+	}
 
-Write-Output "Cleaning up"
-Move-Item -Path "$temp\dbatools-master\*" $path
-Remove-Item -Path "$temp\dbatools-master"
-Remove-Item -Path $zipfile
+	Write-Output "Downloading archive from github"
+	try
+	{
+		Invoke-WebRequest $url -OutFile $zipfile
+	}
+	catch
+	{
+		#try with default proxy and usersettings
+		Write-Output "Probably using a proxy for internet access, trying default proxy settings"
+		(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+		Invoke-WebRequest $url -OutFile $zipfile
+	}
 
-Write-Output "Done! Please report any bugs to clemaire@gmail.com."
-if ((Get-Command -Module dbatools).count -eq 0) { Import-Module "$path\dbatools.psd1" }
-Get-Command -Module dbatools
-Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
+	# Unblock if there's a block
+	Unblock-File $zipfile -ErrorAction SilentlyContinue
+
+	Write-Output "Unzipping"
+	# Keep it backwards compatible
+	$shell = New-Object -COM Shell.Application
+	$zipPackage = $shell.NameSpace($zipfile)
+	$destinationFolder = $shell.NameSpace($temp)
+	$destinationFolder.CopyHere($zipPackage.Items())
+
+	Write-Output "Cleaning up"
+	Move-Item -Path "$temp\dbatools-master\*" $path
+	Remove-Item -Path "$temp\dbatools-master"
+	Remove-Item -Path $zipfile
+
+	Write-Output "Done! Please report any bugs to clemaire@gmail.com."
+	if ((Get-Command -Module dbatools).count -eq 0) { Import-Module "$path\dbatools.psd1" }
+	Get-Command -Module dbatools
+	Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
