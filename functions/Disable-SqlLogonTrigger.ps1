b85d79c025025aff87edcc3cc2e5133760ce81cc
dansqldba <dalexandero@gmail.com>
2016-07-12 10:51:15 +0100

    Removed template content
    Removed more of the template content to reduce the risk of people
    running the cmdlet and receiving errors.

--- functions/Disable-SqlLogonTrigger.ps1
+++ functions/Disable-SqlLogonTrigger.ps1
@@ -66,22 +66,12 @@ Disables all user logon triggers, Microsoft shipped triggers are untouched.
 		[Alias("ServerInstance", "SqlInstance")]
 		[object]$SqlServer,
 		[System.Management.Automation.PSCredential]$SqlCredential,
-        [string[]]$TriggerName,
         [switch]$DisableAll
 	)
 	DynamicParam { if ($sqlserver) { return (Get-ParamSqlServerTriggers -SqlServer $sqlserver -SqlCredential $SourceSqlCredential) } }
 		
 	BEGIN
 	{
-		# Create your supporting functions here. Check SharedFunctions.ps1 first to see if it already has what you need.
-		
-		Function Do-Something {
-			# Remember that the whole .ps1 shares variables, so
-			# you do not need (though you can) to specify the params again
-			
-			return $sourceserver.name
-			
-		}
 		
 		Write-Output "Attempting to connect to SQL Server.."
 		
@@ -97,82 +87,11 @@ Disables all user logon triggers, Microsoft shipped triggers are untouched.
 			throw "Collection Sets are only supported in SQL Server 2008 and above. Quitting."
 		}
 		
-		# Used a dynamic parameter? Convert from RuntimeDefinedParameter object to regular array
-        # da: What does this mean?
-		$Logins = $psboundparameters.Logins
 	}
 	
 	PROCESS
 	{
-		foreach ($login in $logins)
-		{
-			# Use SMO as much as possible. Using T-SQL is generally an exception.		
-			if ($login.LoginType -eq "WindowsUser" -or $login.LoginType -eq "WindowsGroup")
-			{				
-				If ($Pscmdlet.ShouldProcess($source, "Doing something to that"))
-				{
-					try
-					{
-						Do-Something $that
-					}
-					catch
-					{
-						# Always use the Write-Exception shared function.
-						Write-Exception $_
-						throw "sometimes I throw, but sometimes I continue. I always use write-exception, though so the person can see details."
-					}
-				}
-			}
-			
-			# SqlConnections are built into the SMO connections already. You can either use the current context or a specific database
-			
-			# ExecuteScalar returns 1 row with 1 column
-			$hashedpass = $sourceserver.ConnectionContext.ExecuteScalar($sql)
-			
-			# Need to execute something in a specific database and have it return a datatable?
-			$hashedpassdt = $sourceserver.databases['myspecificdb'].ExecuteWithResults($sql)
-			
-			# Use if you find yourself having a super long if/else, you may need to use continue instead.
-			# Need to write a warning?
-			
-			if ($whatever -eq "we should just skip this, actually")
-			{
-					Write-Warning "$($login.LoginType) logins not supported. $($login.name) skipped."
-					continue
-			}
-				
-			# Don't forget the version is stored in the SMO server object
-			
-			if ($sourceserver.versionMajor -ge 9)
-			{
-				# This script can support 2000, but this portion will only work in 2005 and above.
-				
-			}
-			
-			# Oh, also, if you find yourself doing a bunch of elseif, remember how awesome switch is
-			switch ($sourceserver.VersionMajor)
-			{
-				9 { $sqlversion = "SQL Server 2005 $servicePack" }
-				10 {
-					if ($sourceserver.VersionMinor -eq 0)
-					{
-						$sqlversion = "SQL Server 2008 $servicePack"
-					}
-					else
-					{
-						$sqlversion = "SQL Server 2008 R2 $servicePack"
-					}
-				}
-				11 { $sqlversion = "SQL Server 2012 $servicePack" }
-				12 { $sqlversion = "SQL Server 2014 $servicePack" }
-				13 { $sqlversion = "SQL Server 2016 $servicePack" }
-				default
-				{
-					Write-Warning "SQL version not currently supported."
-					continue
-				}
-			}
-		}
+		
 	}
 	
 	# END is to disconnect from servers and finish up the script. When using the pipeline, things in here will be executed last and only once.
@@ -181,7 +100,8 @@ Disables all user logon triggers, Microsoft shipped triggers are untouched.
 		If ($Pscmdlet.ShouldProcess("console", "Showing final message"))
 		{
 			Write-Output "SQL Logon Trigger disabled"
-			$sourceserver.ConnectionContext.Disconnect()
 		}
+		
+		$sourceserver.ConnectionContext.Disconnect()
 	}
 }
