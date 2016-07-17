8c0483676d675e39f3b61733c5f7361ebb9cd3e1
ctrlbold <clemaire@gmail.com>
2016-07-16 17:09:07 -1000

    fix some typos

--- functions/Show-SqlServerFileSystem.ps1
+++ functions/Show-SqlServerFileSystem.ps1
@@ -45,7 +45,7 @@ Show-SqlServerFileSystem -SqlServer sqlserver2014a
 Shows a GUI and uses Windows Authentication to log into the SQL Server. Returns a string of the path you selected.
 	
 .EXAMPLE   
-Show-SqlServerFileSystem -Source sqlserver2014a -Destination sqlcluster -SqlCredential $cred
+Show-SqlServerFileSystem -Source sqlserver2014a -SqlCredential $cred
 
 Shows a GUI and uses SQL credentials to log into the SQL Server. Returns a string of the path you selected.
 	
@@ -159,7 +159,7 @@ Shows a GUI and uses SQL credentials to log into the SQL Server. Returns a strin
         <TreeView Name="treeview" Height="462" Width="391" Background="#FFFFFF" BorderBrush="#FFFFFF" Foreground="#FFFFFF" Margin="11,36,11,79"/>
         <Label x:Name="label" Content="Select the folder:" HorizontalAlignment="Left" Margin="15,4,0,0" VerticalAlignment="Top"/>
         <Label x:Name="path" Content="Selected Path" HorizontalAlignment="Left" Margin="15,502,0,0" VerticalAlignment="Top"/>
-        <TextBox Name="textbox" HorizontalAlignment="Left" Height="23" Margin="111,504,0,0" TextWrapping="Wrap" Text="C:\blah" VerticalAlignment="Top" Width="292"/>
+        <TextBox Name="textbox" HorizontalAlignment="Left" Height="23" Margin="111,504,0,0" TextWrapping="Wrap" Text="C:\" VerticalAlignment="Top" Width="292"/>
         <Button Name="okbutton" Content="OK" HorizontalAlignment="Left" Margin="241,540,0,0" VerticalAlignment="Top" Width="75"/>
         <Button Name="cancelbutton" Content="Cancel" HorizontalAlignment="Left" Margin="328.766,540,0,0" VerticalAlignment="Top" Width="75"/>
     </Grid>
