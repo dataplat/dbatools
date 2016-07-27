9118c3e3da9183540e664955bfdf93bb4aaed687
ctrlbold <clemaire@gmail.com>
2016-07-27 06:32:04 -1000

    initial commit with template

---
+++ functions/Test-SqlPartitionAlignment.ps1
@@ -0,0 +1,84 @@
+﻿Function Test-SqlPartitionAlignment
+{
+<#
+.SYNOPSIS 
+Test-SqlPartitionAlignment migrates SQL Agent categories from one SQL Server to another. This is similar to sp_add_category.
+
+https://msdn.microsoft.com/en-us/library/ms181597.aspx
+
+.DESCRIPTION
+By default, all SQL Agent categories for Jobs, Operators and Alerts are copied.  
+
+.PARAMETER SqlServer
+The SQL Server instance.You must have sysadmin access and server version must be SQL Server version 2000 or higher.
+
+.PARAMETER SqlCredential
+Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. 
+
+.NOTES 
+Author: Chrissy LeMaire (@cl), netnerds.net
+Requires: sysadmin access on SQL Servers
+
+dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+Copyright (C) 2016 Chrissy LeMaire
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+.LINK
+https://dbatools.io/Test-SqlPartitionAlignment
+
+.EXAMPLE
+Test-SqlPartitionAlignment -SqlServer sqlserver2014a
+Copies all policies and conditions from sqlserver2014a to sqlcluster, using Windows credentials. 
+
+.EXAMPLE   
+Test-SqlPartitionAlignment -SqlServer sqlserver2014a -SqlCredential $cred
+Does this, using SQL credentials for sqlserver2014a and Windows credentials for sqlcluster.
+
+.EXAMPLE   
+Test-SqlPartitionAlignment -SqlServer sqlserver2014 -WhatIf
+Shows what would happen if the command were executed.
+	
+.EXAMPLE   
+Test-SqlPartitionAlignment -SqlServer sqlserver2014a
+Does this 
+	
+#>
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	Param (
+		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Alias("ServerInstance", "SqlInstance")]
+		[object]$SqlServer,
+		[object]$SqlCredential,
+		[switch]$Detailed
+	)
+	
+	
+	DynamicParam { if ($sqlserver) { return Get-ParamSqlDatabases -SqlServer $sqlserver -SqlCredential $SqlCredential } }
+	
+	BEGIN
+	{
+		
+		$sourceserver = Connect-SqlServer -SqlServer $sqlserver -SqlCredential $SqlCredential
+		$source = $sourceserver.DomainInstanceName
+		
+		$Databases = $psboundparameters.Databases
+	}
+	
+	PROCESS
+	{
+		
+		
+	}
+	
+	END
+	{
+		$sourceserver.ConnectionContext.Disconnect()
+		
+		
+	}
+}
