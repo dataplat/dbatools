95c20562dbed43082661a21c96008135f668d8a6
ctrlbold <clemaire@gmail.com>
2016-07-28 12:52:07 -1000

    updates

--- functions/Set-SqlTempDBConfiguration.ps1
+++ functions/Set-SqlTempDBConfiguration.ps1
@@ -19,15 +19,11 @@ Original Author: Michael Fal (@Mike_Fal), http://mikefal.net
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
 Copyright (C) 2016 Chrissy LeMaire
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -256,6 +252,5 @@ Returns PSObject representing tempdb configuration.
 		END
 		{
 			$server.ConnectionContext.Disconnect()
-			
 		}
 	}
