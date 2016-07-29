59f3bd0862499c4d3eae6355988ea6a41dd4f9d2
Lauren LeMaire <l.e.holtzman@gmail.com>
2016-07-29 19:03:50 +0200

    Removed line breaks; fixed some spelling errors

--- functions/Copy-SqlOperator.ps1
+++ functions/Copy-SqlOperator.ps1
@@ -5,9 +5,48 @@ Function Copy-SqlOperator
 Copy-SqlOperator migrates operators from one SQL Server to another. 
 
 .DESCRIPTION
-By default, all operators are copied. The -Operators parameter is autopopulated for command-line completion and can be used to copy only specific operator s.
+By default, all operators are copied. The -Operators parameter is autopopulated for command-line completion and can be used to copy only specific operators.
 
-If the associated credential for the does not exist on the destination, it will be skipped. If the operator already exists on the destination, it will be skipped unless -Force is used.  
+If the associated credentials for the operator do not exist on the destination, it will be skipped. If the operator already exists on the destination, it will be skipped unless -Force is used.  
+
+.PARAMETER Source
+Source SQL Server.You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+
+.PARAMETER Destination
+Destination Sql Server. You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+
+.PARAMETER SourceSqlCredential
+Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+
+$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter. 
+
+Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
+To connect as a different Windows user, run PowerShell as that user.
+
+.PARAMETER DestinationSqlCredential
+Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+
+$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter. 
+
+Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
+
+To connect as a different Windows user, run PowerShell as that user.
+
+.NOTES 
+Author: Chrissy LeMaire (@cl), netnerds.net
+Requires: sysadmin access on SQL Servers
+
+dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+Copyright (C) 2016 Chrissy LeMaire
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+.LINK
+https://dbatools.io/Copy-SqlOperator
 
 .EXAMPLE   
 Copy-SqlOperator -Source sqlserver2014a -Destination sqlcluster
@@ -17,8 +56,7 @@ Copies all operators from sqlserver2014a to sqlcluster, using Windows credential
 .EXAMPLE   
 Copy-SqlOperator -Source sqlserver2014a -Destination sqlcluster -Operator PSOperator -SourceSqlCredential $cred -Force
 
-Copies a single operator, the PSOperator operator from sqlserver2014a to sqlcluster, using SQL credentials for sqlserver2014a
-and Windows credentials for sqlcluster. If an operator with the same name exists on sqlcluster, it will be dropped and recreated because -Force was used.
+Copies a single operator, the PSOperator operator from sqlserver2014a to sqlcluster, using SQL credentials for sqlserver2014a and Windows credentials for sqlcluster. If an operator with the same name exists on sqlcluster, it will be dropped and recreated because -Force was used.
 
 .EXAMPLE   
 Copy-SqlOperator -Source sqlserver2014a -Destination sqlcluster -WhatIf -Force
