f7b91bb8edb40e5017edd9c997084b860d380cd5
ctrlbold <clemaire@gmail.com>
2016-08-16 06:32:17 -1000

    in case someone sends a named instance to Join-AdminUnc internal function

--- functions/SharedFunctions.ps1
+++ functions/SharedFunctions.ps1
@@ -744,6 +744,8 @@ Internal function. Parses a path to make it an admin UNC.
 	if (!$filepath) { return }
 	if ($filepath.StartsWith("\\")) { return $filepath }
 	
+	$servername = $servername.Split("\")[0]
+	
 	if ($filepath.length -gt 0 -and $filepath -ne [System.DbNull]::Value)
 	{
 		$newpath = Join-Path "\\$servername\" $filepath.replace(':', '$')
