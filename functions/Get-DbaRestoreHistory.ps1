66801171659960bea4042d1650c250395a0ec752
ctrlbold <clemaire@gmail.com>
2016-08-24 00:03:54 -1000

    initial commit

---
+++ functions/Get-DbaRestoreHistory.ps1
@@ -0,0 +1,214 @@
+﻿Function Get-DbaRestoreHistory
+{
+<#
+.SYNOPSIS
+Returns restore history details for databases on a SQL Server
+	
+.DESCRIPTION
+By default, this command will return the server name, database, username, restore type, date, from file and to files.
+
+Thanks to https://www.mssqltips.com/sqlservertip/1724/when-was-the-last-time-your-sql-server-database-was-restored/ for the query and https://sqlstudies.com/2016/07/27/when-was-this-database-restored/ for the idea.
+	
+.PARAMETER SqlServer
+The SQL Server that you're connecting to.
+
+.PARAMETER Credential
+Credential object used to connect to the SQL Server as a different user
+
+.PARAMETER Databases
+Return restore information for only specific databases
+
+.PARAMETER Exclude
+Return restore information for all but these specific databases
+
+.PARAMETER Since
+Datetime object used to narrow the results to a date
+
+.PARAMETER Detailed
+Returns a ton of information about the backup history with a max of 1000 rows
+	
+.PARAMETER Force
+Returns a ton of information about the backup history with no max rows
+
+.NOTES 
+dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+Copyright (C) 2016 Chrissy LeMaire
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+.LINK
+https://dbatools.io/Get-DbaRestoreHistory
+
+.EXAMPLE
+Get-DbaRestoreHistory -SqlServer sqlserver2014a, sql2016
+
+Returns server name, database, username, restore type, date, from file and to files for all restored databases on sqlserver2014a and sql2016.
+
+.EXAMPLE   
+Get-DbaRestoreHistory -SqlServer sqlserver2014a -Databases db1, db2
+
+Returns restore information only for databases db1 and db2 on sqlserve2014a
+	
+.EXAMPLE   
+Get-DbaRestoreHistory -SqlServer sqlserver2014a -Detailed -Exclude db1
+
+Lots of detailed information for all databases on sqlserver2014a except db1
+
+.EXAMPLE   
+Get-SqlRegisteredServerName -SqlServer sql2016 | Get-DbaRestoreHistory
+
+Returns restore information for every server listed in the Central Management Server on sql2016
+	
+#>
+	[CmdletBinding()]
+	Param (
+		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Alias("ServerInstance", "SqlInstance")]
+		[string[]]$SqlServer,
+		[PsCredential]$Credential,
+		[datetime]$Since,
+		[switch]$Detailed,
+		[switch]$Force
+	)
+	
+	DynamicParam { if ($SqlServer) { return Get-ParamSqlDatabases -SqlServer $SqlServer[0] -SqlCredential $Credential } }
+	
+	BEGIN
+	{
+		# Convert from RuntimeDefinedParameter object to regular array
+		$databases = $psboundparameters.Databases
+		$exclude = $psboundparameters.Exclude
+		
+		$collection = New-Object System.Collections.ArrayList
+		
+		if ($Since -ne $null)
+		{
+			$Since = $Since.ToString("yyyy-MM-dd HH:mm:ss")
+		}
+	}
+	
+	PROCESS
+	{
+		foreach ($server in $SqlServer)
+		{
+			try
+			{
+				$sourceserver = Connect-SqlServer -SqlServer $server -SqlCredential $Credential
+				
+				if ($detailed -eq $true)
+				{
+					$select = "SELECT top 1000 *"
+				}
+				elseif ($force -eq $true)
+				{
+					$select = "SELECT * FROM"
+				}
+				else
+				{
+					$select = "Select
+					@@Servername AS [Server],
+					rsh.destination_database_name AS [Database],
+					rsh.user_name AS [Username],
+					CASE WHEN rsh.restore_type = 'D' THEN 'Database'
+					WHEN rsh.restore_type = 'F' THEN 'File'
+					WHEN rsh.restore_type = 'G' THEN 'Filegroup'
+					WHEN rsh.restore_type = 'I' THEN 'Differential'
+					WHEN rsh.restore_type = 'L' THEN 'Log'
+					WHEN rsh.restore_type = 'V' THEN 'Verifyonly'
+					WHEN rsh.restore_type = 'R' THEN 'Revert'
+					ELSE rsh.restore_type
+					END AS [RestoreType],
+						   rsh.restore_date AS [Date],
+						   bmf.physical_device_name AS [From],
+						   rf.destination_phys_name AS [To]"
+				}
+				
+				$from = " FROM msdb.dbo.restorehistory rsh
+				INNER JOIN msdb.dbo.backupset bs ON rsh.backup_set_id = bs.backup_set_id
+				INNER JOIN msdb.dbo.restorefile rf ON rsh.restore_history_id = rf.restore_history_id
+				INNER JOIN msdb.dbo.backupmediafamily bmf ON bmf.media_set_id = bs.media_set_id"
+				
+				if ($exclude.length -gt 0 -or $databases.length -gt 0 -or $Since.length -gt 0)
+				{
+					$where = " WHERE "
+				}
+				
+				$wherearray = @()
+				
+				if ($exclude.length -gt 0)
+				{
+					$dblist = $exclude -join "','"
+					$wherearray += " destination_database_name not in ('$dblist')"
+				}
+				
+				if ($databases.length -gt 0)
+				{
+					$dblist = $databases -join "','"
+					$wherearray += "destination_database_name in ('$dblist')"
+				}
+				
+				if ($databases.length -gt 0)
+				{
+					$dblist = $databases -join "','"
+					$wherearray += "destination_database_name in ('$dblist')"
+				}
+				
+				if ($Since -ne $null)
+				{
+					$wherearray += "rsh.restore_date >= '$since'"
+				}
+				
+				if ($where.length -gt 0)
+				{
+					$wherearray = $wherearray -join " and "
+					$where = "$where $wherearray"
+				}
+				
+				$sql = "$select $from $where"
+				Write-Verbose $sql
+				$results = $sourceserver.ConnectionContext.ExecuteWithResults($sql).Tables
+			}
+			catch
+			{
+				Write-Warning "$_ `nMoving on"
+				continue
+			}
+			
+			if ($Detailed -eq $false -and $Force -eq $false)
+			{
+				$dbs = $results.Rows | Select-Object Server, Database -Unique
+				
+				foreach ($db in $dbs)
+				{
+					$dbrows = $results.Rows | Where-Object { $_.Server -eq $db.Server -and $_.Database -eq $db.Database }
+					
+					$allfrom = ($dbrows | Select-Object From -Unique).From
+					$allto = ($dbrows | Select-Object To -Unique).To
+					
+					$collection += [PSCustomObject]@{
+						Server = $dbrows[0].Server
+						Database = $dbrows[0].Database
+						Username = $dbrows[0].Username
+						RestoreType = $dbrows[0].RestoreType
+						Date = $dbrows[0].Date
+						From = $allfrom
+						To = $allto
+					}
+				}
+			}
+			else
+			{
+				$collection += $results
+			}
+		}
+	}
+	
+	END
+	{
+		return $collection
+	}
+}
