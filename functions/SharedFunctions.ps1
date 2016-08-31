31cf8f9d0008b2b61eb3b3345c283176bd71d3de
ctrlbold <clemaire@gmail.com>
2016-08-30 23:05:27 -1000

    omg this is so cool, but internal use only

--- functions/SharedFunctions.ps1
+++ functions/SharedFunctions.ps1
@@ -1603,52 +1603,46 @@ Internal command
 		[Parameter(Mandatory = $true)]
 		[object]$Server,
 		[System.Management.Automation.PSCredential]$Credential,
+		[Parameter(Mandatory = $true)]
 		[scriptblock]$ScriptBlock,
 		[string[]]$ArgumentList
 	)
 	
-	$ArgumentList += $server
+	if ($Server.GetType() -eq [Microsoft.SqlServer.Management.Smo.Server])
+	{
+		$server = $server.ComputerNamePhysicalNetBIOS
+	}
 	
+	# Remove instance name if it as passed
+	$server = ($Server.Split("\"))[0]
+	
+	if ($Server -eq $env:COMPUTERNAME -or $Server -eq 'localhost' -or $Server -eq '.')
+	{
+		$Server = 'localhost'
+		if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+		{
+			throw "Get-DbaStartupParameter must be run with elevated privileges for the local host."
+		}
+	}
+	
+	$ipaddr = (Test-Connection $server -Count 1 -ErrorAction Stop).Ipv4Address
+	$ArgumentList += $ipaddr
+		
 	[scriptblock]$setupScriptBlock = {
-		$Server = $args[$args.GetUpperBound(0)]
+		$ipaddr = $args[$args.GetUpperBound(0)]
 		
 		# Just in case we go remote, ensure the assembly is loaded
 		[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
 		
-		if ($server.GetType() -eq [Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer])
-		{
-			$null = $server.Initialize()
-			return $server
-		}
-		
-		if ($Server.GetType() -eq [Microsoft.SqlServer.Management.Smo.Server])
-		{
-			$server = $server.ComputerNamePhysicalNetBIOS
-		}
-		
-		# Remove instance name if it as passed
-		$server = ($Server.Split("\"))[0]
-		
-		if ($Server -eq $env:COMPUTERNAME -or $Server -eq 'localhost' -or $Server -eq '.')
-		{
-			$Server = 'localhost'
-			if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-			{
-				throw "Get-DbaStartupParameter must be run with elevated privileges for the local host."
-			}
-		}
-		
-		$ipaddr = (Test-Connection $server -Count 1 -ErrorAction Stop).Ipv4Address
-		
 		$wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $ipaddr
-		$null = $server.Initialize()
+		$null = $wmi.Initialize()
 	}
 	
 	$prescriptblock = $setupScriptBlock.ToString()
 	$postscriptblock = $ScriptBlock.ToString()
 	
 	$scriptblock = [ScriptBlock]::Create("$prescriptblock  $postscriptblock")
-	
+		
 	try
 	{
 		if ($credential.username -ne $null)
@@ -1664,13 +1658,19 @@ Internal command
 	{
 		try
 		{
+			Write-Verbose "Local connection attempt failed. Connecting remotely."
+			
+			# For surely resolve stuff
+			$hostname = [System.Net.Dns]::gethostentry($ipaddr)
+			$hostname = $hostname.HostName
+			
 			if ($credential.username -ne $null)
 			{
-				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -Credential $Credential -ComputerName [System.Net.Dns]::gethostentry($ipaddr)
+				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -Credential $Credential -ComputerName $hostname
 			}
 			else
 			{
-				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -ComputerName [System.Net.Dns]::gethostentry($ipaddr)
+				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -ComputerName $hostname
 				
 			}
 		}
@@ -1680,6 +1680,4 @@ Internal command
 			throw $_
 		}
 	}
-	
-	return $server
 }
