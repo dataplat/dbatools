f3347c1623b1f7a4daa12f63e42ccd0c727a7bce
ctrlbold <clemaire@gmail.com>
2016-08-30 22:37:42 -1000

    Revert "Revert "adding wmi management since a few commands are using it""
    This reverts commit 2fbb13a958832628fd8e6e73a2bf1545da4406fe.

--- functions/SharedFunctions.ps1
+++ functions/SharedFunctions.ps1
@@ -1589,3 +1589,97 @@ Function Update-SqlPermissions
 		}
 	}
 }
+
+
+Function Invoke-DbaManagedComputer
+{
+<#
+.SYNOPSIS
+Internal command
+	
+#>	
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[object]$Server,
+		[System.Management.Automation.PSCredential]$Credential,
+		[scriptblock]$ScriptBlock,
+		[string[]]$ArgumentList
+	)
+	
+	$ArgumentList += $server
+	
+	[scriptblock]$setupScriptBlock = {
+		$Server = $args[$args.GetUpperBound(0)]
+		
+		# Just in case we go remote, ensure the assembly is loaded
+		[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
+		
+		if ($server.GetType() -eq [Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer])
+		{
+			$null = $server.Initialize()
+			return $server
+		}
+		
+		if ($Server.GetType() -eq [Microsoft.SqlServer.Management.Smo.Server])
+		{
+			$server = $server.ComputerNamePhysicalNetBIOS
+		}
+		
+		# Remove instance name if it as passed
+		$server = ($Server.Split("\"))[0]
+		
+		if ($Server -eq $env:COMPUTERNAME -or $Server -eq 'localhost' -or $Server -eq '.')
+		{
+			$Server = 'localhost'
+			if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+			{
+				throw "Get-DbaStartupParameter must be run with elevated privileges for the local host."
+			}
+		}
+		
+		$ipaddr = (Test-Connection $server -Count 1 -ErrorAction Stop).Ipv4Address
+		
+		$wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $ipaddr
+		$null = $server.Initialize()
+	}
+	
+	$prescriptblock = $setupScriptBlock.ToString()
+	$postscriptblock = $ScriptBlock.ToString()
+	
+	$scriptblock = [ScriptBlock]::Create("$prescriptblock  $postscriptblock")
+	
+	try
+	{
+		if ($credential.username -ne $null)
+		{
+			Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -Credential $Credential
+		}
+		else
+		{
+			Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+		}
+	}
+	catch
+	{
+		try
+		{
+			if ($credential.username -ne $null)
+			{
+				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -Credential $Credential -ComputerName [System.Net.Dns]::gethostentry($ipaddr)
+			}
+			else
+			{
+				Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -ComputerName [System.Net.Dns]::gethostentry($ipaddr)
+				
+			}
+		}
+		catch
+		{
+			Write-Exception $_
+			throw $_
+		}
+	}
+	
+	return $server
+}
