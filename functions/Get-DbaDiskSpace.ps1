468d82a52fa35282341e391e586a48460a39b28a
Jagoop <jakob@bindslet.dk>
2016-09-29 23:00:44 +0200

    Fix #190, added bytes as unit, filesystem, fragmentation and -Detailed

--- functions/Get-DbaDiskSpace.ps1
+++ functions/Get-DbaDiskSpace.ps1
@@ -3,24 +3,35 @@
 <#
 .SYNOPSIS
 Displays Disk information for all local drives on a server
-	
+
 .DESCRIPTION
-Returns a custom object with Server name, name of disk, label of disk, total size, free size and percent free.
-	
+Returns a custom object with Server name, name of disk, label of disk, total size, free size, percent free, block size and filesystem.
+
+Requires: Windows administrator access on SQL Servers
+
 .PARAMETER ComputerName
 The SQL Server (or server in general) that you're connecting to. The -SqlServer parameter also works.
 
 .PARAMETER Unit
-Display the disk space information in a specific unit. Valid values incldue 'KB', 'MB', 'GB', 'TB', and 'PB'. Default is GB.
-	
+Display the disk space information in a specific unit. Valid values include 'Bytes', 'KB', 'MB', 'GB', 'TB', and 'PB'. Default is GB.
+
 .PARAMETER CheckForSql
 Check to see if any SQL Data or Log files exists on the disk. Uses Windows authentication to connect by default.
 
 .PARAMETER SqlCredential
 If you want to use SQL Server Authentication to connect.
 
+.PARAMETER FileSystem
+Displays the filesystem (ie. NTFS, FAT32, etc.)
+
+.PARAMETER CheckFragmentation
+Includes a check for fragmentation in all filesystems. This will increase the runtime of the function.
+
+.PARAMETER Detailed
+Includes the same information as -FileSystem -CheckForSql, as well as including Volumes ("\\?\Volume").
+
 .NOTES
-Requires: Windows sysadmin access on SQL Servers
+Author: Chrissy LeMaire (clemaire@gmail.com) & Jakob Bindslet (jakob@bindslet.dk)
 
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
 Copyright (C) 2016 Chrissy LeMaire
@@ -39,59 +50,86 @@ Get-DbaDiskSpace -ComputerName sqlserver2014a
 
 Shows disk space for sqlserver2014a in GB
 
-.EXAMPLE   
+.EXAMPLE
 Get-DbaDiskSpace -ComputerName sqlserver2014a -Unit TB
 
 Shows disk space for sqlserver2014a in TB
 
-.EXAMPLE   
+.EXAMPLE
 Get-DbaDiskSpace -ComputerName server1, server2, server3 -Unit MB
 
 Returns a custom object filled with information for server1, server2 and server3, in MB
-	
+
+.EXAMPLE
+Get-DbaDiskSpace -ComputerName Odin1 -Detailed | Format-Table -AutoSize
+
+Server Name                                              Label SizeInGB FreeInGB PercentFree BlockSize IsSqlDisk FileSystem
+------ ----                                              ----- -------- -------- ----------- --------- --------- ----------
+Odin1  \\?\Volume{2ec825ee-4f4a-4608-8a79-ee6d09f10340}\ Spare   931,37     48,3        5,19      4096 False     NTFS
+Odin1  C:\                                               System  232,54    14,97        6,44      4096 False     NTFS
+Odin1  D:\                                               Disk1  3724,87   193,82         5,2     65536 True      NTFS
+Odin1  F:\                                               Disk2  2792,87    77,76        2,78      4096 False     NTFS
+
 #>
 	[CmdletBinding(SupportsShouldProcess = $true)]
 	Param (
-		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[Alias("ServerInstance", "SqlInstance", "SqlServer")]
-		[string[]]$ComputerName,
-		[ValidateSet('KB', 'MB', 'GB', 'TB', 'PB')]
+		[String[]]$ComputerName,
+		[ValidateSet('Bytes', 'KB', 'MB', 'GB', 'TB', 'PB')]
 		[String]$Unit = "GB",
-		[switch]$CheckForSql,
-		[object]$SqlCredential
+		[Switch]$CheckForSql,
+		[Object]$SqlCredential,
+        [Switch]$Detailed,
+        [Switch]$Filesystem,
+        [Switch]$CheckFragmentation
 	)
-	
+
 	BEGIN
 	{
 		Function Get-AllDiskSpace
 		{
-			
-			$measure = "1$unit"
+			if ($Unit -eq 'Bytes')
+			{
+				$measure = "1"
+			}
+			else
+			{
+				$measure = "1$unit"
+			}
 			$alldisks = @()
-			
+
 			try
 			{
 				$ipaddr = (Test-Connection $server -Count 1 -ErrorAction SilentlyContinue).Ipv4Address | Select-Object -First 1
-				
+
 			}
 			catch
 			{
 				Write-Warning "Can't connect to $server"
 				return
 			}
-			
+
 			try
 			{
-				$query = "Select SystemName, Name, DriveType, FileSystem, FreeSpace, Capacity, Label, BlockSize from Win32_Volume where DriveType = 2 or DriveType = 3"
-				$disks = Get-WmiObject -ComputerName $ipaddr -Query $query | Sort-Object -Property Name
+				if ($CheckFragmentation)
+				{
+					$disks = Get-WmiObject -Class Win32_Volume -Namespace 'root\CIMV2' -ComputerName $ipaddr | Where-Object DriveType -in (2,3)
+					$disks = $disks | Select-Object SystemName, Name, DriveType, FileSystem, FreeSpace, Capacity, Label, BlockSize, @{Name="FilePercentFragmentation"; Expression={"$($_.defraganalysis().defraganalysis.FilePercentFragmentation)"}}
+				}
+				else
+				{
+					$query = "Select SystemName, Name, DriveType, FileSystem, FreeSpace, Capacity, Label, BlockSize from Win32_Volume where DriveType = 2 or DriveType = 3"
+					$disks = Get-WmiObject -ComputerName $ipaddr -Query $query | Sort-Object -Property Name
+				}
 			}
 			catch
 			{
 				Write-Warning "Can't connect to WMI on $server"
 				return
 			}
-			
-			if ($CheckForSql -eq $true)
+
+			if ($CheckForSql -or $Detailed)
 			{
 				$sqlservers = @()
 				$sqlservices = Get-Service -ComputerName $ipaddr | Where-Object { $_.DisplayName -like 'SQL Server (*' }
@@ -99,7 +137,7 @@ Returns a custom object filled with information for server1, server2 and server3
 				{
 					$instance = $service.DisplayName.Replace('SQL Server (', '')
 					$instance = $instance.TrimEnd(')')
-					
+
 					if ($instance -eq 'MSSQLSERVER')
 					{
 						$sqlservers += $ipaddr
@@ -110,7 +148,7 @@ Returns a custom object filled with information for server1, server2 and server3
 					}
 				}
 			}
-			
+
 			foreach ($disk in $disks)
 			{
 				$diskname = $disk.Name
@@ -137,57 +175,57 @@ Returns a custom object filled with information for server1, server2 and server3
 						}
 					}
 				}
-				
-				if (!$diskname.StartsWith("\\"))
+
+				if (!$diskname.StartsWith("\\") -or $Detailed)
 				{
-					$total = "{0:n2}" -f ($disk.Capacity/$measure)
-					$free = "{0:n2}" -f ($disk.Freespace/$measure)
-					$percentfree = "{0:n2}" -f (($disk.Freespace / $disk.Capacity) * 100)
-					
-					if ($CheckForSql -eq $true)
-					{
-						$alldisks += [PSCustomObject]@{
-							Server = $server
-							Name = $diskname
-							Label = $disk.Label
-							"SizeIn$unit" = $total
-							"FreeIn$unit" = $free
-							PercentFree = $percentfree
-							BlockSize = $disk.BlockSize
-							IsSqlDisk = $sqldisk
-						}
-					}
-					else
-					{
-						$alldisks += [PSCustomObject]@{
-							Server = $server
-							Name = $diskname
-							Label = $disk.Label
-							"SizeIn$unit" = $total
-							"FreeIn$unit" = $free
-							PercentFree = $percentfree
-							BlockSize = $disk.BlockSize
-						}
-					}
+					$total = [math]::round($disk.Capacity / $measure, 2)
+					$free = [math]::round($disk.Freespace/$measure, 2)
+					$percentfree = [math]::round(($disk.Freespace / $disk.Capacity) * 100, 2)
+
+					$diskinfo = [PSCustomObject]@{
+						Server = $server
+						Name = $diskname
+						Label = $disk.Label
+						"SizeIn$unit" = $total
+						"FreeIn$unit" = $free
+						PercentFree = $percentfree
+						BlockSize = $disk.BlockSize
+                    }
+
+                    if ($CheckForSql -or $Detailed)
+                    {
+                        Add-Member -InputObject $diskinfo -MemberType Noteproperty IsSqlDisk -value $sqldisk
+                    }
+
+                    if ($FileSystem -or $Detailed)
+                    {
+                        Add-Member -InputObject $diskinfo -MemberType Noteproperty FileSystem -value $disk.FileSystem
+                    }
+
+                    if ($CheckFragmentation)
+                    {
+                        Add-Member -InputObject $diskinfo -MemberType Noteproperty FileSystem -value $disk.FileSystem
+                    }
+                    $alldisks += $diskinfo
 				}
 			}
 			return $alldisks
 		}
-		
+
 		$collection = New-Object System.Collections.ArrayList
 		$processed = New-Object System.Collections.ArrayList
 	}
-	
+
 	PROCESS
 	{
-		
+
 		foreach ($server in $ComputerName)
 		{
 			if ($server -match '\\')
 			{
 				$server = $server.Split('\')[0]
 			}
-			
+
 			if ($server -notin $processed)
 			{
 				$null = $processed.Add($server)
@@ -197,9 +235,9 @@ Returns a custom object filled with information for server1, server2 and server3
 			{
 				continue
 			}
-			
+
 			$data = Get-AllDiskSpace $server
-			
+
 			if ($data.Count -gt 1)
 			{
 				$data.GetEnumerator() | ForEach-Object { $null = $collection.Add($_) }
@@ -210,7 +248,7 @@ Returns a custom object filled with information for server1, server2 and server3
 			}
 		}
 	}
-	
+
 	END
 	{
 		return $collection
