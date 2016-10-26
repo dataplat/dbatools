9c6ef21a761d84f9a6771b4e7f60a580fe781629
ctrlbold <clemaire@gmail.com>
2016-10-26 00:50:26 -1000

    moved to kill to be closer to the drop

--- functions/Copy-SqlLogin.ps1
+++ functions/Copy-SqlLogin.ps1
@@ -177,7 +177,6 @@ Limitations: Does not support Application Roles yet
 						Write-Output "Force was specified. Attempting to drop $username on $destination"
 						try
 						{
-							$destserver.EnumProcesses() | Where { $_.Login -eq $username } | ForEach-Object { $destserver.KillProcess($_.spid) }
 							
 							$owneddbs = $destserver.Databases | Where { $_.Owner -eq $username }
 							
@@ -197,6 +196,7 @@ Limitations: Does not support Application Roles yet
 								$ownedjob.Alter()
 							}
 							
+							$destserver.EnumProcesses() | Where-Object { $_.Login -eq $username } | ForEach-Object { $destserver.KillProcess($_.spid) }
 							$login.drop()
 							Write-Output "Successfully dropped $username on $destination"
 						}
