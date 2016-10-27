b7503e78e82a80be89073b13a189ec2773754bbd
ctrlbold <clemaire@gmail.com>
2016-10-26 23:31:23 -1000

    grammar

--- functions/Test-DbaMaxDop.ps1
+++ functions/Test-DbaMaxDop.ps1
@@ -73,6 +73,8 @@ Get Max DOP setting for servers sql2016 with the recommended value. As the -Deta
 	
 	BEGIN
 	{
+		$notesDopLT = "Before changing MaxDop, consider that the lower value may have been intentionally set."
+		$notesDopGT = "Before changing MaxDop, consider that the higher value may have been intentionally set."
 		$notesDopZero = "As a good starting point you should use the recommended setting. Then, monitor the instance and decide whether to keep or change the value."
 		$notesDopOne = "Some applications like SharePoint, Dynamics NAV, SAP, BizTalk has the need to use MAXDOP = 1. Please confirm that your instance is not supporting one of these applications prior to changing the MaxDop."
 		$notesAsRecommended = "Configuration is as recommended"
