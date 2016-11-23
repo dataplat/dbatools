d105a2d7d0f9d77164032b31801d3e85fa3bd1cd
SQLDBAWithABeard <mrrobsewell@outlook.com>
2016-11-23 14:33:11 +0000

    Added Parameter help for Confirm, No Recovery and (apprently unused) Includesupportdbs to pass Pester Tests

--- functions/Start-SqlMigration.ps1
+++ functions/Start-SqlMigration.ps1
@@ -157,6 +157,15 @@ For other migration objects, it will just drop existing items and readd, if -for
 .PARAMETER WhatIf
 Shows what would happen if the command were to run. No actions are actually performed.
 
+.PARAMETER NoRecovery
+Leaves the databases in No Recovery state to enable further backups to be added
+
+.PARAMETER IncludeSupportDbs
+Appears to not be used
+
+.PARAMETER Confirm 
+Prompts you for confirmation before executing any changing operations within the command. 
+
 .NOTES 
 Author: Chrissy LeMaire
 Limitations: 	Doesn't cover what it doesn't cover (certificates, etc)
