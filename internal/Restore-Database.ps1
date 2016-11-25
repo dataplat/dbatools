4c481b03f731bc09cd418786aa7641b8ab85be4e
ctrlbold <clemaire@gmail.com>
2016-11-25 13:22:34 +0100

    Revert "added stuff for beard"
    This reverts commit b86904118cc713c8506a3b6370d85c8665c8005e.

--- internal/Restore-Database.ps1
+++ internal/Restore-Database.ps1
@@ -7,33 +7,33 @@ Function Restore-Database
 #>
 	[CmdletBinding()]
 	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
 		[Alias("ServerInstance", "SqlInstance")]
 		[object]$SqlServer,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
 		[string]$dbname,
-		[string[]]$backupfile,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$backupfile,
 		[string]$filetype = "Database",
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
 		[object]$filestructure,
-		[switch]$norecovery,
-		[switch]$ReplaceDatabase,
-		[Alias("Tsql")]
-		[switch]$ScriptOnly,
+		[switch]$norecovery = $true,
 		[System.Management.Automation.PSCredential]$SqlCredential
 	)
 	
 	$server = Connect-SqlServer -SqlServer $SqlServer -SqlCredential $SqlCredential
 	$servername = $server.name
 	$server.ConnectionContext.StatementTimeout = 0
-	$restore = New-Object Microsoft.SqlServer.Management.Smo.Restore
-	$restore.ReplaceDatabase = $ReplaceDatabase
+	$restore = New-Object "Microsoft.SqlServer.Management.Smo.Restore"
+	$restore.ReplaceDatabase = $true
 	
-	if ($filestructure.values -ne $null)
+	foreach ($file in $filestructure.values)
 	{
-		$filestructure = $filestructure.values
-	}
-	
-	foreach ($file in $filestructure)
-	{
-		$movefile = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile
+		$movefile = New-Object "Microsoft.SqlServer.Management.Smo.RelocateFile"
 		$movefile.LogicalFileName = $file.logical
 		$movefile.PhysicalFileName = $file.physical
 		$null = $restore.RelocateFiles.Add($movefile)
@@ -41,43 +41,31 @@ Function Restore-Database
 	
 	try
 	{
+		
 		$percent = [Microsoft.SqlServer.Management.Smo.PercentCompleteEventHandler] {
 			Write-Progress -id 1 -activity "Restoring $dbname to $servername" -percentcomplete $_.Percent -status ([System.String]::Format("Progress: {0} %", $_.Percent))
 		}
-		
 		$restore.add_PercentComplete($percent)
 		$restore.PercentCompleteNotification = 1
 		$restore.add_Complete($complete)
-		$restore.ReplaceDatabase = $ReplaceDatabase
+		$restore.ReplaceDatabase = $true
 		$restore.Database = $dbname
 		$restore.Action = $filetype
 		$restore.NoRecovery = $norecovery
+		$device = New-Object -TypeName Microsoft.SqlServer.Management.Smo.BackupDeviceItem
+		$device.name = $backupfile
+		$device.devicetype = "File"
+		$restore.Devices.Add($device)
 		
-		foreach ($file in $backupfile)
-		{
-			$device = New-Object -TypeName Microsoft.SqlServer.Management.Smo.BackupDeviceItem
-			$device.name = $file
-			$device.devicetype = "File"
-			$restore.Devices.Add($device)
-		}
+		Write-Progress -id 1 -activity "Restoring $dbname to $servername" -percentcomplete 0 -status ([System.String]::Format("Progress: {0} %", 0))
+		$restore.sqlrestore($server)
+		Write-Progress -id 1 -activity "Restoring $dbname to $servername" -status "Complete" -Completed
 		
-		if ($ScriptOnly)
-		{
-			$restore.Script($server)
-		}
-		else
-		{
-			Write-Progress -id 1 -activity "Restoring $dbname to $servername" -percentcomplete 0 -status ([System.String]::Format("Progress: {0} %", 0))
-			$restore.sqlrestore($server)
-			Write-Progress -id 1 -activity "Restoring $dbname to $servername" -status "Complete" -Completed
-			
-			return "Success"
-		}
+		return $true
 	}
 	catch
 	{
-		Write-Warning $_.Exception
-		Write-Exception $_
-		return "Failed: $_"
+		Write-Error "Restore failed: $($_.Exception)"
+		return $false
 	}
 }
