bebfdac28cc2aa271907eb23584f15ff1002462f
ctrlbold <clemaire@gmail.com>
2016-11-25 14:57:47 +0100

    internal command to detect if a command is still running

---
+++ internal/Test-SqlCompletedStatement.ps1
@@ -0,0 +1,23 @@
+﻿Function Test-SqlCompletedStatement
+{
+	param (
+		[object]$server,
+		[string]$dbname,
+		[string]$sql,
+		[int]$sqlpid
+	)
+	
+	if ($sqlpid) { " and session_id = $sqlpid" }
+	$sql = $sql.Replace("'", "''")
+	
+	$testsql = "select sqltext.text FROM sys.dm_exec_requests req CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext where text = '$sql' $sqlpid"
+	
+	if ($server.ConnectionContext.ExecuteScalar($testsql) -ne $null)
+	{
+		return $false
+	}
+	else
+	{
+		return $true
+	}
+}
