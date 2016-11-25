bfeabde014a9847c626709e6700f2a52d348c15a
ctrlbold <clemaire@gmail.com>
2016-11-25 02:07:31 +0100

    moved start-dbcccheck to internal, redirected its warning output

--- functions/Test-DbaBackup.ps1
+++ functions/Test-DbaBackup.ps1
@@ -23,34 +23,6 @@ $scred = Get-Credential, then pass $scred object to the -SqlCredential parameter
 
 Windows Authentication will be used if SqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. To connect as a different Windows user, run PowerShell as that user.
 
-.PARAMETER DestinationServer
-If specified this is the server that the Agent Jobs will be created on. By default this is the same server as the SQLServer.You must have sysadmin access and server version must be SQL Server version 2000 or higher.
-
-.PARAMETER Databases
-The database name to remove or an array of database names eg $Databases = 'DB1','DB2','DB3'
-
-.PARAMETER NoDBCCCheck
-If this switch is used the initial DBCC CHECK DB will be skipped. This will make the process quicker but will also create an agent job to restore a database backup containing a corrupt database. 
-A second DBCC CHECKDB is performed on the restored database so you will still be notified BUT USE THIS WITH CARE
-
-.PARAMETER BackupFolder
-Final (Golden) Backup Folder for storing the backups. Be Careful if you are using a source and destination server that you use the full UNC path eg \\SERVER1\BACKUPSHARE\
-
-.PARAMETER JobOwner
-The account that will own the Agent Jobs - Defaults to sa
-
-.PARAMETER UseDefaultFilePaths
-Use the instance default file paths for the mdf and ldf files to restore the database if not set will use the original file paths
-
-.PARAMETER DBCCErrorFolder 
-FolderPath for DBCC Error Output - defaults to C:\temp
-
-.PARAMETER AllDatabases
-Runs the script for every user databases on a server - Useful when decomissioning a server - That would need a DestinationServer set
-
-.PARAMETER Force
-This switch will continue to perform rest of the actions and will create an Agent Job with DBCCERROR in the name and a Backup file with DBCC in the name
-
 .NOTES 
 Author: Chrissy LeMaire (@cl), netnerds.net
 Requires: sysadmin access on SQL Servers
@@ -76,46 +48,6 @@ from that backup. It will drop the database, run the agent job to restore it, pe
 
 Any DBCC errors will be written to your documents folder
 
-.EXAMPLE 
-$Databases = 'DemoNCIndex','RemoveTestDatabase'
-Test-DbaBackup -SqlServer 'Fade2Black' -Databases $Databases -BackupFolder 'C:\MSSQL\Backup\Rationalised - DO NOT DELETE'
-
-For the databases 'DemoNCIndex','RemoveTestDatabase' on the server Fade2Black Will perform a DBCC CHECKDB and if there are no errors 
-backup the database to the folder C:\MSSQL\Backup\Rationalised - DO NOT DELETE. It will then create an Agent Job for each database 
-to restore the database from that backup. It will drop the database, run the agent job, perform a DBCC ChECK DB and then drop the database
-
-Any DBCC errors will be written to your documents folder
-
-.EXAMPLE 
-Test-DbaBackup -SqlServer 'Fade2Black' -DestinationServer JusticeForAll -Databases RideTheLightning -BackupFolder '\\BACKUPSERVER\BACKUPSHARE\MSSQL\Rationalised - DO NOT DELETE'
-
-For the database RideTheLightning on the server Fade2Black Will perform a DBCC CHECKDB and if there are no errors 
-backup the database to the folder \\BACKUPSERVER\BACKUPSHARE\MSSQL\Rationalised - DO NOT DELETE It will then create an Agent Job on the server 
-JusticeForAll to restore the database from that backup. It will drop the database on Fade2Black, run the agent job to restore it on JusticeForAll, 
-perform a DBCC ChECK DB and then drop the database
-
-Any DBCC errors will be written to your documents folder
-.EXAMPLE 
-Test-DbaBackup -SqlServer IronMaiden -Databases $Databases -DestinationServer TheWildHearts -DBCCErrorFolder C:\DBCCErrors -BackupFolder z:\Backups -NoDBCCCheck -UseDefaultFilePaths -JobOwner 'THEBEARD\Rob' 
-
-For the databases $Databases on the server IronMaiden Will NOT perform a DBCC CHECKDB 
-It will backup the databases to the folder Z:\Backups It will then create an Agent Job on the server with a Job Owner of THEBEARD\Rob 
-TheWildHearts to restore the database from that backup using the instance default filepaths. 
-It will drop the database on IronMaiden, run the agent job to restore it on TheWildHearts using the default file paths for the instance, perform 
-a DBCC ChECK DB and then drop the database
-
-Any DBCC errors will be written to your documents folder
-
-.EXAMPLE 
-Test-DbaBackup -SqlServer IronMaiden -Databases $Databases -DestinationServer TheWildHearts -DBCCErrorFolder C:\DBCCErrors -BackupFolder z:\Backups -UseDefaultFilePaths -ContinueAfterDbccError
-
-For the databases $Databases on the server IronMaiden will backup the databases to the folder Z:\Backups It will then create an Agent Job
-TheWildHearts to restore the database from that backup using the instance default filepaths. 
-It will drop the database on IronMaiden, run the agent job to restore it on TheWildHearts using the default file paths for the instance, perform 
-a DBCC ChECK DB and then drop the database
-
-If there is a DBCC Error it will continue to perform rest of the actions and will create an Agent Job with DBCCERROR in the name and a Backup file with DBCCError in the name
-
 
 #>
 	[CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = "Default")]
@@ -125,7 +57,7 @@ If there is a DBCC Error it will continue to perform rest of the actions and wil
 		[object]$SqlServer,
 		[object]$SqlCredential,
 		[object]$Destination = $SqlServer,
-		[string]$BackupPath,
+		[string]$Path,
 		[string]$BackupsDirectory,
 		[string]$DataDirectory,
 		[string]$LogDirectory,
@@ -179,34 +111,8 @@ If there is a DBCC Error it will continue to perform rest of the actions and wil
 		{
 			$databases = $sourceserver.databases.Name
 		}
-		
-		Function Start-DbccCheck
-		{
-			param (
-				[object]$server,
-				[string]$dbname
-			)
-			
-			$servername = $server.name
-			$db = $server.databases[$dbname]
-			
-			if ($Pscmdlet.ShouldProcess($sourceserver, "Running dbcc check on $dbname on $servername"))
-			{
-				try
-				{
-					$null = $db.CheckTables('None')
-					Write-Verbose "Dbcc CHECKDB finished successfully for $dbname on $servername"
-					return "Success"
-				}
-				catch
-				{
-					Write-Exception $_ -WarningAction SilentlyCOntinue
-					$inner = $_.Exception.Message
-					return "Failure: $inner"
-				}
-			}
-		}
 	}
+	
 	PROCESS
 	{
 		<#
@@ -299,7 +205,7 @@ If there is a DBCC Error it will continue to perform rest of the actions and wil
 					if ($Pscmdlet.ShouldProcess($dbname, "Running Dbcc CHECKDB on $dbname on $destination"))
 					{
 						Write-Verbose "Starting Dbcc CHECKDB for $dbname on $destination"
-						$dbccresult = Start-DbccCheck -Server $destserver -DbName $dbname
+						$dbccresult = Start-DbccCheck -Server $destserver -DbName $dbname 3>$null
 					}
 					
 					if ($Pscmdlet.ShouldProcess($dbname, "Dropping Database $dbname on $destination"))
---
+++ internal/Start-DbccCheck.ps1
@@ -0,0 +1,26 @@
+﻿Function Start-DbccCheck
+{
+	param (
+		[object]$server,
+		[string]$dbname
+	)
+	
+	$servername = $server.name
+	$db = $server.databases[$dbname]
+	
+	if ($Pscmdlet.ShouldProcess($sourceserver, "Running dbcc check on $dbname on $servername"))
+	{
+		try
+		{
+			$null = $db.CheckTables('None')
+			Write-Verbose "Dbcc CHECKDB finished successfully for $dbname on $servername"
+			return "Success"
+		}
+		catch
+		{
+			Write-Exception $_
+			$inner = $_.Exception.Message
+			return "Failure: $inner"
+		}
+	}
+}
