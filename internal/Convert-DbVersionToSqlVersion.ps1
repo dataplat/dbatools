dd8ecceea298d6210c92034dda9a9666610cee8e
ctrlbold <clemaire@gmail.com>
2016-11-25 12:52:19 +0100

    updated to be prettier. Thanks @PowerDBAKlaas !

--- internal/Convert-DbVersionToSqlVersion.ps1
+++ internal/Convert-DbVersionToSqlVersion.ps1
@@ -4,22 +4,23 @@
 		[string]$dbversion
 	)
 	
-	switch ($dbversion)
+	$dbversion = switch ($dbversion)
 	{
-		856 { $dbversion = "SQL Server vNext CTP1" }
-		852 { $dbversion = "SQL Server 2016" }
-		829 { $dbversion = "SQL Server 2016 Prerelease" }
-		782 { $dbversion = "SQL Server 2014" }
-		706 { $dbversion = "SQL Server 2012" }
-		684 { $dbversion = "SQL Server 2012 CTP1" }
-		661 { $dbversion = "SQL Server 2008 R2" }
-		660 { $dbversion = "SQL Server 2008 R2" }
-		655 { $dbversion = "SQL Server 2008 SP2+" }
-		612 { $dbversion = "SQL Server 2005" }
-		611 { $dbversion = "SQL Server 2005" }
-		539 { $dbversion = "SQL Server 2000" }
-		515 { $dbversion = "SQL Server 7.0" }
-		408 { $dbversion = "SQL Server 6.5" }
+		856 { "SQL Server vNext CTP1" }
+		852 { "SQL Server 2016" }
+		829 { "SQL Server 2016 Prerelease" }
+		782 { "SQL Server 2014" }
+		706 { "SQL Server 2012" }
+		684 { "SQL Server 2012 CTP1" }
+		661 { "SQL Server 2008 R2" }
+		660 { "SQL Server 2008 R2" }
+		655 { "SQL Server 2008 SP2+" }
+		612 { "SQL Server 2005" }
+		611 { "SQL Server 2005" }
+		539 { "SQL Server 2000" }
+		515 { "SQL Server 7.0" }
+		408 { "SQL Server 6.5" }
+		default { $dbversion }
 	}
 	
 	return $dbversion
