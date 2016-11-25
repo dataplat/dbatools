f8d1f7a7530d3425f99000de04e6293607e0b35d
ctrlbold <clemaire@gmail.com>
2016-11-25 12:01:02 +0100

    used global param instead

--- functions/Read-DbaBackupHeader.ps1
+++ functions/Read-DbaBackupHeader.ps1
@@ -129,25 +129,7 @@ Also returns detailed information about each of the datafiles contained in the b
 			$version = $datatable.Columns.Add("SQLVersion")
 			$dbversion = $datatable.rows[0].DatabaseVersion
 			
-			switch ($dbversion)
-			{
-				856 { $dbversion = "SQL Server vNext CTP1" }
-				852 { $dbversion = "SQL Server 2016" }
-				829 { $dbversion = "SQL Server 2016 Prerelease" }
-				782 { $dbversion = "SQL Server 2014" }
-				706 { $dbversion = "SQL Server 2012" }
-				684 { $dbversion = "SQL Server 2012 CTP1" }
-				661 { $dbversion = "SQL Server 2008 R2" }
-				660 { $dbversion = "SQL Server 2008 R2" }
-				655 { $dbversion = "SQL Server 2008 SP2+" }
-				612 { $dbversion = "SQL Server 2005" }
-				611 { $dbversion = "SQL Server 2005" }
-				539 { $dbversion = "SQL Server 2000" }
-				515 { $dbversion = "SQL Server 7.0" }
-				408 { $dbversion = "SQL Server 6.5" }
-			}
-			
-			$datatable.rows[0].SQLVersion = $dbversion
+			$datatable.rows[0].SQLVersion = (Convert-DbVersionToSqlVersion $dbversion)
 			
 			if ($Simple)
 			{
