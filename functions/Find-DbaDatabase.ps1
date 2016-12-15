bc3a448ee1db7a3c3f56c598c6bcb49f16bd3024
Staggerlee011 <stebennettsjb@gmail.com>
2016-12-15 09:27:17 +0000

    Find-DbaDatabase
    v1

---
+++ functions/Find-DbaDatabase.ps1
@@ -0,0 +1,37 @@
+﻿FUNCTION Find-DbaDatabase
+{
+	[CmdletBinding()]
+	Param (
+		[parameter(Position = 0, Mandatory = $true)]
+		[Alias("ServerInstance", "SqlInstance", "SqlServers")]
+		[string[]]$SqlServer,
+		[System.Management.Automation.PSCredential]$SqlCredential,
+        [string]$Database
+	)
+	BEGIN 
+        {
+
+        }
+    PROCESS
+        {
+            
+            FOREACH ($server in $SqlServer)
+                {
+                    TRY
+			            {
+				            Write-Verbose "Connecting to $server"
+                            $server = Connect-SqlServer -SqlServer $SqlServer -SqlCredential $sqlcredential
+			            }
+		            CATCH
+			            {
+				            Write-Warning "Failed to connect to: $server"
+                            break
+			            }
+                    #conn to db list
+                }
+        }
+    END
+        {
+
+        }
+}
