55656e42f930d15412c9ed7a8509045985af31da
ctrlbold <clemaire@gmail.com>
2016-12-17 03:13:54 +0100

    added progress bar and made table locks default. no idea if any of this works.

--- functions/Write-DbaData.ps1
+++ functions/Write-DbaData.ps1
@@ -13,6 +13,40 @@ The SQL Server that you're connecting to.
 .PARAMETER SqlCredential
 Credential object used to connect to the SQL Server as a different user be it Windows or SQL Server. Windows users are determiend by the existence of a backslash, so if you are intending to use an alternative Windows connection instead of a SQL login, ensure it contains a backslash.
 
+.PARAMETER Data
+Info
+	
+.PARAMETER Schema
+Info
+	
+.PARAMETER Table
+Info
+	
+.PARAMETER BatchSize
+Info
+	
+.PARAMETER NotifyAfter
+Info
+	
+.PARAMETER NoTableLock
+Info
+		
+.PARAMETER CheckConstraints
+Info
+		
+.PARAMETER FireTriggers
+Info
+		
+.PARAMETER KeepIdentity
+Info
+		
+.PARAMETER KeepNulls
+Info
+	
+		
+.PARAMETER Trucnate
+Info
+	
 	
 .NOTES
 dbatools PowerShell module (https://dbatools.io)
@@ -27,7 +61,7 @@ You should have received a copy of the GNU General Public License along with thi
 .EXAMPLE
 Write-DbaData -SqlServer sql2014
 
-Creates an SMO Server object that connects using Windows Authentication
+Info
 	
 #>	
 	[CmdletBinding(SupportsShouldProcess = $true)]
@@ -43,8 +77,7 @@ Creates an SMO Server object that connects using Windows Authentication
 		[string]$Table,
 		[int]$BatchSize = 50000,
 		[int]$NotifyAfter,
-		[switch]$TableLock,
-		# change to $NoTableLock?
+		[switch]$NoTableLock,
 		[switch]$CheckConstraints,
 		[switch]$FireTriggers,
 		[switch]$KeepIdentity,
@@ -55,9 +88,130 @@ Creates an SMO Server object that connects using Windows Authentication
 	
 	DynamicParam { if ($sqlserver) { return Get-ParamSqlDatabase -SqlServer $SqlServer -SqlCredential $SqlCredential } }
 	
+	BEGIN
+	{
+		# Getting the total rows copied is a challenge. Use SqlBulkCopyExtension.
+		# http://stackoverflow.com/questions/1188384/sqlbulkcopy-row-count-when-complete
+		
+		$source = 'namespace System.Data.SqlClient
+		{    
+			using Reflection;
+
+			public static class SqlBulkCopyExtension
+			{
+				const String _rowsCopiedFieldName = "_rowsCopied";
+				static FieldInfo _rowsCopiedField = null;
+
+				public static int RowsCopiedCount(this SqlBulkCopy bulkCopy)
+				{
+					if (_rowsCopiedField == null) _rowsCopiedField = typeof(SqlBulkCopy).GetField(_rowsCopiedFieldName, BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);            
+					return (int)_rowsCopiedField.GetValue(bulkCopy);
+				}
+			}
+		}'
+		
+		Add-Type -ReferencedAssemblies 'System.Data.dll' -TypeDefinition $source -ErrorAction SilentlyContinue
+
+		$tablelock = $NoTableLock -eq $false
+	}
+	
 	PROCESS
 	{
-	<#
+		$database = $psboundparameters.Database
+		$dotcount = ([regex]::Matches($table, ".")).count
+		
+		if ($dotcount -eq 1)
+		{
+			$schema = $Table.Split(".")[0]
+			$table = $Table.Split(".")[1]
+		}
+		
+		if ($dotcount -eq 2)
+		{
+			$database = $Table.Split(".")[0]
+			$schema = $Table.Split(".")[1]
+			$table = $Table.Split(".")[2]
+		}
+		
+		$fqtn = "$database.$Schema.$table"
+		
+		$validtypes = @([System.Data.Common.DbDataReader], [System.Data.DataTable], [System.Data.DataRow[]], [System.Data.IDataReader])
+		
+		if ($data.GetType() -notin $validtypes)
+		{
+			Write-Warning "Data is not of the right type (DbDataReader, DataTable, DataRow, or IDataReader)."
+			continue
+		}
+		
+		$server = Connect-SqlServer-SqlServer $SqlServer -SqlCredential $SqlCredential
+		
+		if ($database.length -gt 0) { $server.ConnectionContext.DatabaseName = $Database }
+		
+		$tableExists = $server.Databases[$Database] | Where-Object { $_.Tables.Name -eq 'allcountries' -and $_.Tables.Schema -eq 'dbo' }
+		
+		
+		if ($tableExists -eq $null)
+		{
+			Write-Warning "$fqtn does not exist"
+			continue
+		}
+		
+		$bulkCopyOptions = @()
+		$options = "TableLock", "CheckConstraints", "FireTriggers", "KeepIdentity", "KeepNulls", "Default", "Truncate"
+		
+		foreach ($option in $options)
+		{
+			$optionValue = Get-Variable $option -ValueOnly -ErrorAction SilentlyContinue
+			if ($optionValue -eq $true) { $bulkCopyOptions += "$option" }
+		}
+		
+		$bulkCopyOptions = $bulkCopyOptions -join " & "
+		
+		if ($Pscmdlet.ShouldProcess($SqlServer, "Truncating $fqtn"))
+		{
+			if ($truncate -eq $true)
+			{
+				try
+				{
+					$null = $server.Databases[$database].ExecuteNonQuery("TRUNCATE TABLE [$fqtn]")
+				}
+				catch
+				{
+					Write-Warning "Could not truncate $fqtn"
+				}
+			}
+		}
+		
+		# nooooo idea if this'll work with pipelines. May have to move to end.
+		if ($Pscmdlet.ShouldProcess($SqlServer, "Writing data to server"))
+		{
+			$bulkcopy = New-Object Data.SqlClient.SqlBulkCopy($server, $bulkCopyOptions)
+			$bulkcopy.DestinationTableName = $fqtn
+			$bulkcopy.BatchSize = $batchsize
+			
+			$resultcount = $datatable.rows.count
+			# Add rowcount output
+			$bulkCopy.Add_SqlRowscopied({
+					$script:totalrows = $args[1].RowsCopied
+					$resultcount = $datatable.rows.count
+					$percent = [int](($script:totalrows/$resultcount) * 100)
+					$timetaken = [math]::Round($elapsed.Elapsed.TotalSeconds, 2)
+					Write-Progress -id 1 -activity "Inserting $resultcount rows" -percentcomplete $percent `
+								   -status ([System.String]::Format("Progress: {0} rows ({1}%) in {2} seconds", $script:totalrows, $percent, $timetaken))
+					
+				})
+			
+			$bulkCopy.WriteToServer($data)
+			if ($resultcount -is [int]) { Write-Progress -id 1 -activity "Inserting $resultcount rows" -status "Complete" -Completed }
+			
+		}
+		$bulkcopy.Close()
+		$bulkcopy.Dispose()
+	}
+}
+
+##### DONE
+		<#
 		function New-SqlTable # Can be improved but i'm bizzy, mmm actually, will implement later
 		{
 			$Data.Tables.Rows[0]
@@ -106,79 +260,3 @@ Creates an SMO Server object that connects using Windows Authentication
 			}
 		}
 	#>
-		
-		$database = $psboundparameters.Database
-		$dotcount = ([regex]::Matches($table, ".")).count
-		
-		if ($dotcount -eq 1)
-		{
-			$schema = $Table.Split(".")[0]
-			$table = $Table.Split(".")[1]
-		}
-		
-		if ($dotcount -eq 2)
-		{
-			$database = $Table.Split(".")[0]
-			$schema = $Table.Split(".")[1]
-			$table = $Table.Split(".")[2]
-		}
-		
-		$fqtn = "$database.$Schema.$table"
-		
-		$validtypes = @([System.Data.Common.DbDataReader], [System.Data.DataTable], [System.Data.DataRow[]], [System.Data.IDataReader])
-		
-		if ($data.GetType() -notin $validtypes)
-		{
-			Write-Warning "Data is not of the right type (DbDataReader, DataTable, DataRow, or IDataReader)."
-			continue
-		}
-		
-		$server = Connect-SqlServer-SqlServer $SqlServer -SqlCredential $SqlCredential
-		
-		if ($database.length -gt 0) { $server.ConnectionContext.DatabaseName = $Database }
-		
-		$tableExists = $server.Databases[$Database] | Where-Object { $_.Tables.Name -eq 'allcountries' -and $_.Tables.Schema -eq 'dbo' }
-		
-		if ($tableExists -eq $null)
-		{
-			Write-Warning "$fqtn does not exist"
-			continue
-		}
-		
-		$bulkCopyOptions = @()
-		$options = "TableLock", "CheckConstraints", "FireTriggers", "KeepIdentity", "KeepNulls", "Default", "Truncate"
-		
-		foreach ($option in $options)
-		{
-			$optionValue = Get-Variable $option -ValueOnly -ErrorAction SilentlyContinue
-			if ($optionValue -eq $true) { $bulkCopyOptions += "$option" }
-		}
-		
-		$bulkCopyOptions = $bulkCopyOptions -join " & "
-		
-		if ($Pscmdlet.ShouldProcess($SqlServer, "Truncating $fqtn"))
-		{
-			if ($truncate -eq $true)
-			{
-				try
-				{
-					$null = $server.Databases[$database].ExecuteNonQuery("TRUNCATE TABLE [$fqtn]")
-				}
-				catch
-				{
-					Write-Warning "Could not truncate $fqtn"
-				}
-			}
-		}
-		
-		if ($Pscmdlet.ShouldProcess($SqlServer, "Writing data to server"))
-		{
-			$bulkcopy = New-Object Data.SqlClient.SqlBulkCopy($server, $bulkCopyOptions)
-			$bulkcopy.DestinationTableName = $fqtn
-			$bulkcopy.BatchSize = $batchsize
-			$bulkcopy.WriteToServer($data)
-			$bulkcopy.Close()
-			$bulkcopy.Dispose()
-		}
-	}
-}
