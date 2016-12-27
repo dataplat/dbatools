a0f7948c37efe1088e71a7cf7ec9d46caf36b2ba
ctrlbold <clemaire@gmail.com>
2016-12-27 18:13:12 +0100

    added support for dynamic params, added docs

--- functions/Get-DbaXEventsSession.ps1
+++ functions/Get-DbaXEventsSession.ps1
@@ -1,4 +1,5 @@
-﻿ function Get-DbaXEventsSession {
+﻿function Get-DbaXEventsSession
+{
  <#
 .SYNOPSIS
 Get a list of Extended Events Sessions
@@ -11,6 +12,9 @@ The SQL Instances that you're connecting to.
 
 .PARAMETER Credential
 Credential object used to connect to the SQL Server as a different user
+
+.PARAMETER Sessions
+Only return specific sessions. This parameter is auto-populated.
 
 .NOTES
 Author: Klaas Vandenberghe ( @PowerDBAKlaas )
@@ -40,69 +44,82 @@ Returns a formatted table displaying ComputerName, SQLInstance, Session, and Sta
 
 Returns a custom object with ComputerName, SQLInstance, Session, StartTime, Status and other properties, from multiple SQL Instances.
 #>
-    [CmdletBinding()]
-    param (
+	[CmdletBinding()]
+	param (
 		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[Alias("ServerInstance", "SqlServer")]
 		[string[]]$SqlInstance,
 		[PsCredential]$SqlCredential
-    )
-    BEGIN {
-        if ([System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.XEvent") -eq $null)
+	)
+	
+	DynamicParam { if ($SqlInstance) { return (Get-ParamSqlExtendedEvents -SqlServer $SqlInstance[0] -SqlCredential $SqlCredential) } }
+	
+	BEGIN
+	{
+		if ([System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.XEvent") -eq $null)
 		{
 			throw "SMO version is too old. To collect Extended Events, you must have Shared Management Objects 2008 R2 or higher installed."
 		}
-    }
-    PROCESS {
-        foreach ( $instance in $SqlInstance )
-        {
-            Write-Verbose "Connecting to $instance ."
+		$sessions = $psboundparameters.Sessions
+	}
+	PROCESS
+	{
+		foreach ($instance in $SqlInstance)
+		{
+			Write-Verbose "Connecting to $instance ."
 			try
 			{
 				$server = Connect-SqlServer -SqlServer $instance -SqlCredential $Credential -ErrorAction SilentlyContinue
-                Write-Verbose "SQL Instance $instance is version $($server.versionmajor) ."
-            }
-            catch
-            {
-                Write-Warning " Failed to connect to $instance ."
-                continue
-            }
-            if($server.versionmajor -ge 11)
-            {
-		        $SqlConn = $server.ConnectionContext.SqlConnectionObject
-		        $SqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $SqlConn
-		        $XEStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $SqlStoreConnection
-                Write-Verbose "Getting XEvents Sessions on $instance ."
-
-                try
-                {
-                $XEStore.sessions | 
-                    ForEach-Object {
-                        [PSCustomObject]@{
-                            ComputerName = $server.NetName
-                            SQLInstance = $server.ServiceName
-                            Session = $_.Name
-                            Status = switch ( $_.IsRunning ) { $true {"Running"} $false {"Stopped"} }
-                            StartTime = $_.StartTime
-                            AutoStart = $_.AutoStart
-                            State = $_.State
-                            Targets = $_.Targets
-                            Events = $_.Events
-                            MaxMemory = $_.MaxMemory
-                            MaxEventSize = $_.MaxEventSize
-                            }
-                    }
-	            }
-                catch
-                {
-                Write-Warning "Failed to get XEvents Sessions on $instance ."
-                }
-            }
-            else
-            {
-                Write-Warning "SQL Instance $instance is SQL Version $($server.versionmajor) . This is not supported."
-            }
-        }
-    }
-    END {}
+				Write-Verbose "SQL Instance $instance is version $($server.versionmajor) ."
+			}
+			catch
+			{
+				Write-Warning " Failed to connect to $instance ."
+				continue
+			}
+			if ($server.versionmajor -ge 11)
+			{
+				$SqlConn = $server.ConnectionContext.SqlConnectionObject
+				$SqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $SqlConn
+				$XEStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $SqlStoreConnection
+				Write-Verbose "Getting XEvents Sessions on $instance ."
+				
+				$xesessions = $XEStore.sessions
+				
+				if ($sessions)
+				{
+					$xesessions = $xesessions | Where-Object { $_.Name -in $sessions }
+				}
+				
+				try
+				{
+					$xesessions |
+					ForEach-Object {
+						[PSCustomObject]@{
+							ComputerName = $server.NetName
+							SQLInstance = $server.ServiceName
+							Session = $_.Name
+							Status = switch ($_.IsRunning) { $true { "Running" } $false { "Stopped" } }
+							StartTime = $_.StartTime
+							AutoStart = $_.AutoStart
+							State = $_.State
+							Targets = $_.Targets
+							Events = $_.Events
+							MaxMemory = $_.MaxMemory
+							MaxEventSize = $_.MaxEventSize
+						}
+					}
+				}
+				catch
+				{
+					Write-Warning "Failed to get XEvents Sessions on $instance ."
+				}
+			}
+			else
+			{
+				Write-Warning "SQL Instance $instance is SQL Version $($server.versionmajor) . This is not supported."
+			}
+		}
+	}
+	END { }
 }
