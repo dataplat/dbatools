82f47aa763603fbf87c812dea05bfec3882fe5ac
ctrlbold <clemaire@gmail.com>
2016-12-28 12:58:40 +0100

    changed things, added things, need to rename file next

--- functions/Get-DBAUptime.ps1
+++ functions/Get-DBAUptime.ps1
@@ -1,4 +1,5 @@
-function Get-DbaUptime {
+function Get-DbaUptime
+{
 <#
 .SYNOPSIS
 Returns the uptime of the SQL Server instance, and if required the hosting windows server
@@ -17,11 +18,15 @@ Credential object used to connect to the SQL Server as a different user
 .PARAMETER WindowsCredential
 Credential object used to connect to the SQL Server as a different user
 
-.PARAMETER SQLOnly
-Excludes the windows server information
+.PARAMETER SqlOnly
+Excludes the Windows server information
+
+.PARAMETER WindowsOnly
+Excludes the SQL server information
 
 .NOTES 
 Original Author: Stuart Moore (@napalmgram), stuart-moore.com
+	
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
 Copyright (C) 2016 Chrissy LeMaire
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -29,7 +34,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 .LINK
-https://dbatools.io/Get-DBAUptime
+https://dbatools.io/Get-DbaUptime
 
 .EXAMPLE
 Get-DbaUptime -SqlServer SqlBox1\Instance2
@@ -60,122 +65,113 @@ Returns an object with SQL Server start time, uptime as TimeSpan object, uptime 
 		[Alias("SqlCredential")]
 		[PsCredential]$Credential,
 		[PsCredential]$WindowsCredential,
-		[Switch]$SQLOnly
-
+		[Switch]$SqlOnly,
+		[Switch]$WindowsOnly
+		
 	)
-
-    	BEGIN
-	{
-        $functionname = "Get-DBAUptime"
-		$collection = New-Object System.Collections.ArrayList
-	}
-    	PROCESS
+	
+	PROCESS
 	{
 		foreach ($servername in $SqlServer)
 		{
-            write-verbose "$functionname - server = $servername"
-		Write-Verbose "Connecting to $SqlServer"
-		try
-		{
-			$server = Connect-SqlServer -SqlServer $SqlServer -SqlCredential $Credential -ErrorVariable ConnectError
-			
-		}
-		catch
-		{
-			Write-Warning $_
-			continue
-		}
-			
-			if ($server.VersionMajor -lt 9)
+			if ($WindowsOnly -ne $true)
 			{
-				if ($servercount -eq 1)
+				
+				Write-Verbose "Connecting to $servername"
+				try
 				{
-					throw "SQL Server 2000 not supported."
+					$server = Connect-SqlServer -SqlServer $SqlServer -SqlCredential $Credential -ErrorVariable ConnectError
+					
 				}
-				else
+				catch
+				{
+					Write-Warning $_
+					continue
+				}
+				
+				if ($server.VersionMajor -lt 9)
 				{
 					Write-Warning "SQL Server 2000 not supported. Skipping $servername."
 					Continue
 				}
+				
+				#Get TempDB creation date
+				$SQLStartTime = $server.Databases["TempDB"].CreateDate
+				$SQLUptime = New-TimeSpan -start $SQLStartTime -end (get-date)
+				$SQLUptimeString = "{0} days {1} hours {2} minutes {3} seconds" -f $($SQLUptime.Days), $($SQLUptime.Hours), $($SQLUptime.Minutes), $($SQLUptime.Seconds)
 			}
-                #Get TempDB creation date
-                $SQLStartTime = $server.Databases["TempDB"].CreateDate
-                $SQLUptime = New-TimeSpan  -start $SQLStartTime -end  (get-date)
-				$SQLUptimeString =  "{0} days {1} hours {2} minutes {3} seconds" -f $($SQLUptime.Days), $($SQLUptime.Hours), $($SQLUptime.Minutes), $($SQLUptime.Seconds)
+			
+			if ($SqlOnly -ne $true)
+			{
+				$WindowsServerName = (Resolve-DbaNetworkName $servername).ComputerName
 
-
-				if ($SQLOnly -ne $true)
+				try
 				{
-					$ClusterCheck = Get-DbaClusterActiveNode -SqlServer $servername
-					if ($ClusterCheck -eq 'Not a clustered instance' )
+					Write-Verbose "Getting WinBootTime via CimInstance for $servername"
+					$WinBootTime = (Get-CimInstance -ClassName win32_operatingsystem -ComputerName $windowsServerName).lastbootuptime
+					$WindowsUptime = New-TimeSpan -start $WinBootTime -end (get-date)
+					$WindowsUptimeString = "{0} days {1} hours {2} minutes {3} seconds" -f $($WindowsUptime.Days), $($WindowsUptime.Hours), $($WindowsUptime.Minutes), $($WindowsUptime.Seconds)
+					
+				}
+				catch
+				{
+					try
 					{
-						$WindowsServerName = ($servername.split("\"))[0]
-					}
-					else
-					{
-						$WindowsServerName = $ClusterCheck
-					}
-					$CimError = 0
-					try {
-						Write-Verbose "$functionname - Getting WinBootTime via CimInstance"
-						$WinBootTime = (Get-CimInstance -ClassName win32_operatingsystem -ComputerName $windowsServerName).lastbootuptime
+						Write-Verbose "$functionname - Getting WinBootTime via CimInstance DCOM"
+						$CimOption = New-CimSessionOption -Protocol DCOM
+						$CimSession = New-CimSession -Credential:$WindowsCredential -ComputerName $WindowsServerName -SessionOption $CimOption
+						$WinBootTime = ($CimSession | Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 						$WindowsUptime = New-TimeSpan -start $WinBootTime -end (get-date)
 						$WindowsUptimeString = "{0} days {1} hours {2} minutes {3} seconds" -f $($WindowsUptime.Days), $($WindowsUptime.Hours), $($WindowsUptime.Minutes), $($WindowsUptime.Seconds)
 						
 					}
-					catch [System.Exception] {
+					catch
+					{
 						Write-Exception $_
-						$CimError += 1
-
-					}
-					if ($CimError -gt 0)
-					{
-						try {
-							Write-Verbose "$functionname - Getting WinBootTime via CimInstance DCOM"
-							$CimOption = New-CimSessionOption -Protocol DCOM
-							$CimSession = New-CimSession -Credential:$WindowsCredential -ComputerName $WindowsServerName -SessionOption $CimOption
-							$WinBootTime = ($CimSession | Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
-							$WindowsUptime = New-TimeSpan -start $WinBootTime -end (get-date)
-							$WindowsUptimeString = "{0} days {1} hours {2} minutes {3} seconds" -f $($WindowsUptime.Days), $($WindowsUptime.Hours), $($WindowsUptime.Minutes), $($WindowsUptime.Seconds)
-
-						}
-						catch [System.Exception] {
-							Write-Exception $_
-							$CimError += 1
-						}
-					}
-					if ($WindowsUptimeString -eq '')
-					{
-						#Skip the windows results as they'll either be garbage or not there.
-						$SqlOnly = $true
 					}
 				}
-				if ($SQLOnly -eq $true)
+				
+				if ($null -eq $WinBootTime)
 				{
-					$null = $collection.Add([PSCustomObject]@{
-							SQLServer = $servername
-							SQLStartTime = $SQLStartTime
-							SQLUptimeString = $SQLUptimeString
-							SQLUptime = $SQLUptime
-					})
-				}else{
-					[PSCustomObject]@{
-							SQLServer = $server.Name
-							InstanceName = $server.ServiceName
-							ComputerName = $server.NetName
-							SQLStartTime = $SQLStartTime
-							SQLUptimeString = $SQLUptimeString
-							SQLUptime = $SQLUptime
-							WindowsBootTime = $WinBootTime
-							WindowsUptime = $WindowsUptime
-							WindowsUptimeString = $WindowsUptimeString
-					}
+					#Skip the windows results as they'll either be garbage or not there.
+					$SqlOnly = $true
 				}
-
-        }
-    }
-    END 
-    {
-            return 
-    }
+			}
+			
+			if ($SqlOnly -eq $true)
+			{
+				[PSCustomObject]@{
+					InstanceName = $server.ServiceName
+					ComputerName = $server.NetName
+					SqlInstance = $server.Name
+					SqlStartTime = $SQLStartTime
+					SqlUptimeString = $SQLUptimeString
+					SqlUptime = $SQLUptime
+				}
+			}
+			elseif ($WindowsOnly -eq $true)
+			{
+				[PSCustomObject]@{
+					ComputerName = $WindowsServerName
+					WindowsBootTime = $WinBootTime
+					WindowsUptime = $WindowsUptime
+					WindowsUptimeString = $WindowsUptimeString
+				}
+			}
+			else
+			{
+				[PSCustomObject]@{
+					InstanceName = $server.ServiceName
+					ComputerName = $WindowsServerName
+					SqlInstance = $server.Name
+					SqlStartTime = $SQLStartTime
+					SqlUptimeString = $SQLUptimeString
+					SqlUptime = $SQLUptime
+					WindowsBootTime = $WinBootTime
+					WindowsUptime = $WindowsUptime
+					WindowsUptimeString = $WindowsUptimeString
+				}
+			}
+		}
+	}
 }
