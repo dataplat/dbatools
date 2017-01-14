45f67ab04bc865402e6bdd409cbf2f724706e8ee
SQLDBAWithABeard <mrrobsewell@outlook.com>
2017-01-14 13:37:26 +0000

    Added loading internal function

--- tests/Get-DbaJobOutputFile.Tests.ps1
+++ tests/Get-DbaJobOutputFile.Tests.ps1
@@ -14,12 +14,13 @@ if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
 
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
 Import-Module $PSScriptRoot\..\functions\$sut -Force
+. $PSScriptRoot\..\Internal\Connect-SQLServer.ps1 -Force
 Import-Module PSScriptAnalyzer
 ## Added PSAvoidUsingPlainTextForPassword as credential is an object and therefore fails. We can ignore any rules here under special circumstances agreed by admins :-)
 $Rules = (Get-ScriptAnalyzerRule).Where{$_.RuleName -notin ('PSAvoidUsingPlainTextForPassword') }
 $Name = $sut.Split('.')[0]
 
-  <#  Describe 'Script Analyzer Tests' -Tag @('ScriptAnalyzer'){ 
+   Describe 'Script Analyzer Tests' -Tag @('ScriptAnalyzer'){ 
             Context 'Testing $sut for Standard Processing' {
                 foreach ($rule in $rules) { 
                     $i = $rules.IndexOf($rule)
@@ -28,16 +29,16 @@ $Name = $sut.Split('.')[0]
                     }
                 }
             }
-        } #>
+        } 
    ## needs some proper tests for the function here
     Describe "$Name Tests" -Tag @('Command'){
         Context "Input Validation" { 
           It 'SqlServer parameter is empty' { 
-            { Get-DbaAgentJobOutputFile -SqlServer ''  -WarningAction Stop 3> $null } | Should Throw 
+            { Get-DbaJobOutputFile -SqlServer ''  -WarningAction Stop 3> $null } | Should Throw 
          } 
           It 'SqlServer parameter host cannot be found' { 
             Mock Connect-SqlServer { throw System.Data.SqlClient.SqlException } 
-            { Get-DbaMaxMemory -SqlServer 'ABC' -WarningAction Stop 3> $null } | Should Throw 
+            { Get-DbaJobOutputFile  -SqlServer 'ABC' -WarningAction Stop 3> $null } | Should Throw 
          } 
 		} ## End Context Input
     }
