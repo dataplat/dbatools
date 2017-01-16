93be6a82994916e08167721d41bd0aa93c7c895b
Staggerlee011 <stebennettsjb@gmail.com>
2017-01-16 09:10:54 +0000

    revision
    missed help section off.

--- functions/Find-DbaLoginInGroup.ps1
+++ functions/Find-DbaLoginInGroup.ps1
@@ -1,5 +1,46 @@
 ﻿function Find-DbaLoginInGroup
 {
+<#
+.SYNOPSIS
+Finds Logins in Active Directory groups. 
+
+.DESCRIPTION
+Outputs all the active directory groups 
+	
+.NOTES 
+Original Author: Stephen Bennett, https://sqlnotesfromtheunderground.wordpress.com/
+
+dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+Copyright (C) 2016 Chrissy LeMaire
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+.PARAMETER SqlServer
+SQLServer name or SMO object representing the SQL Server to connect to. This can be a
+collection and recieve pipeline input.
+
+.PARAMETER SqlCredential
+PSCredential object to connect under. If not specified, currend Windows login will be used.
+
+.PARAMETER Login
+Find all AD Groups used on the instance that an individual login is a memeber of.
+	
+.LINK
+https://dbatools.io/Find-DbaLoginInGroup
+
+.EXAMPLE
+Find-DbaLoginInGroup -SqlInstance DEV01 -Login "MyDomain\Stephen.Bennett"
+
+Returns all active directory groups with logins on Sql Instance DEV01 that contain the AD user Stephen.Bennett.
+
+#>
 	[CmdletBinding()]
 	Param (
 		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
@@ -33,7 +74,7 @@
 	        catch
 	        {
 	            Write-Warning "Failed to connect to: $Instance"
-                break
+                continue
 	        }
 
             $AdGroups = $server.Logins | Where {$_.LoginType -eq "WindowsGroup" -and $_.Name -ne "BUILTIN\Administrators" -and $_.Name -notlike "*NT SERVICE*"}
@@ -76,7 +117,7 @@
                     write-warning "Failed to find Login: $Login as a Login or in a group connecting to server: $server"
                     break
                 }
-                #$FoundYou
+
                 foreach($gf in $FoundYou)
                 {
                 
@@ -89,7 +130,7 @@
                     }
                     Select-DefaultField -InputObject $output -Property ComputerName, SqlInstance, Login, Member  
                 }
-            } # foreach login
-        } # foreach
-    } # process
+            } 
+        } 
+    }
 }
