45b633b94292d46d5cf6d2a6edc6e29d02fbfb50
ctrlbold <clemaire@gmail.com>
2017-01-28 03:32:31 +0100

    Fixes #524  - thanks @Staggerlee011 for the code
    doing it this way because there's an merge conflict

--- functions/Test-DbaValidLogin.ps1
+++ functions/Test-DbaValidLogin.ps1
@@ -86,14 +86,21 @@ Tests all logins excluding any that are from the mydomain Domain
 			}
 			return $dn
 		}
-		
-		$alldomains = $domains = @()
-		$currentforest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
-		$alldomains += $currentforest.Domains.name | Where-Object { $_ -notin $excludedomains }
-		
-		$cd = $currentforest.Domains | Where-Object { $_.name -notin $excludedomains }
-		
-		foreach ($domain in $cd)
+		try
+		{
+			$alldomains = $domains = @()
+			$currentforest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+			$alldomains += $currentforest.Domains.name | Where-Object { $_ -notin $excludedomains }
+			
+			$cd = $currentforest.Domains | Where-Object { $_.name -notin $excludedomains }
+		}
+		catch
+		{
+			Write-warning "No Active Directory domains Found."
+			break
+		}
+	
+	foreach ($domain in $cd)
 		{
 			try
 			{
