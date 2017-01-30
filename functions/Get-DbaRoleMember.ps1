1893d7a56f4b85a06ff7818c569ed462fd2af33e
ctrlbold <clemaire@gmail.com>
2017-01-30 14:26:54 +0100

    Get-DbaRoleMember fix

--- functions/Get-DbaRoleMember.ps1
+++ functions/Get-DbaRoleMember.ps1
@@ -83,74 +83,75 @@ Returns a gridview displaying SQLServer, Database, Role, Member for both ServerR
 	{
 		foreach ($instance in $sqlinstance)
 		{
-			$server = $null
-			$server = Connect-SqlServer -SqlServer $instance -SqlCredential $sqlCredential
-			if ($Server.count -eq 1)
+			Write-Verbose "Connecting to $instance"
+			try
 			{
-				if ($IncludeServerLevel)
+				$server = Connect-SqlServer -SqlServer $instance -SqlCredential $Credential
+			}
+			catch
+			{
+				Write-Warning "Can't connect to $instance"
+				Continue
+			}
+			
+			if ($IncludeServerLevel)
+			{
+				Write-Verbose "Server Role Members included"
+				$instroles = $null
+				Write-Verbose "Getting Server Roles on $instance"
+				$instroles = $server.roles
+				if ($NoFixedRole)
 				{
-					Write-Verbose "Server Role Members included"
-					$instroles = $null
-					Write-Verbose "Getting Server Roles on $instance"
-					$instroles = $server.roles
-					if ($NoFixedRole)
-					{
-						$instroles = $instroles | Where-Object { $_.isfixedrole -eq $false }
-					}
-					ForEach ($instrole in $instroles)
-					{
-						Write-Verbose "Getting Server Role Members for $instrole on $instance"
-						$irmembers = $null
-						$irmembers = $instrole.enumserverrolemembers()
-						ForEach ($irmem in $irmembers)
-						{
-							[PSCustomObject]@{
-								SQLInstance = $instance
-								Database = $null
-								Role = $instrole.name
-								Member = $irmem.tostring()
-							}
-						}
-					}
+					$instroles = $instroles | Where-Object { $_.isfixedrole -eq $false }
 				}
-				
-				$dbs = $server.Databases
-				
-				if ($databases.count -gt 0)
+				ForEach ($instrole in $instroles)
 				{
-					$dbs = $dbs | Where-Object { $databases -contains $_.Name  }
-				}
-				
-				foreach ($db in $dbs)
-				{
-					$dbroles = $db.roles
-					Write-Verbose "Getting Database Roles for $($db.name) on $instance"
-					
-					if ($NoFixedRole)
+					Write-Verbose "Getting Server Role Members for $instrole on $instance"
+					$irmembers = $null
+					$irmembers = $instrole.enumserverrolemembers()
+					ForEach ($irmem in $irmembers)
 					{
-						$dbroles = $dbroles | Where-Object { $_.isfixedrole -eq $false }
-					}
-					
-					foreach ($dbrole in $dbroles)
-					{
-						Write-Verbose "Getting Database Role Members for $dbrole in $($db.name) on $instance"
-						$dbmembers = $dbrole.enummembers()
-						ForEach ($dbmem in $dbmembers)
-						{
-							[PSCustomObject]@{
-								SqlInstance = $instance
-								Database = $db.name
-								Role = $dbrole.name
-								Member = $dbmem.tostring()
-							}
+						[PSCustomObject]@{
+							SQLInstance = $instance
+							Database = $null
+							Role = $instrole.name
+							Member = $irmem.tostring()
 						}
 					}
 				}
 			}
-			else
+			
+			$dbs = $server.Databases
+			
+			if ($databases.count -gt 0)
 			{
-				Write-Warning "Can't connect to $instance. Moving on."
-				Continue
+				$dbs = $dbs | Where-Object { $databases -contains $_.Name }
+			}
+			
+			foreach ($db in $dbs)
+			{
+				$dbroles = $db.roles
+				Write-Verbose "Getting Database Roles for $($db.name) on $instance"
+				
+				if ($NoFixedRole)
+				{
+					$dbroles = $dbroles | Where-Object { $_.isfixedrole -eq $false }
+				}
+				
+				foreach ($dbrole in $dbroles)
+				{
+					Write-Verbose "Getting Database Role Members for $dbrole in $($db.name) on $instance"
+					$dbmembers = $dbrole.enummembers()
+					ForEach ($dbmem in $dbmembers)
+					{
+						[PSCustomObject]@{
+							SqlInstance = $instance
+							Database = $db.name
+							Role = $dbrole.name
+							Member = $dbmem.tostring()
+						}
+					}
+				}
 			}
 		}
 	}
