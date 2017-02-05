52062a8e5bf2fe0cd15155a50e5159f87527b678
Stuart Moore <stuart@stuart-moore.com>
2017-02-05 17:01:07 +0000

    remove comment

--- functions/Get-DbaBackupHistory.ps1
+++ functions/Get-DbaBackupHistory.ps1
@@ -300,7 +300,6 @@ Lots of detailed information for all databases on sqlserver2014a and sql2016.
 				{
 					Write-Debug $sql
 					$results = $sourceserver.ConnectionContext.ExecuteWithResults($sql).Tables.Rows | Select-Object * -ExcludeProperty BackupSetRank, RowError, Rowstate, table, itemarray, haserrors
-					#Adding a hidden Fullname Property so we can pipe this into the new restore functions
 					$results = $results | select-object *, @{Name="Fullname";expression={$_.Path}}
 					foreach ($result in $results)
 					{ 
