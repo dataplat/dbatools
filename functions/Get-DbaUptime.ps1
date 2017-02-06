40eef82bbccc09ee8dcce2223d79f358b514ad78
ctrlbold <clemaire@gmail.com>
2017-02-06 09:29:18 +0100

    added credential

--- functions/Get-DbaUptime.ps1
+++ functions/Get-DbaUptime.ps1
@@ -112,7 +112,7 @@ Returns an object with SQL Server start time, uptime as TimeSpan object, uptime 
 			
 			if ($SqlOnly -ne $true)
 			{
-				$WindowsServerName = (Resolve-DbaNetworkName $servername -Turbo).ComputerName
+				$WindowsServerName = (Resolve-DbaNetworkName $servername -Credential $WindowsCredential).ComputerName
 
 				try
 				{
