cda5e44bba34f90bf348ad128b3a5db0714e4a2a
Stuart Moore <stuart@stuart-moore.com>
2017-02-14 12:00:53 +0000

    Initial redesign of function

--- functions/Backup-DbaDatabase.ps1
+++ functions/Backup-DbaDatabase.ps1
@@ -3,32 +3,86 @@
 # If backup file isn't provided, auto generate in Default backup dir?
 # Can you please compare the options Microsoft provides? If it's too crazy, we can make this 1.0
 # needs to accept credential and needs help, per usual, this is just a skeleton.
-
+#>
 Function Backup-DbaDatabase
+{
+	[CmdletBinding()]
+	param (
+		[parameter(ValueFromPipeline = $True)]
+		[object[]]$DatabaseName, # Gotten from Get-DbaDatabase
+		[object]$SqlInstance,
+		[System.Management.Automation.PSCredential]$SqlCredential,
+		[string]$BackupFile,
+		[switch]$NoCopyOnly,
+		[ValidateSet('Full', 'Log', 'Differential','Diff','Database')] # Unsure of the names
+		[string]$BackupType = "Full",
+		[int]$FileCount = 1
+
+	)
+	BEGIN
+	{
+		$FunctionName = $FunctionName =(Get-PSCallstack)[0].Command
+		$Databases = @()
+	}
+	PROCESS
+	{
+	
+		foreach ($Name in $DatabaseName)
 		{
-			[CmdletBinding()]
-			param (
-				[object]$SqlInstance,
-				[string]$DatabaseName,
-				[string]$BackupFile,
-				[switch]$NoCopyOnly,
-				[ValidateSet('Full', 'Log', 'Differential')] # Unsure of the names
-				[string]$Type = "Full",
-				[int]$FileCount = 1
-				[parameter(ValueFromPipeline = $True)]
-				[object]$Database # Gotten from Get-DbaDatabase
-			)
+			if ($Name -is [String])
+			{
+				$Databases += [PSCustomObject]@{Name = $Name; RecoveryModel=''}
+			}
+			elseif ($Name -is [System.Object] -and $Name.Name.Length -ne 0 )
+			{
+				$Databases += [PSCustomObject]@{Name = 'name'; RecoveryModel= $RecoveryModel}
+			}
+		}
+	}
+	END
+	{
+		try 
+		{
+			$Server = Connect-SqlServer -SqlServer $SqlInstance -SqlCredential $SqlCredential	          
+		}
+		catch {
+            $server.ConnectionContext.Disconnect()
+			Write-Warning "$FunctionName - Cannot connect to $SqlInstance" -WarningAction Stop
+		}
+		$BackupHistory = Get-DbaBackupHistory -SqlServer $SqlInstance -databases ($Databases.Name -join ',') -LastFull
+		ForEach ($Database in $Databases)
+		{
+			if ($Database.RecoveryModel -ne '')
+			{
+				$Database.RecoveryModel = $server.databases[$Database.Name].RecoveryModel
+			}
 			
-			if ($Type -eq "Full") { $type = "Database" } #maybe others?
+			if ($Database.RecoveryModel -eq 'Simple' -and $BackupType -eq 'Log')
+			{
+				Write-Warning "$FunctionName - $($Database.Name) is in simple recovery mode, cannot take log backup"
+				break
+			}
+			$FullExists = $BackupHistory | Where-Object {$_.Database -eq $Database.Name}
+			if ($BackupType -ne "Full" -and $FullExists.length -eq 0)
+			{
+				Write-Warning "$FunctionName - $($Database.Name) does not have an existing full backup, cannot take log or differentialbackup"
+				break				
+			}
+
 			$val = 0
 			$copyonly = !$NoCopyOnly
 			
 			$server.ConnectionContext.StatementTimeout = 0
 			$backup = New-Object Microsoft.SqlServer.Management.Smo.Backup
-			$backup.Database = $dbname
+			$backup.Database = $Database.Name
+			$Type = "Database"
+			if ($BackupType -eq "Log") {$Type = "Log" }
 			$backup.Action = $Type
 			$backup.CopyOnly = $copyonly
-			
+			if ($Type -eq "Differential")
+			{
+				$backup.Incremental = $true
+			}
 			while ($val -lt $filecount)
 			{
 				$device = New-Object Microsoft.SqlServer.Management.Smo.BackupDeviceItem
@@ -37,21 +91,20 @@ Function Backup-DbaDatabase
 				$backup.Devices.Add($device)
 				$val++
 			}
-			
 			$percent = [Microsoft.SqlServer.Management.Smo.PercentCompleteEventHandler] {
-				Write-Progress -id 1 -activity "Backing up database $dbname to $backupfile" -percentcomplete $_.Percent -status ([System.String]::Format("Progress: {0} %", $_.Percent))
+				Write-Progress -id 1 -activity "Backing up database $($Database.Name)  to $backupfile" -percentcomplete $_.Percent -status ([System.String]::Format("Progress: {0} %", $_.Percent))
 			}
 			$backup.add_PercentComplete($percent)
 			$backup.PercentCompleteNotification = 1
 			$backup.add_Complete($complete)
 			
-			Write-Progress -id 1 -activity "Backing up database $dbname to $backupfile" -percentcomplete 0 -status ([System.String]::Format("Progress: {0} %", 0))
-			Write-Output "Backing up $dbname"
+			Write-Progress -id 1 -activity "Backing up database $($Database.Name)  to $backupfile" -percentcomplete 0 -status ([System.String]::Format("Progress: {0} %", 0))
+			Write-Output "Backing up $($Database.Name)"
 			
 			try
 			{
 				$backup.SqlBackup($server)
-				Write-Progress -id 1 -activity "Backing up database $dbname to $backupfile" -status "Complete" -Completed
+				Write-Progress -id 1 -activity "Backing up database $($Database.Name)  to $backupfile" -status "Complete" -Completed
 				Write-Output "Backup succeeded"
 				return $true
 			}
@@ -63,4 +116,6 @@ Function Backup-DbaDatabase
 			}
 		}
 	}
-#>
+
+}
+
