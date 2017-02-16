16e3a9e26dfa22a6a98a4d753161045a241266b7
ctrlbold <clemaire@gmail.com>
2017-02-16 18:35:41 +0100

    fix encoding issue

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-﻿[cmdletbinding()]
+[CmdletBinding()]
 param (
 	[string]$Path
 )
@@ -68,35 +68,35 @@ else
 }
 
 Write-Output "Downloading archive from github"
-try
-{
-	Invoke-WebRequest $url -OutFile $zipfile
-}
-catch
-{
-	#try with default proxy and usersettings
-	Write-Output "Probably using a proxy for internet access, trying default proxy settings"
-	(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-	Invoke-WebRequest $url -OutFile $zipfile
-}
-
-# Unblock if there's a block
-Unblock-File $zipfile -ErrorAction SilentlyContinue
-
-Write-Output "Unzipping"
-
-# Keep it backwards compatible
-$shell = New-Object -ComObject Shell.Application
-$zipPackage = $shell.NameSpace($zipfile)
-$destinationFolder = $shell.NameSpace($temp)
-$destinationFolder.CopyHere($zipPackage.Items())
-
-Write-Output "Cleaning up"
-Move-Item -Path "$temp\dbatools-master\*" $path
-Remove-Item -Path "$temp\dbatools-master"
-Remove-Item -Path $zipfile
-
-Write-Output "Done! Please report any bugs to clemaire@gmail.com."
-if ((Get-Command -Module dbatools).count -eq 0) { Import-Module "$path\dbatools.psd1" -Force }
-Get-Command -Module dbatools
-Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
+	try
+	{
+		Invoke-WebRequest $url -OutFile $zipfile
+	}
+	catch
+	{
+		#try with default proxy and usersettings
+		Write-Output "Probably using a proxy for internet access, trying default proxy settings"
+		(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+		Invoke-WebRequest $url -OutFile $zipfile
+	}
+	
+	# Unblock if there's a block
+	Unblock-File $zipfile -ErrorAction SilentlyContinue
+	
+	Write-Output "Unzipping"
+	
+	# Keep it backwards compatible
+	$shell = New-Object -COM Shell.Application
+	$zipPackage = $shell.NameSpace($zipfile)
+	$destinationFolder = $shell.NameSpace($temp)
+	$destinationFolder.CopyHere($zipPackage.Items())
+	
+	Write-Output "Cleaning up"
+	Move-Item -Path "$temp\dbatools-master\*" $path
+	Remove-Item -Path "$temp\dbatools-master"
+	Remove-Item -Path $zipfile
+	
+	Write-Output "Done! Please report any bugs to clemaire@gmail.com."
+	if ((Get-Command -Module dbatools).count -eq 0) { Import-Module "$path\dbatools.psd1" -Force }
+	Get-Command -Module dbatools
+	Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
