a13c3479f8dd75c18b8751064b551da1bf5c33c9
Sander Stad <sander@sqlstad.nl>
2017-02-19 14:21:31 +0100

    Major update
    Added -Silent switch
    Added regions to easier toggle the code
    Changed $SqlCredential type from obejct to
    System.Management.Automation.PSCredential
    Changed Write-Output, Write-Verbose and Write-Warning to Write-Message
    Changed the where statements to Whrere-Object
    Simplified the ShouldProcess checks for the while loops
    Changed the output of the query in verbose to be easier to read

--- functions/Invoke-DbaLogShippingRecovery.ps1
+++ functions/Invoke-DbaLogShippingRecovery.ps1
@@ -2,7 +2,7 @@
 {
 <#
 .SYNOPSIS 
-Invoke-DbaLogShippingRecovery recovers log shipped databases to enable them after a migration or disaster.
+Invoke-DbaLogShippingRecovery recovers log shipped databases to a normal state to act upon a migration or disaster.
 
 .DESCRIPTION
 By default all the databases for a particular instance are recovered.
@@ -10,22 +10,21 @@ If the database is in the right state, either standby or recovering, the process
 
 At first the function will check if the backup source directory can still be reached.
 If so it will look up the last transaction log backup for the database. If that backup file is not the last copied file the log shipping copy job will be started.
-If the directory cannot be reached the function will continue to the restoring process.
-After the copy job check is performed the job is disabled to prevent the job to run
+If the directory cannot be reached for the function will continue to the restoring process.
+After the copy job check is performed the job is disabled to prevent the job to run.
 
-For the restore the log shipping statu i checked in the msdb database.
-If the last restored file is not the same as the last file name found the restore will be executed.
+For the restore the log shipping status is checked in the msdb database.
+If the last restored file is not the same as the last file name found, the log shipping restore job will be executed.
 After the restore job check is performed the job is disabled to prevent the job to run
 
 The last part is to set the databse online by restoring the databases with recovery
-
 
 .PARAMETER SqlServer
 SQLServer name or SMO object representing the SQL Server to connect to
 
 .PARAMETER Database
-Database to perform the restore for. This value can also be piped. 
-If this value is not supplied all databases will be recovered
+Database to perform the restore for. This value can also be piped enabling multiple databases to be recovered. 
+If this value is not supplied all databases will be recovered.
 
 .PARAMETER SqlCredential
 Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
@@ -41,7 +40,7 @@ By default the database is restored to a functional state (Normal).
 
 
 .NOTES 
-Author: Sander Stad (@sqlstad), sqlstad.com
+Author: Sander Stad (@sqlstad), sqlstad.nl
 Requires: sysadmin access on SQL Servers
 
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
@@ -78,7 +77,7 @@ db1, db2, db3, db4 | Invoke-DbaLogShippingRecovery -SqlServer 'server1' -Verbose
 Recovers the database db1, db2, db3, db4 to a normal status
 
 .EXAMPLE   
-Invoke-DbaLogShippingRecovery -SqlServer 'server1' -Verbose -WhatIf
+Invoke-DbaLogShippingRecovery -SqlServer 'server1' -WhatIf
 
 Shows what would happen if the command were executed.
 
@@ -86,48 +85,45 @@ Shows what would happen if the command were executed.
     [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
     param
 	(
-        [Parameter(Position=1, Mandatory=$true)]
+        [Parameter(Mandatory=$true, Position=1)]
 		[Alias("ServerInstance", "SqlInstance")]
-        [string]$SqlServer,
-        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$true)]
+        [object]$SqlServer,
+        [Parameter(Mandatory=$false, Position=2, ValueFromPipeline=$true)]
         [string[]]$Database = $null,
-        [Parameter(Position=3, Mandatory=$false)][object]$SqlCredential,
-        [Parameter(Position=4, Mandatory=$false)][switch]$NoRecovery
+        [Parameter(Mandatory=$false, Position=3)][switch]$NoRecovery,
+        [Parameter(Mandatory=$false, Position=4)][bool]$Silent,
+        [Parameter(Mandatory=$false, Position=5)][System.Management.Automation.PSCredential]$SqlCredential
 	)
 
     BEGIN
     {
-        Write-Output "Attempting to connect to Sql Server.."
+        Write-Message -Message "Attempting to connect to Sql Server.." -Level 2 -Silent $Silent
 		$server = Connect-SqlServer -SqlServer $SqlServer -SqlCredential $SqlCredential
+        $SqlServer.gettype()
     }
 
     PROCESS
     {
-        Write-Output "Started Log Shipping Recovery."
+        Write-Message -Message "Started Log Shipping Recovery." -Level 2 -Silent $Silent
 
+        #region Query setup
         # Query for retrieving the log shipping information
         $query = "
-        SELECT  lss.primary_server ,
-                lss.primary_database ,
-                lsd.secondary_database ,
-                lss.backup_source_directory ,
-                lss.backup_destination_directory ,
-                lss.last_copied_file ,
-                lss.last_copied_date ,
-                lsd.last_restored_file,
-		        sj1.name AS 'copyjob',
-		        sj2.name AS 'restorejob'
-        FROM    msdb.dbo.log_shipping_secondary AS lss
-                INNER JOIN msdb.dbo.log_shipping_secondary_databases AS lsd ON lsd.secondary_id = lss.secondary_id
-		        INNER JOIN msdb.dbo.sysjobs AS sj1 ON sj1.job_id = lss.copy_job_id
-		        INNER JOIN msdb.dbo.sysjobs AS sj2 ON sj2.job_id = lss.restore_job_id "
+----------------------------------------------------------------
+SELECT  lss.primary_server, lss.primary_database, lsd.secondary_database, lss.backup_source_directory,
+        lss.backup_destination_directory, lss.last_copied_file, lss.last_copied_date,
+        lsd.last_restored_file, sj1.name AS 'copyjob', sj2.name AS 'restorejob'
+FROM    msdb.dbo.log_shipping_secondary AS lss
+        INNER JOIN msdb.dbo.log_shipping_secondary_databases AS lsd ON lsd.secondary_id = lss.secondary_id
+		INNER JOIN msdb.dbo.sysjobs AS sj1 ON sj1.job_id = lss.copy_job_id
+		INNER JOIN msdb.dbo.sysjobs AS sj2 ON sj2.job_id = lss.restore_job_id "
         
         # Check if they commandlet is eecuted from a pipeline
 		if ($PSCmdlet.MyInvocation.ExpectingInput) 
 		{
 			$db = $_
             
-            $query += "WHERE lsd.secondary_database = '$db'"
+            $query += "WHERE lsd.secondary_database = '$db' "
 		}
 		else
 		{
@@ -139,16 +135,23 @@ Shows what would happen if the command were executed.
                 $query += "WHERE lsd.secondary_database = '$db'"
             }
 		}
+        
+        # Add an extra line to easier read the query when in verbose mode
+        $query +="
+----------------------------------------------------------------"
+        #endregion Query setup
 
         # Retrieve the log shipping information from the secondary instance
         try{
-            Write-Verbose "Retrieving log shipping information from the secondary instance"
-            $logshipping_details = Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'msdb' -Query $query
+            Write-Message -Message "Retrieving log shipping information from the secondary instance" -Level 5 -Silent $Silent
+            $logshipping_details = Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'msdb' -Query $query 
         }
         catch
         {
+            Stop-Function -Message "Error retrieving the log shipping details" -Silent $Silent -InnerErrorRecord $_ -Target $SqlServer
             Write-Error $_.Exception.Message
-            Break
+            
+            return
         }
         
         # Check if there are any databases to recover
@@ -162,36 +165,32 @@ Shows what would happen if the command were executed.
                 if($server.Databases[$ls.secondary_database].Status -in ('Normal, Standby', 'Standby', 'Restoring'))
                 {
 
-                    Write-Output ("Started Recovery for '" + $ls.secondary_database + "'")
+                    Write-Message -Message ("Started Recovery for '" + $ls.secondary_database + "'") -Level 2 -Silent $Silent
 
+                    #region Copy of remaining backup files
                     # Check if the backup source directory can be reached
                     if(Test-Path $ls.backup_source_directory)
                     {
                         # Get the last file from the backup source directory
-                        $latestfile = Get-ChildItem -Path $ls.backup_source_directory -filter ("*" + $ls.primary_database + "*") | where {($_.Extension -eq '.trn') } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+                        $latestfile = Get-ChildItem -Path $ls.backup_source_directory -filter ("*" + $ls.primary_database + "*") | Where-Object {($_.Extension -eq '.trn') } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
                     
                         # Check if the latest file is also the latest copied file
                         if($latestfile.Name -ne [string]$ls.last_copied_file.Split('\')[-1])
                         {
-                            Write-Verbose "Synchronizing the latest transaction log backup file(s)"
+                            Write-Message -Message "Synchronizing the latest transaction log backup file(s)" -Level 5 -Silent $Silent
 
                             # Start the job to get the latest files
                             if($PSCmdlet.ShouldProcess($SqlServer, ("Starting copy job " + $ls.copyjob)))
                             {
                                 $server.JobServer.Jobs[$ls.copyjob].Start()
 
-                                Write-Verbose ("Waiting for files to get copied to '" + $ls.backup_destination_directory + "'")
+                                Write-Message -Message ("Copying files to '" + $ls.backup_destination_directory + "'") -Level 5 -Silent $Silent
+                            
+                                # Check if the file has been copied
+                                $query = "SELECT last_copied_file FROM msdb.dbo.log_shipping_secondary WHERE primary_database = '" + $ls.primary_database + "'"
+                                $latestcopy = Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'msdb' -Query $query
 
-                            }
-
-                            # Check if the file has been copied
-                            $query = "SELECT last_copied_file FROM msdb.dbo.log_shipping_secondary WHERE primary_database = '" + $ls.primary_database + "'"
-                            $latestcopy = Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'msdb' -Query $query
-
-                            # Waiting for the copy action to complete
-                            if($PSCmdlet.ShouldProcess($ls.backup_destination_directory, "Waiting for the copy action to complete.. "))
-                            {
-                                Write-Verbose "Waiting for the copy action to complete.. "
+                                Write-Message -Message "Waiting for the copy action to complete.." -Level 5 -Silent $Silent
 
                                 while($latestfile.Name -ne [string]$latestcopy.last_copied_file.Split('\')[-1])
                                 {
@@ -203,18 +202,19 @@ Shows what would happen if the command were executed.
                                 }
                             }
 
-                            Write-Verbose "Copying of backup files finished"
+                            Write-Message -Message "Copying of backup files finished" -Level 5 -Silent $Silent
                         }
                     }
                     else
                     {
-                        Write-Verbose "Couldn't reach the backup source directory. Continuing..."   
+                        Write-Message -Message "Couldn't reach the backup source directory. Continuing..." -Level 5 -Silent $Silent 
                     }
+                    #endregion
 
                     # Disable the log shipping copy job on the secondary instance
                     if($PSCmdlet.ShouldProcess($SqlServer, ("Disabling copy job " + $ls.copyjob)))
                     {
-                        Write-Verbose ("Disabling copy job " + $ls.copyjob)
+                        Write-Message -Message ("Disabling copy job " + $ls.copyjob) -Level 5 -Silent $Silent
                         $server.JobServer.Jobs[$ls.copyjob].IsEnabled = $false
                         $server.JobServer.Jobs[$ls.copyjob].Alter()
                     }
@@ -223,21 +223,18 @@ Shows what would happen if the command were executed.
                     $query = "SELECT last_restored_file FROM dbo.log_shipping_secondary_databases WHERE secondary_database = '" + $ls.secondary_database + "'"
                     $latestrestore = Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'msdb' -Query $query
 
+                    #region Restore of remaining backup files
                     # Check if the last copied file is newer than the last restored file
                     if($latestfile.Name -ne [string]$latestrestore.last_restored_file.Split('\')[-1])
                     {
-                        Write-verbose "Last file found has not yet been restored yet"
+                        Write-Message -Message "Last file found has not yet been restored yet" -Level 5 -Silent $Silent
                         # Start the restore job
                         if($PSCmdlet.ShouldProcess($SqlServer, ("Starting restore job " + $ls.restorejob)))
                         {
-                            Write-Verbose ("Starting restore job " + $ls.restorejob)
+                            Write-Message -Message ("Starting restore job " + $ls.restorejob) -Level 5 -Silent $Silent
                             $server.JobServer.Jobs[$ls.restorejob].Start()
-                        }
                         
-                        # Waiting for the restore action to complete
-                        if($PSCmdlet.ShouldProcess($ls.secondary_database, "Waiting for the restore action to complete.."))
-                        {
-                            Write-Verbose "Waiting for the restore action to complete.."
+                            Write-Message -Message "Waiting for the restore action to complete.."-Level 5 -Silent $Silent
 
                             # Check if the jobs is still running
                             while($latestfile.Name -ne [string]$latestrestore.last_restored_file.Split('\')[-1])
@@ -250,15 +247,17 @@ Shows what would happen if the command were executed.
                             }
                         }
                     }
+                    #endregion Restore of remaining backup files
 
                     # Disable the log shipping restore job on the secondary instance
                     if($PSCmdlet.ShouldProcess($SqlServer, ("Disabling restore job " + $ls.restorejob)))
                     {
-                        Write-Verbose ("Disabling restore job " + $ls.restorejob)
+                        Write-Message -Message ("Disabling restore job " + $ls.restorejob) -Level 5 -Silent $Silent
                         $server.JobServer.Jobs[$ls.restorejob].IsEnabled = $false
                         $server.JobServer.Jobs[$ls.restorejob].Alter()
                     }
 
+                    #region Restore database to normal state
                     # Check for the last time if everything is up-to-date
                     if($latestfile.Name -eq [string]$latestrestore.last_restored_file.Split('\')[-1])
                     {
@@ -267,39 +266,39 @@ Shows what would happen if the command were executed.
                         {
                             if($PSCmdlet.ShouldProcess($ls.secondary_database, "Restoring database with recovery"))
                             {
-                                Write-Output "Restoring the database to it's normal state"
+                                Write-Message -Message "Restoring the database to it's normal state"
                                 $query = "RESTORE DATABASE " + $ls.secondary_database + " WITH RECOVERY"
                                 Invoke-Sqlcmd2 -ServerInstance $SqlServer -Database 'master' -Query $query 
                             }
                         }
                         elseif($NoRecovery -eq $true)
                         {
-                            Write-Output "Skipping restore with recovery"
+                            Write-Message -Message "Skipping restore with recovery" -Level 2 -Silent $Silent
                         }
                     }
+                    #endregion Restore database to normal state
 
-                    # Disable the monitor if present
                 }
                 else
                 {
-                    Write-Warning "The database '$db' doesn't have the right status to be recovered"
+                    Write-Message "The database '$db' doesn't have the right status to be recovered" -Level 3 -Silent $Silent
                 }
             }
         }
         else
         {
-            Write-Warning "The database '$db' is not configured for log shipping."
+            Write-Message -Message "The database '$db' is not configured for log shipping." -Level 2 -Warning -Silent $Silent
         }
 
-        Write-Output ("Finished Recovery for '" + $ls.secondary_database + "'")
+        Write-Message -Message ("Finished Recovery for '" + $ls.secondary_database + "'") -Level 2 -Silent $Silent
 
         # Reset the log ship details
-        $logshipping_details = $null 
+        $logshipping_details = $null
     }
 
     END
     {
-        Write-Output "Finished Log Shipping Recovery"
+        Write-Message -Message "Finished Log Shipping Recovery" -Level 1 -Silent $Silent
     }
 
 }
