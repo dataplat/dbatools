5cd1ab6552a539840c88f74fc5f4d25e432c169d
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-02-19 02:21:27 +0100

    Fixed Test

--- tests/Stop-Function.Tests.ps1
+++ tests/Stop-Function.Tests.ps1
@@ -13,7 +13,8 @@ if ($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
 
 
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
-Import-Module $PSScriptRoot\..\internal\$sut -Force
+. $PSScriptRoot\..\internal\$sut
+. $PSScriptRoot\..\internal\Write-Message.ps1
 Import-Module PSScriptAnalyzer
 ## Added PSAvoidUsingPlainTextForPassword as credential is an object and therefore fails. We can ignore any rules here under special circumstances agreed by admins :-)
 $Rules = (Get-ScriptAnalyzerRule).Where{ $_.RuleName -notin ('PSAvoidUsingPlainTextForPassword') }
