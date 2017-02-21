ceff182e4364f5a67a90228a9dee0788d3e74332
ctrlbold <clemaire@gmail.com>
2017-02-22 00:19:16 +0100

    updated path

--- internal/configurations/paths.ps1
+++ internal/configurations/paths.ps1
@@ -6,7 +6,7 @@ This is designed for all paths you need configurations for.
 Set-DbaConfig -Name 'Path.DbatoolsData' -Value "$($env:AppData)\PowerShell\dbatools" -Default -DisableHandler -Description "The path where dbatools stores persistent data on a per user basis."
 
 # The default path where dbatools stores temporary data
-Set-DbaConfig -Name 'Path.DbatoolsTemp' -Value $env:TEMP -Default -DisableHandler -Description "The path where dbatools stores temporary data."
+Set-DbaConfig -Name 'Path.DbatoolsTemp' -Value ([System.IO.Path]::GetTempPath()).TrimEnd("\") -Default -DisableHandler -Description "The path where dbatools stores temporary data."
 
 # The default path for writing logs
 Set-DbaConfig -Name 'Path.DbatoolsLogPath' -Value "$($env:AppData)\PowerShell\dbatools" -Default -Description "The path where dbatools writes all its logs and debugging information."
