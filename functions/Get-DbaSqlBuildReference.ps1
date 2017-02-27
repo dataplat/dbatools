c2bcd11c42c9833935648b46d3c64be0f02e9a17
niphlod <niphlod@gmail.com>
2017-02-27 22:28:13 +0100

    fixing a few things

--- functions/Get-DbaSqlBuildReference.ps1
+++ functions/Get-DbaSqlBuildReference.ps1
@@ -3,16 +3,19 @@ Function Get-DbaSqlBuildReference
 <#
 .SYNOPSIS
 Returns SQL Server Build infos on a SQL instance
-	
+
 .DESCRIPTION
 Returns info about the specific build of a SQL instance, including the SP, the CU and the reference KB, wherever possible.
 It also includes End Of Support dates as specified on Microsoft Lifecycle Policy
-	
+
 .PARAMETER SqlInstance
 The SQL Server that you're connecting to.
 
-.PARAMETER SqlCredential
-Credential object used to connect to the SQL Server as a different user
+.PARAMETER Credential
+Credential object used to connect to the SQL Server as a different user.
+
+.PARAMETER Build
+Instead of connecting to a real instance, pass a string identifying the build to get the info back.
 
 .NOTES
 Author: niphlod
@@ -28,29 +31,40 @@ You should have received a copy of the GNU General Public License along with thi
 https://dbatools.io/Get-DbaSqlBuildReference
 
 .EXAMPLE
-Get-DbaSqlBuildReference -SqlServer sqlserver2014a
+Get-DbaSqlBuildReference -SqlInstance sqlserver2014a
 
-Returns SqlInstance, SPLevel, CULevel, KBLevel for server sqlserver2014a
+Returns SqlInstance, SPLevel, CULevel, KBLevel, SupportedUntil for server sqlserver2014a
 
 .EXAMPLE
 $cred = Get-Credential sqladmin
-Get-DbaSqlBuildReference -SqlServer sqlserver2014a -SqlCredential $cred
+Get-DbaSqlBuildReference -SqlInstance sqlserver2014a -SqlCredential $cred
+
 Does the same as above but logs in as SQL user "sqladmin"
 
 .EXAMPLE
-Get-DbaSqlBuildReference -SqlServer sqlserver2014a -SqlCredential $cred
-Does the same as above but logs in as SQL user "sqladmin"
-	
+Get-DbaSqlBuildReference -Build "12.00.4502"
+
+Returns information about a build identified by  "12.00.4502" (which is SQL 2014 with SP1 and CU11)
+
 #>
 
 	[CmdletBinding()]
+
 	Param (
-		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Parameter(
+            ParameterSetName='Server',
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
 		[Alias("ServerInstance", "SqlServer")]
 		[string[]]$SqlInstance,
-		[Alias("Credential")]
+		[parameter(ParameterSetName = "Server")]
 		[PSCredential]
-		[System.Management.Automation.CredentialAttribute()]$SqlCredential
+		[System.Management.Automation.CredentialAttribute()]$Credential,
+		[Parameter(
+            ParameterSetName='BuildString'
+        )]
+        [string[]]$Build
 	)
 
 	BEGIN {
@@ -67,35 +81,55 @@ Does the same as above but logs in as SQL user "sqladmin"
 			return Find-DbaSqlBuildReferenceIndex | ConvertFrom-Json
 		}
 
+		function Compare-DbaSqlBuildGreater([string]$firstref, [string]$secondref) {
+			$first = $firstref.split('.') | Foreach-Object { [convert]::ToInt32($_) }
+			$second = $secondref.split('.') | Foreach-Object { [convert]::ToInt32($_) }
+			$x = 0
+			while($true) {
+				if($first[$x] -gt $second[$x]) {
+					return $true
+				}
+				$x += 1
+				if($x -gt $first.Length) {
+					return $false
+				}
+			}
+		}
+
 		$moduledirectory = (Get-Module -Name dbatools).ModuleBase
 
 		$IdxRef = Get-DbaSqlBuildReferenceIndex
-	}
-	PROCESS {
-		foreach ($instance in $SqlInstance) {
-			Write-Verbose "Connecting to $instance"
-			try {
-				$server = Connect-SqlServer -SqlServer $instance -SqlCredential $SqlCredential
-			} catch {
-				Write-Warning "Can't connect to $instance"
-				Continue
-			}
-			$LookFor = "$($server.Version.Major).$($server.Version.Minor).$($server.Version.Build)"
+		$LastUpdated = Get-Date -Date $IdxRef.LastUpdated
+		if ($LastUpdated -lt (Get-Date).AddDays(-45)) {
+			Write-Warning "Index is Stale, Last Update on: $(Get-Date -Date $LastUpdated -f s)"
+		}
+
+		function Resolve-DbaSqlBuild([string]$build) {
+			$LookFor = ($build.split('.')[0..2] | Foreach-Object { [convert]::ToInt32($_) }) -join '.'
 			Write-Verbose "Looking for $LookFor"
-			$IdxVersion = $IdxRef | Where-Object Version -like "$($server.Version.Major).$($server.Version.Minor).*"
+			$SqlVersion = $build.split('.')[0..1] -join '.'
+			$IdxVersion = $IdxRef.Data | Where-Object Version -like "$SqlVersion.*"
 			$Detected = @{}
 			$MatchType = 'Approximate'
-			Write-Verbose "We have $($IdxVersion.Length) in store"
+			Write-Verbose "We have $($IdxVersion.Length) in store for this Release"
 			If($IdxVersion.Length -eq 0) {
-				Write-Warning "No info in store for this Version"
-				$Detected.Warning = "No info in store for this Version"
+				Write-Warning "No info in store for this Release"
+				$Detected.Warning = "No info in store for this Release"
 			}
+			$LastVer = $IdxVersion[0]
 			foreach($el in $IdxVersion) {
 				if($null -ne $el.Name) {
 					$Detected.Name = $el.Name
 				}
+				if(Compare-DbaSqlBuildGreater -firstref $el.Version -secondref $LookFor) {
+					$MatchType = 'Approximate'
+					$Detected.Warning = "$LookFor not found, closest build we have is $($LastVer.Version)"
+					break
+				}
+				$LastVer = $el
 				if($null -ne $el.SP) {
 					$Detected.SP = $el.SP
+					$Detected.CU = $null
 				}
 				if($null -ne $el.CU) {
 					$Detected.CU = $el.CU
@@ -109,10 +143,25 @@ Does the same as above but logs in as SQL user "sqladmin"
 					break
 				}
 			}
+			return $Detected
+		}
+
+	}
+	PROCESS {
+		foreach ($instance in $SqlInstance) {
+			Write-Verbose "Connecting to $instance"
+			try {
+				$server = Connect-SqlServer -SqlServer $instance -SqlCredential $Credential
+			} catch {
+				Write-Warning "Can't connect to $instance"
+				Continue
+			}
+			$LookFor = $server.Version.toString()
+			$Detected = Resolve-DbaSqlBuild $LookFor
 			$object = [PSCustomObject]@{
-					ComputerName  = $server.NetName
-					InstanceName  = $server.ServiceName
 					SqlInstance   = $server.Name
+					InstanceName  = $server.ServiceName
+					ComputerName  = $server.NetName
 					NameLevel = $Detected.Name
 					SPLevel = $Detected.SP
 					CULevel = $Detected.CU
@@ -120,9 +169,24 @@ Does the same as above but logs in as SQL user "sqladmin"
 					SupportedUntil = $Detected.SupportedUntil
 					MatchType = $MatchType
 					Warning = $Detected.Warning
+					Version = $LookFor
 					Instance = $server
 			}
-			Select-DefaultView -InputObject $object -Property SqlInstance, NameLevel, SPLevel, CULevel, KBLevel, SupportedUntil, MatchType, Warning
+			Select-DefaultView -InputObject $object -Property SqlInstance, Version, NameLevel, SPLevel, CULevel, KBLevel, SupportedUntil, MatchType, Warning
+		}
+		foreach($buildstr in $Build) {
+			$Detected = Resolve-DbaSqlBuild $buildstr
+			$object = [PSCustomObject]@{
+					NameLevel = $Detected.Name
+					SPLevel = $Detected.SP
+					CULevel = $Detected.CU
+					KBLevel = $Detected.KB
+					SupportedUntil = $Detected.SupportedUntil
+					MatchType = $MatchType
+					Warning = $Detected.Warning
+					Version = $buildstr
+			}
+			Select-DefaultView -InputObject $object -Property Version, NameLevel, SPLevel, CULevel, KBLevel, SupportedUntil, MatchType, Warning
 		}
 	}
 }
