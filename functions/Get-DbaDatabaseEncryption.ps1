fd22967e779097412003c55d1ff6a46cf89bdea8
Stephen Bennett <Stephen.bennett@creator.co.uk>
2017-02-27 12:04:16 +0000

    Get-DbaDatabaseEncryption
    Get-DbaDatabaseEncryption - lists al encryption used in databases on
    server

---
+++ functions/Get-DbaDatabaseEncryption.ps1
@@ -0,0 +1,129 @@
+﻿function Get-DbaDatabaseEncryption {
+	[CmdletBinding()]
+	param ([parameter(ValueFromPipeline, Mandatory = $true)]
+		[Alias("ServerInstance", "SqlInstance")]
+		[object[]]$SqlServer,
+		[System.Management.Automation.PSCredential]$SqlCredential,
+		[switch]$IncludeSystemDBs)
+	
+	DynamicParam { if ($SqlServer) { return Get-ParamSqlDatabases -SqlServer $SqlServer[0] -SqlCredential $SourceSqlCredential } }
+	
+	BEGIN
+	{
+		$databases = $psboundparameters.Databases
+        $exclude = $psboundparameters.Exclude
+    }
+
+	PROCESS
+	{
+		foreach ($s in $SqlServer)
+		{
+			#For each SQL Server in collection, connect and get SMO object
+			Write-Verbose "Connecting to $s"
+			$server = Connect-SqlServer $s -SqlCredential $SqlCredential
+			#If IncludeSystemDBs is true, include systemdbs
+			#only look at online databases (Status equal normal)
+			try
+			{
+				if ($databases.length -gt 0)
+				{
+					$dbs = $server.Databases | Where-Object { $databases -contains $_.Name }
+				}
+				elseif ($IncludeSystemDBs)
+				{
+					$dbs = $server.Databases | Where-Object { $_.status -eq 'Normal' }
+				}
+				else
+				{
+					$dbs = $server.Databases | Where-Object { $_.status -eq 'Normal' -and $_.IsSystemObject -eq 0 }
+				}
+				
+				if ($exclude.length -gt 0)
+				{
+					$dbs = $dbs | Where-Object { $exclude -notcontains $_.Name }
+				}
+			}
+			catch
+			{
+				Write-Exception $_
+				throw "Unable to gather dbs for $($s.name)"
+				continue
+			}
+			
+			foreach ($db in $dbs)
+			{    
+                if ($db.EncryptionEnabled -eq $true)
+                {
+       
+            $out = [PSCustomObject]@{
+                        Server = $server.name
+                        Instance = $server.InstanceName
+                        Database = $db
+                        Encryption = "EncryptionEnabled (tde)"
+                        Name =  $null
+                        LastBackup = $null 
+                        PrivateKeyEncryptionType = $null
+                        EncryptionAlgorithm = $null
+                        KeyLength = $null
+                        Owner = $null			
+                    } 
+                    $out
+                }
+
+                foreach ($cert in $db.Certificates)
+                {
+    
+ $out = [PSCustomObject]@{
+                        Server = $server.name
+                        Instance = $server.InstanceName
+                        Database = $db
+                        Encryption = "Certificate"
+                        Name =  $cert.Name
+                        LastBackup = $cert.LastBackupDate
+                        PrivateKeyEncryptionType = $cert.PrivateKeyEncryptionType
+                        EncryptionAlgorithm = $null
+                        KeyLength = $null
+                        Owner = $cert.Owner			
+                    }
+                     $out
+                }
+                
+                foreach ($ak in $db.AsymmetricKeys)
+                {
+  
+$out = [PSCustomObject]@{
+                        Server = $server.name
+                        Instance = $server.InstanceName
+                        Database = $db
+                        Encryption = "Asymentric key"
+                        Name =  $ak.Name
+                        LastBackup = $null
+                        PrivateKeyEncryptionType = $ak.PrivateKeyEncryptionType
+                        EncryptionAlgorithm = $ak.KeyEncryptionAlgorithm
+                        KeyLength = $ak.KeyLength
+                        Owner = $ak.Owner	
+                    }
+                     $out
+                }
+                foreach ($sk in $db.SymmetricKeys)
+                {
+
+   $out = [PSCustomObject]@{
+                        Server = $server.name
+                        Instance = $server.InstanceName
+                        Database = $db
+                        Encryption = "Symmetric key"
+                        Name =  $sk.Name
+                        LastBackup = $null
+                        PrivateKeyEncryptionType = $sk.PrivateKeyEncryptionType
+                        EncryptionAlgorithm = $ak.EncryptionAlgorithm
+                        KeyLength = $sk.KeyLength
+                        Owner = $sk.Owner	
+                    }
+                     $out
+                }
+            }
+       }  
+   }		
+}
+
