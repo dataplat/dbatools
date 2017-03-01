f44f2ead373a2427614ab9431bca87226051c104
Rob Sewell <mrrobsewell@outlook.com>
2017-03-01 11:17:10 +0000

    Get-DBAJobOutputFile following review

--- functions/Get-DbaJobOutputFile.ps1
+++ functions/Get-DbaJobOutputFile.ps1
@@ -17,11 +17,8 @@
 .PARAMETER JobName
     The Agent Job Name to provide Output File Path for. Also available dynamically. If ommitted all Agent Jobs will be used
 
-.PARAMETER OpenFile
-    Uses Invoke-Item to open the file if it is available
-
 .EXAMPLE
-   Get-DbaJobOutputFile -SqlServer SERVERNAME -JobName 'The Agent Job' 
+   Get-DbaJobOutputFile -SqlServer SERVERNAME -Jobs 'The Agent Job' 
 
    This will return the paths to the output files for each of the job step of the The Agent Job Job 
    on the SERVERNAME instance  
@@ -33,23 +30,36 @@
    on the SERVERNAME instance   
 
 .EXAMPLE
-   Get-DbaJobOutputFile -SqlServer SERVERNAME -JobName 'The Agent Job' -OpenFile 
+   Get-DbaJobOutputFile -SqlServer SERVERNAME,SERVERNAME2 -Jobs 'The Agent Job'
 
    This will return the paths to the output files for each of the job step of the The Agent Job Job 
-   on the SERVERNAME instance and open the files if they are available
+   on the SERVERNAME instance and SERVERNAME2
 
 .EXAMPLE
    $Servers = 'SERVER','SERVER\INSTANCE1'
-   Get-DbaJobOutputFile -SqlServer $Servers -JobName 'The Agent Job' -OpenFile 
+   Get-DbaJobOutputFile -SqlServer $Servers -Jobs 'The Agent Job' -OpenFile 
 
    This will return the paths to the output files for each of the job step of the The Agent Job Job 
    on the SERVER instance and the SERVER\INSTANCE1 and open the files if they are available
+
+.EXAMPLE 
+   Get-DbaJobOutputFile -SqlServer SERVERNAME  | Out-GridView
+
+   This will return the paths to the output files for each of the job step of all the Agent Jobs
+   on the SERVERNAME instance and Pipe them to Out-GridView
+
+.EXAMPLE 
+   (Get-DbaJobOutputFile -SqlServer SERVERNAME | ogv -PassThru).FileName | Invoke-Item
+
+   This will return the paths to the output files for each of the job step of all the Agent Jobs
+   on the SERVERNAME instance and Pipe them to Out-GridView and enable you to choose the output
+   file and open it
 
 .NOTES
    AUTHOR - Rob Sewell https://sqldbawithabeard.com
    DATE - 30/10/2016
 #>
-[CmdletBinding(SupportsShouldProcess = $true)]
+[CmdletBinding()]
 param
 (# The Server/instance 
         [Parameter(Mandatory=$true,HelpMessage='The SQL Server Instance', 
@@ -65,44 +75,13 @@ param
                    ValueFromPipelineByPropertyName=$true, 
                    ValueFromRemainingArguments=$false, 
                    Position=1)]
-        [System.Management.Automation.PSCredential]$SqlCredential,
-        [Parameter(Mandatory=$false,HelpMessage='Want to open the file')] 
-        [switch]$OpenFile)
+        [System.Management.Automation.PSCredential]$SqlCredential
+)
     DynamicParam { if ($SqlServer) { return (Get-ParamSqlJobs -SqlServer $SqlServer -SqlCredential $SourceSqlCredential) } }
 
 	BEGIN 
     {
-		$jobname = $psboundparameters.Jobs
-        # Create Table Object
-         $table = New-Object system.Data.DataTable $TableName 
-         # Create Columns
-         $col0 = New-Object system.Data.DataColumn Server,([string])
-         $col1 = New-Object system.Data.DataColumn Instance,([string])
-         $col2 = New-Object system.Data.DataColumn Job,([string])
-         $col3 = New-Object system.Data.DataColumn JobStep,([string])
-         $col4 = New-Object system.Data.DataColumn FileName,([string])
-
-         #Add the Columns to the table
-         $table.columns.add($col0)
-         $table.columns.add($col1)
-         $table.columns.add($col2)
-         $table.columns.add($col3)
-         $table.columns.add($col4)
-        function Add-Row 
-        {
-        # Create a new Row
-            $row = $table.NewRow() 
-            # Add values to new row
-            $row.Server = $Server.ComputerNamePhysicalNetBIOS
-            $row.Instance = $Server.Instancename
-            $row.Job = $Job.Name
-            $row.JobStep = $step.Name
-            $row.FileName = $FileName
-          
-            #Add new row to table
-            $table.Rows.Add($row)
-        }
-         
+		$jobname = $psboundparameters.Jobs   
     }
     PROCESS
     {
@@ -113,89 +92,57 @@ param
             if ($JobName)
             {
                 $Job = $server.JobServer.Jobs[$JobName]
-                $Servername = $Server.ComputerNamePhysicalNetBIOS
                 foreach($Step in $Job.JobSteps)
                 {
-                $fileName = $Step.OutputFileName
-                if($fileName -eq '')
-                {
-                    Add-Row 
-                }
-                else
-                {         
-                    if($FileName.StartsWith('\\') -eq $false)
-            {
-                $fileName = '\\' + $Servername + '\' + $Filename.Replace(':','$')
-                Add-Row 
-            }
-            else
-            {
-              Add-Row 
-            }
-            if($OpenFile)
-            {
-                if(Test-Path $fileName)
-                {
-                    If ($Pscmdlet.ShouldProcess("$FileName", "Opening File $FileName"))
-				    {
-                        Invoke-Item $fileName
+                    if($Step.OutputFileName)
+                    {
+                        $fileName = Join-AdminUNC $Server.ComputerNamePhysicalNetBIOS $Step.OutputFileName
+                    }
+                    else
+                    {
+                        $fileName = 'No Output File'
+                    }
+                    [pscustomobject]@{
+                    ComputerName = $Server.ComputerNamePhysicalNetBIOS
+                    InstanceName = $Server.Instancename
+                    SqlInstance = $Server.Name
+                    Job = $Job.Name
+                    JobStep = $step.Name
+                    FileName = $FileName
                     }
                 }
-                else
-                {
-                    Write-Output 'No File to open'
-                }
-            }
-        }
-            }
             }
             else
             {
                 foreach($Job in $Jobs)
                 {
-            $Servername = $Server.ComputerNamePhysicalNetBIOS
-            foreach($Step in $Job.JobSteps)
-            {
-                $fileName = $Step.OutputFileName
-                if($fileName -eq '')
-                {
-                    Add-Row 
-                }
-                else
-                {        
-                    if($FileName.StartsWith('\\') -eq $false)
+                    foreach($Step in $Job.JobSteps)
                     {
-                $fileName = '\\' + $Servername + '\' + $Filename.Replace(':','$')
-                Add-Row 
-            }
-                    else
-                    {
-                Add-Row 
-            }
-                    if($OpenFile)
-                    {
-                if(Test-Path $fileName)
-                {
-                    If ($Pscmdlet.ShouldProcess("$FileName", "Opening File $FileName"))
-				    {
-                        Invoke-Item $fileName
+                        if($Step.OutputFileName)
+                        {
+                            $fileName = Join-AdminUNC $Server.ComputerNamePhysicalNetBIOS $Step.OutputFileName
+                        }
+                        else
+                        {
+                            $fileName = 'No Output File'
+                        }
+                        [pscustomobject]@{
+                        ComputerName = $Server.ComputerNamePhysicalNetBIOS
+                        InstanceName = $Server.Instancename
+                        SqlInstance = $Server.Name
+                        Job = $Job.Name
+                        JobStep = $step.Name
+                        FileName = $FileName
+                        }
                     }
                 }
-                else
-                {
-                    Write-Output 'No File to open'
-                }
-            }
-                }
-            }
-        }
             }             
             $server.ConnectionContext.Disconnect()
         }
     }
     END
     {
-        $table  
+
     }
 }
 
