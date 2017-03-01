566065aacdca17c13243b14b61c796378beeb69c
Shaneis <shaneoneillis@gmail.com>
2017-03-01 00:38:21 +0000

    Handle multiple server instances being passed in

--- functions/Get-DbaTempdbUsage.ps1
+++ functions/Get-DbaTempdbUsage.ps1
@@ -38,41 +38,55 @@
 	Param (
 		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[Alias("ServerInstance", "SqlInstance")]
-		[string]$SqlServer,
+		[string[]]$SqlServer,
 		[object]$SqlCredential,
 		[switch]$Detailed
 	)
 	
 	BEGIN
 	{
-		$server = Connect-SqlServer -SqlServer $sqlserver -SqlCredential $SqlCredential -RegularUser
-		$computername = $server.ComputerNamePhysicalNetBIOS
+
 	}
 	
 	PROCESS
 	{
-        if ($server.VersionMajor -le 9)
+        foreach($SqlServ in $SqlServer)
         {
-            Write-Warning "This function is only supported in SQL Server 2008 or higher."
-            break
-        }
-        
-        if ($Detailed -eq $true)
-        {
-            $sql = 'SELECT t.session_id AS spid, r.command AS statement_command, r.start_time, t.user_objects_alloc_page_count * 8 AS user_object_allocated_space, t.user_objects_dealloc_page_count * 8 AS user_object_deallocated_space, t.internal_objects_alloc_page_count * 8 AS internal_object_allocated_space, t.internal_objects_dealloc_page_count * 8 AS internal_object_deallocated_space, r.reads AS request_reads, r.writes AS request_writes, r.logical_reads AS request_logical_reads, r.cpu_time AS request_cpu_time, s.is_user_process, s.[status], DB_NAME(s.database_id) AS originating_database_name, s.login_name, s.original_login_name, s.nt_domain , s.nt_user_name, s.[host_name], s.[program_name], s.login_time, s.last_request_start_time, s.last_request_end_time FROM sys.dm_db_session_space_usage AS t INNER JOIN sys.dm_exec_sessions AS s ON s.session_id = t.session_id INNER JOIN sys.dm_exec_requests AS r ON r.session_id = s.session_id WHERE t.user_objects_alloc_page_count + t.user_objects_dealloc_page_count + t.internal_objects_alloc_page_count + t.internal_objects_dealloc_page_count > 0'
-            $datatable = $server.ConnectionContext.ExecuteWithResults($sql).Tables
-            return $datatable
-        }
-        else 
-        {
-            $sql = 'SELECT t.session_id AS spid, r.command AS statement_command, r.start_time, t.user_objects_alloc_page_count * 8 AS user_object_allocated_space, t.user_objects_dealloc_page_count * 8 AS user_object_deallocated_space, t.internal_objects_alloc_page_count * 8 AS internal_object_allocated_space, t.internal_objects_dealloc_page_count * 8 AS internal_object_deallocated_space, r.reads AS request_reads, r.writes AS request_writes, r.logical_reads AS request_logical_reads, r.cpu_time AS request_cpu_time, s.is_user_process, s.[status], DB_NAME(s.database_id) AS originating_database_name, s.login_name FROM sys.dm_db_session_space_usage AS t INNER JOIN sys.dm_exec_sessions AS s ON s.session_id = t.session_id INNER JOIN sys.dm_exec_requests AS r ON r.session_id = s.session_id WHERE t.user_objects_alloc_page_count + t.user_objects_dealloc_page_count + t.internal_objects_alloc_page_count + t.internal_objects_dealloc_page_count > 0;'
-            $datatable = $server.ConnectionContext.ExecuteWithResults($sql).Tables
-            return $datatable
+            try
+            {
+		        $server = Connect-SqlServer -SqlServer $SqlServ -SqlCredential $SqlCredential -RegularUser
+            }
+            catch
+            {
+                Write-Warning "Cannot connect to $SqlServ"
+                continue
+            }
+
+            if ($server.VersionMajor -le 9)
+            {
+                Write-Warning "This function is only supported in SQL Server 2008 or higher."
+                continue
+            }
+            
+            if ($Detailed -eq $true)
+            {
+                $sql = 'SELECT t.session_id AS spid, r.command AS statement_command, r.start_time, t.user_objects_alloc_page_count * 8 AS user_object_allocated_space, t.user_objects_dealloc_page_count * 8 AS user_object_deallocated_space, t.internal_objects_alloc_page_count * 8 AS internal_object_allocated_space, t.internal_objects_dealloc_page_count * 8 AS internal_object_deallocated_space, r.reads AS request_reads, r.writes AS request_writes, r.logical_reads AS request_logical_reads, r.cpu_time AS request_cpu_time, s.is_user_process, s.[status], DB_NAME(s.database_id) AS originating_database_name, s.login_name, s.original_login_name, s.nt_domain , s.nt_user_name, s.[host_name], s.[program_name], s.login_time, s.last_request_start_time, s.last_request_end_time FROM sys.dm_db_session_space_usage AS t INNER JOIN sys.dm_exec_sessions AS s ON s.session_id = t.session_id INNER JOIN sys.dm_exec_requests AS r ON r.session_id = s.session_id WHERE t.user_objects_alloc_page_count + t.user_objects_dealloc_page_count + t.internal_objects_alloc_page_count + t.internal_objects_dealloc_page_count > 0'
+                $datatable = $server.ConnectionContext.ExecuteWithResults($sql).Tables
+                return $datatable
+            }
+            else 
+            {
+                $sql = 'SELECT t.session_id AS spid, r.command AS statement_command, r.start_time, t.user_objects_alloc_page_count * 8 AS user_object_allocated_space, t.user_objects_dealloc_page_count * 8 AS user_object_deallocated_space, t.internal_objects_alloc_page_count * 8 AS internal_object_allocated_space, t.internal_objects_dealloc_page_count * 8 AS internal_object_deallocated_space, r.reads AS request_reads, r.writes AS request_writes, r.logical_reads AS request_logical_reads, r.cpu_time AS request_cpu_time, s.is_user_process, s.[status], DB_NAME(s.database_id) AS originating_database_name, s.login_name FROM sys.dm_db_session_space_usage AS t INNER JOIN sys.dm_exec_sessions AS s ON s.session_id = t.session_id INNER JOIN sys.dm_exec_requests AS r ON r.session_id = s.session_id WHERE t.user_objects_alloc_page_count + t.user_objects_dealloc_page_count + t.internal_objects_alloc_page_count + t.internal_objects_dealloc_page_count > 0;'
+                $datatable = $server.ConnectionContext.ExecuteWithResults($sql).Tables
+                return $datatable
+            }
+
+            $SqlServ.ConnectionContext.Disconnect()
         }
 	}
 	
 	END
 	{
-		$server.ConnectionContext.Disconnect()
+
 	}
 }
