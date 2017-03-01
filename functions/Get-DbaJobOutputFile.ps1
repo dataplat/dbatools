d0d4a3d1951f751df4766ac7f8b9b3d272f3c411
Rob Sewell <mrrobsewell@outlook.com>
2017-03-01 12:54:12 +0000

    Added disclaimer

--- functions/Get-DbaJobOutputFile.ps1
+++ functions/Get-DbaJobOutputFile.ps1
@@ -58,6 +58,13 @@
 .NOTES
    AUTHOR - Rob Sewell https://sqldbawithabeard.com
    DATE - 30/10/2016
+
+    dbatools PowerShell module (https://dbatools.io)
+    Copyright (C) 2016 Chrissy LeMaire
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
 #>
 [CmdletBinding()]
 param
@@ -92,6 +99,11 @@ param
             if ($JobName)
             {
                 $Job = $server.JobServer.Jobs[$JobName]
+            }
+            else
+            {
+                foreach($Job in $Jobs)
+                {
                 foreach($Step in $Job.JobSteps)
                 {
                     if($Step.OutputFileName)
--- functions/Set-DbaJobOutputFile.ps1
+++ functions/Set-DbaJobOutputFile.ps1
@@ -28,6 +28,13 @@ Function Set-DbaJobOutPutFile
 .NOTES
    AUTHOR - Rob Sewell https://sqldbawithabeard.com
    DATE - 30/10/2016
+
+    dbatools PowerShell module (https://dbatools.io)
+    Copyright (C) 2016 Chrissy LeMaire
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
 #>
 [CmdletBinding(SupportsShouldProcess = $true)]
 param
