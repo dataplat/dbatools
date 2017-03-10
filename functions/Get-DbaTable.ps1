2da58f91e6205d1692b1dbaa0b78db09bf249c27
Staggerlee011 <stebennettsjb@gmail.com>
2017-03-10 12:00:08 +0000

    Get-DbaTable
    fixes

--- functions/Get-DbaTable.ps1
+++ functions/Get-DbaTable.ps1
@@ -1,9 +1,8 @@
 ﻿function Get-DbaTable
 {
 <#
-<#
 .SYNOPSIS
-Returns a summary of information on the table
+Returns a summary of information on the tables
 .DESCRIPTION
 Shows table information around table row and data sizes and if it has any table type information. 
 .PARAMETER SqlInstance
@@ -12,7 +11,7 @@ collection and recieve pipeline input
 .PARAMETER SqlCredential
 PSCredential object to connect as. If not specified, currend Windows login will be used.
 .PARAMETER Database
-Define the database you wish to search
+Define the databases you wish to search
 .PARAMETER IncludeSystemDBs
 Switch parameter that when used will display system database information
 .PARAMETER Table
@@ -38,6 +37,14 @@ Return all tables in the Test1 database
 .EXAMPLE
 Get-DbaTable -SqlInstance DEV01 -Database MyDB -Table MyTable
 Return only information on the table MyTable from the database MyDB
+.EXAMPLE
+Get-DbaTable -SqlInstance DEV01 -Database MyDB -Table MyTable
+using pipeline
+.EXAMPLE
+Get-DbaTable -SqlInstance DEV01 -Database MyDB -Table MyTable
+with multiple instances
+.EXAMPLE
+
 
 #>
 	[CmdletBinding()]
@@ -46,8 +53,8 @@ Return only information on the table MyTable from the database MyDB
 		[object[]]$SqlInstance,
 		[System.Management.Automation.PSCredential]$SqlCredential,
 		[switch]$IncludeSystemDBs,
-		[switch]$Silent,
-		[string[]]$Table
+		[string[]]$Table,
+		[switch]$Silent
 	)
 	
 	DynamicParam { if ($SqlInstance) { return Get-ParamSqlDatabases -SqlServer $SqlInstance[0] -SqlCredential $SourceSqlCredential } }
@@ -57,6 +64,8 @@ Return only information on the table MyTable from the database MyDB
 		$databases = $psboundparameters.Databases
 		$exclude = $psboundparameters.Exclude
 		
+        $defaultprops = @("Parent", "Schema", "Name", "IndexSpaceUsed", "DataSpaceUsed", "RowCount", "HasClusteredIndex", "IsFileTable", "IsMemoryOptimized", "IsPartitioned", "FullTextIndex", "ChangeTrackingEnabled")
+
 		$fqtns = @()
 		
 		if ($Table)
@@ -65,36 +74,37 @@ Return only information on the table MyTable from the database MyDB
 			{
 				$dotcount = ([regex]::Matches($t, "\.")).count
 				
+                $database = $NULL
+                $Schema = $NULL
+
 				if ($dotcount -eq 1)
 				{
 					$schema = $t.Split(".")[0]
-					$table = $t.Split(".")[1]
+					$tbl = $t.Split(".")[1]
 				}
 				
 				if ($dotcount -eq 2)
 				{
 					$database = $t.Split(".")[0]
 					$schema = $t.Split(".")[1]
-					$table = $t.Split(".")[2]
+					$tbl = $t.Split(".")[2]
 				}
 				
 				$fqtn = [PSCustomObject] @{
 					Database = $database
 					Schema = $Schema
-					Table = $table
+					Table = $tbl
 				}
 				$fqtns += $fqtn
 			}
 		}
+        #$fqtns
 	}
 	
 	PROCESS
 	{
 		foreach ($instance in $sqlinstance)
-		{
-			#For each SQL Server in collection, connect and get SMO object
-			Write-Message -Level Verbose -Message "Connecting to $instance"
-			
+		{	
 			try
 			{
 				Write-Message -Level Verbose -Message "Connecting to $instance"
@@ -102,7 +112,7 @@ Return only information on the table MyTable from the database MyDB
 			}
 			catch
 			{
-				Stop-Function -Message "Failed to connect to: $instance" -Continue -Target $instance
+				Stop-Function -Message "Failed to connect to: $instance" -Continue -Target $instance -InnerErrorRecord $_
 			}
 			
 			#If IncludeSystemDBs is true, include systemdbs
@@ -129,48 +139,47 @@ Return only information on the table MyTable from the database MyDB
 			}
 			catch
 			{
-				Stop-Function -Message "Unable to gather dbs for $instance" -Target $instance -Continue
+				Stop-Function -Message "Unable to gather dbs for $instance" -Target $instance -Continue -InnerErrorRecord $_
 			}
-			
-			foreach ($db in $dbs)
-			{
+            
+            foreach ($db in $dbs)
+		    {
 				Write-Message -Level Verbose -Message "Processing $db"
 				
 				$d = $server.Databases[$db]
-				if ($fqtns)
+				if ($fqtns.Count -gt 0)
 				{
 					foreach ($fqtn in $fqtns)
 					{
-						if ($fqtn.schema)
+                        if ($fqtn.schema -ne $NULL)
 						{
 							try
 							{
-								$db.Tables | Where-Object { $_.name -eq $table -and $_.Schema -eq $schema } | Select-DefaultView -Property Parent, Schema, Name, IndexSpaceUsed, DataSpaceUsed, RowCount, HasClusteredIndex, IsFileTable, IsMemoryOptimized, IsPartitioned, FullTextIndex, ChangeTrackingEnabled
+								$db.Tables | Where-Object { $_.name -eq $tbl -and $_.Schema -eq $schema } | Select-DefaultView -Property $defaultprops
 							}
 							catch
 							{
-								Write-Message -Level Warning "Could not find table name: $($fqtn.table) schema: $($fqtn.schema)"
+								Write-Message -Level Warning -Message "Could not find table name: $($fqtn.tbl) schema: $($fqtn.schema)" -ErrorRecord $_
 							}
 						}
 						else
 						{
 							try
 							{
-								$db.Tables | Where-Object { $_.name -eq $table } | Select-DefaultView -Property Parent, Schema, Name, IndexSpaceUsed, DataSpaceUsed, RowCount, HasClusteredIndex, IsFileTable, IsMemoryOptimized, IsPartitioned, FullTextIndex, ChangeTrackingEnabled
+								$db.Tables | Where-Object { $_.name -eq $tbl } | Select-DefaultView -Property $defaultprops
 							}
 							catch
 							{
-								Write-Message -Level Warning "Could not find table name: $($fqtn.table) schema: $($fqtn.schema)"
+								Write-Message -Level Warning -Message "Could not find table name: $($fqtn.tbl)" -ErrorRecord $_
 							}
 						}
-						
 					}
 				}
 				else
 				{
-					$db.Tables | Select-DefaultView -Property Parent, Schema, Name, IndexSpaceUsed, DataSpaceUsed, RowCount, HasClusteredIndex, IsFileTable, IsMemoryOptimized, IsPartitioned, FullTextIndex, ChangeTrackingEnabled
+					$db.Tables | Select-DefaultView -Property $defaultprops
 				}
 			}
-		}
+        }
 	}
 }
