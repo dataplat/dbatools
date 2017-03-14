31533b27f12862edf43ca353ce2e0f91e6f5fbd4
Chrissy LeMaire <ctrlbold@users.noreply.github.com>
2017-03-14 22:10:29 +0100

    added full object, additional details and updated help

--- functions/Test-DbaLinkedServerConnection.ps1
+++ functions/Test-DbaLinkedServerConnection.ps1
@@ -7,7 +7,7 @@ Test all linked servers from the sql servers passed
 .DESCRIPTION
 Test each linked server on the instance
 
-.PARAMETER SqlServer
+.PARAMETER SqlInstance
 The SQL Server that you're connecting to.
 
 .PARAMETER SqlCredential
@@ -21,16 +21,31 @@ Copyright (C) 2017 Chrissy LeMaire
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 	
 .LINK
 https://dbatools.io/Test-DbaLinkedServerConnection
 
 .EXAMPLE
-Test-DbaLinkedServerConnection -SqlServer DEV01
+Test-DbaLinkedServerConnection -SqlInstance DEV01
 
 Test all Linked Servers for the SQL Server instance DEV01
 
+.EXAMPLE
+Test-DbaLinkedServerConnection -SqlInstance sql2016 | Out-File C:\temp\results.txt
+
+Test all Linked Servers for the SQL Server instance sql2016 and output results to file
+
+.EXAMPLE
+Test-DbaLinkedServerConnection -SqlInstance sql2016, sql2014, sql2012
+
+Test all Linked Servers for the SQL Server instances sql2016, sql2014 and sql2012
+
+.EXAMPLE
+$servers = "sql2016","sql2014","sql2012"
+$servers | Test-DbaLinkedServerConnection -SqlCredential (Get-Credential sqladmin)
+
+Test all Linked Servers for the SQL Server instances sql2016, sql2014 and sql2012 using SQL login credentials
+	
 #>
 	[CmdletBinding()]
 	param (
@@ -55,22 +70,29 @@ Test all Linked Servers for the SQL Server instance DEV01
 			}
 			
 			foreach ($ls in $server.LinkedServers)
-			{ 
-      				if ($LSname -ne $null)
-      				{
-       					try
-       					{
-        					$LSname.testconnection()                     
-        					$Connectivity = $true
-        					"$(Get-Date) $ServerName $LSname connection success." | Out-File $OutputFile -Append
-       					} 
-       					catch 
-       					{    
-       						$Connectivity = $false
-       						"$(Get-Date) $ServerName $LSname connection failure." | Out-File $OutputFile -Append
-       					}
-      				}
-     			}
+			{
+				try
+				{
+					$null = $ls.TestConnection()
+					$result = "Success"
+					$connectivity = $true
+				}
+				catch
+				{
+					$result = $_.Exception.InnerException
+					$connectivity = $false
+				}
+				
+				[PSCustomObject]@{
+					ComputerName = $server.NetName
+					InstanceName = $server.ServiceName
+					SqlInstance = $server.DomainInstanceName
+					LinkedServerName = $ls.Name
+					RemoteServer = $ls.DataSource
+					Connectivity = $connectivity
+					Result = $result
+				} | Select-DefaultView -ExcludeProperty ComputerName, InstanceName
+			}
 		}
 	}
 }
