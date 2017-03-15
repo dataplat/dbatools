3b708724c46018ef73a9ff1e7180e970374b1d87
niphlod <niphlod@gmail.com>
2017-03-15 22:41:51 +0100

    fixes #959
    Everything else is just cosmethics, in the wait of a general
    style-rewrite

--- functions/Test-DbaValidLogin.ps1
+++ functions/Test-DbaValidLogin.ps1
@@ -1,21 +1,21 @@
 Function Test-DbaValidLogin
 {
 <#
-.SYNOPSIS 
-Test-DbaValidLogin Finds any logins on SQL instance that are AD logins with either disabled AD user accounts or ones that nolonger exist
+.SYNOPSIS
+Test-DbaValidLogin finds any logins on SQL instance that are AD logins with either disabled AD user accounts or ones that no longer exist
 
 .DESCRIPTION
 The purpose of this function is to find SQL Server logins that are used by active directory users that are either disabled or removed from the domain. It allows you to 
-keep your logins accurate and up to date by removing accounts that are no longer needed. 
+keep your logins accurate and up to date by removing accounts that are no longer needed.
 
 .PARAMETER SQLServer
-SQL Server.You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+SQL instance to check. You must have sysadmin access and server version must be SQL Server version 2000 or greater.
 
 .PARAMETER SqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted.
 
 .PARAMETER Logins
-Filters the results to only the login you wish 
+Filters the results to only the login you wish
 
 .PARAMETER Exclude
 Excludes any login you pass into it from the results.
@@ -24,12 +24,12 @@ Excludes any login you pass into it from the results.
 By default the function returns both Logins and Groups. you can use the FilterBy parameter to only return Groups (GroupsOnly) or Logins (LoginsOnly)
 
 .PARAMETER ExcludeDomains
-Bu default we tranverse all domains in the forest and all trusted domains. You can exclude domains by adding them to the ExcludeDomains
+By default we traverse all domains in the forest and all trusted domains. You can exclude domains by adding them to the ExcludeDomains
 
 .PARAMETER Detailed
-Returns a more detailed result, showing if the login on SQL Server is enabled or disabled and what type of account it is in AD 
+Returns a more detailed result, showing if the login on SQL Server is enabled or disabled and what type of account it is in AD
 
-.NOTES 
+.NOTES
 Author: Stephen Bennett: https://sqlnotesfromtheunderground.wordpress.com/
 Author: Chrissy LeMaire (@cl), netnerds.net
 
@@ -43,12 +43,12 @@ You should have received a copy of the GNU General Public License along with thi
 .LINK
 https://dbatools.io/Test-DbaValidLogin
 
-.EXAMPLE   
+.EXAMPLE
 Test-DbaValidLogin -SqlServer Dev01
 
 Tests all logins in the domain ran from (check $env:domain) that are either disabled or do not exist
 
-.EXAMPLE   
+.EXAMPLE
 Test-DbaValidLogin -SqlServer Dev01 -FilterBy GroupsOnly -Detailed
 
 Tests all Active directory groups that have logins on Dev01 returning a detailed view.
@@ -70,9 +70,9 @@ Tests all logins excluding any that are from the mydomain Domain
 		[string[]]$ExcludeDomains,
 		[switch]$Detailed
 	)
-	
+
 	DynamicParam { if ($SqlServer) { return Get-ParamSqlLogins -SqlServer $SqlServer[0] -SqlCredential $SqlCredential -WindowsOnly } }
-	
+
 	BEGIN
 	{
 		function ConvertTo-Dn ([string]$dns)
@@ -91,7 +91,7 @@ Tests all logins excluding any that are from the mydomain Domain
 			$alldomains = $domains = @()
 			$currentforest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
 			$alldomains += $currentforest.Domains.name | Where-Object { $_ -notin $excludedomains }
-			
+
 			$cd = $currentforest.Domains | Where-Object { $_.name -notin $excludedomains }
 		}
 		catch
@@ -99,8 +99,8 @@ Tests all logins excluding any that are from the mydomain Domain
 			Write-warning "No Active Directory domains Found."
 			break
 		}
-	
-	foreach ($domain in $cd)
+
+		foreach ($domain in $cd)
 		{
 			try
 			{
@@ -112,9 +112,9 @@ Tests all logins excluding any that are from the mydomain Domain
 				Write-Warning "Couldn't contact $domain"
 			}
 		}
-		
+
 		$alldomains = $alldomains | Select-Object -Unique
-		
+
 		foreach ($domain in $alldomains)
 		{
 			try
@@ -125,7 +125,7 @@ Tests all logins excluding any that are from the mydomain Domain
 				$reflection.InvokeMember("Init", "InvokeMethod", $Null, $translate, (3, $Null))
 				$reflection.InvokeMember("Set", "InvokeMethod", $Null, $translate, (1, $dn))
 				$netbios = $reflection.InvokeMember("Get", "InvokeMethod", $Null, $translate, 3).Trim("\")
-				
+
 				$domains += [pscustomobject]@{
 					DNS = $domain
 					DN = $dn
@@ -138,11 +138,11 @@ Tests all logins excluding any that are from the mydomain Domain
 				Write-Warning "Removing $domain from domain list"
 			}
 		}
-		
+
 		$Logins = $psboundparameters.Logins
 		$Exclude = $psboundparameters.Exclude
 	}
-	
+
 	PROCESS
 	{
 		foreach ($instance in $sqlserver)
@@ -157,7 +157,7 @@ Tests all logins excluding any that are from the mydomain Domain
 				Write-Warning "Failed to connect to: $instance"
 				continue
 			}
-			
+
 			if ($Logins)
 			{
 				$windowslogins = $server.Logins | Where-Object { $Logins -contains $_.Name }
@@ -187,26 +187,24 @@ Tests all logins excluding any that are from the mydomain Domain
 						$windowsGroups = $allwindowsloginsgroups | Where-Object { $_.LoginType -eq 'WindowsGroup' }
 						$windowsGroups = $windowsGroups | Where-Object { $_.Name.StartsWith("NT ") -eq $false -and $_.Name -notmatch $SqlServer.Split("\\")[0] -and $_.Name.StartsWith("BUILTIN") -eq $false }
 					}
-					
+
 				}
 			}
-			
+
 			if ($exclude)
 			{
 				$windowslogins = $windowslogins | Where-Object { $Logins -notcontains $_.Name }
 				$windowsGroups = $windowsGroups | Where-Object { $Logins -notcontains $_.Name }
-				
 			}
-			
+
 			foreach ($login in $windowslogins)
 			{
-				
 				$adlogin = $login.Name
 				Write-Verbose "Parsing Login $adlogin"
 				$domain, $username = $adlogin.Split("\")
-				$filter = "(&(objectCategory=User)(sAMAccountName=$username))" # won't work with groups			
+				$filter = "(&(objectCategory=User)(sAMAccountName=$username))" # won't work with groups
 				Write-Verbose $filter
-				
+
 				if ($env:USERDOMAIN -eq $domain)
 				{
 					$searcher = New-Object System.DirectoryServices.DirectorySearcher
@@ -220,19 +218,23 @@ Tests all logins excluding any that are from the mydomain Domain
 					$searcher.SearchRoot = $ad
 					$searcher.Filter = $filter
 				}
+				$exists = $false
 				try
 				{
 					$founduser = $searcher.findOne()
+					if ($founduser) {
+						$exists = $true
+					}
 				}
 				catch
 				{
 					Write-Warning "AD Searcher Error for $username"
 				}
 				$value = $founduser.Properties.useraccountcontrol
-				
-				$enabled = $exists = $false
+
+				$enabled = $false
 				$adlogindetails = 'unknown'
-				
+
 				## values from  http://www.netvision.com/ad_useraccountcontrol.php
 				switch ($value)
 				{
@@ -300,23 +302,21 @@ Tests all logins excluding any that are from the mydomain Domain
 						$enabled = $false
 						$adlogindetails = 'Disabled, Smartcard Required, Password Doesnt Expire & Not Required'
 					}
-					590336
-					{
+					590336 {
 						$enabled = $true
 						$adlogindetails = 'Enabled, User Cannot Change Password & Password Never Expires'
 					}
 					$null
 					{
-						$exists = $true
+						$enabled = 'Unknown'
 					}
 					default
 					{
 						Write-Verbose "unknown value passed from useraccountcontrol Server: $sqlServer Login: $username Domain: $domain Value: $value"
-						$exists = 'Unknown'
 						$enabled = 'Unknown'
 					}
 				}
-				
+
 				if ($Detailed)
 				{
 					[PSCustomObject]@{
@@ -324,7 +324,7 @@ Tests all logins excluding any that are from the mydomain Domain
 						Domain = $domain
 						Login = $username
 						Type = "User"
-						Found = $exists -ne $true
+						Found = $exists
 						Enabled = $enabled
 						DisabledInSQLServer = $login.IsDisabled
 						ADLoginDetails = $adlogindetails
@@ -337,20 +337,20 @@ Tests all logins excluding any that are from the mydomain Domain
 						Domain = $domain
 						Login = $username
 						Type = "User"
-						Found = $exists -ne $true
+						Found = $exists
 						Enabled = $enabled
 					}
 				}
 			} # foreach login
-			
+
 			foreach ($login in $windowsGroups)
 			{
 				$adlogin = $login.Name
 				Write-Verbose "Parsing Group $adlogin"
 				$domain, $username = $adlogin.Split("\")
-				$filter = "(&(objectCategory=group)(sAMAccountName=$username))" # won't work with groups			
+				$filter = "(&(objectCategory=group)(sAMAccountName=$username))" # won't work with users
 				Write-Verbose $filter
-				
+
 				if ($env:USERDOMAIN -eq $domain)
 				{
 					$searcher = New-Object System.DirectoryServices.DirectorySearcher
@@ -372,16 +372,12 @@ Tests all logins excluding any that are from the mydomain Domain
 				{
 					Write-Warning "AD Searcher Error for $username on $SqlServer"
 				}
-				
+
 				$enabled = $exists = $false
-				
+
 				if ($founduser)
 				{
-					$enabled = $true
-				}
-				else
-				{
-					$exists = $true
+					$enabled = $exists = $true
 				}
 				if ($Detailed)
 				{
@@ -390,7 +386,7 @@ Tests all logins excluding any that are from the mydomain Domain
 						Domain = $domain
 						Login = $username
 						Type = "Group"
-						Found = $exists -ne $true
+						Found = $exists
 						Enabled = $enabled
 						DisabledInSQLServer = $login.IsDisabled
 						ADLoginDetails = 'AD group'
@@ -403,7 +399,7 @@ Tests all logins excluding any that are from the mydomain Domain
 						Domain = $domain
 						Login = $username
 						Type = "Group"
-						Found = $exists -ne $true
+						Found = $exists
 						Enabled = $enabled
 					}
 				}
