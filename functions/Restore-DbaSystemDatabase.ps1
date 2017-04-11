4f241a920137bbbf1097d8ef65f38df2f291603b
Stuart Moore <stuart@stuart-moore.com>
2017-04-11 11:08:04 +0100

    Adding help and examples to Restore-DbaSysDatabases
    Keeping the pester monster happy

--- functions/Restore-DbaSystemDatabase.ps1
+++ functions/Restore-DbaSystemDatabase.ps1
@@ -1,5 +1,71 @@
 function Restore-DbaSystemDatabase
 {
+    <#
+.SYNOPSIS
+Restores the SQL Server system databases (master, mode, msdb)
+.DESCRIPTION
+Performs all the actions required for restoring SQL Server system databases
+
+For master the SQL Server instance will be started in single user mode to allow the restore
+For msdb or model, the SQL Agent service will be stopped to allow exclusive access and then restarted afterwards
+
+Startup parameters will be modified, but an existing ones will be push back in after success (or failure)
+
+.PARAMETER SqlInstance
+The SQL Server instance targetted for restores
+
+.PARAMETER SqlCredential
+SQL Server credential (Windows or SQL Accounnt) with permission to log on to the SQL instance to perform the restores
+
+.PARAMETER Credential
+Windows credential with permission to log on to the server running the SQL instance (this is required for the stop/start action). If not present, the account running the function's credentials will be used
+
+.PARAMETER BackupPath
+Path to the backup files to be used for the restore. Multiple paths can be specified 
+
+.PARAMETER RestoreTime
+DateTime parameter to say to which point in time the system database(s) should be restored to
+
+.PARAMETER master
+Switch to indicate that the master database should be restored
+
+.PARAMETER model
+Switch to indicate that the model database should be restored
+
+.PARAMETER msdb
+Switch to indicate that the msdb database should be restored
+
+.NOTES
+Original Author: Stuart Moore (@napalmgram), stuart-moore.com
+
+dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+Copyright (C) 2016 Chrissy LeMaire
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+.EXAMPLE 
+Restore-DbaSystemDatabase -SqlServer server1\prod1 -BackupPath \\server2\backups\master\master_20170411.bak -master
+
+This will restore the master database on the server1\prod1 instance from the master db backup in \\server2\backups\master\master_20170411.bak
+
+.EXAMPLE
+Restore-DbaSystemDatabase -SqlServer server1\prod1 -BackupPath \\server2\backups\msdb\msdb_20170411.bak -msdb
+
+This will restore the msdb database on the server1\prod1 instance from the msdb db backup in \\server2\backups\master\master_20170411.bak
+
+.EXAMPLE
+Restore-DbaSystemDatabase -SqlServer server1\prod1 -BackupPath \\server2\backups\master\,\\server2\backups\model\,\\server2\backups\msdb\  -msdb -model -master
+
+This will restore the master, model and msdb on server1\prod1 to the most recent points in the backups in \\server2\backups\master\,\\server2\backups\model\,\\server2\backups\msdb\ respectively
+
+.EXAMPLE
+Restore-DbaSystemDatabase -SqlServer server1\prod1 -BackupPath \\server2\backups\master\,\\server2\backups\model\,\\server2\backups\msdb\  -msdb -model -master -RestoreTime (Get-Date).AddHours(-2)
+
+This will restore the master, model and msdb on server1\prod1 to a point in time 2 hours ago from the backups in \\server2\backups\master\,\\server2\backups\model\,\\server2\backups\msdb\ respectively
+
+#>
     [CmdletBinding(SupportsShouldProcess=$true)]
 	param ([parameter(ValueFromPipeline, Mandatory = $true)]
 		[Alias("ServerInstance", "SqlInstance")]
@@ -7,10 +73,10 @@ function Restore-DbaSystemDatabase
 		[PSCredential]$Credential,
         [PSCredential]$SqlCredential,
         [String[]]$BackupPath,
-        [DateTime]$RestorePoint,
-        [switch]$master,
-        [Switch]$model,
-        [Switch]$msdb
+        [DateTime]$RestoreTime,
+        [switch]$Master,
+        [Switch]$Model,
+        [Switch]$Msdb
 	)
 
     $FunctionName =(Get-PSCallstack)[0].Command
