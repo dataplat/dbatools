5b698dccbc03d8b22ce5594767eb7bf554f3be9d
ctrlbold <clemaire@gmail.com>
2017-04-14 00:49:41 +0200

    updated notes

--- functions/Export-DbaOperator.ps1
+++ functions/Export-DbaOperator.ps1
@@ -56,9 +56,10 @@
 
 	.NOTES
 	Tags: Migration, Backup
-
-	dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-	Copyright (C) 2016 Chrissy LeMaire
+	
+	Website: https://dbatools.io
+	Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+	License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 	.LINK
 	https://dbatools.io/Export-DbaOperator
