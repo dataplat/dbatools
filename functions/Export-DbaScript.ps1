44c1777249158b030aa42be7ae39b3553dbe6c56
ctrlbold <clemaire@gmail.com>
2017-04-30 15:59:11 +0200

    added more info

--- functions/Export-DbaScript.ps1
+++ functions/Export-DbaScript.ps1
@@ -111,7 +111,7 @@
 				$shortype = $typename.Split(".")[-1]
 			}
 			else {
-				Stop-Function -Message "InputObject is not a SQL Management Object" -Silent $Silent -Category InvalidData -Continue -Target $object
+				Stop-Function -Message "InputObject is of type $typename which is not a SQL Management Object. Only SMO objects are supported." -Silent $Silent -Category InvalidData -Continue -Target $object
 			}
 			
 			if ($shortype -in "LinkedServer", "Credential", "Login") {
@@ -188,7 +188,7 @@
 			}
 			
 			if (!$passthru) {
-				Write-Message -Level Output -Message "Completed export for $object on $server"
+				Write-Message -Level Output -Message "Exported $object on $server to $actualpath"
 			}
 		}
 	}
