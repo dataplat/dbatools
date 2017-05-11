f4da850c689ddc8aebbf795665f8f6900af4efb3
ctrlbold <clemaire@gmail.com>
2017-05-11 15:00:14 +0200

    udpates

--- functions/Import-DbaCertificate.ps1
+++ functions/Import-DbaCertificate.ps1
@@ -1,17 +1,16 @@
-Function Import-DbaCertificate
-{
+Function Import-DbaCertificate {
 <#
 .SYNOPSIS
-Imports certificates from .cer files using smo.
+Imports certificates from .cer files using SMO.
 
 .DESCRIPTION
-Imports certificates from.cer files using smo.
+Imports certificates from.cer files using SMO.
 
-.PARAMETER SqlServer
+.PARAMETER SqlInstance
 The SQL Server to create the certificates on.
 
 .PARAMETER Path
-The Path the contains the certificate and private key files.
+The Path the contains the certificate and private key files. The path can be a directory or a specific certificate.
 
 .PARAMETER SqlCredential
 Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
@@ -19,9 +18,6 @@ Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integ
 $scred = Get-Credential, this pass $scred object to the param. 
 
 Windows Authentication will be used if DestinationSqlCredential is not specified. To connect as a different Windows user, run PowerShell as that user.	
-
-.PARAMETER Certificates
-Imports just the certificates specified.
 
 .PARAMETER Password
 Secure string used to decrypt the private key.
@@ -31,6 +27,9 @@ Shows what would happen if the command were to run. No actions are actually perf
 
 .PARAMETER Confirm 
 Prompts you for confirmation before executing any changing operations within the command. 
+
+.PARAMETER Silent 
+Use this switch to disable any kind of verbose messages
 
 .NOTES
 Original Author: Jess Pomfret (@jpomfret)
@@ -44,59 +43,65 @@ License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 Import-DbaCertificate -SqlServer Server1 -Path \\Server1\Certificates -password (ConvertTo-SecureString -force -AsPlainText GoodPass1234!!)
 Imports all the certificates in the specified path.
 
-.EXAMPLE
-Import-DbaCertificate -SqlServer Server1 -Path \\Server1\Certificates -Certificates "CertTDE"
-Prompts for password then imports certificate in the specified path named 'CertTDE'
-
 #>
 	[CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
 	param (
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
-		[Alias("ServerInstance","SqlInstance")]
-        [object]$SqlServer,
-        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Alias("ServerInstance", "SqlServer")]
+		[object]$SqlInstance,
+		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[object[]]$Path,
-        [System.Management.Automation.PSCredential]$SqlCredential,
-		[Array]$Certificates,
-		[Security.SecureString] $Password = (Read-Host "Password" -AsSecureString),
-		[switch]$Silent	
+		[System.Management.Automation.PSCredential]$SqlCredential,
+		[Security.SecureString]$Password = (Read-Host "Password" -AsSecureString),
+		[switch]$Silent
 	)
-
-	BEGIN {
-		$server = Connect-SqlServer $SqlServer $SqlCredential
+	
+	begin {
+		function new-smocert ($directory, $certname) {
+			if ($Pscmdlet.ShouldProcess("$cert on $SqlInstance", "Importing Certificate")) {
+				$smocert = New-Object $cert Microsoft.SqlServer.Management.Smo.Certificate
+				$smocert.Name = $certname
+				$smocert.Parent = $server.Databases['master']
+				Write-Message -Level Verbose -Message ("Creating Certificate: $certname")
+				try {
+					$smocert.Create("$directory\$certname.cer", 1, "$directory\$certname.pvk", [System.Runtime.InteropServices.marshal]::PtrToStringAuto([System.Runtime.InteropServices.marshal]::SecureStringToBSTR($password)))
+				}
+				catch {
+					Write-Message -Level Warning -Message $_ -ErrorRecord $_ -Target $instance
+				}
+			}
+		}
+		
+		try {
+			Write-Message -Level Verbose -Message "Connecting to $SqlInstance"
+			$server = Connect-SqlServer -SqlServer $SqlInstance -SqlCredential $sqlcredential
+		}
+		catch {
+			Stop-Function -Message "Failed to connect to: $SqlInstance" -Continue -Target $SqlInstance -InnerErrorRecord $_
+		}
 	}
 	
-	PROCESS {
-        if (!$path.StartsWith('\')) {
-            Stop-Function -Message "Path should be a UNC share." -Continue
-        }
-
-        $Path = $Path.TrimEnd('\')
-
-        if(!$Certificates) {
-            $Certificates =  Get-ChildItem \\svtsqlrestore\BackupTest\cert *.cer | Select-Object -Expand Basename
-        }
-
-        foreach($Certificate in $Certificates) {
-
-            if ($Pscmdlet.ShouldProcess("[$certificate]' on $SqlServer", "Importing Certificate")) {
-                $Cert = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Certificate
-                $Cert.name = $Certificate
-                $Cert.parent = $Server.Databases['Master']
-                Write-Message -Level Verbose -Message ("Creating Certificate: {0}" -f $Certificate)
-                try {
-                    $Cert.Create("$Path\$Certificate.cer", 1, "$Path\$Certificate.pvk", [System.Runtime.InteropServices.marshal]::PtrToStringAuto([System.Runtime.InteropServices.marshal]::SecureStringToBSTR($password)))
-                } catch {
-                    Write-Message -Level Warning -Message $_ -ErrorRecord $_ -Target $instance
-                }
-            }
-        }    
-
-    }
-    END {
-		$server.ConnectionContext.Disconnect()
-
+	process {
+		if (Test-FunctionInterrupt) { return }
+		
+		foreach ($fullname in $path) {
+			
+			if (![dbavalidate]::IsLocalhost($SqlInstance) -and !$fullname.StartsWith('\')) {
+				Stop-Function -Message "Path must be a UNC share when SQLInstance is not local." -Continue
+			}
+			
+			$item = Get-Item $fullname
+			
+			if ($item -is [System.IO.DirectoryInfo]) {
+				foreach ($cert in (Get-ChildItem $fullname\* -Include *.crt, *.cer)) {
+					new-smocert -directory $fullname -certname $cert.BaseName
+				}
+			}
+			else {
+				$directory = Split-Path $fullname
+				new-smocert -directory $directory -certname $item.BaseName
+			}
+		}
 	}
-
 }
