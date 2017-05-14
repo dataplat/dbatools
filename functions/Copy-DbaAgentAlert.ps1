3bc90d6dc0c5016a695f7aaa20ba96a6802ecc52
Shawn Melton <wshawnmelton@gmail.com>
2017-05-14 06:27:22 -0500

    Add DbaDeprecation

--- functions/Copy-DbaAgentAlert.ps1
+++ functions/Copy-DbaAgentAlert.ps1
@@ -262,6 +262,7 @@ Shows what would happen if the command were executed using force.
 	{
 		$sourceserver.ConnectionContext.Disconnect()
 		$destserver.ConnectionContext.Disconnect()
-		If ($Pscmdlet.ShouldProcess("console", "Showing finished message")) { Write-Output "Alert migration finished" }
+        If ($Pscmdlet.ShouldProcess("console", "Showing finished message")) { Write-Output "Alert migration finished" }
+        Test-DbaDeprecation -DeprecatedOn "1.0.0" -Alias Copy-SqlAlert
 	}
 }
