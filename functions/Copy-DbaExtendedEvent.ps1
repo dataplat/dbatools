31fba35b6f43bf8ba74691a1e206dc8b22c7dfda
Shawn Melton <wshawnmelton@gmail.com>
2017-05-14 06:29:20 -0500

    Add DbaDeprecation

--- functions/Copy-DbaExtendedEvent.ps1
+++ functions/Copy-DbaExtendedEvent.ps1
@@ -187,7 +187,8 @@ Copies two Extended Events, CheckQueries and MonitorUserDefinedException, from s
 	{
 		$sourceserver.ConnectionContext.Disconnect()
 		$destserver.ConnectionContext.Disconnect()
-		If ($Pscmdlet.ShouldProcess("console", "Showing finished message")) { Write-Output "Extended Event migration finished" }
+        If ($Pscmdlet.ShouldProcess("console", "Showing finished message")) { Write-Output "Extended Event migration finished" }
+        Test-DbaDeprecation -DeprecatedOn "1.0.0" -Alias Copy-SqlExtendedEvent
 	}
 }
 
