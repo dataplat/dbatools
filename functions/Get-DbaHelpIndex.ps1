1b553fb0e3ae5c085298fa832006d51ac53adab9
ctrlbold <clemaire@gmail.com>
2017-05-17 13:35:14 +0200

    tepp

--- functions/Get-DbaHelpIndex.ps1
+++ functions/Get-DbaHelpIndex.ps1
@@ -54,13 +54,9 @@ Returns the numerical data in a more user readable format (numerical separator w
 
 .NOTES 
 Original Author: Nic Cain, https://sirsql.net/
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program.  if not, see <http://www.gnu.org/licenses/>.
-You should have received a copy of the GNU General Public License
-along with this program.  if not, see <http://www.gnu.org/licenses/>.	
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .LINK
 https://dbatools.io/Get-DbaHelpIndex
