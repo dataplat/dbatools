acbd7ecb9f8673874ad3e146a7b64745497da252
Shawn Melton <wshawnmelton@gmail.com>
2017-05-17 00:19:45 -0500

    Updates to Test-DbaVirtualLogFile

--- functions/Test-DbaVirtualLogFile.ps1
+++ functions/Test-DbaVirtualLogFile.ps1
@@ -1,6 +1,5 @@
-function Test-DbaVirtualLogFile
-{
-<#
+function Test-DbaVirtualLogFile {
+    <#
 .SYNOPSIS
 Returns database virtual log file information for database files on a SQL instance.
 
@@ -21,28 +20,24 @@ SQLServer name or SMO object representing the SQL Server to connect to. This can
 .PARAMETER SqlCredential
 PSCredential object to connect under. If not specified, current Windows login will be used.
 
-.PARAMETER IncludeSystemDBs
-Switch parameter that when used will display system database information
-
-.PARAMETER Databases
-Specify one or more databases to process.
+.PARAMETER Database
+The database(s) to process - this list is autopopulated from the server. If unspecified, all databases will be processed.
 
 .PARAMETER Exclude
-Specify one or more databases to exclude.
+The database(s) to exclude - this list is autopopulated from the server
+
+.PARAMETER IncludeSystemDBs
+Switch parameter that when used will display system database information
 
 .PARAMETER Detailed
 Returns all information provided by DBCC LOGINFO plus the server name and database name
 
 .NOTES
 Tags: DisasterRecovery, Backup
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .LINK
 https://dbatools.io/Test-DbaVirtualLogFile
@@ -67,105 +62,86 @@ Test-DbaVirtualLogFile -SqlServer sqlcluster -Databases db1, db2
 
 Returns VLF counts for the db1 and db2 databases on sqlcluster.
 #>
-	[CmdletBinding()]
-	[OutputType([System.Collections.ArrayList])]
-	param ([parameter(ValueFromPipeline, Mandatory = $true)]
-		[Alias("ServerInstance", "SqlInstance")]
-		[object[]]$SqlServer,
-		[System.Management.Automation.PSCredential]$SqlCredential,
-		[switch]$IncludeSystemDBs,
-		[switch]$Detailed
-	)
+    [CmdletBinding()]
+    [OutputType([System.Collections.ArrayList])]
+    param ([parameter(ValueFromPipeline, Mandatory = $true)]
+        [Alias("ServerInstance", "SqlInstance")]
+        [object[]]$SqlServer,
+        [System.Management.Automation.PSCredential]$SqlCredential,
+        [Alias("Databases")]
+        [object[]]$Database,
+        [object[]]$Exclude,
+        [switch]$IncludeSystemDBs,
+        [switch]$Detailed
+    )
 
-	DynamicParam { if ($SqlServer) { return Get-ParamSqlDatabases -SqlServer $SqlServer[0] -SqlCredential $SqlCredential } }
+    PROCESS {
+        foreach ($servername in $SqlServer) {
+            Write-Verbose "Connecting to $servername"
+            try {
+                $server = Connect-SqlServer $servername -SqlCredential $SqlCredential
+            }
+            catch {
+                Write-Warning "Can't connect to $instance, skipping..."
+                Continue
+            }
 
-	BEGIN
-	{
-		$databases = $psboundparameters.Databases
-		$exclude = $psboundparameters.Exclude
-	}
+            $dbs = $server.Databases
+            #If IncludeSystemDBs is true, include systemdbs
+            #only look at online databases (Status equal normal)
 
-	PROCESS
-	{
-		foreach ($servername in $SqlServer)
-		{
-			Write-Verbose "Connecting to $servername"
-			try {
-				$server = Connect-SqlServer $servername -SqlCredential $SqlCredential
-			}
-			catch
-			{
-				Write-Warning "Can't connect to $instance, skipping..."
-				Continue
-			}
+            if ($database.count -gt 0) {
+                $dbs = $dbs | Where-Object { $database -contains $_.Name }
+            }
+            if ($Exclude) {
+                $dbs = $dbs | Where-Object Name -notin $Exclude
+            }
 
-			$dbs = $server.Databases
-			#If IncludeSystemDBs is true, include systemdbs
-			#only look at online databases (Status equal normal)
+            if ($IncludeSystemDBs) {
+                $dbs = $dbs | Where-Object { $_.status -eq 'Normal' }
+            }
+            else {
+                $dbs = $dbs | Where-Object { $_.status -eq 'Normal' -and $_.IsSystemObject -eq 0 }
+            }
 
-			if ($databases.count -gt 0)
-			{
-				$dbs = $dbs | Where-Object { $databases -contains $_.Name }
-			}
-			if ($exclude.count -gt 0)
-			{
-				$dbs = $dbs | Where-Object { $exclude -notcontains $_.Name }
-			}
+            foreach ($db in $dbs) {
+                try {
+                    Write-Verbose "Querying $($db.name) on $servername."
+                    #Execute query against individual database and add to output
 
-			if ($IncludeSystemDBs)
-			{
-				$dbs = $dbs | Where-Object { $_.status -eq 'Normal' }
-			}
-			else
-			{
-				$dbs = $dbs | Where-Object { $_.status -eq 'Normal' -and $_.IsSystemObject -eq 0 }
-			}
+                    if ($Detailed -eq $true) {
+                        $table = New-Object System.Data.Datatable
+                        $servercolumn = $table.Columns.Add("Server")
+                        $servercolumn.DefaultValue = $server.name
+                        $dbcolumn = $table.Columns.Add("Database")
+                        $dbcolumn.DefaultValue = $db.name
 
-			foreach ($db in $dbs)
-			{
-				try
-				{
-					Write-Verbose "Querying $($db.name) on $servername."
-					#Execute query against individual database and add to output
+                        $temptable = $db.ExecuteWithResults("DBCC LOGINFO").Tables
 
-					if ($Detailed -eq $true)
-					{
-						$table = New-Object System.Data.Datatable
-						$servercolumn = $table.Columns.Add("Server")
-						$servercolumn.DefaultValue = $server.name
-						$dbcolumn = $table.Columns.Add("Database")
-						$dbcolumn.DefaultValue = $db.name
+                        foreach ($column in $temptable.Columns) {
+                            $null = $table.Columns.Add($column.ColumnName)
+                        }
 
-						$temptable = $db.ExecuteWithResults("DBCC LOGINFO").Tables
-
-						foreach ($column in $temptable.Columns)
-						{
-							$null = $table.Columns.Add($column.ColumnName)
-						}
-
-						foreach ($row in $temptable.rows)
-						{
-							$table.ImportRow($row)
-						}
+                        foreach ($row in $temptable.rows) {
+                            $table.ImportRow($row)
+                        }
 						
-						$table
-					}
-					else
-					{
-						[PSCustomObject]@{
-								Server = $server.name
-								Database = $db.name
-								Count = $db.ExecuteWithResults("DBCC LOGINFO").Tables.Rows.Count
-							}
-					}
-				}
-				catch
-				{
-					Write-Exception $_
-					Write-Warning "Unable to query $($db.name) on $servername"
-					continue
-				}
-			}
-		}
-	}
+                        $table
+                    }
+                    else {
+                        [PSCustomObject]@{
+                            Server   = $server.name
+                            Database = $db.name
+                            Count    = $db.ExecuteWithResults("DBCC LOGINFO").Tables.Rows.Count
+                        }
+                    }
+                }
+                catch {
+                    Write-Exception $_
+                    Write-Warning "Unable to query $($db.name) on $servername"
+                    continue
+                }
+            }
+        }
+    }
 }
