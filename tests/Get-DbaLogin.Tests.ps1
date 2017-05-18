5379cb002821137fadb71ebe530b9aee3f26572e
gbargsley <gbargsley@gmail.com>
2017-05-18 15:16:03 -0500

    Initial Commit Get-DbaLogin-Test

---
+++ tests/Get-DbaLogin.Tests.ps1
@@ -0,0 +1,18 @@
+﻿#Thank you Warren http://ramblingcookiemonster.github.io/Testing-DSC-with-Pester-and-AppVeyor/
+
+Describe "Get-DbaLogin Integration Tests" -Tags "Integrationtests" {
+
+    Context "Does sql instance have a SA account" {
+        $results = Get-DbaLogin -SqlInstance localhost -Login sa 
+        It "Should report that one account named SA exists" {
+            $results.Count | Should Be 1
+        }
+    }
+
+    Context "Check that SA account is enabled" {
+            $results = Get-DbaLogin -SqlInstance localhost -Login sa
+            It "Should say the SA account is disabled FALSE" {
+                $results.IsDisabled | Should Be "False"
+            }
+        }
+}
