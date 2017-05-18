a5ac015263815b67d7e0debbc908ecfd2b09731d
ctrlbold <clemaire@gmail.com>
2017-05-18 15:55:07 +0200

    so dreamy!!

---
+++ tests/Restore-DbaDatabase.Tests.ps1
@@ -0,0 +1,71 @@
+﻿#Thank you Warren http://ramblingcookiemonster.github.io/Testing-DSC-with-Pester-and-AppVeyor/
+
+if (-not $PSScriptRoot) {
+	$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$Verbose = @{ }
+if ($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master") {
+	$Verbose.add("Verbose", $True)
+}
+
+$scriptname = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
+$Name = $scriptname.Split('.')[0]
+
+Describe 'Script Analyzer Tests' -Tags @('ScriptAnalyzer') {
+	Context "Testing $Name for Standard Processing" {
+		foreach ($rule in $ScriptAnalyzerRules) {
+			$i = $ScriptAnalyzerRules.IndexOf($rule)
+			It "passes the PSScriptAnalyzer Rule number $i - $rule  " {
+				(Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\internal\$scriptname" -IncludeRule $rule.RuleName).Count | Should Be 0
+			}
+		}
+	}
+}
+
+# Test functionality
+
+Describe "Restore-DbaDatabase Integration Tests" -Tags "Integrationtests" {
+	Context "Properly restores a database on the local drive using Path" {
+		$results = Restore-DbaDatabase -SqlServer localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+		It "Should Return the proper backup file location" {
+			$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+		}
+		It "Should return successful restore" {
+			$results.RestoreComplete | Should Be $true
+		}
+	}
+
+Context "Ensuring warning is thrown if database already exists" {
+		$results = Restore-DbaDatabase -SqlServer localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -WarningVariable warning
+		It "Should warn" {
+			$warning | Should Match "exists and will not be overwritten"
+		}
+		It "Should not return object" {
+			$results | Should Be $null
+		}
+	}
+	
+	Context "Database is properly removed for next test" {
+		$results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+		It "Should say the status was dropped" {
+			$results.Status | Should Be "Dropped"
+		}
+	}
+	
+	Context "Properly restores a database on the local drive using piped Get-ChildItem results" {
+		$results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlServer localhost
+		It "Should Return the proper backup file location" {
+			$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+		}
+		It "Should return successful restore" {
+			$results.RestoreComplete | Should Be $true
+		}
+	}
+	
+	Context "Database is properly removed again" {
+		$results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+		It "Should say the status was dropped" {
+			$results.Status | Should Be "Dropped"
+		}
+	}
+}
--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -6,15 +6,16 @@ Import-Module C:\projects\dbatools\dbatools.psd1
 $sql2008 = "localhost\sql2008r2sp2"
 $sql2016 = "localhost\sql2016"
 
-Write-Output "Cloning lab"
-git clone -q --branch=master https://github.com/sqlcollaborative/appveyor-lab.git C:\projects\appveyor-lab
-
-Write-Output "Listing directory"
-Get-ChildItem C:\projects\appveyor-lab\sql2008-backups
-
 Write-Output "Creating migration & backup directories"
 New-Item -Path C:\projects\migration -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 New-Item -Path C:\projects\backups -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path C:\github -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+
+Write-Output "Cloning lab materials"
+git clone -q --branch=master https://github.com/sqlcollaborative/appveyor-lab.git C:\github\appveyor-lab
+
+# Write-Output "Listing directory"
+# Get-ChildItem C:\github\appveyor-lab\sql2008-backups
 
 # Write-Output "Creating network share workaround"
 # New-SmbShare -Name migration -path C:\projects\migration -FullAccess 'ANONYMOUS LOGON', 'Everyone' | Out-Null
