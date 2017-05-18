b2ad009e498c054635bfe037cee22b9cf793d764
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2017-05-18 12:50:54 +0200

    Test Redesign to pre-v2 (#1257)

--- tests/Get-DbaPagefileSetting.Tests.ps1
+++ tests/Get-DbaPagefileSetting.Tests.ps1
@@ -1,41 +1,33 @@
 #Thank you Warren http://ramblingcookiemonster.github.io/Testing-DSC-with-Pester-and-AppVeyor/
 
-if(-not $PSScriptRoot)
-{
+if (-not $PSScriptRoot) {
     $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 }
-$Verbose = @{}
-if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
-{
-    $Verbose.add("Verbose",$True)
+$Verbose = @{ }
+if ($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master") {
+    $Verbose.add("Verbose", $True)
 }
 
-
-
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
-Import-Module $PSScriptRoot\..\functions\$sut -Force
-Import-Module PSScriptAnalyzer
-## Added PSAvoidUsingPlainTextForPassword as credential is an object and therefore fails. We can ignore any rules here under special circumstances agreed by admins :-)
-$Rules = (Get-ScriptAnalyzerRule).Where{$_.RuleName -notin ('PSAvoidUsingPlainTextForPassword') }
 $Name = $sut.Split('.')[0]
 
-    Describe 'Script Analyzer Tests' {
-            Context "Testing $Name for Standard Processing" {
-                foreach ($rule in $rules) { 
-                    $i = $rules.IndexOf($rule)
-                    It "passes the PSScriptAnalyzer Rule number $i - $rule  " {
-                        (Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\functions\$sut" -IncludeRule $rule.RuleName ).Count | Should Be 0 
-                    }
-                }
+Describe 'Script Analyzer Tests' -Tag @('ScriptAnalyzer') {
+    Context "Testing $Name for Standard Processing" {
+        foreach ($rule in $ScriptAnalyzerRules) {
+            $i = $ScriptAnalyzerRules.IndexOf($rule)
+            It "passes the PSScriptAnalyzer Rule number $i - $rule  " {
+                (Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\functions\$sut" -IncludeRule $rule.RuleName).Count | Should Be 0
             }
         }
-   ## needs some proper tests for the function here
-    Describe "$Name Tests"{
-        Context "Some Tests" {
-              It "Should have some Pester Tests added" {
-                  $true | Should Be $true
-              }
-		}
     }
-    
-    
+}
+
+Describe "$Name Tests" -Tag @('Command') {
+    InModuleScope 'dbatools' {
+        Context " There should be some functional tests here" {
+            It "Does a thing" {
+                $ActualValue | Should Be $ExpectedValue
+            }
+        } # Context
+    } #modulescope
+} #describe
--- tests/appveyor.pester.ps1
+++ tests/appveyor.pester.ps1
@@ -6,70 +6,73 @@
 param([switch]$Finalize)
 
 #Initialize some variables, move to the project root
-    $PSVersion = $PSVersionTable.PSVersion.Major
-    $TestFile = "TestResultsPS$PSVersion.xml"
-    $ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
-    Set-Location $ProjectRoot
-   
+$PSVersion = $PSVersionTable.PSVersion.Major
+$TestFile = "TestResultsPS$PSVersion.xml"
+$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+$ModuleBase = $ProjectRoot
+Set-Location $ProjectRoot
+
+Import-Module "$ProjectRoot\dbatools.psm1" -DisableNameChecking
+$ScriptAnalyzerRules = Get-ScriptAnalyzerRule
 
 #Run a test with the current version of PowerShell
 #Make things faster by removing most output
-    if(-not $Finalize)
-    {
-        "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
-		
-        Import-Module Pester
-		Set-Variable ProgressPreference -Value SilentlyContinue
-        Invoke-Pester -Quiet -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
-        Export-Clixml -Path "$ProjectRoot\PesterResults$PSVersion.xml"
-    }
+if(-not $Finalize)
+{
+    "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
+	
+    Import-Module Pester
+	Set-Variable ProgressPreference -Value SilentlyContinue
+    Invoke-Pester -Quiet -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
+    Export-Clixml -Path "$ProjectRoot\PesterResults$PSVersion.xml"
+}
 
 #If finalize is specified, check for failures and 
-    else
-    {
-        #Show status...
-            $AllFiles = Get-ChildItem -Path $ProjectRoot\*Results*.xml | Select -ExpandProperty FullName
-            "`n`tSTATUS: Finalizing results`n"
-            "COLLATING FILES:`n$($AllFiles | Out-String)"
+else
+{
+    #Show status...
+    $AllFiles = Get-ChildItem -Path $ProjectRoot\*Results*.xml | Select-Object -ExpandProperty FullName
+    "`n`tSTATUS: Finalizing results`n"
+    "COLLATING FILES:`n$($AllFiles | Out-String)"
 
-        #Upload results for test page
-            Get-ChildItem -Path "$ProjectRoot\TestResultsPS*.xml" | Foreach-Object {
-        
-                $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
-                $Source = $_.FullName
+    #Upload results for test page
+    Get-ChildItem -Path "$ProjectRoot\TestResultsPS*.xml" | Foreach-Object {
 
-                "UPLOADING FILES: $Address $Source"
+        $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+        $Source = $_.FullName
 
-                (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
-            }
+        "UPLOADING FILES: $Address $Source"
 
-        #What failed?
-            $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults*.xml" | Import-Clixml )
-            
-            $FailedCount = $Results |
-                Select -ExpandProperty FailedCount |
-                Measure-Object -Sum |
-                Select -ExpandProperty Sum
-    
-            if ($FailedCount -gt 0) {
-
-                $FailedItems = $Results |
-                    Select -ExpandProperty TestResult |
-                    Where {$_.Passed -notlike $True}
-
-                "FAILED TESTS SUMMARY:`n"
-                $FailedItems | ForEach-Object {
-                    $Test = $_
-                    [pscustomobject]@{
-                        Describe = $Test.Describe
-                        Context = $Test.Context
-                        Name = "It $($Test.Name)"
-                        Result = $Test.Result
-                    }
-                } |
-                    Sort Describe, Context, Name, Result |
-                    Format-List
-
-                throw "$FailedCount tests failed."
-            }
+        (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
     }
+
+    #What failed?
+    $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults*.xml" | Import-Clixml )
+    
+    $FailedCount = $Results |
+        Select-Object -ExpandProperty FailedCount |
+        Measure-Object -Sum |
+        Select-Object -ExpandProperty Sum
+
+    if ($FailedCount -gt 0) {
+
+        $FailedItems = $Results |
+            Select-Object -ExpandProperty TestResult |
+            Where-Object {$_.Passed -notlike $True}
+
+        "FAILED TESTS SUMMARY:`n"
+        $FailedItems | ForEach-Object {
+            $Test = $_
+            [pscustomobject]@{
+                Describe = $Test.Describe
+                Context = $Test.Context
+                Name = "It $($Test.Name)"
+                Result = $Test.Result
+            }
+        } |
+            Sort-Object Describe, Context, Name, Result |
+            Format-List
+
+        throw "$FailedCount tests failed."
+    }
+}
