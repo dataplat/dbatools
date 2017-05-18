395f36c9e82c10139e53addef660b10984821674
Stuart Moore <stuart@stuart-moore.com>
2017-05-18 08:42:01 +0100

    Moved to raw and removed some stuff that was autoconverting to char array

--- functions/Test-DbaLastBackup.ps1
+++ functions/Test-DbaLastBackup.ps1
@@ -340,7 +340,7 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 						$restoreresult = "Restore not located on shared location"
 						$dbccresult = "Skipped"
 					}
-					elseif ((Test-DbaSqlPath -SqlServer $destserver -Path $lastbackup[0].Path[0]) -eq $false) {
+					elseif ((Test-DbaSqlPath -SqlServer $destserver -Path $lastbackup[0].Path) -eq $false) {
 						Write-Message -Level Verbose -Message "SQL Server cannot find backup"
 						$fileexists = $false
 						$restoreresult = "Skipped"
