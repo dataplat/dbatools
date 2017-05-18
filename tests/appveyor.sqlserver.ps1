fd7582bde065f90ff58a1c4f290cb806608149d5
ctrlbold <clemaire@gmail.com>
2017-05-18 17:53:34 +0200

    quick changes, sorry

--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -1,16 +1,21 @@
+# Imports some assemblies
+Write-Output "Importing dbatools"
+Import-Module C:\projects\dbatools\dbatools.psd1
+
 # This script spins up two local instances
 $sql2008 = "localhost\sql2008r2sp2"
 $sql2016 = "localhost\sql2016"
 
-Write-Output "Cloning lab"
-git clone -q --branch=master https://github.com/sqlcollaborative/appveyor-lab.git C:\projects\appveyor-lab
-
-Write-Output "Listing directory"
-Get-ChildItem C:\projects\appveyor-lab\sql2008-backups
-
 Write-Output "Creating migration & backup directories"
 New-Item -Path C:\projects\migration -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 New-Item -Path C:\projects\backups -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path C:\github -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+
+Write-Output "Cloning lab materials"
+git clone -q --branch=master https://github.com/sqlcollaborative/appveyor-lab.git C:\github\appveyor-lab
+
+# Write-Output "Listing directory"
+# Get-ChildItem C:\github\appveyor-lab\sql2008-backups
 
 # Write-Output "Creating network share workaround"
 # New-SmbShare -Name migration -path C:\projects\migration -FullAccess 'ANONYMOUS LOGON', 'Everyone' | Out-Null
@@ -36,20 +41,3 @@ foreach ($instance in $instances) {
 	Write-Output "Starting $instance"
 	Start-Service "MSSQL`$$instance"
 }
-
-<#
-Write-Output "Importing dbatools"
-Import-Module C:\projects\dbatools\dbatools.psd1
-
-Write-Output "Beginning restore"
-Get-ChildItem C:\projects\appveyor-lab\sql2008-backups | Restore-DbaDatabase -SqlServer $sql2008
-
-Write-Output "Attempting to perform migration - will bomb, need to submit a PR to not require network share"
-Copy-DbaDatabase -Source $sql2008 -Destination $sql2016 -BackupRestore -NetworkShare "\\$env:computername\migration" -AllDatabases -Silent:$false
-
-Write-Output "Trying some backups"
-Backup-DbaDatabase -SqlInstance $sql2008 -BackupDirectory C:\projects\backups
-
-Write-Output "Login import"
-Invoke-DbaSqlCmd -ServerInstance $sql2016 -InputFile C:\projects\appveyor-lab\sql2008-logins.sql
-#>
