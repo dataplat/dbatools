dee16d168ae3a3d87c42f2851035910b2bfb4046
ctrlbold <clemaire@gmail.com>
2017-05-19 15:02:05 +0200

    changed to simple

--- tests/Get-DbaDatabase.Tests.ps1
+++ tests/Get-DbaDatabase.Tests.ps1
@@ -9,10 +9,10 @@ Describe "Get-DbaDatabase Integration Tests" -Tags "Integrationtests" {
         }
     }
 
-    Context "Check that master database is in FULL recovery mode" {
+    Context "Check that master database is in Simple recovery mode" {
             $results = Get-DbaDatabase -SqlInstance localhost -Database master
-            It "Should say the recovery mode of master is Full" {
-                $results.RecoveryModel | Should Be "Full"
+            It "Should say the recovery mode of master is Simple" {
+                $results.RecoveryModel | Should Be "Simple"
             }
         }
 }
