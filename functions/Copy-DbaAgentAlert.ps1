7145b8d283df63412c7fbdc0948f26ddfe6fd637
Shawn Melton <wshawnmelton@gmail.com>
2017-05-19 11:50:20 -0500

    Adding object output and comments on bugs

--- functions/Copy-DbaAgentAlert.ps1
+++ functions/Copy-DbaAgentAlert.ps1
@@ -103,60 +103,93 @@ function Copy-DbaAgentAlert {
 
 		$serverAlerts = $sourceServer.JobServer.Alerts
 		$destAlerts = $destServer.JobServer.Alerts
-
+		
 		if ($IncludeDefaults -eq $true) {
-			if ($PSCmdlet.ShouldProcess($Destination, "Creating Alert Defaults")) {
+            if ($PSCmdlet.ShouldProcess($Destination, "Creating Alert Defaults")) {
+                $copyAgentAlertStatus = [pscustomobject]@{
+                    SourceServer      = $sourceServer.Name
+                    DestinationServer = $destServer.Name
+                    Name              = "Alert Defaults"
+                    Type              = "Alert Defaults"
+                    Status            = $null
+                    DateTime          = [sqlcollective.dbatools.Utility.DbaDateTime](Get-Date)
+                }
 				try {
-					Write-Message -Message "Copying Alert Defaults" -Level Output
+					Write-Message -Message "Creating Alert Defaults" -Level Output
 					$sql = $sourceServer.JobServer.AlertSystem.Script() | Out-String
 					$sql = $sql -replace [Regex]::Escape("'$source'"), [Regex]::Escape("'$Destination'")
 
 					Write-Message -Message $sql -Level Debug
 					$null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
+					
+					$copyAgentAlertStatus.Status = "Successful"
 				}
 				catch {
-					Stop-Function -Message "Issue creating alert defaults" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
+                    $copyAgentAlertStatus.Status = "Failed"
+					$copyAgentAlertStatus
+					Stop-Function -Message "Issue creating alert defaults" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer -Continue
 				}
+				$copyAgentAlertStatus
 			}
 		}
 
 		foreach ($alert in $serverAlerts) {
 			$alertName = $alert.name
-			if ($alerts.count -gt 0 -and $alerts -notcontains $alertName) { continue }
+            $copyAgentAlertStatus = [pscustomobject]@{
+                SourceServer      = $sourceServer.Name
+                DestinationServer = $destServer.Name
+                Name              = $alertName
+                Type              = "Agent Alert"
+                Status            = $null
+                DateTime          = [sqlcollective.dbatools.Utility.DbaDateTime](Get-Date)
+            }
+
+			if ($alerts.count -gt 0 -and $alerts -notcontains $alertName) { 
+				continue 
+			}
 
 			if ($destAlerts.name -contains $alert.name) {
-				if ($force -eq $false) {
-					Write-Message -Messsage "Alert $alertName exists at destination. Use -Force to drop and migrate." -Level Warning
+                if ($force -eq $false) {					
+                    $copyAgentAlertStatus.Status = "Skipped"
+					$copyAgentAlertStatus
+					Write-Message -Message "Alert $alertName exists at destination. Use -Force to drop and migrate." -Level Warning
 					continue
 				}
 
 				if ($PSCmdlet.ShouldProcess($Destination, "Dropping alert $alertName and recreating")) {
-					try {
-						Write-Message -Message "Dropping Alert $alertName on $destServer" -Level Verbose
+                    try {
+                        Write-Message -Message "Dropping Alert $alertName on $destServer" -Level Verbose
 
-						$sql = "EXEC msdb.dbo.sp_delete_alert @name = N'$($alert.name)';"
-						Write-Message -Message $sql -Level Debug
-						$null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
-					}
-					catch {
-						Stop-Function -Message "Issue dropping/recreating alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer -Continue
-					}
+                        $sql = "EXEC msdb.dbo.sp_delete_alert @name = N'$($alert.name)';"
+                        Write-Message -Message $sql -Level Debug
+                        $null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
+                    }
+                    catch {
+                        $copyAgentAlertStatus.Status = "Failed"
+						$copyAgentAlertStatus
+                        Stop-Function -Message "Issue dropping/recreating alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer -Continue
+                    }
 				}
 			}
 
-			if ($PSCmdlet.ShouldProcess($Destination, "Creating Alert $alertName")) {
-				try {
-					Write-Message -Message "Copying Alert $alertName" -Level Output
-					$sql = $alert.Script() | Out-String
-					$sql = $sql -replace [Regex]::Escape("'$source'"), [Regex]::Escape("'$Destination'")
-					$sql = $sql -replace "@job_id=N'........-....-....-....-............", "@job_id=N'00000000-0000-0000-0000-000000000000"
+            if ($PSCmdlet.ShouldProcess($Destination, "Creating Alert $alertName")) {
+                try {
+                    Write-Message -Message "Copying Alert $alertName" -Level Output
+                    $sql = $alert.Script() | Out-String
+                    $sql = $sql -replace [Regex]::Escape("'$source'"), [Regex]::Escape("'$Destination'")
+                    $sql = $sql -replace "@job_id=N'........-....-....-....-............", "@job_id=N'00000000-0000-0000-0000-000000000000"
 
-					Write-Message -Message $sql -Level Debug
-					$null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
-				}
-				catch {
-					Stop-Function -Message "Issue creating alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
-				}
+                    Write-Message -Message $sql -Level Debug
+                    $null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
+
+                    $copyAgentAlertStatus.Status = "Successful"
+					$copyAgentAlertStatus
+                }
+                catch {
+                    $copyAgentAlertStatus.Status = "Failed"
+					$copyAgentAlertStatus
+                    Stop-Function -Message "Issue creating alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer -Continue
+                }
 			}
 
 			$destServer.JobServer.Alerts.Refresh()
@@ -166,53 +199,79 @@ function Copy-DbaAgentAlert {
 			$notifications = $alert.EnumNotifications()
 			$jobName = $alert.JobName
 
-			# Super workaround but it works
-			if ($alert.JobId -ne '00000000-0000-0000-0000-000000000000') {
+            # JobId = 00000000-0000-0000-0000-000 means the Alertdoes not execute/is attached to a SQL Agent Job.
+            if ($alert.JobId -ne '00000000-0000-0000-0000-000000000000') {
+                $copyAgentAlertStatus = [pscustomobject]@{
+                    SourceServer      = $sourceServer.Name
+                    DestinationServer = $destServer.Name
+                    Name              = $alertName
+                    Type              = "Attach to Job"
+                    Status            = $null
+                    DateTime          = [sqlcollective.dbatools.Utility.DbaDateTime](Get-Date)
+                }
 				if ($PSCmdlet.ShouldProcess($Destination, "Adding $alertName to $jobName")) {
-					try {
-						Write-Message -Message "Adding $alertName to $jobName" -Level Output
-						$newJob = $destServer.JobServer.Jobs[$jobName]
-						$newJobId = ($newJob.JobId) -replace " ", ""
-						$sql = $sql -replace '00000000-0000-0000-0000-000000000000', $newJobId
-						$sql = $sql -replace 'sp_add_alert', 'sp_update_alert'
+                    try {
+						<# THERE needs to be validation within this block to see if the $jobName actually exists on the source server. #>
+                        Write-Message -Message "Adding $alertName to $jobName" -Level Output
+                        $newJob = $destServer.JobServer.Jobs[$jobName]
+                        $newJobId = ($newJob.JobId) -replace " ", ""
+                        $sql = $sql -replace '00000000-0000-0000-0000-000000000000', $newJobId
+                        $sql = $sql -replace 'sp_add_alert', 'sp_update_alert'
 
-						Write-Message -Message $sql -Level Debug
-						$null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
-					}
-					catch {
-						Stop-Function -Message "Issue adding alert to job" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
-					}
+                        Write-Message -Message $sql -Level Debug
+                        $null = $destServer.ConnectionContext.ExecuteNonQuery($sql)
+						
+                        $copyAgentAlertStatus.Status = "Successful"
+                        $copyAgentAlertStatus
+                    }
+                    catch {
+                        $copyAgentAlertStatus.Status = "Failed"
+                        $copyAgentAlertStatus
+                        Stop-Function -Message "Issue adding alert to job" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
+                    }
 				}
 			}
 
-			if ($PSCmdlet.ShouldProcess($Destination, "Moving Notifications $alertName")) {
-				try {
-					# cant add them this way, we need to modify the existing one or give all options that are supported.
-					foreach ($notify in $notifications) {
-						$notifyCollection = @()
-						if ($notify.UseNetSend -eq $true) {
-							Write-Message -Message "Adding net send" -Level Verbose
-							$notifyCollection += "NetSend"
-						}
+            if ($PSCmdlet.ShouldProcess($Destination, "Moving Notifications $alertName")) {
+                try {
+                    $copyAgentAlertStatus = [pscustomobject]@{
+                        SourceServer      = $sourceServer.Name
+                        DestinationServer = $destServer.Name
+                        Name              = $alertName
+                        Type              = "Notifications"
+                        Status            = $null
+                        DateTime          = [sqlcollective.dbatools.Utility.DbaDateTime](Get-Date)
+                    }
+                    # cant add them this way, we need to modify the existing one or give all options that are supported.
+                    foreach ($notify in $notifications) {
+                        $notifyCollection = @()
+                        if ($notify.UseNetSend -eq $true) {
+                            Write-Message -Message "Adding net send" -Level Verbose
+                            $notifyCollection += "NetSend"
+                        }
 
-						if ($notify.UseEmail -eq $true) {
-							Write-Message -Message "Adding email" -Level Output
-							$notifyCollection += "NotifyEmail"
-						}
+                        if ($notify.UseEmail -eq $true) {
+                            Write-Message -Message "Adding email" -Level Output
+                            $notifyCollection += "NotifyEmail"
+                        }
 
-						if ($notify.UsePager -eq $true) {
-							Write-Message -Message "Adding pager" -Level Output
-							$notifyCollection += "Pager"
-						}
+                        if ($notify.UsePager -eq $true) {
+                            Write-Message -Message "Adding pager" -Level Output
+                            $notifyCollection += "Pager"
+                        }
 
-						$notifyMethods = $notifyCollection -join ", "
-						$newAlert.AddNotification($notify.OperatorName, [Microsoft.SqlServer.Management.Smo.Agent.NotifyMethods]$notifyMethods)
-					}
-				}
-				catch {
-					Stop-Function -Message "Issue moving notifications for the alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
-				}
-			}
+                        $notifyMethods = $notifyCollection -join ", "
+                        $newAlert.AddNotification($notify.OperatorName, [Microsoft.SqlServer.Management.Smo.Agent.NotifyMethods]$notifyMethods)
+                    }
+                    $copyAgentAlertStatus.Status = "Successful"
+                    $copyAgentAlertStatus
+                }
+                catch {
+                    $copyAgentAlertStatus.Status = "Failed"
+                    $copyAgentAlertStatus
+                    Stop-Function -Message "Issue moving notifications for the alert" -Category InvalidOperation -InnerErrorRecord $_ -Target $destServer
+                }
+            }
 		}
 	}
 	end {
