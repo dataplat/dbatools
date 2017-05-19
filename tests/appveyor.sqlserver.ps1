7ed31099caed5fbe3e46fd4aef7040fb23a30601
ctrlbold <clemaire@gmail.com>
2017-05-19 22:37:56 +0200

    as if

--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -52,12 +52,12 @@ do {
 while ((Get-Service 'SQLAgent$sql2016').Status -ne 'Running' -or $i++ -gt 10)
 
 # Add some jobs to the sql2008r2sp2 instance (1433 = default)
-foreach ($file in (Get-ChildItem C:\github\appveyor-lab\sql2008-startup\*.sql -Recurse)) {
+foreach ($file in (Get-ChildItem C:\github\appveyor-lab\sql2008-startup\*.sql -Recurse -ErrorAction SilentlyContinue)) {
 	Write-Output "Executing startup script against SQL Server 2008 R2 - $file"
 	Invoke-DbaSqlCmd -ServerInstance localhost -InputFile $file
 }
 
-foreach ($file in (Get-ChildItem C:\github\appveyor-lab\sql2016-startup\*.sql -Recurse)) {
+foreach ($file in (Get-ChildItem C:\github\appveyor-lab\sql2016-startup\*.sql -Recurse -ErrorAction SilentlyContinue)) {
 	Write-Output "Executing startup script against SQL Server 2016 - $file"
 	Invoke-DbaSqlCmd -ServerInstance localhost\sql2016 -InputFile $file
 }
