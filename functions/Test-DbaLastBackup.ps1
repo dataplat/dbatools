aa7922c8ba19b69817cde3612667233f3e6d87dc
ctrlbold <clemaire@gmail.com>
2017-05-22 21:59:42 +0200

    @Stuart-Moore check it

--- functions/Test-DbaLastBackup.ps1
+++ functions/Test-DbaLastBackup.ps1
@@ -264,7 +264,6 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 					
 					Write-Message -Level Verbose -Message "Processing $dbname"
 					
-					$TrustBackupHistory =  $true
 					$copysuccess = $true
 					$db = $sourceserver.databases[$dbname]
 					
@@ -275,7 +274,6 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 					
 					$lastbackup = Get-DbaBackupHistory -SqlInstance $sourceserver -Database $dbname -Last -IgnoreCopyOnly:$ignorecopyonly -raw
 					if ($CopyFile) {
-						$TrustBackupHistory =  $false
 						try {
 							Write-Message -Level Verbose -Message "Gathering information for file copy"
 							$removearray = @()
@@ -372,10 +370,10 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 								Write-Message -Level Verbose -Message "Performing restore"
 								$startRestore = Get-Date
 								if ($verifyonly) {
-									$restoreresult = $lastbackup | Restore-DbaDatabase -SqlInstance $destserver -RestoredDatababaseNamePrefix $prefix -DestinationFilePrefix $Prefix -DestinationDataDirectory $datadirectory -DestinationLogDirectory $logdirectory -VerifyOnly:$VerifyOnly -IgnoreLogBackup:$IgnoreLogBackup -TrustBackupHistory:$TrustBackupHistory
+									$restoreresult = $lastbackup | Restore-DbaDatabase -SqlInstance $destserver -RestoredDatababaseNamePrefix $prefix -DestinationFilePrefix $Prefix -DestinationDataDirectory $datadirectory -DestinationLogDirectory $logdirectory -VerifyOnly:$VerifyOnly -IgnoreLogBackup:$IgnoreLogBackup
 								}
 								else {
-									$restoreresult = $lastbackup | Restore-DbaDatabase -SqlInstance $destserver -RestoredDatababaseNamePrefix $prefix -DestinationFilePrefix $Prefix -DestinationDataDirectory $datadirectory -DestinationLogDirectory $logdirectory -IgnoreLogBackup:$IgnoreLogBackup -TrustDbBackupHistory:$TrustBackupHistory
+									$restoreresult = $lastbackup | Restore-DbaDatabase -SqlInstance $destserver -RestoredDatababaseNamePrefix $prefix -DestinationFilePrefix $Prefix -DestinationDataDirectory $datadirectory -DestinationLogDirectory $logdirectory -IgnoreLogBackup:$IgnoreLogBackup
 								}
 								
 								$endRestore = Get-Date
