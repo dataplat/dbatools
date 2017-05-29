63bd38423e9239c630222a0ff66c3f55bf859ab9
Shawn Melton <wshawnmelton@gmail.com>
2017-05-28 22:41:22 -0500

    tepp add get-dbaschemachangehistory

--- functions/Get-DbaSchemaChangeHistory.ps1
+++ functions/Get-DbaSchemaChangeHistory.ps1
@@ -1,11 +1,11 @@
-FUNCTION Get-DbaSchemaChangeHistory {
-    <#
+function Get-DbaSchemaChangeHistory {
+	<#
 	.SYNOPSIS
 	Gets DDL changes logged in the system trace.
 
 	.DESCRIPTION
-    Queries the default system trace for any DDL changes in the specified timeframe
-    Only works with SQL 2005 and later, as the system trace didn't exist before then
+	Queries the default system trace for any DDL changes in the specified timeframe
+	Only works with SQL 2005 and later, as the system trace didn't exist before then
 
 	.PARAMETER SqlInstance
 	SQL Server name or SMO object representing the SQL Server to connect to. This can be a collection and recieve pipeline input to allow the function
@@ -17,21 +17,21 @@ FUNCTION Get-DbaSchemaChangeHistory {
 	.PARAMETER Database
 	The database(s) to process - this list is autopopulated from the server. If unspecified, all databases will be processed.
 
-	.PARAMETER Exclude
+	.PARAMETER ExcludeDatabase
 	The database(s) to exclude - this list is autopopulated from the server
 		
-    .PARAMETER Since
-    A date from which DDL changes should be returned. Default is to start at the beggining of the current trace file
+	.PARAMETER Since
+	A date from which DDL changes should be returned. Default is to start at the beggining of the current trace file
 
-    .PARAMETER Object
-    The name of a SQL Server object you want to look for changes on
+	.PARAMETER Object
+	The name of a SQL Server object you want to look for changes on
 
 	.PARAMETER Silent 
 	Use this switch to disable any kind of verbose messages
 	
 	.NOTES
+	Tags: Migration, Backup, Databases
 	Original Author: Stuart Moore (@napalmgram - http://stuart-moore.com)
-	Tags: Migration, Backup
 	
 	Website: https://dbatools.io
 	Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
@@ -42,8 +42,8 @@ FUNCTION Get-DbaSchemaChangeHistory {
 
 	.EXAMPLE
 	Get-DbaSchemaChangeHistory -SqlInstance localhost
-    
-    Returns all DDL changes made in all databases on the SQL Server instance localhost since the system trace began
+	
+	Returns all DDL changes made in all databases on the SQL Server instance localhost since the system trace began
 
 	.EXAMPLE
 	Get-DbaSchemaChangeHistory -SqlInstance localhost -Since (Get-Date).AddDays(-7)
@@ -55,99 +55,94 @@ FUNCTION Get-DbaSchemaChangeHistory {
 
 	Returns all DDL changes made in the Prod and Finance databases on the SQL Server instance localhost in the last 7 days
 	
-    .EXAMPLE
+	.EXAMPLE
 	Get-DbaSchemaChangeHistory -SqlInstance localhost -Database Finance -Object AccountsTable -Since (Get-Date).AddDays(-7)
 
 	Returns all DDL changes made  to the AccountsTable object in the Finance database on the SQL Server instance localhost in the last 7 days
 
 	#>
 	
-    [CmdletBinding()]
-    param (
-        [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
-        [Alias("ServerInstance", "SqlServer")]
-        [DbaInstanceParameter[]]$SqlInstance,
+	[CmdletBinding()]
+	param (
+		[parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
 		[Alias("Credential")]
 		[PSCredential][System.Management.Automation.CredentialAttribute()]
 		$SqlCredential,
 		[Alias("Databases")]
 		[object[]]$Database,
-		[object[]]$Exclude,
-        [DbaDateTime]$Since,
-        [string[]]$Object,
+		[object[]]$ExcludeDatabase,
+		[DbaDateTime]$Since,
+		[string[]]$Object,
 		[switch]$Silent
-    )
+	)
 	
-    process {
-        foreach ($instance in $SqlInstance) {
-            Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+	process {
+		foreach ($instance in $SqlInstance) {
+			Write-Message -Level Verbose -Message "Attempting to connect to $instance"
 			
-            try {
-                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
-            }
-            catch {
-                Stop-Function -Message "Can't connect to $instance or access denied. Skipping." -Continue
-            }
-            if ($Server.Version.Major -le 8)
-            {
-                Stop-Function -Message "This command doesn't support SQL Server 2000, sorry about that" 
-                return
-            }
-            $TraceFileQuery = "select path from sys.traces where is_default = 1"
+			try {
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
+			}
+			catch {
+				Stop-Function -Message "Can't connect to $instance or access denied. Skipping." -Continue
+			}
+			if ($Server.Version.Major -le 8) {
+				Stop-Function -Message "This command doesn't support SQL Server 2000, sorry about that" 
+				return
+			}
+			$TraceFileQuery = "select path from sys.traces where is_default = 1"
 			$TraceFile = $server.ConnectionContext.ExecuteWithResults($TraceFileQuery).Tables.Rows | Select-Object Path
 			
-			if ($null -eq $database) { $database = $server.databases.name }
+			if ($null -eq $Database) { $Database = $server.databases.name }
 			
-			if ($exclude) {
-				$database = $database | Where-Object { $_ -notin $exclude }
+			if ($ExcludeDatabase) {
+				$database = $database | Where-Object { $_ -notin $ExcludeDatabase }
 			}
 			
-			foreach ($db in $database)
-            {
-                if ($server.databases[$db].status -notlike '*normal*')
-                {
-                    Stop-Function -Message "Can't open database $db. Skipping." -Continue
+			foreach ($db in $Database) {
+				if ($server.databases[$db].status -notlike '*normal*') {
+					Stop-Function -Message "Can't open database $db. Skipping." -Continue
 				}
 				
 				$sql = "select SERVERPROPERTY('MachineName') AS ComputerName, 
 						ISNULL(SERVERPROPERTY('InstanceName'), 'MSSQLSERVER') AS InstanceName, 
 						SERVERPROPERTY('ServerName') AS SqlInstance,
-                        tt.databasename as 'DatabaseName',
-                        starttime as 'StartTime',
-                        Sessionloginname as 'LoginName',
-                        NTusername as 'UserName',
-                        applicationname as 'ApplicationName',
-                        case eventclass
-                            When '46' Then 'Create'
-                            when '47' Then 'Drop'
-                            when '164' then 'Alter'
-                        end as 'DDLOperation',
-                        s.name+'.'+o.name as 'Object',
-                        o.type_desc as 'ObjectType'
-                        from 
-                        sys.objects o  inner join
-                        sys.schemas s on s.schema_id=o.schema_id
-                        cross apply (select * from ::fn_trace_gettable('$($TraceFile.path)',default) where ObjectID=o.object_id ) tt
-                        where tt.objecttype not in (21587)
-                        and tt.DatabaseID=db_id()
-                        and tt.EventSubClass=0"
+						tt.databasename as 'DatabaseName',
+						starttime as 'StartTime',
+						Sessionloginname as 'LoginName',
+						NTusername as 'UserName',
+						applicationname as 'ApplicationName',
+						case eventclass
+							When '46' Then 'Create'
+							when '47' Then 'Drop'
+							when '164' then 'Alter'
+						end as 'DDLOperation',
+						s.name+'.'+o.name as 'Object',
+						o.type_desc as 'ObjectType'
+						from 
+						sys.objects o  inner join
+						sys.schemas s on s.schema_id=o.schema_id
+						cross apply (select * from ::fn_trace_gettable('$($TraceFile.path)',default) where ObjectID=o.object_id ) tt
+						where tt.objecttype not in (21587)
+						and tt.DatabaseID=db_id()
+						and tt.EventSubClass=0"
 				
-                if ($null -ne $since)
-                {
-                     $sql =  $sql +" and tt.StartTime>'$Since' "
-                }
-                if ($null -ne $object)
-                {
-                     $sql =  $sql + " and o.name in ('$($object -join ''',''')') "
+				if ($null -ne $since) {
+					$sql =  $sql +" and tt.StartTime>'$Since' "
+				}
+				if ($null -ne $object) {
+					$sql =  $sql + " and o.name in ('$($object -join ''',''')') "
 				}
 				
 				$sql =  $sql + " order by tt.StartTime asc"
-                Write-Message -Level Verbose -Message "Querying Database $db on $instance"
+				Write-Message -Level Verbose -Message "Querying Database $db on $instance"
 				Write-Message -Level Debug -Message "SQL: $sql"
 				
-				$server.databases[$db].ExecuteWithResults($sql).Tables.Rows  | Select-DefaultView -Property ComputerName, InstanceName, SqlInstance, DatabaseName,starttime, LoginName, UserName, ApplicationName, DDLOperation, Object, ObjectType
-            }	
-        }
-    }
+				$server.databases[$db].ExecuteWithResults($sql).Tables.Rows  | Select-DefaultView -Property ComputerName, InstanceName, SqlInstance, DatabaseName, starttime, LoginName, UserName, ApplicationName, DDLOperation, Object, ObjectType
+			}	
+		}
+	}
 }
 
