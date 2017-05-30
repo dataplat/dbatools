e8e93f1eb8697a918ae28de9fc00fbef858fb447
Cláudio Silva <claudiosil100@gmail.com>
2017-05-30 14:59:13 +0100

    Update .Notes
    Updated notes to match new Licence info outline

--- functions/Repair-DbaOrphanUser.ps1
+++ functions/Repair-DbaOrphanUser.ps1
@@ -74,21 +74,9 @@ Will also remove all users that does not have their matching login by calling Re
 .NOTES
 Tags: Orphan
 Original Author: Claudio Silva (@ClaudioESSilva)
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .LINK
 https://dbatools.io/Repair-DbaOrphanUser
