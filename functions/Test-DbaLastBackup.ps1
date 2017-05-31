26823bdb11618d453ea391cb8224beccb63fa09a
ctrlbold <clemaire@gmail.com>
2017-05-31 10:26:20 +0200

    fixed a bad check real quick

--- functions/Test-DbaLastBackup.ps1
+++ functions/Test-DbaLastBackup.ps1
@@ -211,8 +211,8 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 			}
 			
 			if ($instance -ne $destination -and !$CopyFile) {
-				$sourcerealname = $sourceserver.DomainInstanceName
-				$destrealname = $sourceserver.DomainInstanceName
+				$sourcerealname = $sourceserver.ComputerNetBiosName
+				$destrealname = $destserver.ComputerNetBiosName
 				
 				if ($BackupFolder) {
 					if ($BackupFolder.StartsWith("\\") -eq $false -and $sourcerealname -ne $destrealname) {
