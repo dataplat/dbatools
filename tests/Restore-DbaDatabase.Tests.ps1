6d30e29be77addd08567b68f6f8b17a2727d8a29
Stuart Moore <stuart@stuart-moore.com>
2017-05-31 12:18:47 +0100

    Adding some more pester tests for Restores to catch a couple of common fails

--- tests/Restore-DbaDatabase.Tests.ps1
+++ tests/Restore-DbaDatabase.Tests.ps1
@@ -1,63 +1,115 @@
 ﻿Describe "Restore-DbaDatabase Integration Tests" -Tags "Integrationtests" {
-	Context "Properly restores a database on the local drive using Path" {
-		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
-		$results = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
-		It "Should Return the proper backup file location" {
-			$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
-		}
-		It "Should return successful restore" {
-			$results.RestoreComplete | Should Be $true
-		}
-	}
+    Context "Properly restores a database on the local drive using Path" {
+        $null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
+        $results = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+        It "Should Return the proper backup file location" {
+            $results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+        }
+        It "Should return successful restore" {
+            $results.RestoreComplete | Should Be $true
+        }
+    }
 	
-	Context "Ensuring warning is thrown if database already exists" {
-		$results = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -WarningVariable warning
-		It "Should warn" {
-			$warning | Should Match "exists and will not be overwritten"
-		}
-		It "Should not return object" {
-			$results | Should Be $null
-		}
-	}
+    Context "Ensuring warning is thrown if database already exists" {
+        $results = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -WarningVariable warning
+        It "Should warn" {
+            $warning | Should Match "exists and will not be overwritten"
+        }
+        It "Should not return object" {
+            $results | Should Be $null
+        }
+    }
 	
-	Context "Database is properly removed for next test" {
-		$results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
-		It "Should say the status was dropped" {
-			$results.Status | Should Be "Dropped"
-		}
-	}
+    Context "Database is properly removed for next test" {
+        $results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+        It "Should say the status was dropped" {
+            $results.Status | Should Be "Dropped"
+        }
+    }
 	
-	Context "Properly restores a database on the local drive using piped Get-ChildItem results" {
-		$results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost
-		It "Should Return the proper backup file location" {
-			$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
-		}
-		It "Should return successful restore" {
-			$results.RestoreComplete | Should Be $true
-		}
-	}
+    Context "Properly restores a database on the local drive using piped Get-ChildItem results" {
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost
+        It "Should Return the proper backup file location" {
+            $results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+        }
+        It "Should return successful restore" {
+            $results.RestoreComplete | Should Be $true
+        }
+    }
+  
+    Context "Database is properly removed again" {
+        $results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+        It "Should say the status was dropped" {
+            $results.Status | Should Be "Dropped"
+        }
+    }
 	
-	Context "Database is properly removed again" {
-		$results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
-		It "Should say the status was dropped" {
-			$results.Status | Should Be "Dropped"
-		}
-	}
+    Context "Database is restored with correct renamings" {
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost -DestinationFilePrefix prefix
+        It "Should return successful restore with prefix" {
+            $results.RestoreComplete | Should Be $true
+        }
+        It "Should return the 2 prefixed files" {
+            (($results.RestoredFile -split ',').substring(0, 6) -eq 'prefix').count | Should be 2
+        }
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost -DestinationFileSuffix suffix -WithReplace
+        It "Should return successful restore with suffix" {
+            $results.RestoreComplete | Should Be $true
+        }
+        It "Should return the 2 suffixed files" {
+            (($Results.RestoredFile -split ',') -match "suffix\.").count | Should be 2
+        }
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost -DestinationFileSuffix suffix -DestinationFilePrefix prefix -WithReplace
+        It "Should return successful restore with suffix and prefix" {
+            $results.RestoreComplete | Should Be $true
+        }
+        It "Should return the 2 prefixed and suffixed files" {
+            (($Results.RestoredFile -split ',') -match "^prefix.*suffix\.").count | Should be 2
+        }
+    }
 	
+    Context "Folder restore options" {
+        $DataFolder = 'c:\github\data'
+        $LogFolder = 'C:\github\log'
+        New-Item -Type Directory $DataFolder
+        new-Item -Type Directory $LogFolder
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost -DestinationDataDirectory $DataFolder
+        It "Should return successful restore with DestinationDataDirectory" {
+            $results.RestoreComplete | Should Be $true
+        }
+        It "Should have moved all files to $DataFolder" {
+            (($results.restoredfilefull -split ',') -like "$DataFolder*").count | Should be 2
+        }
+        $results = Get-ChildItem C:\github\appveyor-lab\singlerestore\singlerestore.bak | Restore-DbaDatabase -SqlInstance localhost -DestinationDataDirectory $DataFolder -DestinationLogDirectory $LogFolder -WithReplace
+        It "Should have moved data file to $DataFolder" {
+            (($results.restoredfilefull -split ',') -like "$DataFolder*").count | Should be 1
+        }
+        It "Should have moved data file to $LogFolder" {
+            (($results.restoredfilefull -split ',') -like "$LogFolder*").count | Should be 1
+        }
+	}
+
+    Context "Database is properly removed again" {
+        $results = Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+        It "Should say the status was dropped" {
+            $results.Status | Should Be "Dropped"
+        }
+    }
+
 	Context "Properly restores an instance using ola-style backups" {
-		$results = Get-ChildItem C:\github\appveyor-lab\sql2008-backups | Restore-DbaDatabase -SqlInstance localhost
-		It "Restored files count should be right" {
-			$results.databasename.count | Should Be 30
+        $results = Get-ChildItem C:\github\appveyor-lab\sql2008-backups | Restore-DbaDatabase -SqlInstance localhost
+        It "Restored files count should be right" {
+            $results.databasename.count | Should Be 30
+        }
+        It "Should return successful restore" {
+            ($results.Restorecomplete -contains $false) | Should Be $false
 		}
-		It "Should return successful restore" {
-			($results.Restorecomplete -contains $false) | Should Be $false
+    }
+
+    Context "All user databases are removed" {
+        $results = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
+        It "Should say the status was dropped" {
+            $results.ForEach{ $_.Status | Should Be "Dropped" }
 		}
-	}
-	
-	Context "All user databases are removed" {
-		$results = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
-		It "Should say the status was dropped" {
-			$results.ForEach{ $_.Status | Should Be "Dropped" }
-		}
-	}
+    }
 }
