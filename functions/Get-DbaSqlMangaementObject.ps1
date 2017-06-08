341ae088ac08cfe17f486fdc850771fdc7470d3c
dbaduck <ben@benmiller.net>
2017-06-08 11:44:19 -0600

    Changes to new function

---
+++ functions/Get-DbaSqlMangaementObject.ps1
@@ -0,0 +1,13 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.140
+	 Created on:   	6/8/2017 11:41 AM
+	 Created by:   	DBADUCK3
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
