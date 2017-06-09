9620c92fe68c76d84b8fe9d09a54a9bdf4a2980a
ctrlbold <clemaire@gmail.com>
2017-06-09 12:49:38 +0200

    whoops

--- functions/Remove-DbaComputerCertificate.ps1
+++ functions/Remove-DbaComputerCertificate.ps1
@@ -1,41 +1,28 @@
 ﻿function Remove-DbaComputerCertificate {
 <#
 .SYNOPSIS
-Removes a computer certificate useful for Forcing Encryption
+Removes a computer certificate - useful for removing easily certs from remote computers
 
 .DESCRIPTION
-Removes a computer certificate - signed by an Active Directory CA, using the Web Server certificate. Self-signing is not currenty supported but feel free to add it.
-	
-By default, a key with a length of 1024 and a friendly name of the machines FQDN is generated.
-	
-This command was originally intended to help automate the process so that SSL certificates can be available for enforcing encryption on connections.
-	
-It makes a lot of assumptions - namely, that your account is allowed to auto-enroll and that you have permission to do everything it needs to do ;)
-
-References:
-http://sqlmag.com/sql-server/7-steps-ssl-encryption
-https://azurebi.jppp.org/2016/01/23/using-lets-encrypt-certificates-for-secure-sql-server-connections/
-https://blogs.msdn.microsoft.com/sqlserverfaq/2016/09/26/creating-and-registering-ssl-certificates/
-
-The certificate is generated using AD's webserver SSL template on the client machine and pushed to the remote machine.
+Removes a computer certificate from a local or remote compuer
 
 .PARAMETER ComputerName
-The target SQL Server - defaults to localhost. If target is a cluster, you must also specify InstanceClusterName (see below)
+The target computer - defaults to localhost
 
 .PARAMETER Credential
-Allows you to login to $ComputerName using alternative credentials.
+Allows you to login to $ComputerName using alternative credentials
 
 .PARAMETER Store
-Certificate store - defaults to LocalMachine
+Certificate store - defaults to LocalMachine (otherwise exceptions can be thrown on remote connections)
 
 .PARAMETER Folder
-Certificate folder - defaults to My (Personal)
+Certificate folder
 	
 .PARAMETER Certificate
 The target certificate object
 
-.PARAMETER Path
-The path to the target certificate object
+.PARAMETER Thumbprint
+The thumbprint of the certificate object 
 
 .PARAMETER WhatIf 
 Shows what would happen if the command were to run. No actions are actually performed. 
@@ -54,15 +41,14 @@ Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .EXAMPLE
-Remove-DbaComputerCertificate -ComputerName Server1 -Path C:\temp\cert.cer
+Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94
 
-Removes the local C:\temp\cer.cer to the remote server Server1 in LocalMachine\My (Personal)
+Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the LocalMachine store on Server1
 
 .EXAMPLE
-Remove-DbaComputerCertificate -Path C:\temp\cert.cer
+Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 -Store User -Folder My
 
-Removes the local C:\temp\cer.cer to the local computer's LocalMachine\My (Personal) certificate store
-
+Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the User\My (Personal) store on Server1
 #>
 	[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
 	param (
@@ -71,70 +57,51 @@ Removes the local C:\temp\cer.cer to the local computer's LocalMachine\My (Perso
 		[System.Management.Automation.PSCredential]$Credential,
 		[parameter(ParameterSetName = "Certificate", ValueFromPipeline)]
 		[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-		[string]$Path,
+		[string]$Thumbprint,
 		[string]$Store = "LocalMachine",
-		[string]$Folder = "My",
+		[string]$Folder,
 		[switch]$Silent
 	)
 	
 	process {
 		
-		if (!$Certificate -and !$Path) {
-			Write-Message -Level Warning -Message "You must specify either Certificate or Path"
+		if (!$Certificate -and !$Thumbprint) {
+			Write-Message -Level Warning -Message "You must specify either Certificate or Thumbprint"
 			return
 		}
 		
-		if (!(Test-Path -Path $path)) {
-			Write-Message -Level Warning -Message "$Path does not exist"
-			return
+		if ($Certificate) {
+			$thumbprint = $Certificate.Thumbprint
 		}
 		
 		foreach ($computer in $computername) {
-			if (![dbavalidate]::IsLocalhost($computer) -and !$Password) {
-				Write-Message -Level Output -Message "You have specified a remote computer. A password is required for private key encryption/decryption for import."
-				$Password = Read-Host -AsSecureString -Prompt "Password"
-			}
 			
-			if ($Certificate) {
-				$tempdir = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
-				$tempfile = "$tempdir\cert.cer"
-				$certdata = $certificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::PFX, $password)
-			}
-			
-			if ($path) {
-				$file = [System.IO.File]::ReadAllBytes($path)
-				$Certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-				$Certificate.Import($file, $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
-			}
-			
-			if (![dbavalidate]::IsLocalhost($computer)) {
-				if ($PScmdlet.ShouldProcess("local", "Generating pfx and reading from disk")) {
-					Write-Message -Level Output -Message "Exporting PFX with password to $temppfx"
-					$certdata = $certificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::PFX, $password)
+			$scriptblock = {
+				$thumbprint = $args[0]
+				$Store = $args[1]
+				$Folder = $args[2]
+				
+				Write-Verbose "Searching Cert:\$Store\$Folder for thumbprint: $thumbprint"
+				$cert = Get-ChildItem "Cert:\$store\$folder" -Recurse | Where-Object { $_.Thumbprint -eq $cert.Thumbprint }
+				
+				if ($cert) {
+					$cert | Remove-Item
+					$status = "Removed"
+				}
+				else {
+					$status = "Certificate not found in Cert:\$Store\$Folder"
+				}
+				
+				[pscustomobject]@{
+					ComputerName = $env:COMPUTERNAME
+					Thumbprint = $thumbprint
+					Status = $status
 				}
 			}
 			
-			$scriptblock = {
-				$Store = $args[2]
-				$Folder = $args[3]
-				
-				$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-				$cert.Import($args[0], $args[1], [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
-				Write-Verbose "Importing cert to $Folder\$Store"
-				$tempstore = New-Object System.Security.Cryptography.X509Certificates.X509Store($Folder, $Store)
-				$tempstore.Open('ReadWrite')
-				$tempstore.Add($cert)
-				$tempstore.Close()
-				
-				Write-Verbose "Searching Cert:\$Store\$Folder"
-				Get-ChildItem "Cert:\$store\$folder" -Recurse | Where-Object { $_.Thumbprint -eq $cert.Thumbprint }
-				
-				Remove-Item -Path $tempfile
-			}
-			
-			if ($PScmdlet.ShouldProcess("local", "Connecting to $computer to import cert")) {
+			if ($PScmdlet.ShouldProcess("local", "Connecting to $computer to remove cert from Cert:\$Store\$Folder")) {
 				try {
-					Invoke-Command2 -ComputerName $computer -Credential $Credential -ArgumentList $certdata, $Password, $store, $folder -ScriptBlock $scriptblock -ErrorAction Stop
+					Invoke-Command2 -ComputerName $computer -Credential $Credential -ArgumentList $thumbprint, $store, $folder -ScriptBlock $scriptblock -ErrorAction Stop
 				}
 				catch {
 					Stop-Function -Message $_ -ErrorRecord $_ -Target $computer -Continue
--- functions/Remove-DbaNetworkCertificate.ps1
+++ functions/Remove-DbaNetworkCertificate.ps1
@@ -1,31 +1,16 @@
-﻿function Remove-DbaComputerCertificate {
+﻿function Remove-DbaNetworkCertificate {
 <#
 .SYNOPSIS
-Removes a computer certificate - useful for removing certs from remote computers
+Removes the network certificate for SQL Server instance
 
 .DESCRIPTION
-Removes a computer certificate from a local or remote compuer
+Removes the network certificate for SQL Server instance. This setting is found in Configuration Manager.
 
-.PARAMETER ComputerName
-The target SQL Server - defaults to localhost
+.PARAMETER SqlInstance
+The target SQL Server - defaults to localhost. If target is a cluster, you must also specify InstanceClusterName (see below)
 
 .PARAMETER Credential
-Allows you to login to $ComputerName using alternative credentials
-
-.PARAMETER Store
-Certificate store - defaults to LocalMachine
-
-.PARAMETER Folder
-Certificate folder - defaults to My (Personal)
-	
-.PARAMETER Certificate
-The target certificate object
-
-.PARAMETER Thumbprint
-The thumbprint of the certificate object 
-
-.PARAMETER Path
-The path to the target certificate object 
+Allows you to login to the computer (not sql instance) using alternative credentials.
 
 .PARAMETER WhatIf 
 Shows what would happen if the command were to run. No actions are actually performed. 
@@ -44,88 +29,101 @@ Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .EXAMPLE
-Remove-DbaComputerCertificate -ComputerName Server1 -Path C:\temp\cert.cer
+Remove-DbaNetworkCertificate
 
-Removes the local C:\temp\cer.cer to the remote server Server1 in LocalMachine\My (Personal)
+Removes the Network Certificate for the default instance (MSSQLSERVER) on localhost
 
 .EXAMPLE
-Remove-DbaComputerCertificate -Path C:\temp\cert.cer
+Remove-DbaNetworkCertificate -SqlInstance sql1\SQL2008R2SP2
 
-Removes the local C:\temp\cer.cer to the local computer's LocalMachine\My (Personal) certificate store
+Removes the Network Certificate for the SQL2008R2SP2 instance on sql1
+
+.EXAMPLE
+Remove-DbaNetworkCertificate -SqlInstance localhost\SQL2008R2SP2 -WhatIf
+
+Shows what would happen if the command were run
 
 #>
-	[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low", DefaultParameterSetName = 'Default')]
 	param (
-		[Alias("ServerInstance", "SqlServer", "SqlInstance")]
-		[DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
+		[Alias("ServerInstance", "SqlServer", "ComputerName")]
+		[DbaInstanceParameter[]]$SqlInstance = $env:COMPUTERNAME,
 		[System.Management.Automation.PSCredential]$Credential,
-		[parameter(ParameterSetName = "Certificate", ValueFromPipeline)]
-		[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-		[string]$Path,
-		[string]$Thumbprint,
-		[string]$Store = "LocalMachine",
-		[string]$Folder = "My",
 		[switch]$Silent
 	)
-	
 	process {
-		
-		if (!$Certificate -and !$Path -and !$Thumbprint) {
-			Write-Message -Level Warning -Message "You must specify either Certificate, Path or Thumbprint"
-			return
-		}
-		
-		foreach ($computer in $computername) {
+		foreach ($instance in $sqlinstance) {
 			
-			$scriptblock = {
-				$thumbprint = $args[0]
-				$Store = $args[1]
-				$Folder = $args[2]
-				$Certificate = $args[3]
-				$Path = $args[4]
-				$Thumbprint = $args[5]
+			Test-RunAsAdmin -ComputerName $instance.ComputerName
+			
+			Write-Message -Level Output -Message "Resolving hostname"
+			$resolved = Resolve-DbaNetworkName -ComputerName $instance -Turbo
+			
+			if ($null -eq $resolved) {
+				Write-Message -Level Warning -Message "Can't resolve $instance"
+				return
+			}
+			
+			Write-Message -Level Output -Message "Connecting to SQL WMI on $($instance.ComputerName)"
+			try {
+				$sqlwmi = Invoke-ManagedComputerCommand -Server $instance.ComputerName -ScriptBlock { $wmi.Services } -Credential $Credential -ErrorAction Stop | Where-Object DisplayName -eq "SQL Server ($($instance.instancename))"
+			}
+			catch {
+				Stop-Function -Message $_ -Target $sqlwmi
+				return
+			}
+			
+			$regroot = ($sqlwmi.AdvancedProperties | Where-Object Name -eq REGROOT).Value
+			$vsname = ($sqlwmi.AdvancedProperties | Where-Object Name -eq VSNAME).Value
+			$instancename = $instance.instancename # $sqlwmi.DisplayName.Replace('SQL Server (', '').Replace(')', '') # Don't clown, I don't know regex :(
+			$serviceaccount = $sqlwmi.ServiceAccount
+			
+			if ([System.String]::IsNullOrEmpty($regroot)) {
+				$regroot = $sqlwmi.AdvancedProperties | Where-Object { $_ -match 'REGROOT' }
+				$vsname = $sqlwmi.AdvancedProperties | Where-Object { $_ -match 'VSNAME' }
 				
-				if ($Certificate) {
-					$thumbprint = $Certificate.Thumbprint
-				}
-				
-				if ($path) {
-					
-					if (!(Test-Path -Path $path)) {
-						Write-Message -Level Warning -Message "$Path does not exist"
-						return
-					}
-					
-					$file = [System.IO.File]::ReadAllBytes($path)
-					$Certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-					$Certificate.Import($file, $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
-					$thumbprint = $Certificate.Thumbprint
-				}
-				
-				Write-Verbose "Searching Cert:\$Store\$Folder for thumbprint: $thumbprint"
-				$cert = Get-ChildItem "Cert:\$store\$folder" -Recurse | Where-Object { $_.Thumbprint -eq $cert.Thumbprint }
-				
-				if ($cert) {
-					$cert | Remove-Item
-					$status = "Removed"
+				if (![System.String]::IsNullOrEmpty($regroot)) {
+					$regroot = ($regroot -Split 'Value\=')[1]
+					$vsname = ($vsname -Split 'Value\=')[1]
 				}
 				else {
-					$status = "Certificate not found in Cert:\$Store\$Folder"
-				}
-				
-				[pscustomobject]@{
-					ComputerName = $computer
-					Thumbprint = $thumbprint
-					Status = $status
+					Write-Message -Level Warning -Message "Can't find instance $vsname on $env:COMPUTERNAME"
+					return
 				}
 			}
 			
-			if ($PScmdlet.ShouldProcess("local", "Connecting to $computer to remove cert from Cert:\$Store\$Folder")) {
+			if ([System.String]::IsNullOrEmpty($vsname)) { $vsname = $instance.ComputerName }
+			
+			Write-Message -Level Output -Message "Regroot: $regroot"
+			Write-Message -Level Output -Message "ServiceAcct: $serviceaccount"
+			Write-Message -Level Output -Message "InstanceName: $instancename"
+			Write-Message -Level Output -Message "VSNAME: $vsname"
+			
+			$scriptblock = {
+				$regroot = $args[0]
+				$serviceaccount = $args[1]
+				$instancename = $args[2]
+				$vsname = $args[3]
+				
+				$regpath = "Registry::HKEY_LOCAL_MACHINE\$($args[0])\MSSQLServer\SuperSocketNetLib"
+				$cert = (Get-ItemProperty -Path $regpath -Name Certificate).Certificate
+				Set-ItemProperty -Path $regpath -Name Certificate -Value $null
+				
+				[pscustomobject]@{
+					ComputerName = $env:COMPUTERNAME
+					InstanceName = $instancename
+					SqlInstance = $vsname
+					ServiceAccount = $serviceaccount
+					RemovedThumbprint = $cert.Thumbprint
+				}
+			}
+			
+			if ($PScmdlet.ShouldProcess("local", "Connecting to $ComputerName to remove the cert")) {
 				try {
-					Invoke-Command2 -ComputerName $computer -Credential $Credential -ArgumentList $thumbprint, $store, $folder, $Certificate, $path, $thumbprint -ScriptBlock $scriptblock -ErrorAction Stop
+					Invoke-Command2 -ComputerName $resolved.fqdn -Credential $Credential -ArgumentList $regroot, $serviceaccount, $instancename, $vsname -ScriptBlock $scriptblock -ErrorAction Stop
 				}
 				catch {
-					Stop-Function -Message $_ -ErrorRecord $_ -Target $computer -Continue
+					Stop-Function -Message $_ -ErrorRecord $_ -Target $ComputerName -Continue
 				}
 			}
 		}
