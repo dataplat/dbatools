1b89f06dc44d42fd5619aa6032d39114ffa8a3a6
ctrlbold <clemaire@gmail.com>
2017-06-13 21:47:54 +0200

    1.0 updates

--- functions/Install-DbaFirstResponderKit.ps1
+++ functions/Install-DbaFirstResponderKit.ps1
@@ -11,12 +11,11 @@ First Responder Kit links:
 http://FirstResponderKit.org
 https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit
 
-
 .PARAMETER SqlInstance
 SQL Server instance or collection of SQL Server instances
 
 .PARAMETER Database
-Database to store the FRK stored procs, typically master
+Database to store the FRK stored procs, typically master and master by default
 
 .PARAMETER SqlCredential
 Use SqlCredential to connect to SqlInstance with SQL authentication. 
@@ -24,21 +23,9 @@ If SqlCredential is not specified, Windows authentication will be used.
 
 .NOTES 
 Original author: Tara Kizer, Brent Ozar Unlimited (https://www.brentozar.com/)
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 .LINK
 https://dbatools.io/Install-DbaFirstResponderKit
@@ -63,28 +50,31 @@ Logs into server1\instance1 with SQL authentication and then installs the FRK in
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-		#[Alias("ServerInstance", "SqlInstance")]
-		[object]$SqlInstance,
-		[object]$Database,
-		[object]$SqlCredential
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$SqlCredential,
+		[object]$Database = "master",
+		[switch]$Silent
 	)
 	
-	BEGIN {
+	begin {
+		$url = 'https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/archive/master.zip'
 		$temp = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
 		$zipfile = "$temp\SQL-Server-First-Responder-Kit-master.zip"
 		$zipfolder = "$temp\SQL-Server-First-Responder-Kit-master\"
 		
-		$url = 'https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/archive/master.zip'
-		
 		if ($zipfile | Test-Path) {
-			Remove-Item -Path $zipfile
+			Remove-Item -Path $zipfile -ErrorAction SilentlyContinue
 		}
 		
 		if ($zipfolder | Test-Path) {
-			Remove-Item -Path $zipfolder -Recurse
+			Remove-Item -Path $zipfolder -Recurse -ErrorAction SilentlyContinue
 		}
 		
-		Write-Host "Downloading and unzipping the First Responder Kit zip file."
+		$null = New-Item -ItemType Directory -Path $zipfolder -ErrorAction SilentlyContinue
+		
+		Write-Message -Level Verbose -Message "Downloading and unzipping the First Responder Kit zip file."
 		
 		try {
 			try {
@@ -113,32 +103,42 @@ Logs into server1\instance1 with SQL authentication and then installs the FRK in
 			Stop-Function -Message "Couldn't download the First Responder Kit. Download and install manually from https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/archive/master.zip." -ErrorRecord $_
 			return
 		}
-		
 	}
 	
-	PROCESS {
-		Foreach ($instance in $SqlInstance) {
+	process {
+		if (Test-FunctionInterrupt) { return }
+		
+		foreach ($instance in $SqlInstance) {
 			try {
-				Write-Host "Connecting to $instance"
-				$Connection = Connect-DbaSqlServer -SqlInstance $instance -Credential $sqlcredential
+				Write-Message -Level Verbose -Message "Connecting to $instance"
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
 			}
 			catch {
-				Write-Host "Failed to connect to $instance : $($_.Exception.Message)"
+				Stop-Function -Message "Failed to connect to: $instance" -Target $instance -ErrorRecord $_ -Continue
 			}
+			
+			Write-Message -Level Output -Message "Starting installing/updating the First Responder Kit stored procedures in $database on $instance."
 			
 			# Install/Update each FRK stored procedure
 			Get-ChildItem $zipfolder -Filter sp_Blitz*.sql | Foreach-Object {
 				if ($_.Name -ne "sp_BlitzRS.sql") {
-					Write-Host "Installing/Updating $_."
-					
+					Write-Message -Level Output -Message "Installing/Updating $_"
 					$sql = [IO.File]::ReadAllText($_.FullName)
 					
-					$null = $Connection.databases[$Database].ExecuteNonQuery($sql)
+					foreach ($query in ($sql -Split "\nGO\b")) {
+						$query = $query.Trim()
+						if ($query) {
+							try {
+								$null = $server.Query($query, $Database)
+							}
+							catch {
+								Write-Message -Level Warning -Message "Could not execute at least one portion of $($_.Name)"
+							}
+						}
+					}
 				}
 			}
-			
-			Write-Host "Finished installing/updating the First Responder Kit stored procedures in $database on $instance."
+			Write-Message -Level Output -Message "Finished installing/updating the First Responder Kit stored procedures in $database on $instance."
 		}
 	}
-	END { }
 }
