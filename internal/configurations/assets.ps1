98bd446c59de42740297b8f4eaf8316f35f39011
Simone Bizzotto <niphlod@gmail.com>
2017-06-19 17:27:54 +0200

    configurations\assets, fixed typo

--- internal/configurations/assets.ps1
+++ internal/configurations/assets.ps1
@@ -3,4 +3,4 @@ This is designed for all online assets you need configurations for.
 #>
 
 # The default path where dbatools stores persistent data
-Set-DbaConfig -Name 'assets.sqlbuildreference' -Value 'https://sqlcollaborative.github.io/assets/dbatools-buildref-index.json' -Default -DisableHandler -Description "The url where dbatools fetches the up to date builreference index"
+Set-DbaConfig -Name 'assets.sqlbuildreference' -Value 'https://sqlcollaborative.github.io/assets/dbatools-buildref-index.json' -Default -DisableHandler -Description "The url where dbatools fetches the up to date buildreference index (e.g. for Get-DbaSqlBuildReference)"
