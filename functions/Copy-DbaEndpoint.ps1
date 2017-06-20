f9281e61e8df3361e89822d7a86ece4db3272b12
Shawn Melton <wshawnmelton@gmail.com>
2017-06-19 19:08:32 -0500

    format help, add tepp parameter to help

--- functions/Copy-DbaEndpoint.ps1
+++ functions/Copy-DbaEndpoint.ps1
@@ -1,141 +1,145 @@
 function Copy-DbaEndpoint {
-    <#
-.SYNOPSIS 
-Copy-DbaEndpoint migrates server endpoints from one SQL Server to another. 
+	<#
+		.SYNOPSIS
+			Copy-DbaEndpoint migrates server endpoints from one SQL Server to another.
 
-.DESCRIPTION
-By default, all endpoints are copied. The -Endpoints parameter is autopopulated for command-line completion and can be used to copy only specific endpoints.
+		.DESCRIPTION
+			By default, all endpoints are copied. The -Endpoints parameter is autopopulated for command-line completion and can be used to copy only specific endpoints.
 
-If the endpoint already exists on the destination, it will be skipped unless -Force is used. 
+			If the endpoint already exists on the destination, it will be skipped unless -Force is used.
 
-.PARAMETER Source
-Source SQL Server.You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+		.PARAMETER Source
+			Source SQL Server.You must have sysadmin access and server version must be SQL Server version 2000 or greater.
 
-.PARAMETER Destination
-Destination Sql Server. You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+		.PARAMETER SourceSqlCredential
+			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
 
-.PARAMETER SourceSqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+			$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter.
 
-$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter. 
+			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
+			To connect as a different Windows user, run PowerShell as that user.
 
-Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
-To connect as a different Windows user, run PowerShell as that user.
+		.PARAMETER Destination
+			Destination Sql Server. You must have sysadmin access and server version must be SQL Server version 2000 or greater.
 
-.PARAMETER DestinationSqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+		.PARAMETER DestinationSqlCredential
+			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
 
-$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter. 
+			$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter.
 
-Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
-To connect as a different Windows user, run PowerShell as that user.
+			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
+			To connect as a different Windows user, run PowerShell as that user.
 
-.PARAMETER WhatIf 
-Shows what would happen if the command were to run. No actions are actually performed. 
+		.PARAMETER Endpoint
+			The endpoint(s) to process - this list is auto populated from the server. If unspecified, all endpoints will be processed.
 
-.PARAMETER Confirm 
-Prompts you for confirmation before executing any changing operations within the command. 
+		.PARAMETER ExcludeEndpoint
+			The endpoint(s) to exclude - this list is auto populated from the server
 
-.PARAMETER Force
-Drops and recreates the endpoint if it exists
+		.PARAMETER WhatIf
+			Shows what would happen if the command were to run. No actions are actually performed.
 
-.NOTES
-Tags: Migration
-Author: Chrissy LeMaire (@cl), netnerds.net
-Requires: sysadmin access on SQL Servers
+		.PARAMETER Confirm
+			Prompts you for confirmation before executing any changing operations within the command.
 
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
+		.PARAMETER Force
+			Drops and recreates the endpoint if it exists
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+		.PARAMETER Silent
+			Use this switch to disable any kind of verbose messages
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+		.NOTES
+			Tags: Migration
+			Author: Chrissy LeMaire (@cl), netnerds.net
+			Requires: sysadmin access on SQL Servers
 
-You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
-.LINK
-https://dbatools.io/Copy-DbaEndpoint
+		.LINK
+			https://dbatools.io/Copy-DbaEndpoint
 
-.EXAMPLE   
-Copy-DbaEndpoint -Source sqlserver2014a -Destination sqlcluster
+		.EXAMPLE
+			Copy-DbaEndpoint -Source sqlserver2014a -Destination sqlcluster
 
-Copies all server endpoints from sqlserver2014a to sqlcluster, using Windows credentials. If endpoints with the same name exist on sqlcluster, they will be skipped.
+			Copies all server endpoints from sqlserver2014a to sqlcluster, using Windows credentials. If endpoints with the same name exist on sqlcluster, they will be skipped.
 
-.EXAMPLE   
-Copy-DbaEndpoint -Source sqlserver2014a -Destination sqlcluster -Endpoint tg_noDbDrop -SourceSqlCredential $cred -Force
+		.EXAMPLE
+			Copy-DbaEndpoint -Source sqlserver2014a -SourceSqlCredential $cred -Destination sqlcluster -Endpoint tg_noDbDrop -Force
 
-Copies a single endpoint, the tg_noDbDrop endpoint from sqlserver2014a to sqlcluster, using SQL credentials for sqlserver2014a and Windows credentials for sqlcluster. If an endpoint with the same name exists on sqlcluster, it will be dropped and recreated because -Force was used.
+			Copies a single endpoint, the tg_noDbDrop endpoint from sqlserver2014a to sqlcluster, using SQL credentials for sqlserver2014a and Windows credentials for sqlcluster. If an endpoint with the same name exists on sqlcluster, it will be dropped and recreated because -Force was used.
 
-.EXAMPLE   
-Copy-DbaEndpoint -Source sqlserver2014a -Destination sqlcluster -WhatIf -Force
+		.EXAMPLE
+			Copy-DbaEndpoint -Source sqlserver2014a -Destination sqlcluster -WhatIf -Force
 
-Shows what would happen if the command were executed using force.
-#>
-    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
-    param (
-        [parameter(Mandatory = $true)]
-        [DbaInstanceParameter]$Source,
-        [parameter(Mandatory = $true)]
-        [DbaInstanceParameter]$Destination,
-        [System.Management.Automation.PSCredential]$SourceSqlCredential,
-        [System.Management.Automation.PSCredential]$DestinationSqlCredential,
-        [switch]$Force
-    )
+			Shows what would happen if the command were executed using force.
+	#>
+	[CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+	param (
+		[parameter(Mandatory = $true)]
+		[DbaInstanceParameter]$Source,
+		[parameter(Mandatory = $true)]
+		[DbaInstanceParameter]$Destination,
+		[System.Management.Automation.PSCredential]$SourceSqlCredential,
+		[System.Management.Automation.PSCredential]$DestinationSqlCredential,
+		[switch]$Force
+	)
 
-    begin {
+	begin {
 
-        $sourceserver = Connect-SqlInstance -SqlInstance $Source -SqlCredential $SourceSqlCredential
-        $destserver = Connect-SqlInstance -SqlInstance $Destination -SqlCredential $DestinationSqlCredential
-		
-        $source = $sourceserver.DomainInstanceName
-        $destination = $destserver.DomainInstanceName
-		
-        if ($sourceserver.versionMajor -lt 9 -or $destserver.versionMajor -lt 9) {
-            throw "Server Endpoints are only supported in SQL Server 2008 and above. Quitting."
-        }
-    }
-    process {
+		$sourceserver = Connect-SqlInstance -SqlInstance $Source -SqlCredential $SourceSqlCredential
+		$destserver = Connect-SqlInstance -SqlInstance $Destination -SqlCredential $DestinationSqlCredential
 
-        $serverendpoints = $sourceserver.Endpoints | Where-Object { $_.IsSystemObject -eq $false }
-        $destendpoints = $destserver.Endpoints
-		
-        foreach ($endpoint in $serverendpoints) {
-            $endpointname = $endpoint.name
-			
-            if ($endpoints.length -gt 0 -and $endpoints -notcontains $endpointname) { continue }
-			
-            if ($destendpoints.name -contains $endpointname) {
-                if ($force -eq $false) {
-                    Write-Warning "Server endpoint $endpointname exists at destination. Use -Force to drop and migrate."
-                    continue
-                }
-                else {
-                    If ($Pscmdlet.ShouldProcess($destination, "Dropping server endpoint $endpointname and recreating")) {
-                        try {
-                            Write-Output "Dropping server endpoint $endpointname"
-                            $destserver.endpoints[$endpointname].Drop()
-                        }
-                        catch { 
-                            Write-Exception $_ 
-                            continue
-                        }
-                    }
-                }
-            }
-			
-            If ($Pscmdlet.ShouldProcess($destination, "Creating server endpoint $endpointname")) {
-                try {
-                    Write-Output "Copying server endpoint $endpointname"
-                    $destserver.ConnectionContext.ExecuteNonQuery($endpoint.Script()) | Out-Null
-                }
-                catch {
-                    Write-Exception $_
-                }
-            }
+		$source = $sourceserver.DomainInstanceName
+		$destination = $destserver.DomainInstanceName
 
-        }
-    }
-    end {
-        Test-DbaDeprecation -DeprecatedOn "1.0.0" -Silent:$false -Alias Copy-SqlEndpoint
-    }
+		if ($sourceserver.versionMajor -lt 9 -or $destserver.versionMajor -lt 9) {
+			throw "Server Endpoints are only supported in SQL Server 2008 and above. Quitting."
+		}
+	}
+	process {
+
+		$serverendpoints = $sourceserver.Endpoints | Where-Object { $_.IsSystemObject -eq $false }
+		$destendpoints = $destserver.Endpoints
+
+		foreach ($endpoint in $serverendpoints) {
+			$endpointname = $endpoint.name
+
+			if ($endpoints.length -gt 0 -and $endpoints -notcontains $endpointname) { continue }
+
+			if ($destendpoints.name -contains $endpointname) {
+				if ($force -eq $false) {
+					Write-Warning "Server endpoint $endpointname exists at destination. Use -Force to drop and migrate."
+					continue
+				}
+				else {
+					If ($Pscmdlet.ShouldProcess($destination, "Dropping server endpoint $endpointname and recreating")) {
+						try {
+							Write-Output "Dropping server endpoint $endpointname"
+							$destserver.endpoints[$endpointname].Drop()
+						}
+						catch {
+							Write-Exception $_
+							continue
+						}
+					}
+				}
+			}
+
+			If ($Pscmdlet.ShouldProcess($destination, "Creating server endpoint $endpointname")) {
+				try {
+					Write-Output "Copying server endpoint $endpointname"
+					$destserver.ConnectionContext.ExecuteNonQuery($endpoint.Script()) | Out-Null
+				}
+				catch {
+					Write-Exception $_
+				}
+			}
+
+		}
+	}
+	end {
+		Test-DbaDeprecation -DeprecatedOn "1.0.0" -Silent:$false -Alias Copy-SqlEndpoint
+	}
 }
