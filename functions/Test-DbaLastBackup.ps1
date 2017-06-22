935801345dc0907b85816b412195c585ac4f6f73
Stuart Moore <stuart@stuart-moore.com>
2017-06-22 15:32:42 +0100

    IgnoreLogBackup option is now a bit more useful!

--- functions/Test-DbaLastBackup.ps1
+++ functions/Test-DbaLastBackup.ps1
@@ -266,7 +266,18 @@ Copies the backup files for sql2014 databases to sql2016 default backup location
 						Stop-Function -Message "$dbname does not exist on $source." -Continue
 					}
 					
-					$lastbackup = Get-DbaBackupHistory -SqlInstance $sourceserver -Database $dbname -Last -IgnoreCopyOnly:$ignorecopyonly -raw
+					if (Was-Bound "IgnoreLogBackup"){
+						Write-Message -Level Verbose -Message "Skipping Log backups as requested"
+						$lastbackup = @()
+						$lastbackup += $full = Get-DbaBackupHistory -SqlInstance $sourceserver -Database $dbname -IgnoreCopyOnly:$ignorecopyonly -raw	-LastFull
+						$diff = Get-DbaBackupHistory -SqlInstance $sourceserver -Database $dbname -IgnoreCopyOnly:$ignorecopyonly -raw -LastDiff
+						if ($full.start -le $diff.start){
+							$lastbackup += $diff
+						}									
+					}
+					else {
+						$lastbackup = Get-DbaBackupHistory -SqlInstance $sourceserver -Database $dbname -Last -IgnoreCopyOnly:$ignorecopyonly -raw
+					}
 					if ($CopyFile) {
 						try {
 							Write-Message -Level Verbose -Message "Gathering information for file copy"
