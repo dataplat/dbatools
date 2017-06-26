2e78337238e40d48d68453b155d904a1b7e691d1
Shawn Melton <wshawnmelton@gmail.com>
2017-06-26 06:32:19 +0000

    forgot to add license in help

--- functions/Copy-DbaExtendedEvent.ps1
+++ functions/Copy-DbaExtendedEvent.ps1
@@ -53,6 +53,10 @@ function Copy-DbaExtendedEvent {
 			Author: Chrissy LeMaire (@cl), netnerds.net
 			Requires: sysadmin access on SQL Servers
 
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
 		.LINK
 			https://dbatools.io/Copy-DbaExtendedEvent
 
