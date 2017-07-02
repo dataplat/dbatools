6206fba696061512f8475e56f413e128ac253807
ctrlbold <clemaire@gmail.com>
2017-07-02 15:39:52 +0200

    removed long running test

--- tests/Get-DbaAgentJobOutputFile.Tests.ps1
+++ tests/Get-DbaAgentJobOutputFile.Tests.ps1
@@ -1,12 +1,15 @@
 ## needs some proper tests for the function here
 Describe "Get-DbaAgentJobOutputFile Unit Tests" -Tag 'Unittests' {
     Context "Input Validation" {
-        It 'SqlServer parameter is empty' {
+		It 'SqlInstance parameter is empty' {
             { Get-DbaAgentJobOutputFile -SqlInstance '' -WarningAction Stop 3> $null } | Should Throw
-        }
-        It 'SqlServer parameter host cannot be found' {
+		}
+		<#
+		This takes 15 seconds to timeout for not much reward
+		It 'SqlInstance parameter host cannot be found' {
             Mock Connect-SqlInstance { throw System.Data.SqlClient.SqlException }
             { Get-DbaAgentJobOutputFile -SqlInstance 'ABC' -WarningAction Stop 3> $null } | Should Throw
         }
+		#>
     }
 }
