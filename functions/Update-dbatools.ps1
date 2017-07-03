5e45b65bd680b792b325029459959ed8a49889b0
Andy Levy <andy@levyclan.us>
2017-07-03 14:40:07 -0400

    Add support for installing the development branch
    Change casing to match other dbatools cmdlets, add support for `WhatIf` and `Verbose`, allow user to select the development branch

--- functions/Update-dbatools.ps1
+++ functions/Update-dbatools.ps1
@@ -1,11 +1,20 @@
-Function Update-Dbatools
+Function Update-DbaTools
 {
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact="Low")]
+param(
+	[parameter(Mandatory=$false)]
+	[Alias("dev","devbranch")]
+	[switch]$Development
+)
 <#
 .SYNOPSIS
 Exported function. Updates dbatools. Deletes current copy and replaces it with freshest copy.
 
 .DESCRIPTION
 Exported function. Updates dbatools. Deletes current copy and replaces it with freshest copy.
+
+.PARAMETER Development
+Installs the current development branch of dbatools instead of the latest release.
 
 .NOTES 
 dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
@@ -28,10 +37,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 https://dbatools.io/Update-dbatools
 
 .EXAMPLE
-Update-dbatools
+Update-DbaTools
 
 Updates dbatools. Deletes current copy and replaces it with freshest copy.
-	
+
+.EXAMPLE
+Update-DbaTools -dev
+
+Updates dbatools to the current development branch. Deletes current copy and replaces it with latest from github
+
 #>	
-	Invoke-Expression (Invoke-WebRequest -UseBasicParsing https://dbatools.io/in).Content
+	$MyModuleBase = (Get-Module -name dbatools).ModuleBase;
+	$InstallScript = join-path -path $MyModuleBase -ChildPath "install.ps1";
+	if($Development) {
+		Write-Verbose "Installing dev/beta channel via $Installscript";
+		if ($PSCmdlet.ShouldProcess("development branch","Updating dbatools")) {
+			& $InstallScript -beta;
+		}
+	}
+	else {
+		Write-Verbose "Installing release version via $Installscript"
+		if ($PSCmdlet.ShouldProcess("release branch","Updating dbatools")) {
+			& $InstallScript;
+		}
+	}
 }
