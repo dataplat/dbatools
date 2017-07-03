4efc2553a8315e9f0b1cc626d33dcb8c32adaf1f
Shawn Melton <wshawnmelton@gmail.com>
2017-07-02 21:52:36 -0500

    adjust parameter list, add tepp params

--- functions/Copy-DbaSqlPolicyManagement.ps1
+++ functions/Copy-DbaSqlPolicyManagement.ps1
@@ -4,8 +4,8 @@ function Copy-DbaSqlPolicyManagement {
 			Migrates SQL Policy Based Management Objects, including both policies and conditions.
 
 		.DESCRIPTION
-			By default, all policies and conditions are copied. If an object already exist on the destination, it will be skipped unless -Force is used. 
-				
+			By default, all policies and conditions are copied. If an object already exist on the destination, it will be skipped unless -Force is used.
+
 			The -Policy and -Condition parameters are autopopulated for command-line completion and can be used to copy only specific objects.
 
 		.PARAMETER Source
@@ -14,9 +14,9 @@ function Copy-DbaSqlPolicyManagement {
 		.PARAMETER SourceSqlCredential
 			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
 
-			$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter. 
+			$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter.
 
-			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
+			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
 			To connect as a different Windows user, run PowerShell as that user.
 
 		.PARAMETER Destination
@@ -25,9 +25,9 @@ function Copy-DbaSqlPolicyManagement {
 		.PARAMETER DestinationSqlCredential
 			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
 
-			$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter. 
+			$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter.
 
-			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials. 	
+			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
 			To connect as a different Windows user, run PowerShell as that user.
 
 		.PARAMETER Policy
@@ -45,13 +45,13 @@ function Copy-DbaSqlPolicyManagement {
 		.PARAMETER Force
 			If policies exists on destination server, it will be dropped and recreated.
 
-		.PARAMETER WhatIf 
-			Shows what would happen if the command were to run. No actions are actually performed. 
+		.PARAMETER WhatIf
+			Shows what would happen if the command were to run. No actions are actually performed.
 
-		.PARAMETER Confirm 
-			Prompts you for confirmation before executing any changing operations within the command. 
+		.PARAMETER Confirm
+			Prompts you for confirmation before executing any changing operations within the command.
 
-		.PARAMETER Silent 
+		.PARAMETER Silent
 			Use this switch to disable any kind of verbose messages
 
 		.NOTES
@@ -64,24 +64,24 @@ function Copy-DbaSqlPolicyManagement {
 			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
 		.LINK
-			https://dbatools.io/Copy-DbaSqlPolicyManagement 
+			https://dbatools.io/Copy-DbaSqlPolicyManagement
 
-		.EXAMPLE   
+		.EXAMPLE
 			Copy-DbaSqlPolicyManagement -Source sqlserver2014a -Destination sqlcluster
 
-			Copies all policies and conditions from sqlserver2014a to sqlcluster, using Windows credentials. 
+			Copies all policies and conditions from sqlserver2014a to sqlcluster, using Windows credentials.
 
-		.EXAMPLE   
+		.EXAMPLE
 			Copy-DbaSqlPolicyManagement -Source sqlserver2014a -Destination sqlcluster -SourceSqlCredential $cred
 
 			Copies all policies and conditions from sqlserver2014a to sqlcluster, using SQL credentials for sqlserver2014a and Windows credentials for sqlcluster.
 
-		.EXAMPLE   
+		.EXAMPLE
 			Copy-DbaSqlPolicyManagement -Source sqlserver2014a -Destination sqlcluster -WhatIf
 
 			Shows what would happen if the command were executed.
-			
-		.EXAMPLE   
+
+		.EXAMPLE
 			Copy-DbaSqlPolicyManagement -Source sqlserver2014a -Destination sqlcluster -Policy 'xp_cmdshell must be disabled'
 
 			Copies only one policy, 'xp_cmdshell must be disabled' from sqlserver2014a to sqlcluster. No conditions are migrated.
@@ -90,52 +90,59 @@ function Copy-DbaSqlPolicyManagement {
 	param (
 		[parameter(Mandatory = $true)]
 		[DbaInstanceParameter]$Source,
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$SourceSqlCredential,
 		[parameter(Mandatory = $true)]
 		[DbaInstanceParameter]$Destination,
-		[System.Management.Automation.PSCredential]$SourceSqlCredential,
-		[System.Management.Automation.PSCredential]$DestinationSqlCredential,
-		[switch]$Force
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$DestinationSqlCredential,
+		[object[]]$Policy,
+		[object[]]$ExcludePolicy,
+		[object[]]$Condition,
+		[object[]]$ExcludeCondition,
+		[switch]$Force,
+		[switch]$Silent
 	)
 
 	begin {
 		if ([System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Dmf") -eq $null) {
 			throw "SMO version is too old. To migrate Policies, you must have SQL Server Management Studio 2008 R2 or higher installed."
 		}
-		
+
 		$sourceserver = Connect-SqlInstance -SqlInstance $Source -SqlCredential $SourceSqlCredential
 		$destserver = Connect-SqlInstance -SqlInstance $Destination -SqlCredential $DestinationSqlCredential
-		
+
 		$source = $sourceserver.DomainInstanceName
 		$destination = $destserver.DomainInstanceName
 
 		if ($sourceserver.versionMajor -lt 10 -or $destserver.versionMajor -lt 10) {
 			throw "Policy Management is only supported in SQL Server 2008 and above. Quitting."
 		}
-		
+
 	}
 	process {
 
 		$sourceSqlConn = $sourceserver.ConnectionContext.SqlConnectionObject
 		$sourceSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $sourceSqlConn
 		$sourceStore = New-Object  Microsoft.SqlServer.Management.DMF.PolicyStore $sourceSqlStoreConnection
-		
+
 		$destSqlConn = $destserver.ConnectionContext.SqlConnectionObject
 		$destSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $destSqlConn
 		$destStore = New-Object  Microsoft.SqlServer.Management.DMF.PolicyStore $destSqlStoreConnection
-		
+
 		$storepolicies = $sourceStore.policies | Where-Object { $_.IsSystemObject -eq $false }
 		$storeconditions = $sourceStore.conditions | Where-Object { $_.IsSystemObject -eq $false }
-		
+
 		if ($Policy.length -gt 0) { $storepolicies = $storepolicies | Where-Object { $Policy -contains $_.Name } }
 		if ($Condition.length -gt 0) { $storeconditions = $storeconditions | Where-Object { $Condition -contains $_.Name } }
-		
+
 		if ($Policy.length -gt 0 -and $Condition.length -eq 0) { $storeconditions = $null }
 		if ($Condition.length -gt 0 -and $Policy.length -eq 0) { $storepolicies = $null }
-		
-		<# 
+
+		<#
 						Conditions
 		#>
-		
+
 		Write-Output "Migrating conditions"
 		foreach ($condition in $storeconditions) {
 			$conditionName = $condition.name
@@ -149,7 +156,7 @@ function Copy-DbaSqlPolicyManagement {
 					if ($Pscmdlet.ShouldProcess($destination, "Attempting to drop $conditionName")) {
 						Write-Verbose "Condition '$conditionName' exists on $destination"
 						Write-Verbose "Force specified. Dropping $conditionName."
-						
+
 						try {
 							$dependentpolicies = $deststore.conditions[$conditionName].EnumDependentPolicies()
 							foreach ($dependent in $dependentpolicies) {
@@ -165,7 +172,7 @@ function Copy-DbaSqlPolicyManagement {
 					}
 				}
 			}
-			
+
 			if ($Pscmdlet.ShouldProcess($destination, "Migrating condition $conditionName")) {
 				try {
 					$sql = $condition.ScriptCreate().GetScript() | Out-String
@@ -180,11 +187,11 @@ function Copy-DbaSqlPolicyManagement {
 				}
 			}
 		}
-		
-		<# 
+
+		<#
 						Policies
 		#>
-		
+
 		Write-Output "Migrating policies"
 		foreach ($policy in $storepolicies) {
 			$policyName = $policy.name
@@ -198,7 +205,7 @@ function Copy-DbaSqlPolicyManagement {
 					if ($Pscmdlet.ShouldProcess($destination, "Attempting to drop $policyName")) {
 						Write-Verbose "Policy '$policyName' exists on $destination"
 						Write-Verbose "Force specified. Dropping $policyName."
-						
+
 						try {
 							$deststore.policies[$policyName].Drop()
 							$deststore.policies.refresh()
@@ -210,7 +217,7 @@ function Copy-DbaSqlPolicyManagement {
 					}
 				}
 			}
-			
+
 			if ($Pscmdlet.ShouldProcess($destination, "Migrating policy $policyName")) {
 				try {
 					$deststore.conditions.Refresh()
