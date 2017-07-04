f6e917a63b0e9be2eb5ec92b8688a90cdec16280
PowerDBAKlaas <klaas@powerdba.eu>
2017-07-04 15:50:22 +0200

    initial commit of Get-DbaDatabaseUser

---
+++ functions/Get-DbaDatabaseUser.ps1
@@ -0,0 +1,103 @@
+﻿Function Get-DbaDatabaseUser {
+	<#
+.SYNOPSIS
+Gets database users
+
+.DESCRIPTION
+Gets database users
+
+.PARAMETER SqlInstance
+The target SQL Server instance
+
+.PARAMETER SqlCredential
+Allows you to login to SQL Server using alternative credentials
+
+.PARAMETER Database
+To get users from specific database
+
+.PARAMETER ExcludeDatabase
+The database(s) to exclude - this list is auto populated from the server
+
+.PARAMETER WhatIf
+Shows what would happen if the command were to run. No actions are actually performed
+
+.PARAMETER Confirm
+Prompts you for confirmation before executing any changing operations within the command
+
+.PARAMETER Silent
+Use this switch to disable any kind of verbose messages
+
+.NOTES
+Tags: security, Databases
+Author: Klaas Vandenberghe ( @PowerDbaKlaas )
+
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+.EXAMPLE
+Get-DbaDatabaseUser -SqlInstance sql2016
+
+Gets all database users
+
+.EXAMPLE
+Get-DbaDatabaseUser -SqlInstance Server1 -Database db1
+
+Gets the users for the db1 database
+
+#>
+	[CmdletBinding()]
+	param (
+		[parameter(Mandatory, ValueFromPipeline)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+		[PSCredential][System.Management.Automation.CredentialAttribute()]$SqlCredential,
+		[object[]]$Database,
+		[object[]]$ExcludeDatabase,
+		[switch]$Silent
+	)
+
+	process {
+		foreach ($instance in $SqlInstance) {
+			try {
+				Write-Message -Level Verbose -Message "Connecting to $instance"
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
+			}
+			catch {
+				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+			}
+			
+			$databases = $server.Databases
+			
+			if ($Database) {
+				$databases = $databases | Where-Object Name -In $Database
+			}
+			if ($ExcludeDatabase) {
+				$databases = $databases | Where-Object Name -NotIn $ExcludeDatabase
+			}
+
+			foreach ($db in $databases) {
+				if (!$db.IsAccessible) {
+					Write-Message -Level Warning -Message "Database $db is not accessible. Skipping."
+					continue
+				}
+
+				$users = $db.users
+
+				if (!$users) {
+					Write-Message -Message "No users exist in the $db database on $instance" -Target $db -Level Verbose
+					continue
+				}
+                $users | foreach {
+
+				Add-Member -InputObject $_ -MemberType NoteProperty -Name ComputerName -value $server.NetName
+				Add-Member -InputObject $_ -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
+				Add-Member -InputObject $_ -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
+				Add-Member -InputObject $_ -MemberType NoteProperty -Name Database -value $db.Name
+
+				Select-DefaultView -InputObject $_ -Property ComputerName, InstanceName, SqlInstance, Database, CreateDate, DateLastModified, Name, Login, LoginType, AuthenticationType, State, HasDbAccess, DefaultSchema
+                }
+			}
+		}
+	}
+}
