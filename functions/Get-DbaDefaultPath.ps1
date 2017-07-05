d10452cbf7a43c298455e7c6bc42ae3ae10a11d6
ctrlbold <clemaire@gmail.com>
2017-07-05 23:47:55 +0200

    updates

--- functions/Get-DbaDefaultPath.ps1
+++ functions/Get-DbaDefaultPath.ps1
@@ -57,25 +57,46 @@
 				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
 			}
 			
-			
 			$datapath = $server.DefaultFile
+			
+			if ($datapath.Length -eq 0) {
+				$datapath = $server.ConnectionContext.ExecuteScalar("SELECT SERVERPROPERTY('InstanceDefaultDataPath')")
+			}
+			
+			<# eh, this can be a failback if we can get it to work ;)
+			if ($datapath -eq [System.DBNull]::Value -or $datapath.Length -eq 0) {
+				$datapath = $server.ConnectionContext.ExecuteScalar("master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'DefaultData'")
+			}
+			#>
+			
+			if ($datapath -eq [System.DBNull]::Value -or $datapath.Length -eq 0) {
+				$datapath = Split-Path (Get-DbaDatabase -SqlInstance $server -Database model).FileGroups[0].Files[0].FileName
+			}
 			
 			if ($datapath.Length -eq 0) {
 				$datapath = $server.Information.MasterDbPath
 			}
 			
-			if ($datapath.Length -eq 0) {
-				$datapath = $server.ConnectionContext.ExecuteScalar("select SERVERPROPERTY('InstanceDefaultDataPath') as physical_name")
-			}
-			
 			$logpath = $server.DefaultLog
 			
 			if ($logpath.Length -eq 0) {
-				$logpath = $server.Information.MasterDbLogPath
+				$logpath = $server.ConnectionContext.ExecuteScalar("SELECT SERVERPROPERTY('InstanceDefaultLogPath')")
 			}
-
+			
+			<# 
+			eh, this can be a failback if we can get it to work ;)
+			right now, the Get-Database thing below causes enumeration which slows stuff down on systems with hundreds of dbs
+			if ($logpath -eq [System.DBNull]::Value -or $logpath.Length -eq 0) {
+				$logpath = $server.ConnectionContext.ExecuteScalar("master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'DefaultLog'")
+			}
+			#>
+			
+			if ($logpath -eq [System.DBNull]::Value -or $logpath.Length -eq 0) {
+				$logpath = Split-Path (Get-DbaDatabase -SqlInstance $server -Database model).LogFiles.FileName
+			}
+			
 			if ($logpath.Length -eq 0) {
-				$logpath = $server.ConnectionContext.ExecuteScalar("select SERVERPROPERTY('InstanceDefaultLogPath') as physical_name")
+				$logpath = $server.Information.MasterDbLogPath
 			}
 			
 			$datapath = $datapath.Trim().TrimEnd("\")
