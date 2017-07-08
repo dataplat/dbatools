af87966cd45671800f2c4076bb2da3f07d535ea8
Stuart Moore <stuart@stuart-moore.com>
2017-07-08 21:48:00 +0100

    adding exception on db kill for continue option

--- internal/Restore-DBFromFilteredArray.ps1
+++ internal/Restore-DBFromFilteredArray.ps1
@@ -94,7 +94,9 @@ Function Restore-DBFromFilteredArray {
                     try {
                         Write-Message -Level Verbose -Message "Set $DbName single_user to kill processes"
                         Stop-DbaProcess -SqlInstance $Server -Databases $Dbname -WarningAction Silentlycontinue
-                        Invoke-DbaSqlcmd -ServerInstance:$SqlInstance -Credential:$SqlCredential -query "Alter database $DbName set offline with rollback immediate; alter database $DbName set restricted_user; Alter database $DbName set online with rollback immediate" -database master
+                        if ($Continue -eq $false) {
+                            Invoke-DbaSqlcmd -ServerInstance:$SqlInstance -Credential:$SqlCredential -query "Alter database $DbName set offline with rollback immediate; alter database $DbName set restricted_user; Alter database $DbName set online with rollback immediate" -database master
+                        }
                         $server.ConnectionContext.Connect()
                     }
                     catch {
