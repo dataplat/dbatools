4ddaccb5b2a10a8041d4a56f2db6bf52152ccded
Simone Bizzotto <niphlod@gmail.com>
2017-07-10 15:23:29 +0200

    Copy-DbaLogin, retain old value to print proper message

--- functions/Copy-DbaLogin.ps1
+++ functions/Copy-DbaLogin.ps1
@@ -299,6 +299,7 @@ function Copy-DbaLogin {
 						# we end up here when the default database on source doesn't exist on dest
 						# if source login is a sysadmin, then set the default database to master
 						# if not, set it to tempdb (see #303)
+						$OrigdefaultDb = $defaultDb
 						try { $sourcesysadmins = $sourceServer.roles['sysadmin'].EnumMemberNames() }
 						catch { $sourcesysadmins = $sourceServer.roles['sysadmin'].EnumServerRoleMembers() }
 						if ($sourcesysadmins -contains $userName) {
@@ -306,7 +307,7 @@ function Copy-DbaLogin {
 						} else {
 							$defaultDb = "tempdb"
 						}
-						Write-Message -Level Warning -Message "$defaultDb does not exist on destination. Setting defaultdb to $defaultDb."
+						Write-Message -Level Warning -Message "$OrigdefaultDb does not exist on destination. Setting defaultdb to $defaultDb."
 					}
 
 					Write-Message -Level Verbose -Message "Set $userName defaultdb to $defaultDb"
