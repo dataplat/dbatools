d1ecf482b8978e0544bcc7b1f5887a8b8d553165
Shawn Melton <wshawnmelton@gmail.com>
2017-07-13 18:07:34 -0500

    format help, adjust parameter singular

--- functions/Get-DbaAvailabilityGroup.ps1
+++ functions/Get-DbaAvailabilityGroup.ps1
@@ -1,74 +1,74 @@
-Function Get-DbaAvailabilityGroup {
+function Get-DbaAvailabilityGroup {
 	<#
-.SYNOPSIS 
-Outputs information of the Availabilty Group(s) found on the server.
+		.SYNOPSIS 
+			Outputs information of the Availability Group(s) found on the server.
 
-.DESCRIPTION
-By default outputs a small set of information around the Availability Group found on the server.
+		.DESCRIPTION
+			By default outputs a small set of information around the Availability Group found on the server.
 
-.PARAMETER SqlInstance
-The SQL Server instance. You must have sysadmin access and server version must be SQL Server version 2012 or higher.
+		.PARAMETER SqlInstance
+			The SQL Server instance. You must have sysadmin access and server version must be SQL Server version 2012 or higher.
 
-.PARAMETER SqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. 
+		.PARAMETER SqlCredential
+			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. 
 
-.PARAMETER Detailed
-Output is expanded with more information around each Availability Group replica found on the server.
+		.PARAMETER Detailed
+			Output is expanded with more information around each Availability Group replica found on the server.
 
-.PARAMETER AvailabilityGroups
-Specify the Availability Group name that you want to get information on.
+		.PARAMETER AvailabilityGroup
+			Specify the Availability Group name that you want to get information on.
 
-.PARAMETER Simple
-Show only server name, availability groups and role.
+		.PARAMETER Simple
+			Show only server name, availability groups and role.
 
-.PARAMETER Detailed
-Shows detailed information about the AGs including EndpointUrl and BackupPriority.
+		.PARAMETER Detailed
+			Shows detailed information about the AGs including EndpointUrl and BackupPriority.
 
-.PARAMETER IsPrimary
-Returns true or false for the server passed in.
+		.PARAMETER IsPrimary
+			Returns true or false for the server passed in.
 
-.PARAMETER WhatIf 
-Shows what would happen if the command were to run. No actions are actually performed. 
+		.PARAMETER WhatIf 
+			Shows what would happen if the command were to run. No actions are actually performed. 
 
-.PARAMETER Confirm 
-Prompts you for confirmation before executing any changing operations within the command. 
+		.PARAMETER Confirm 
+			Prompts you for confirmation before executing any changing operations within the command. 
 
-.NOTES
-Tags: DisasterRecovery, Backup
-Original Author: Shawn Melton (@wsmelton) | Chrissy LeMaire (@ctrlb)
+		.NOTES
+			Tags: DisasterRecovery, AG, AvailabilityGroup
+			Original Author: Shawn Melton (@wsmelton) | Chrissy LeMaire (@ctrlb)
 
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+		.LINK
+			https://dbatools.io/Get-DbaAvailabilityGroup
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+		.EXAMPLE
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a
 
-You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+			Returns basic information on all the Availability Group(s) found on sqlserver2014a
 
-.LINK
-https://dbatools.io/Get-DbaAvailabilityGroup
+		.EXAMPLE   
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -Simple
 
-.EXAMPLE
-Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a
-Returns basic information on all the Availability Group(s) found on sqlserver2014a
+			Show only server name, availability groups and role
 
-.EXAMPLE   
-Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -Simple
-Show only server name, availability groups and role
+		.EXAMPLE   
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -Detailed
 
-.EXAMPLE   
-Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -Detailed
-Returns basic information plus additional info on each replica for all Availability Group(s) on sqlserver2014a
+			Returns basic information plus additional info on each replica for all Availability Group(s) on sqlserver2014a
 
-.EXAMPLE   
-Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a
-Shows basic information on the Availability Group AG-a on sqlserver2014a
-	
-.EXAMPLE   
-Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a -IsPrimary
-Returns true/false if the server, sqlserver2014a, is the primary replica for AG-a Availability Group
-#>
+		.EXAMPLE   
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a
+
+			Shows basic information on the Availability Group AG-a on sqlserver2014a
+			
+		.EXAMPLE   
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a -IsPrimary
+
+			Returns true/false if the server, sqlserver2014a, is the primary replica for AG-a Availability Group
+	#>
 	[CmdletBinding(SupportsShouldProcess = $true)]
 	Param (
 		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
