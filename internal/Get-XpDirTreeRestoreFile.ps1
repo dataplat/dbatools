b1d552de20f09c159ed3f65d68954d9387083e87
potatoqualitee <clemaire@gmail.com>
2017-07-16 16:45:41 +0200

    fix query again

--- internal/Get-XpDirTreeRestoreFile.ps1
+++ internal/Get-XpDirTreeRestoreFile.ps1
@@ -48,7 +48,7 @@ function Get-XpDirTreeRestoreFile {
 	}
 	
 	$query = "EXEC master.sys.xp_dirtree '$Path',1,1;"
-	$queryResult = Invoke-DbaSqlcmd -ServerInstance $server -Credential $SqlCredential -Database tempdb -Query $query
+	$queryResult = $server.Query($query,'tempdb')
 	
 	$dirs = $queryResult | where-object file -eq 0
 	$Results = @()
