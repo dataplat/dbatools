240b3dc8933db4853e9fdf3e34c3dd81d6cf7527
potatoqualitee <clemaire@gmail.com>
2017-07-16 11:52:43 +0200

    fixes #1621 - made remote too

--- functions/Get-DbaSqlManagementObject.ps1
+++ functions/Get-DbaSqlManagementObject.ps1
@@ -7,6 +7,12 @@
 			The Get-DbaSqlManagementObject returns an object with the Version and the 
 			Add-Type Load Template for each version on the server.
 
+		.PARAMETER ComputerName
+			The name of the target you would like to check
+	
+		.PARAMETER Credential
+			This command uses Windows credentials. This parameter allows you to connect remotely as a different user.
+	
 		.PARAMETER VersionNumber
 			This is the specific version number you are looking for. The function will look 
 			for that version only.
@@ -36,48 +42,70 @@
 			Returns just the version specified. If the version does not exist then it will return nothing.
 		
 	#>
+	#>
 	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory = $false, Position = 0)]
-		[int]$VersionNumber = 0,
+		[parameter(ValueFromPipeline)]
+		[Alias("ServerInstance", "SqlServer", "SqlInstance")]
+		[DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$Credential,
+		[int]$VersionNumber,
 		[switch]$Silent
 	)
-	process {
-		Write-Message -Level Verbose -Message "Checking currently loaded SMO version"
-		$loadedversion = ((([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.Fullname -like "Microsoft.SqlServer.SMO,*" }).FullName -Split ", ")[1]).TrimStart("Version=")
-		
-		Write-Message -Level Verbose -Message "Looking for SMO in the Global Assembly Cache"
-		
-		$smolist = (Get-ChildItem -Path "$env:SystemRoot\assembly\GAC_MSIL\Microsoft.SqlServer.Smo" | Sort-Object Name -Descending).Name
-		
-		$VersionList = [System.Collections.ArrayList]@()
-		
-		foreach ($version in $smolist) {
-			$array = $version.Split("__")
-			if ($VersionNumber -eq 0) {
-				Write-Message -Level Verbose -Message "Did not pass a version, looking for all versions"
-				$currentversion = $array[0]
-				$VersionList += [PSCustomObject]@{
-					Version = $currentversion
-					LoadedAssembly = $currentversion -eq $loadedversion
-					LoadTemplate = "Add-Type -AssemblyName `"Microsoft.SqlServer.Smo, Version=$($array[0]), Culture=neutral, PublicKeyToken=89845dcd8080cc91`""
-				}
+	
+	begin {
+		$scriptblock = {
+			$VersionNumber = [int]$args[0]
+			
+			Write-Verbose "Checking currently loaded SMO version"
+			$loadedversion = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.Fullname -like "Microsoft.SqlServer.SMO,*" }
+			
+			if ($loadedversion) {
+				$loadedversion = (($loadedversion.FullName -Split ", ")[1]).TrimStart("Version=")
 			}
-			else {
-				Write-Message -Level Verbose -Message "Passed version $VersionNumber, looking for that specific version"
-				if ($array[0].StartsWith("$VersionNumber.")) {
-					
-					Write-Message -Level Verbose -Message "Found the Version $VersionNumber"
+			
+			Write-Verbose "Looking for SMO in the Global Assembly Cache"
+			
+			$smolist = (Get-ChildItem -Path "$env:SystemRoot\assembly\GAC_MSIL\Microsoft.SqlServer.Smo" | Sort-Object Name -Descending).Name
+			
+			foreach ($version in $smolist) {
+				$array = $version.Split("__")
+				if ($VersionNumber -eq 0) {
+					Write-Verbose "Did not pass a version, looking for all versions"
 					$currentversion = $array[0]
-					$VersionList += [PSCustomObject]@{
+					[PSCustomObject]@{
+						ComputerName = $env:COMPUTERNAME
 						Version = $currentversion
-						LoadedAssembly = $currentversion -eq $loadedversion
+						Loaded = $currentversion -eq $loadedversion
 						LoadTemplate = "Add-Type -AssemblyName `"Microsoft.SqlServer.Smo, Version=$($array[0]), Culture=neutral, PublicKeyToken=89845dcd8080cc91`""
 					}
-					break
+				}
+				else {
+					Write-Verbose "Passed version $VersionNumber, looking for that specific version"
+					if ($array[0].StartsWith("$VersionNumber.")) {
+						Write-Verbose "Found the Version $VersionNumber"
+						$currentversion = $array[0]
+						[PSCustomObject]@{
+							ComputerName = $env:COMPUTERNAME
+							Version = $currentversion
+							Loaded = $currentversion -eq $loadedversion
+							LoadTemplate = "Add-Type -AssemblyName `"Microsoft.SqlServer.Smo, Version=$($array[0]), Culture=neutral, PublicKeyToken=89845dcd8080cc91`""
+						}
+					}
 				}
 			}
 		}
-		return $VersionList
+	}
+	
+	process {
+		foreach ($computer in $ComputerName.ComputerName) {
+			try {
+				Invoke-Command2 -Verbose -ComputerName $computer -ScriptBlock $scriptblock -Credential $Credential -ArgumentList $VersionNumber -ErrorAction Stop
+			}
+			catch {
+				Stop-Function -Continue -Message "Faiure" -ErrorRecord $_ -Target $ComputerName
+			}
+		}
 	}
 }
