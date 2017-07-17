49fd4d009bc710504f6ad1f94a8359de13b021d0
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2017-07-18 01:56:50 +0200

    Test-ElevationRequirement (#1716)
    * Added new test function
    
    * Added silent parameter
    
    * Added help, Added Test-ElevationRequirement
    
    Plus a minor bugfix
    
    * Updated elevation check
    
    * Updated Elevation test and messaging
    
    * Updated elevation test and messaging
    
    * Updated elevation check and messaging
    
    * bugfix test
    
    * Updated elevation test
    
    * Updated formatting and testing
    
    * Updated validation and messaging
    
    Also added pipeline support and fixed minor bugs
    
    * Updated messaging, pipeline, elevation test
    
    * Removed deprecated function

--- functions/Remove-DbaComputerCertificate.ps1
+++ functions/Remove-DbaComputerCertificate.ps1
@@ -1,72 +1,86 @@
 ﻿function Remove-DbaComputerCertificate {
 <#
-.SYNOPSIS
-Removes a computer certificate - useful for removing easily certs from remote computers
-
-.DESCRIPTION
-Removes a computer certificate from a local or remote compuer
-
-.PARAMETER ComputerName
-The target computer - defaults to localhost
-
-.PARAMETER Credential
-Allows you to login to $ComputerName using alternative credentials
-
-.PARAMETER Store
-Certificate store - defaults to LocalMachine (otherwise exceptions can be thrown on remote connections)
-
-.PARAMETER Folder
-Certificate folder
+	.SYNOPSIS
+		Removes a computer certificate - useful for removing easily certs from remote computers
 	
-.PARAMETER Certificate
-The target certificate object
-
-.PARAMETER Thumbprint
-The thumbprint of the certificate object 
-
-.PARAMETER WhatIf 
-Shows what would happen if the command were to run. No actions are actually performed. 
-
-.PARAMETER Confirm 
-Prompts you for confirmation before executing any changing operations within the command. 
-
-.PARAMETER Silent 
-Use this switch to disable any kind of verbose messages
-
-.NOTES
-Tags: Certificate
-
-Website: https://dbatools.io
-Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
-
-.EXAMPLE
-Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94
-
-Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the LocalMachine store on Server1
-
-.EXAMPLE
-Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 -Store User -Folder My
-
-Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the User\My (Personal) store on Server1
+	.DESCRIPTION
+		Removes a computer certificate from a local or remote compuer
+	
+	.PARAMETER ComputerName
+		The target computer - defaults to localhost
+	
+	.PARAMETER Credential
+		Allows you to login to $ComputerName using alternative credentials
+	
+	.PARAMETER Certificate
+		The target certificate object
+	
+	.PARAMETER Thumbprint
+		The thumbprint of the certificate object
+	
+	.PARAMETER Store
+		Certificate store - defaults to LocalMachine (otherwise exceptions can be thrown on remote connections)
+	
+	.PARAMETER Folder
+		Certificate folder
+	
+	.PARAMETER Silent
+		Use this switch to disable any kind of verbose messages
+	
+	.PARAMETER WhatIf
+		Shows what would happen if the command were to run. No actions are actually performed.
+	
+	.PARAMETER Confirm
+		Prompts you for confirmation before executing any changing operations within the command.
+	
+	.EXAMPLE
+		Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94
+		
+		Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the LocalMachine store on Server1
+	
+	.EXAMPLE
+		Remove-DbaComputerCertificate -ComputerName Server1 -Thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 -Store User -Folder My
+		
+		Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in the User\My (Personal) store on Server1
+	
+	.NOTES
+		Tags: Certificate
+		
+		Website: https://dbatools.io
+		Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 #>
 	[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
 	param (
 		[Alias("ServerInstance", "SqlServer", "SqlInstance")]
-		[DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
-		[PSCredential][System.Management.Automation.CredentialAttribute()]$Credential,
+		[DbaInstanceParameter[]]
+		$ComputerName = $env:COMPUTERNAME,
+		
+		[PSCredential]
+		[System.Management.Automation.CredentialAttribute()]
+		$Credential,
+		
 		[parameter(ParameterSetName = "Certificate", ValueFromPipeline)]
-		[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-		[string]$Thumbprint,
-		[string]$Store = "LocalMachine",
-		[string]$Folder,
-		[switch]$Silent
+		[System.Security.Cryptography.X509Certificates.X509Certificate2]
+		$Certificate,
+		
+		[string]
+		$Thumbprint,
+		
+		[string]
+		$Store = "LocalMachine",
+		
+		[string]
+		$Folder,
+		
+		[switch]
+		$Silent
 	)
 	
 	process {
 		
-		if (!$Certificate -and !$Thumbprint) {
-			Write-Message -Level Warning -Message "You must specify either Certificate or Thumbprint"
+		if (-not $Certificate -and -not $Thumbprint) {
+			Stop-Function -Message "You must specify either Certificate or Thumbprint" -Category InvalidArgument
 			return
 		}
 		
@@ -94,8 +108,8 @@ Removes certificate with thumbprint C2BBE81A94FEE7A26FFF86C2DFDAF6BFD28C6C94 in 
 				
 				[pscustomobject]@{
 					ComputerName = $env:COMPUTERNAME
-					Thumbprint = $thumbprint
-					Status = $status
+					Thumbprint   = $thumbprint
+					Status	     = $status
 				}
 			}
 			
