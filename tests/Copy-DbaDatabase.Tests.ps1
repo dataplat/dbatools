5125877d3519e55536162f794a1aaba0ae36f02b
ConstaineK <Constantine@Constantinekokkinos.com>
2017-07-18 21:33:54 +0100

    changing instance names to match the standard names

--- tests/Copy-DbaDatabase.Tests.ps1
+++ tests/Copy-DbaDatabase.Tests.ps1
@@ -1,8 +1,8 @@
 ﻿Describe "Copy-DbaDatabase Integration Tests" -Tags "Integrationtests" {
     # constants
-    $Sql2008R2SP2 = "localhost\sql2008r2sp2"
-    $Sql2016 = "localhost\sql2016"
-    $Instances = @( $Sql2008R2SP2, $Sql2016 )        
+    $sql2008 = "localhost\sql2008"
+    $sql2016 = "localhost\sql2016"
+    $Instances = @( $sql2008, $sql2016 )        
     $BackupLocation = "C:\github\appveyor-lab\singlerestore\singlerestore.bak"    
     $NetworkPath = "C:\temp"
 
@@ -12,19 +12,19 @@
     }
     
     # Restore and set owner for Single Restore
-    $DatabaseName = ( Restore-DbaDatabase -SqlInstance $Sql2008R2SP2 -Path $BackupLocation ).DatabaseName
-    Set-DbaDatabaseOwner -SqlInstance $Sql2008R2SP2 -Database $DatabaseName -TargetLogin sa
+    $DatabaseName = ( Restore-DbaDatabase -SqlInstance $sql2008 -Path $BackupLocation ).DatabaseName
+    Set-DbaDatabaseOwner -SqlInstance $sql2008 -Database $DatabaseName -TargetLogin sa
 
 
     # no matter where I put the import, this stop-message fails.
     Context "Restores database with the same properties." {
         It "Should copy a database and retain its name, recovery model, and status." {
             
-            $db1 = Get-DbaDatabase -SqlInstance $Sql2008R2SP2 -Database $DatabaseName
+            $db1 = Get-DbaDatabase -SqlInstance $sql2008 -Database $DatabaseName
 
-            Copy-DbaDatabase -Source $Sql2008R2SP2 -Destination $Sql2016 -Database $DatabaseName -BackupRestore -NetworkShare $NetworkPath
+            Copy-DbaDatabase -Source $sql2008 -Destination $sql2016 -Database $DatabaseName -BackupRestore -NetworkShare $NetworkPath
             
-            $db2 = Get-DbaDatabase -SqlInstance $Sql2016 -Database $DatabaseName            
+            $db2 = Get-DbaDatabase -SqlInstance $sql2016 -Database $DatabaseName            
             $db2 | Should Not BeNullOrEmpty
             
             # Compare its valuable.
