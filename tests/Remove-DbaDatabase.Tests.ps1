52fa1b60ae4ed48e463c03a03e3a144e71fdc97b
Constantine <constantine@constantinekokkinos.com>
2017-07-18 20:03:48 +0100

    Additional Copy-DbaDatabase/Remove-DbaDatabase tests as well as additional parameter to Remove-DbaDatabase (#1740)
    * Changing a bunch of stuff to figure out pester pieces
    
    * working Copy-DbaDatabase starting tests
    
    * modifying to make match appveyor setup
    
    * remove module import and fix cleanup task
    
    * changing param to singular, fixing missing help, trying to make cleanup work

---
+++ tests/Remove-DbaDatabase.Tests.ps1
@@ -0,0 +1,25 @@
+﻿Describe "Remove-DbaDatabase Integration Tests" -Tags "Integrationtests" {
+	Context "Should not attempt to remove system databases and skip them if provided" {
+        $Sql2008 = "localhost\sql2008r2sp2"
+        $dbs = @( "master", "model", "tempdb", "msdb" )
+        
+		It "Should not attempt to remove system databases." {                        
+            foreach ($db in $dbs) { 
+                $db1 = Get-DbaDatabase -SqlInstance $Sql2008 -Database $db
+                Remove-DbaDatabase -SqlInstance $Sql2008 -Database $db
+                $db2 = Get-DbaDatabase -SqlInstance $Sql2008 -Database $db
+                $db2.Name | Should Be $db1.Name
+            } 
+        }
+
+        It "Should not take system databases offline or change their status." {
+            foreach ($db in $dbs) { 
+                $db1 = Get-DbaDatabase -SqlInstance $Sql2008 -Database $db
+                Remove-DbaDatabase -SqlInstance $Sql2008 -Database $db 
+                $db2 = Get-DbaDatabase -SqlInstance $Sql2008 -Database $db                
+                $db2.Status | Should Be $db1.Status
+                $db2.IsAccessible | Should Be $db1.IsAccessible                
+            } 
+        }               
+	}
+}
