dc2a7133c0c0d4a47f0971fe6bc670d00145b2ce
ConstaineK <Constantine@Constantinekokkinos.com>
2017-07-18 18:52:14 +0100

    modifying to make match appveyor setup

--- tests/Copy-DbaDatabase.Tests.ps1
+++ tests/Copy-DbaDatabase.Tests.ps1
@@ -1,40 +1,31 @@
 ﻿Describe "Copy-DbaDatabase Integration Tests" -Tags "Integrationtests" {
     import-module dbatools
     # constants
-    $sql8 = "localhost" #"localhost\sql2008r2sp2"
-    $sql16 = "localhost\sql2014" #"localhost\sql2016"
-    $inst = @( $sql8, $sql16 )        
-    $BackupLocation = "E:\backups\test\appveyor-lab\singlerestore\singlerestore.bak" #"C:\github\appveyor-lab\singlerestore\singlerestore.bak"    
-    $NetworkPath = "\\Greybox\test"
-    # cleanup
-    $inst | % { 
-        $Databases = Get-DbaDatabase -SqlInstance $_ -NoSystemDb                       
-        foreach ($db in $Databases){
-            Write-Verbose "Attempting to remove $db from $_" 
-            Remove-DbaDatabase -SqlInstance $_ -Database $db.Name -Confirm:$false
-        }
-    }
+    $Sql2008R2SP2 = "localhost\sql2008r2sp2"
+    $Sql2016 = "localhost\sql2016"
+    $Instances = @( $Sql2008R2SP2, $Sql2016 )        
+    $BackupLocation = "C:\github\appveyor-lab\singlerestore\singlerestore.bak"    
+    $NetworkPath = "C:\temp"
 
-    # prep
-    try { 
-        $DatabaseName = ( Restore-DbaDatabase -SqlInstance $sql8 -Path $BackupLocation ).DatabaseName
-        Set-DbaDatabaseOwner -SqlInstance $Sql8 -Database $DatabaseName -TargetLogin sa
+    # cleanup
+    $Instances | foreach { 
+        Get-DbaDatabase -SqlInstance $_ -NoSystemDb -Database SingleRestore | Remove-DbaDatabase -SqlInstance $_ -Database $db.Name -Confirm:$false
     }
-    catch {        
-        throw "Could not restore database."
-    }
-    if ($DatabaseName -eq $null){        
-        throw "Could not restore database."
-    }
+    
+    # Restore and set owner for Single Restore
+    $DatabaseName = ( Restore-DbaDatabase -SqlInstance $Sql2008R2SP2 -Path $BackupLocation ).DatabaseName
+    Set-DbaDatabaseOwner -SqlInstance $Sql2008R2SP2 -Database $DatabaseName -TargetLogin sa
+
 
     # no matter where I put the import, this stop-message fails.
     Context "Restores database with the same properties." {
-        It "Should have the same database properties" {
-            # Copy it to the second instance.                   
-            $db1 = Get-DbaDatabase -SqlInstance $sql8 -Database $DatabaseName
+        It "Should copy a database and retain its name, recovery model, and status." {
+            
+            $db1 = Get-DbaDatabase -SqlInstance $Sql2008R2SP2 -Database $DatabaseName
 
-            Copy-DbaDatabase -Source $sql8 -Destination $sql16 -Database $DatabaseName -BackupRestore -NetworkShare $NetworkPath
-            $db2 = Get-DbaDatabase -SqlInstance $sql16 -Database $DatabaseName            
+            Copy-DbaDatabase -Source $Sql2008R2SP2 -Destination $Sql2016 -Database $DatabaseName -BackupRestore -NetworkShare $NetworkPath
+            
+            $db2 = Get-DbaDatabase -SqlInstance $Sql2016 -Database $DatabaseName            
             $db2 | Should Not BeNullOrEmpty
             
             # Compare its valuable.
