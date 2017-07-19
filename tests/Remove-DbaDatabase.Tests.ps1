3427be27e6871602c51a7288cfd594f46d632bc4
ConstaineK <Constantine@Constantinekokkinos.com>
2017-07-19 07:44:19 +0100

    Remove-DbaDatabase user database removal test

--- tests/Remove-DbaDatabase.Tests.ps1
+++ tests/Remove-DbaDatabase.Tests.ps1
@@ -1,5 +1,5 @@
 ﻿Describe "Remove-DbaDatabase Integration Tests" -Tags "Integrationtests" {
-	Context "Should not attempt to remove system databases and skip them if provided" {
+	Context "Should not munge system databases unless explicitly told to." {
         $sql2008 = "localhost"
         $dbs = @( "master", "model", "tempdb", "msdb" )
         
@@ -21,11 +21,15 @@
                 $db2.IsAccessible | Should Be $db1.IsAccessible                
             } 
         }
-        
-        It "Should remove a non system database when asked to." {
+    }
+    Context "Should remove user databases and return useful errors if it cannot." {
+        It "Should remove a non system database." {
+            $null = Remove-DbaDatabase -SqlInstance $sql2008 -Database singlerestore
             Restore-DbaDatabase -SqlInstance $sql2008 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+            $db = Get-DbaDatabase -SqlInstance $sql2008 -Database SingleRestore
+            $db | Should Exist
             Remove-DbaDatabase -SqlInstance $sql2008 -Database singlerestore
-            $db = Get-DbaDatabase -Database singlerestore
+            $db = Get-DbaDatabase SqlInstance $sql2008 -Database singlerestore
             $db | Should Be NullOrEmpty
         }
 	}
