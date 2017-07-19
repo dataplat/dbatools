d47eb9da078d848e58d3cc54af8b172196b47761
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2017-07-19 13:07:20 +0200

    CK's changes (#1753)
    * added no confirm
    
    * updating removedbadatbasetests
    
    * modifying copydb
    
    * remove confirm
    
    * fixed remove test
    
    @ConstantineK - turns out variables don't persist outside of context!
    Something to keep in mind when we move to instance vars
    
    * fixed test

--- tests/Copy-DbaDatabase.Tests.ps1
+++ tests/Copy-DbaDatabase.Tests.ps1
@@ -1,37 +1,66 @@
 ﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "Copy-DbaDatabase Integration Tests" -Tags "Integrationtests" {
-    # constants
-    $Sql2008R2SP2 = "localhost\sql2008r2sp2"
-    $Sql2016 = "localhost\sql2016"
-    $Instances = @( $Sql2008R2SP2, $Sql2016 )        
-    $BackupLocation = "C:\github\appveyor-lab\singlerestore\singlerestore.bak"    
-    $NetworkPath = "C:\temp"
-
-    # cleanup
-    foreach ($instance in $Instances) {
-        Remove-DbaDatabase -SqlInstance $instance -Confirm:$false -Database singlerestore
-    }
-    
-    # Restore and set owner for Single Restore
-    $DatabaseName = ( Restore-DbaDatabase -SqlInstance $Sql2008R2SP2 -Path $BackupLocation ).DatabaseName
-    Set-DbaDatabaseOwner -SqlInstance $Sql2008R2SP2 -Database $DatabaseName -TargetLogin sa
-
-
-    # no matter where I put the import, this stop-message fails.
-    Context "Restores database with the same properties." {
-        It "Should copy a database and retain its name, recovery model, and status." {
-            
-            $db1 = Get-DbaDatabase -SqlInstance $Sql2008R2SP2 -Database $DatabaseName
-
-            Copy-DbaDatabase -Source $Sql2008R2SP2 -Destination $Sql2016 -Database $DatabaseName -BackupRestore -NetworkShare $NetworkPath
-            
-            $db2 = Get-DbaDatabase -SqlInstance $Sql2016 -Database $DatabaseName            
-            $db2 | Should Not BeNullOrEmpty
-            
-            # Compare its valuable.
-            $db1.Name | Should Be $db2.Name
-            $db1.RecoveryModel | Should Be $db2.RecoveryModel
-            $db1.Status | Should be $db2.Status
-        }        
-    }    
+	# constants
+	$script:sql2008 = "localhost\sql2008r2sp2"
+	$script:sql2016 = "localhost\sql2016"
+	$Instances = @($script:sql2008, $script:sql2016)
+	$BackupLocation = "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+	$NetworkPath = "C:\temp"
+	
+	# cleanup
+	foreach ($instance in $Instances) {
+		Remove-DbaDatabase -SqlInstance $instance -Confirm:$false -Database singlerestore
+	}
+	
+	# Restore and set owner for Single Restore
+	$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -WithReplace
+	Set-DbaDatabaseOwner -SqlInstance $script:sql2008 -Database singlerestore -TargetLogin sa
+	
+	Context "Restores database with the same properties." {
+		It "Should copy a database and retain its name, recovery model, and status." {
+			
+			$db1 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore
+			
+			Copy-DbaDatabase -Source $script:sql2008 -Destination $script:sql2016 -Database singlerestore -BackupRestore -NetworkShare $NetworkPath
+			
+			$db2 = Get-DbaDatabase -SqlInstance $script:sql2016 -Database singlerestore
+			$db2 | Should Not BeNullOrEmpty
+			
+			# Compare its valuable.
+			$db1.Name | Should Be $db2.Name
+			$db1.RecoveryModel | Should Be $db2.RecoveryModel
+			$db1.Status | Should be $db2.Status
+		}
+	}
+	
+	Remove-DbaDatabase -SqlInstance $script:sql2016 -Confirm:$false -Database singlerestore
+	
+	Context "Detach, copies and attaches database with the same properties." {
+		It "Should copy a database and retain its name, recovery model, and status. Should also reattach source" {
+			Set-Service BITS -StartupType Automatic
+			Get-Service BITS | Start-Service -ErrorAction SilentlyContinue
+			
+			$copy = Copy-DbaDatabase -Source $script:sql2008 -Destination $script:sql2016 -Database singlerestore -DetachAttach -Reattach -Force
+			$copy.Status | Should Be "Successful"
+			
+			# Get it again cuz it was reattached
+			$db1 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore
+			$db1 | Should Not Be $null
+			
+			$db2 = Get-DbaDatabase -SqlInstance $script:sql2016 -Database singlerestore
+			$db2 | Should Not Be $null
+						
+			# Compare its value.
+			$db1.Name | Should Be $db2.Name
+			$db1.Tables.Count | Should Be $db2.Tables.Count
+			$db1.Status | Should be $db2.Status
+			$db1.RecoveryModel | Should be $db2.RecoveryModel
+		}
+	}
+	
+	Context "Clean up" {
+		foreach ($instance in $instances) {
+			Get-DbaDatabase -SqlInstance $instance -NoSystemDb | Remove-DbaDatabase -Confirm:$false
+		}
+	}
 }
