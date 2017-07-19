17f4fc0a274680afb03dd4156a52d56adc449b3d
potatoqualitee <clemaire@gmail.com>
2017-07-19 16:27:26 +0200

    changed database

--- tests/Test-DbaLastBackup.Tests.ps1
+++ tests/Test-DbaLastBackup.Tests.ps1
@@ -4,18 +4,18 @@ Describe "Test-DbaLastBackup Integration Tests" -Tags "Integrationtests" {
     Context "Setup removes, restores and backups on the local drive for Test-DbaLastBackup" {
 		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
 
-		$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -WithReplace
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Full
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Full
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Differential
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Log
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Log
-		$null = Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Backup-DbaDatabase -Type Log
+		$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\testlastbackup.bak -WithReplace
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Full
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Full
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Differential
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Log
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Log
+		$null = Get-DbaDatabase -SqlInstance localhost -Database testlastbackup | Backup-DbaDatabase -Type Log
 	}
 	
 
     Context "Test a single database" {
-        $results = Test-DbaLastBackup -SqlInstance localhost -Database singlerestore
+        $results = Test-DbaLastBackup -SqlInstance localhost -Database testlastbackup
 		
         It "Should return success" {
 			$results.RestoreResult | Should Be "Success"
@@ -24,6 +24,7 @@ Describe "Test-DbaLastBackup Integration Tests" -Tags "Integrationtests" {
 	}
 	
 	$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
+	
 	<#
 	Context "Testing the whole instance" {
 		$results = Test-DbaLastBackup -SqlInstance localhost -ExcludeDatabase tempdb
