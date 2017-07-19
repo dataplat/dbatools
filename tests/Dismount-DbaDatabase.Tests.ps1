ba20e0417192b646665fdd412a0c3b8cd98b6c60
potatoqualitee <clemaire@gmail.com>
2017-07-19 10:26:58 +0200

    Fix dismount database test

---
+++ tests/Dismount-DbaDatabase.Tests.ps1
@@ -0,0 +1,28 @@
+﻿Describe "Dismount-DbaDatabase Integration Tests" -Tags "Integrationtests" {
+	
+	$dbname = "singlerestore"
+	$null = Get-DbaDatabase -SqlInstance localhost -Database $dbname | Remove-DbaDatabase
+	
+	Context "Setup removes, restores and backups on the local drive for Dismount-DbaDatabase" {
+		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
+		$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -DatabaseName $dbname -DestinationFilePrefix $dbname
+		$null = Get-DbaDatabase -SqlInstance localhost -Database $dbname | Backup-DbaDatabase
+	}
+	
+	Context "Detaches a single database and tests to ensure the alias still exists" {
+		$results = Detach-DbaDatabase -SqlInstance localhost -Database $dbname -Force
+		
+		It "Should return success" {
+			$results.DetachResult | Should Be "Success"
+		}
+		
+		It "Should return that the database is only Database" {
+			$results.Database | Should Be $dbname
+		}
+	}
+	
+	Context "Reattaches and deletes" {
+		$null = Attach-DbaDatabase -SqlInstance localhost -Database $dbname
+		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
+	}
+}
