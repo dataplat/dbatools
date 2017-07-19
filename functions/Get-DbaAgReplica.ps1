24c2116630aaee8af406d203a1d01da7f37af086
Shawn <shawn@base.local>
2017-07-19 02:08:22 +0100

    Initial version of command

---
+++ functions/Get-DbaAgReplica.ps1
@@ -0,0 +1,104 @@
+function Get-DbaAgReplica {
+	<#
+		.SYNOPSIS
+			Outputs the Availability Group(s)' Replica object found on the server.
+
+		.DESCRIPTION
+			Default view provides most common set of properties for information on the Availability Group(s)' Replica.
+
+		.PARAMETER SqlInstance
+			The SQL Server instance. Server version must be SQL Server version 2012 or higher.
+
+		.PARAMETER SqlCredential
+			Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted.
+
+		.PARAMETER AvailabilityGroup
+			Specify the Availability Group name that you want to get information on.
+
+		.PARAMETER Replica
+			Specify the replica to pull information on, is dependent up name that you want to get information on.
+
+		.PARAMETER Silent
+			Use this switch to disable any kind of verbose messages
+
+		.NOTES
+			Tags: DisasterRecovery, AG, AvailabilityGroup, Replica
+			Original Author: Shawn Melton (@wsmelton) | Chrissy LeMaire (@ctrlb)
+
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+		.LINK
+			https://dbatools.io/Get-DbaAvailabilityGroup
+
+		.EXAMPLE
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a
+
+			Returns basic information on all the Availability Group(s) found on sqlserver2014a
+
+		.EXAMPLE
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a
+
+			Shows basic information on the Availability Group AG-a on sqlserver2014a
+
+		.EXAMPLE
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a | Select *
+
+			Returns full object properties on all Availability Group(s) on sqlserver2014a
+
+		.EXAMPLE
+			Get-DbaAvailabilityGroup -SqlInstance sqlserver2014a -AvailabilityGroup AG-a -IsPrimary
+
+			Returns true/false if the server, sqlserver2014a, is the primary replica for AG-a Availability Group
+	#>
+	[CmdletBinding()]
+	param (
+		[parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+		[PSCredential][System.Management.Automation.CredentialAttribute()]
+		$SqlCredential,
+		[parameter(ValueFromPipeline = $true)]
+		[object[]]$AvailabilityGroup,
+		[object[]]$Replica,
+		[switch]$IsPrimary,
+		[switch]$Silent
+	)
+
+	process {
+		foreach ($serverName in $SqlInstance) {
+			try {
+				$server = Connect-SqlInstance -SqlInstance $serverName -SqlCredential $SqlCredential -MinimumVersion 11
+			}
+			catch {
+				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+			}
+
+			if ($server.IsHadrEnabled -eq $false) {
+				Stop-Function -Message "Availability Group (HADR) is not configured for the instance: $serverName" -Target $serverName -Continue
+			}
+
+			$ags = $server.AvailabilityGroups
+			if ($AvailabilityGroup) {
+				$ags = $ags | Where-Object Name -in $AvailabilityGroup
+			}
+
+			foreach ($ag in $ags) {
+				$replicas = $ag.AvailabilityReplicas
+				if ($Replica -and $replicas.Name -notin $Replica) {
+					continue
+				}
+				foreach ($currentReplica in $replicas) {
+
+					Add-Member -Force -InputObject $currentReplica -MemberType NoteProperty -Name ComputerName -value $server.NetName
+					Add-Member -Force -InputObject $currentReplica -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
+					Add-Member -Force -InputObject $currentReplica -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
+
+					$defaults = 'ComputerName','InstanceName','SqlInstance','Parent as AvailabilityGroup','Name as Replica','AvailabilityMode','BackupPriority','EndpointUrl','SessionTimeout','FailoverMode','ReadonlyRoutingList'
+					Select-DefaultView -InputObject $currentReplica -Property $defaults
+				}
+			}
+		}
+	}
+}
