62b75543dccead77fc5bd79bf66503e05e34dcd4
ConstaineK <Constantine@Constantinekokkinos.com>
2017-07-19 06:44:22 +0100

    adding tests to remove-dbadatabase to highlight the issue

--- tests/Remove-DbaDatabase.Tests.ps1
+++ tests/Remove-DbaDatabase.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿Describe "Remove-DbaDatabase Integration Tests" -Tags "Integrationtests" {
 	Context "Should not attempt to remove system databases and skip them if provided" {
-        $sql2008 = "localhost\sql2008r2sp2"
+        $sql2008 = localhost
         $dbs = @( "master", "model", "tempdb", "msdb" )
         
 		It "Should not attempt to remove system databases." {                        
@@ -20,6 +20,13 @@
                 $db2.Status | Should Be $db1.Status
                 $db2.IsAccessible | Should Be $db1.IsAccessible                
             } 
-        }               
+        }
+        
+        It "Should remove a non system database when asked to." {
+            Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+            Remove-DbaDatabase -SqlInstance localhost -Database singlerestore
+            $db = Get-DbaDatabase -Database singlerestore
+            $db | Should Be NullOrEmpty
+        }
 	}
 }
