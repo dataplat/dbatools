700e65a5740badad9d4ee13a7e1d6e74841128ae
potatoqualitee <clemaire@gmail.com>
2017-07-19 12:33:08 +0200

    fixed remove test
    @ConstantineK - turns out variables don't persist outside of context!
    Something to keep in mind when we move to instance vars

--- tests/Remove-DbaDatabase.Tests.ps1
+++ tests/Remove-DbaDatabase.Tests.ps1
@@ -1,23 +1,23 @@
 ﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "Remove-DbaDatabase Integration Tests" -Tags "Integrationtests" {
 	Context "Should not munge system databases unless explicitly told to." {
-        $sql2008 = "localhost\sql2008r2sp2"
+        $script:sql2008 = "localhost\sql2008r2sp2"
         $dbs = @( "master", "model", "tempdb", "msdb" )
         
 		It "Should not attempt to remove system databases." {                        
             foreach ($db in $dbs) { 
-                $db1 = Get-DbaDatabase -SqlInstance $sql2008 -Database $db
-                Remove-DbaDatabase -SqlInstance $sql2008 -Database $db
-                $db2 = Get-DbaDatabase -SqlInstance $sql2008 -Database $db
+                $db1 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database $db
+                Remove-DbaDatabase -SqlInstance $script:sql2008 -Database $db
+                $db2 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database $db
                 $db2.Name | Should Be $db1.Name
             } 
         }
 
         It "Should not take system databases offline or change their status." {
             foreach ($db in $dbs) { 
-                $db1 = Get-DbaDatabase -SqlInstance $sql2008 -Database $db
-                Remove-DbaDatabase -SqlInstance $sql2008 -Database $db 
-                $db2 = Get-DbaDatabase -SqlInstance $sql2008 -Database $db                
+                $db1 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database $db
+                Remove-DbaDatabase -SqlInstance $script:sql2008 -Database $db 
+                $db2 = Get-DbaDatabase -SqlInstance $script:sql2008 -Database $db                
                 $db2.Status | Should Be $db1.Status
                 $db2.IsAccessible | Should Be $db1.IsAccessible                
             } 
@@ -25,13 +25,11 @@ Describe "Remove-DbaDatabase Integration Tests" -Tags "Integrationtests" {
     }
     Context "Should remove user databases and return useful errors if it cannot." {
         It "Should remove a non system database." {
-            Remove-DbaDatabase -SqlInstance $sql2008 -Database singlerestore
-            Restore-DbaDatabase -SqlInstance $sql2008 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
-            $db = Get-DbaDatabase -SqlInstance $sql2008 -Database SingleRestore
-            $db | Should Exist
-            Remove-DbaDatabase -SqlInstance $sql2008 -Database singlerestore
-            $db = Get-DbaDatabase SqlInstance $sql2008 -Database singlerestore
-            $db | Should Be NullOrEmpty
+			Remove-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore
+            Restore-DbaDatabase -SqlInstance $script:sql2008 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+            (Get-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore).IsAccessible | Should Be $true
+            Remove-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore
+            Get-DbaDatabase -SqlInstance $script:sql2008 -Database singlerestore | Should Be $null
         }
 	}
 }
