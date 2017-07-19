50cd144b919769daaab31c68a8d2dcc8355b0e46
potatoqualitee <clemaire@gmail.com>
2017-07-19 11:25:36 +0200

    fixed dismount test - learned about Contexts

--- tests/Dismount-DbaDatabase.Tests.ps1
+++ tests/Dismount-DbaDatabase.Tests.ps1
@@ -3,10 +3,15 @@ Describe "Dismount-DbaDatabase Integration Tests" -Tags "Integrationtests" {
 	$dbname = "detachattach"
 	$null = Get-DbaDatabase -SqlInstance localhost -Database $dbname | Remove-DbaDatabase
 	
-	Context "Setup removes, restores and backups on the local drive for Dismount-DbaDatabase" {
+	Context "Setup removes, restores and backups and preps reattach" {
 		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
-		$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\detachattach\detachattach.bak -DatabaseName $dbname -DestinationFilePrefix $dbname -WithReplace
-		$null = Get-DbaDatabase -SqlInstance localhost -Database $dbname | Backup-DbaDatabase -Type Full
+		$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\detachattach\detachattach.bak -WithReplace
+		
+		$script:fileStructure = New-Object System.Collections.Specialized.StringCollection
+		
+		foreach ($file in (Get-DbaDatabaseFile -SqlInstance localhost -Database $dbname).PhysicalName) {
+			$null = $script:fileStructure.Add($file)
+		}
 	}
 	
 	Context "Detaches a single database and tests to ensure the alias still exists" {
@@ -22,7 +27,7 @@ Describe "Dismount-DbaDatabase Integration Tests" -Tags "Integrationtests" {
 	}
 	
 	Context "Reattaches and deletes" {
-		$null = Attach-DbaDatabase -SqlInstance localhost -Database $dbname
+		$null = Attach-DbaDatabase -SqlInstance localhost -Database $dbname -FileStructure $script:fileStructure
 		$null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
 	}
 }
