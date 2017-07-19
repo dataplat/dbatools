6cba6270e71d41f296abffda157ac40777173207
potatoqualitee <clemaire@gmail.com>
2017-07-19 08:47:27 +0200

    Revert "temporarily disable tests"
    This reverts commit cab32374c47be556fe31c626941212970fe2c937.

--- tests/Dismount-DbaDatabase.Tests.ps1
+++ tests/Dismount-DbaDatabase.Tests.ps1
@@ -2,9 +2,9 @@
 
     Context "Setup removes, restores and backups on the local drive for Dismount-DbaDatabase" {
         $null = Get-DbaDatabase -SqlInstance localhost -NoSystemDb | Remove-DbaDatabase
-        #$null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+        $null = Restore-DbaDatabase -SqlInstance localhost -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
     }
-	<#
+	
     Context "Detaches a single database and tests to ensure the alias still exists" {
         $results = Detach-DbaDatabase -SqlInstance localhost -Database singlerestore
 		
@@ -21,5 +21,4 @@
 		$null = Attach-DbaDatabase -SqlInstance localhost -Database singlerestore
 		Get-DbaDatabase -SqlInstance localhost -Database singlerestore | Remove-DbaDatabase
 	}
-	#>
 }
