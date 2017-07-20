ba02cfc2ed0e7f6cdb33591326b93e717df94445
potatoqualitee <clemaire@gmail.com>
2017-07-20 22:36:02 +0200

    Test - @ConstantineK - please note my use of a constant here

---
+++ tests/Get-DbaRegisteredServersStore.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+Describe "Get-DbaRegisteredServersStore  Integration Tests" -Tags "IntegrationTests" {
+	Context "Components are properly retreived" {
+		
+		It "Should return the right values" {
+			$results = Get-DbaRegisteredServersStore -SqlInstance localhost\sql2016
+			$results.InstanceName | Should Be "SQL2016"
+			$results.DisplayName | Should Be "Central Management Servers"
+		}
+	}
+}
