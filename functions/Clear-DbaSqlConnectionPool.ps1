292fe8cb5c9636523c152591eceedabfd73abdb6
jpomfret <jpomfret7@gmail.com>
2017-07-24 19:07:09 -0400

    Formatted help according to template, add cred type, case change on variables

--- functions/Clear-DbaSqlConnectionPool.ps1
+++ functions/Clear-DbaSqlConnectionPool.ps1
@@ -11,13 +11,13 @@ function Clear-DbaSqlConnectionPool {
 		Ref: https://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlconnection.clearallpools(v=vs.110).aspx
 
 	.PARAMETER ComputerName
-		Target computer(s). If no computer name is specified, the local computer is targeted
+		Target computer(s). If no computer name is specified, the local computer is targeted.
 
 	.PARAMETER Credential
 		Alternate credential object to use for accessing the target computer(s).
 
 	.PARAMETER Silent
-		Use this switch to disable any kind of verbose messages
+		Use this switch to disable any kind of verbose messages.
 
 	.NOTES
 		Tags: Connection
@@ -44,7 +44,7 @@ function Clear-DbaSqlConnectionPool {
 		[Parameter(ValueFromPipeline = $true)]
 		[Alias("cn", "host", "Server")]
 		[DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
-		[PSCredential]$Credential,
+		[PSCredential][System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty,
 		[switch]$Silent
 	)
 
@@ -52,23 +52,23 @@ function Clear-DbaSqlConnectionPool {
 	{
 		# TODO: https://jamessdixon.wordpress.com/2013/01/22/ado-net-and-connection-pooling
 
-		foreach ($Computer in $ComputerName)
+		foreach ($computer in $ComputerName)
 		{
-			if ($Computer -ne $env:COMPUTERNAME -and $Computer -ne "localhost" -and $Computer -ne "." -and $Computer -ne "127.0.0.1")
+			if ($computer -ne $env:COMPUTERNAME -and $Computer -ne "localhost" -and $computer -ne "." -and $computer -ne "127.0.0.1")
 			{
-				Write-Message -Level Verbose -Message "Clearing all pools on remote computer $Computer"
+				Write-Message -Level Verbose -Message "Clearing all pools on remote computer $computer"
 				if (Test-Bound 'Credential')
 				{
-					Invoke-Command2 -ComputerName $Computer -Credential $Credential -ScriptBlock { [System.Data.SqlClient.SqlConnection]::ClearAllPools() }
+					Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock { [System.Data.SqlClient.SqlConnection]::ClearAllPools() }
 				}
 				else
 				{
-					Invoke-Command2 -ComputerName $Computer -ScriptBlock { [System.Data.SqlClient.SqlConnection]::ClearAllPools() }
+					Invoke-Command2 -ComputerName $computer -ScriptBlock { [System.Data.SqlClient.SqlConnection]::ClearAllPools() }
 				}
 			}
 			else
 			{
-				Write-Verbose "Clearing all local pools"
+				Write-Message -Level Verbose -Message "Clearing all local pools"
 				if (Test-Bound 'Credential')
 				{
 					Invoke-Command2 -Credential $Credential -ScriptBlock { [System.Data.SqlClient.SqlConnection]::ClearAllPools() }
