aeb4978179ecab568990c8528839b9e03ad72b2d
js0505 <js_0505@yahoo.com>
2017-07-25 12:42:34 -0500

    Update of Test-DbaCompression

--- functions/Test-DbaCompression.ps1
+++ functions/Test-DbaCompression.ps1
@@ -2,16 +2,15 @@
 <#
 	.SYNOPSIS
 		Returns tables and indexes with preferred compression setting.
+
      .DESCRIPTION
-		This function returns the results of a fulle table/index compression analysis.
+		This function returns the results of a full table/index compression analysis.
         This function returns the best option to date for either NONE, Page, or Row Compression.
         Remember Uptime is critical, the longer uptime, the more accurate the analysis is.
         You would probably be best if you utilized Get-DbaUptime first, before running this command.
 		
 		Test-DbaCompression script derived from GitHub and the tigertoolbox 
         (https://github.com/Microsoft/tigertoolbox/tree/master/Evaluate-Compression-Gains)
-	
-	 	Currently very large databases may present an issue, but a fix is i the works. 
 	
 	.PARAMETER SqlInstance
 		SqlInstance name or SMO object representing the SQL Server to connect to. This can be a collection and recieve pipeline input
@@ -33,7 +32,7 @@
 		Use this if you want the function to throw terminating errors you want to catch.
 	
 	.NOTES
-		Author: Jason Squires (@js_0505)
+		Author: Jason Squires (@js_0505, jstexasdba@gmail.com)
 		Tags: Compression, Table, Database
 		Website: https://dbatools.io
 		Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
@@ -66,6 +65,7 @@
 	
 	    This produces a full analysis of all your servers listed and is pushed to a csv for you to 
         analyize.
+
 #>
 	[CmdletBinding(DefaultParameterSetName = "Default")]
 	param (
@@ -94,99 +94,130 @@
 	begin {
 		Write-Message -Level System -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")"
 		$sql = "SET NOCOUNT ON;
-				CREATE TABLE ##testdbacompression ([Schema] sysname
-					,[Table_Name] sysname
-					,[Index_Name] sysname NULL
+                
+                IF OBJECT_ID('tempdb..##testdbacompression' , 'U') IS NOT NULL
+                DROP TABLE ##testdbacompression
+				
+                IF OBJECT_ID('tempdb..##tmpEstimateRow' , 'U') IS NOT NULL
+                DROP TABLE ##tmpEstimateRow
+
+                IF OBJECT_ID('tempdb..##tmpEstimatePage' , 'U') IS NOT NULL
+                DROP TABLE ##tmpEstimatePage
+
+                CREATE TABLE ##testdbacompression ([Schema] sysname
+					,[TableName] sysname
+					,[IndexName] sysname NULL
 					,[Partition] int
-					,[Index_ID] int
-					,[Index_Type] VARCHAR(12)
-					,[Percent_Scan] smallint
-					,[Percent_Update] smallint
-					,[ROW_estimate_Pct_of_orig] bigint
-					,[PAGE_estimate_Pct_of_orig] bigint
-					,[Compression_Type_Recommendation] VARCHAR(7)
-					,size_cur bigint
-					,size_req bigint
+					,[IndexID] int
+					,[IndexType] VARCHAR(12)
+					,[PercentScan] smallint
+					,[PercentUpdate] smallint
+					,[ROWestimatePctoforig] bigint
+					,[PAGEestimatePctoforig] bigint
+					,[CompressionTypeRecommendation] VARCHAR(7)
+					,sizecur bigint
+					,sizereq bigint
+					,percentcompression numeric(10,2)
 				);
+
 				CREATE TABLE ##tmpEstimateRow (
 					objname sysname
 					,schname sysname
 					,indid int
 					,partnr int
-					,size_cur bigint
-					,size_req bigint
-					,sample_cur bigint
-					,sample_req bigint
+					,sizecur bigint
+					,sizereq bigint
+					,samplecur bigint
+					,samplereq bigint
 				);
+
 				CREATE TABLE ##tmpEstimatePage (
 					objname sysname
 					,schname sysname
 					,indid int
 					,partnr int
-					,size_cur bigint
-					,size_req bigint
-					,sample_cur bigint
-					,sample_req bigint
+					,sizecur bigint
+					,sizereq bigint
+					,samplecur bigint
+					,samplereq bigint
 				);
+
 				INSERT INTO ##testdbacompression 
 				([Schema]
-				,[Table_Name]
-				,[Index_Name]
+				,[TableName]
+				,[IndexName]
 				,[Partition]
-				,[Index_ID]
-				,[Index_Type]
-				,[Percent_Scan]
-				,[Percent_Update]
+				,[IndexID]
+				,[IndexType]
+				,[PercentScan]
+				,[PercentUpdate]
 				)
-				SELECT s.name AS [Schema], o.name AS [Table_Name], x.name AS [Index_Name],
-				       i.partition_number AS [Partition], i.index_id AS [Index_ID], x.type_desc AS [Index_Type],
-				       i.range_scan_count * 100.0 / (i.range_scan_count + i.leaf_insert_count + i.leaf_delete_count + i.leaf_update_count + i.leaf_page_merge_count + i.singleton_lookup_count) AS [Percent_Scan],
-				       i.leaf_update_count * 100.0 / (i.range_scan_count + i.leaf_insert_count + i.leaf_delete_count + i.leaf_update_count + i.leaf_page_merge_count + i.singleton_lookup_count) AS [Percent_Update]
+				SELECT s.name AS [Schema], o.name AS [TableName], x.name AS [IndexName],
+				       i.partition_number AS [Partition], i.Index_ID AS [IndexID], x.type_desc AS [IndexType],
+				       i.range_scan_count * 100.0 / (i.range_scan_count + i.leaf_insert_count + i.leaf_delete_count + i.leaf_update_count + i.leaf_page_merge_count + i.singleton_lookup_count) AS [PercentScan],
+				       i.leaf_update_count * 100.0 / (i.range_scan_count + i.leaf_insert_count + i.leaf_delete_count + i.leaf_update_count + i.leaf_page_merge_count + i.singleton_lookup_count) AS [PercentUpdate]
 				FROM sys.dm_db_index_operational_stats (db_id(), NULL, NULL, NULL) i
 					INNER JOIN sys.objects o ON o.object_id = i.object_id
 					INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
-					INNER JOIN sys.indexes x ON x.object_id = i.object_id AND x.index_id = i.index_id
+					INNER JOIN sys.indexes x ON x.object_id = i.object_id AND x.Index_ID = i.Index_ID
+					INNER JOIN sys.partitions p on x.object_id = p.object_id and x.Index_ID = p.Index_ID
 				WHERE (i.range_scan_count + i.leaf_insert_count + i.leaf_delete_count + leaf_update_count + i.leaf_page_merge_count + i.singleton_lookup_count) <> 0
-					AND objectproperty(i.object_id,'IsUserTable') = 1
-				ORDER BY [Table_Name] ASC;
+					AND objectproperty(i.object_id,'IsUserTable') = 1 and p.data_compression_desc = 'NONE' and p.rows>0
+				ORDER BY [TableName] ASC;
+
 				DECLARE @schema sysname, @tbname sysname, @ixid int
-				DECLARE cur CURSOR FAST_FORWARD FOR SELECT [Schema], [Table_Name], [Index_ID] FROM ##testdbacompression
+				DECLARE cur CURSOR FAST_FORWARD FOR SELECT [Schema], [TableName], [IndexID] FROM ##testdbacompression
 				OPEN cur
 				FETCH NEXT FROM cur INTO @schema, @tbname, @ixid
 				WHILE @@FETCH_STATUS = 0
 				BEGIN
-					--SELECT @schema, @tbname
+					DECLARE @sqlcmd NVARCHAR(500)
+					SET @sqlcmd = 'EXEC sp_estimate_data_compression_savings ''' + @schema + ''', ''' + @tbname + ''', ''' + cast(@ixid as varchar)+ ''', NULL, ''ROW''';
 					INSERT INTO ##tmpEstimateRow
 					(objname 
 					,schname 
 					,indid 
 					,partnr 
-					,size_cur 
-					,size_req 
-					,sample_cur 
-					,sample_req 
+					,sizecur 
+					,sizereq 
+					,samplecur 
+					,samplereq 
 					)
-					EXEC ('sp_estimate_data_compression_savings ''' + @schema + ''', ''' + @tbname + ''', ''' + @ixid + ''', NULL, ''ROW''' );
+                    EXECUTE sp_executesql @sqlcmd
+                    
+                    SET @sqlcmd = 'EXEC sp_estimate_data_compression_savings ''' + @schema + ''', ''' + @tbname + ''', ''' + cast(@ixid as varchar)+ ''', NULL, ''PAGE''';
 					INSERT INTO ##tmpEstimatePage
-					EXEC ('sp_estimate_data_compression_savings ''' + @schema + ''', ''' + @tbname + ''', ''' + @ixid + ''', NULL, ''PAGE''');
-					FETCH NEXT FROM cur INTO @schema, @tbname, @ixid
-				END
+					(objname 
+					,schname 
+					,indid 
+					,partnr 
+					,sizecur 
+					,sizereq 
+					,samplecur 
+					,samplereq 
+					)
+                    EXECUTE sp_executesql @sqlcmd
+
+                    FETCH NEXT FROM cur INTO @schema, @tbname, @ixid
+				
+                END
 				CLOSE cur
 				DEALLOCATE cur;
-				WITH tmp_cte (objname, schname, indid, pct_of_orig_row, pct_of_orig_page, size_cur,size_req) 
+
+				WITH tmp_cte (objname, schname, indid, pct_of_orig_row, pct_of_orig_page, sizecur,sizereq) 
 				     AS (SELECT tr.objname, 
 				                tr.schname, 
 				                tr.indid, 
-				                ( tr.sample_req * 100 ) / CASE 
-				                                            WHEN tr.sample_cur = 0 THEN 1 
-				                                            ELSE tr.sample_cur 
+				                ( tr.samplereq * 100 ) / CASE 
+				                                            WHEN tr.samplecur = 0 THEN 1 
+				                                            ELSE tr.samplecur 
 				                                          END AS pct_of_orig_row, 
-				                ( tp.sample_req * 100 ) / CASE 
-				                                            WHEN tp.sample_cur = 0 THEN 1 
-				                                            ELSE tp.sample_cur 
+				                ( tp.samplereq * 100 ) / CASE 
+				                                            WHEN tp.samplecur = 0 THEN 1 
+				                                            ELSE tp.samplecur 
 				                                          END AS pct_of_orig_page,
-								tr.size_cur,
-								tr.size_req
+								tr.sizecur,
+								tr.sizereq
 				         FROM   ##tmpestimaterow tr 
 				                INNER JOIN ##tmpestimatepage tp 
 				                        ON tr.objname = tp.objname 
@@ -194,54 +225,62 @@
 				                           AND tr.indid = tp.indid 
 				                           AND tr.partnr = tp.partnr) 
 				UPDATE ##testdbacompression 
-				SET    [row_estimate_pct_of_orig] = tcte.pct_of_orig_row, 
-				       [page_estimate_pct_of_orig] = tcte.pct_of_orig_page,
-					   size_cur=tcte.size_cur,
-					   size_req=tcte.size_req
+				SET    [ROWestimatePctoforig] = tcte.pct_of_orig_row, 
+				       [PAGEestimatePctoforig] = tcte.pct_of_orig_page,
+					   sizecur=tcte.sizecur,
+					   sizereq=tcte.sizereq
 				FROM   tmp_cte tcte, 
 				       ##testdbacompression tcomp 
-				WHERE  tcte.objname = tcomp.table_name 
+				WHERE  tcte.objname = tcomp.TableName 
 				       AND tcte.schname = tcomp.[schema] 
-				       AND tcte.indid = tcomp.index_id; 
-				WITH tmp_cte2 (table_name, [schema], index_id, [compression_type_recommendation] 
+				       AND tcte.indid = tcomp.IndexID; 
+
+				WITH tmp_cte2 (TableName, [schema], IndexID, [CompressionTypeRecommendation] 
 				     ) 
-				     AS (SELECT table_name, 
+				     AS (SELECT TableName, 
 				                [schema], 
-				                index_id, 
+				                IndexID, 
 				                CASE 
-				                  WHEN [row_estimate_pct_of_orig] >= 100 
-				                       AND [page_estimate_pct_of_orig] >= 100 THEN 'NO_GAIN' 
-				                  WHEN [percent_update] >= 10 THEN 'ROW' 
-				                  WHEN [percent_scan] <= 1 
-				                       AND [percent_update] <= 1 
-				                       AND [row_estimate_pct_of_orig] < 
-				                           [page_estimate_pct_of_orig] 
+				                  WHEN [ROWestimatePctoforig] >= 100 
+				                       AND [PAGEestimatePctoforig] >= 100 THEN 'NO_GAIN' 
+				                  WHEN [PercentUpdate] >= 10 THEN 'ROW' 
+				                  WHEN [PercentScan] <= 1 
+				                       AND [PercentUpdate] <= 1 
+				                       AND [ROWestimatePctoforig] < 
+				                           [PAGEestimatePctoforig] 
 				                THEN 
 				                  'ROW' 
-				                  WHEN [percent_scan] <= 1 
-				                       AND [percent_update] <= 1 
-				                       AND [row_estimate_pct_of_orig] > 
-				                           [page_estimate_pct_of_orig] 
+				                  WHEN [PercentScan] <= 1 
+				                       AND [PercentUpdate] <= 1 
+				                       AND [ROWestimatePctoforig] > 
+				                           [PAGEestimatePctoforig] 
 				                THEN 
 				                  'PAGE' 
-				                  WHEN [percent_scan] >= 60 
-				                       AND [percent_update] <= 5 THEN 'PAGE' 
-				                  WHEN [percent_scan] <= 35 
-				                       AND [percent_update] <= 5 THEN '?' 
+				                  WHEN [PercentScan] >= 60 
+				                       AND [PercentUpdate] <= 5 THEN 'PAGE' 
+				                  WHEN [PercentScan] <= 35 
+				                       AND [PercentUpdate] <= 5 THEN '?' 
 				                  ELSE 'ROW' 
 				                END 
 				         FROM   ##testdbacompression) 
+
 				UPDATE ##testdbacompression 
-				SET    [compression_type_recommendation] = 
-				       tcte2.[compression_type_recommendation] 
+				SET    [CompressionTypeRecommendation] = 
+				       tcte2.[CompressionTypeRecommendation]
 				FROM   tmp_cte2 tcte2, 
 				       ##testdbacompression tcomp2 
-				WHERE  tcte2.table_name = tcomp2.table_name 
+				WHERE  tcte2.TableName = tcomp2.TableName 
 				       AND tcte2.[schema] = tcomp2.[schema] 
-				       AND tcte2.index_id = tcomp2.index_id; 
+				       AND tcte2.IndexID = tcomp2.IndexID; 
+				
+				UPDATE ##testdbacompression
+				set percentcompression = 100 -(cast([sizereq] as numeric(10,2)) * 100/([sizecur]-ABS(SIGN([sizecur]))+1)) 
+				from ##testdbacompression 
+
 				SET NOCOUNT ON;
-				DECLARE @UpTime VARCHAR(12), @StartDate DATETIME, @sqlmajorver int, @sqlcmd NVARCHAR(500), @params NVARCHAR(500)
+				DECLARE @UpTime VARCHAR(12), @StartDate DATETIME, @sqlmajorver int,  @params NVARCHAR(500)
 				SELECT @sqlmajorver = CONVERT(int, (@@microsoftversion / 0x1000000) & 0xff);
+
 				IF @sqlmajorver = 9
 				BEGIN
 					SET @sqlcmd = N'SELECT @StartDateOUT = login_time, @UpTimeOUT = DATEDIFF(mi, login_time, GETDATE()) FROM master..sysprocesses WHERE spid = 1';
@@ -250,28 +289,37 @@
 				BEGIN
 					SET @sqlcmd = N'SELECT @StartDateOUT = sqlserver_start_time, @UpTimeOUT = DATEDIFF(mi,sqlserver_start_time,GETDATE()) FROM sys.dm_os_sys_info';
 				END
+
 				SET @params = N'@StartDateOUT DATETIME OUTPUT, @UpTimeOUT VARCHAR(12) OUTPUT';
+
 				EXECUTE sp_executesql @sqlcmd, @params, @StartDateOUT=@StartDate OUTPUT, @UpTimeOUT=@UpTime OUTPUT;
-				--SELECT @StartDate AS Collecting_Data_Since, CONVERT(VARCHAR(4),@UpTime/60/24) + 'd ' + CONVERT(VARCHAR(4),@UpTime/60%24) + 'h ' + CONVERT(VARCHAR(4),@UpTime%60) + 'm' AS Collecting_Data_For
+
 				SELECT 
 				DBName = DB_Name()
 				,[Schema] 
-				,[Table_Name] 
-				,[Index_Name] 
+				,[TableName] 
+				,[IndexName] 
 				,[Partition] 
-				,[Index_ID] 
-				,[Index_Type] 
-				,[Percent_Scan] 
-				,[Percent_Update] 
-				,[ROW_estimate_Pct_of_orig] 
-				,[PAGE_estimate_Pct_of_orig]
-				,[Compression_Type_Recommendation] 
-				,size_curKB = [size_cur]
-				,size_reqKB = [size_req]
+				,[IndexID] 
+				,[IndexType] 
+				,[PercentScan] 
+				,[PercentUpdate] 
+				,[ROWestimatePctoforig] 
+				,[PAGEestimatePctoforig]
+				,[CompressionTypeRecommendation] 
+				,sizecurKB = [sizecur]
+				,sizereqKB = [sizereq]
+                ,percentcompression
 				FROM ##testdbacompression;
-				DROP TABLE ##testdbacompression
-				DROP TABLE ##tmpEstimateRow
-				DROP TABLE ##tmpEstimatePage;"
+
+				IF OBJECT_ID('tempdb..##setdbacompression' , 'U') IS NOT NULL
+                DROP TABLE ##testdbacompression
+				
+                IF OBJECT_ID('tempdb..##tmpEstimateRow' , 'U') IS NOT NULL
+                DROP TABLE ##tmpEstimateRow
+
+                IF OBJECT_ID('tempdb..##tmpEstimatePage' , 'U') IS NOT NULL
+                DROP TABLE ##tmpEstimatePage;"
 	}
 	
 	process {
@@ -327,18 +375,19 @@
 							SqlInstance = $server.DomainInstanceName
 							Database = $row.DBName
 							Schema = $row.Schema
-							Table_Name = $row.Table_Name
-							Index_Name = $row.Index_Name
+							TableName = $row.TableName
+							IndexName = $row.IndexName
 							Partition = $row.Partition
-							Index_ID = $row.Index_ID
-							Index_Type = $row.Index_Type
-							Percent_Scan = $row.Percent_Scan
-							Percent_Update = $row.Percent_Update
-							ROW_estimate_Pct_of_orig = $row.ROW_estimate_Pct_of_orig
-							PAGE_estimate_Pct_of_orig = $row.PAGE_estimate_Pct_of_orig
-							Compression_Type_Recommendation = $row.Compression_Type_Recommendation
-							size_curKB = $row.size_curKB
-							size_reqKB = $row.size_reqKB
+							IndexID = $row.IndexID
+							IndexType = $row.IndexType
+							PercentScan = $row.PercentScan
+							PercentUpdate = $row.PercentUpdate
+							ROWestimatePctoforig = $row.ROWestimatePctoforig
+							PAGEestimatePctoforig = $row.PAGEestimatePctoforig
+							CompressionTypeRecommendation = $row.CompressionTypeRecommendation
+							sizecurKB = $row.sizecurKB
+							sizereqKB = $row.sizereqKB
+                            percentcompression = $row.percentcompression
 						                  }
 				        }
 				    }
