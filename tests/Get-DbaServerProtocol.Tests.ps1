d822e1c2e0154362666e187cb3e462c480bb693c
potatoqualitee <clemaire@gmail.com>
2017-07-25 10:02:26 +0200

    added test for Get-DbaServerProtoco

---
+++ tests/Get-DbaServerProtocol.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Get some server protocols" {
+		$results = Get-DbaServerProtocol
+		It "Should return some protocols" {
+			$results.Count | Should BeGreaterThan 1
+			$results | Where-Object { $_.ProtocolDisplayName -eq 'TCP/IP' } | Should Not Be $null
+		}
+	}
+}
