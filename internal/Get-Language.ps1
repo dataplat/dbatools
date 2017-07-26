0852d5774ad3066ea570d52ae597b678e03b0e5d
potatoqualitee <clemaire@gmail.com>
2017-07-26 14:24:58 +0200

    fixed get langauge, thanks @FriedrichWeinmann ! this is way better

---
+++ internal/Get-Language.ps1
@@ -0,0 +1,31 @@
+﻿function Get-Language {
+	<#
+		.SYNOPSIS
+			Converts Microsoft's language ID to human readable format 
+
+		.DESCRIPTION
+			Converts Microsoft's language ID to human readable format
+
+		.PARAMETER Id
+			The language ID
+
+		.EXAMPLE
+			Get-Language 1033
+
+			Returns a pscustomobject with id, alias and name
+	#>
+	[CmdletBinding()]
+	param (
+		[int]$id
+	)
+	process {
+		
+		$culture = [System.Globalization.CultureInfo]::GetCultureInfo($id)
+		
+		[pscustomobject]@{
+			LanguageID	   = $culture.LCID
+			Name		   = $culture.Name
+			Alias		   = $culture.DisplayName
+		}
+	}
+}
