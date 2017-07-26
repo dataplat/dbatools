b254b7133f85f71a9da09e496fc7a16d8edfafc9
Shawn Melton <wshawnmelton@gmail.com>
2017-07-25 21:53:28 -0500

    1.0 version

--- functions/Get-DbaOperatingSystem.ps1
+++ functions/Get-DbaOperatingSystem.ps1
@@ -48,24 +48,57 @@ function Get-DbaOperatingSystem {
 		foreach ($computer in $ComputerName) {
 			Write-Message -Level Verbose -Message "Attempting to connect to $computer"
 			$server = Resolve-DbaNetworkName -ComputerName $computer -Credential $Credential
-			
+
 			$computerResolved = $server.ComputerName
-			
+
 			if (!$computerResolved) {
 				Write-Message -Level Warning -Message "Unable to resolve hostname of $computer. Skipping."
 				continue
 			}
-			
+
 			if (Test-Bound "Credential") {
 				$os = Get-DbaCmObject -ClassName Win32_OperatingSystem -ComputerName $computerResolved -Credential $Credential
 				$tz = Get-DbaCmObject -ClassName Win32_TimeZone -ComputerName $computerResolved -Credential $Credential
+				$powerPlan = Get-DbaCmObject -ClassName Win32_PowerPlan -Namespace "root\cimv2\power" -ComputerName $computerResolved -Credential $Credential | Select-Object ElementName, InstanceId, IsActive
 			}
 			else {
 				$os = Get-DbaCmObject -ClassName Win32_OperatingSystem -ComputerName $computerResolved
 				$tz = Get-DbaCmObject -ClassName Win32_TimeZone -ComputerName $computerResolved
+				$powerPlan = Get-DbaCmObject -ClassName Win32_PowerPlan -Namespace "root\cimv2\power" -ComputerName $computerResolved | Select-Object ElementName, InstanceId, IsActive
 			}
 
-			Add-Member -Force -InputObject $os -MemberType NoteProperty -Name ComputerName -Value $computerResolved
+			$activePowerPlan = ($powerPlan | Where-Object IsActive).ElementName -join ','
+
+			$inputObject = [PSCustomObject]@{
+				ComputerName = $computerResolved
+				InstanceName = $computer.InstanceName
+				SqlInstance = $computer.SqlInstanceName
+				Manufacturer = $os.Manufacturer
+				Organization = $os.Organization
+				Architecture = $os.OSArchitecture
+				Version = $os.Version
+				Build = $os.BuildNumber
+				InstallDate = [DbaDateTime]$os.InstallDate
+				LastBootTime = [DbaDateTime]$os.LastBootUpTime
+				LocalDateTime = [DbaDateTime]$os.LocalDateTime
+				TimeZone = $tz.Caption
+				TimeZoneStandard = $tz.StandardName
+				TimeZoneDaylight = $tz.DaylightName
+				BootDevice = $os.BootDevice
+				TotalVisibleMemory = [DbaSize]$os.TotalVisibleMemorySize
+				FreePhysicalMemory = [DbaSize]$os.FreePhysicalMemory
+				TotalVirtualMemory = [DbaSize]$os.TotalVirtualMemorySize
+				FreeVirtualMemory = [DbaSize]$os.FreeVirtualMemory
+				ActivePowerPlan = $activePowerPlan
+				Language = $os.OSLanguage
+				CodeSet = $os.CodeSet
+				CountryCode = $os.CountryCode
+				Locale = $os.Locale
+			}
+
+			$defaults = 'ComputerName','InstanceName','SqlInstance','Manufacturer','Architecture','Build','Version','InstallDate','LastBootTime','LocalDateTime'
+
+			Select-DefaultView -InputObject $inputObject -Property $defaults
 		} #end foreach instance
 	}
 }
