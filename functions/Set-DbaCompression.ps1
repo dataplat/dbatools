6693453d338926129e0508515e96b86dcc9f17c4
js0505 <js_0505@yahoo.com>
2017-07-26 12:46:16 -0500

    Updated to use EngineEdition instead of Edition

--- functions/Set-DbaCompression.ps1
+++ functions/Set-DbaCompression.ps1
@@ -434,7 +434,7 @@
 			
             $Server.ConnectionContext.StatementTimeout = 0
                 
-                if ($Server.Edition -eq 'Standard Edition (64-bit)' -or $Server.Edition -eq 'Standard Edition (32-bit)' -and $Server.VersionMajor -lt '13')
+                if ($Server.EngineEdition -eq 'Standard' -and $Server.VersionMajor -lt '13')
                     {
                     Stop-Function -Message "Only SQL Server Enterprise Edition supports compression on $Server" -Target $Server -ErrorRecord $_ -Continue
                     }
