9d87549d69f541ad9cc30b927bf35fbef86f8462
niphlod <niphlod@gmail.com>
2017-07-28 22:30:35 +0200

    tests gentlyfication
    (do Orphaned)

--- tests/Find-DbaOrphanedFile.Tests.ps1
+++ tests/Find-DbaOrphanedFile.Tests.ps1
@@ -2,24 +2,29 @@
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-$null = Get-DbaDatabase -SqlInstance $script:instance1 -NoSystemDb | Remove-DbaDatabase
-$server = Connect-DbaSqlServer -SqlInstance $script:instance1
-$dbname = "findme"
-$server.Query("CREATE DATABASE $dbname")
-
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-	Context "Detaches a single database and tests to ensure the alias still exists" {
+	Context "Orphaned files are correctly identified" {
+		BeforeAll {
+			$server = Connect-DbaSqlServer -SqlInstance $script:instance1
+			$dbname = "dbatoolsci_findme"
+			$server.Query("CREATE DATABASE $dbname")
+		}
 		$null = Detach-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Force
 		$results = Find-DbaOrphanedFile -SqlInstance $script:instance1
-		It "Should find two files" {
+		It "Finds two files" {
 			$results.Count | Should Be 2
+		}
+		It "Has the correct properties" {
+			$ExpectedProps = 'ComputerName,InstanceName,SqlInstance,Filename,RemoteFilename'.Split(',')
+			($results.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
 		}
 		
 		$results.FileName | Remove-Item
 		
 		$results = Find-DbaOrphanedFile -SqlInstance $script:instance1
-		It "Should find zero files" {
+		It "Finds zero files after cleaning up" {
 			$results.Count | Should Be 0
 		}
+		
 	}
 }
