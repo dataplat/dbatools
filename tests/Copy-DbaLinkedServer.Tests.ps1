c50c7f8951ee726f526b208ac7f5d620445f6214
potatoqualitee <clemaire@gmail.com>
2017-07-28 22:15:55 +0200

    updated test

--- tests/Copy-DbaLinkedServer.Tests.ps1
+++ tests/Copy-DbaLinkedServer.Tests.ps1
@@ -12,56 +12,65 @@ try {
 }
 catch {
 	Write-Host "DAC not working this round, likely due to Appveyor resources"
-	return
+	#return
 }
 
+$dropsql = "EXEC master.dbo.sp_dropserver @server=N'dbatools-localhost', @droplogins='droplogins';
+			EXEC master.dbo.sp_dropserver @server=N'dbatools-localhost2', @droplogins='droplogins'"
+
+$createsql = "EXEC master.dbo.sp_addlinkedserver @server = N'dbatools-localhost', @srvproduct=N'SQL Server';
+		EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'dbatools-localhost',@useself=N'False',@locallogin=NULL,@rmtuser=N'testuser1',@rmtpassword='supfool';
+		EXEC master.dbo.sp_addlinkedserver @server = N'dbatools-localhost2', @srvproduct=N'SQL Server';
+		EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'dbatools-localhost2',@useself=N'False',@locallogin=NULL,@rmtuser=N'testuser1',@rmtpassword='supfool';"
 # One more for the road - clearing the connection pool is important for DAC since only one is allowed
 Clear-DbaSqlConnectionPool
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	Context "Create new linked server" {
-		$LinkedServers = $script:Instances | Get-DbaLinkedServer -LinkedServer localhost, localhost2
-		foreach ($LinkedServer in $LinkedServers) {
-			$LinkedServer.Drop()
+		$server1 = Connect-DbaSqlServer -SqlInstance $script:instance1
+		$server2 = Connect-DbaSqlServer -SqlInstance $script:instance2
+		try {
+			$server1.Query($dropsql)
+			$server2.Query($dropsql)
+		}
+		catch {
+			# dont care
 		}
 		
-		Clear-DbaSqlConnectionPool
-		$server = Connect-DbaSqlServer -SqlInstance $script:instance1
-		$server.Query("EXEC master.dbo.sp_addlinkedserver @server = N'localhost', @srvproduct=N'SQL Server'
-		EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'localhost',@useself=N'False',@locallogin=NULL,@rmtuser=N'testuser1',@rmtpassword='supfool'
-		EXEC master.dbo.sp_addlinkedserver @server = N'localhost2', @srvproduct=N'SQL Server'
-		EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'localhost2',@useself=N'False',@locallogin=NULL,@rmtuser=N'testuser1',@rmtpassword='supfool'
-		")
-		
-		Context "Copy Credential with the same properties." {
-			It "Should copy successfully" {
-				$results = Copy-DbaLinkedServer -Source $script:instance1 -Destination $script:instance2 -LinkedServer localhost
-				$results.Status | Should Be "Successful"
-			}
-			
-			It "Should retain its same properties" {
-				
-				$LinkedServer1 = Get-DbaLinkedServer -SqlInstance $script:instance1 -LinkedServer localhost
-				$LinkedServer2 = Get-DbaLinkedServer -SqlInstance $script:instance2 -LinkedServer localhost
-				
-				# Compare its value
-				$LinkedServer1.Name | Should Be $LinkedServer2.Name
-				$LinkedServer1.LinkedServer | Should Be $LinkedServer2.LinkedServer
-			}
-		}
-		Clear-DbaSqlConnectionPool
-		Context "No overwrite and cleanup" {
-			$results = Copy-DbaLinkedServer -Source $script:instance1 -Destination $script:instance2 -LinkedServer localhostcred -WarningVariable warning 3>&1
-			It "Should not attempt overwrite" {
-				$warning | Should Match "exists"
-				
-			}
-			# Finish up
-			$LinkedServers = $script:Instances | Get-DbaLinkedServer -LinkedServer localhost, localhost2
-			foreach ($LinkedServer in $LinkedServers) {
-				$LinkedServer.Drop()
-			}
-		}
-		Clear-DbaSqlConnectionPool
+		$server1.Query($createsql)
 	}
+	
+	Context "Copy Credential with the same properties." {
+		It "Should copy successfully" {
+			$results = Copy-DbaLinkedServer -Source $script:instance1 -Destination $script:instance2 -LinkedServer dbatools-localhost
+			$results.Status | Should Be "Successful"
+		}
+		
+		It "Should retain its same properties" {
+			
+			$LinkedServer1 = Get-DbaLinkedServer -SqlInstance $script:instance1 -LinkedServer dbatools-localhost
+			$LinkedServer2 = Get-DbaLinkedServer -SqlInstance $script:instance2 -LinkedServer dbatools-localhost
+			
+			# Compare its value
+			$LinkedServer1.Name | Should Be $LinkedServer2.Name
+			$LinkedServer1.LinkedServer | Should Be $LinkedServer2.LinkedServer
+		}
+	}
+	Clear-DbaSqlConnectionPool
+	Context "No overwrite and cleanup" {
+		$results = Copy-DbaLinkedServer -Source $script:instance1 -Destination $script:instance2 -LinkedServer dbatools-localhost -WarningVariable warning 3>&1
+		It "Should not attempt overwrite" {
+			$warning | Should Match "exists"
+			
+		}
+		
+		try {
+			$server1.Query($dropsql)
+			$server2.Query($dropsql)
+		}
+		catch {
+			# dont care
+		}
+	}
+	Clear-DbaSqlConnectionPool
 }
