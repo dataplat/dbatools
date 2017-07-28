34f985a2681ec81f9c3875e8a8394c514534f4fa
potatoqualitee <clemaire@gmail.com>
2017-07-28 20:57:41 +0200

    changed to instance2

--- tests/Test-DbaDbCompression.Tests.ps1
+++ tests/Test-DbaDbCompression.Tests.ps1
@@ -1,17 +1,19 @@
 ﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
+
 <#
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	Context "Properly restores a database on the local drive using Path" {
-		$null = Restore-DbaDatabase -SqlInstance $script:instance1 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
-		$results = Test-DbaDbCompression -SqlInstance $script:instance1 -Database singlerestore
+		$null = Restore-DbaDatabase -SqlInstance $script:instance2 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
+		$results = Test-DbaDbCompression -SqlInstance $script:instance2 -Database singlerestore
 		It "Should Return the proper backup file location" {
-			$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+			#$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
 		}
 		It "Should return successful restore" {
-			$results.RestoreComplete | Should Be $true
+			#$results.RestoreComplete | Should Be $true
 		}
+		$null = Remove-DbaDatabase -SqlInstance $script:instance2 -Database singlerestore
 	}
 }
 #>
