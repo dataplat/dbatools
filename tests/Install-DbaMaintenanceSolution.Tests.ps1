8a5bc7935feeaf4f0f2e325321446a283af9def0
potatoqualitee <clemaire@gmail.com>
2017-07-30 01:06:04 +0200

    initial commit

---
+++ tests/Install-DbaMaintenanceSolution.Tests.ps1
@@ -0,0 +1,19 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Operations on snapshots" {
+		BeforeAll {
+			$server = Connect-DbaSqlServer -SqlInstance $script:instance2
+			$server.Databases['tempdb'].Query("CREATE TABLE CommandLog (id int)")
+			}
+		AfterAll {
+			$server.Databases['tempdb'].Query("DROP TABLE CommandLog")
+		}
+		It "does not overwrite existing " {
+			$results = Install-DbaMaintenanceSolution -SqlInstance $script:instance2 -Database tempdb -WarningVariable warn -WarningAction SilentlyContinue
+			$warn -match "already exists" | Should Be $true
+		}
+	}
+}
