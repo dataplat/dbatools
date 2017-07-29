b89aeeec935ae7cd864471e2b03b2aa50f6051b4
potatoqualitee <clemaire@gmail.com>
2017-07-30 00:06:43 +0200

    updated test - can't get newly dbs to show compression

--- tests/Test-DbaDbCompression.Tests.ps1
+++ tests/Test-DbaDbCompression.Tests.ps1
@@ -2,18 +2,10 @@
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-<#
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-	Context "Properly restores a database on the local drive using Path" {
-		$null = Restore-DbaDatabase -SqlInstance $script:instance2 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
-		$results = Test-DbaDbCompression -SqlInstance $script:instance2 -Database singlerestore
-		It "Should Return the proper backup file location" {
-			#$results.BackupFile | Should Be "C:\github\appveyor-lab\singlerestore\singlerestore.bak"
+	Context "command can run" {
+		It "should be able to run - not much to test here" {
+			Test-DbaDbCompression -SqlInstance $script:instance2 -Database tempdb | Should Be $null
 		}
-		It "Should return successful restore" {
-			#$results.RestoreComplete | Should Be $true
-		}
-		$null = Remove-DbaDatabase -SqlInstance $script:instance2 -Database singlerestore
 	}
 }
-#>
