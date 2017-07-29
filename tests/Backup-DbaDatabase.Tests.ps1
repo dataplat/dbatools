d77b9c7cb6ae5f0893317d050d05c01ddbe40617
niphlod <niphlod@gmail.com>
2017-07-29 14:40:34 +0200

    Backup-DbaDatabase, tests improvements
    (do Backup-DbaDatabase)

--- tests/Backup-DbaDatabase.Tests.ps1
+++ tests/Backup-DbaDatabase.Tests.ps1
@@ -14,31 +14,49 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 		}
 	}
 	#>
-	
+	BeforeAll {
+		$DestBackupDir = 'C:\Temp\backups'
+		$random = Get-Random
+		$DestDbRandom = "dbatools_ci_backupdbadatabase$random"
+		if (-Not(Test-Path $DestBackupDir)) {
+			New-Item -Type Container -Path $DestBackupDir
+		}
+		Get-DbaDatabase -SqlInstance $script:instance1 -Database "dbatoolsci_singlerestore" | Remove-DbaDatabase
+		Get-DbaDatabase -SqlInstance $script:instance2 -Database $DestDbRandom | Remove-DbaDatabase
+	}
+	AfterAll {
+		Get-DbaDatabase -SqlInstance $script:instance1 -Database "dbatoolsci_singlerestore" | Remove-DbaDatabase
+		Get-DbaDatabase -SqlInstance $script:instance2 -Database $DestDbRandom | Remove-DbaDatabase
+		if (Test-Path $DestBackupDir) {
+			Remove-Item $DestBackupDir -Force -Recurse
+		}
+	}
 	Context "Should not backup if database and exclude match" {
-		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory C:\temp\backups -Database master -Exclude master
+		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory $DestBackupDir -Database master -Exclude master
 		It "Should not return object" {
 			$results | Should Be $null
 		}
 	}
 	
 	Context "Database should backup 1 database" {
-		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory C:\temp\backups -Database master
+		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory $DestBackupDir -Database master
 		It "Database backup object count should be 1" {
 			$results.DatabaseName.Count | Should Be 1
+			$results.BackupComplete | Should Be $true
 		}
 	}
 	
 	Context "Database should backup 2 databases" {
-		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory C:\temp\backups -Database master, msdb
+		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory $DestBackupDir -Database master, msdb
 		It "Database backup object count should be 2" {
-			$results.DatabaseName.Count  | Should Be 2
+			$results.DatabaseName.Count | Should Be 2
+			$results.BackupComplete | Should Be @($true, $true)
 		}
 	}
 	
 	Context "Backup can pipe to restore" {
-		$null = Restore-DbaDatabase -SqlServer $script:instance1 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak
-		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory C:\temp\backups -Database singlerestore | Restore-DbaDatabase -SqlInstance localhost\sql2016 -WithReplace
+		$null = Restore-DbaDatabase -SqlServer $script:instance1 -Path C:\github\appveyor-lab\singlerestore\singlerestore.bak -DatabaseName "dbatoolsci_singlerestore"
+		$results = Backup-DbaDatabase -SqlInstance $script:instance1 -BackupDirectory $DestBackupDir -Database "dbatoolsci_singlerestore" | Restore-DbaDatabase -SqlInstance $script:instance2 -DatabaseName $DestDbRandom
 		It "Should return successful restore" {
 			$results.RestoreComplete | Should Be $true
 		}
