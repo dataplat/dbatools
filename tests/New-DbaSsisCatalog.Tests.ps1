57c1e153c4f6e0106387cd72b42061bb0c4ad901
potatoqualitee <clemaire@gmail.com>
2017-07-30 12:48:11 +0200

    adding test

---
+++ tests/New-DbaSsisCatalog.Tests.ps1
@@ -0,0 +1,26 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Catalog is added properly" {
+		# database name is currently fixed
+		$database = "SSISDB"
+		$db = Get-DbaDatabase -SqlInstance $ssisserver -Database SSISDB
+		
+		if (-not $db) {
+			$password = ConvertTo-SecureString MyVisiblePassWord -AsPlainText -Force
+			$results = New-DbaSsisCatalog -SqlInstance $ssisserver -Password $password
+			
+			It "uses the specified database" {
+				$results.SsisCatalog | Should Be $database
+			}
+			
+			It "creates the catalog" {
+				$results.Created | Should Be $true
+			}
+			
+			Remove-DbaDatabase -SqlInstance $ssisserver -Database $database
+		}
+	}
+}
--- tests/constants.ps1
+++ tests/constants.ps1
@@ -7,5 +7,5 @@ else {
 	$script:instance2 = "localhost\sql2016"
 	$script:appeyorlabrepo = "C:\github\appveyor-lab"
 	$instances = @($script:instance1, $script:instance2)
-	$ssis = "localhost\sql2016"
+	$ssisserver = "localhost\sql2016"
 }
