bddec2f209b36388cd8a1f794628013bfc1861f9
potatoqualitee <clemaire@gmail.com>
2017-08-01 13:02:09 +0200

    fixes plus test

--- tests/Get-DbaPagefileSetting.Tests.ps1
+++ tests/Get-DbaPagefileSetting.Tests.ps1
@@ -1,11 +1,20 @@
-$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1","")
+$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-Describe "$commandname Unit Test" -Tags Unittest {
-	Context 'Doing something awesome' {
-		It 'It should have tests' {
-			$true | Should be $true
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Page file settings return proper info" {
+		$results = Get-DbaPageFileSetting
+		
+		if ($results.AutoPageFile) {
+			It "returns no filename" {
+				$results.FileName | Should Be $null
+			}
+		}
+		else {
+			It "return a filename" {
+				$results.FileName | Should Not Be $null
+			}
 		}
 	}
 }
