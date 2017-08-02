94cb3b898300a29973145f47cba1e890956d9d3a
niphlod <niphlod@gmail.com>
2017-08-02 22:33:04 +0200

    tests, removing empty ones and small fixes

--- tests/Remove-DbaDatabase.Tests.ps1
+++ tests/Remove-DbaDatabase.Tests.ps1
@@ -7,7 +7,7 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
        
         $dbs = @( "master", "model", "tempdb", "msdb" )
         
-		It "Should not attempt to remove system databases." {                        
+		It "Should not attempt to remove system databases." {
             foreach ($db in $dbs) { 
                 $db1 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $db
                 Remove-DbaDatabase -SqlInstance $script:instance1 -Database $db
@@ -20,9 +20,9 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
             foreach ($db in $dbs) { 
                 $db1 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $db
                 Remove-DbaDatabase -SqlInstance $script:instance1 -Database $db 
-                $db2 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $db                
+                $db2 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $db
                 $db2.Status | Should Be $db1.Status
-                $db2.IsAccessible | Should Be $db1.IsAccessible                
+                $db2.IsAccessible | Should Be $db1.IsAccessible
             } 
         }
     }
