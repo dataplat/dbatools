0126b194d07a63c2f81841168e3a5faf5e4684d9
Shawn Melton <wshawnmelton@gmail.com>
2017-08-02 20:40:25 -0500

    Add parameters to help and examples

--- functions/Get-DbaSqlLog.ps1
+++ functions/Get-DbaSqlLog.ps1
@@ -2,50 +2,66 @@
 <#
 	.SYNOPSIS
 		Gets the "SQL Error Log" of an instance
-	
+
 	.DESCRIPTION
 		Gets the "SQL Error Log" of an instance. Returns all 10 error logs by default.
 
 	.PARAMETER SqlInstance
 		The SQL Server instance, or instances.
-	
+
 	.PARAMETER SqlCredential
 		Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted.
-	
-	.PARAMETER LogNumber 
-		An Int32 value that specifies the index number of the error log required. 
+
+	.PARAMETER LogNumber
+		An Int32 value that specifies the index number of the error log required.
 		Error logs are listed 0 through 99, where 0 is the current error log and 99 is potential oldest log file.
 
 		SQL Server errorlog rollover defaults to 6, but can be increased to 99. https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/scm-services-configure-sql-server-error-logs
-	
-	.PARAMETER Silent 
+
+	.PARAMETER Source
+		Filter results based on the Source of the error (e.g. Logon, Server, etc.)
+
+	.PARAMETER Text
+		Filter results based on a pattern of text (e.g. "login failed", "error: 12345").
+
+	.PARAMETER Silent
 		Use this switch to disable any kind of verbose messages
-	
+
 	.NOTES
 		Tags: Logging
 		Website: https://dbatools.io
 		Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0	
-	
+		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
 	.LINK
 		https://dbatools.io/Get-DbaSqlLog
-	
+
 	.EXAMPLE
-		Get-DbaSqlLog -SqlInstance sql01\sharepoint 
-		
-		Returns the entire error log for the SQL Server on sql01\sharepoint 
+		Get-DbaSqlLog -SqlInstance sql01\sharepoint
+
+		Returns every log entry from sql01\sharepoint SQL Server instance.
 
 	.EXAMPLE
 		Get-DbaSqlLog -SqlInstance sql01\sharepoint -LogNumber 3, 6
-		
-		Returns log numbers 3 and 6 for the SQL Server on sql01\sharepoint 
-	
+
+		Returns all log entries for log number 3 and 6 on sql01\sharepoint SQL Server instance.
+
+	.EXAMPLE
+		Get-DbaSqlLog -SqlInstance sql01\sharepoint -Source Logon
+
+		Returns every log entry, with a source of Logon, from sql01\sharepoint SQL Server instance.
+
+	.EXAMPLE
+		Get-DbaSqlLog -SqlInstance sql01\sharepoint -LogNumber 3 -Text "login failed"
+
+		Returns every log entry for log number 3, with "login failed" in the text, from sql01\sharepoint SQL Server instance.
+
 	.EXAMPLE
 		$servers = "sql2014","sql2016", "sqlcluster\sharepoint"
 		$servers | Get-DbaSqlLog -LogNumber 0
-		
+
 		Returns the most recent SQL Server error logs for "sql2014","sql2016" and "sqlcluster\sharepoint"
-#>	
+#>
 	[CmdletBinding()]
 	param (
 		[Parameter(ValueFromPipeline = $true)]
@@ -61,14 +77,14 @@
 	process {
 		foreach ($instance in $SqlInstance) {
 			Write-Message -Level Verbose -Message "Attempting to connect to $instance"
-			
+
 			try {
 				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
 			}
 			catch {
 				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
 			}
-			
+
 			if ($LogNumber) {
 				foreach ($number in $lognumber) {
 					foreach ($object in $server.ReadErrorLog($number)) {
@@ -79,7 +95,7 @@
 						Add-Member -Force -InputObject $object -MemberType NoteProperty ComputerName -value $server.NetName
 						Add-Member -Force -InputObject $object -MemberType NoteProperty InstanceName -value $server.ServiceName
 						Add-Member -Force -InputObject $object -MemberType NoteProperty SqlInstance -value $server.DomainInstanceName
-						
+
 						# Select all of the columns you'd like to show
 						Select-DefaultView -InputObject $object -Property ComputerName, InstanceName, SqlInstance, LogDate, 'ProcessInfo as Source', Text
 					}
@@ -94,7 +110,7 @@
 					Add-Member -Force -InputObject $object -MemberType NoteProperty ComputerName -value $server.NetName
 					Add-Member -Force -InputObject $object -MemberType NoteProperty InstanceName -value $server.ServiceName
 					Add-Member -Force -InputObject $object -MemberType NoteProperty SqlInstance -value $server.DomainInstanceName
-					
+
 					# Select all of the columns you'd like to show
 					Select-DefaultView -InputObject $object -Property ComputerName, InstanceName, SqlInstance, LogDate, 'ProcessInfo as Source', Text
 				}
