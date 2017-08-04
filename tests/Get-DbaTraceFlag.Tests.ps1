88162fc6a65b881dc53c59168ff90cc36d4b718e
potatoqualitee <clemaire@gmail.com>
2017-08-04 15:11:19 +0200

    changing instance

--- tests/Get-DbaTraceFlag.Tests.ps1
+++ tests/Get-DbaTraceFlag.Tests.ps1
@@ -6,7 +6,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 	Context "Verifying TraceFlag output" {
 		BeforeAll {
 			$safetraceflag = 3226
-			$server = Connect-DbaSqlServer -SqlInstance $script:instance1
+			$server = Connect-DbaSqlServer -SqlInstance $script:instance2
 			$startingtfs = $server.Query("DBCC TRACESTATUS(-1)")
 			$startingtfscount = $startingtfs.Count
 			
@@ -23,16 +23,16 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 		
 		It "Has the right default properties" {
 			$expectedProps = 'ComputerName,InstanceName,SqlInstance,TraceFlag,Global,Status'.Split(',')
-			$results = Get-DbaTraceFlag -SqlInstance $script:instance1
+			$results = Get-DbaTraceFlag -SqlInstance $script:instance2
 			($results[0].PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames | Sort-Object) | Should Be ($expectedProps | Sort-Object)
 		}
 		
 		It "Returns filtered results" {
-			$results = Get-DbaTraceFlag -SqlInstance $script:instance1 -TraceFlag $safetraceflag
+			$results = Get-DbaTraceFlag -SqlInstance $script:instance2 -TraceFlag $safetraceflag
 			$results.TraceFlag.Count | Should Be 1
 		}
 		It "Returns following number of TFs: $startingtfscount" {
-			$results = Get-DbaTraceFlag -SqlInstance $script:instance1
+			$results = Get-DbaTraceFlag -SqlInstance $script:instance2
 			$results.TraceFlag.Count | Should Be $startingtfscount
 		}
 	}
