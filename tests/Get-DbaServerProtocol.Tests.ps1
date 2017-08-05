3a1642c731420fa5a861dde281cfc7e503b824c3
potatoqualitee <clemaire@gmail.com>
2017-08-05 13:08:48 +0200

    to match dev

--- tests/Get-DbaServerProtocol.Tests.ps1
+++ tests/Get-DbaServerProtocol.Tests.ps1
@@ -3,11 +3,19 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-	Context "Get some server protocols" {
-		$results = Get-DbaServerProtocol
-		It "Should return some protocols" {
-			$results.Count | Should BeGreaterThan 1
-			$results | Where-Object { $_.ProtocolDisplayName -eq 'TCP/IP' } | Should Not Be $null
+	
+	Context "Command actually works" {
+		$results = Get-DbaServerProtocol -ComputerName $script:instance1, $script:instance2
+		
+		It "shows some services" {
+			$results.DisplayName | Should Not Be $null
+		}
+		
+		$results = $results | Where-Object Name -eq Tcp
+		It "can get TCPIP" {
+			foreach ($result in $results) {
+				$result.Name -eq "Tcp" | Should Be $true
+			}
 		}
 	}
 }
