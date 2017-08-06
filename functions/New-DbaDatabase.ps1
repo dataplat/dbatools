0c4fd81a89db45157e5874808e4412caab14248f
unknown <matthew.darwin@btinternet.com>
2017-08-06 12:23:07 +0100

    add loop for multi instance support

--- functions/New-DbaDatabase.ps1
+++ functions/New-DbaDatabase.ps1
@@ -95,219 +95,223 @@ Creates a database named TestDatabase on instance sql1 with a user filegroup wit
         
         if (Test-FunctionInterrupt) { return }
 
-        #Verbose message to show the server
-        Write-Message -message "Connecting to server $SqlInstance" -Level Verbose
-
-        #instantiate the sql server object
-	    try {
-		    $SQLServer = Connect-SqlInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
-	    }
-	    catch {
-		    Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
-	    }
-
-        #check to see if the database already exists.
-        if ($SQLServer.Databases[$DatabaseName].Name -ne $Null)
+        foreach ($Instance in $SqlInstance) 
         {
-            Stop-Function -Message "Database $DatabaseName already exists on $TargetServer" -Target $instance -Continue
-        }
 
-        # if we are using the default file locations, get them from the server
-        if ($UseDefaultFileLocations -eq $true)
-        {
-            #get the default file locations; if the master is in the default location we use that path as the default file will not be set
-            $LocalDataDrive = if ($SQLServer.DefaultFile -eq [DBNULL]::value)
-                                        {$SQLServer.MasterDBPath} 
-                                    else
-                                        {$SQLServer.DefaultFile}
-            $LocalLogDrive = $SQLServer.DefaultLog
-        }
-        elseif ($UseDefaultFileLocations -eq $false)
-        {
-            $LocalDataDrive = $NonDefaultFileLocation
-            $LocanewlLogDrive = $NonDefaultLogLocation
-        }
+            #Verbose message to show the server
+            Write-Message -message "Connecting to server $Instance" -Level Verbose
+
+            #instantiate the sql server object
+	        try {
+		        $SQLServer = Connect-SqlInstance -SqlInstance $Instance -SqlCredential $SqlCredential
+	        }
+	        catch {
+		        Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $Instance -Continue
+	        }
+
+            #check to see if the database already exists.
+            if ($SQLServer.Databases[$DatabaseName].Name -ne $Null)
+            {
+                Stop-Function -Message "Database $DatabaseName already exists on $Instance" -Target $Instance -Continue
+            }
+
+            # if we are using the default file locations, get them from the server
+            if ($UseDefaultFileLocations -eq $true)
+            {
+                #get the default file locations; if the master is in the default location we use that path as the default file will not be set
+                $LocalDataDrive = if ($SQLServer.DefaultFile -eq [DBNULL]::value)
+                                            {$SQLServer.MasterDBPath} 
+                                        else
+                                            {$SQLServer.DefaultFile}
+                $LocalLogDrive = $SQLServer.DefaultLog
+            }
+            elseif ($UseDefaultFileLocations -eq $false)
+            {
+                $LocalDataDrive = $NonDefaultFileLocation
+                $LocanewlLogDrive = $NonDefaultLogLocation
+            }
         
-        #output message in verbose mode
-        Write-Message -message "Set local Data drive to $LocalDataDrive and local log drive to $LocalLogDrive" -level verbose
+            #output message in verbose mode
+            Write-Message -message "Set local Data drive to $LocalDataDrive and local log drive to $LocalLogDrive" -level verbose
 
-        #create the new db object
+            #create the new db object
 
-        try
-        {
-            write-message -message "Creating smo object for new database $DatabaseName" -level verbose
-            $NewDB = New-Object Microsoft.SqlServer.Management.Smo.Database($SQLServer, $DatabaseName)
-        }
-        catch
-        {
-            Stop-Function -Message "Error creating database object for $DatabaseName on server $Sqlserver" -ErrorRecord $_ -Target $instane -Continue
-        }
-
-        #add the primary filegroup and a primary file
-        try
-        {
-            write-message -message "Creating PRIMARY filegroup" -level Verbose
-            $PrimaryFG = new-object Microsoft.SqlServer.Management.Smo.Filegroup($NewDB, "PRIMARY")
-            $NewDB.Filegroups.Add($PrimaryFG)
-        }
-        catch
-        {
-            Stop-Function -Message "Error creating Primary filegroup object" -ErrorRecord $_ -Target $instance -Continue
-        }
-
-        #add the primary file
-        try
-        {
-            $PrimaryFileName = $DatabaseName + "_PRIMARY"
-            Write-Message -message "Creating file name $PrimaryFileName in filegroup PRIMARY" -level verbose
-
-            #create the filegroup object
-            $PrimaryFile = new-object Microsoft.SqlServer.Management.Smo.DataFile($PrimaryFG, $PrimaryFileName)
-            $PrimaryFile.FileName = $LocalDataDrive + "\" + $PrimaryFileName + ".mdf"
-            $PrimaryFile.Size = ($PrimaryFileSize * 1024)
-            $PrimaryFile.GrowthType = "KB"
-            $PrimaryFile.Growth = ($PrimaryFileGrowth * 1024)
-            $PrimaryFile.MaxSize = ($PrimaryFileMaxSize * 1024)
-            $PrimaryFile.IsPrimaryFile = "true"
-            #add the file to the filegroup
-            $PrimaryFG.Files.Add($PrimaryFile)
-        }
-        catch
-        {
-            Stop-Function -Message "Error adding file to Primary filegroup" -ErrorRecord $_ -Target $instance -Continue
-        }
-
-        #add the user data file group
-        try
-        {
-            $UserFilegroupName = $DatabaseName + "_MainData"
-            write-Message -message "Creating user filegroup $UserFileGroupName" -level Verbose
-            
-            $UserFG = new-object Microsoft.SqlServer.Management.Smo.Filegroup($NewDB, $UserFilegroupName)
-            $NewDB.Filegroups.Add($UserFG)
-        }
-        catch
-        {
-            Stop-Function -Message "Error creating user filegroup" -ErrorRecord $_ -Target $instance -Continue
-        }
-
-        #add the required number of files to the filegroup in a loop
-        #set the filecounter
-        $FileCounter = 1
-
-        #open a loop while the filecounter is less than the required number of files
-        While ($FileCounter -le $NumberOfFilesInUserFilegroup)
-        {
-            #Set the file name
             try
             {
-                $UserFileName = $UserFileGroupName + "_" + [string]$FileCounter
-                Write-Message -message "Creating file name $UserFileName in filegroup $UserFileGroupName" -Level Verbose
-                #create the smo object for the file
-                $UserFile = new-object Microsoft.SQLServer.Management.Smo.Datafile($UserFG, $UserFileName)
-                $UserFile.FileName = $LocalDataDrive + "\" + $USerFileName + ".ndf"
-                $UserFile.Size = ($UserDataFileSize * 1024)
-                $UserFile.GrowthType = "KB"
-                $UserFile.Growth = ($UserDataFileGrowth * 1024)
-                $UserFile.MaxSize = ($USerDataFileMaxSize * 1024)
+                write-message -message "Creating smo object for new database $DatabaseName" -level verbose
+                $NewDB = New-Object Microsoft.SqlServer.Management.Smo.Database($SQLServer, $DatabaseName)
+            }
+            catch
+            {
+                Stop-Function -Message "Error creating database object for $DatabaseName on server $Sqlserver" -ErrorRecord $_ -Target $Instance -Continue
+            }
+
+            #add the primary filegroup and a primary file
+            try
+            {
+                write-message -message "Creating PRIMARY filegroup" -level Verbose
+                $PrimaryFG = new-object Microsoft.SqlServer.Management.Smo.Filegroup($NewDB, "PRIMARY")
+                $NewDB.Filegroups.Add($PrimaryFG)
+            }
+            catch
+            {
+                Stop-Function -Message "Error creating Primary filegroup object" -ErrorRecord $_ -Target $instance -Continue
+            }
+
+            #add the primary file
+            try
+            {
+                $PrimaryFileName = $DatabaseName + "_PRIMARY"
+                Write-Message -message "Creating file name $PrimaryFileName in filegroup PRIMARY" -level verbose
+
+                #create the filegroup object
+                $PrimaryFile = new-object Microsoft.SqlServer.Management.Smo.DataFile($PrimaryFG, $PrimaryFileName)
+                $PrimaryFile.FileName = $LocalDataDrive + "\" + $PrimaryFileName + ".mdf"
+                $PrimaryFile.Size = ($PrimaryFileSize * 1024)
+                $PrimaryFile.GrowthType = "KB"
+                $PrimaryFile.Growth = ($PrimaryFileGrowth * 1024)
+                $PrimaryFile.MaxSize = ($PrimaryFileMaxSize * 1024)
+                $PrimaryFile.IsPrimaryFile = "true"
                 #add the file to the filegroup
-                $UserFG.Files.Add($UserFile)
+                $PrimaryFG.Files.Add($PrimaryFile)
             }
             catch
             {
-                Stop-Function -Message "Error adding file $FileCounter to $UserFileGroupName" -ErrorRecord $_ -Target $instance -Continue
+                Stop-Function -Message "Error adding file to Primary filegroup" -ErrorRecord $_ -Target $instance -Continue
             }
-            #increment the file counter
-            $FileCounter = $FileCounter + 1
-        }
 
-        #now create the log file
-        try
-        {
-            $LogName = $DatabaseName + "_Log"
-            write-message -message "Creating log $LogName" -level verbose
-            #add the log to the db
-            $TLog = new-object Microsoft.SqlServer.Management.Smo.LogFile($NewDB, $LogName)
-            $TLog.FileName = $LocalLogDrive + "\" + $LogName + ".ldf"
-            $TLog.Size = ($LogSize * 1024)
-            $TLog.GrowthType = "KB"
-            $TLog.Growth = ($LogGrowth * 1024)
-            #add the log to the db
-            $NewDB.LogFiles.Add($TLog)
-        }
-        catch
-        {
-            Stop-Function -Message "Error adding log file to database." -ErrorRecord $_ -Target $instance -Continue
-        }
-
-        #set database settings; collation, owner, recovery model
-
-        #set the collation
-        if ($Collation.Length -eq 0)
-        {
-            $Message = "USing default server collation"
-            Write-Verbose $Message
-        }
-        else
-        {
-            $Message = "Setting collation to $Collation"
-            Write-Verbose $Message
-            $NewDB.Collation = $Collation
-        }
-
-        #set the recovery model
-        if ($RecoveryModel.Length -eq 0)
-        {
-            $Message = "Using default recovery model from the Model database"
-            Write-Verbose $Message
-        }
-        else
-        {
-            Write-Message -message "Setting recovery model to $RecoveryModel" -level Verbose
-            $NewDB.RecoveryModel = $RecoveryModel
-        }
-
-        #we should now be able to create the db, and run any other config settings afterwards
-        Write-Message -message "Creating Datbase $DatabaseName" -level verbose
-        if ($PSCmdlet.ShouldProcess($instance, "Creating the database on $instance")) 
-        {
+            #add the user data file group
             try
             {
-                $NewDb.Create()
+                $UserFilegroupName = $DatabaseName + "_MainData"
+                write-Message -message "Creating user filegroup $UserFileGroupName" -level Verbose
+            
+                $UserFG = new-object Microsoft.SqlServer.Management.Smo.Filegroup($NewDB, $UserFilegroupName)
+                $NewDB.Filegroups.Add($UserFG)
             }
             catch
             {
-                Stop-Function -Message "Error creating Database $DatabaseName on server $SqlInstance" -ErrorRecord $_ -Target $instance -Continue
-            }      
+                Stop-Function -Message "Error creating user filegroup" -ErrorRecord $_ -Target $instance -Continue
+            }
 
+            #add the required number of files to the filegroup in a loop
+            #set the filecounter
+            $FileCounter = 1
 
-            #now do post db creation work, set the dbowner and set the default filegroup
-            #Set the owner
-            Write-Message -message "Setting database owner to $DatabaseOwner" -level verbose
+            #open a loop while the filecounter is less than the required number of files
+            While ($FileCounter -le $NumberOfFilesInUserFilegroup)
+            {
+                #Set the file name
+                try
+                {
+                    $UserFileName = $UserFileGroupName + "_" + [string]$FileCounter
+                    Write-Message -message "Creating file name $UserFileName in filegroup $UserFileGroupName" -Level Verbose
+                    #create the smo object for the file
+                    $UserFile = new-object Microsoft.SQLServer.Management.Smo.Datafile($UserFG, $UserFileName)
+                    $UserFile.FileName = $LocalDataDrive + "\" + $USerFileName + ".ndf"
+                    $UserFile.Size = ($UserDataFileSize * 1024)
+                    $UserFile.GrowthType = "KB"
+                    $UserFile.Growth = ($UserDataFileGrowth * 1024)
+                    $UserFile.MaxSize = ($USerDataFileMaxSize * 1024)
+                    #add the file to the filegroup
+                    $UserFG.Files.Add($UserFile)
+                }
+                catch
+                {
+                    Stop-Function -Message "Error adding file $FileCounter to $UserFileGroupName" -ErrorRecord $_ -Target $instance -Continue
+                }
+                #increment the file counter
+                $FileCounter = $FileCounter + 1
+            }
+
+            #now create the log file
             try
             {
-                $NewDB.SetOwner($DatabaseOwner)
+                $LogName = $DatabaseName + "_Log"
+                write-message -message "Creating log $LogName" -level verbose
+                #add the log to the db
+                $TLog = new-object Microsoft.SqlServer.Management.Smo.LogFile($NewDB, $LogName)
+                $TLog.FileName = $LocalLogDrive + "\" + $LogName + ".ldf"
+                $TLog.Size = ($LogSize * 1024)
+                $TLog.GrowthType = "KB"
+                $TLog.Growth = ($LogGrowth * 1024)
+                #add the log to the db
+                $NewDB.LogFiles.Add($TLog)
             }
             catch
             {
-                Stop-Function -Message "Error setting Database Owner to $DatabaseOwner" -ErrorRecord $_ -Target $instance -Continue
+                Stop-Function -Message "Error adding log file to database." -ErrorRecord $_ -Target $instance -Continue
             }
 
-            #set the user filegroup to be the default
-            write-message -message "Setting default filegroup to $UserFileGroupName" -level verbose
-            try
+            #set database settings; collation, owner, recovery model
+
+            #set the collation
+            if ($Collation.Length -eq 0)
             {
-                $NewDB.SetDefaultFileGroup($UserFileGroupName)
+                $Message = "USing default server collation"
+                Write-Verbose $Message
             }
-            catch
+            else
             {
-                Stop-Function -Message "Error setting default filegorup to $UserFileGroupName" -ErrorRecord $_ -Target $instance -Continue
+                $Message = "Setting collation to $Collation"
+                Write-Verbose $Message
+                $NewDB.Collation = $Collation
             }
 
+            #set the recovery model
+            if ($RecoveryModel.Length -eq 0)
+            {
+                $Message = "Using default recovery model from the Model database"
+                Write-Verbose $Message
+            }
+            else
+            {
+                Write-Message -message "Setting recovery model to $RecoveryModel" -level Verbose
+                $NewDB.RecoveryModel = $RecoveryModel
+            }
+
+            #we should now be able to create the db, and run any other config settings afterwards
+            Write-Message -message "Creating Datbase $DatabaseName" -level verbose
+            if ($PSCmdlet.ShouldProcess($instance, "Creating the database on $instance")) 
+            {
+                try
+                {
+                    $NewDb.Create()
+                }
+                catch
+                {
+                    Stop-Function -Message "Error creating Database $DatabaseName on server $SqlInstance" -ErrorRecord $_ -Target $instance -Continue
+                }      
+
+
+                #now do post db creation work, set the dbowner and set the default filegroup
+                #Set the owner
+                Write-Message -message "Setting database owner to $DatabaseOwner" -level verbose
+                try
+                {
+                    $NewDB.SetOwner($DatabaseOwner)
+                }
+                catch
+                {
+                    Stop-Function -Message "Error setting Database Owner to $DatabaseOwner" -ErrorRecord $_ -Target $instance -Continue
+                }
+
+                #set the user filegroup to be the default
+                write-message -message "Setting default filegroup to $UserFileGroupName" -level verbose
+                try
+                {
+                    $NewDB.SetDefaultFileGroup($UserFileGroupName)
+                }
+                catch
+                {
+                    Stop-Function -Message "Error setting default filegorup to $UserFileGroupName" -ErrorRecord $_ -Target $instance -Continue
+                }
+
+            }
+
+            #Write completed message
+            Write-Message -message "Completed creating database $DatabaseName on server $sqlInstance" -level Verbose
         }
-
-        #Write completed message
-        Write-Message -message "Completed creating database $DatabaseName on server $sqlInstance" -level Verbose
 
     }
 }
