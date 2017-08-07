cd24b8110e49abbc5ccf96a39e189c17b6369d37
potatoqualitee <clemaire@gmail.com>
2017-08-08 00:16:14 +0200

    renamed and added test warning (do snapshot)

--- tests/Remove-DbaDatabaseSnapshot.Tests.ps1
+++ tests/Remove-DbaDatabaseSnapshot.Tests.ps1
@@ -2,6 +2,21 @@
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+if ($env:appveyor) {
+	Write-Warning "detected appveyor"
+	Get-Service | Where-Object { $_.DisplayName -match 'SQL Server (SQL2008R2SP2)' -or $_.DisplayName -match 'SQL Server (SQL2016)' } | Restart-Service -Force
+	do {
+		Start-Sleep 1
+		$null = (& sqlcmd -S $script:instance1 -b -Q "select 1" -d master)
+	}
+	while ($lastexitcode -ne 0 -and $t++ -lt 10)
+	
+	do {
+		Start-Sleep 1
+		$null = (& sqlcmd -S $script:instance2 -b -Q "select 1" -d master)
+	}
+	while ($lastexitcode -ne 0 -and $s++ -lt 10)
+}
 
 # Targets only instance2 because it's the only one where Snapshots can happen
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
