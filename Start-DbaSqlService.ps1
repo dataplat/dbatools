eaa037d4fef007997754740a00857c049399dfad
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2017-08-08 16:08:16 -0500

    Fixing formatting issues in help

---
+++ Start-DbaSqlService.ps1
@@ -0,0 +1,124 @@
+Function Start-DbaSqlService {
+<#
+		.SYNOPSIS
+		Starts SQL Server services on a computer. 
+		
+		.DESCRIPTION
+		Starts the SQL Server related services on one or more computers. Will follow SQL Server service dependencies.
+		
+		Requires Local Admin rights on destination computer(s).
+		
+		.PARAMETER ComputerName
+		The SQL Server (or server in general) that you're connecting to. This command handles named instances.
+		
+		.PARAMETER InstanceName
+		Only affects services that belong to the specific instances.
+		
+		.PARAMETER Credential
+		Credential object used to connect to the computer as a different user.
+		
+		.PARAMETER Type
+		Use -Type to collect only services of the desired SqlServiceType.
+		Can be one of the following: "Agent","Browser","Engine","FullText","SSAS","SSIS","SSRS","AnalysisServer",
+			"ReportServer","Search","SqlAgent","SqlBrowser","SqlServer","SqlServerIntegrationService"
+		
+		.PARAMETER Timeout
+		How long to wait for the start/stop request completion before moving on. Specify 0 to wait indefinitely.
+		
+		.PARAMETER ServiceCollection
+		A collection of services from Get-DbaSqlService
+		
+		.PARAMETER Silent
+		Use this switch to disable any kind of verbose messages
+		
+		.PARAMETER WhatIf
+		Shows what would happen if the cmdlet runs. The cmdlet is not run.
+		
+		.PARAMETER Confirm
+		Prompts you for confirmation before running the cmdlet.
+		
+		.NOTES
+		Author: Kirill Kravtsov( @nvarscar )
+		
+		dbatools PowerShell module (https://dbatools.io)
+		Copyright (C) 2017 Chrissy LeMaire
+		This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+		This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+		You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+		
+		.LINK
+		https://dbatools.io/Start-DbaSqlService
+		
+		.EXAMPLE
+		Start-DbaSqlService -ComputerName sqlserver2014a
+		
+		Starts the SQL Server related services on computer sqlserver2014a.
+		
+		.EXAMPLE   
+		'sql1','sql2','sql3'| Get-DbaSqlService | Start-DbaSqlService
+		
+		Gets the SQL Server related services on computers sql1, sql2 and sql3 and starts them.
+		
+		.EXAMPLE
+		Start-DbaSqlService -ComputerName sql1,sql2 -Instance MSSQLSERVER
+		
+		Starts the SQL Server services related to the default instance MSSQLSERVER on computers sql1 and sql2.
+		
+		.EXAMPLE
+		Start-DbaSqlService -ComputerName $MyServers -Type SSRS
+		
+		Starts the SQL Server related services of type "SSRS" (Reporting Services) on computers in the variable MyServers.
+
+#>
+	[CmdletBinding(DefaultParameterSetName = "Server", SupportsShouldProcess = $true)]
+	Param (
+		[Parameter(ParameterSetName = "Server", Position = 1)]
+		[Alias("cn", "host", "Server")]
+		[DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
+		[Alias("Instance")]
+		[string[]]$InstanceName,
+		[ValidateSet("Agent", "Browser", "Engine", "FullText", "SSAS", "SSIS", "SSRS", "AnalysisServer", "ReportServer", "Search", "SqlAgent", "SqlBrowser", "SqlServer", "SqlServerIntegrationService")]
+		[string[]]$Type,
+		[parameter(ValueFromPipeline = $true, Mandatory = $true, ParameterSetName = "Service")]
+		[object[]]$ServiceCollection,
+		[int]$Timeout = 30,
+		[PSCredential]$Credential,
+		[switch]$Silent
+	)
+	begin {
+		if ($Type) {
+			foreach ($i in 0 .. ($Type.Length - 1)) {
+				$Type[$i] = switch ($Type[$i]) {
+					"Agent" { "SqlAgent" }
+					"Browser" { "SqlBrowser" }
+					"Engine" { "SqlServer" }
+					"FullText" { "FullText Search" }
+					"SSAS" { "AnalysisServer" }
+					"SSIS" { "SqlServerIntegrationService" }
+					"SSRS" { "ReportServer" }
+					default { $_ }
+				}
+			}
+		}
+		$processArray = @()
+		if ($PsCmdlet.ParameterSetName -eq "Server") {
+			$parameters = @{ }
+			if ($ComputerName) { $parameters.ComputerName = $ComputerName }
+			if ($InstanceName) { $parameters.InstanceName = $InstanceName }
+			if ($Type) { $parameters.Type = $Type }
+			if ($Credential) { $parameters.Credential = $Credential }
+			$serviceCollection = Get-DbaSqlService @parameters
+		}
+	}
+	process {
+		#Get all the objects from the pipeline before proceeding
+		$processArray += $serviceCollection
+	}
+	end {
+		$processArray = $processArray | Where-Object { (!$InstanceName -or $_.InstanceName -in $InstanceName) -and (!$Type -or $_.ServiceType -in $Type) }
+		if ($processArray) {
+			Update-DbaSqlServiceStatus -ServiceCollection $processArray -Action 'start' -Timeout $Timeout -Silent $Silent
+		}
+		else { Write-Message -Level Warning -Silent $Silent -Message "No SQL Server services found with current parameters." }
+	}
+}
