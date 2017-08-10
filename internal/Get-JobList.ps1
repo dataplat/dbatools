b7bd1abef4e0f4441a820d4dd5ffced7fcd4d1f9
Shawn Melton <wshawnmelton@gmail.com>
2017-08-09 21:22:45 -0500

    Working on additional filter

--- internal/Get-JobList.ps1
+++ internal/Get-JobList.ps1
@@ -8,8 +8,10 @@ function Get-JobList {
 		SQL Server instance
 	.PARAMETER SqlCredential
 		Credential to use if SqlInstance did not include it.
-	.PARAMETER Filter
+	.PARAMETER JobFilter
 		Object of jobs to filter on, also supports wildcard patterns
+	.PARAMETER StepFilter
+		Object of job steps to filter on, also supports wildcard patterns
 	.PARAMETER Not
 		Reverse results where object returned excludes filtered content.
 	.PARAMETER Silent
@@ -19,27 +21,27 @@ function Get-JobList {
 
 		Returns the full JobServer.Jobs object found on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance sql2016 -Filter '*job*'
+		Get-JobList -SqlInstance sql2016 -JobFilter '*job*'
 
 		Returns the Job object for each job name found to have "job" in the name on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance sql2016 -Filter '*job*' -Not
+		Get-JobList -SqlInstance sql2016 -JobFilter '*job*' -Not
 
 		Returns any Job object that does not have "job" in the name on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance YourServer -Filter 'JobName'
+		Get-JobList -SqlInstance YourServer -JobFilter 'JobName'
 
 		Returns the Job object where the job name is 'JobName' on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance YourServer -Filter 'JobName' -Not
+		Get-JobList -SqlInstance YourServer -JobFilter 'JobName' -Not
 
 		Returns any Job object where the job name is not 'JobName' on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance YourServer -Filter job_3_upload, job_3_download
+		Get-JobList -SqlInstance YourServer -JobFilter job_3_upload, job_3_download
 
 		Returns the Job object for where job is job_3_upload or job_3_download on sql2016
 	.EXAMPLE
-		Get-JobList -SqlInstance YourServer -Filter job_3_upload, job_3_download -Not
+		Get-JobList -SqlInstance YourServer -JobFilter job_3_upload, job_3_download -Not
 
 		Returns any Job object where job is not job_3_upload or job_3_download on sql2016
 	.NOTES
@@ -54,7 +56,8 @@ function Get-JobList {
 		[Parameter(ValueFromPipeline = $true)]
 		[DbaInstanceParameter]$SqlInstance,
 		[PSCredential]$SqlCredential,
-		[string[]]$Filter,
+		[string[]]$JobFilter,
+		[string[]]$StepFilter,
 		[switch]$Not,
 		[switch]$Silent
 	)
@@ -62,31 +65,55 @@ function Get-JobList {
 		$server= Connect-SqlInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
 
 		$jobs = $server.JobServer.Jobs
-		if (Test-Bound 'Filter') {
-			if ($Filter.Count -gt 1) {
+		if ( (Test-Bound 'JobFilter') -or (Test-Bound 'StepFilter') ) {
+			if ($JobFilter.Count -gt 1) {
 				if ($Not) {
-					$jobs | Where-Object Name -NotIn $Filter
+					$jobs | Where-Object Name -NotIn $JobFilter
 				}
 				else {
-					$jobs | Where-Object Name -In $Filter
+					$jobs | Where-Object Name -In $JobFilter
 				}
 			}
 			else {
 				foreach ($job in $jobs) {
-					if ($Filter -match '`*') {
+					if ($JobFilter -match '`*') {
 						if ($Not) {
-							$job | Where-Object Name -NotLike $Filter
+							$job | Where-Object Name -NotLike $JobFilter
 						}
 						else {
-							$job | Where-Object Name -Like $Filter
+							$job | Where-Object Name -Like $JobFilter
 						}
 					}
 					else {
 						if ($Not) {
-							$job | Where-Object Name -ne $Filter
+							$job | Where-Object Name -NE $JobFilter
 						}
 						else {
-							$job | Where-Object Name -eq $Filter
+							$job | Where-Object Name -EQ $JobFilter
+						}
+					}
+					if ($StepFilter -match '`*') {
+						if ($Not) {
+							$job.JobSteps | Where-Object Name -NotLike $StepFilter
+						}
+						else {
+							$job.JobSteps | Where-Object Name -Like $StepFilter
+						}
+					}
+					elseif ($StepName.Count -gt 1) {
+						if ($Not) {
+							$job.JobSteps | Where-Object Name -NotIn $StepName
+						}
+						else {
+							$job.JobSteps | Where-Object Name -In $StepName
+						}
+					}
+					else {
+						if ($Not) {
+							$job.JobSteps | Where-Object Name -NE $StepName
+						}
+						else {
+							$job.JobSteps | Where-Object Name -EQ $StepName
 						}
 					}
 				}
