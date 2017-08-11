dd26e4588982b8d979afbb330c860d5ea05c3849
Simone Bizzotto <niphlod@gmail.com>
2017-08-11 13:27:30 +0200

    Get-DbaAgentJobOutputFile, add step id
    because others may need it

--- functions/Get-DbaAgentJobOutputFile.ps1
+++ functions/Get-DbaAgentJobOutputFile.ps1
@@ -22,9 +22,13 @@
 		.PARAMETER ExcludeJob
 			The job(s) to exclude - this list is auto-populated from the server
 
+		.PARAMETER Silent
+			Use this switch to disable any kind of verbose messages
+
 		.NOTES
 			Tags: Agent, Job
 			Author: Rob Sewell (https://sqldbawithabeard.com)
+			Editor: niphlod
 			
 			Website: https://dbatools.io
 			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
@@ -33,45 +37,45 @@
 		.EXAMPLE
 			Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME -Job 'The Agent Job' 
 
-			This will return the paths to the output files for each of the job step of the The Agent Job Job 
+			This will return the configured paths to the output files for each of the job step of the The Agent Job Job 
 			on the SERVERNAME instance  
 
 		.EXAMPLE
 			Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME 
 
-			This will return the paths to the output files for each of the job step of all the Agent Jobs
+			This will return the configured paths to the output files for each of the job step of all the Agent Jobs
 			on the SERVERNAME instance   
 
 		.EXAMPLE
 			Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME,SERVERNAME2 -Job 'The Agent Job'
 
-			This will return the paths to the output files for each of the job step of the The Agent Job Job 
+			This will return the configured paths to the output files for each of the job step of the The Agent Job Job 
 			on the SERVERNAME instance and SERVERNAME2
 
 		.EXAMPLE
 			$Servers = 'SERVER','SERVER\INSTANCE1'
 			Get-DbaAgentJobOutputFile -SqlInstance $Servers -Job 'The Agent Job' -OpenFile 
 
-			This will return the paths to the output files for each of the job step of the The Agent Job Job 
+			This will return the configured paths to the output files for each of the job step of the The Agent Job Job 
 			on the SERVER instance and the SERVER\INSTANCE1 and open the files if they are available
 
 		.EXAMPLE 
 			Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME  | Out-GridView
 
-			This will return the paths to the output files for each of the job step of all the Agent Jobs
+			This will return the configured paths to the output files for each of the job step of all the Agent Jobs
 			on the SERVERNAME instance and Pipe them to Out-GridView
 
 		.EXAMPLE 
 			(Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME | ogv -PassThru).FileName | Invoke-Item
 
-			This will return the paths to the output files for each of the job step of all the Agent Jobs
+			This will return the configured paths to the output files for each of the job step of all the Agent Jobs
 			on the SERVERNAME instance and Pipe them to Out-GridView and enable you to choose the output
 			file and open it
 		
 		.EXAMPLE 
 			Get-DbaAgentJobOutputFile -SqlInstance SERVERNAME -Verbose
 
-			This will return the paths to the output files for each of the job step of all the Agent Jobs
+			This will return the configured paths to the output files for each of the job step of all the Agent Jobs
 			on the SERVERNAME instance and also show the job steps without an output file
 	#>
     [CmdletBinding()]
@@ -92,26 +96,27 @@
             Position = 1)]
         [PSCredential]$SqlCredential,
         [object[]]$Job,
-		[object[]]$ExcludeJob
+        [object[]]$ExcludeJob,
+        [switch]$Silent
     )
 
     process {
         foreach ($instance in $sqlinstance) {
+            Write-Message -Message "Attempting to connect to $instance" -Level Verbose
             try {
-                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
+                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
             }
             catch {
-                Write-Warning "Failed to connect to: $instance"
-                continue
+                Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
-			
+
             $jobs = $Server.JobServer.Jobs
             if ($Job) {
                 $jobs = $jobs | Where-Object Name -In $Job
             }
             if ($ExcludeJob) {
-				$jobs = $jobs | Where-Object Name -NotIn $ExcludeJob
-			}
+                $jobs = $jobs | Where-Object Name -NotIn $ExcludeJob
+            }
             foreach ($j in $Jobs) {
                 foreach ($Step in $j.JobSteps) {
                     if ($Step.OutputFileName) {
@@ -120,13 +125,14 @@
                             InstanceName         = $server.ServiceName
                             SqlInstance          = $server.DomainInstanceName
                             Job                  = $j.Name
-                            JobStep              = $step.Name
+                            JobStep              = $Step.Name
                             OutputFileName       = $Step.OutputFileName
                             RemoteOutputFileName = Join-AdminUNC $Server.ComputerNamePhysicalNetBIOS $Step.OutputFileName
-                        }
+                            StepId               = $Step.Id
+                        } | Select-DefaultView -ExcludeProperty StepId
                     }
                     else {
-                        Write-Verbose "$step for $j has no output file"
+                        Write-Message -Level Verbose -Message "$step for $j has no output file"
                     }
                 }
             }
