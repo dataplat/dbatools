f6edd7608fd4da80fd4b1ee30013735af1dbe39d
Shawn Melton <wshawnmelton@gmail.com>
2017-08-14 03:06:56 +0000

    New Get-DbaHadr Returns whether IsHadrEnabled is true or false for SQL Server service

---
+++ tests/Get-DbaHadr.Tests.ps1
@@ -0,0 +1,30 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+Describe "Get-DbaHadr Unit Tests" -Tag "UnitTests" {
+	Context "Validate parameters" {
+		$paramCount = 3
+		$defaultParamCount = 13
+		[object[]]$params = (Get-ChildItem function:\Get-DbaHadr).Parameters.Keys
+		$knownParameters = 'SqlInstance', 'SqlCredential', 'AllowException'
+		it "Should contian our parameters" {
+			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
+		}
+		it "Should only contain our parameters" {
+			$params.Count - $defaultParamCount | Should Be $paramCount
+		}
+	}
+}
+Describe "Get-DbaHadr Integration Test" -Tag "IntegrationTests" {
+	$results = Get-DbaHadr -SqlInstance $script:instance2
+
+	Context "Validate output" {
+		it "Should have correct properties" {
+			$ExpectedProps = 'ComputerName,InstanceName,SqlInstance,IsHadrEnabled'.Split(',')
+			($results.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
+		}
+		it "Should return false" {
+			$results.IsHadrEnabled | Should Be $false
+		}
+	}
+}
