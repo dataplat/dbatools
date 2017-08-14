355d8f637b76605eff990c8967425e180662b7ec
Shawn Melton <wshawnmelton@gmail.com>
2017-08-14 18:40:42 -0500

    adjust input testing agreplica command

--- tests/Get-DbaAgReplica.Tests.ps1
+++ tests/Get-DbaAgReplica.Tests.ps1
@@ -3,24 +3,20 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Unit Tests" -Tag 'UnitTests' {
-	InModuleScope dbatools {
-		Context "Validate parameters" {
-			$params = (Get-ChildItem function:\Get-DbaAgReplica).Parameters	
-			it "should have a parameter named SqlInstance" {
-				$params.ContainsKey("SqlInstance") | Should Be $true
-			}
-			it "should have a parameter named Credential" {
-				$params.ContainsKey("SqlCredential") | Should Be $true
-			}
-			it "should have a parameter named Silent" {
-				$params.ContainsKey("Silent") | Should Be $true
-			}
+	Context "Validate parameters" {
+		$paramCount = 5
+		<# 
+			Get commands, Default count = 11
+			Commands with SupportShouldProcess = 13
+		#>
+		$defaultParamCount = 11
+		[object[]]$params = (Get-ChildItem function:\Get-DbaAgReplica).Parameters.Keys
+		$knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Replica', 'Silent'
+		it "Should contian our parameters" {
+			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
 		}
-		Context "Validate input" {
-			it "Should throw message if SqlInstance is not accessible" {
-				mock Resolve-DbaNetworkName {$null}
-				{Get-DbaAgReplica -SqlInstance 'DoesNotExist142' -WarningAction Stop 3> $null} | Should Throw
-			}
+		it "Should only contain our parameters" {
+			$params.Count - $defaultParamCount | Should Be $paramCount
 		}
 	}
 }
