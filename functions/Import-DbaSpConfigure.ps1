2450a03bb6e50908477e23dfd0f87b393eb676a8
Andy Levy <andy@levyclan.us>
2017-08-21 21:36:19 -0400

    Help content, style & formatting

--- functions/Import-DbaSpConfigure.ps1
+++ functions/Import-DbaSpConfigure.ps1
@@ -1,118 +1,106 @@
-Function Import-DbaSpConfigure
-{
- <#
-.SYNOPSIS
- Updates sp_configure settings on destination server.
+function Import-DbaSpConfigure {
+	<#
+		.SYNOPSIS
+			Updates sp_configure settings on destination server.
 
-.DESCRIPTION
-Updates sp_configure settings on destination server.
+		.DESCRIPTION
+			Updates sp_configure settings on destination server.
 
-.PARAMETER Source
-Source SQL Server.You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+		.PARAMETER Source
+			Source SQL Server. You must have sysadmin access and server version must be SQL Server version 2000 or higher.
 
-.PARAMETER Destination
-Destination SQL Server. You must have sysadmin access and server version must be SQL Server version 2000 or greater.
+		.PARAMETER SourceSqlCredential
+			Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
 
-.PARAMETER SourceSqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+			$scred = Get-Credential, then pass $scred object to the -SourceSqlCredential parameter.
 
-$scred = Get-Credential, this pass $scred object to the param. 
+			Windows Authentication will be used if SourceSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
 
-Windows Authentication will be used if DestinationSqlCredential is not specified. To connect as a different Windows user, run PowerShell as that user.	
+			To connect as a different Windows user, run PowerShell as that user.
 
-.PARAMETER SqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+		.PARAMETER Destination
+			Destination SQL Server. You must have sysadmin access and the server must be SQL Server 2000 or higher.
 
-$scred = Get-Credential, this pass $scred object to the param. 
+		.PARAMETER DestinationSqlCredential
+			Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
 
-Windows Authentication will be used if DestinationSqlCredential is not specified. To connect as a different Windows user, run PowerShell as that user.	
+			$dcred = Get-Credential, then pass this $dcred to the -DestinationSqlCredential parameter.
 
-.PARAMETER DestinationSqlCredential
-Allows you to login to servers using SQL Logins as opposed to Windows Auth/Integrated/Trusted. To use:
+			Windows Authentication will be used if DestinationSqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
 
-$dcred = Get-Credential, this pass this $dcred to the param. 
+			To connect as a different Windows user, run PowerShell as that user.
 
-Windows Authentication will be used if DestinationSqlCredential is not specified. To connect as a different Windows user, run PowerShell as that user.	
+		.PARAMETER Path
+			Specifies the path to a SQL script file holding sp_configure queries for each of the settings to be changed.
 
-.PARAMETER SqlInstance
-The SQL Server that you're connecting to.
+		.PARAMETER Force
+			If this switch is enabled, no version check between Source and Destination is performed. By default, the major and minor versions of Source and Destination must match when copying sp_configure settings.
 
-.PARAMETER Path
-The Path to the SQL File
+		.PARAMETER WhatIf
+			If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
-.PARAMETER Force
-Overrides Major Version Check
+		.PARAMETER Confirm
+			If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
 
-.PARAMETER WhatIf 
-Shows what would happen if the command were to run. No actions are actually performed. 
+		.EXAMPLE
+			Import-DbaSpConfigure sqlserver sqlcluster $SourceSqlCredential $DestinationSqlCredential
 
-.PARAMETER Confirm 
-Prompts you for confirmation before executing any changing operations within the command. 
+			Imports the sp_configure settings from the source server sqlserver and sets them on the sqlcluster server
+			using the SQL credentials stored in the variables
 
-.EXAMPLE
-Import-DbaSpConfigure sqlserver sqlcluster $SourceSqlCredential $DestinationSqlCredential
+		.EXAMPLE
+			Import-DbaSpConfigure -SqlInstance sqlserver -Path .\spconfig.sql -SqlCredential $SqlCredential
 
-Imports the spconfigure settings from the source server sqlserver and sets them on the sqlcluster server
-using the SQL credentials stored in the variables
+			Imports the sp_configure settings from the file .\spconfig.sql and sets them on the sqlcluster server
+			using the SQL credential stored in the variables
 
-.EXAMPLE
-Import-DbaSpConfigure -SqlInstance sqlserver -Path .\spconfig.sql -SqlCredential $SqlCredential
+		.OUTPUTS
+			$true if success
+			$false if failure
 
-Imports the spconfigure settings from the file .\spconfig.sql and sets them on the sqlcluster server
-using the SQL credential stored in the variables
-
-.OUTPUTS
-    $true if success
-    $false if failure
-
-#>
+	#>
 	[CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
 	param (
 		[DbaInstanceParameter]$Source,
 		[DbaInstanceParameter]$Destination,
 		[PSCredential]$SourceSqlCredential,
 		[PSCredential]$DestinationSqlCredential,
-		[Alias("ServerInstance","SqlServer")]
+		[Alias("ServerInstance", "SqlServer")]
 		[DbaInstanceParameter]$SqlInstance,
 		[string]$Path,
 		[PSCredential]$SqlCredential,
 		[switch]$Force
 		
 	)
-	BEGIN {
+	begin {
 	
-		if ($Path.length -eq 0)
-		{
+		if ($Path.length -eq 0) {
 			$sourceserver = Connect-SqlInstance -SqlInstance $Source -SqlCredential $SourceSqlCredential
 			$destserver = Connect-SqlInstance -SqlInstance $Destination -SqlCredential $DestinationSqlCredential
 			
 			$source = $sourceserver.DomainInstanceName
 			$destination = $destserver.DomainInstanceName
-		} else {
+		}
+		else {
 			$server = Connect-SqlInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
-			if ((Test-Path $Path) -eq $false) { throw "File Not Found" }
+			if ((Test-Path $Path) -eq $false) {
+				throw "File Not Found"
+			}
 		}
 	
 	}
-	PROCESS
-	{
-		
-		if ($Path.length -eq 0)
-		{
-			
-			If ($Pscmdlet.ShouldProcess($destination, "Export sp_configure"))
-			{
+	process {
+		if ($Path.length -eq 0) {
+			if ($Pscmdlet.ShouldProcess($destination, "Export sp_configure")) {
 				$sqlfilename = Export-SqlSpConfigure $sourceserver
 			}
 			
-			if ($sourceserver.versionMajor -ne $destserver.versionMajor -and $force -eq $false)
-			{
+			if ($sourceserver.versionMajor -ne $destserver.versionMajor -and $force -eq $false) {
 				Write-Warning "Source SQL Server major version and Destination SQL Server major version must match for sp_configure migration. Use -Force to override this precaution or check the exported sql file, $sqlfilename, and run manually."
 				return
 			}
 			
-			If ($Pscmdlet.ShouldProcess($destination, "Execute sp_configure"))
-			{
+			If ($Pscmdlet.ShouldProcess($destination, "Execute sp_configure")) {
 				$sourceserver.Configuration.ShowAdvancedOptions.ConfigValue = $true
 				$sourceserver.Query("RECONFIGURE WITH OVERRIDE") | Out-Null
 				$destserver.Configuration.ShowAdvancedOptions.ConfigValue = $true
@@ -120,57 +108,56 @@ using the SQL credential stored in the variables
 				
 				$destprops = $destserver.Configuration.Properties
 				
-				foreach ($sourceprop in $sourceserver.Configuration.Properties)
-				{
+				foreach ($sourceprop in $sourceserver.Configuration.Properties) {
 					$displayname = $sourceprop.DisplayName
 					
-					$destprop = $destprops | where-object{ $_.Displayname -eq $displayname }
-					if ($destprop -ne $null)
-					{
-						try
-						{
+					$destprop = $destprops | where-object { $_.Displayname -eq $displayname }
+					if ($destprop -ne $null) {
+						try {
 							$destprop.configvalue = $sourceprop.configvalue
 							$destserver.Query("RECONFIGURE WITH OVERRIDE") | Out-Null
-							Write-Output "updated $($destprop.displayname) to $($sourceprop.configvalue)"
+							Write-Output "updated $($destprop.displayname) to $($sourceprop.configvalue)."
 						}
-						catch { Write-Error "Could not $($destprop.displayname) to $($sourceprop.configvalue). Feature may not be supported." }
+						catch {
+							Write-Error "Could not $($destprop.displayname) to $($sourceprop.configvalue). Feature may not be supported."
+						}
 					}
 				}
-				try { $destserver.Configuration.Alter() }
-				catch { $needsrestart = $true }
+				try {
+					$destserver.Configuration.Alter()
+				}
+				catch {
+					$needsrestart = $true
+				}
 				
 				$sourceserver.Configuration.ShowAdvancedOptions.ConfigValue = $false
 				$sourceserver.Query("RECONFIGURE WITH OVERRIDE") | Out-Null
 				$destserver.Configuration.ShowAdvancedOptions.ConfigValue = $false
 				$destserver.Query("RECONFIGURE WITH OVERRIDE") | Out-Null
 				
-				if ($needsrestart -eq $true)
-				{
+				if ($needsrestart -eq $true) {
 					Write-Warning "Some configuration options will be updated once SQL Server is restarted."
 				}
-				else { Write-Output "Configuration option has been updated." }
+				else {
+					Write-Output "Configuration option has been updated."
+				}
 			}
 			
-			If ($Pscmdlet.ShouldProcess($destination, "Removing temp file"))
-			{
+			if ($Pscmdlet.ShouldProcess($destination, "Removing temp file")) {
 				Remove-Item $sqlfilename -ErrorAction SilentlyContinue
 			}
 			
 		}
-		else
-		{
-			If ($Pscmdlet.ShouldProcess($destination, "Importing sp_configure from $Path"))
-			{	
+		else {
+			if ($Pscmdlet.ShouldProcess($destination, "Importing sp_configure from $Path")) {	
 				$server.Configuration.ShowAdvancedOptions.ConfigValue = $true
 				$sql = Get-Content $Path
-				foreach ($line in $sql)
-				{
-					try
-					{
-						$server.Query($line) | Out-Null; Write-Output "Successfully executed $line"
+				foreach ($line in $sql) {
+					try {
+						$server.Query($line) | Out-Null
+						Write-Output "Successfully executed $line."
 					}
-					catch
-					{
+					catch {
 						Write-Error "$line failed. Feature may not be supported."
 					}
 				}
@@ -179,18 +166,17 @@ using the SQL credential stored in the variables
 			}
 		}
 	}
-	END
-	{
+	end {
 		if ($Path.length -gt 0) { 
 			$server.ConnectionContext.Disconnect() 
-		} else {
+		}
+		else {
 			$sourceserver.ConnectionContext.Disconnect() 
 			$destserver.ConnectionContext.Disconnect() 
 		}
 	
-		If ($Pscmdlet.ShouldProcess("console", "Showing finished message"))
-		{
-			Write-Output "SQL Server configuration options migration finished"
+		If ($Pscmdlet.ShouldProcess("console", "Showing finished message")) {
+			Write-Output "SQL Server configuration options migration finished."
 		}
 		
 		Test-DbaDeprecation -DeprecatedOn "1.0.0" -Silent:$false -Alias Import-SqlSpConfigure
