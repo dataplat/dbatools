354c656802194039c8e186d25e2b1d1764429f18
Justin Dearing <zippy1981@gmail.com>
2017-08-31 09:06:13 -0400

    Added unit tests for the IP address and string constructor

---
+++ bin/projects/dbatools/dbatools.Tests/Parameter/DbaInstanceParamaterTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sqlcollaborative.Dbatools.Parameter
+{
+    [TestClass]
+    public class DbaInstanceParamaterTest
+    {
+        [TestMethod]
+        public void TestStringConstructor()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter("someMachine");
+
+            Assert.AreEqual("someMachine", dbaInstanceParamater.FullName);
+            Assert.IsFalse(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that localhost\instancename is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestLocalhostNamedInstance()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter("localhost\\sql2008r2sp2");
+
+            Assert.AreEqual("localhost\\sql2008r2sp2", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that [localhost\instancename] is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestBracketedName()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter("[computerName]");
+
+            Assert.AreEqual("computerName", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that [localhost\instancename] is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestBracketedNameWithInstance()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter("[computerName\\sql2008r2sp2]");
+
+            Assert.AreEqual("computerName\\sql2008r2sp2", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that . is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestDotHostname()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter(".");
+
+            Assert.AreEqual(".", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that 127.0.0.1 is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestLocalhostIPConstructor()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter(IPAddress.Loopback);
+
+            Assert.AreEqual("127.0.0.1", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+
+        /// <summary>
+        /// Checks that . is treated as a localhost connection
+        /// </summary>
+        [TestMethod]
+        public void TestLocalhostIPv6Constructor()
+        {
+            var dbaInstanceParamater = new DbaInstanceParameter(IPAddress.IPv6Loopback);
+
+            Assert.AreEqual("::1", dbaInstanceParamater.FullName);
+            Assert.IsTrue(dbaInstanceParamater.IsLocalHost);
+        }
+    }
+}
