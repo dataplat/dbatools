f70ebee643c9d231dfa46ed9b2f38c394ce2362c
gbargsley <gbargsley@gmail.com>
2017-09-19 19:39:43 -0500

    Initial Commit Get-DbaSuspectPages

---
+++ functions/Get-DbaSuspectPages.ps1
@@ -0,0 +1,88 @@
+function Get-DbaSuspectPages {
+	<#
+.SYNOPSIS
+Returns data that is stored in SQL for Suspect Pages on the specied SQL Server Instance
+
+.DESCRIPTION
+This function returns any records that were stored due to suspect pages in databases on a SQL Server Instance.
+
+.PARAMETER SqlInstance
+A SQL Server instance to connect to
+
+.PARAMETER SqlCredential
+A credeial to use to conect to the SQL Instance rather than using Windows Authentication
+
+.PARAMETER Silent
+Use this switch to disable any kind of verbose messages
+	
+.NOTES
+Tags: Pages, DBCC
+Website: https://dbatools.io
+Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+.EXAMPLE
+Get-DbaSuspectPages -SqlInstance sql2016 
+
+Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
+
+#>
+	[CmdletBinding()]
+	Param (
+		[parameter(Position = 0, Mandatory, ValueFromPipeline)]
+		[Alias("ServerInstance", "SqlServer")]
+		[DbaInstanceParameter[]]$SqlInstance,
+        [PSCredential]$SqlCredential,
+		[switch]$Silent
+	)
+	
+	process {
+		
+		foreach ($instance in $sqlinstance) {
+			try {
+				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 9
+			}
+			catch {
+				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+				return
+			}
+			
+			$sql = "Select
+			DB_NAME(database_id) as DBName, 
+			file_id, 
+			page_id, 
+			CASE event_type 
+			WHEN 1 THEN '823 or 824 or Torn Page'
+			WHEN 2 THEN 'Bad Checksum'
+			WHEN 3 THEN 'Torn Page'
+			WHEN 4 THEN 'Restored'
+			WHEN 5 THEN 'Repaired (DBCC)'
+			WHEN 7 THEN 'Deallocated (DBCC)'
+			END as EventType, 
+			error_count, 
+			last_update_date
+			from msdb.dbo.suspect_pages"
+				
+			try {
+				$server.Query($sql) 
+			}
+			catch {
+				Stop-Function -Message "Error returned from SQL Server: $_" -Target $server -InnerErrorRecord $_
+			}
+
+			foreach ($row in $server.Query($sql)) {
+                [PSCustomObject]@{
+                  ComputerName   = $server.NetName
+                  InstanceName   = $server.ServiceName
+                  SqlInstance    = $server.DomainInstanceName
+                  DBName         = $row.DBName
+                  FileId         = $row.file_id
+                  PageId         = $row.page_id
+                  EventType      = $row.EventType
+                  ErrorCount     = $row.error_count
+                  LastUpdateDate = $row.last_update_date
+				} | Select-DefaultView 
+			}
+		}   
+    }
+}
