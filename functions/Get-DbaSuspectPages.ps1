61b4dbb66c32d995ac2f7e5275c6b0ce115979d1
gbargsley <gbargsley@gmail.com>
2017-09-19 20:05:57 -0500

    Added database parameter

--- functions/Get-DbaSuspectPages.ps1
+++ functions/Get-DbaSuspectPages.ps1
@@ -10,13 +10,18 @@ This function returns any records that were stored due to suspect pages in datab
 A SQL Server instance to connect to
 
 .PARAMETER SqlCredential
-A credeial to use to conect to the SQL Instance rather than using Windows Authentication
+A credential to use to conect to the SQL Instance rather than using Windows Authentication
+
+.PARAMETER Database
+The database to return. If unspecified, all records will be returned.
 
 .PARAMETER Silent
 Use this switch to disable any kind of verbose messages
 	
 .NOTES
 Tags: Pages, DBCC
+Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
+
 Website: https://dbatools.io
 Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
@@ -32,6 +37,7 @@ Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
 		[parameter(Position = 0, Mandatory, ValueFromPipeline)]
 		[Alias("ServerInstance", "SqlServer")]
 		[DbaInstanceParameter[]]$SqlInstance,
+		[object[]]$Database,
         [PSCredential]$SqlCredential,
 		[switch]$Silent
 	)
@@ -67,7 +73,11 @@ Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
 				$server.Query($sql) 
 			}
 			catch {
-				Stop-Function -Message "Error returned from SQL Server: $_" -Target $server -InnerErrorRecord $_
+				Stop-Function -Message "Issue collecting data on $server" -Target $server -ErrorRecord $_
+			}
+			
+			if ($Database) {
+				$results = $results | Where-Object { $_.Name -EQ $Database }
 			}
 
 			foreach ($row in $server.Query($sql)) {
