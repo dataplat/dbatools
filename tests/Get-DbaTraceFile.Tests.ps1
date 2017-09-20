867faff593ff906b2146f5ca322c7b4c370e1112
potatoqualitee <clemaire@gmail.com>
2017-09-20 17:51:26 +0200

    updated test

--- tests/Get-DbaTraceFile.Tests.ps1
+++ tests/Get-DbaTraceFile.Tests.ps1
@@ -3,11 +3,35 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-    Context "Test Check Default Trace" {
-
-        $results = Get-DbaTraceFile -SqlInstance $script:instance2 
+	BeforeAll {
+		$traceconfig = Get-DbaSpConfigure -SqlInstance $script:instance2 -ConfigName DefaultTraceEnabled
+		
+		if ($traceconfig.RunningValue -eq $false) {
+			$server = Connect-DbaSqlServer -SqlInstance $script:instance2
+			$server.Query("EXEC sp_configure 'show advanced options', 1;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")
+			$server.Query("EXEC sp_configure 'default trace enabled', 1;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")
+			$server.Query("EXEC sp_configure 'show advanced options', 0;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")		
+		}
+	}
+	
+	AfterAll {
+		if ($traceconfig.RunningValue -eq $false) {
+			$server.Query("EXEC sp_configure 'show advanced options', 1;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")
+			$server.Query("EXEC sp_configure 'default trace enabled', 0;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")
+			$server.Query("EXEC sp_configure 'show advanced options', 0;")
+			$server.Query("RECONFIGURE WITH OVERRIDE")
+			#$null = Set-DbaSpConfigure -SqlInstance $script:instance2 -ConfigName DefaultTraceEnabled -Value $false
+		}
+	}
+	Context "Test Check Default Trace" {
+		$results = Get-DbaTraceFile -SqlInstance $script:instance2 
         It "Should find at least one trace file" {
-            ($results).Count -gt 0 | Should Be $true
+            $results.Id.Count -gt 0 | Should Be $true
         }
     }
 }
