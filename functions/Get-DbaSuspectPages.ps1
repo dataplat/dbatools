a2eac33f16d696d45633b36b5055a6ab71ff591c
Garry Bargsley <gbargsley@gmail.com>
2017-09-20 14:56:00 -0500

    First Commit

--- functions/Get-DbaSuspectPages.ps1
+++ functions/Get-DbaSuspectPages.ps1
@@ -1,35 +1,35 @@
 function Get-DbaSuspectPages {
 	<#
-.SYNOPSIS
-Returns data that is stored in SQL for Suspect Pages on the specied SQL Server Instance
+		.SYNOPSIS
+		Returns data that is stored in SQL for Suspect Pages on the specied SQL Server Instance
 
-.DESCRIPTION
-This function returns any records that were stored due to suspect pages in databases on a SQL Server Instance.
+		.DESCRIPTION
+		This function returns any records that were stored due to suspect pages in databases on a SQL Server Instance.
 
-.PARAMETER SqlInstance
-A SQL Server instance to connect to
+		.PARAMETER SqlInstance
+		A SQL Server instance to connect to
 
-.PARAMETER SqlCredential
-A credential to use to conect to the SQL Instance rather than using Windows Authentication
+		.PARAMETER SqlCredential
+		A credential to use to conect to the SQL Instance rather than using Windows Authentication
 
-.PARAMETER Database
-The database to return. If unspecified, all records will be returned.
+		.PARAMETER Database
+		The database to return. If unspecified, all records will be returned.
 
-.PARAMETER Silent
-Use this switch to disable any kind of verbose messages
-	
-.NOTES
-Tags: Pages, DBCC
-Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
+		.PARAMETER Silent
+		Use this switch to disable any kind of verbose messages
+			
+		.NOTES
+		Tags: Pages, DBCC
+		Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
 
-Website: https://dbatools.io
-Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+		Website: https://dbatools.io
+		Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
-.EXAMPLE
-Get-DbaSuspectPages -SqlInstance sql2016 
+		.EXAMPLE
+		Get-DbaSuspectPages -SqlInstance sql2016 
 
-Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
+		Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
 
 #>
 	[CmdletBinding()]
@@ -37,7 +37,7 @@ Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
 		[parameter(Position = 0, Mandatory, ValueFromPipeline)]
 		[Alias("ServerInstance", "SqlServer")]
 		[DbaInstanceParameter[]]$SqlInstance,
-		[object[]]$Database,
+		[object]$Database,
         [PSCredential]$SqlCredential,
 		[switch]$Silent
 	)
@@ -54,44 +54,44 @@ Retrieve any records stored for Suspect Pages on the sql2016 SQL Server.
 			}
 			
 			$sql = "Select
-			DB_NAME(database_id) as DBName, 
-			file_id, 
-			page_id, 
-			CASE event_type 
-			WHEN 1 THEN '823 or 824 or Torn Page'
-			WHEN 2 THEN 'Bad Checksum'
-			WHEN 3 THEN 'Torn Page'
-			WHEN 4 THEN 'Restored'
-			WHEN 5 THEN 'Repaired (DBCC)'
-			WHEN 7 THEN 'Deallocated (DBCC)'
-			END as EventType, 
-			error_count, 
-			last_update_date
-			from msdb.dbo.suspect_pages"
+					DB_NAME(database_id) as DBName, 
+					file_id, 
+					page_id, 
+					CASE event_type 
+					WHEN 1 THEN '823 or 824 or Torn Page'
+					WHEN 2 THEN 'Bad Checksum'
+					WHEN 3 THEN 'Torn Page'
+					WHEN 4 THEN 'Restored'
+					WHEN 5 THEN 'Repaired (DBCC)'
+					WHEN 7 THEN 'Deallocated (DBCC)'
+					END as EventType, 
+					error_count, 
+					last_update_date
+					from msdb.dbo.suspect_pages"
 				
 			try {
-				$server.Query($sql) 
+				$results = $server.Query($sql) 
 			}
 			catch {
 				Stop-Function -Message "Issue collecting data on $server" -Target $server -ErrorRecord $_
 			}
 			
 			if ($Database) {
-				$results = $results | Where-Object { $_.Name -EQ $Database }
+				$results = $results | Where-Object DBName -EQ $Database
 			}
 
-			foreach ($row in $server.Query($sql)) {
-                [PSCustomObject]@{
-                  ComputerName   = $server.NetName
-                  InstanceName   = $server.ServiceName
-                  SqlInstance    = $server.DomainInstanceName
-                  DBName         = $row.DBName
-                  FileId         = $row.file_id
-                  PageId         = $row.page_id
-                  EventType      = $row.EventType
-                  ErrorCount     = $row.error_count
-                  LastUpdateDate = $row.last_update_date
-				} | Select-DefaultView 
+		}
+		foreach ($row in $results) {
+			[PSCustomObject]@{
+			  ComputerName   = $server.NetName
+			  InstanceName   = $server.ServiceName
+			  SqlInstance    = $server.DomainInstanceName
+			  DBName         = $row.DBName
+			  FileId         = $row.file_id
+			  PageId         = $row.page_id
+			  EventType      = $row.EventType
+			  ErrorCount     = $row.error_count
+			  LastUpdateDate = $row.last_update_date
 			}
 		}   
     }
