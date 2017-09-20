7f9060b142c8f649ceb5dc522ca014d8b65f7e56
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2017-09-20 23:05:05 +0200

    changed back to Failure - instance name is in the exception

--- functions/Test-DbaNetworkLatency.ps1
+++ functions/Test-DbaNetworkLatency.ps1
@@ -85,7 +85,7 @@ function Test-DbaNetworkLatency {
 					$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
 				}
 				catch {
-					Stop-Function -Message "Failed to connect to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+					Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
 				}
 				
 				do {
