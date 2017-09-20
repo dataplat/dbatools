bc7c80aeeb0062b16e128cf06a00d0bbf2aefdd4
Garry Bargsley <gbargsley@gmail.com>
2017-09-20 07:44:22 -0500

    Hopefully uploading final version to correct place.

--- functions/Get-DbaTraceFile.ps1
+++ functions/Get-DbaTraceFile.ps1
@@ -12,7 +12,7 @@ A SQL Server instance to connect to
 .PARAMETER SqlCredential
 A credeial to use to conect to the SQL Instance rather than using Windows Authentication
 
-.PARAMETER IsDefault
+.PARAMETER Default
 Switch that will only return the information for the default system trace
 
 .PARAMETER Silent
@@ -20,6 +20,9 @@ Use this switch to disable any kind of verbose messages
 	
 .NOTES
 Tags: Security, Trace
+
+Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
+
 Website: https://dbatools.io
 Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
@@ -27,10 +30,10 @@ License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 .EXAMPLE
 Get-DbaTraceFile -SqlInstance sql2016 
 
-Lists the tracefiles on the sql2016 SQL Server.
+Lists all the tracefiles on the sql2016 SQL Server.
 	
 .EXAMPLE
-Get-DbaTraceFile -SqlInstance sql2016 -IsDefault
+Get-DbaTraceFile -SqlInstance sql2016 -Default
 
 Lists the default trace information on the sql2016 SQL Server. 
 	
@@ -41,7 +44,7 @@ Lists the default trace information on the sql2016 SQL Server.
 		[Alias("ServerInstance", "SqlServer")]
 		[DbaInstanceParameter[]]$SqlInstance,
         [PSCredential]$SqlCredential,
-        [switch]$IsDefault,
+        [switch]$Default,
 		[switch]$Silent
 	)
 	
@@ -56,24 +59,44 @@ Lists the default trace information on the sql2016 SQL Server.
 				return
 			}
             
-            if ($IsDefault) {
-                try {
-                   $sql = "Select * from sys.traces where is_default = 1"
-					$server.Query($sql) 
-                }
-                catch {
-					Stop-Function -Message "Error returned from SQL Server: $_" -Target $server -InnerErrorRecord $_
-                }
-            }
-            else {
-				try {
-                    $sql = "Select * from sys.traces"
-					$server.Query($sql)
-				}
-				catch {
-					Stop-Function -Message "Error returned from SQL Server: $_" -Target $server -InnerErrorRecord $_
-                }
-            }
+			$sql = "SELECT id, status, path, max_size, stop_time, max_files, is_rowset, is_rollover, is_shutdown, is_default, buffer_count, buffer_size, file_position, reader_spid, start_time, last_event_time, event_count, dropped_event_count FROM sys.traces"
+
+			try {
+				$results = $server.Query($sql)
 			}
-        }
+			catch {
+				Stop-Function -Message "Issue collecting trace data on $server" -Target $server -ErrorRecord $_
+			}
+			
+            if ($Default) {
+				$results = $results | Where-Object { $_.is_default }
+			}
+		}
+
+		foreach ($row in $results) {
+			[PSCustomObject]@{
+			  ComputerName  = $server.NetName
+			  InstanceName  = $server.ServiceName
+			  SqlInstance   = $server.DomainInstanceName
+			  Id      		= $row.id
+			  Status        = $row.status
+			  Path      	= $row.path
+			  MaxSize    	= $row.max_size
+			  StopTime      = $row.stop_time
+			  MaxFiles    	= $row.max_files
+			  IsRowset   	= $row.is_rowset
+			  IsRollover   	= $row.is_rollover
+			  IsShutdown 	= $row.is_shutdown
+			  IsDefault    	= $row.is_default
+			  BufferCount   = $row.buffer_count
+			  BufferSize    = $row.buffer_size
+			  FilePosition  = $row.file_position
+			  ReaderSpid    = $row.reader_spid
+			  StartTime    	= $row.start_time
+			  LastEventTime = $row.last_event_time
+			  EventCount    = $row.event_count
+			  DroppedEventCount = $row.dropped_event_count
+			} 
+		}
+	}
 }
