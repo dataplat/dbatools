5df35adc513b8086f65a93671d3df739232ff36f
Garry Bargsley <gbargsley@gmail.com>
2017-09-20 19:27:55 -0500

    Added a test

---
+++ tests/Get-DbaSuspectPages.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Testing if suspect pages are present" {
+		BeforeAll {
+			$db = Get-DbaDatabase -SqlInstance $script:instance2 -Database msdb
+            $db.Query("CREATE DATABASE dbatoolsci_suspectpages (id int identity)")
+            $dbId = Get-DbaDatabase -SqlInstance $script:instance2 -Database dbatoolsci_suspectpages | Select-Object ID
+			$db.Query("INSERT INTO msdb.dbo.suspect_pages VALUES($dbId,1,33,2,6,GETDATE())")
+		}
+		AfterAll {
+			$db.Query("DELETE FROM msdb.dbo.suspect_pages")
+		}
+		
+		$results = Get-DbaSuspectPages -SqlInstance $script:instance2
+		
+		It "function should find one record in suspect_pages table" {
+			$results.file_id.Count -eq 1 | Should Be $true
+		}
+	}
+}
