4e906928e9ac8d5c694267f2a85720e735361e91
Garry Bargsley <gbargsley@gmail.com>
2017-09-21 19:36:24 -0500

    Changed name and logic in beforeall

--- tests/Get-DbaSuspectPage.Tests.ps1
+++ tests/Get-DbaSuspectPage.Tests.ps1
@@ -5,15 +5,13 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	Context "Testing if suspect pages are present" {
 		BeforeAll {
-			$db = Get-DbaDatabase -SqlInstance $script:instance1 -Database msdb
-            $db.Query("CREATE DATABASE dbatoolsci_suspectpages (id int identity)")
 			$db.Query("INSERT INTO msdb.dbo.suspect_pages VALUES(1,1,33,2,6,GETDATE())")
 		}
 		AfterAll {
 			$db.Query("DELETE FROM msdb.dbo.suspect_pages")
 		}
 		
-		$results = Get-DbaSuspectPages -SqlInstance $script:instance1
+		$results = Get-DbaSuspectPage -SqlInstance $script:instance1
 		
 		It "function should find one record in suspect_pages table" {
 			$results.file_id.Count -eq 1 | Should Be $true
