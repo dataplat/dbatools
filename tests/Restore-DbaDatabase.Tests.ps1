bf6a5729d3598683177de0966d7abaa5f871a6a8
Stuart Moore <stuart@stuart-moore.com>
2017-09-27 08:32:23 +0100

    this is getting embarassing now, need my own appveyor setup

--- tests/Restore-DbaDatabase.Tests.ps1
+++ tests/Restore-DbaDatabase.Tests.ps1
@@ -332,8 +332,8 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             ($results.RestoreComplete -contains $false) | Should Be $false    
         }
         It "Should have no Warnining or errors" {
-            ($WarnVar -eq '') | Should Be $true
-            ($ErrVar -eq '') | Should be $true
+            ('' -eq $WarnVar) | Should Be $true
+            ('' -eq $ErrVar) | Should be $true
         }
     }
     Clear-DbaSqlConnectionPool
