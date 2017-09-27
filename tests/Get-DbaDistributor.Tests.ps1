66b04718a292fa784908b882c70bccd890be2572
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2017-09-27 12:49:59 +0200

    removed space

--- tests/Get-DbaDistributor.Tests.ps1
+++ tests/Get-DbaDistributor.Tests.ps1
@@ -3,7 +3,6 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-	
 	Context "ensuring accuracy of results" {
 		$results = Get-DbaDistributor -SqlInstance $script:instance1
 		It "accurately reports that the distributor is not installed" {
