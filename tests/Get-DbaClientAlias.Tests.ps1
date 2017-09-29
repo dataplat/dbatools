abfb80f5edc7c2cd7734dfe9747b07361739bee2
potatoqualitee <clemaire@gmail.com>
2017-09-29 22:11:09 +0200

    remove test alias

--- tests/Get-DbaClientAlias.Tests.ps1
+++ tests/Get-DbaClientAlias.Tests.ps1
@@ -3,9 +3,14 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	BeforeAll {
+		$newalias = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias -Verbose:$false
+	}
+	AfterAll {
+		$newalias | Remove-DbaClientAlias
+	}
 	
-	Context "adds the alias" {
-		$null = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias -Verbose:$false
+	Context "gets the alias" {
 		$results = Get-DbaClientAlias
 		It "returns accurate information" {
 			$results.AliasName -contains 'dbatoolscialias' | Should Be $true
