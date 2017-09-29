1f90dd1daee54e7a97b091fee2b74daa7cf3a225
potatoqualitee <clemaire@gmail.com>
2017-09-29 05:23:07 +0200

    added required tests - need to create remove command

---
+++ tests/New-DbaClientAlias.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	
+	Context "adds the alias" {
+		$results = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias-new
+		It "returns accurate information" {
+			$results.AliasName | Should Be dbatoolscialias-new, dbatoolscialias-new
+		}
+	}
+}
