1d90c4da061049251c14a6d8f1075885908b7f26
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-09-29 07:50:44 +0200

    Re-formatted and implemented pipeline properly

--- functions/Add-DbaComputerCertificate.ps1
+++ functions/Add-DbaComputerCertificate.ps1
@@ -54,87 +54,87 @@
       Adds the local C:\temp\cer.cer to the local computer's LocalMachine\My (Personal) certificate store.
 
 #>
-  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
-  param (
-    [Alias("ServerInstance", "SqlServer", "SqlInstance")]
-    [DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
-    [PSCredential]$Credential,
-    [securestring]$Password,
-    [parameter(ValueFromPipeline)]
-    [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-    [string]$Path,
-    [string]$Store = "LocalMachine",
-    [string]$Folder = "My",
-    [switch]$Silent
-  )
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+	param (
+		[Alias("ServerInstance", "SqlServer", "SqlInstance")][DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
+		[PSCredential]$Credential,
+		[securestring]$Password,
+		[parameter(ValueFromPipeline)][System.Security.Cryptography.X509Certificates.X509Certificate2[]]$Certificate,
+		[string]$Path,
+		[string]$Store = "LocalMachine",
+		[string]$Folder = "My",
+		[switch]$Silent
+	)
 	
-  begin {
+	begin {
 		
-    if ($Path) {
-      if (!(Test-Path -Path $Path)) {
-        Stop-Function -Message "Path ($Path) does not exist."
-        return
-      }
-
-      try {
-        # This may be too much, but ¯\_(ツ)_/¯
-        $bytes = [System.IO.File]::ReadAllBytes($Path)
-        $Certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-        $Certificate.Import($bytes, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
-      }
-      catch {
-        Stop-Function -Message "Can't import certificate." -ErrorRecord $_ -Continue
-      }
-    }
-  }
-  process {
-    if (Test-FunctionInterrupt) { return }
-		
-    if (!$Certificate -and !$Path) {
-      Write-Message -Level Warning -Message "You must specify either Certificate or Path"
-      return
-    }
-		
-    if ($Certificate) {
-      try {
-        $certData = $Certificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::PFX, $Password)
-      }
-      catch {
-        Stop-Function -Message "Can't export certificate" -ErrorRecord $_ -Continue
-      }
-    }
-		
-    foreach ($computer in $ComputerName) {
+		if ($Path) {
+			if (!(Test-Path -Path $Path)) {
+				Stop-Function -Message "Path ($Path) does not exist." -Category InvalidArgument
+				return
+			}
 			
-      if (![dbavalidate]::IsLocalhost($computer) -and !$Password) {
-        $Password = ((65 .. 90) + (97 .. 122) | Get-Random -Count 29 | ForEach-Object { [char]$_ }) -join "" | ConvertTo-SecureString -AsPlainText -Force
-      }
+			try {
+				# This may be too much, but ¯\_(ツ)_/¯
+				$bytes = [System.IO.File]::ReadAllBytes($Path)
+				$Certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+				$Certificate.Import($bytes, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
+			}
+			catch {
+				Stop-Function -Message "Can't import certificate." -ErrorRecord $_
+				return
+			}
+		}
+	}
+	process {
+		if (Test-FunctionInterrupt) { return }
+		
+		if (-not $Certificate) {
+			Stop-Function -Message "You must specify either Certificate or Path" -Category InvalidArgument
+			return
+		}
+		
+		foreach ($cert in $Certificate) {
 			
-      $scriptBlock = {
-        $Store = $args[2]
-        $Folder = $args[3]
+			try {
+				$certData = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::PFX, $Password)
+			}
+			catch {
+				Stop-Function -Message "Can't export certificate" -ErrorRecord $_ -Continue
+			}
+			
+			foreach ($computer in $ComputerName) {
+			
+				if ((-not $computer.IsLocalhost) -and (-not $Password)) {
+					$Password = ((65 .. 90) + (97 .. 122) | Get-Random -Count 29 | ForEach-Object { [char]$_ }) -join "" | ConvertTo-SecureString -AsPlainText -Force
+				}
 				
-        $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-        $cert.Import($args[0], $args[1], [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
-        Write-Verbose "Importing cert to $Folder\$Store"
-        $tempStore = New-Object System.Security.Cryptography.X509Certificates.X509Store($Folder, $Store)
-        $tempStore.Open('ReadWrite')
-        $tempStore.Add($cert)
-        $tempStore.Close()
+				$scriptBlock = {
+					$Store = $args[2]
+					$Folder = $args[3]
+					
+					$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+					$cert.Import($args[0], $args[1], [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::DefaultKeySet)
+					Write-Verbose "Importing cert to $Folder\$Store"
+					$tempStore = New-Object System.Security.Cryptography.X509Certificates.X509Store($Folder, $Store)
+					$tempStore.Open('ReadWrite')
+					$tempStore.Add($cert)
+					$tempStore.Close()
+					
+					Write-Verbose "Searching Cert:\$Store\$Folder"
+					Get-ChildItem "Cert:\$Store\$Folder" -Recurse | Where-Object { $_.Thumbprint -eq $cert.Thumbprint }
+				}
 				
-        Write-Verbose "Searching Cert:\$Store\$Folder"
-        Get-ChildItem "Cert:\$Store\$Folder" -Recurse | Where-Object { $_.Thumbprint -eq $cert.Thumbprint }
-      }
-			
-      if ($PScmdlet.ShouldProcess("local", "Connecting to $computer to import cert")) {
-        try {
-          Invoke-Command2 -ComputerName $computer -Credential $Credential -ArgumentList $certdata, $Password, $Store, $Folder -ScriptBlock $scriptblock -ErrorAction Stop |
-            Select-DefaultView -Property FriendlyName, DnsNameList, Thumbprint, NotBefore, NotAfter, Subject, Issuer
-        }
-        catch {
-          Stop-Function -Message "Failure" -ErrorRecord $_ -Target $computer -Continue
-        }
-      }
-    }
-  }
+				if ($PScmdlet.ShouldProcess("local", "Connecting to $computer to import cert")) {
+					try {
+						Invoke-Command2 -ComputerName $computer -Credential $Credential -ArgumentList $certdata, $Password, $Store, $Folder -ScriptBlock $scriptblock -ErrorAction Stop |
+						Select-DefaultView -Property FriendlyName, DnsNameList, Thumbprint, NotBefore, NotAfter, Subject, Issuer
+					}
+					catch {
+						Stop-Function -Message "Failure" -ErrorRecord $_ -Target $computer -Continue
+					}
+				}
+			}
+		}
+	}
 }
