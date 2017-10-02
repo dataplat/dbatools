00b2011d984eae59957d8526285ca5a8ac4ce421
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-10-02 19:20:57 +0200

    Implementing maintenance task library stuff

---
+++ internal/scripts/dbatools-maintenance.ps1
@@ -0,0 +1,23 @@
+﻿$scriptBlock = {
+	$script:___ScriptName = 'maintenance'
+	
+	try {
+		#region Main Execution
+		while ($true) {
+			# This portion is critical to gracefully closing the script
+			if ([Sqlcollaborative.Dbatools.Runspace.RunspaceHost]::Runspaces[$___ScriptName.ToLower()].State -notlike "Running") {
+				break
+			}
+			
+			Start-Sleep -Seconds 5
+		}
+		#endregion Main Execution
+	}
+	catch { }
+	finally {
+		[Sqlcollaborative.Dbatools.Runspace.RunspaceHost]::Runspaces[$___ScriptName.ToLower()].SignalStopped()
+	}
+}
+
+Register-DbaRunspace -ScriptBlock $scriptBlock -Name "maintenance"
+Start-DbaRunspace -Name "maintenance"
