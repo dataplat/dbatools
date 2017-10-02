6dfc83fa3571474f8f006a87393c029f2e737ce4
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-10-02 20:45:15 +0200

    Adding maintenance infrastructure

---
+++ internal/maintenance/tempcleanup.ps1
@@ -0,0 +1,4 @@
+﻿$scriptBlock = {
+	Get-ChildItem -Path $env:TEMP -Filter dbatools*.dll | Remove-Item -ErrorAction Ignore
+}
+Register-DbaMaintenanceTask -Name "tempcleanuo" -ScriptBlock $scriptBlock -Once -Delay (New-TimeSpan -Minutes 5) -Priority Low
