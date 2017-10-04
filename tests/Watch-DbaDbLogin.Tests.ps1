851a12456b9ee04d1c11af9e0b9a0a6b8c2c4773
potatoqualitee <clemaire@gmail.com>
2017-10-04 16:35:34 +0200

    Get-DbaInstance rename

--- tests/Watch-DbaDbLogin.Tests.ps1
+++ tests/Watch-DbaDbLogin.Tests.ps1
@@ -46,7 +46,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 	}
 	$script:instance1, $script:instance2 | Out-File $testFile
 	AfterAll {
-		$null = (Get-DbaInstance -SqlInstance $script:instance1).Databases[$databaseName].Query("DROP TABLE $tableName")
+		$null = (Connect-DbaInstance -SqlInstance $script:instance1).Databases[$databaseName].Query("DROP TABLE $tableName")
 	}
 	Context "Command actually works" {
 		Watch-DbaDbLogin -SqlInstance $script:instance1 -Database $databaseName -Table $tableName -ServersFromFile $testFile -Silent
