195a09b5e7b8339043be37c2e6281e9ad54feb96
potatoqualitee <clemaire@gmail.com>
2017-10-05 02:42:40 +0200

    made more efficient

--- tests/Read-DbaXEventFile.Tests.ps1
+++ tests/Read-DbaXEventFile.Tests.ps1
@@ -5,8 +5,12 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 	Context "Verifying command output" {
 		It "returns some results" {
-			$results = Get-DbaXEventSession -SqlInstance $script:instance2 | Read-DbaXEventFile
+			$results = Get-DbaXEventSession -SqlInstance $script:instance2 | Read-DbaXEventFile -Raw
 			[System.Linq.Enumerable]::Count($results) -gt 1 | Should Be $true
+		}
+		It "returns some results" {
+			$results = Get-DbaXEventSession -SqlInstance $script:instance2 | Read-DbaXEventFile
+			$results.Count -gt 1 | Should Be $true
 		}
 	}
 }
