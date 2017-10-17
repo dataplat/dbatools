d23b3bfaacca4bb1cc87072a014e60071703db85
niphlod <niphlod@gmail.com>
2017-10-17 23:30:47 +0200

    OpenCover, fixing paths #2 (do Identity)

--- tests/appveyor.opencoverfix.ps1
+++ tests/appveyor.opencoverfix.ps1
@@ -3,7 +3,7 @@ Write-Host -ForeGroundColor cyan "Fixing Opencover"
 $file.load("$($env:APPVEYOR_BUILD_FOLDER)\coverage_orig.xml")
 $xml_modules = $file.SelectNodes("/CoverageSession/Modules/Module/ModuleName[text() = 'dbatools'][1]")
 foreach($fpath in $xml_modules.ParentNode.Files.File) {
-    $fpath.fullPath = $fpath.fullPath.Replace($env:APPVEYOR_BUILD_FOLDER, '')
+    $fpath.fullPath = $fpath.fullPath.Replace("$($env:APPVEYOR_BUILD_FOLDER)\", '').Replace('\', '/')
 }
 $file.save("$($env:APPVEYOR_BUILD_FOLDER)\coverage.xml")
 
