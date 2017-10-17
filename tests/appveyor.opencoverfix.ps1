d12a03a9b95526ceabeac2d5ecac78bb755cbd94
niphlod <niphlod@gmail.com>
2017-10-17 23:08:34 +0200

    OpenCover, fixing paths

---
+++ tests/appveyor.opencoverfix.ps1
@@ -0,0 +1,9 @@
+Write-Host -ForeGroundColor cyan "Fixing Opencover"
+[System.Xml.XmlDocument]$file = New-Object System.Xml.XmlDocument
+$file.load("$($env:APPVEYOR_BUILD_FOLDER)\coverage_orig.xml")
+$xml_modules = $file.SelectNodes("/CoverageSession/Modules/Module/ModuleName[text() = 'dbatools'][1]")
+foreach($fpath in $xml_modules.ParentNode.Files.File) {
+    $fpath.fullPath = $fpath.fullPath.Replace($env:APPVEYOR_BUILD_FOLDER, '')
+}
+$file.save("$($env:APPVEYOR_BUILD_FOLDER)\coverage.xml")
+
