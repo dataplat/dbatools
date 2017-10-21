ada767e3b828a61bec96c7a97fd919dda031f543
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-10-21 12:41:05 +0200

    Added missing documentation

--- functions/Set-DbaConfig.ps1
+++ functions/Set-DbaConfig.ps1
@@ -8,6 +8,10 @@ function Set-DbaConfig
 			This function creates or changes configuration values.
 			These are used in dbatools to provide dynamic configuration information outside the PowerShell variable system.
 		
+		.PARAMETER FullName
+			The full name of a configuration element. Must be namespaced <Module>.<Name>.
+			The name can have any number of sub-segments, in order to better group configurations thematically.
+		
 		.PARAMETER Name
 			Name of the configuration entry. If an entry of exactly this non-casesensitive name already exists, its value will be overwritten.
 			Duplicate names across different modules are possible and will be treated separately.
@@ -16,15 +20,24 @@ function Set-DbaConfig
 			Is Equivalent to
 			-Name "Server" -Module "Nordwind"
 		
-		.PARAMETER Value
-			The value to assign to the named configuration element.
-		
 		.PARAMETER Module
 			This allows grouping configuration elements into groups based on the module/component they server.
 			If this parameter is not set, the configuration element is stored under its name only, which increases the likelyhood of name conflicts in large environments.
-    
-        .PARAMETER Description
-            Using this, the configuration setting is given a description, making it easier for a user to comprehend, what a specific setting is for.
+		
+		.PARAMETER Value
+			The value to assign to the named configuration element.
+		
+		.PARAMETER Description
+			Using this, the configuration setting is given a description, making it easier for a user to comprehend, what a specific setting is for.
+		
+		.PARAMETER Validation
+			The name of the validation script used for input validation.
+			These can be used to validate make sure that input is of the proper data type.
+			New validation scripts can be registered using Register-PSFConfigValidation
+		
+		.PARAMETER Handler
+			A scriptblock that is executed when a value is being set.
+			Is only executed if the validation was successful (assuming there was a validation, of course)
 		
 		.PARAMETER Hidden
 			Setting this parameter hides the configuration from casual discovery. Configurations with this set will only be returned by Get-Config, if the parameter "-Force" is used.
@@ -33,27 +46,37 @@ function Set-DbaConfig
 		.PARAMETER Default
 			Setting this parameter causes the system to treat this configuration as a default setting. If the configuration already exists, no changes will be performed.
 			Useful in scenarios where for some reason it is not practical to automatically set defaults before loading userprofiles.
-    
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-            
-        .PARAMETER DisableHandler
-            Internal Use Only.
-            This parameter disables the configuration handlers.
-            Configuration handlers are designed to automatically validate and process input set to a config value, in addition to writing the value.
-            In many cases, this is used to improve performance, by forking the value location also to a static C#-field, which is then used, rather than searching a Hashtable.
-            Sometimes it may only be used to introduce input validation.
-            During module import, some handlers are registered and many values written to configuration.
-            However, some of those values actually are already set as default values within the library. Processing a handler will cost a few ms.
-            Add up a couple dozen such events and the delay is very notable. This parameter is designed to be used during module import only, in order to speed up the import.
+		
+		.PARAMETER Initialize
+			Use this when setting configurations as part of module import.
+			When initializing a configuration, it will only do a thing if the configuration hasn't already been initialized (So if you load the module multiple times or in multiple runspaces, it won't make a difference)
+			Also, if there already was a non-initialized setting set for a given configuration, it will then try to set the old value again.
+			This value will be processed by handlers, if any are set.
+		
+		.PARAMETER DisableValidation
+			This parameters disables the input validation - if any - when processing a setting.
+			Normally this shouldn't be circumvented, but just in case, it can be disabled.
+		
+		.PARAMETER DisableHandler
+			Internal Use Only.
+			This parameter disables the configuration handlers.
+			Configuration handlers are designed to automatically validate and process input set to a config value, in addition to writing the value.
+			In many cases, this is used to improve performance, by forking the value location also to a static C#-field, which is then used, rather than searching a Hashtable.
+			Sometimes it may only be used to introduce input validation.
+			During module import, some handlers are registered and many values written to configuration.
+			However, some of those values actually are already set as default values within the library. Processing a handler will cost a few ms.
+			Add up a couple dozen such events and the delay is very notable.
+		
+		.PARAMETER EnableException
+			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+			This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+			Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 		
 		.EXAMPLE
 			PS C:\> Set-DbaConfig -Name 'User' -Value "Friedrich" -Description "The user under which the show must go on."
-	
+			
 			Creates a configuration entry named "User" with the value "Friedrich"
-	
+		
 		.EXAMPLE
 			PS C:\> Set-DbaConfig -Name 'mymodule.User' -Value "Friedrich" -Description "The user under which the show must go on." -Handler $scriptBlock -Initialize -Validation String
 			
@@ -67,22 +90,22 @@ function Set-DbaConfig
 			This is the default example for modules using the configuration system.
 			Note: While the -Handler parameter is optional, it is important to add it at the initial initialize call, if you are planning to add it.
 			Only then will the system validate previous settings (such as what a user might have placed in his user profile)
-	
+		
 		.EXAMPLE
 			PS C:\> Set-DbaConfig 'ConfigLink' 'https://www.example.com/config.xml' 'Company' -Hidden
-	
+			
 			Creates a configuration entry named "ConfigLink" in the "Company" module with the value 'https://www.example.com/config.xml'.
 			This entry is hidden from casual discovery using Get-Config.
-	
+		
 		.EXAMPLE
 			PS C:\> Set-DbaConfig 'Network.Firewall' '10.0.0.2' -Default
-	
+			
 			Creates a configuration entry named "Firewall" in the "Network" module with the value '10.0.0.2'
 			This is only set, if the setting does not exist yet. If it does, this command will apply no changes.
 		
 		.NOTES
 			Author: Friedrich Weinmann
-            Tags: Config
+			Tags: Config
 	#>
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
 	[CmdletBinding(DefaultParameterSetName = "FullName")]
