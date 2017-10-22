ddc4ae9d166280b2927dc0de6ab530ce6f502b02
Stuart Moore <stuart@stuart-moore.com>
2017-10-22 14:55:03 +0100

    fixing some pester bits due to backup resolution

--- tests/Get-DbaBackupInformation.Tests.ps1
+++ tests/Get-DbaBackupInformation.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	
 	BeforeAll {
-		$DestBackupDir = 'C:\Temp\GetTackups'
+		$DestBackupDir = 'C:\Temp\GetBackups'
 		if (-Not(Test-Path $DestBackupDir)) {
 			New-Item -Type Container -Path $DestBackupDir
         }
@@ -30,47 +30,47 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     
     AfterAll {
 		$null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname | Remove-DbaDatabase -Confirm:$false
-    	$null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    	$null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname2 | Remove-DbaDatabase -Confirm:$false
     }
     
-    Context "Get last history for all database" {
+    Context "Get history for all database" {
 		$results = Get-DbaBackupInformation -SqlInstance $script:instance1 -Path $DestBackupDir
-		It "Should be 8 backups returned" {
+		It "Should be 6 backups returned" {
 			$results.count | Should Be 6
 		}
-		It "Should return 2 full backus" {
+		It "Should return 2 full backups" {
 			($results | Where-Object {$_.Type -eq 'Database'}).count | Should be 2
 		}
-		It "Should return 4 log backups" {
+		It "Should return 2 log backups" {
 			($results | Where-Object {$_.Type -eq 'Transaction Log'}).count | Should be 2
         }
     }
 
-    Context "Get last history for one database" {
+    Context "Get history for one database" {
 		$results = Get-DbaBackupInformation -SqlInstance $script:instance1 -Path $DestBackupDir -DatabaseName $dbname2
-		It "Should be 4 backups returned" {
-			$results.count | Should Be 4
+		It "Should be 3 backups returned" {
+			$results.count | Should Be 3
 		}
 		It "Should Be 1 full backup" {
 			($results | Where-Object {$_.Type -eq 'Database'}).count | Should be 1
 		}
-		It "Should be 2 log backups" {
-			($results | Where-Object {$_.Type -eq 'Transaction Log'}).count | Should be 2
+		It "Should be 1 log backups" {
+            ($results | Where-Object {$_.Type -eq 'Transaction Log'}).count | Should be 1
         }
         It "Should only be backups of $dbname2"{
             ($results | Where-Object {$_.Database -ne $dbname2 }).count | Should Be 0
         }
     }
 
-    Context "Get last history for one instance" {
+    Context "Get history for one instance" {
 		$results = Get-DbaBackupInformation -SqlInstance $script:instance1 -Path $DestBackupDir -SourceInstance $script:instance2
-		It "Should be 4 backups returned" {
-			$results.count | Should Be 4
+		It "Should be 3 backups returned" {
+			$results.count | Should Be 3
 		}
 		It "Should Be 1 full backup" {
 			($results | Where-Object {$_.Type -eq 'Database'}).count | Should be 1
 		}
-		It "Should be 2 log backups" {
+		It "Should be 1 log backups" {
 			($results | Where-Object {$_.Type -eq 'Transaction Log'}).count | Should be 2
         }
         It "Should only be backups of $dbname2"{
@@ -80,9 +80,9 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 
     Context "Check the export/import of backup history" {
         $results = Get-DbaBackupInformation -SqlInstance $script:instance1 -Path $DestBackupDir -SourceInstance $script:instance1
-        $results | ConvertTo-Json | Out-File "$DestBackuDir\history.json"
-        $import = Get-Content "$DestBackuDir\history.json" -Raw | ConvertFrom-Json
-        $results = $import | Restore-DbaDatabse -SqlInstance $script:instance1 -DestinationFilePrefix hist -RestoredDatababaseNamePrefix hist -TrustDbBackupHistory
+        $results | ConvertTo-Json | Out-File "$DestBackupDir\history.json"
+        $import = Get-Content "$DestBackupDir\history.json" -Raw | ConvertFrom-Json
+        $results = $import | Restore-DbaDatabase -SqlInstance $script:instance1 -DestinationFilePrefix hist -RestoredDatababaseNamePrefix hist -TrustDbBackupHistory
         It "Should restore cleanly" {
             ($results | Where-Object {$_.RestoreComplete -eq $false}).count | Should be 0
         }
