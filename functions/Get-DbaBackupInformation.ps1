0c3a1300c1ec14abbedec2c26d1a7bf3663e817b
Stuart Moore <stuart@stuart-moore.com>
2017-10-22 10:47:31 +0100

    moving anonymisation to seperate function, so removing switch

--- functions/Get-DbaBackupInformation.ps1
+++ functions/Get-DbaBackupInformation.ps1
@@ -73,8 +73,7 @@ function Get-DbaBackupInformation {
         [string[]]$SourceInstance,
         [Switch]$XpDirTree,
         [switch]$Recurse,
-        [switch]$EnableException,
-        [switch]$Anonymise
+        [switch]$EnableException
       
     )
     begin {
