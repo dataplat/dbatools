f21aa3a86d76036d4a1b52a53d496b3556e9ca17
niphlod <niphlod@gmail.com>
2017-10-23 22:14:34 +0200

    scenario autodetection

--- tests/pester.groups.ps1
+++ tests/pester.groups.ps1
@@ -2,54 +2,9 @@
 
 $TestsRunGroups = @{
 	# run on scenario 2008R2
-	"2008R2"            = @(
-		'Add-DbaComputerCertificate',
-		'Connect-DbaInstance',
-		'Get-DbaAgentJobs',
-		'Get-DbaAgentJobOutputFile',
-		'Get-DbaBackupHistory',
-		'Get-DbaComputerSystem',
-		'Get-DbaDatabase',
-		'Get-DbaDatabaseEncryption',
-		'Get-DbaDbStoredProcedure',
-		'Get-DbaLogin',
-		'Get-DbaOperatingSystem',
-		'Get-DbaProcess',
-		'Get-DbaRegisteredServerName',
-		'Get-DbaSchemaChangeHistory',
-		'Get-DbaSpConfigure',
-		'Get-DbaSqlLog',
-		'Get-DbaSqlModule',
-		'Invoke-DbaCycleErrorLog',
-		'Invoke-DbaDiagnosticQuery',
-		'Mount-DbaDatabase',
-		'Remove-DbaDatabase -Confirm:$false',
-		'Restore-DbaDatabase',
-		'Set-DbaSpConfigure',
-		'Test-DbaIdentityUsage'
-	)
+	"2008R2"            = 'autodetect_$script:instance1'
 	# run on scenario 2016
-	"2016"              = @(
-		'Get-DbaAgDatabase',
-		'Get-DbaAgHadr',
-		'Get-DbaAgReplica',
-		'Get-DbaAvailabilityGroup',
-		'Get-DbaDatabaseSnapshot',
-		'Get-DbaFile',
-		'Get-DbaLastBackup',
-		'Get-DbaRegisteredServersStore',
-		'Get-DbaTraceFlag',
-		'Install-DbaMaintenanceSolution',
-		'Measure-DbaBackupThroughput',
-		'New-DbaAgentJob',
-		'New-DbaAgentJobStep',
-		'New-DbaDatabaseSnapshot',
-		'New-DbaSsisCatalog',
-		'Remove-DbaDatabaseSnapshot',
-		'Restore-DbaFromDatabaseSnapshot',
-		'Test-DbaDbCompression',
-		'Test-DbaLastBackup'
-	)
+	"2016"              = 'autodetect_$script:instance2'
 	#run on scenario 2016_service - SQL Server service tests that might disrupt other tests
 	"2016_service" = @(
 		'Start-DbaSqlService',
