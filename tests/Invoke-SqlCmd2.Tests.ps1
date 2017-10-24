8d8762810238bbc6e432efc62d8ccd9a6e8ce860
potatoqualitee <clemaire@gmail.com>
2017-10-24 14:57:22 +0200

    new test

--- tests/Invoke-SqlCmd2.Tests.ps1
+++ tests/Invoke-SqlCmd2.Tests.ps1
@@ -3,10 +3,12 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+	$results = Invoke-SqlCmd2 -ServerInstance $script:instance1 -Database tempdb -Query "Select 'hello' as TestColumn"
 	It "returns a datatable" {
 		$results.GetType().Name -eq "DataRow" | Should Be $true
 	}
 	
 	It "returns the proper result" {
+		$results.TestColumn -eq 'hello' | Should Be $true
 	}
 }
