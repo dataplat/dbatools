554012e85bab9020c4268f89752ebca0ff7e09e8
potatoqualitee <clemaire@gmail.com>
2017-10-24 14:57:14 +0200

    update from primary repo and test

---
+++ tests/Invoke-SqlCmd2.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+	It "returns a datatable" {
+		$results.GetType().Name -eq "DataRow" | Should Be $true
+	}
+	
+	It "returns the proper result" {
+	}
+}
