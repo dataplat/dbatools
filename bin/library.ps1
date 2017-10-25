e7059e44be8edf21bee4b7776cf64396fc00ecb5
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-10-26 00:56:31 +0200

    Increment version number

--- bin/library.ps1
+++ bin/library.ps1
@@ -1,5 +1,5 @@
 ﻿# Current library Version the module expects
-$currentLibraryVersion = New-Object System.Version(0, 9, 0, 32)
+$currentLibraryVersion = New-Object System.Version(0, 9, 0, 33)
 
 <#
 Library Versioning 101:
