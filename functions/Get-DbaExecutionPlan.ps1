e9654161f15ec546fc97f93c08a4bcf1d34c2bf8
Shaneis <shaneoneillis@gmail.com>
2017-10-27 07:02:21 +0100

    Renamed variables to match what we're searching for

--- functions/Get-DbaExecutionPlan.ps1
+++ functions/Get-DbaExecutionPlan.ps1
@@ -198,7 +198,7 @@ Gets super detailed information for execution plans on only for AdventureWorks20
 					$simple = ([xml]$row.SingleStatementPlan).ShowPlanXML.BatchSequence.Batch.Statements.StmtSimple
 					$sqlhandle = "0x"; $row.sqlhandle | ForEach-Object { $sqlhandle += ("{0:X}" -f $_).PadLeft(2, "0") }
 					$planhandle = "0x"; $row.planhandle | ForEach-Object { $planhandle += ("{0:X}" -f $_).PadLeft(2, "0") }
-					$implicitConversion = $simple.QueryPlan.Warnings.PlanAffectingConvert;
+					$planWarnings = $simple.QueryPlan.Warnings.PlanAffectingConvert;
 
 					[pscustomobject]@{
 						ComputerName = $server.NetName
@@ -232,7 +232,7 @@ Gets super detailed information for execution plans on only for AdventureWorks20
 						BatchSimpleXml = ([xml]$row.BatchQueryPlan).ShowPlanXML.BatchSequence.Batch.Statements.StmtSimple
 						BatchQueryPlanRaw = [xml]$row.BatchQueryPlan
 						SingleStatementPlanRaw = [xml]$row.SingleStatementPlan
-						PlanWarnings = $implicitConversion
+						PlanWarnings = $planWarnings
 					} | Select-DefaultView -ExcludeProperty BatchQueryPlan, SingleStatementPlan, BatchConditionXmlRaw, BatchQueryPlanRaw, SingleStatementPlanRaw, PlanWarnings
 				}
 			}
