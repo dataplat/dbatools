33effb40c2fcea7ef59f1bf5fe29d5c63f4189eb
Stuart Moore <stuart@stuart-moore.com>
2017-10-27 10:48:31 +0100

    unit tests done

--- tests/Select-DbaBackupInformation.Tests.ps1
+++ tests/Select-DbaBackupInformation.Tests.ps1
@@ -5,156 +5,201 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 	InModuleScope dbatools {
 		Context "Empty TLog Backup Issues" {
-            $Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\EmptyTlogData.json -raw)
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
+           	$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\EmptyTlogData.json -raw)
+			
 			$Output = Select-DbaBackupInformation -BackupHistory $header -EnableException:$true
 			
 			It "Should return an array of 3 items" {
-				$Output[0].values.count | Should be 3
+				$Output.count | Should be 3
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 0 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
 			}
 			It "Should return 2 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 2
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 2
 			}
 		}
 		Context "General Diff Restore" {
             $Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
-			$Output = Select-DbaBackupInformation -SqlInstance 'TestSQL' -Files "c:\dummy.txt"
+
+			$Output = Select-DbaBackupInformation -BackupHistory $header -EnableException:$true
 			
 			It "Should return an array of 7 items" {
-				$Output[0].values.count | Should be 7
+				$Output.count | Should be 7
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 1 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 5 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 5
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 5
+			}
+		}
+	
+
+		Context "General Diff Restore from Pipeline" {
+			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
+
+			$Output = $Header | Select-DbaBackupInformation -EnableException:$true
+			
+			It "Should return an array of 7 items" {
+				$Output.count | Should be 7
+			}
+			It "Should return 1 Full backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+			}
+			It "Should return 1 Diff backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+			}
+			It "Should return 5 log backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 5
+			}
+		}
+		Context "General Diff Restore from Pipeline with IgnoreDiff" {
+			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
+
+			$Output = $Header | Select-DbaBackupInformation -EnableException:$true -IgnoreDiff
+			
+			It "Should return an array of 9 items" {
+				$Output.count | Should be 9
+			}
+			It "Should return 1 Full backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+			}
+			It "Should return 0 Diff backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
+			}
+			It "Should return 8 log backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 8
+			}
+		}
+		Context "General Diff Restore from Pipeline with IgnoreLog" {
+			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
+
+			$Output = $Header | Select-DbaBackupInformation -EnableException:$true -IgnoreLogs
+			
+			It "Should return an array of 2 items" {
+				$Output.count | Should be 2
+			}
+			It "Should return 1 Full backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+			}
+			It "Should return 1 Diff backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+			}
+			It "Should return 0 log backups" {
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 0
 			}
 		}
 		Context "Server/database names and file paths have commas and spaces" {
 			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\RestoreCommaIssues.json -raw)
-            Mock Read-DbaBackupHeader { $Header }
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
+
 			$Output = Select-DbaBackupInformation -BackupHistory $header
 			
 			It "Should return an array of 7 items" {
-				$Output[0].values.count | Should be 7
+				$Output.count | Should be 7
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 1 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 5 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 5
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 5
 			}
 		}
 		Context "Missing Diff Restore" {
 			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
-            $header = $header | Where-Object { $_.BackupTypeDescription -ne 'Database Differential' }
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
+			$header = $header | Where-Object { $_.BackupTypeDescription -ne 'Database Differential' }
+			
 			$Output = Select-DbaBackupInformation  -BackupHistory $header
-			$Output
+
 			It "Should return an array of 9 items" {
-				$Output[0].values.count | Should be 9
+				$Output.count | Should be 9
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 0 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
 			}
 			It "Should return 8 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 8
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 8
 			}
 		}
 		Context "Overlapping Diff and log Restore" {
             $Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffIssues.json -raw)
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
+
+			
 			$RestoreDate =  Get-date "2017-07-18 09:00:00"
 			$Output = Select-DbaBackupInformation  -BackupHistory $Header -RestoreTime $RestoreDate
-			$Output
+			
 			It "Should return an array of 193 items" {
-				$Output[0].values.count | Should be 194
+				$Output.count | Should be 194
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 1 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 192 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 192
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 192
 			}
 			It "Should not contain the Log backup with LastLsn 17126786000011867500001 " {
-				($Output[0].values | Where-Object { $_.LastLsn -eq '17126786000011867500001' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.LastLsn -eq '17126786000011867500001' } | Measure-Object).count | Should Be 0
 			}
 		}
 		Context "When FirstLSN ne CheckPointLsn" {
             $Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\chkptLSN-ne-firstLSN.json -raw)
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
+
+			
 			$RestoreDate = Get-date "2017-07-18 09:00:00"
 			$Output = Select-DbaBackupInformation  -BackupHistory $Header -RestoreTime $RestoreDate
-			$Output
+			
 			It "Should return an array of 193 items" {
-				$Output[0].values.count | Should be 194
+				$Output.count | Should be 194
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 1 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 191 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 192
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 192
 			}
 			It "Should not contain the Log backup with LastLsn 17126786000011867500001 " {
-				($Output[0].values | Where-Object { $_.LastLsn -eq '17126786000011867500001' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.LastLsn -eq '17126786000011867500001' } | Measure-Object).count | Should Be 0
 			}
 		}
 		Context "When TLogs between full's FirstLsn and LastLsn" {
             $Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\TLogBWFirstLastLsn.json -raw)
-            $Header | %{$_ | Add-Member -Name 'Type' -Type noteProperty -Value $_.BackupTypeDescription}
-            $Header | %{$_ | Add-Member -Name 'Database' -Type noteProperty -Value $_.DatabaseName}
-			Mock Read-DbaBackupHeader { $Header }
+
+			
 			$RestoreDate = Get-date "2017-07-18 09:00:00"
 			$Output = Select-DbaBackupInformation -BackupHistory $Header -RestoreTime $RestoreDate
-			$Output
+
 			It "Should return an array of 3 items" {
-				$Output[0].values.count | Should be 3
+				$Output.count | Should be 3
 			}
 			It "Should return 1 Full backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database' } | Measure-Object).count | Should Be 1
 			}
 			It "Should return 0 Diff backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Database Differential' } | Measure-Object).count | Should Be 0
 			}
 			It "Should return 2 log backups" {
-				($Output[0].values | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 2
+				($Output | Where-Object { $_.BackupTypeDescription -eq 'Transaction Log' } | Measure-Object).count | Should Be 2
 			}
 			It "Should not contain the Log backup with LastLsn 14975000000265600001 " {
-				($Output[0].values | Where-Object { $_.LastLsn -eq '14975000000265600001' } | Measure-Object).count | Should Be 0
+				($Output | Where-Object { $_.LastLsn -eq '14975000000265600001' } | Measure-Object).count | Should Be 0
 			}
 		}
 	}
