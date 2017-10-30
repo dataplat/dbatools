ab76ae1dca8d777a4b2f9d506901e6fb74c1c300
niphlod <niphlod@gmail.com>
2017-10-30 21:30:13 +0100

    Start/Stop-DbaXeSession, param help fix
    Adds a proper definition for pipelining parameter

--- functions/Stop-DbaXESession.ps1
+++ functions/Stop-DbaXESession.ps1
@@ -17,6 +17,9 @@ function Stop-DbaXESession {
 
 	.PARAMETER AllSessions
 	Stop all Extended Events sessions on an instance, ignoring the packaged sessions: AlwaysOn_health, system_health, telemetry_xevents.
+	
+	.PARAMETER SessionCollection
+	Internal parameter to support piping from Get-DbaXESession
 
 	.PARAMETER EnableException
 	By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
