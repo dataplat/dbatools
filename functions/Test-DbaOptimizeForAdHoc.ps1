51b2abe0bd120d91591f8dec67057d4f52890c6c
Shawn Melton <wshawnmelton@gmail.com>
2017-10-30 05:27:35 -0500

    add enableexception parameter

--- functions/Test-DbaOptimizeForAdHoc.ps1
+++ functions/Test-DbaOptimizeForAdHoc.ps1
@@ -21,6 +21,9 @@ function Test-DbaOptimizeForAdHoc {
 
 			Windows Authentication will be used if SqlCredential is not specified. To connect as a different Windows user, run PowerShell as that user.
 
+	.PARAMETER EnableException
+			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message. This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting. Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
 		.NOTES
 			Tags: Configure, SPConfigure
 			Author: Brandon Abshire, netnerds.net
@@ -42,7 +45,8 @@ function Test-DbaOptimizeForAdHoc {
 		[parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
 		[Alias("ServerInstance", "SqlServer", "SqlServers")]
 		[DbaInstance[]]$SqlInstance,
-		[PSCredential]$SqlCredential
+		[PSCredential]$SqlCredential,
+		[switch][Alias('Silent')]$EnableException
 	)
 
 	begin {
