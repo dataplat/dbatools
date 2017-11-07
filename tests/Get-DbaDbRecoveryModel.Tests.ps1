2ea096871da09f0ca2ba578f8c159220252511ac
potatoqualitee <clemaire@gmail.com>
2017-11-07 23:24:34 +0100

    added test

---
+++ tests/Get-DbaDbRecoveryModel.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Recovery model is correctly identified" {
+		$results = Get-DbaDbRecoveryModel -SqlInstance $script:instance2 -Database master
+		
+		It "returns a single database" {
+			$results.Count | Should Be 1
+		}
+		
+		It "returns a the correct recovery model" {
+			$results.RecoveryModel -eq 'Simple' | Should Be $true
+		}
+		
+		$results = Get-DbaDbRecoveryModel -SqlInstance $script:instance2
+		
+		It "returns accurate number of results" {
+			$results.Count -ge 4 | Should Be $true
+		}
+	}
+}
