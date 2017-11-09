0d4fdf8b2f717494fa4d14d0846d385fa34ca3f6
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2017-11-09 23:08:01 +0100

    Caught screenshot issue (#2623)
    would create proper screenshots if the host was not powershell.exe

--- functions/New-DbatoolsSupportPackage.ps1
+++ functions/New-DbatoolsSupportPackage.ps1
@@ -64,54 +64,56 @@ function New-DbatoolsSupportPackage {
         function Get-ShellBuffer {
             [CmdletBinding()]
             Param ()
-            
-            # Define limits
-            $rec = New-Object System.Management.Automation.Host.Rectangle
-            $rec.Left = 0
-            $rec.Right = $host.ui.rawui.BufferSize.Width - 1
-            $rec.Top = 0
-            $rec.Bottom = $host.ui.rawui.BufferSize.Height - 1
-            
-            # Load buffer
-            $buffer = $host.ui.rawui.GetBufferContents($rec)
-            
-            # Convert Buffer to list of strings
-            $int = 0
-            $lines = @()
-            while ($int -le $rec.Bottom) {
-                $n = 0
-                $line = ""
-                while ($n -le $rec.Right) {
-                    $line += $buffer[$int, $n].Character
-                    $n++
-                }
-                $line = $line.TrimEnd()
-                $lines += $line
-                $int++
-            }
-            
-            # Measure empty lines at the beginning
-            $int = 0
-            $temp = $lines[$int]
-            while ($temp -eq "") { $int++; $temp = $lines[$int] }
-            
-            # Measure empty lines at the end
-            $z = $rec.Bottom
-            $temp = $lines[$z]
-            while ($temp -eq "") { $z--; $temp = $lines[$z] }
-            
-            # Skip the line launching this very function
-            $z--
-            
-            # Measure empty lines at the end (continued)
-            $temp = $lines[$z]
-            while ($temp -eq "") { $z--; $temp = $lines[$z] }
-            
-            # Cut results to the limit and return them
-            return $lines[$int .. $z]
-            
-        }
-        #endregion Helper functions
+			
+			try {
+				# Define limits
+				$rec = New-Object System.Management.Automation.Host.Rectangle
+				$rec.Left = 0
+				$rec.Right = $host.ui.rawui.BufferSize.Width - 1
+				$rec.Top = 0
+				$rec.Bottom = $host.ui.rawui.BufferSize.Height - 1
+				
+				# Load buffer
+				$buffer = $host.ui.rawui.GetBufferContents($rec)
+				
+				# Convert Buffer to list of strings
+				$int = 0
+				$lines = @()
+				while ($int -le $rec.Bottom) {
+					$n = 0
+					$line = ""
+					while ($n -le $rec.Right) {
+						$line += $buffer[$int, $n].Character
+						$n++
+					}
+					$line = $line.TrimEnd()
+					$lines += $line
+					$int++
+				}
+				
+				# Measure empty lines at the beginning
+				$int = 0
+				$temp = $lines[$int]
+				while ($temp -eq "") { $int++; $temp = $lines[$int] }
+				
+				# Measure empty lines at the end
+				$z = $rec.Bottom
+				$temp = $lines[$z]
+				while ($temp -eq "") { $z--; $temp = $lines[$z] }
+				
+				# Skip the line launching this very function
+				$z--
+				
+				# Measure empty lines at the end (continued)
+				$temp = $lines[$z]
+				while ($temp -eq "") { $z--; $temp = $lines[$z] }
+				
+				# Cut results to the limit and return them
+				return $lines[$int .. $z]
+			}
+			catch { }
+		}
+		#endregion Helper functions
     }
     PROCESS {
         $filePathXml = "$($Path.Trim('\'))\dbatools_support_pack_$(Get-Date -Format "yyyy_MM_dd-HH_mm_ss").xml"
