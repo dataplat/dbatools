f489921509378bbb317869ae690e68496d9f80c5
Constantine <constantine@constantinekokkinos.com>
2017-11-09 12:31:43 -0800

    Changed save method to work (#2618)
    Updated to use io.file to write a bytestream instead of out-file, tested, worked great.

--- functions/Save-DbaDiagnosticQueryScript.ps1
+++ functions/Save-DbaDiagnosticQueryScript.ps1
@@ -90,7 +90,7 @@ If Path is not specified, the "My Documents" location will be used.
     try {
       $page = Invoke-WebRequest -Uri $doc.URL -ErrorAction Stop
       $filename = "{0}\SQLServerDiagnosticQueries_{1}_{2}.sql" -f $Path, $doc.SQLVersion, "$($doc.FileYear)$($doc.FileMonth)"
-      $page.content | Out-File -LiteralPath $filename
+      [io.file]::WriteAllBytes($filename,$page.content)
     }
     catch {
       Stop-Function -Message "Requesting and writing file failed: $_" -Target $filename -InnerErrorRecord $_
