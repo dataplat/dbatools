43ee11acdf31a644fe58420be1e825aff65172a5
Shawn Melton <wshawnmelton@gmail.com>
2017-11-10 22:52:30 -0600

    Had to adjust sleep time Since the start-job has to load the module it has to sleep longer before the test continues.

--- tests/Get-DbaWaitingTask.Tests.ps1
+++ tests/Get-DbaWaitingTask.Tests.ps1
@@ -37,7 +37,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 	$flag = "dbatools_$(Get-Random)"
 	$time = '00:15:00'
 	$sql = "SELECT '$flag'; WAITFOR DELAY '$time'"
-	$instance = 'localhost'
+	$instance = $script:instance2
 
 	$modulePath = 'C:\Github\dbatools\dbatools.psd1'
 	$job = 'YouHaveBeenFoundWaiting'
@@ -45,43 +45,44 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 	Start-Job -Name $job -ScriptBlock {
 		Import-Module $args[0];
 		(Connect-DbaInstance -SqlInstance $args[1] -ClientName dbatools-waiting).Query($args[2])
-		} -ArgumentList $modulePath, $instance, $sql
+	} -ArgumentList $modulePath, $instance, $sql
 
-	Start-Sleep -Seconds 5
+	<#
+		**This has to sleep as it can take a couple seconds for the job to start**
+		Setting it lower will cause issues, you have to consider the Start-Job has to load the module which takes on average 3-4 seconds itself before it executes the command.
+
+		If someone knows a cleaner method by all means adjust this test.
+	#>
+	Start-Sleep -Seconds 8
+
 	$process = Get-DbaProcess -SqlInstance $instance | Where-Object Program -eq 'dbatools-waiting' | Select-Object -ExpandProperty Spid
 
-	# Context "Command actually works" {
-	# 	$results = Get-DbaWaitingTask -SqlInstance $script:instance2 -Spid $process.Spid
-	# 	It "Should have correct properties" {
-	# 		$ExpectedProps = 'ComputerName,InstanceName,SqlInstance,Spid,Thread,Scheduler,WaitMs,WaitType,BlockingSpid,ResourceDesc,NodeId,Dop,DbId,URL,QueryPlan,SqlText'.Split(',')
-	# 		($results[0].PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
-	# 	}
-	# 	Get-Job -Name $Job.Name | Remove-Job -Force -ErrorAction SilentlyContinue
-	# }
-
-	# if ($process.Spid -ne $null) {
+	if ($process -ne $null) {
 		Context "Command actually works" {
 			$results = Get-DbaWaitingTask -SqlInstance $instance -Spid $process
 			It "Should have correct properties" {
 				$ExpectedProps = 'ComputerName,InstanceName,SqlInstance,Spid,Thread,Scheduler,WaitMs,WaitType,BlockingSpid,ResourceDesc,NodeId,Dop,DbId,InfoUrl,QueryPlan,SqlText'.Split(',')
 				($results.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
 			}
-Write-Host "$($results.Spid) spid running"
-Write-Host "$($process) process running"
 			It "Should have command of 'WAITFOR'" {
-				$results.Command | Should BeLike "*WAITFOR*"
+				$results.WaitType | Should BeLike "*WAITFOR*"
 			}
 		}
 
-		$isProcess = Get-DbaProcess -SqlInstance $instance -Spid $process.Spid
+		$isProcess = Get-DbaProcess -SqlInstance $instance -Spid $process
 		if ($isProcess) {
-			# Stop-DbaProcess -SqlInstance $script:instance2 -Spid $process
-		}
-		# Get-Job -Name $job | Remove-Job -Force -ErrorAction SilentlyContinue
+			Stop-DbaProcess -SqlInstance $instance -Spid $process
 
-	# }
-	# else {
-	# 	$cmdName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-	# 	Write-Host "$cmdName - Test process could not be generated" -ForegroundColor Cyan
-	# }
+			# I've had a few cases where first run didn't actually kill the process
+			$isProcess = Get-DbaProcess -SqlInstance $instance -Spid $process
+			if ($isProcess) {
+				Stop-DbaProcess -SqlInstance $instance -Spid $process -ErrorAction SilentlyContinue
+			}
+		}
+		Get-Job -Name $job | Remove-Job -Force -ErrorAction SilentlyContinue
+	}
+	else {
+		$cmdName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+		Write-Host "$cmdName - Test process could not be generated" -ForegroundColor Cyan
+	}
 }
