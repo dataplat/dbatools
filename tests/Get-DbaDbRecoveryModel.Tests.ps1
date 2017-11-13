be3a3d976f1393a688bd2a437e452ab9c7d399f5
niphlod <niphlod@gmail.com>
2017-11-13 23:23:40 +0100

    Get-DbaDbRecoveryModel, add tests for RecoveryModel

--- tests/Get-DbaDbRecoveryModel.Tests.ps1
+++ tests/Get-DbaDbRecoveryModel.Tests.ps1
@@ -10,7 +10,7 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 			$results.Count | Should Be 1
 		}
 		
-		It "returns a the correct recovery model" {
+		It "returns the correct recovery model" {
 			$results.RecoveryModel -eq 'Simple' | Should Be $true
 		}
 		
@@ -18,6 +18,26 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 		
 		It "returns accurate number of results" {
 			$results.Count -ge 4 | Should Be $true
+		}
+	}
+	Context "RecoveryModel parameter works" {
+		BeforeAll {
+			$server = Connect-DbaInstance -SqlInstance $script:instance2
+			$dbname = "dbatoolsci_getrecoverymodel"
+			Get-DbaDatabase -SqlInstance $server -Database $dbname | Remove-DbaDatabase -Confirm:$false
+			$server.Query("CREATE DATABASE $dbname; ALTER DATABASE $dbname SET RECOVERY BULK_LOGGED WITH NO_WAIT;")
+		}
+		AfterAll {
+			Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+		}
+		
+		It "gets the newly created database with the correct recovery model" {
+			$results = Get-DbaDbRecoveryModel -SqlInstance $script:instance2 -Database $dbname
+			$results.RecoveryModel -eq 'BulkLogged' | Should Be $true
+		}
+		It "honors the RecoveryModel parameter filter" {
+			$results = Get-DbaDbRecoveryModel -SqlInstance $script:instance2 -RecoveryModel BulkLogged
+			$results.Name -contains $dbname | Should Be $true
 		}
 	}
 }
