ddb3161fe66e78cf1ec80d465d80b234836af5c2
potatoqualitee <clemaire@gmail.com>
2017-11-15 02:09:03 +0100

    added for provider compat

--- functions/Get-DbaFile.ps1
+++ functions/Get-DbaFile.ps1
@@ -179,7 +179,7 @@ Finds files in E:\Dir1 ending with ".fsf" and ".mld" for both the servers sql201
 			Write-Message -Level Verbose -Message "Adding paths"
 			$sql = Get-SQLDirTreeQuery $Path
 			Write-Message -Level Verbose -Message $sql
-			$datatable = $server.Query($sql)
+			$datatable = $server.ConnectionContext.ExecuteWithResults($sql).Tables.Rows
 			
 			Write-Message -Level Verbose -Message "$($datatable.Rows.Count) files found."
 			if ($FileTypeComparison) {
