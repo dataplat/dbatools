f58bdd5f15e986546feaeb04ccb69c0fe0444626
Simone Bizzotto <niphlod@gmail.com>
2017-11-16 17:44:06 +0100

    Enhancement/ci pipeline (#2673)
    * CI rationalization (and shutup)
    
    * appveyor, dropping msbuild.log artifact
    
    * appveyor, adding flags

---
+++ tests/appveyor.post.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host -Object "appveyor.post: Sending coverage data" -ForeGroundColor DarkGreen
+Push-AppveyorArtifact PesterResultsCoverage.json -FileName "PesterResultsCoverage"
+codecov -f PesterResultsCoverage.json --flag "ps,$($env:SCENARIO.toLower())" | Out-Null
+# DLL unittests only in default scenario
+if($env:SCENARIO -eq 'default') {
+  Write-Host -Object "appveyor.post: DLL unittests"  -ForeGroundColor DarkGreen
+  choco install opencover.portable | Out-Null
+  OpenCover.Console.exe `
+    -register:user `
+    -target:"vstest.console.exe" `
+    -targetargs:"/logger:Appveyor bin\projects\dbatools\dbatools.Tests\bin\Debug\dbatools.Tests.dll" `
+    -output:"coverage.xml" `
+    -filter:"+[dbatools]*" `
+    -returntargetcode | Out-Null
+  Push-AppveyorArtifact coverage.xml -FileName "OpenCover c# report"
+  codecov -f "coverage.xml" --flag "dll,$($env:SCENARIO.toLower())" | Out-Null
+}
