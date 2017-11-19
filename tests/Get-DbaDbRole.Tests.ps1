a442c579c64379431d2b5f522b475f501724e26d
PowerDBAKlaas <klaas@powerdba.eu>
2017-11-19 12:02:04 +0100

    correct parameters in tests

--- tests/Get-DbaDbRole.Tests.ps1
+++ tests/Get-DbaDbRole.Tests.ps1
@@ -10,10 +10,10 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 				- Commands that *do not* include SupportShouldProcess, set defaultParamCount    = 11
 				- Commands that *do* include SupportShouldProcess, set defaultParamCount        = 13
 		#>
-		$paramCount = 7
+		$paramCount = 6
 		$defaultParamCount = 11
 		[object[]]$params = (Get-ChildItem function:\Get-DbaDbRole).Parameters.Keys
-		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'ServerRole', 'ExcludeServerRole', 'Login'
+		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'Database', 'ExcludeDatabase', 'ExcludeFixedRole'
 		It "Should contain our specific parameters" {
 			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
 		}
@@ -31,11 +31,11 @@ Describe "Get-DbaDbRole Integration Tests" -Tag "IntegrationTests" {
                 $results = Get-DbaDbRole -SqlInstance $script:instance2 -Database master
 				$results.where({$_.Database -ne 'master'}).count | Should Be 0
 			}
-			It "returns no fixed roles with -NoFixedRole" {
-                $results = Get-DbaDbRole -SqlInstance $script:instance2 -NoFixedRole
+			It "returns no fixed roles with -ExcludeFixedRole" {
+                $results = Get-DbaDbRole -SqlInstance $script:instance2 -ExcludeFixedRole
 				$results.where({$_.name -match 'db_datareader|db_datawriter|db_ddladmin'}).count | Should Be 0
 			}
-			It "returns fixed roles without -NoFixedRole" {
+			It "returns fixed roles without -ExcludeFixedRole" {
                 $results = Get-DbaDbRole -SqlInstance $script:instance2
 				$results.where({$_.name -match 'db_datareader|db_datawriter|db_ddladmin'}).count | Should BeGreaterThan 0
 			}
