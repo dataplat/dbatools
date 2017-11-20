2966ed04ddf1c2762bf85805b7bfdc59735b7fea
Shawn Melton <wshawnmelton@gmail.com>
2017-11-19 21:36:54 -0600

    Adjusting logins in output, removing param

--- functions/Get-DbaServerRole.ps1
+++ functions/Get-DbaServerRole.ps1
@@ -4,7 +4,7 @@ function Get-DbaServerRole {
 			Gets the list of server-level roles with the logins that are members of that role.
 
 		.DESCRIPTION
-			Gets the list of server-level role for SQL Server instance. Output will include the logins that are members of the server-level role(s).
+			Gets the list of server-level roles for SQL Server instance.
 
 		.PARAMETER SqlInstance
 			The SQL Server instance. Server version must be SQL Server version 2005 or higher.
@@ -25,10 +25,7 @@ function Get-DbaServerRole {
 			Server-Level role to exclude from results.
 
 		.PARAMETER ExcludeFixedRole
-			Filter the fixed server-level roles. Only applies to SQL Server 2017 that supports custom server-level roles.
-
-		.PARAMETER Login
-			SQL Server login to filter results, will only return roles where the login(s) are a member.
+			Filter the fixed server-level roles. Only applies to SQL Server 2017 that supports creation of server-level roles.
 
 		.PARAMETER EnableException
 			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message. This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting. Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
@@ -57,7 +54,7 @@ function Get-DbaServerRole {
 		.EXAMPLE
 			Get-DbaServerRole -SqlInstance sql2017a -ExcludeFixedRole
 
-			Outputs the user server-level role(s) on sql2017a instance. A SQL Server 2017 supported feature only.
+			Outputs the server-level role(s) that are not fixed roles on sql2017a instance.
 
 		.EXAMPLE
 			Get-DbaServerRole -SqlInstance sql2016a -Login Bob
@@ -81,8 +78,6 @@ function Get-DbaServerRole {
 		[object[]]$ServerRole,
 		[object[]]$ExcludeServerRole,
 		[switch]$ExcludeFixedRole,
-		[Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-		[object[]]$Login,
 		[switch]$EnableException
 	)
 
@@ -110,29 +105,14 @@ function Get-DbaServerRole {
 
 			foreach ($role in $roles) {
 				$members = $role.EnumMemberNames()
-				if ($Login) {
-					$members = $members | Where-Object {$_ -in $Login}
-				}
 
-				if ($members) {
-					foreach ($member in $members) {
-						Add-Member -Force -InputObject $role -MemberType NoteProperty -Name Login -Value $member
-						Add-Member -Force -InputObject $role -MemberType NoteProperty -Name ComputerName -value $server.NetName
-						Add-Member -Force -InputObject $role -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
-						Add-Member -Force -InputObject $role -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
+				Add-Member -Force -InputObject $role -MemberType NoteProperty -Name Login -Value $members
+				Add-Member -Force -InputObject $role -MemberType NoteProperty -Name ComputerName -value $server.NetName
+				Add-Member -Force -InputObject $role -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
+				Add-Member -Force -InputObject $role -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
 
-						$default = 'ComputerName', 'InstanceName', 'SqlInstance', 'Id', 'Name as Role', 'IsFixedRole', 'Owner', 'Login', 'DateCreated', 'DateModified'
-						Select-DefaultView -InputObject $role -Property $default
-					}
-				}
-				if (!$members -and !$Login) {
-					Add-Member -Force -InputObject $role -MemberType NoteProperty -Name ComputerName -value $server.NetName
-					Add-Member -Force -InputObject $role -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
-					Add-Member -Force -InputObject $role -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
-
-					$default = 'ComputerName', 'InstanceName', 'SqlInstance', 'Id', 'Name as Role', 'IsFixedRole', 'Owner', 'Login', 'DateCreated', 'DateModified'
-					Select-DefaultView -InputObject $role -Property $default
-				}
+				$default = 'ComputerName', 'InstanceName', 'SqlInstance', 'Name as Role', 'IsFixedRole', 'DateCreated', 'DateModified'
+				Select-DefaultView -InputObject $role -Property $default
 			}
 		}
 	}
