ccf70e15c35b74b0ff859391e71259cd681c005e
Shawn Melton <wshawnmelton@gmail.com>
2017-11-19 21:37:07 -0600

    adjusting test for changes to login data

--- tests/Get-DbaServerRole.Tests.ps1
+++ tests/Get-DbaServerRole.Tests.ps1
@@ -17,10 +17,10 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 				- Commands that *do not* include SupportShouldProcess, set defaultParamCount    = 11
 				- Commands that *do* include SupportShouldProcess, set defaultParamCount        = 13
 		#>
-		$paramCount = 7
+		$paramCount = 6
 		$defaultParamCount = 11
 		[object[]]$params = (Get-ChildItem function:\Get-DbaServerRole).Parameters.Keys
-		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'ServerRole', 'ExcludeServerRole', 'ExcludeFixedRole', 'Login'
+		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'ServerRole', 'ExcludeServerRole', 'ExcludeFixedRole'
 		It "Should contain our specific parameters" {
 			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
 		}
@@ -68,16 +68,6 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 		It "Shows only one value with ServerRole parameter" {
 			$results = Get-DbaServerRole -SqlInstance $script:instance2 -ServerRole sysadmin
 			$results[0].Role | Should Be "sysadmin"
-		}
-
-		It "Returns [sa] member name for sysadmin role" {
-			$results = Get-DbaServerRole -SqlInstance $script:instance2 -ServerRole sysadmin -Login 'sa'
-			$results.Login -contains "sa" | Should Be $true
-		}
-
-		It "Returns nothing for [sa] being member of diskadmin role" {
-			$results = Get-DbaServerRole -SqlInstance $script:instance2 -ServerRole diskadmin -Login 'sa'
-			$results | Should BeNullOrEmpty
 		}
 
 		It "Should exclude sysadmin from output" {
