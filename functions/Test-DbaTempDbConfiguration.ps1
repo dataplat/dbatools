750b27f902cd26be168c00207a873542d3d86bae
Shawn Melton <wshawnmelton@gmail.com>
2017-11-28 08:13:42 -0600

    Addressing #2217

--- functions/Test-DbaTempDbConfiguration.ps1
+++ functions/Test-DbaTempDbConfiguration.ps1
@@ -26,11 +26,14 @@ function Test-DbaTempDbConfiguration {
 
 			To connect as a different Windows user, run PowerShell as that user.
 
+		.PARAMETER Detailed
+			Output all properties, will be depreciated in 1.0.0 release.
+
 		.PARAMETER EnableException
 			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
 			This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
 			Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-			
+
 		.NOTES
 			Tags: tempdb, configuration
 			Author: Michael Fal (@Mike_Fal), http://mikefal.net
@@ -59,9 +62,12 @@ function Test-DbaTempDbConfiguration {
 		[Alias("ServerInstance", "SqlServer")]
 		[DbaInstance[]]$SqlInstance,
 		[PSCredential]$SqlCredential,
+		[switch]$Detailed,
 		[switch][Alias('Silent')]$EnableException
 	)
 	begin {
+		Test-DbaDeprecation -DeprecatedOn 1.0.0 -Parameter Detailed
+
 		$result = @()
 	}
 	process {
