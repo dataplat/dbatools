9281156f7227a864bf5ecd5854228d38dda62fbe
Shawn Melton <wshawnmelton@gmail.com>
2017-11-29 08:20:49 -0600

    corrected function reference

--- tests/Find-DbaOrphanedFile.Tests.ps1
+++ tests/Find-DbaOrphanedFile.Tests.ps1
@@ -11,7 +11,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 		#>
 		$paramCount = 7
 		$defaultParamCount = 11
-		[object[]]$params = (Get-ChildItem function:\Verb-DbaXyz).Parameters.Keys
+		[object[]]$params = (Get-ChildItem function:\Find-DbaOrphanedFile).Parameters.Keys
 		$knownParameters = 'SqlInstance', 'SqlCredential', 'Path', 'FileType', 'LocalOnly', 'RemoteOnly', 'EnableException'
 		It "Should contain our specific parameters" {
 			( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
