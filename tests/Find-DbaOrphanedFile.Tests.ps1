1c9649d5888b9113f6afe3d31149c4bd3f690ee7
potatoqualitee <clemaire@gmail.com>
2017-11-30 19:09:27 +0100

    try to see if this is the problem (do *Orphaned*)

--- tests/Find-DbaOrphanedFile.Tests.ps1
+++ tests/Find-DbaOrphanedFile.Tests.ps1
@@ -27,7 +27,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
 			$server = Connect-DbaInstance -SqlInstance $script:instance2
 			$dbname = "dbatoolsci_findme"
 			Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
-			$null = $server.Query("CREATE DATABASE $dbname")
+			#$null = $server.Query("CREATE DATABASE $dbname")
 			$result = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname
 			if ($result.count -eq 0) {
 				it "has failed setup" {
