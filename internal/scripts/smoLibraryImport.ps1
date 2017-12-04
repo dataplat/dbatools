7c503b6cd4d1e0f59423ef8c58566bf862663a3a
potatoqualitee <clemaire@gmail.com>
2017-12-04 23:25:55 +0100

    added dll load in order

--- internal/scripts/smoLibraryImport.ps1
+++ internal/scripts/smoLibraryImport.ps1
@@ -151,14 +151,14 @@ $names = @(
 		'Microsoft.SqlServer.Dmf.Adapters',
 		'Microsoft.SqlServer.DmfSqlClrWrapper',
 		
-		'Microsoft.Data.Tools.Schema.Sql',
 		'Microsoft.Data.Tools.Utilities',
 		'Microsoft.SqlServer.TransactSql.ScriptDom',
-		'Microsoft.Data.Tools.Schema.Utilities.Sql',
 		'Microsoft.Data.Tools.Components',
 		'Microsoft.Data.Tools.Contracts',
 		'Microsoft.SqlServer.Dac.Extensions',
 		'Microsoft.SqlServer.Dac'
+		#'Microsoft.Data.Tools.Schema.Sql',
+		#'Microsoft.Data.Tools.Schema.Utilities.Sql'
 	)
 	#endregion Names
 	
