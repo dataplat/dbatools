88325f7f31d7828cd83c099b8554f2f1774a8a1c
Simone Bizzotto <niphlod@gmail.com>
2017-12-04 23:16:32 +0100

    internal, trailing spaces, folding, scriptanalyzer

--- internal/type-extensions.ps1
+++ internal/type-extensions.ps1
@@ -2,44 +2,44 @@
 if (-not ([Sqlcollaborative.Dbatools.dbaSystem.SystemHost]::ModuleImported)) {
 	# Implement query accelerator for the server object
 	Update-TypeData -TypeName Microsoft.SqlServer.Management.Smo.Server -MemberName Query -MemberType ScriptMethod -Value {
-		Param (
+		param (
 			$Query,
-			
+
 			$Database = "master",
-			
+
 			$AllTables = $false
 		)
-		
+
 		if ($AllTables) { ($this.Databases[$Database].ExecuteWithResults($Query)).Tables }
 		else { ($this.Databases[$Database].ExecuteWithResults($Query)).Tables[0] }
 	} -ErrorAction Ignore
-	
+
 	Update-TypeData -TypeName Microsoft.SqlServer.Management.Smo.Server -MemberName Invoke -MemberType ScriptMethod -Value {
-		Param (
+		param (
 			$Command,
-			
+
 			$Database = "master"
 		)
-		
+
 		$this.Databases[$Database].ExecuteNonQuery($Command)
 	} -ErrorAction Ignore
-	
+
 	Update-TypeData -TypeName Microsoft.SqlServer.Management.Smo.Database -MemberName Query -MemberType ScriptMethod -Value {
-		Param (
+		param (
 			$Query,
-			
+
 			$AllTables = $false
 		)
-		
+
 		if ($AllTables) { ($this.ExecuteWithResults($Query)).Tables }
 		else { ($this.ExecuteWithResults($Query)).Tables[0] }
 	} -ErrorAction Ignore
-	
+
 	Update-TypeData -TypeName Microsoft.SqlServer.Management.Smo.Database -MemberName Invoke -MemberType ScriptMethod -Value {
-		Param (
+		param (
 			$Command
 		)
-		
+
 		$this.ExecuteNonQuery($Command)
 	} -ErrorAction Ignore
 }
