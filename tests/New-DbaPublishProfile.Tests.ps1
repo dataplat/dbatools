e91d0656a94fbc45a83757575f31c5cc820c7252
potatoqualitee <clemaire@gmail.com>
2017-12-06 17:05:18 +0100

    updated to make appveyor tests more reliable

--- tests/New-DbaPublishProfile.Tests.ps1
+++ tests/New-DbaPublishProfile.Tests.ps1
@@ -7,7 +7,7 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 		$dbname = "dbatoolsci_publishprofile"
 		$server = Connect-DbaInstance -SqlInstance $script:instance1
 		$null = $server.Query("Create Database [$dbname]")
-		$db = Get-DbaDatabase -SqlInstance $server -Database $dbname
+		$db = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname
 		$null = $db.Query("CREATE TABLE dbo.example (id int); 
 			INSERT dbo.example
 			SELECT top 100 1 
@@ -15,11 +15,11 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	}
 	AfterAll {
 		Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
-		Remove-Item -Confirm:$false -Path $script:publishprofile.FileName -ErrorAction SilentlyContinue
 	}
 	
 	It "returns the right results" {
-		$script:publishprofile = New-DbaPublishProfile -SqlInstance $script:instance1 -Database $dbname
-		$script:publishprofile.FileName -match 'publish.xml' | Should Be $true
+		$publishprofile = New-DbaPublishProfile -SqlInstance $script:instance1 -Database $dbname
+		$publishprofile.FileName -match 'publish.xml' | Should Be $true
+		Remove-Item -Confirm:$false -Path $publishprofile.FileName -ErrorAction SilentlyContinue
 	}
 }
