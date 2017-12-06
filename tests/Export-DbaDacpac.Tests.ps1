e0e8d9a817bb231ab5d954394d9d0bc408f90cda
potatoqualitee <clemaire@gmail.com>
2017-12-06 08:35:15 +0100

    this command is causing problems

--- tests/Export-DbaDacpac.Tests.ps1
+++ tests/Export-DbaDacpac.Tests.ps1
@@ -6,18 +6,18 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	BeforeAll {
 		$dbname = "dbatoolsci_exportdacpac"
 		$server = Connect-DbaInstance -SqlInstance $script:instance1
-		$null = $server.Query("Create Database [$dbname]")
-		$db = Get-DbaDatabase -SqlInstance $server -Database $dbname
-		$null = $db.Query("CREATE TABLE dbo.example (id int); 
-			INSERT dbo.example
-			SELECT top 100 1 
-			FROM sys.objects")
 	}
 	AfterAll {
 		Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
 	}
 	Context "Testing the command" {
 		It -Skip "exports a dacpac" {
+			$null = $server.Query("Create Database [$dbname]")
+			$db = Get-DbaDatabase -SqlInstance $server -Database $dbname
+			$null = $db.Query("CREATE TABLE dbo.example (id int); 
+			INSERT dbo.example
+			SELECT top 100 1 
+			FROM sys.objects")
 			$results = Export-DbaDacpac -SqlInstance $script:instance1 -Database $dbname
 			$path = ($results).Path
 			Test-Path -Path $path | Should Be $true
