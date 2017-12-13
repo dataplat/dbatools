1e16f139f7415d41c82e81d2dd5a0fe5b0a6cf87
potatoqualitee <clemaire@gmail.com>
2017-12-13 22:54:27 +0100

    cert tests and fixes

--- tests/Backup-DbaDbCertificate.Tests.ps1
+++ tests/Backup-DbaDbCertificate.Tests.ps1
@@ -6,15 +6,14 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 	Context "Can create a database certificate" {
 		BeforeAll {
 			$null = New-DbaDatabaseMasterKey -SqlInstance $script:instance1 -Database tempdb -Password $(ConvertTo-SecureString -String "GoodPass1234!" -AsPlainText -Force) -Confirm:$false
-			$certificateName1 = "Cert_$(Get-random)"
 		}
 		AfterAll {
-			$null = Remove-DbaDbCertificate -SqlInstance $script:instance1 -Certificate $certificateName1 -Database tempdb -Confirm:$false
+			$null = Remove-DbaDbCertificate -SqlInstance $script:instance1 -Database tempdb -Confirm:$false
 			$null = Remove-DbaDatabaseMasterKey -SqlInstance $script:instance1 -Database tempdb -Confirm:$false
 		}
-		
-		$cert = New-DbaDbCertificate -SqlInstance $script:instance1 -Name $certificateName1 -Database tempdb
-		$results = Backup-DbaDbCertificate -SqlInstance $script:instance1 -Certificate $cert.Certificate -Database tempdb
+			
+		$cert = New-DbaDbCertificate -SqlInstance $script:instance1 -Database tempdb
+		$results = Backup-DbaDbCertificate -SqlInstance $script:instance1 -Certificate $cert.Name -Database tempdb
 		$null = Remove-Item -Path $results.ExportPath -ErrorAction SilentlyContinue -Confirm:$false
 		
 		It "backs up the db cert" {
