fe0bcc551c9ffa8ade7da95745198fc6b3c93d65
Stuart Moore <stuart@stuart-moore.com>
2017-12-15 18:19:33 +0000

    fixed test-dbalsnchain test

--- tests/Test-DbaLsnChain.Tests.ps1
+++ tests/Test-DbaLsnChain.Tests.ps1
@@ -6,7 +6,8 @@ Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 	InModuleScope dbatools {
 		Context "General Diff restore" {
 			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
-
+			$header | Add-Member -Type NoteProperty -Name FullName -Value 1
+			
 			$filteredFiles = $header | Select-DbaBackupInformation
 			It "Should Return 7" {
 				$FilteredFiles.count | should be 7
@@ -17,6 +18,8 @@ Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 			}
 			$Header = ConvertFrom-Json -InputObject (Get-Content $PSScriptRoot\..\tests\ObjectDefinitions\BackupRestore\RawInput\DiffRestore.json -raw)
 			$header = $Header | Where-Object { $_.BackupTypeDescription -ne 'Database Differential' } 
+			$header | Add-Member -Type NoteProperty -Name FullName -Value 1
+			
 			$FilteredFiles = $Header | Select-DbaBackupInformation
 			It "Should return true if we remove diff backup" {
 				$Output = Test-DbaLsnChain -WarningAction SilentlyContinue -FilteredRestoreFiles ($FilteredFiles | Where-Object { $_.BackupTypeDescription -ne 'Database Differential' })
