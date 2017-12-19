2fc5c5d5a1775a664d3d736a10ec2bfbd072ee0f
Stuart Moore <stuart@stuart-moore.com>
2017-12-19 15:18:18 +0000

    oops, broke import'

--- bin/library.ps1
+++ bin/library.ps1
@@ -1,5 +1,5 @@
 ﻿# Current library Version the module expects
-$currentLibraryVersion = New-Object System.Version(0, 9, 1, 38)
+$currentLibraryVersion = New-Object System.Version(0, 9, 1, 37)
 
 <#
 Library Versioning 101:
