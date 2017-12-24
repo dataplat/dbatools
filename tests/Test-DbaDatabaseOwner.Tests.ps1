4b3259bed4dc1182aff4c43f5e85a0aba7ec392e
Shawn Melton <wshawnmelton@gmail.com>
2017-12-24 00:46:46 -0600

    Few adjustments on here I'll explain in comment of PR

--- tests/Test-DbaDatabaseOwner.Tests.ps1
+++ tests/Test-DbaDatabaseOwner.Tests.ps1
@@ -24,7 +24,7 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 { Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' } | Should Not throw
             } #It
             It "Should not return if no wrong owner for default" {
@@ -46,13 +46,13 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 { Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' } | Should Not throw
             } #It
             It "Should return wrong owner information for one database with no owner specified" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
@@ -68,19 +68,19 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 $Result = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName'
-                $Result.SqlInstance | Should Be 'SQLServerName'
-                $Result.Database | Should Be 'db1';
-                $Result.DBState | Should Be 'Normal';
-                $Result.CurrentOwner | Should Be 'WrongOWner';
-                $Result.TargetOwner | Should Be 'sa';
-                $Result.OwnerMatch | Should Be $False
+                $Result[0].SqlInstance | Should Be 'SQLServerName'
+                $Result[0].Database | Should Be 'db1';
+                $Result[0].DBState | Should Be 'Normal';
+                $Result[0].CurrentOwner | Should Be 'WrongOWner';
+                $Result[0].TargetOwner | Should Be 'sa';
+                $Result[0].OwnerMatch | Should Be $False
             } # it
             It "Should return information for one database with correct owner with detail parameter" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
@@ -96,7 +96,7 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 $Result = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' -Detailed
                 $Result.SqlInstance | Should Be 'SQLServerName'
                 $Result.Database | Should Be 'db1';
@@ -108,7 +108,7 @@ Describe "$Name Tests"{
             It "Should return wrong owner information for one database with no owner specified and multiple databases" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
@@ -129,19 +129,19 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 $Result = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName'
-                $Result.SqlInstance | Should Be 'SQLServerName'
-                $Result.Database | Should Be 'db1';
-                $Result.DBState | Should Be 'Normal';
-                $Result.CurrentOwner | Should Be 'WrongOWner';
-                $Result.TargetOwner | Should Be 'sa';
-                $Result.OwnerMatch | Should Be $False
+                $Result[0].SqlInstance | Should Be 'SQLServerName'
+                $Result[0].Database | Should Be 'db1';
+                $Result[0].DBState | Should Be 'Normal';
+                $Result[0].CurrentOwner | Should Be 'WrongOWner';
+                $Result[0].TargetOwner | Should Be 'sa';
+                $Result[0].OwnerMatch | Should Be $False
             } # it
             It "Should return wrong owner information for two databases with no owner specified and multiple databases" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
@@ -162,7 +162,7 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 $Result = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName'
                 $Result[0].SqlInstance | Should Be 'SQLServerName'
                 $Result[1].SqlInstance | Should Be 'SQLServerName'
@@ -177,20 +177,21 @@ Describe "$Name Tests"{
                 $Result[0].OwnerMatch | Should Be $False
                 $Result[1].OwnerMatch | Should Be $False
             } # it
-            It "Should notify if Target Login does not exist on Server" {
+
+            It "Should call Stop-Function one time if Target Login does not exist on Server" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
                                 Status = 'Normal';
-                                Owner = 'WrongOWner'
+                                Owner = 'WrongOwner'
                             }
                             @{
                                 Name = 'db2';
                                 Status = 'Normal';
-                                Owner = 'WrongOWner'
+                                Owner = 'WrongOwner'
                             }
                         ); #databases
                         Logins = [object]@(
@@ -201,13 +202,20 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
-                { Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' -TargetLogin WrongLogin } | Should Throw 'Invalid login:'
+                Mock Stop-Function {}
+
+                $null = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' -TargetLogin 'WrongLogin' -EnableException
+                $assertMockParams = @{
+                    'CommandName' = 'Stop-Function'
+                    'Times' = 1
+                    'Exactly' = $true
+                }
+                Assert-MockCalled @assertMockParams
             } # it
             It "Returns all information with detailed for correct and incorrect owner" {
                 Mock Connect-SQLInstance -MockWith {
                     [object]@{
-                        Name = 'SQLServerName';
+                        DomainInstanceName = 'SQLServerName';
                         Databases = [object]@(
                             @{
                                 Name = 'db1';
@@ -228,7 +236,7 @@ Describe "$Name Tests"{
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                
+
                 $Result = Test-DbaDatabaseOwner -SqlInstance 'SQLServerName' -Detailed
                 $Result[0].SqlInstance | Should Be 'SQLServerName'
                 $Result[1].SqlInstance | Should Be 'SQLServerName'
