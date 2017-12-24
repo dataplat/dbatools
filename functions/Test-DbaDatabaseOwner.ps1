65733850e13387668304579ea149661c43e4e502
Shawn Melton <wshawnmelton@gmail.com>
2017-12-24 00:46:26 -0600

    Test bound need -Not added, not match instead of contains

--- functions/Test-DbaDatabaseOwner.ps1
+++ functions/Test-DbaDatabaseOwner.ps1
@@ -13,15 +13,15 @@ function Test-DbaDatabaseOwner {
 			Best Practice reference: http://weblogs.sqlteam.com/dang/archive/2008/01/13/Database-Owner-Troubles.aspx
 
 		.NOTES
-			Tags: 
+			Tags:
 			Author: Michael Fal (@Mike_Fal), http://mikefal.net
 			Website: https://dbatools.io
 			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
-		.PARAMETER SqlInstance 
+		.PARAMETER SqlInstance
 			Specifies the SQL Server instance(s) to scan.
-			
+
 		.PARAMETER SqlCredential
 			Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
 
@@ -33,22 +33,22 @@ function Test-DbaDatabaseOwner {
 
 		.PARAMETER Database
 			Specifies the database(s) to process. Options for this list are auto-populated from the server. If unspecified, all databases will be processed.
-		
+
 		.PARAMETER ExcludeDatabase
 			Specifies the database(s) to exclude from processing. Options for this list are auto-populated from the server.
-		
+
 		.PARAMETER TargetLogin
 			Specifies the login that you wish check for ownership. This defaults to 'sa' or the sysadmin name if sa was renamed. This must be a valid security principal which exists on the target server.
 
 		.PARAMETER Detailed
 			Deprecated
-	
+
 		.PARAMETER EnableException
 			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
 			This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
 			Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-			
-	
+
+
 		.LINK
 			https://dbatools.io/Test-DbaDatabaseOwner
 
@@ -75,7 +75,7 @@ function Test-DbaDatabaseOwner {
 		[Switch]$Detailed,
 		[switch]$EnableException
 	)
-	
+
 	begin {
 		Test-DbaDeprecation -DeprecatedOn "1.0.0" -Parameter "Detailed"
 	}
@@ -88,36 +88,36 @@ function Test-DbaDatabaseOwner {
 			catch {
 				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
 			}
-			
+
 			# dynamic sa name for orgs who have changed their sa name
-			if (Test-Bound -ParameterName TargetLogin) {
+			if (Test-Bound -ParameterName TargetLogin -Not) {
 				$TargetLogin = ($server.logins | Where-Object { $_.id -eq 1 }).Name
 			}
-			
+
 			#Validate login
-			if (($server.Logins.Name) -notcontains $TargetLogin) {
+			if (($server.Logins.Name) -notmatch $TargetLogin) {
 				Stop-Function -Message "$TargetLogin is not a valid login on $instance. Moving on." -Target $instance -Continue
 			}
 		}
 		#use online/available dbs
 		$dbs = $server.Databases
-		
+
 		#filter database collection based on parameters
 		if ($Database) {
 			$dbs = $dbs | Where-Object { $Database -contains $_.Name }
 		}
-		
+
 		if ($ExcludeDatabase) {
 			$dbs = $dbs | Where-Object Name -NotIn $ExcludeDatabase
 		}
-		
+
 		#for each database, create custom object for return set.
 		foreach ($db in $dbs) {
-			
+
 			if ($db.IsAccessible -eq $false) {
 				Stop-Function -Message "The database $db is not accessible. Skipping database." -Continue -Target $db
 			}
-			
+
 			Write-Message -Level Verbose -Message "Checking $db"
 			[pscustomobject]@{
 				ComputerName	  = $server.NetName
