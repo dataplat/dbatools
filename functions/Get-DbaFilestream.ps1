eadd924d15341d69efa9ad90854c62bb60394106
Shawn Melton <wshawnmelton@gmail.com>
2017-12-26 13:30:50 -0600

    Add missing parameter from help

--- functions/Get-DbaFilestream.ps1
+++ functions/Get-DbaFilestream.ps1
@@ -18,7 +18,10 @@ function Get-DbaFilestream {
 
             To connect as a different Windows user, run PowerShell as that user.
 
-        .PARAMETER EnableException
+		.PARAMETER Credential
+			Credential object used to connect to the computer as a different user.
+
+		.PARAMETER EnableException
             By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
             This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
