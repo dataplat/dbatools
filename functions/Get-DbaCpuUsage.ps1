30520249c7156db2fa630aabebc9b610d7b11cd5
potatoqualitee <clemaire@gmail.com>
2018-01-01 14:51:23 +0100

    added help for Threshold

--- functions/Get-DbaCpuUsage.ps1
+++ functions/Get-DbaCpuUsage.ps1
@@ -24,7 +24,10 @@
 
 	.PARAMETER Credential
 		Allows you to login to the Windows Server using alternative credentials.
-		
+	
+	.PARAMETER Threshold
+		CPU threshold.
+	
 	.PARAMETER EnableException
 		By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
 		This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
