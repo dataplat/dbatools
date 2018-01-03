951120fed40eaea575bd07ff9046047d095335ce
Shawn Melton <wshawnmelton@gmail.com>
2018-01-03 12:29:10 -0600

    Additional columns for driveletter and physical name

--- functions/Find-DbaDatabaseGrowthEvent.ps1
+++ functions/Find-DbaDatabaseGrowthEvent.ps1
@@ -158,6 +158,8 @@ function Find-DbaDatabaseGrowthEvent {
                                 CONVERT(INT, [EventClass]) AS EventClass,
                             [DatabaseName],
                             [Filename],
+                            mf.physical_name AS PhysicalName,
+                            LEFT(mf.physical_name,1) AS DriveLetter,
                             CONVERT(INT,(Duration/1000)) AS Duration,
                             DATEADD (MINUTE, DATEDIFF(MINUTE, GETDATE(), GETUTCDATE()), [StartTime]) AS StartTimeUTC,  -- Convert to UTC time
                             DATEADD (MINUTE, DATEDIFF(MINUTE, GETDATE(), GETUTCDATE()), [EndTime]) AS EndTimeUTC,  -- Convert to UTC time
@@ -168,7 +170,8 @@ function Find-DbaDatabaseGrowthEvent {
                             HostName,
                             SessionLoginName,
                             SPID
-                        FROM::fn_trace_gettable( @base_tracefilename, DEFAULT )
+                        FROM::fn_trace_gettable( @base_tracefilename, DEFAULT ) AS t
+                        INNER JOIN sys.master_files AS mf ON mf.name = t.Filename
                         WHERE
                             [EventClass] IN ($eventClassFilter)
                             AND [ServerName] = @@SERVERNAME
@@ -185,10 +188,18 @@ function Find-DbaDatabaseGrowthEvent {
                         0 AS [EventClass],
                         0 [DatabaseName],
                         0 AS [Filename],
+                        0 AS [PhysicalName],
+                        0 AS [DriveLetter],
                         0 AS [Duration],
+                        0 AS [StartTimeUTC],
+                        0 AS [EndTimeUTC],
                         0 AS [StartTime],
                         0 AS [EndTime],
                         0 AS ChangeInSize
+                        0 AS [ApplicationName],
+                        0 AS [HostName],
+                        0 AS [SessionLoginName]
+                        0 AS [SPID]
             END	TRY
             BEGIN CATCH
                 SELECT
@@ -200,10 +211,18 @@ function Find-DbaDatabaseGrowthEvent {
                     ERROR_NUMBER() AS [EventClass],
                     ERROR_SEVERITY() AS [DatabaseName],
                     ERROR_STATE() AS [Filename],
-                    ERROR_MESSAGE() AS [Duration],
+                    ERROR_MESSAGE() AS [PhysicalName],
+                    1 AS [DriveLetter],
+                    1 AS [Duration],
+                    1 AS [StartTimeUTC],
+                    1 AS [EndTimeUTC],
                     1 AS [StartTime],
                     1 AS [EndTime],
                     1 AS [ChangeInSize]
+                    1 AS [ApplicationName],
+                    1 AS [HostName],
+                    1 AS [SessionLoginName]
+                    1 AS [SPID]
             END CATCH"
     }
     process {
