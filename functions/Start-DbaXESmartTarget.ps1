2b596e94a9838dccc27ac85b8baa444a6403a3bc
potatoqualitee <clemaire@gmail.com>
2018-01-05 15:56:07 +0100

    added job support

--- functions/Start-DbaXESmartTarget.ps1
+++ functions/Start-DbaXESmartTarget.ps1
@@ -31,6 +31,9 @@
     .PARAMETER Template
     Path to our built in templates
 
+    .PARAMETER NotAsJob
+    By default, this this command will start a job that runs in the background. Use NotAsJob to output to screen indefinitely.
+    
     .PARAMETER EnableException
     By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
     This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
@@ -87,49 +90,80 @@
         [switch]$FailOnProcessingError,
         [object[]]$Responder,
         [string[]]$Template,
+        [switch]$NotAsJob,
         [switch]$EnableException
     )
     begin {
-        try {
-            Add-Type -Path "$script:PSModuleRoot\bin\XESmartTarget\XESmartTarget.Core.dll" -ErrorAction Stop
-        }
-        catch {
-            Stop-Function -Message "Could not load XESmartTarget.Core.dll" -ErrorRecord $_ -Target "XESmartTarget"
-            return
+        function Start-SmartFunction {
+            [CmdletBinding()]
+            param (
+                [parameter(Mandatory, ValueFromPipeline)]
+                [Alias("ServerInstance", "SqlServer")]
+                [DbaInstanceParameter[]]$SqlInstance,
+                [PSCredential]$SqlCredential,
+                [string]$Database,
+                [parameter(Mandatory)]
+                [string]$Session,
+                [switch]$FailOnProcessingError,
+                [object[]]$Responder,
+                [string[]]$Template,
+                [switch]$NotAsJob,
+                [switch]$EnableException
+            )
+            begin {
+                try {
+                    Add-Type -Path "$script:PSModuleRoot\bin\XESmartTarget\XESmartTarget.Core.dll" -ErrorAction Stop
+                }
+                catch {
+                    Stop-Function -Message "Could not load XESmartTarget.Core.dll" -ErrorRecord $_ -Target "XESmartTarget"
+                    return
+                }
+            }
+            process {
+                if (Test-FunctionInterrupt) { return }
+                
+                foreach ($instance in $SqlInstance) {
+                    try {
+                        Write-Message -Level Verbose -Message "Connecting to $instance"
+                        $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 11
+                    }
+                    catch {
+                        Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+                    }
+                    
+                    $target = New-Object -TypeName XESmartTarget.Core.Target
+                    $target.ServerName = $instance
+                    $target.SessionName = $Session
+                    $target.FailOnProcessingError = $FailOnProcessingError
+                    
+                    if ($SqlCredential) {
+                        $target.UserName = $SqlCredential.UserName
+                        $target.Password = $SqlCredential.GetNetworkCredential().Password
+                    }
+                    
+                    foreach ($response in $Responder) {
+                        $target.Responses.Add($response)
+                    }
+                    
+                    try {
+                        $target.Start()
+                    }
+                    catch {
+                        Stop-Function -Message "Failure" -ErrorRecord $_ -Target "XESmartTarget" -Continue
+                    }
+                }
+            }
         }
     }
     process {
-        if (Test-FunctionInterrupt) { return }
-
-        foreach ($instance in $SqlInstance) {
-            try {
-                Write-Message -Level Verbose -Message "Connecting to $instance"
-                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 11
-            }
-            catch {
-                Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
-            }
-
-            $target = New-Object -TypeName XESmartTarget.Core.Target
-            $target.ServerName = $instance
-            $target.SessionName = $Session
-            $target.FailOnProcessingError = $FailOnProcessingError
-
-            if ($SqlCredential) {
-                $target.UserName = $SqlCredential.UserName
-                $target.Password = $SqlCredential.GetNetworkCredential().Password
-            }
-
-            foreach ($response in $Responder) {
-                $target.Responses.Add($response)
-            }
-
-            try {
-                $target.Start()
-            }
-            catch {
-                Stop-Function -Message "Failure" -ErrorRecord $_ -Target "XESmartTarget" -Continue
+        if ($NotAsJob) {
+            Start-SmartFunction @PSBoundParameters
+        }
+        else {
+            $date = (Get-Date -UFormat "%m%d%Y%H%M%S")
+            Start-Job -Name "XESmartTarget-$session-$date" -ArgumentList $PSBoundParameters -ScriptBlock {
+                Start-DbaXESmartTarget -SqlInstance $args.SqlInstance.InputObject -SqlCredential $args.SqlCredential -Database $args.Database -Session $args.Session -NotAsJob
             }
         }
-    }
+    }   
 }
