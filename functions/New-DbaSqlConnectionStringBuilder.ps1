e2f2574df67d3449b843fed763b28f5e38e82d41
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-06 05:08:57 +0100

    Replaced tab with whitespaces :(

--- functions/New-DbaSqlConnectionStringBuilder.ps1
+++ functions/New-DbaSqlConnectionStringBuilder.ps1
@@ -60,7 +60,7 @@ function New-DbaSqlConnectionStringBuilder {
     #>
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "")]
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingUserNameAndPassWordParams", "")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingUserNameAndPassWordParams", "")]
     param (
         [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
         [string[]]$ConnectionString = "",
