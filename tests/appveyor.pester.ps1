fecee6314116ee32cb9a6908cc3fde52d9258965
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-09 01:05:56 +0100

    Redesigned import sequence
    First fix attempt denied access to internal functions.
    Shifted to explicit update type-data, which is now done as part of manifest.

--- tests/appveyor.pester.ps1
+++ tests/appveyor.pester.ps1
@@ -50,7 +50,9 @@ Remove-Module dbatools -ErrorAction Ignore
 #imports the psm1 to be able to use internal functions in tests
 Import-Module "$ModuleBase\dbatools.psm1"
 #imports the module making sure DLL is loaded ok
-Import-Module "$ModuleBase\dbatools.psd1" -Force
+Import-Module "$ModuleBase\dbatools.psd1"
+
+Update-TypeData -AppendPath "$ModuleBase\xml\dbatools.types.ps1xml"
 Start-Sleep 5
 
 function Get-CoverageIndications($Path, $ModuleBase) {
