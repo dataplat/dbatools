0ad099b6cdb4fa2af822b5b533deeea130719587
potatoqualitee <clemaire@gmail.com>
2018-01-10 15:10:32 +0100

    trim trailing space

--- functions/Get-DbaXESessionTemplate.ps1
+++ functions/Get-DbaXESessionTemplate.ps1
@@ -2,13 +2,13 @@
  <#
     .SYNOPSIS
     Parses Extended Event XML templates. Defaults to parsing templates in our template repository (\bin\xetemplates\)
-    
+
     .DESCRIPTION
     Parses Extended Event XML templates. Defaults to parsing templates in our template repository (\bin\xetemplates\)
-    
+
     .PARAMETER Path
     The path to the template directory. Defaults to our template repository (\bin\xetemplates\)
-    
+
     .PARAMETER EnableException
     By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
     This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
@@ -23,20 +23,20 @@
     https://dbatools.io/Get-DbaXESessionTemplate
 
     .EXAMPLE
-    Get-DbaXESessionTemplate 
-    
+    Get-DbaXESessionTemplate
+
     Returns information about all the templates in the local dbatools repository
-    
+
     .EXAMPLE
     Get-DbaXESessionTemplate | Out-GridView -PassThru | Import-DbaXESessionTemplate -SqlInstance sql2017 | Start-DbaXESession
-    
+
     Allows you to select a Session template then import to an instance named
-    
+
     .EXAMPLE
     Get-DbaXESessionTemplate -Path "$home\Documents\SQL Server Management Studio\Templates\XEventTemplates"
-    
+
     Returns information about all the templates in your local XEventTemplates repository
-    
+
 #>
     [CmdletBinding()]
     param (
@@ -53,7 +53,7 @@
                 catch {
                     Stop-Function -Message "Failure" -ErrorRecord $_ -Target $file -Continue
                 }
-                
+
                 foreach ($session in $xml.event_sessions) {
                     [pscustomobject]@{
                         Name     = $session.event_session.name
