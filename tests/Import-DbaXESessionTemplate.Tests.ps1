363bdd29f4193673c07727b96b4eb9c43002e48c
potatoqualitee <clemaire@gmail.com>
2018-01-10 19:59:47 +0100

    added test template test

---
+++ tests/Import-DbaXESessionTemplate.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    AfterAll {
+        $null = Get-DbaXESession -SqlInstance $script:instance2 -Session db_ola_health | Stop-DbaXESession | Remove-DbaXESession
+    }
+    Context "Test Importing Session Template" {
+        $results = Import-DbaXESessionTemplate -SqlInstance $script:instance2 -Template db_ola_health | Start-DbaXESession
+        It "session imports and is running" {
+            $results.Name | Should Be "db_ola_health"
+            $results.Status | Should Be "Running"
+        }
+    }
+}
