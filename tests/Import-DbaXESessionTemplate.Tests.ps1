ffdbeac5ad297263a51a750beab263837b822f7e
potatoqualitee <clemaire@gmail.com>
2018-01-11 22:22:57 +0100

    made smaller test

--- tests/Import-DbaXESessionTemplate.Tests.ps1
+++ tests/Import-DbaXESessionTemplate.Tests.ps1
@@ -4,12 +4,12 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     AfterAll {
-        $null = Get-DbaXESession -SqlInstance $script:instance2 -Session 'Profiler TSQL Duration' | Stop-DbaXESession | Remove-DbaXESession
+        $null = Get-DbaXESession -SqlInstance $script:instance2 -Session 'Function Executions' | Stop-DbaXESession | Remove-DbaXESession
     }
     Context "Test Importing Session Template" {
-        $results = Import-DbaXESessionTemplate -SqlInstance $script:instance2 -Template 'Profiler TSQL Duration' | Start-DbaXESession
+        $results = Import-DbaXESessionTemplate -SqlInstance $script:instance2 -Template 'Function Executions' | Start-DbaXESession
         It "session imports and is running" {
-            $results.Name | Should Be "Profiler TSQL Duration"
+            $results.Name | Should Be "Function Executions"
             $results.Status | Should Be "Running"
         }
     }
