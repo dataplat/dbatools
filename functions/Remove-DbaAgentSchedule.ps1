df6289ba53b9a83ccbb9a5d3a83508fd2b3e5d2c
Sander Stad <sander@sqlstad.nl>
2018-01-12 09:38:02 +0100

    Changed tabs to spaces

--- functions/Remove-DbaAgentSchedule.ps1
+++ functions/Remove-DbaAgentSchedule.ps1
@@ -25,9 +25,9 @@ Shows what would happen if the command were to run. No actions are actually perf
 Prompts you for confirmation before executing any changing operations within the command.
 
 .PARAMETER EnableException
-		By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-		This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-		Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
 .PARAMETER Force
 The force parameter will ignore some errors in the parameters and assume defaults.
