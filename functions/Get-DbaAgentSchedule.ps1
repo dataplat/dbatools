cff4ac28d8220970a96bb392f45c2e0328008535
Sander Stad <sander@sqlstad.nl>
2018-01-12 09:23:58 +0100

    Added pipeline support for schedule
    Added schedule parameter to Get-DbaAgentSchedule to make it possible to
    filter schedules
    Added pipeline support for Remove-DbaAgentSchedule

--- functions/Get-DbaAgentSchedule.ps1
+++ functions/Get-DbaAgentSchedule.ps1
@@ -1,56 +1,57 @@
 function Get-DbaAgentSchedule {
     <#
-        .SYNOPSIS
-            Returns all SQL Agent Shared Schedules on a SQL Server Agent.
+		.SYNOPSIS
+			Returns all SQL Agent Shared Schedules on a SQL Server Agent.
 
-        .DESCRIPTION
-            This function returns SQL Agent Shared Schedules.
+		.DESCRIPTION
+			This function returns SQL Agent Shared Schedules.
 
-        .PARAMETER SqlInstance
-            SqlInstance name or SMO object representing the SQL Server to connect to.
-            This can be a collection and receive pipeline input.
+		.PARAMETER SqlInstance
+			SqlInstance name or SMO object representing the SQL Server to connect to.
+			This can be a collection and receive pipeline input.
 
-        .PARAMETER SqlCredential
-            PSCredential object to connect as. If not specified, current Windows login will be used.
+		.PARAMETER SqlCredential
+			PSCredential object to connect as. If not specified, current Windows login will be used.
 
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+		.PARAMETER EnableException
+			By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+			This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+			Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-        .NOTES
-            Tags: Agent, Schedule
-            Author: Chris McKeown (@devopsfu), http://www.devopsfu.com
+		.NOTES
+			Tags: Agent, Schedule
+			Author: Chris McKeown (@devopsfu), http://www.devopsfu.com
 
-            Website: https://dbatools.io
-            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-            License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+			Website: https://dbatools.io
+			Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+			License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 
-        .LINK
-            https://dbatools.io/Get-DbaAgentSchedule
+		.LINK
+			https://dbatools.io/Get-DbaAgentSchedule
 
-        .EXAMPLE
-            Get-DbaAgentSchedule -SqlInstance localhost
+		.EXAMPLE
+			Get-DbaAgentSchedule -SqlInstance localhost
 
-            Returns all SQL Agent Shared Schedules on the local default SQL Server instance
+			Returns all SQL Agent Shared Schedules on the local default SQL Server instance
 
-        .EXAMPLE
-            Get-DbaAgentSchedule -SqlInstance localhost, sql2016
+		.EXAMPLE
+			Get-DbaAgentSchedule -SqlInstance localhost, sql2016
 
-            Returns all SQL Agent Shared Schedules for the local and sql2016 SQL Server instances
-    #>
+			Returns all SQL Agent Shared Schedules for the local and sql2016 SQL Server instances
+	#>
     [CmdletBinding()]
     param (
         [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
         [Alias("ServerInstance", "Instance", "SqlServer")]
         [DbaInstanceParameter[]]$SqlInstance,
+        [Alias("Schedules")]
+        [object[]]$Schedule,
         [PSCredential]$SqlCredential,
         [switch][Alias('Silent')]$EnableException
     )
 
     begin {
         function Get-ScheduleDescription {
-            [CmdletBinding()]
             param (
                 [Parameter(Mandatory = $true)]
                 [ValidateNotNullOrEmpty()]
@@ -237,18 +238,27 @@ function Get-DbaAgentSchedule {
                 Stop-Function -Message "$($server.Edition) does not support SQL Server Agent. Skipping $server." -Continue
             }
 
-            $defaults = "ComputerName", "InstanceName", "SqlInstance", "Name as ScheduleName", "ActiveEndDate", "ActiveEndTimeOfDay", "ActiveStartDate", "ActiveStartTimeOfDay", "DateCreated", "FrequencyInterval", "FrequencyRecurrenceFactor", "FrequencyRelativeIntervals", "FrequencySubDayInterval", "FrequencySubDayTypes", "FrequencyTypes", "IsEnabled", "JobCount", "Description"
-
-            foreach ($schedule in $server.JobServer.SharedSchedules) {
-                $description = Get-ScheduleDescription -Schedule $schedule
-
-                Add-Member -Force -InputObject $schedule -MemberType NoteProperty ComputerName -value $server.NetName
-                Add-Member -Force -InputObject $schedule -MemberType NoteProperty InstanceName -value $server.ServiceName
-                Add-Member -Force -InputObject $schedule -MemberType NoteProperty SqlInstance -value $server.DomainInstanceName
-                Add-Member -Force -InputObject $schedule -MemberType NoteProperty Description -Value $description
-
-                Select-DefaultView -InputObject $schedule -Property $defaults
+            if ($Schedule) {
+                $scheduleCollection = $server.JobServer.SharedSchedules | Where-Object { $_.Name -in $Schedule }
             }
+            else {
+                $scheduleCollection = $server.JobServer.SharedSchedules
+            }
+
         }
+
+        $defaults = "ComputerName", "InstanceName", "SqlInstance", "Name as ScheduleName", "ActiveEndDate", "ActiveEndTimeOfDay", "ActiveStartDate", "ActiveStartTimeOfDay", "DateCreated", "FrequencyInterval", "FrequencyRecurrenceFactor", "FrequencyRelativeIntervals", "FrequencySubDayInterval", "FrequencySubDayTypes", "FrequencyTypes", "IsEnabled", "JobCount", "Description"
+
+        foreach ($schedule in $scheduleCollection) {
+            $description = Get-ScheduleDescription -Schedule $schedule
+
+            Add-Member -Force -InputObject $schedule -MemberType NoteProperty ComputerName -value $server.NetName
+            Add-Member -Force -InputObject $schedule -MemberType NoteProperty InstanceName -value $server.ServiceName
+            Add-Member -Force -InputObject $schedule -MemberType NoteProperty SqlInstance -value $server.DomainInstanceName
+            Add-Member -Force -InputObject $schedule -MemberType NoteProperty Description -Value $description
+
+            Select-DefaultView -InputObject $schedule -Property $defaults
+        }
+
     }
 }
