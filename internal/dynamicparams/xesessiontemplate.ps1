c4025309183290762c4797eacc4c66ec6bd0778b
potatoqualitee <clemaire@gmail.com>
2018-01-12 15:20:01 +0100

    fixed output to add single quotes

--- internal/dynamicparams/xesessiontemplate.ps1
+++ internal/dynamicparams/xesessiontemplate.ps1
@@ -13,8 +13,11 @@ $ScriptBlock = {
         $commandAst,
         $fakeBoundParameter
     )
-
-    (Get-ChildItem "$script:PSModuleRoot\bin\xetemplates\*.xml").BaseName
+    
+    $files = (Get-ChildItem "$script:PSModuleRoot\bin\xetemplates\*.xml").BaseName
+    foreach ($file in $files) {
+        "'$file'"    
+    }
 }
 
 Register-DbaTeppScriptblock -ScriptBlock $ScriptBlock -Name xesessiontemplate
