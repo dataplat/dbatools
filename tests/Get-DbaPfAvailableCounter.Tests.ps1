fa9299b64e01df273b8c35673b2f88e6b5eea830
potatoqualitee <clemaire@gmail.com>
2018-01-17 10:11:12 +0100

    added Get-DbaPfAvailableCounter

---
+++ tests/Get-DbaPfAvailableCounter.Tests.ps1
@@ -0,0 +1,24 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    BeforeEach {
+        $null = Get-DbaPfDataCollectorSetTemplate -Template 'Long Running Queries' | Import-DbaPfDataCollectorSetTemplate
+    }
+    AfterAll {
+        $null = Get-DbaPfDataCollectorSet -CollectorSet 'Long Running Queries' | Remove-DbaPfDataCollectorSet -Confirm:$false
+    }
+    Context "Verifying command returns all the required results" {
+        It "returns the correct values" {
+            $results = Get-DbaPfAvailableCounter
+            $results.Count -gt 1000 | Should Be $true
+        }
+        It "returns are pipable into Add-DbaPfDataCollectorCounter" {
+            $results = Get-DbaPfAvailableCounter -Pattern *sql* | Select-Object -First 3 | Add-DbaPfDataCollectorCounter -CollectorSet 'Long Running Queries' -Collector DataCollector01 -WarningAction SilentlyContinue
+            foreach ($result in $results) {
+                $result.Name -match "sql" | Should Be $true
+            }
+        }
+    }
+}
