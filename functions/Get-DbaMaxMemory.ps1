a8daeb9bc56b7dadbd17a9a2085d07f843e92414
Shawn Melton <wshawnmelton@gmail.com>
2018-01-19 10:03:56 -0600

    convert to spaces

--- functions/Get-DbaMaxMemory.ps1
+++ functions/Get-DbaMaxMemory.ps1
@@ -1,6 +1,6 @@
 #ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
 function Get-DbaMaxMemory {
-	<#
+    <#
         .SYNOPSIS
             Gets the 'Max Server Memory' configuration setting and the memory of the server.  Works on SQL Server 2000-2014.
 
@@ -41,40 +41,40 @@ function Get-DbaMaxMemory {
 
             Find all servers in Server Central Management Server that have 'Max Server Memory' set to higher than the total memory of the server (think 2147483647)
     #>
-	[CmdletBinding()]
-	param (
-		[parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
-		[Alias("ServerInstance", "SqlServer", "SqlServers")]
-		[DbaInstanceParameter[]]$SqlInstance,
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
+        [Alias("ServerInstance", "SqlServer", "SqlServers")]
+        [DbaInstanceParameter[]]$SqlInstance,
         [PSCredential]$SqlCredential,
         [Alias('Silent')]
-		[switch]$EnableException
-	)
+        [switch]$EnableException
+    )
 
-	process {
-		foreach ($instance in $SqlInstance) {
-			try {
-				Write-Message -Level Verbose -Message "Connecting to $instance"
-				$server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
-			}
-			catch {
-				Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
-			}
+    process {
+        foreach ($instance in $SqlInstance) {
+            try {
+                Write-Message -Level Verbose -Message "Connecting to $instance"
+                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential
+            }
+            catch {
+                Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+            }
 
-			$totalMemory = $server.PhysicalMemory
+            $totalMemory = $server.PhysicalMemory
 
-			# Some servers under-report by 1MB.
-			if (($totalMemory % 1024) -ne 0) {
+            # Some servers under-report by 1MB.
+            if (($totalMemory % 1024) -ne 0) {
                 $totalMemory = $totalMemory + 1
             }
 
-			[pscustomobject]@{
-				ComputerName = $server.NetName
-				InstanceName = $server.ServiceName
-				SqlInstance  = $server.DomainInstanceName
-				TotalMB      = [int]$totalMemory
-				SqlMaxMB     = [int]$server.Configuration.MaxServerMemory.ConfigValue
-			} | Select-DefaultView -ExcludeProperty Server
-		}
-	}
+            [pscustomobject]@{
+                ComputerName = $server.NetName
+                InstanceName = $server.ServiceName
+                SqlInstance  = $server.DomainInstanceName
+                TotalMB      = [int]$totalMemory
+                SqlMaxMB     = [int]$server.Configuration.MaxServerMemory.ConfigValue
+            } | Select-DefaultView -ExcludeProperty Server
+        }
+    }
 }
