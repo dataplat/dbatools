6286f683f1eab4f92b68b7af6472639348d8debd
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-21 12:13:24 +0100

    Reworked to meet standards

--- bin/library.ps1
+++ bin/library.ps1
@@ -1,5 +1,5 @@
 ﻿# Current library Version the module expects
-$currentLibraryVersion = New-Object System.Version(0, 9, 1, 39)
+$currentLibraryVersion = New-Object System.Version(0, 9, 1, 40)
 
 <#
 Library Versioning 101:
