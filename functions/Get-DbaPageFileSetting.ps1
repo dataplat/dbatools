6eea778a7be06351adb934b1b8523bb3b7601974
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-21 13:16:49 +0100

    reordered notes and variable casing

--- functions/Get-DbaPageFileSetting.ps1
+++ functions/Get-DbaPageFileSetting.ps1
@@ -21,6 +21,14 @@ function Get-DbaPageFileSetting {
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
     
+    .NOTES
+        Tags: CIM
+        Author: Klaas Vandenberghe ( @PowerDBAKlaas )
+        
+        dbatools PowerShell module (https://dbatools.io)
+        Copyright (C) 2016 Chrissy LeMaire
+        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+    
     .EXAMPLE
         Get-DbaPageFileSetting -ComputerName ServerA,ServerB
         
@@ -30,14 +38,6 @@ function Get-DbaPageFileSetting {
         'ServerA' | Get-DbaPageFileSetting
         
         Returns a custom object displaying ComputerName, AutoPageFile, FileName, Status, LastModified, LastAccessed, AllocatedBaseSize, InitialSize, MaximumSize, PeakUsage, CurrentUsage  for ServerA
-    
-    .NOTES
-        Tags: CIM
-        Author: Klaas Vandenberghe ( @PowerDBAKlaas )
-        
-        dbatools PowerShell module (https://dbatools.io)
-        Copyright (C) 2016 Chrissy LeMaire
-        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
     
     .LINK
         https://dbatools.io/Get-DbaPageFileSetting
@@ -52,8 +52,8 @@ function Get-DbaPageFileSetting {
         [switch][Alias('Silent')]$EnableException
     )
     process {
-        foreach ($Computer in $ComputerName) {
-            Write-Message -Level VeryVerbose -Message "Connecting to $($Computer.ComputerName)" -Target $Computer
+        foreach ($computer in $ComputerName) {
+            Write-Message -Level VeryVerbose -Message "Connecting to $($computer.ComputerName)" -Target $computer
             $splatDbaCmObject = @{
                 ComputerName   = $computer
                 EnableException = $true
@@ -69,13 +69,13 @@ function Get-DbaPageFileSetting {
                 }
             }
             catch {
-                Stop-Function -Message "Failed to retrieve information from $($Computer.ComputerName)" -ErrorRecord $_ -Target $Computer -Continue
+                Stop-Function -Message "Failed to retrieve information from $($computer.ComputerName)" -ErrorRecord $_ -Target $computer -Continue
             }
             
             if (-not $CompSys.automaticmanagedpagefile) {
                 # pagefile is not automatic managed, so return settings
                 New-Object Sqlcollaborative.Dbatools.Computer.PageFileSetting -Property @{
-                    ComputerName         = $Computer.ComputerName
+                    ComputerName         = $computer.ComputerName
                     AutoPageFile         = $CompSys.automaticmanagedpagefile
                     FileName             = $PF.name # deprecated !
                     Status               = $PF.status # deprecated !
@@ -91,7 +91,7 @@ function Get-DbaPageFileSetting {
             else {
                 # pagefile is automatic managed, so there are no settings
                 New-Object Sqlcollaborative.Dbatools.Computer.PageFileSetting -Property @{
-                    ComputerName         = $Computer
+                    ComputerName         = $computer
                     AutoPageFile         = $CompSys.automaticmanagedpagefile
                     FileName             = $null
                     Status               = $null
