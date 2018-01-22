adb7148d15a7c73efa1cbf3abbeb9cc36d67a574
Andy Levy <andy@levyclan.us>
2018-01-21 22:29:51 -0500

    Help content, style, grammar cleanup

--- functions/Remove-DbaXESession.ps1
+++ functions/Remove-DbaXESession.ps1
@@ -1,66 +1,72 @@
 ﻿function Remove-DbaXESession {
     <#
-    .SYNOPSIS
-    Removes Extended Events sessions.
+        .SYNOPSIS
+            Removes Extended Events sessions.
 
-    .DESCRIPTION
-    This script removes Extended Events sessions on a SQL Server instance.
+        .DESCRIPTION
+            This script removes Extended Events sessions on a SQL Server instance.
 
-    .PARAMETER SqlInstance
-    The SQL Instances that you're connecting to.
+        .PARAMETER SqlInstance
+            Target SQL Server. You must have sysadmin access and server version must be SQL Server version 2008 or higher.
 
-    .PARAMETER SqlCredential
-    Credential object used to connect to the SQL Server as a different user
+        .PARAMETER SqlCredential
+            Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
 
-    .PARAMETER Session
-    Only remove specific Extended Events sessions.
+            $scred = Get-Credential, then pass $scred object to the -SqlCredential parameter.
 
-    .PARAMETER AllSessions
-    Remove all Extended Events sessions on an instance, ignoring the packaged sessions: AlwaysOn_health, system_health, telemetry_xevents.
+            Windows Authentication will be used if SqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
 
-    .PARAMETER InputObject
-    Internal parameter to support piping from Get-DbaXESession
+            To connect as a different Windows user, run PowerShell as that user.
 
-    .PARAMETER WhatIf
-    Shows what would happen if the command were to run. No actions are actually performed.
+        .PARAMETER Session
+            Specifies a list of Extended Events sessions to remove.
 
-    .PARAMETER Confirm
-    Prompts you for confirmation before executing any changing operations within the command.
+        .PARAMETER AllSessions
+            If this switch is enabled, all Extended Events sessions will be removed except the packaged sessions AlwaysOn_health, system_health, telemetry_xevents.
+
+        .PARAMETER InputObject
+            Accepts a collection of XEsession objects as output by Get-DbaXESession.
+
+        .PARAMETER WhatIf
+            If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+        .PARAMETER Confirm
+            If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+            
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+        .NOTES
+            Tags: Xevent
+            Website: https://dbatools.io
+            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+            License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+
+        .LINK
+            https://dbatools.io/Remove-DbaXESession
+
+        .EXAMPLE
+            Remove-DbaXESession -SqlInstance sql2012 -AllSessions
+
+            Removes all Extended Event Session on the sqlserver2014 instance.
+
+        .EXAMPLE
+            Remove-DbaXESession -SqlInstance sql2012 -Session xesession1,xesession2
+
+            Removes the xesession1 and xesession2 Extended Event sessions.
         
-    .PARAMETER EnableException
-    By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-    This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-    Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        .EXAMPLE
+            Get-DbaXESession -SqlInstance sql2017 | Remove-DbaXESession -Confirm:$false
 
-    .NOTES
-    Tags: Xevent
-    Website: https://dbatools.io
-    Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-    License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+            Removes all sessions from sql2017, bypassing prompts.
 
-    .LINK
-    https://dbatools.io/Remove-DbaXESession
+        .EXAMPLE
+            Get-DbaXESession -SqlInstance sql2012 -Session xesession1 | Remove-DbaXESession
 
-    .EXAMPLE
-    Remove-DbaXESession -SqlInstance sql2012 -AllSessions
-
-    Removes all Extended Event Session on the sqlserver2014 instance.
-
-    .EXAMPLE
-    Remove-DbaXESession -SqlInstance sql2012 -Session xesession1,xesession2
-
-    Removes the xesession1 and xesession2 Extended Event sessions.
-    
-    .EXAMPLE
-    Get-DbaXESession -SqlInstance sql2017 | Remove-DbaXESession -Confirm:$false
-    Removes all sessions from sql2017, bypassing prompting
-
-    .EXAMPLE
-    Get-DbaXESession -SqlInstance sql2012 -Session xesession1 | Remove-DbaXESession
-
-    Removes the sessions returned from the Get-DbaXESession function.
-
-#>
+            Removes the sessions returned from the Get-DbaXESession function.
+    #>
     [CmdletBinding(DefaultParameterSetName = 'Session', SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
         [parameter(Position = 1, Mandatory, ParameterSetName = 'Session')]
@@ -94,11 +100,11 @@
                     try {
                         $xe.Drop()
                         [pscustomobject]@{
-                            ComputerName     = $xe.Parent.NetName
-                            InstanceName     = $xe.Parent.ServiceName
-                            SqlInstance      = $xe.Parent.DomainInstanceName
-                            Session          = $session
-                            Status           = "Successful"
+                            ComputerName = $xe.Parent.NetName
+                            InstanceName = $xe.Parent.ServiceName
+                            SqlInstance  = $xe.Parent.DomainInstanceName
+                            Session      = $session
+                            Status       = "Successful"
                         }
                     }
                     catch {
--- functions/Stop-DbaXESmartTarget.ps1
+++ functions/Stop-DbaXESmartTarget.ps1
@@ -1,46 +1,45 @@
 ﻿function Stop-DbaXESmartTarget {
- <#
-    .SYNOPSIS
-    Stops an XESmartTarget PowerShell Job. Useful if you want to run a target, but not right now.
+    <#
+        .SYNOPSIS
+            Stops an XESmartTarget PowerShell Job. Useful if you want to run a target, but not right now.
 
-    .DESCRIPTION
-    Stops an XESmartTarget PowerShell Job. Useful if you want to run a target, but not right now.
+        .DESCRIPTION
+            Stops an XESmartTarget PowerShell Job. Useful if you want to run a target, but not right now.
 
-    .PARAMETER InputObject
-    The XESmartTarget job object
+        .PARAMETER InputObject
+            The XESmartTarget job object.
 
-    .PARAMETER WhatIf
-    Shows what would happen if the command were to run. No actions are actually performed.
+        .PARAMETER WhatIf
+            If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
-    .PARAMETER Confirm
-    Prompts you for confirmation before executing any changing operations within the command.
+        .PARAMETER Confirm
+            If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+            
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-    .PARAMETER EnableException
-    By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-    This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-    Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        .NOTES
+            Website: https://dbatools.io
+            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+            License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+            SmartTarget: by Gianluca Sartori (@spaghettidba)
 
-    .NOTES
-    Website: https://dbatools.io
-    Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-    License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
-    SmartTarget: by Gianluca Sartori (@spaghettidba)
+        .LINK
+            https://dbatools.io/Stop-DbaXESmartTarget
+            https://github.com/spaghettidba/XESmartTarget/wiki
 
-    .LINK
-    https://dbatools.io/Stop-DbaXESmartTarget
-    https://github.com/spaghettidba/XESmartTarget/wiki
+        .EXAMPLE
+            Get-DbaXESmartTarget | Stop-DbaXESmartTarget
 
-    .EXAMPLE
-    Get-DbaXESmartTarget | Stop-DbaXESmartTarget
+            Stops all XESmartTarget jobs.
 
-    Stops all XESmartTarget jobs
+        .EXAMPLE
+            Get-DbaXESmartTarget | Where-Object Id -eq 2 | Stop-DbaXESmartTarget
 
-    .EXAMPLE
-    Get-DbaXESmartTarget | Where-Object Id -eq 2 | Stop-DbaXESmartTarget
-
-    Stops a specific XESmartTarget job
-
-#>
+            Stops a specific XESmartTarget job.
+    #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
         [parameter(Mandatory, ValueFromPipeline)]
@@ -51,7 +50,7 @@
         if ($Pscmdlet.ShouldProcess("localhost", "Stopping job $id")) {
             try {
                 $id = $InputObject.Id
-                Write-Message -Level Output -Message "Stopping job $id, this may take a couple minutes"
+                Write-Message -Level Output -Message "Stopping job $id, this may take a couple minutes."
                 Get-Job -ID $InputObject.Id | Stop-Job
                 Write-Message -Level Output -Message "Successfully Stopped $id. If you need to remove the job for good, use Remove-DbaXESmartTarget."
             }
