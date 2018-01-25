c06e0760522cdefbb49bc75260f3aebfe081f27c
Shawn Melton <wshawnmelton@gmail.com>
2018-01-25 11:29:41 -0600

    formatting, add BoH tags Also add PSSA rule suppression

--- internal/functions/Invoke-Command2.ps1
+++ internal/functions/Invoke-Command2.ps1
@@ -1,36 +1,38 @@
-﻿function Invoke-Command2 {
+#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
+function Invoke-Command2 {
     <#
-    .SYNOPSIS
-        Wrapper function that calls Invoke-Command and gracefully handles credentials.
+        .SYNOPSIS
+            Wrapper function that calls Invoke-Command and gracefully handles credentials.
 
-    .DESCRIPTION
-        Wrapper function that calls Invoke-Command and gracefully handles credentials.
+        .DESCRIPTION
+            Wrapper function that calls Invoke-Command and gracefully handles credentials.
 
-    .PARAMETER ComputerName
-        Default: $env:COMPUTERNAME
-        The computer to invoke the scriptblock on.
+        .PARAMETER ComputerName
+            Default: $env:COMPUTERNAME
+            The computer to invoke the scriptblock on.
 
-    .PARAMETER Credential
-        The credentials to use.
-        Can accept $null on older PowerShell versions, since it expects type object, not PSCredential
+        .PARAMETER Credential
+            The credentials to use.
+            Can accept $null on older PowerShell versions, since it expects type object, not PSCredential
 
-    .PARAMETER ScriptBlock
-        The code to run on the targeted system
+        .PARAMETER ScriptBlock
+            The code to run on the targeted system
 
-    .PARAMETER ArgumentList
-        Any arguments to pass to the scriptblock being run
+        .PARAMETER ArgumentList
+            Any arguments to pass to the scriptblock being run
 
-    .PARAMETER Raw
-        Passes through the raw return data, rather than prettifying stuff.
+        .PARAMETER Raw
+            Passes through the raw return data, rather than prettifying stuff.
 
-    .EXAMPLE
-        PS C:\> Invoke-Command2 -ComputerName sql2014 -Credential $Credential -ScriptBlock { dir }
+        .EXAMPLE
+            PS C:\> Invoke-Command2 -ComputerName sql2014 -Credential $Credential -ScriptBlock { dir }
 
-        Executes the scriptblock '{ dir }' on the computer sql2014 using the credentials stored in $Credential.
-        If $Credential is null, no harm done.
-#>
+            Executes the scriptblock '{ dir }' on the computer sql2014 using the credentials stored in $Credential.
+            If $Credential is null, no harm done.
+    #>
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUsePSCredentialType", "")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "")]
     param (
         [DbaInstanceParameter]$ComputerName = $env:COMPUTERNAME,
         [object]$Credential,
@@ -38,46 +40,53 @@
         [object[]]$ArgumentList,
         [switch]$Raw
     )
-    
+    <# Note: Credential stays as an object type for legacy reasons. #>
+
     $InvokeCommandSplat = @{
-        ScriptBlock  = $ScriptBlock
+        ScriptBlock = $ScriptBlock
     }
-    if ($ArgumentList) { $InvokeCommandSplat["ArgumentList"] = $ArgumentList }
-    if (-not $ComputerName.IsLocalhost) {
-        $runspaceid = [System.Management.Automation.Runspaces.Runspace]::DefaultRunspace.InstanceId
-        $sessionname = "dbatools_$runspaceid"
-        
+    if ($ArgumentList) {
+        $InvokeCommandSplat["ArgumentList"] = $ArgumentList
+    }
+    if (-not $ComputerName.IsLocalHost) {
+        $runspaceId = [System.Management.Automation.Runspaces.Runspace]::DefaultRunspace.InstanceId
+        $sessionName = "dbatools_$runspaceId"
+
         # Retrieve a session from the session cache, if available (it's unique per runspace)
-        if (-not ($currentsession = [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionGet($runspaceid, $ComputerName.ComputerName) | Where-Object State -Match "Opened|Disconnected")) {
+        if (-not ($currentSession = [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionGet($runspaceId, $ComputerName.ComputerName) | Where-Object State -Match "Opened|Disconnected")) {
             $timeout = New-PSSessionOption -IdleTimeout (New-TimeSpan -Minutes 10).TotalMilliSeconds
             if ($Credential) {
-                $InvokeCommandSplat["Session"] = (New-PSSession -ComputerName $ComputerName.ComputerName -Name $sessionname -SessionOption $timeout -Credential $Credential -ErrorAction Stop)
+                $InvokeCommandSplat["Session"] = (New-PSSession -ComputerName $ComputerName.ComputerName -Name $sessionName -SessionOption $timeout -Credential $Credential -ErrorAction Stop)
             }
             else {
-                $InvokeCommandSplat["Session"] = (New-PSSession -ComputerName $ComputerName.ComputerName -Name $sessionname -SessionOption $timeout -ErrorAction Stop)
+                $InvokeCommandSplat["Session"] = (New-PSSession -ComputerName $ComputerName.ComputerName -Name $sessionName -SessionOption $timeout -ErrorAction Stop)
             }
-            $currentsession = $InvokeCommandSplat["Session"]
+            $currentSession = $InvokeCommandSplat["Session"]
         }
         else {
-            if ($currentsession.State -eq "Disconnected") {
-                $null = $currentsession | Connect-PSSession -ErrorAction Stop
+            if ($currentSession.State -eq "Disconnected") {
+                $null = $currentSession | Connect-PSSession -ErrorAction Stop
             }
-            $InvokeCommandSplat["Session"] = $currentsession
-            
+            $InvokeCommandSplat["Session"] = $currentSession
+
             # Refresh the session registration if registered, to reset countdown until purge
-            [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionSet($runspaceid, $ComputerName.ComputerName, $currentsession)
+            [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionSet($runspaceId, $ComputerName.ComputerName, $currentSession)
         }
     }
-    
-    if ($Raw) { Invoke-Command @InvokeCommandSplat }
-    else { Invoke-Command @InvokeCommandSplat | Select-Object -Property * -ExcludeProperty PSComputerName, RunspaceId, PSShowComputerName }
-    
+
+    if ($Raw) {
+        Invoke-Command @InvokeCommandSplat
+    }
+    else {
+        Invoke-Command @InvokeCommandSplat | Select-Object -Property * -ExcludeProperty PSComputerName, RunspaceId, PSShowComputerName
+    }
+
     if (-not $ComputerName.IsLocalhost) {
         # Tell the system to clean up if the session expires
-        [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionSet($runspaceid, $ComputerName.ComputerName, $currentsession)
-        
+        [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionSet($runspaceId, $ComputerName.ComputerName, $currentSession)
+
         if (-not (Get-DbaConfigValue -FullName 'PSRemoting.Sessions.Enable' -Fallback $true)) {
-            $currentsession | Remove-PSSession
+            $currentSession | Remove-PSSession
         }
     }
 }
