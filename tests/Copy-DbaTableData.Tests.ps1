841f7261511d02967ab2193ccf28ffebc24b2468
Christoph Jahn <christoph.jahn@scherdel.de>
2018-01-30 11:21:34 +0100

    Fixing Copy-DbaTableData wrong DestinationDatabase #3173

--- tests/Copy-DbaTableData.Tests.ps1
+++ tests/Copy-DbaTableData.Tests.ps1
@@ -5,21 +5,31 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
         $db = Get-DbaDatabase -SqlInstance $script:instance1 -Database tempdb
+        $db2 = Get-DbaDatabase -SqlInstance $script:instance2 -Database tempdb
         $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example (id int);
             INSERT dbo.dbatoolsci_example
             SELECT top 100 1
             FROM sys.objects")
         $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example2 (id int)")
+        $null = $db2.Query("CREATE TABLE dbo.dbatoolsci_example3 (id int)")
     }
     AfterAll {
         $null = $db.Query("DROP TABLE dbo.dbatoolsci_example")
         $null = $db.Query("DROP TABLE dbo.dbatoolsci_example2")
+        $null = $db2.Query("DROP TABLE dbo.dbatoolsci_example3")
     }
 
     It "copies the table data" {
         $null = Copy-DbaTableData -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example -DestinationTable dbatoolsci_example2
         $table1count = $db.Query("select id from dbo.dbatoolsci_example")
         $table2count = $db.Query("select id from dbo.dbatoolsci_example2")
+        $table1count.Count -eq $table2count.Count
+    }
+
+    It "copies the table data to another instance" {
+        $null = Copy-DbaTableData -SqlInstance $script:instance1 -Destination $script:instance2 -Database tempdb -Table dbatoolsci_example -DestinationTable dbatoolsci_example3
+        $table1count = $db.Query("select id from dbo.dbatoolsci_example")
+        $table2count = $db2.Query("select id from dbo.dbatoolsci_example3")
         $table1count.Count -eq $table2count.Count
     }
 
