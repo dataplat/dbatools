854d1ff97c9522e62f347a6f211532a5235c612f
Rob Sewell <mrrobsewell@outlook.com>
2018-02-06 12:23:58 +0000

    For the right Pester test

--- tests/Get-DbaOperatingSystem.Tests.ps1
+++ tests/Get-DbaOperatingSystem.Tests.ps1
@@ -1,5 +1,5 @@
 
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "",Justification="This is a Pester Test so its ok")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingComputerNameHardcoded", "",Justification="This is a Pester Test so its ok")]
 Param()
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
