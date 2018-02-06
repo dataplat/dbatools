ea27ade919538d7e1aca9355af1ed5f75fc4c7b8
Rob Sewell <mrrobsewell@outlook.com>
2018-02-06 12:16:17 +0000

    To get rid of errors from the PowerShell Gallery

--- tests/Backup-DbaDatabaseMasterKey.Tests.ps1
+++ tests/Backup-DbaDatabaseMasterKey.Tests.ps1
@@ -1,4 +1,6 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
+﻿
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "",Justification="This is a Pester Test so its ok")]
+Param()
 $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
