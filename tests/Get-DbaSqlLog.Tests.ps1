35e1fff875564dd883a354b88cf82ef1343d7521
Rob Sewell <mrrobsewell@outlook.com>
2018-02-06 12:26:48 +0000

    stop powershell gallery ester tests failing

--- tests/Get-DbaSqlLog.Tests.ps1
+++ tests/Get-DbaSqlLog.Tests.ps1
@@ -1,3 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "",Justification="This is a Pester Test so its ok")]
+Param()
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
