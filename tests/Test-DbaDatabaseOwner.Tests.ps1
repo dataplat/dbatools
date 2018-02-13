f954a7fa52f4112a267d3e8cb8ef83332ac9a653
Joshua Corrick <Joshua.corrick@live.com>
2018-02-13 08:32:25 -0500

    Including Parameter Check in Test; Adding Tag to Describe block

--- tests/Test-DbaDatabaseOwner.Tests.ps1
+++ tests/Test-DbaDatabaseOwner.Tests.ps1
@@ -2,7 +2,26 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-Describe "$Name Tests" {
+Describe "$CommandName Unit Tests" -Tag "UnitTests" {
+    Context "Validate parameters" {
+        <#
+            The $paramCount is adjusted based on the parameters your command will have.
+
+            The $defaultParamCount is adjusted based on what type of command you are writing the test for:
+                - Commands that *do not* include SupportShouldProcess, set defaultParamCount    = 11
+                - Commands that *do* include SupportShouldProcess, set defaultParamCount        = 13
+        #>
+        $paramCount = 7
+        $defaultParamCount = 11
+        [object[]]$params = (Get-ChildItem function:\Test-DbaDatabaseOwner).Parameters.Keys
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'TargetLogin', 'EnableException', 'Detailed'
+        It "Should contain our specific parameters" {
+            ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
+        }
+        It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        }
+    }
     InModuleScope 'dbatools' {
         Context "Connects to SQL Server" {
             It -Skip "Should not throw" {
