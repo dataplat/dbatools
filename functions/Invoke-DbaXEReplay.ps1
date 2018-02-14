a074a869fad367e71c06ee27bf797ecb6fcd6899
potatoqualitee <clemaire@gmail.com>
2018-02-14 16:29:04 +0100

    added invoke replay, hushed output

--- functions/Invoke-DbaXEReplay.ps1
+++ functions/Invoke-DbaXEReplay.ps1
@@ -2,32 +2,32 @@
 function Invoke-DbaXeReplay {
     <#
         .SYNOPSIS
-            A command to run explicit T-SQL commands or files.
+            This command replays events from Read-DbaXEFile on one or more target servers
 
         .DESCRIPTION
-            This function is a wrapper command around Invoke-SqlCmd2.
-            It was designed to be more convenient to use in a pipeline and to behave in a way consistent with the rest of our functions.
+            This command replays events from Read-DbaXEFile. It is simplistic in its approach.
+    
+            - Writes all queries to a temp sql file
+            - Execues temp file with sqlcmd
+            - Deletes temp file
 
         .PARAMETER SqlInstance
-            Target SQL Server(s). You must have sysadmin access and server version must be SQL Server version 2008 or higher.
+            Target SQL Server(s)
 
         .PARAMETER SqlCredential
-            Allows you to login to servers using SQL Logins instead of Windows Authentication (AKA Integrated or Trusted). To use:
-
-            $scred = Get-Credential, then pass $scred object to the -SqlCredential parameter.
-
-            Windows Authentication will be used if SqlCredential is not specified. SQL Server does not accept Windows credentials being passed as credentials.
-
-            To connect as a different Windows user, run PowerShell as that user.
+            Used to provide alternative credentials.
 
         .PARAMETER Database
-            The database(s) to execute the queries against. If left blank, the original database name will be used.
+            The initial starting database.
 
         .PARAMETER Event
             Each Response can be limited to processing specific events, while ignoring all the other ones. When this attribute is omitted, all events are processed.
 
+        .PARAMETER Raw
+            By dafault, the results of sqlcmd are collected, cleaned up and displayed. If you'd like to see all results immeidately, use Raw.
+    
         .PARAMETER InputObject
-            Accepts the object output by Read-DbaXESession or Watch-DbaXESession.
+            Accepts the object output of Read-DbaXESession.
 
         .PARAMETER EnableException
             By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
@@ -41,19 +41,20 @@ function Invoke-DbaXeReplay {
             License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
     
         .EXAMPLE
-            Read-DbaXEFile -Path C:\temp\sample.xel | Invoke-DbaSqlQuery -SqlInstance sql2017 -Database tempdb
+            Read-DbaXEFile -Path C:\temp\sample.xel | Invoke-DbaXeReplay -SqlInstance sql2017
 
             Runs all batch_text for sql_batch_completed against tempdb on sql2017.
+        
+        .EXAMPLE
+            Read-DbaXEFile -Path C:\temp\sample.xel | Invoke-DbaXeReplay -SqlInstance sql2017 -Database planning -Event sql_batch_completed
+
+            Sets the *initial* database to planning then runs only sql_batch_completed against sql2017.
 
         .EXAMPLE
-            Read-DbaXEFile -Path C:\temp\sample.xel | Invoke-DbaSqlQuery -SqlInstance sql2017, sql2016 -Database tempdb, db1
+            Read-DbaXEFile -Path C:\temp\sample.xel | Invoke-DbaXeReplay -SqlInstance sql2017, sql2016
 
-            Runs all batch_text for sql_batch_completed against tempdb and db1 on servers sql2017 and sql2016.
+            Runs all batch_text for sql_batch_completed against tempdb on sql2017 and sql2016
 
-        .EXAMPLE
-            Watch-DbaXESession -SqlInstance sql2017 -Session 'Profile New App' | Invoke-DbaSqlQuery -SqlInstance sql2017, sql2016 -Database tempdb, db1
-
-            Runs all batch_text for sql_batch_completed against tempdb and db1 on servers sql2017 and sql2016.
     #>
     Param (
         [Parameter(Mandatory)]
@@ -62,39 +63,45 @@ function Invoke-DbaXeReplay {
         [Alias("Credential")]
         [PsCredential]$SqlCredential,
         [string[]]$Database,
-        [string[]]$Event = @('sql_batch_completed','rcp_completed'),
+        [string[]]$Event = @('sql_batch_completed', 'rcp_completed'),
         [Parameter(Mandatory, ValueFromPipeline)]
         [object]$InputObject,
+        [switch]$Raw,
         [switch]$EnableException
     )
-
+    
+    begin {
+        $querycolumns = 'statement', 'batch_text'
+        $timestamp = (Get-Date -Format yyyyMMddHHmm)
+        $temp = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
+        $filename = "$temp\dbatools-replay-$timestamp.sql"
+        Set-Content $filename -Value $null
+        
+        if (-not (Get-Command sqlcmd -ErrorAction SilentlyContinue)) {
+            Stop-Function -Message "sqlcmd is required but does not exist on this machine. We've asked Microsoft if we can include it in dbatools and are currently awaiting a response."
+        }
+    }
     process {
-        if ($InputObject.name -notin $Event) {
+        if (Test-FunctionInterrupt) { return }
+        if ($InputObject.Name -notin $Event) {
             continue
         }
-
-        if (Test-Bound -ParameterName Database -Not) {
-            $Database = switch ($InputObject.database_id) {
-                1 { "master" }
-                2 { "tempdb" }
-                3 { "model" }
-                4 { "msdb" }
-                default { "$($InputObject.database_name)".Trim() }
-            }
-            if (-not $Database) {
-                $Database = "tempdb"
-            }
-        }
-
-        $querycolumns = 'statement', 'batch_text'
-
+        
         if ($InputObject.statement) {
-            $query = $InputObject.statement
+            if ($InputObject.statement -notmatch "ALTER EVENT SESSION") {
+                Add-Content -Path $filename -Value $InputObject.statement
+                Add-Content -Path $filename -Value "GO"
+            }
         }
         else {
-            $query = $InputObject.batch_text
+            if ($InputObject.batch_text -notmatch "ALTER EVENT SESSION") {
+                Add-Content -Path $filename -Value $InputObject.batch_text
+                Add-Content -Path $filename -Value "GO"
+            }
         }
-
+    }
+    end {
+        if (Test-FunctionInterrupt) { return }
         foreach ($instance in $SqlInstance) {
             try {
                 Write-Message -Level VeryVerbose -Message "Connecting to $instance." -Target $instance
@@ -103,22 +110,33 @@ function Invoke-DbaXeReplay {
             catch {
                 Stop-Function -Message "Failure" -ErrorRecord $_ -Target $instance -Continue
             }
-
-            foreach ($currentdb in $Database) {
-                $db = Get-DbaDatabase -SqlInstance $server -Database $Database
-                try {
-                    $db.Query($query)
-                    [pscustomobject]@{
-                        SqlInstance       = $instance
-                        Database          = $currentdb
-                        Query             = $query
-                    }
+            
+            
+            if ($Raw) {
+                if (Test-Bound -ParameterName SqlCredential) {
+                    sqlcmd -S $instance -i $filename -U $SqlCredential.Username -P $SqlCredential.GetNetworkCredential().Password
+                    continue
                 }
-                catch {
-                    $message = $_.Exception.InnerException.InnerException | Out-String
-                    Stop-Function -Message "Query ($query) against $currentdb on $instance failed | $message."
+                else {
+                    sqlcmd -S $instance -i $filename
+                    continue
+                }
+            }
+            
+            if (Test-Bound -ParameterName SqlCredential) {
+                $output = sqlcmd -S $instance -i $filename -U $SqlCredential.Username -P $SqlCredential.GetNetworkCredential().Password
+            }
+            else {
+                $output = sqlcmd -S $instance -i $filename
+            }
+            
+            foreach ($line in $output) {
+                $newline = $line.Trim()
+                if ($newline -and $newline -notmatch "------------------------------------------------------------------------------------") {
+                    "$newline"
                 }
             }
         }
+        Remove-Item -Path $filename -ErrorAction Ignore
     }
 }
