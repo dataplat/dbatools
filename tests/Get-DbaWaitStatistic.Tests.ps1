57da7834286a7ed56c17c0fd1f25c94456810653
Andrew Wickham <andrew@awickham.com>
2018-02-16 15:56:42 -0500

    Adding test coverage for connection failures

--- tests/Get-DbaWaitStatistic.Tests.ps1
+++ tests/Get-DbaWaitStatistic.Tests.ps1
@@ -32,4 +32,10 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
             }
         }
     }
+
+    Context "Command stops when can't connect" {
+        It "Should warn cannot connect to MadeUpServer" {
+            { Get-DbaAgentJob -SqlInstance MadeUpServer -EnableException } | Should Throw "Can't connect to MadeUpServer"
+        }
+    }    
 }
