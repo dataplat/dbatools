11bf6d3b9bfd0ca7e659190158fa3894f8bb7736
Andrew Wickham <andrew@awickham.com>
2018-02-23 11:59:34 -0500

    Fixes bug #3156 and adds additional test coverage

--- tests/Connect-DbaInstance.Tests.ps1
+++ tests/Connect-DbaInstance.Tests.ps1
@@ -2,6 +2,8 @@
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+$instance1 = 'localhost'
+
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "connection is properly made" {
         $server = Connect-DbaInstance -SqlInstance $script:instance1 -ApplicationIntent ReadOnly
@@ -16,6 +18,41 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 
         It "returns the connection with ApplicationIntent of ReadOnly" {
             $server.ConnectionContext.ConnectionString -match "ApplicationIntent=ReadOnly" | Should Be $true
+        }
+
+        It "sets StatementTimeout to 0" {
+            $server = Connect-DbaInstance -SqlInstance $script:instance1 -StatementTimeout 0
+
+            $server.ConnectionContext.StatementTimeout | Should Be 0
+        }
+
+        It "sets connectioncontext parameters that are provided" {
+            $params = @{
+                'BatchSeparator' = 'GO'
+                'ConnectTimeout' = 1
+                'Database' = 'master'
+                'LockTimeout' = 1
+                'MaxPoolSize' = 20
+                'MinPoolSize' = 1
+                'NetworkProtocol' = 'TcpIp'
+                'PacketSize' = 4096
+                'PooledConnectionLifetime' = 600
+                'WorkstationId' = 'MadeUpServer'
+                'SqlExecutionModes' = 'ExecuteSql'
+                'StatementTimeout' = 0
+            }
+
+            $server = Connect-DbaInstance -SqlInstance $script:instance1 @params
+
+            foreach ($param in $params.GetEnumerator()) {
+                if ($param.Key -eq 'Database') { 
+                    $propName = 'DatabaseName'
+                } else {
+                    $propName = $param.Key
+                }
+
+                $server.ConnectionContext.$propName | Should Be $param.Value
+            }
         }
     }
 }
