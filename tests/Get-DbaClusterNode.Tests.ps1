74e22ac0ae9cf9d6cda5c6521a96cc6ec98ce4f3
Joshua Corrick <Joshua.corrick@live.com>
2018-02-22 23:27:32 -0500

    Updating Typo

--- tests/Get-DbaClusterNode.Tests.ps1
+++ tests/Get-DbaClusterNode.Tests.ps1
@@ -13,7 +13,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         #>
         $paramCount = 5
         $defaultParamCount = 11
-        [object[]]$params = (Get-ChildItem function:\Test-DbaClusterNode).Parameters.Keys
+        [object[]]$params = (Get-ChildItem function:\Get-DbaClusterNode).Parameters.Keys
         $knownParameters = 'SqlInstance', 'SqlCredential', 'ActiveNode', 'EnableException', 'Detailed'
         It "Should contain our specific parameters" {
             ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
