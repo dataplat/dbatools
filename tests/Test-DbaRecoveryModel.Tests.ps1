fd8f9b20c00dc8b15b985d86ac25e1fc53367333
Joshua Corrick <Joshua.corrick@live.com>
2018-02-25 14:59:59 -0500

    Replacing Test

--- tests/Test-DbaRecoveryModel.Tests.ps1
+++ tests/Test-DbaRecoveryModel.Tests.ps1
@@ -13,7 +13,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         #>
         $paramCount = 6
         $defaultParamCount = 11
-        [object[]]$params = (Get-ChildItem function:\Test-DbaFullRecoveryModel).Parameters.Keys
+        [object[]]$params = (Get-ChildItem function:\Test-DbaRecoveryModel).Parameters.Keys
         $knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'EnableException', 'Detailed'
         It "Should contain our specific parameters" {
             ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
