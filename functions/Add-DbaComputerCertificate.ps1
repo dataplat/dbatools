ad4f4eabe998bda31e9199b717561032128b8be9
Andrew Wickham <andrew@awickham.com>
2018-03-01 17:17:02 -0500

    Changing encoding back to UTF-8

--- functions/Add-DbaComputerCertificate.ps1
+++ functions/Add-DbaComputerCertificate.ps1
@@ -1,4 +1,4 @@
-﻿#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
+#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
 function Add-DbaComputerCertificate {
     <#
         .SYNOPSIS
