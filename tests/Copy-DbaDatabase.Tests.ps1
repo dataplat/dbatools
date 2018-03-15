47dc3bc839332100fafac183b9aa101fe7bb4615
niphlod <niphlod@gmail.com>
2018-03-15 22:35:39 +0100

    Copy-DbaDatabase, drop existing correctly

--- tests/Copy-DbaDatabase.Tests.ps1
+++ tests/Copy-DbaDatabase.Tests.ps1
@@ -5,23 +5,16 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
         $NetworkPath = "C:\temp"
-        $backuprestoredb = "dbatoolsci_backuprestore"
-        $detachattachdb = "dbatoolsci_detachattach"
+        $random = Get-Random
+        $backuprestoredb = "dbatoolsci_backuprestore$random"
+        $detachattachdb = "dbatoolsci_detachattach$random"
+        Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance1,$script:instance2 -Database $backuprestoredb, $detachattachdb
         $server = Connect-DbaInstance -SqlInstance $script:instance1
-        Stop-DbaProcess -SqlInstance $script:instance1 -Database model
         $server.Query("CREATE DATABASE $backuprestoredb")
-        $db = Get-DbaDatabase -SqlInstance $script:instance1 -Database $backuprestoredb
-        if (-not $env:appveyor) {
-            if ($db.AutoClose) {
-                $db.AutoClose = $false
-                $db.Alter()
-            }
-        }
-        Stop-DbaProcess -SqlInstance $script:instance1 -Database model
         $server.Query("CREATE DATABASE $detachattachdb")
     }
     AfterAll {
-        Remove-DbaDatabase -Confirm:$false -SqlInstance $Instances -Database $backuprestoredb, $detachattachdb
+        Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance1,$script:instance2 -Database $backuprestoredb, $detachattachdb
     }
 
     Context "Detach Attach" {
@@ -54,30 +47,33 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
         }
     }
 
-    if (-not $env:appveyor) {
-        Context "Backup restore" {
-            It "copies a database and retain its name, recovery model, and status." {
+    Context "Backup restore" {
+        It "copies a database and retain its name, recovery model, and status." {
 
-                Set-DbaDatabaseOwner -SqlInstance $script:instance1 -Database $backuprestoredb -TargetLogin sa
-                Copy-DbaDatabase -Source $script:instance1 -Destination $script:instance2 -Database $backuprestoredb -BackupRestore -NetworkShare $NetworkPath
- 
-                $db1 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $backuprestoredb
-                $db2 = Get-DbaDatabase -SqlInstance $script:instance2 -Database $backuprestoredb
-                $db1 | Should Not BeNullOrEmpty
-                $db2 | Should Not BeNullOrEmpty
+            $null = Set-DbaDatabaseOwner -SqlInstance $script:instance1 -Database $backuprestoredb -TargetLogin sa
+            $null = Copy-DbaDatabase -Source $script:instance1 -Destination $script:instance2 -Database $backuprestoredb -BackupRestore -NetworkShare $NetworkPath
 
-                # Compare its valuable.
-                $db1.Name | Should Be $db2.Name
-                $db1.RecoveryModel | Should Be $db2.RecoveryModel
-                $db1.Status | Should be $db2.Status
-                $db1.Owner | Should be $db2.Owner
-            }
+            $db1 = Get-DbaDatabase -SqlInstance $script:instance1 -Database $backuprestoredb
+            $db2 = Get-DbaDatabase -SqlInstance $script:instance2 -Database $backuprestoredb
+            $db1 | Should Not BeNullOrEmpty
+            $db2 | Should Not BeNullOrEmpty
 
-            It "Should say skipped" {
-                $result = Copy-DbaDatabase -Source $script:instance1 -Destination $script:instance2 -Database $backuprestoredb -BackupRestore -NetworkShare $NetworkPath
-                $result.Status | Should be "Skipped"
-                $result.Notes | Should be "Already exists"
-            }
+            # Compare its valuable.
+            $db1.Name | Should Be $db2.Name
+            $db1.RecoveryModel | Should Be $db2.RecoveryModel
+            $db1.Status | Should be $db2.Status
+            $db1.Owner | Should be $db2.Owner
+        }
+
+        It "Should say skipped" {
+            $result = Copy-DbaDatabase -Source $script:instance1 -Destination $script:instance2 -Database $backuprestoredb -BackupRestore -NetworkShare $NetworkPath
+            $result.Status | Should be "Skipped"
+            $result.Notes | Should be "Already exists"
+        }
+        It "Should overwrite when forced to" {
+            #regr test for #3358
+            $result = Copy-DbaDatabase -Source $script:instance1 -Destination $script:instance2 -Database $backuprestoredb -BackupRestore -NetworkShare $NetworkPath -Force
+            $result.Status | Should be "Successful"
         }
     }
 }
