e3e88ab56357ad043f67ae622884f2c110ef774e
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2018-03-18 12:10:46 +0100

    oops it was tabs

--- internal/configurations/validation/sizestyle.ps1
+++ internal/configurations/validation/sizestyle.ps1
@@ -1,23 +1,23 @@
 Register-DbaConfigValidation -Name "sizestyle" -ScriptBlock {
-	param (
-		$Value
-	)
+    param (
+        $Value
+    )
 
-	$Result = New-Object PSObject -Property @{
-		Success  = $True
-		Value    = $null
-		Message  = ""
-	}
+    $Result = New-Object PSObject -Property @{
+        Success  = $True
+        Value    = $null
+        Message  = ""
+    }
 
-	try { [Sqlcollaborative.Dbatools.Utility.SizeStyle]$style = $Value }
-	catch
-	{
-		$Result.Message = "Not a size style: $Value"
-		$Result.Success = $False
-		return $Result
-	}
+    try { [Sqlcollaborative.Dbatools.Utility.SizeStyle]$style = $Value }
+    catch
+    {
+        $Result.Message = "Not a size style: $Value"
+        $Result.Success = $False
+        return $Result
+    }
 
-	$Result.Value = $style
+    $Result.Value = $style
 
-	return $Result
+    return $Result
 }
