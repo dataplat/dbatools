589c676fdc6c0a024e80058c18ffdccae9eff63a
cviorel <viorel.ciucu@gmail.com>
2018-03-21 14:48:26 +0100

    Refactored to use less tSQL

--- functions/Get-DbaDbExtentDiff.ps1
+++ functions/Get-DbaDbExtentDiff.ps1
@@ -57,6 +57,24 @@ function Get-DbaDbExtentDiff {
         [switch]$EnableException
     )
 
+    begin {
+        $rex = [regex]':(?<extent>[\d]+)\)'
+        function Get-DbaExtents ([string[]]$field) {
+            $res = 0
+            foreach ($f in $field) {
+                $extents = $rex.Matches($f)
+                if ($extents.Count -eq 1) {
+                    $res += 1
+                }
+                else {
+                    $pages = [int]$extents[1].Groups['extent'].Value - [int]$extents[0].Groups['extent'].Value
+                    $res += $pages / 8 + 1
+                }
+            }
+            return $res
+        }
+    }
+
     process {
 
         foreach ($instance in $SqlInstance) {
@@ -68,206 +86,68 @@ function Get-DbaDbExtentDiff {
                 Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
 
-            $SQLskillsDIFForFULL = "
-                IF EXISTS (SELECT * FROM sys.objects WHERE NAME = N'SQLskillsConvertToExtents')
-                DROP FUNCTION [SQLskillsConvertToExtents];
-                GO
-
-                CREATE FUNCTION [SQLskillsConvertToExtents] (
-                @extents    VARCHAR (100))
-                RETURNS INTEGER
-                AS
-                BEGIN
-                DECLARE @extentTotal    INT;
-                DECLARE @colon          INT;
-
-                DECLARE @firstExtent    INT;
-                DECLARE @secondExtent   INT;
-
-                SET @extentTotal = 0;
-                SET @colon = CHARINDEX (':', @extents);
-
-                IF (CHARINDEX (':', @extents, @colon + 1) = 0)
-                SET @extentTotal = 1;
-                ELSE
-                BEGIN
-                SET @firstExtent = CONVERT (INT,
-                SUBSTRING (@extents, @colon + 1, CHARINDEX (')', @extents, @colon) - @colon - 1));
-                SET @colon = CHARINDEX (':', @extents, @colon + 1);
-                SET @secondExtent = CONVERT (INT,
-                SUBSTRING (@extents, @colon + 1, CHARINDEX (')', @extents, @colon) - @colon - 1));
-                SET @extentTotal = (@secondExtent - @firstExtent) / 8 + 1;
-                END
-
-                RETURN @extentTotal;
-                END;
-                GO
-
-                IF OBJECT_ID (N'sp_SQLskillsDIFForFULL') IS NOT NULL
-                DROP PROCEDURE [sp_SQLskillsDIFForFULL];
-                GO
-
-                CREATE PROCEDURE [sp_SQLskillsDIFForFULL] (
-                @dbName SYSNAME)
-                AS
-                BEGIN
-                SET NOCOUNT ON;
-
-                IF EXISTS (SELECT * FROM [tempdb].[sys].[objects] WHERE NAME = N'SQLskillsDBCCPage')
-                DROP TABLE [tempdb].[dbo].[SQLskillsDBCCPage];
-
-                CREATE TABLE tempdb.dbo.SQLskillsDBCCPage (
-                [ParentObject]  VARCHAR (100),
-                [Object]        VARCHAR (100),
-                [Field]         VARCHAR (100),
-                [VALUE]         VARCHAR (100));
-
-                DECLARE @fileID          INT;
-                DECLARE @fileSizePages   INT;
-                DECLARE @extentID        INT;
-                DECLARE @pageID          INT;
-                DECLARE @DIFFTotal       BIGINT;
-                DECLARE @sizeTotal       BIGINT;
-                DECLARE @total           BIGINT;
-                DECLARE @dbccPageString  VARCHAR (200);
-
-                SELECT @DIFFtotal = 0;
-                SELECT @sizeTotal = 0;
-
-                DECLARE [files] CURSOR FOR
-                SELECT [file_id], [size] FROM master.sys.master_files
+            $MasterFilesQuery = "
+                SELECT [file_id], [size], database_id, db_name(database_id) as dbname FROM master.sys.master_files
                 WHERE [type_desc] = N'ROWS'
-                AND [state_desc] = N'ONLINE'
-                AND [database_id] = DB_ID (@dbName);
-
-                OPEN files;
-
-                FETCH NEXT FROM [files] INTO @fileID, @fileSizePages;
-
-                WHILE @@FETCH_STATUS = 0
-                BEGIN
-                SELECT @extentID = 0;
-
-                SELECT @sizeTotal = @sizeTotal + @fileSizePages / 8;
-
-                WHILE (@extentID < @fileSizePages)
-                BEGIN
-                SELECT @pageID = @extentID + 6;
-
-                SELECT @dbccPageString = 'DBCC PAGE (['
-                + @dbName + '], '
-                + CAST (@fileID AS VARCHAR) + ', '
-                + CAST (@pageID AS VARCHAR) + ', 3) WITH TABLERESULTS, NO_INFOMSGS';
-
-                TRUNCATE TABLE [tempdb].[dbo].[SQLskillsDBCCPage];
-                INSERT INTO [tempdb].[dbo].[SQLskillsDBCCPage] EXEC (@dbccPageString);
-
-                SELECT @total = SUM ([tempdb].[dbo].[SQLskillsConvertToExtents] ([Field]))
-                FROM [tempdb].[dbo].[SQLskillsDBCCPage]
-                WHERE [VALUE] = '    CHANGED'
-                AND [ParentObject] LIKE 'DIFF_MAP%';
-
-                SET @DIFFtotal = @DIFFtotal + @total;
-
-                SET @extentID = @extentID + 511232;
-                END
-
-                FETCH NEXT FROM [files] INTO @fileID, @fileSizePages;
-                END;
-
-                DROP TABLE [tempdb].[dbo].[SQLskillsDBCCPage];
-                CLOSE [files];
-                DEALLOCATE [files];
-
-                SELECT
-                @sizeTotal AS [Total Extents],
-                @DIFFtotal AS [Changed Extents],
-                ROUND (
-                (CONVERT (FLOAT, @DIFFtotal) /
-                CONVERT (FLOAT, @sizeTotal)) * 100, 2) AS [Percentage Changed];
-                END;
-                GO
             "
-
-            # Extents are a collection of eight physically contiguous pages and are used to efficiently manage the pages.
-            # All pages are stored in extents.
-            $SQLskillsDIFForFULL2017 = "
-                SELECT
-                total_page_count / 8 as [Total Extents],
-                modified_extent_page_count / 8 as [Changed Extents],
-                (100 * modified_extent_page_count)/total_page_count as [Percentage Changed]
-                FROM sys.dm_db_file_space_usage
-                GO
-            "
-
-            if ($server.VersionMajor -ge 14 ) {
-                $queryToRun = $SQLskillsDIFForFULL2017
-            }
-            else {
-                $queryToRun = $SQLskillsDIFForFULL
-            }
-
-            $parsedQuery = @()
-            foreach ($line in $queryToRun) {
-                $line = $line -replace '(^\s+|\s+$)', ''
-                $line = $line -replace "(?m)^\s+"
-                $line = $line -replace "`t", ''
-                $parsedQuery += $line + "`r`n"
-            }
-
+            # Exclude inaccessible dbs
             if (-not $server.Databases[$Database]) {
                 Stop-Function -Message "Database $Database does not exist on $server"
-                return
-            }
-            else {
-                $db = $server.Databases[$Database]
             }
 
-            # For SQL Server version lower than 2017 we need to create some objects
-            if ($server.VersionMajor -lt 14 ) {
-                try {
-                    foreach ($query in ($parsedQuery -Split "\nGO\b")) {
-                        $null = Invoke-DbaSqlCmd -ServerInstance $instance -Query $query -Credential $SqlCredential -Database tempdb
+            $MasterFiles = $server.Query($MasterFilesQuery)
+            $MasterFiles = $MasterFiles | Where-Object dbname -in $Database
+
+            $MasterFilesGrouped = $MasterFiles | Group-Object -Property dbname
+
+            if ($server.VersionMajor -ge 14 ) {
+                $DBCCPageQueryDMV = "
+                    SELECT
+                    total_page_count / 8 as [ExtentsTotal],
+                    modified_extent_page_count / 8 as [ExtentsChanged],
+                    (100 * modified_extent_page_count)/total_page_count as [ChangedPerc]
+                    FROM sys.dm_db_file_space_usage
+                "
+
+                foreach ($db in $MasterFilesGrouped) {
+                    foreach ($results in $db.Group) {
+                        $DBCCPageResults = $server.Databases[$($results.dbname)].Query($DBCCPageQueryDMV)
+                        [pscustomobject]@{
+                            ComputerName   = $server.NetName
+                            InstanceName   = $server.ServiceName
+                            SqlInstance    = $server.DomainInstanceName
+                            DatabaseName   = $db.Name
+                            ExtentsTotal   = $DBCCPageResults.ExtentsTotal
+                            ExtentsChanged = $DBCCPageResults.ExtentsChanged
+                            ChangedPerc    = $DBCCPageResults.ChangedPerc
+                        }
                     }
                 }
-                catch {
-                    Stop-Function -Message "Could not execute $query in $Database on $instance" -ErrorRecord $_ -Target $db -Continue
-                }
-            }
-
-            if ($server.VersionMajor -lt 14 ) {
-                $runIt = "EXEC [tempdb].[dbo].[sp_SQLskillsDIFForFULL] N`'" + $Database + "`'"
-                $cleanIt = "
-                    IF OBJECT_ID (N'sp_SQLskillsDIFForFULL') IS NOT NULL
-                        DROP PROCEDURE [sp_SQLskillsDIFForFULL];
-                    IF EXISTS (SELECT * FROM sys.objects WHERE NAME = N'SQLskillsConvertToExtents')
-                        DROP FUNCTION [SQLskillsConvertToExtents];
-                "
-            }
-            else {
-                $runIt = $SQLskillsDIFForFULL2017
-            }
-
-            try {
-                $result = $db.Query($runIt)
-                Add-Member -InputObject $result -Name FullName -MemberType NoteProperty -Value $instance.FullName
-                Add-Member -InputObject $result -Name ComputerName -MemberType NoteProperty -Value $instance.ComputerName
-                Add-Member -InputObject $result -Name InstanceName -MemberType NoteProperty -Value $instance.InstanceName
-                Add-Member -InputObject $result -Name Database -MemberType NoteProperty -Value $Database
-                $defaults = 'ComputerName', 'FullName', 'InstanceName', 'Database', 'Total Extents', 'Changed Extents', 'Percentage Changed'
-                Select-DefaultView -InputObject $result -Property $defaults
-            }
-            catch {
-                Stop-Function -Message "Could not execute $query in $Database on $instance" -ErrorRecord $_ -Target $db -Continue
-            }
-
-            # Cleanup
-            if ($server.VersionMajor -lt 14 ) {
-                try {
-                    $null = Invoke-DbaSqlCmd -ServerInstance $instance -Query $cleanIt -Credential $SqlCredential -Database tempdb
-                }
-                catch {
-                    Stop-Function -Message "Could not execute $query in $Database on $instance" -ErrorRecord $_ -Target $db -Continue
+            } else {
+                foreach ($db in $MasterFilesGrouped) {
+                    $sizeTotal = 0
+                    foreach ($results in $db.Group) {
+                        $extentID = 0
+                        $sizeTotal = $sizeTotal + $results.size / 8
+                        $dbExtents = @()
+                        while ($extentID -lt $results.size) {
+                            $pageID = $extentID + 6
+                            $DBCCPageQuery = "DBCC PAGE ('$($results.dbname)', $($results.file_id), $pageID, 3)  WITH TABLERESULTS, NO_INFOMSGS"
+                            $DBCCPageResults = $server.Query($DBCCPageQuery)
+                            $dbExtents += $DBCCPageResults | Where-Object { $_.VALUE -eq '    CHANGED' -And $_.ParentObject -like 'DIFF_MAP*'}
+                            $extentID = $extentID + 511232
+                        }
+                        $extents = Get-DbaExtents $dbExtents.Field
+                        [pscustomobject]@{
+                            ComputerName   = $server.NetName
+                            InstanceName   = $server.ServiceName
+                            SqlInstance    = $server.DomainInstanceName
+                            DatabaseName   = $db.Name
+                            ExtentsTotal   = $sizeTotal
+                            ExtentsChanged = $extents
+                            ChangedPerc    = [math]::Round(($extents / $sizeTotal * 100), 2)
+                        }
+                    }
                 }
             }
         }
