84e415c2f8bf226b1df66f01526712644dca0a4f
potatoqualitee <clemaire@gmail.com>
2018-03-23 23:32:42 +0100

    removed trailing, nulled out warning

--- tests/Get-DbaAgentAlert.Tests.ps1
+++ tests/Get-DbaAgentAlert.Tests.ps1
@@ -4,14 +4,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2 -Database master
-        $server.Query("EXEC msdb.dbo.sp_add_alert @name=N'dbatoolsci test alert',
-    	@message_id=0, 
-		@severity=6, 
-		@enabled=1, 
-		@delay_between_responses=0, 
-		@include_event_description_in=0, 
-		@category_name=N'[Uncategorized]', 
-		@job_id=N'00000000-0000-0000-0000-000000000000'")
+        $server.Query("EXEC msdb.dbo.sp_add_alert @name=N'dbatoolsci test alert',@message_id=0,@severity=6,@enabled=1,@delay_between_responses=0,@include_event_description_in=0,@category_name=N'[Uncategorized]',@job_id=N'00000000-0000-0000-0000-000000000000'")
     }
     AfterAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2 -Database master
