c1878811cbf21fb187161d8382297b153788fd9b
potatoqualitee <clemaire@gmail.com>
2018-03-23 18:58:54 +0100

    removed trailing

--- tests/Copy-DbaAgentAlert.Tests.ps1
+++ tests/Copy-DbaAgentAlert.Tests.ps1
@@ -5,12 +5,12 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2 -Database master
         $server.Query("EXEC msdb.dbo.sp_add_alert @name=N'dbatoolsci test alert',
-    	@message_id=0, 
-		@severity=6, 
-		@enabled=1, 
-		@delay_between_responses=0, 
-		@include_event_description_in=0, 
-		@category_name=N'[Uncategorized]', 
+    	@message_id=0,
+		@severity=6,
+		@enabled=1,
+		@delay_between_responses=0,
+		@include_event_description_in=0,
+		@category_name=N'[Uncategorized]',
 		@job_id=N'00000000-0000-0000-0000-000000000000'")
     }
     AfterAll {
