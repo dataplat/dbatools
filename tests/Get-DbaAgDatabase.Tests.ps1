db94bed7b5369782f9d76b179d62e96a311708e5
potatoqualitee <clemaire@gmail.com>
2018-03-29 19:51:38 +0200

    oops sorry, added back variable

--- tests/Get-DbaAgDatabase.Tests.ps1
+++ tests/Get-DbaAgDatabase.Tests.ps1
@@ -24,7 +24,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 InModuleScope dbatools {
     Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
         Mock Connect-SqlInstance {
-            Import-Clixml C:\github\appveyor-lab\agserver.xml
+            Import-Clixml $script:appveyorlabrepo\agserver.xml
         }
         Context "gets ag databases" {
             $results = Get-DbaAgDatabase -SqlInstance sql2016c
