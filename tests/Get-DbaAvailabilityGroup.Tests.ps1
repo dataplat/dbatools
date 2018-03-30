5c46fe9dcddd3d62f3bad81ad1343e1bf579d22d
potatoqualitee <clemaire@gmail.com>
2018-03-30 22:43:23 +0200

    one more go before moving it to disabled

--- tests/Get-DbaAvailabilityGroup.Tests.ps1
+++ tests/Get-DbaAvailabilityGroup.Tests.ps1
@@ -23,47 +23,6 @@ Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     $dbname = "dbatoolsci_agroupdb"
-    if ($env:COMPUTERNAME -ne 'APPVYR-WIN') {
-        BeforeAll {
-            # $script:instance2 - to make it appear in the proper place on appveyor
-            Get-DbaProcess -SqlInstance $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-            $server = Connect-DbaInstance -SqlInstance $script:instance3
-            $computername = $server.NetName
-            $servicename = $server.ServiceName
-            if ($servicename -eq 'MSSQLSERVER') {
-                $instancename = "$computername"
-            }
-            else {
-                $instancename = "$computername\$servicename"
-            }
-            $dbname = "dbatoolsci_agroupdb"
-            $server.Query("create database $dbname")
-            $backup = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
-            $server.Query("IF NOT EXISTS (select * from sys.symmetric_keys where name like '%DatabaseMasterKey%') CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<StrongPassword>'")
-            $server.Query("IF EXISTS ( SELECT * FROM sys.tcp_endpoints WHERE name = 'End_Mirroring') DROP ENDPOINT endpoint_mirroring")
-            $server.Query("CREATE CERTIFICATE dbatoolsci_AGCert WITH SUBJECT = 'AG Certificate'")
-            $server.Query("CREATE ENDPOINT dbatoolsci_AGEndpoint
-                            STATE = STARTED
-                            AS TCP (LISTENER_PORT = 5022,LISTENER_IP = ALL)
-                            FOR DATABASE_MIRRORING (AUTHENTICATION = CERTIFICATE dbatoolsci_AGCert,ROLE = ALL)")
-            $server.Query("CREATE AVAILABILITY GROUP dbatoolsci_agroup
-                            WITH (DB_FAILOVER = OFF, DTC_SUPPORT = NONE, CLUSTER_TYPE = NONE)
-                            FOR DATABASE $dbname REPLICA ON N'$instancename'
-                            WITH (ENDPOINT_URL = N'TCP://$computername`:5022', FAILOVER_MODE = MANUAL, AVAILABILITY_MODE = SYNCHRONOUS_COMMIT)")
-        }
-        AfterAll {
-            try {
-                if ($backup.BackupPath) { Remove-Item -Path $backup.BackupPath -ErrorAction SilentlyContinue }
-                $server.Query("DROP AVAILABILITY GROUP dbatoolsci_agroup")
-                Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Remove-DbaDatabase -Confirm:$false
-                $server.Query("DROP ENDPOINT dbatoolsci_AGEndpoint")
-                $server.Query("DROP CERTIFICATE dbatoolsci_AGCert")
-            }
-            catch {
-                # dont care
-            }
-        }
-    }
     Context "gets ags" {
         $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3
         It "returns results with proper data" {
