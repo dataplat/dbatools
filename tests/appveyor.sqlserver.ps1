c608119fcd9c7a237a9599eadfaf17e5472da0cf
niphlod <niphlod@gmail.com>
2018-04-03 21:40:33 +0200

    better durations for setup scripts

--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -11,10 +11,10 @@ if ($env:SCENARIO) {
     $Setup_Scripts = $env:SETUP_SCRIPTS.split(',').Trim()
     foreach ($Setup_Script in $Setup_Scripts) {
         $SetupScriptPath = Join-Path $env:APPVEYOR_BUILD_FOLDER $Setup_Script
-        Add-AppveyorTest -Name "Setup" -Framework NUnit -FileName $Setup_Script -Outcome Running
+        Add-AppveyorTest -Name $Setup_Script -Framework NUnit -FileName $Setup_Script -Outcome Running
         $sw = [system.diagnostics.stopwatch]::startNew()
         . $SetupScriptPath
         $sw.Stop()
-        Update-AppveyorTest -Name "Setup" -Framework NUnit -FileName $Setup_Script -Outcome Passed -Duration $sw.ElapsedMilliseconds
+        Update-AppveyorTest -Name $Setup_Script -Framework NUnit -FileName $Setup_Script -Outcome Passed -Duration $sw.ElapsedMilliseconds
     }
 }
