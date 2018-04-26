9388c286621e63ab8b57721e1b0dae52245244b4
Cláudio Silva <claudiosil100@gmail.com>
2018-04-26 10:03:47 +0100

    Fixes indentation

--- tests/Get-DbaDbCheckConstraint.Tests.ps1
+++ tests/Get-DbaDbCheckConstraint.Tests.ps1
@@ -10,10 +10,10 @@ Describe "$CommandName Unit Tests" -Tags 'UnitTests' {
                 - Commands that *do not* include SupportShouldProcess, set defaultParamCount    = 11
                 - Commands that *do* include SupportShouldProcess, set defaultParamCount        = 13
         #>
-        $paramCount = 8
+        $paramCount = 6
         $defaultParamCount = 11
         [object[]]$params = (Get-ChildItem function:\Get-DbaDbCheckConstraint).Parameters.Keys
-		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'Database', 'ExcludeDatabase', 'Table', 'ExcludeTable', 'ExcludeSystemTable'
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'Database', 'ExcludeDatabase', 'ExcludeSystemTable'
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
         }
@@ -24,39 +24,39 @@ Describe "$CommandName Unit Tests" -Tags 'UnitTests' {
 }
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
-	BeforeAll {
+    BeforeAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-		$random = Get-Random
-		$tableName = "dbatools_getdbtbl1"
-		$tableName2 = "dbatools_getdbtbl2"
+        $random = Get-Random
+        $tableName = "dbatools_getdbtbl1"
+        $tableName2 = "dbatools_getdbtbl2"
         $ckName = "dbatools_getdbck"
         $dbname = "dbatoolsci_getdbfk$random"
         $server.Query("CREATE DATABASE $dbname")
-		$server.Query("CREATE TABLE $tableName (idTbl1 INT)", $dbname)
-		$server.Query("CREATE TABLE $tableName2 (idTbl2 INT, idTbl1 INT, id3 INT)", $dbname)
-		$server.Query("ALTER TABLE $tableName2 ADD CONSTRAINT $ckName CHECK (id3 > 10)", $dbname)
+        $server.Query("CREATE TABLE $tableName (idTbl1 INT)", $dbname)
+        $server.Query("CREATE TABLE $tableName2 (idTbl2 INT, idTbl1 INT, id3 INT)", $dbname)
+        $server.Query("ALTER TABLE $tableName2 ADD CONSTRAINT $ckName CHECK (id3 > 10)", $dbname)
     }
 
-	AfterAll {
-		$null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
-	}
+    AfterAll {
+        $null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    }
 
-	Context "Can get table check constraints" {
-		It "returns no check constraints from excluded DB with -ExcludeDatabase" {
-			$results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -ExcludeDatabase master
-			$results.where( {$_.Database -eq 'master'}).count | Should Be 0
-		}
-		It "returns only check constraints from selected DB with -Database" {
-			$results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -Database $dbname
-			$results.where( {$_.Database -ne 'master'}).count | Should Be 1
-		}
-		It "returns no check constraints with -ExcludeTable" {
-			$results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -ExcludeTable $tableName2
-			$results.count | Should Be 0
-		}
-		It "returns check constraints without -ExcludeTable" {
-			$results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2
-			$results.count | Should BeGreaterThan 0
-		}
-	}
+    Context "Can get table check constraints" {
+        It "returns no check constraints from excluded DB with -ExcludeDatabase" {
+            $results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -ExcludeDatabase master
+            $results.where( {$_.Database -eq 'master'}).count | Should Be 0
+        }
+        It "returns only check constraints from selected DB with -Database" {
+            $results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -Database $dbname
+            $results.where( {$_.Database -ne 'master'}).count | Should Be 1
+        }
+        It "returns no check constraints with -ExcludeTable" {
+            $results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2 -ExcludeTable $tableName2
+            $results.count | Should Be 0
+        }
+        It "returns check constraints without -ExcludeTable" {
+            $results = Get-DbaDbCheckConstraint -SqlInstance $script:instance2
+            $results.count | Should BeGreaterThan 0
+        }
+    }
 }
--- tests/Get-DbaDbForeignKey.Tests.ps1
+++ tests/Get-DbaDbForeignKey.Tests.ps1
@@ -10,10 +10,10 @@ Describe "$CommandName Unit Tests" -Tags 'UnitTests' {
                 - Commands that *do not* include SupportShouldProcess, set defaultParamCount    = 11
                 - Commands that *do* include SupportShouldProcess, set defaultParamCount        = 13
         #>
-        $paramCount = 8
+        $paramCount = 6
         $defaultParamCount = 11
         [object[]]$params = (Get-ChildItem function:\Get-DbaDbForeignKey).Parameters.Keys
-		$knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'Database', 'ExcludeDatabase', 'Table', 'ExcludeTable', 'ExcludeSystemTable'
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException', 'Database', 'ExcludeDatabase', 'ExcludeSystemTable'
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
         }
@@ -24,39 +24,39 @@ Describe "$CommandName Unit Tests" -Tags 'UnitTests' {
 }
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
-	BeforeAll {
+    BeforeAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-		$random = Get-Random
-		$tableName = "dbatools_getdbtbl1"
-		$tableName2 = "dbatools_getdbtbl2"
+        $random = Get-Random
+        $tableName = "dbatools_getdbtbl1"
+        $tableName2 = "dbatools_getdbtbl2"
         $fkName = "dbatools_getdbfk"
         $dbname = "dbatoolsci_getdbfk$random"
         $server.Query("CREATE DATABASE $dbname")
-		$server.Query("CREATE TABLE $tableName (idTbl1 INT)", $dbname)
-		$server.Query("CREATE TABLE $tableName2 (idTbl2 INT, idTbl1 INT)", $dbname)
-		$server.Query("ALTER TABLE $tableName2 ADD CONSTRAINT $fkName FOREIGN KEY (idTbl1) REFERENCES $tableName (idTbl1) ON UPDATE NO ACTION ON DELETE NO ACTION ", $dbname)
+        $server.Query("CREATE TABLE $tableName (idTbl1 INT)", $dbname)
+        $server.Query("CREATE TABLE $tableName2 (idTbl2 INT, idTbl1 INT)", $dbname)
+        $server.Query("ALTER TABLE $tableName2 ADD CONSTRAINT $fkName FOREIGN KEY (idTbl1) REFERENCES $tableName (idTbl1) ON UPDATE NO ACTION ON DELETE NO ACTION ", $dbname)
     }
 
-	AfterAll {
-		$null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
-	}
+    AfterAll {
+        $null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    }
 
-	Context "Can get table foreign keys" {
-		It "returns no foreign keys from excluded DB with -ExcludeDatabase" {
-			$results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -ExcludeDatabase master
-			$results.where( {$_.Database -eq 'master'}).count | Should Be 0
-		}
-		It "returns only foreign keys from selected DB with -Database" {
-			$results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -Database $dbname
-			$results.where( {$_.Database -ne 'master'}).count | Should Be 1
-		}
-		It "returns no foreign keys with -ExcludeTable" {
-			$results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -ExcludeTable $tableName2
-			$results.count | Should Be 0
-		}
-		It "returns foreign keys without -ExcludeTable" {
-			$results = Get-DbaDbForeignKey -SqlInstance $script:instance2
-			$results.count | Should BeGreaterThan 0
-		}
-	}
+    Context "Can get table foreign keys" {
+        It "returns no foreign keys from excluded DB with -ExcludeDatabase" {
+            $results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -ExcludeDatabase master
+            $results.where( {$_.Database -eq 'master'}).count | Should Be 0
+        }
+        It "returns only foreign keys from selected DB with -Database" {
+            $results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -Database $dbname
+            $results.where( {$_.Database -ne 'master'}).count | Should Be 1
+        }
+        It "returns no foreign keys with -ExcludeTable" {
+            $results = Get-DbaDbForeignKey -SqlInstance $script:instance2 -ExcludeTable $tableName2
+            $results.count | Should Be 0
+        }
+        It "returns foreign keys without -ExcludeTable" {
+            $results = Get-DbaDbForeignKey -SqlInstance $script:instance2
+            $results.count | Should BeGreaterThan 0
+        }
+    }
 }
