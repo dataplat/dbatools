fb0980e9e4859b26455f7ee7a538502a00281e29
Chrissy LeMaire <clemaire@gmail.com>
2018-05-17 15:52:21 +0200

    remove forced instance

--- tests/Get-DbaDbPageInfo.Tests.ps1
+++ tests/Get-DbaDbPageInfo.Tests.ps1
@@ -1,7 +1,7 @@
 $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
-$script:instance2 = "sql2017"
+
 Describe "$commandname Unit Tests" -Tag "UnitTests" {
     BeforeAll {
         $random = Get-Random
