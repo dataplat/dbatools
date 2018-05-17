7ead2ed81293d2bbab6a43002bd106c7cb16d2ff
Chrissy LeMaire <clemaire@gmail.com>
2018-05-17 12:16:15 +0200

    command and test updates

--- tests/Get-DbaDbPageInfo.Tests.ps1
+++ tests/Get-DbaDbPageInfo.Tests.ps1
@@ -1,62 +1,38 @@
 $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
-
-Describe "$commandname Unit Tests" -Tags "UnitTests" {
-
+$script:instance2 = "sql2017"
+Describe "$commandname Unit Tests" -Tag "UnitTests" {
     BeforeAll {
-        # Get a random value for the database name
         $random = Get-Random
-
-        # Setup the database name
         $dbname = "dbatoolsci_pageinfo_$random"
-
-        # Remove the database if it exists
-        Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
-
-        # Get a server object
-        $server = Connect-DbaInstance -SqlInstance $script:instance1
-
-        if($server.VersionMajor -ge 11){
-            # Create the database
-            $server.Query("CREATE DATABASE $dbname;")
-
-            # Create the test table
-            $server.Databases[$dbname].Query('CREATE TABLE [dbo].[TestTable](TestText VARCHAR(MAX) NOT NULL)')
+        Get-DbaProcess -SqlInstance $script:instance2 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $server.Query("CREATE DATABASE $dbname;")
+        $server.Databases[$dbname].Query('CREATE TABLE [dbo].[TestTable](TestText VARCHAR(MAX) NOT NULL)')
+        $query = "
+                INSERT INTO dbo.TestTable
+                (
+                    TestText
+                )
+                VALUES
+                ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')"
+        
+        # Generate a bunch of extra inserts to create enough pages
+        1..100 | ForEach-Object {
+            $query += ",('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')"
         }
+        $server.Databases[$dbname].Query($query)
     }
-
     AfterAll {
-        # Remove the database if it exists
-        Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
+        Remove-DbaDatabase -SqlInstance $script:instance2 -Database $dbname -Confirm:$false
     }
-
     Context "Count Pages" {
-        if($server.VersionMajor -ge 11){
-            # Setup the initial query
-            $query = "
-INSERT INTO dbo.TestTable
-(
-    TestText
-)
-VALUES
-('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')"
-
-            # Generate a bunch of extra inserts to create enough pages
-            for ($i = 0; $i -lt 500; $i++) {
-                $query += ",('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')"
-            }
-
-            # Execute the query
-            $server.Databases[$dbname].Query($query)
-
-            $result = Get-DbaDbPageInfo -SqlInstance $script:instance1 -Database $dbname
-
-            $result.Count | Should Be 17
-            ($result | Where-Object {$_.IsAllocated -eq $false}).Count | Should Be 3
-            ($result | Where-Object {$_.IsAllocated -eq $true}).Count | Should Be 14
+        $result = Get-DbaDbPageInfo -SqlInstance $script:instance2 -Database $dbname
+        It "returns the proper results" {
+            ($result).Count | Should -Be 9
+            ($result | Where-Object { $_.IsAllocated -eq $false }).Count | Should -Be 5
+            ($result | Where-Object { $_.IsAllocated -eq $true }).Count | Should -Be 4
         }
-
     }
-
 }
