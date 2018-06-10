dadadb47a76f84c309bda2451d0543de5a2befa9
wsmelton <wshawnmelton@gmail.com>
2018-06-10 01:02:01 -0500

    fixing singular of database tag

--- functions/Get-DbaOrphanUser.ps1
+++ functions/Get-DbaOrphanUser.ps1
@@ -31,7 +31,7 @@ function Get-DbaOrphanUser {
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
         .NOTES
-            Tags: Orphan, Databases
+            Tags: Orphan, Database, User, Security, Login
             Author: Claudio Silva (@ClaudioESSilva)
             Author: Garry Bargsley (@gbargsley)
             Editor: Simone Bizzotto (@niphlod)
