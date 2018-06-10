0539586cf00b45187af8925ab10ea5348c136af2
wsmelton <wshawnmelton@gmail.com>
2018-06-09 22:40:18 -0500

    get-dbaopentransaction add tags

--- functions/Get-DbaOpenTransaction.ps1
+++ functions/Get-DbaOpenTransaction.ps1
@@ -19,6 +19,7 @@
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
         .NOTES
+            Tags: Database, Process, Session, ActivityMonitor
             Website: https://dbatools.io
             Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
             License: MIT https://opensource.org/licenses/MIT
