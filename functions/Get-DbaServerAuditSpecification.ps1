78bddaefdb23adb67bd13175f9a149b182dc9150
wsmelton <wshawnmelton@gmail.com>
2018-06-09 22:47:30 -0500

    get-dbaserverauditspec add tags

--- functions/Get-DbaServerAuditSpecification.ps1
+++ functions/Get-DbaServerAuditSpecification.ps1
@@ -1,42 +1,44 @@
 function Get-DbaServerAuditSpecification {
     <#
-.SYNOPSIS
-Gets SQL Security Audit Specification information for each instance(s) of SQL Server.
+        .SYNOPSIS
+            Gets SQL Security Audit Specification information for each instance(s) of SQL Server.
 
-.DESCRIPTION
- The Get-DbaServerAuditSpecification command gets SQL Security Audit Specification information for each instance(s) of SQL Server.
+        .DESCRIPTION
+            The Get-DbaServerAuditSpecification command gets SQL Security Audit Specification information for each instance(s) of SQL Server.
 
-.PARAMETER SqlInstance
-SQL Server name or SMO object representing the SQL Server to connect to. This can be a collection and receive pipeline input to allow the function
-to be executed against multiple SQL Server instances.
+        .PARAMETER SqlInstance
+            SQL Server name or SMO object representing the SQL Server to connect to. This can be a collection and receive pipeline input to allow the function
+            to be executed against multiple SQL Server instances.
 
-.PARAMETER SqlCredential
-Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+        .PARAMETER SqlCredential
+            Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
 
-.PARAMETER EnableException
-        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-.NOTES
-Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
+        .NOTES
+            Tags: Audit, Security, SqlAudit
+            Author: Garry Bargsley (@gbargsley), http://blog.garrybargsley.com
 
-dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-Copyright (C) 2016 Chrissy LeMaire
-License: MIT https://opensource.org/licenses/MIT
+            dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
+            Copyright (C) 2016 Chrissy LeMaire
+            License: MIT https://opensource.org/licenses/MIT
 
-.LINK
-https://dbatools.io/Get-DbaServerAuditSpecification
+        .LINK
+            https://dbatools.io/Get-DbaServerAuditSpecification
 
-.EXAMPLE
-Get-DbaServerAuditSpecification -SqlInstance localhost
-Returns all Security Audit Specifications on the local default SQL Server instance
+        .EXAMPLE
+            Get-DbaServerAuditSpecification -SqlInstance localhost
 
-.EXAMPLE
-Get-DbaServerAuditSpecification -SqlInstance localhost, sql2016
-Returns all Security Audit Specifications for the local and sql2016 SQL Server instances
+            Returns all Security Audit Specifications on the local default SQL Server instance
 
-#>
+        .EXAMPLE
+            Get-DbaServerAuditSpecification -SqlInstance localhost, sql2016
+
+            Returns all Security Audit Specifications for the local and sql2016 SQL Server instances
+    #>
     [CmdletBinding()]
     param (
         [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
