707d570a3035e1298a141178214afb4f92dcc4c6
wsmelton <wshawnmelton@gmail.com>
2018-06-09 23:30:03 -0500

    service commands format help add tags

--- functions/Stop-DbaSqlService.ps1
+++ functions/Stop-DbaSqlService.ps1
@@ -1,85 +1,87 @@
 function Stop-DbaSqlService {
     <#
-    .SYNOPSIS
-    Stops SQL Server services on a computer.
+        .SYNOPSIS
+            Stops SQL Server services on a computer.
 
-    .DESCRIPTION
-    Stops the SQL Server related services on one or more computers. Will follow SQL Server service dependencies.
+        .DESCRIPTION
+            Stops the SQL Server related services on one or more computers. Will follow SQL Server service dependencies.
 
-    Requires Local Admin rights on destination computer(s).
+            Requires Local Admin rights on destination computer(s).
 
-    .PARAMETER ComputerName
-    The SQL Server (or server in general) that you're connecting to. This command handles named instances.
+        .PARAMETER ComputerName
+            The SQL Server (or server in general) that you're connecting to. This command handles named instances.
 
-    .PARAMETER InstanceName
-    Only affects services that belong to the specific instances.
+        .PARAMETER InstanceName
+            Only affects services that belong to the specific instances.
 
-    .PARAMETER Credential
-    Credential object used to connect to the computer as a different user.
+        .PARAMETER Credential
+            Credential object used to connect to the computer as a different user.
 
-    .PARAMETER Type
-    Use -Type to collect only services of the desired SqlServiceType.
-    Can be one of the following: "Agent","Browser","Engine","FullText","SSAS","SSIS","SSRS"
+        .PARAMETER Type
+            Use -Type to collect only services of the desired SqlServiceType.
+            Can be one of the following: "Agent","Browser","Engine","FullText","SSAS","SSIS","SSRS"
 
-    .PARAMETER Timeout
-    How long to wait for the start/stop request completion before moving on. Specify 0 to wait indefinitely.
+        .PARAMETER Timeout
+            How long to wait for the start/stop request completion before moving on. Specify 0 to wait indefinitely.
 
-    .PARAMETER InputObject
-    A collection of services from Get-DbaSqlService
+        .PARAMETER InputObject
+            A collection of services from Get-DbaSqlService
 
-    .PARAMETER Force
-    Use this switch to stop dependent services before proceeding with the specified service
+        .PARAMETER Force
+            Use this switch to stop dependent services before proceeding with the specified service
 
-    .PARAMETER EnableException
-    By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-    This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-    Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-    .PARAMETER WhatIf
-    Shows what would happen if the cmdlet runs. The cmdlet is not run.
+        .PARAMETER WhatIf
+            Shows what would happen if the cmdlet runs. The cmdlet is not run.
 
-    .PARAMETER Confirm
-    Prompts you for confirmation before running the cmdlet.
+        .PARAMETER Confirm
+            Prompts you for confirmation before running the cmdlet.
 
-    .PARAMETER Force
-    Will stop dependent SQL Server agents when stopping Engine services.
+        .PARAMETER Force
+            Will stop dependent SQL Server agents when stopping Engine services.
 
-    .NOTES
-    Author: Kirill Kravtsov( @nvarscar )
-    Tags:
-    dbatools PowerShell module (https://dbatools.io)
-    Copyright (C) 2017 Chrissy LeMaire
-    License: MIT https://opensource.org/licenses/MIT
+        .NOTES
+            Tags: Service, SqlServer, Instance, Connect
+            Author: Kirill Kravtsov( @nvarscar )
 
-    .LINK
-    https://dbatools.io/Stop-DbaSqlService
+            Requires Local Admin rights on destination computer(s).
 
-    .EXAMPLE
-    Stop-DbaSqlService -ComputerName sqlserver2014a
+            dbatools PowerShell module (https://dbatools.io)
+            Copyright (C) 2017 Chrissy LeMaire
+            License: MIT https://opensource.org/licenses/MIT
 
-    Stops the SQL Server related services on computer sqlserver2014a.
+        .LINK
+            https://dbatools.io/Stop-DbaSqlService
 
-    .EXAMPLE
-    'sql1','sql2','sql3'| Get-DbaSqlService | Stop-DbaSqlService
+        .EXAMPLE
+            Stop-DbaSqlService -ComputerName sqlserver2014a
 
-    Gets the SQL Server related services on computers sql1, sql2 and sql3 and stops them.
+            Stops the SQL Server related services on computer sqlserver2014a.
 
-    .EXAMPLE
-    Stop-DbaSqlService -ComputerName sql1,sql2 -Instance MSSQLSERVER
+        .EXAMPLE
+            'sql1','sql2','sql3'| Get-DbaSqlService | Stop-DbaSqlService
 
-    Stops the SQL Server services related to the default instance MSSQLSERVER on computers sql1 and sql2.
+            Gets the SQL Server related services on computers sql1, sql2 and sql3 and stops them.
 
-    .EXAMPLE
-    Stop-DbaSqlService -ComputerName $MyServers -Type SSRS
+        .EXAMPLE
+            Stop-DbaSqlService -ComputerName sql1,sql2 -Instance MSSQLSERVER
 
-    Stops the SQL Server related services of type "SSRS" (Reporting Services) on computers in the variable MyServers.
+            Stops the SQL Server services related to the default instance MSSQLSERVER on computers sql1 and sql2.
 
-    .EXAMPLE
-    Stop-DbaSqlService -ComputerName sql1 -Type Engine -Force
+        .EXAMPLE
+            Stop-DbaSqlService -ComputerName $MyServers -Type SSRS
 
-    Stops SQL Server database engine services on sql1 forcing dependent SQL Server Agent services to stop as well.
+            Stops the SQL Server related services of type "SSRS" (Reporting Services) on computers in the variable MyServers.
 
-#>
+        .EXAMPLE
+            Stop-DbaSqlService -ComputerName sql1 -Type Engine -Force
+
+            Stops SQL Server database engine services on sql1 forcing dependent SQL Server Agent services to stop as well.
+    #>
     [CmdletBinding(DefaultParameterSetName = "Server", SupportsShouldProcess = $true)]
     Param (
         [Parameter(ParameterSetName = "Server", Position = 1)]
