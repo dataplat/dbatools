f86abdfb17bb161ea304f99d28d90982a2cd66d9
Chrissy LeMaire <clemaire@gmail.com>
2018-06-10 00:11:00 -1000

    cleanup

--- tests/Get-DbaResourceGovernorClassiferFunction.Tests.ps1
+++ tests/Get-DbaResourceGovernorClassiferFunction.Tests.ps1
@@ -4,26 +4,26 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $sql = "CREATE FUNCTION fnTimeClassifier()  
-                RETURNS sysname  
-                WITH SCHEMABINDING  
-                AS  
-                BEGIN  
-                     RETURN N'gOffHoursProcessing'  
+        $sql = "CREATE FUNCTION dbatoolsci_fnRG()
+                RETURNS sysname
+                WITH SCHEMABINDING
+                AS
+                BEGIN
+                     RETURN N'gOffHoursProcessing'
                 END"
-        
+
         Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query $sql
-        Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query "ALTER RESOURCE GOVERNOR with (CLASSIFIER_FUNCTION = dbo.fnTimeClassifier); ALTER RESOURCE GOVERNOR RECONFIGURE"
+        Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query "ALTER RESOURCE GOVERNOR with (CLASSIFIER_FUNCTION = dbo.dbatoolsci_fnRG); ALTER RESOURCE GOVERNOR RECONFIGURE"
     }
     AfterAll {
         Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query "ALTER RESOURCE GOVERNOR WITH (CLASSIFIER_FUNCTION = NULL); ALTER RESOURCE GOVERNOR RECONFIGURE"
-        Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query "DROP FUNCTION [dbo].[fnTimeClassifier]"
+        Invoke-DbaSqlQuery -SqlInstance $script:instance2 -Query "DROP FUNCTION [dbo].[dbatoolsci_fnRG]"
     }
-    
+
     Context "Command works" {
         It "returns the proper classifier function" {
             $results = Get-DbaResourceGovernorClassiferFunction -SqlInstance $script:instance2
-            $results.Name | Should -Be 'fnTimeClassifier'
+            $results.Name | Should -Be 'dbatoolsci_fnRG'
         }
     }
 }
