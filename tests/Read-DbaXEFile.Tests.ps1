08caffd34054ad85602f883f28decc1f1a27c5b9
wsmelton <wshawnmelton@gmail.com>
2018-06-17 04:27:17 -0500

    read-dbaxefile test - commentout dll file reference

--- tests/Read-DbaXEFile.Tests.ps1
+++ tests/Read-DbaXEFile.Tests.ps1
@@ -4,10 +4,10 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 $base = (Get-Module -Name dbatools).ModuleBase
 
-Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XE.Core.dll"
-Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.Configuration.dll"
-Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.dll"
-Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.Linq.dll"
+# Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XE.Core.dll"
+# Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.Configuration.dll"
+# Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.dll"
+# Add-Type -Path "$base\bin\smo\Microsoft.SqlServer.XEvent.Linq.dll"
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "Verifying command output" {
