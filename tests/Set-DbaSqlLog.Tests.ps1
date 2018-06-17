27d4f1b87e527d68d61e837104b5c3d0d5552ae0
wsmelton <wshawnmelton@gmail.com>
2018-06-17 02:42:26 -0500

    Set-DbaSqlLog fix few buts in tests

--- tests/Set-DbaSqlLog.Tests.ps1
+++ tests/Set-DbaSqlLog.Tests.ps1
@@ -4,10 +4,10 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag "UnitTests" {
     Context "Validate parameters" {
-        $paramCount = 3
+        $paramCount = 5
         $defaultParamCount = 13
         [object[]]$params = (Get-ChildItem function:\Set-DbaSqlLog).Parameters.Keys
-        $knownParameters = 'SqlInstance', 'SqlCredential', 'SizeInKb', 'NumberLog', 'EnableException'
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'SizeInKb', 'NumberOfLog', 'EnableException'
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
         }
@@ -18,23 +18,40 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
 }
 
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $server = Connect-DbaInstance -SqlInstance $script:instance1
+        $currentNumLogFilesINSTANCE1 = $server.NumberOfLogFiles
+        $currentErrorLogSizeKbINSTANCE1 = $server.ErrorLogSizeKb
 
-    Context "Apply NumOfLog to multiple instances" {
-        $results = Set-DbaMaxDop -SqlInstance $script:instance1, $script:instance2 -NumOfLog 3
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $currentNumLogFilesINSTANCE2 = $server.NumberOfLogFiles
+        $currentErrorLogSizeKbINSTANCE2 = $server.ErrorLogSizeKb
+    }
+    AfterAll {
+        $server = Connect-DbaInstance -SqlInstance $script:instance1
+        $server.NumberOfLogFiles = $currentNumLogFilesINSTANCE1 =
+        $server.ErrorLogSizeKb = $currentErrorLogSizeKbINSTANCE1
+        $server.Alter()
+
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $server.NumberOfLogFiles = $currentNumLogFilesINSTANCE2
+        $server.ErrorLogSizeKb = $currentErrorLogSizeKbINSTANCE2
+        $server.Alter()
+    }
+    Context "Apply NumberOfLog to multiple instances" {
+        $results = Set-DbaSqlLog -SqlInstance $script:instance1, $script:instance2 -NumberOfLog 8
         foreach ($result in $results) {
-            It 'Returns NumOfLog set to 3 for each instance' {
-                $result.CurrentNumberErrorLog | Should Be 3
+            It 'Returns NumberOfLog set to 3 for each instance' {
+                $result.CurrentNumberErrorLogs | Should Be 8
             }
         }
     }
-
     Context "Apply SizeInKb to multiple instances" {
-        $results = Set-DbaMaxDop -SqlInstance $script:instance1, $script:instance2 -SizeInKb 100
+        $results = Set-DbaSqlLog -SqlInstance $script:instance1, $script:instance2 -SizeInKb 100
         foreach ($result in $results) {
             It 'Returns SizeInKb set to 100 for each instance' {
-                $result.CurrentErrorLogSizeInKb | Should Be 100
+                $result.CurrentErrorLogSizeKb | Should Be 100
             }
         }
     }
-
 }
