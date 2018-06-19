3ec9a2596a948acfe529a23d8dae306d5f93329a
Chrissy LeMaire <clemaire@gmail.com>
2018-06-19 01:32:20 -1000

    updated to match new outputs

--- tests/Set-DbaErrorLog.Tests.ps1
+++ tests/Set-DbaErrorLog.Tests.ps1
@@ -7,7 +7,7 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
         $paramCount = 5
         $defaultParamCount = 13
         [object[]]$params = (Get-ChildItem function:\Set-DbaErrorLog).Parameters.Keys
-        $knownParameters = 'SqlInstance', 'SqlCredential', 'SizeInKb', 'NumberOfLog', 'EnableException'
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'LogSize', 'LogCount', 'EnableException'
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
         }
@@ -19,39 +19,41 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
 
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $server = Connect-DbaInstance -SqlInstance $script:instance3
-        $currentNumLogFilesINSTANCE3 = $server.NumberOfLogFiles
-        $currentErrorLogSizeKbINSTANCE3 = $server.ErrorLogSizeKb
-
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-        $currentNumLogFilesINSTANCE2 = $server.NumberOfLogFiles
-        $currentErrorLogSizeKbINSTANCE2 = $server.ErrorLogSizeKb
+        $logfiles = $server.NumberOfLogFiles
+        $logsize = $server.ErrorLogSizeKb
+        
+        $server.NumberOfLogFiles = 4
+        $server.ErrorLogSizeKb = 1024
+        $server.Alter()
     }
     AfterAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-        $server.NumberOfLogFiles = $currentNumLogFilesINSTANCE2
-        $server.ErrorLogSizeKb = $currentErrorLogSizeKbINSTANCE2
-        $server.Alter()
-
-        $server = Connect-DbaInstance -SqlInstance $script:instance3
-        $server.NumberOfLogFiles = $currentNumLogFilesINSTANCE3
-        $server.ErrorLogSizeKb = $currentErrorLogSizeKbINSTANCE3
+        $server.NumberOfLogFiles = $logfiles
+        $server.ErrorLogSizeKb = $logsize
         $server.Alter()
     }
-    Context "Apply NumberOfLog to multiple instances" {
-        $results = Set-DbaErrorLog -SqlInstance $script:instance3, $script:instance2 -NumberOfLog 8
+    Context "Apply LogCount to multiple instances" {
+        $results = Set-DbaErrorLog -SqlInstance $script:instance2, $script:instance1 -LogCount 8 -WarningAction SilentlyContinue -WarningVariable warn
         foreach ($result in $results) {
-            It 'Returns NumberOfLog set to 3 for each instance' {
-                $result.CurrentNumberErrorLogs | Should Be 8
+            It 'Returns LogCount set to 3 for each instance' {
+                $result.LogCount | Should Be 8
             }
         }
+        It "returns a warning for invalid version" {
+            $warn | Should Match 'not supported' 
+        }
     }
-    Context "Apply SizeInKb to multiple instances" {
-        $results = Set-DbaErrorLog -SqlInstance $script:instance3, $script:instance2 -SizeInKb 100
+    Context "Apply LogSize to multiple instances" {
+        $results = Set-DbaErrorLog -SqlInstance $script:instance2, $script:instance1 -LogSize 100 -WarningAction SilentlyContinue -WarningVariable warn2
         foreach ($result in $results) {
-            It 'Returns SizeInKb set to 100 for each instance' {
-                $result.CurrentErrorLogSizeKb | Should Be 100
+            It 'Returns LogSize set to 100 for each instance' {
+                $result.LogSize | Should Be 100
             }
+        }
+        
+        It "returns a warning for invalid version" {
+            $warn2 | Should Match 'not supported'
         }
     }
 }
