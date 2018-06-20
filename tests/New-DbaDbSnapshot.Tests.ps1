765cf19b08ffc8658b6465c2e42f8a4009d7f787
Chrissy LeMaire <clemaire@gmail.com>
2018-06-20 04:47:33 -1000

    test update

--- tests/New-DbaDbSnapshot.Tests.ps1
+++ tests/New-DbaDbSnapshot.Tests.ps1
@@ -12,14 +12,14 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
             { New-DbaDbSnapshot -SqlInstance $script:instance2 *> $null } | Should Not Throw "You must specify"
         }
     }
-
+    
     Context "Operations on not supported databases" {
         It "Doesn't support model, master or tempdb" {
             $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database model, master, tempdb
             $result | Should Be $null
         }
-
     }
+    
     Context "Operations on databases" {
         BeforeAll {
             $server = Connect-DbaInstance -SqlInstance $script:instance2
@@ -35,40 +35,44 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
             Remove-DbaDbSnapshot -SqlInstance $script:instance2 -Database $db1, $db2, $db3 -Confirm:$false
             Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2 -Database $db1, $db2, $db3
         }
-
-        if (-not $env:appveyor) {
-            It "Skips over offline databases nicely" {
-                $server.Query("ALTER DATABASE $db3 SET OFFLINE WITH ROLLBACK IMMEDIATE")
-                $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db3
-                $result | Should Be $null
-                $server.Query("ALTER DATABASE $db3 SET ONLINE WITH ROLLBACK IMMEDIATE")
-            }
+        
+        It "Skips over offline databases nicely" {
+            $server.Query("ALTER DATABASE $db3 SET OFFLINE WITH ROLLBACK IMMEDIATE")
+            $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db3
+            $result | Should Be $null
+            $server.Query("ALTER DATABASE $db3 SET ONLINE WITH ROLLBACK IMMEDIATE")
         }
+        
         It "Refuses to accept multiple source databases with a single name target" {
             { New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db1, $db2 -Name "dbatools_Snapped" } | Should Throw
         }
+        
         It "Halts when path is not accessible" {
             { New-DbaDbSnapshot -SqlInstance $script:instance2 -Database $db1 -Path B:\Funnydbatoolspath -EnableException } | Should Throw
         }
+        
         It "Creates snaps for multiple dbs by default" {
-            $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db1, $db2
-            $result | Should Not Be $null
-            foreach ($r in $result) {
-                $r.SnapshotOf -in @($db1, $db2) | Should Be $true
+            $results = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db1, $db2
+            $results | Should Not Be $null
+            foreach ($result in $results) {
+                $result.SnapshotOf -in @($db1, $db2) | Should Be $true
             }
         }
+        
         It "Creates snap with the correct name" {
             $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db1 -Name "dbatools_SnapMe_right"
             $result | Should Not Be $null
             $result.SnapshotOf | Should Be $db1
             $result.Name | Should Be "dbatools_SnapMe_right"
         }
+        
         It "Creates snap with the correct name template" {
             $result = New-DbaDbSnapshot -SqlInstance $script:instance2 -EnableException -Database $db2 -NameSuffix "dbatools_SnapMe_{0}_funny"
             $result | Should Not Be $null
             $result.SnapshotOf | Should Be $db2
             $result.Name | Should Be ("dbatools_SnapMe_{0}_funny" -f $db2)
         }
+        
         It "has the correct default properties" {
             $result = Get-DbaDbSnapshot -SqlInstance $script:instance2 -Database $db2 | Select-Object -First 1
             $ExpectedPropsDefault = 'ComputerName', 'CreateDate', 'InstanceName', 'Name', 'SnapshotOf', 'SqlInstance'
