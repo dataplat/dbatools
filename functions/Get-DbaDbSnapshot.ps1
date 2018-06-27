fc2271f9686e2831df3a7e74c8b7ff17cba338b9
Chrissy LeMaire <clemaire@gmail.com>
2018-06-27 02:57:25 -1000

    niph's fixes

--- functions/Get-DbaDbSnapshot.ps1
+++ functions/Get-DbaDbSnapshot.ps1
@@ -3,60 +3,41 @@ function Get-DbaDbSnapshot {
     <#
     .SYNOPSIS
         Get database snapshots with details
-
     .DESCRIPTION
         Retrieves the list of database snapshot available, along with their base (the db they are the snapshot of) and creation time
-
     .PARAMETER SqlInstance
         The SQL Server that you're connecting to.
-
     .PARAMETER SqlCredential
         Credential object used to connect to the SQL Server as a different user
-
     .PARAMETER Database
         Return information for only specific databases
-
     .PARAMETER ExcludeDatabase
         The database(s) to exclude - this list is auto-populated from the server
-
     .PARAMETER Snapshot
         Return information for only specific snapshots
-
     .PARAMETER ExcludeSnapshot
         The snapshot(s) to exclude - this list is auto-populated from the server
-
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-
     .NOTES
         Tags: Snapshot
         Author: niphlod
-
         Website: https://dbatools.io
         Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
         License: MIT https://opensource.org/licenses/MIT
-
-
     .LINK
          https://dbatools.io/Get-DbaDbSnapshot
-
     .EXAMPLE
         Get-DbaDbSnapshot -SqlInstance sqlserver2014a
-
         Returns a custom object displaying Server, Database, DatabaseCreated, SnapshotOf, SizeMB, DatabaseCreated
-
     .EXAMPLE
         Get-DbaDbSnapshot -SqlInstance sqlserver2014a -Database HR, Accounting
-
         Returns information for database snapshots having HR and Accounting as base dbs
-
     .EXAMPLE
         Get-DbaDbSnapshot -SqlInstance sqlserver2014a -Snapshot HR_snapshot, Accounting_snapshot
-
         Returns information for database snapshots HR_snapshot and Accounting_snapshot
-
 #>
     [CmdletBinding()]
     param (
@@ -73,7 +54,6 @@ function Get-DbaDbSnapshot {
         [Alias('Silent')]
         [switch]$EnableException
     )
-
     process {
         foreach ($instance in $SqlInstance) {
             Write-Message -Level Verbose -Message "Connecting to $instance"
@@ -83,9 +63,7 @@ function Get-DbaDbSnapshot {
             catch {
                 Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
-
             $dbs = $server.Databases | Where-Object DatabaseSnapshotBaseName
-
             if ($Database) {
                 $dbs = $dbs | Where-Object { $Database -contains $_.DatabaseSnapshotBaseName }
             }
@@ -101,15 +79,9 @@ function Get-DbaDbSnapshot {
             if ($ExcludeSnapshot) {
                 $dbs = $dbs | Where-Object { $ExcludeSnapshot -notcontains $_.Name }
             }
-
             foreach ($db in $dbs) {
                 try {
-                    if ($db.Parent.VersionMajor -ge 10) {
-                        $BytesOnDisk = $db.Query("select top 1 BytesOnDisk from fn_virtualfilestats(db_id('$($db.Name)'),null) S JOIN dbo.sysdatabases D on D.dbid = S.dbid")
-                    }
-                    else {
-                        $BytesOnDisk = $db.Query("select top 1 BytesOnDisk from fn_virtualfilestats(db_id('$($db.Name)'),null) S JOIN sys.databases D on D.database_id = S.dbid")
-                    }
+                    $BytesOnDisk = $db.Query("SELECT SUM(BytesOnDisk) AS BytesOnDisk FROM fn_virtualfilestats(DB_ID(),NULL) S JOIN sys.databases D on D.database_id = S.dbid", $db.Name)
                     Add-Member -Force -InputObject $db -MemberType NoteProperty -Name ComputerName -value $server.NetName
                     Add-Member -Force -InputObject $db -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
                     Add-Member -Force -InputObject $db -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
