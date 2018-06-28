c36954a33c453b7983bb9d7dfbb19eda29b4000c
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-06-28 15:40:15 +0200

    Moved to public, added examples

--- functions/Select-DbaObject.ps1
+++ functions/Select-DbaObject.ps1
@@ -50,6 +50,33 @@
     
     .PARAMETER Index
         Specifies an array of objects based on their index values. Enter the indexes in a comma-separated list.
+    
+    .EXAMPLE
+        PS C:\> Get-ChildItem | Select-DbaObject Name, "Length as Size"
+    
+        Selects the properties Name and Length, renaming Length to Size in the process.
+    
+    .EXAMPLE
+        PS C:\> Import-Csv .\file.csv | Select-DbaObject Name, "Length as Size to DbaSize"
+    
+        Selects the properties Name and Length, renaming Length to Size and converting it to [DbaSize] (a userfriendly representation of size numbers)
+    
+    .EXAMPLE
+        PS C:\> $obj = [PSCustomObject]@{ Name = "Foo" }
+        PS C:\> Get-ChildItem | Select-DbaObject FullName, Length, "Name from obj"
+    
+        Selects the properties FullName and Length from the input and the Name property from the object stored in $obj
+    
+    .EXAMPLE
+        PS C:\> $list = @()
+        PS C:\> $list += [PSCustomObject]@{ Type = "Foo"; ID = 1 }
+        PS C:\> $list += [PSCustomObject]@{ Type = "Bar"; ID = 2 }
+        PS C:\> $obj | Select-DbaObject Name, "ID from list WHERE Type = Name"
+    
+        This allows you to LEFT JOIN contents of another variable.
+        Note that it can only do simple property-matching at this point.
+    
+        It will select Name from the objects stored in $obj, and for each of those the ID Property on any object in $list that has a Type property of equal value as Name on the input.
 #>
     [CmdletBinding(DefaultParameterSetName = 'DefaultParameter', RemotingCapability = 'None')]
     param (
