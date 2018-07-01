42de531b14795ab4f5a60dd24b6f0f65b17c61bb
Chrissy LeMaire <clemaire@gmail.com>
2018-07-01 06:01:18 -1000

    agent updates

--- functions/Start-DbaAgentJob.ps1
+++ functions/Start-DbaAgentJob.ps1
@@ -96,82 +96,85 @@ function Start-DbaAgentJob {
         [Alias('Silent')]
         [switch]$EnableException
     )
-
-    begin{
-        # Check the job parameters
-        if((-not $AllJobs) -and (-not $Job)){
-            Stop-Function -Message "Please use one of the job parameters, either -Job or -AllJobs" -Target $instance -Continue
-        }
-    }
-
     process {
+        if ((Test-Bound -not -ParameterName AllJobs) -and (Test-Bound -not -ParameterName Job) -and (Test-Bound -not -ParameterName InputObject)) {
+            Stop-Function -Message "Please use one of the job parameters, either -Job or -AllJobs. Or pipe in a list of jobs." -Target $instance
+            return
+        }
         # Loop through each of the instances
         foreach ($instance in $SqlInstance) {
-            Write-Verbose "Attempting to connect to $instance"
             try {
+                Write-Message -Level Verbose -Message "Connecting to $instance."
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
             }
             catch {
                 Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
-
+            
             # Check if all the jobs need to included
             if ($AllJobs) {
-                $InputObject = $server.JobServer.Jobs
+                $InputObject += $server.JobServer.Jobs
             }
-
+            
             # If a specific job needs to be added
             if (-not $AllJobs -and $Job) {
                 $InputObject = $server.JobServer.Jobs | Where-Object Name -In $Job
             }
-
+            
             # If a job needs to be excluded
             if ($ExcludeJob) {
                 $InputObject = $InputObject | Where-Object Name -NotIn $ExcludeJob
             }
         }
-
+        
         # Loop through each of the jobs
         foreach ($currentjob in $InputObject) {
             $server = $currentjob.Parent.Parent
             $status = $currentjob.CurrentRunStatus
-
+            
             if ($status -ne 'Idle') {
                 Stop-Function -Message "$currentjob on $server is not idle ($status)" -Target $currentjob -Continue
             }
-
+            
             If ($Pscmdlet.ShouldProcess($server, "Starting job $currentjob")) {
                 # Start the job
+                $lastrun = $currentjob.LastRunDate
+                Write-Message -Level Verbose -Message "Last run date was $lastrun"
                 $null = $currentjob.Start()
-
-                # Wait for a period
+                
+                # Wait and refresh so that it has a chance to change status
                 Start-Sleep -Milliseconds $SleepPeriod
-
-                # Refresh the job information
                 $currentjob.Refresh()
-
+                
+                $i = 0
                 # Check if the status is Idle
-                while ($currentjob.CurrentRunStatus -eq 'Idle' -and $i++ -lt 60) {
+                while (($currentjob.CurrentRunStatus -eq 'Idle' -and $i++ -lt 60)) {
+                    Write-Message -Level Verbose -Message "Job $($currentjob.Name) status is $($currentjob.CurrentRunStatus)"
+                    Write-Message -Level Verbose -Message "Job $($currentjob.Name) last run date is $($currentjob.LastRunDate)"
+                    
+                    Write-Message -Level Verbose -Message "Sleeping for $SleepPeriod ms and refreshing"
                     Start-Sleep -Milliseconds $SleepPeriod
                     $currentjob.Refresh()
+                    
+                    # If it failed fast, speed up output
+                    if ($lastrun -ne $currentjob.LastRunDate) {
+                        $i = 600
+                    }
                 }
-
+                
                 # Wait for the job
-                if ($wait) {
+                if (Test-Bound -ParameterName Wait) {
                     while ($currentjob.CurrentRunStatus -ne 'Idle') {
                         Write-Message -Level Output -Message "$currentjob is $($currentjob.CurrentRunStatus)"
                         Start-Sleep -Seconds $WaitPeriod
                         $currentjob.Refresh()
                     }
-                    $currentjob
+                    Get-DbaAgentJob -SqlInstance $server -Job $currentjob.Name
                 }
                 else {
-                    $currentjob
+                    Get-DbaAgentJob -SqlInstance $server -Job $currentjob.Name
                 }
-            } # End if should process
-
-        } # End foreach job
-
-    } # End process
-
+            }
+        }
+    }
 }
