1bb724cc5a523189910b4ee41d44f41102e108b2
Chrissy LeMaire <clemaire@gmail.com>
2018-07-01 08:21:28 -1000

    made test more universal

--- tests/Get-DbaRegisteredServerStore.Tests.ps1
+++ tests/Get-DbaRegisteredServerStore.Tests.ps1
@@ -4,11 +4,10 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "Components are properly retreived" {
-
         It "Should return the right values" {
             $results = Get-DbaRegisteredServerStore -SqlInstance $script:instance2
-            $results.InstanceName | Should Be "SQL2016"
-            $results.DisplayName | Should Be "Central Management Servers"
+            $results.InstanceName | Should -Not -Be $null
+            $results.DisplayName | Should -Be "Central Management Servers"
         }
     }
 }
