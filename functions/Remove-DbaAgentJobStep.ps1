8562c09bcb06e8a0cc680e0f963dceebe7e210ad
wsmelton <wshawnmelton@gmail.com>
2018-07-03 14:44:30 -0500

    remove -- Updating connection verbose message

--- functions/Remove-DbaAgentJobStep.ps1
+++ functions/Remove-DbaAgentJobStep.ps1
@@ -87,7 +87,7 @@ function Remove-DbaAgentJobStep {
 
     process {
         foreach ($instance in $SqlInstance) {
-            Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+            Write-Message -Level Verbose -Message "Connecting to $instance"
 
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
