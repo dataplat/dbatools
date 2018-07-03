41bf7b9798f11ec3ba96cdf433b961e606aad1bf
wsmelton <wshawnmelton@gmail.com>
2018-07-03 14:36:47 -0500

    Get-DbaAgentLog -- Updating connection verbose message

--- functions/Get-DbaAgentLog.ps1
+++ functions/Get-DbaAgentLog.ps1
@@ -60,7 +60,7 @@ function Get-DbaAgentLog {
     )
     process {
         foreach ($instance in $SqlInstance) {
-            Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+            Write-Message -Level Verbose -Message "Connecting to $instance"
 
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
