ea52af168f4c1be008dff110eb899586ad255d36
wsmelton <wshawnmelton@gmail.com>
2018-07-03 14:41:00 -0500

    Get-DbaPolicy -- Updating connection verbose message

--- functions/Get-DbaPolicy.ps1
+++ functions/Get-DbaPolicy.ps1
@@ -68,7 +68,7 @@ function Get-DbaPolicy {
 
     process {
         foreach ($instance in $SqlInstance) {
-            Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+            Write-Message -Level Verbose -Message "Connecting to $instance"
 
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 10
