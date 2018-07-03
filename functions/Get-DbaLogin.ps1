90d93249023a0f93ccc29b478b667d9951afd48f
wsmelton <wshawnmelton@gmail.com>
2018-07-03 14:40:25 -0500

    Get-DbaLogin -- Updating connection verbose message

--- functions/Get-DbaLogin.ps1
+++ functions/Get-DbaLogin.ps1
@@ -153,7 +153,7 @@ function Get-DbaLogin {
 
     process {
         foreach ($instance in $SqlInstance) {
-            Write-Message -Level Verbose -Message "Attempting to connect to $instance"
+            Write-Message -Level Verbose -Message "Connecting to $instance"
 
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential
