5e8baa4671d5d036f5a15eba9dd3aa8f2662558c
Chrissy LeMaire <clemaire@gmail.com>
2018-07-11 08:19:20 -1000

    remove x

--- tests/Get-DbaPlanCache.Tests.ps1
+++ tests/Get-DbaPlanCache.Tests.ps1
@@ -4,7 +4,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     Context "returns proper information" {
-        xIt "returns correct datatypes" {
+        It "returns correct datatypes" {
             $results = Get-DbaPlanCache -SqlInstance $script:instance1 | Clear-DbaPlanCache -Threshold 1024
             $results.UseCount -is [int] | Should -Be $true
             $results.UseCount -is [dbasize] | Should -Be $true
