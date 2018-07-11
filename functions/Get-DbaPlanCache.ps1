06a437a6e4d05a01ea1fdc354699d745028a575c
Chrissy LeMaire <clemaire@gmail.com>
2018-07-11 07:06:50 -1000

    trimmed trailing

--- functions/Get-DbaPlanCache.ps1
+++ functions/Get-DbaPlanCache.ps1
@@ -2,49 +2,49 @@
     <#
         .SYNOPSIS
             Provides information about adhoc and prepared plan cache usage
-    
+
         .DESCRIPTION
             Checks ahoc and prepared plan cache for each database, if over 100 MBS you should consider you using Remove-DbaQueryPlan to clear the plan caches or turning on optimize for adhoc workloads configuration is running 2008 or later.
-    
+
             References: https://www.sqlskills.com/blogs/kimberly/plan-cache-adhoc-workloads-and-clearing-the-single-use-plan-cache-bloat/
-    
+
             Note: This command returns results from all SQL server instances on the destination server but the process column is specific to -SqlInstance passed.
-    
+
         .PARAMETER SqlInstance
             The SQL Server instance. Server version must be SQL Server version XXXX or higher.
 
         .PARAMETER SqlCredential
            Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
-    
+
         .PARAMETER EnableException
             By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
             This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-    
+
         .NOTES
             Tags: Memory
             Author: Tracy Boggiano, databasesuperhero.com
-    
+
             dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
             Copyright (C) 2016 Chrissy LeMaire
             License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
-    
+
         .LINK
             https://dbatools.io/Get-DbaPlanCache
-    
+
         .EXAMPLE
             Get-DbaPlanCache -SqlInstance sql2017
-    
+
             Returns the single use plan cashe usage information for SQL Server instance 2017
-    
+
         .EXAMPLE
             Get-DbaPlanCache -SqlInstance sql2017
-    
+
             Returns the single use plan cashe usage information for SQL Server instance 2017
-    
+
         .EXAMPLE
-            Get-DbaPlanCache -SqlInstance sql2017 -SqlCredential (Get-Credential sqladmin) 
-    
+            Get-DbaPlanCache -SqlInstance sql2017 -SqlCredential (Get-Credential sqladmin)
+
             Returns the single use plan cashe usage information for SQL Server instance 2017 using login 'sqladmin'
     #>
         [CmdletBinding()]
@@ -56,11 +56,11 @@
             [switch]$EnableException
         )
         begin {
-            $Sql = "SELECT MB = sum(cast((CASE WHEN usecounts = 1 AND objtype IN ('Adhoc', 'Prepared') THEN size_in_bytes ELSE 0 END) as decimal(12, 2))) / 1024 / 1024, 
-                    UseCount = sum(CASE WHEN usecounts = 1 AND objtype IN ('Adhoc', 'Prepared') THEN 1 ELSE 0 END) 
+            $Sql = "SELECT MB = sum(cast((CASE WHEN usecounts = 1 AND objtype IN ('Adhoc', 'Prepared') THEN size_in_bytes ELSE 0 END) as decimal(12, 2))) / 1024 / 1024,
+                    UseCount = sum(CASE WHEN usecounts = 1 AND objtype IN ('Adhoc', 'Prepared') THEN 1 ELSE 0 END)
                 FROM sys.dm_exec_cached_plans;"
         }
-    
+
         process {
             foreach ($instance in $SqlInstance) {
                 try {
@@ -70,15 +70,15 @@
                 catch {
                     Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
                 }
-    
+
                 $results = $server.Query($sql)
-                
+
                 Add-Member -Force -InputObject $results -MemberType NoteProperty -Name ComputerName -value $server.NetName
                 Add-Member -Force -InputObject $results -MemberType NoteProperty -Name InstanceName -value $server.ServiceName
                 Add-Member -Force -InputObject $results -MemberType NoteProperty -Name SqlInstance -value $server.DomainInstanceName
                 Add-Member -Force -InputObject $results -MemberType NoteProperty -Name MB -Value $results.MB
                 Add-Member -Force -InputObject $results -MemberType NoteProperty -Name UseCount -Value $results.UseCount
-    
+
                 Select-DefaultView -InputObject $results -Property ComputerName, InstanceName, SqlInstance, MB, UseCount
             }
         }
--- functions/Remove-DbaPlanCache.ps1
+++ functions/Remove-DbaPlanCache.ps1
@@ -2,28 +2,28 @@
     <#
         .SYNOPSIS
             Removes adhoc and prepared plan caches is single use plans are over defined threshold.
-    
+
         .DESCRIPTION
             Checks ahoc and prepared plan cache for each database, if over 100 MBs removes from the cache.
-    
+
             This command automates that process.
-    
+
             References: https://www.sqlskills.com/blogs/kimberly/plan-cache-adhoc-workloads-and-clearing-the-single-use-plan-cache-bloat/
-    
+
             Note: This command removes the plans from all SQL instances on the destionation server but the process column is specific to -SqlInstance passed.
-    
+
         .PARAMETER SqlInstance
             The SQL Server instance. Server version must be SQL Server version XXXX or higher.
 
         .PARAMETER SqlCredential
            Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
-    
+
         .PARAMETER Threshold
             Memory used threshold.
-    
+
         .PARAMETER WhatIf
             If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-    
+
         .PARAMETER Confirm
             If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
 
@@ -31,26 +31,26 @@
             By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
             This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
             Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-    
+
         .NOTES
             Tags: Memory
             Author: Tracy Boggiano, databasesuperhero.com
-    
+
             dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
             Copyright (C) 2016 Chrissy LeMaire
             License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
-    
+
         .LINK
             https://dbatools.io/Remove-DbaPlanCache
-    
+
         .EXAMPLE
             Remove-DbaPlanCache -SqlInstance sql2017 -Threshold 200
-    
+
             Logs into the SQL Server instance "sql2017" and removes plan caches if over 200 MBs.
-    
+
         .EXAMPLE
-            Remove-DbaPlanCache -SqlInstance sql2017 -SqlCredential (Get-Credential sqladmin) 
-    
+            Remove-DbaPlanCache -SqlInstance sql2017 -SqlCredential (Get-Credential sqladmin)
+
             Logs into the SQL instance using the SQL Login 'sqladmin' and then Windows instance as 'ad\sqldba'
             and removes if Threshold over 100 MBs.
     #>
@@ -67,14 +67,14 @@
             foreach ($instance in $SqlInstance) {
                 try {
                     Write-Message -Level Verbose -Message "Connecting to $instance"
-                    $server = Connect-DbaInstance -SqlInstance $instance -SqlCredential $sqlcredential 
+                    $server = Connect-DbaInstance -SqlInstance $instance -SqlCredential $sqlcredential
                 }
                 catch {
                     Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
                 }
-    
+
                 $results = Get-DbaQueryPlan -SqlInstance $instance -SqlCredential $SqlCredential -Threshold $Threshold
-               
+
                 if ($results.MB -ge $Threshold) {
                     if ($Pscmdlet.ShouldProcess("$server", "Cleared SQL Plans plan cache")) {
                         $server.Query("DBCC FREESYSTEMCACHE('SQL Plans')")
