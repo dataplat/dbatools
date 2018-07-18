d5a8c6a74c96655c32fc2a0407df9a3267ae5f32
Chrissy LeMaire <clemaire@gmail.com>
2018-07-18 12:08:26 -1000

    more detailed object migration

--- functions/Copy-DbaSysDbUserObject.ps1
+++ functions/Copy-DbaSysDbUserObject.ps1
@@ -20,6 +20,12 @@ function Copy-DbaSysDbUserObject {
         .PARAMETER DestinationSqlCredential
             Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
 
+        .PARAMETER Classic
+            Perform the migration the old way
+    
+        .PARAMETER Force
+            Drop destination objects first. Has no effect if you use Classic. This doesn't work really well, honestly.
+
         .PARAMETER WhatIf
             If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
@@ -56,9 +62,26 @@ function Copy-DbaSysDbUserObject {
         [ValidateNotNullOrEmpty()]
         [DbaInstanceParameter[]]$Destination,
         [PSCredential]$DestinationSqlCredential,
+        [switch]$Force,
+        [switch]$Classic,
         [Alias('Silent')]
         [switch]$EnableException
     )
+    begin {
+        function get-sqltypename ($type) {
+            switch ($type) {
+                "VIEW" { "View" }
+                "SQL_TABLE_VALUED_FUNCTION" { "Table Valued Function" }
+                "DEFAULT_CONSTRAINT" { "Default Constraint" }
+                "SQL_STORED_PROCEDURE" { "Stored Procedure" }
+                "RULE" { "Rule" }
+                "SQL_INLINE_TABLE_VALUED_FUNCTION" { "Inline Table Valued Function" }
+                "SQL_TRIGGER" { "Trigger" }
+                "SQL_SCALAR_FUNCTION" { "Scalar Function" }
+                default { $type }
+            }
+        }
+    }
     process {
         try {
             Write-Message -Level Verbose -Message "Connecting to $Source"
@@ -85,59 +108,172 @@ function Copy-DbaSysDbUserObject {
             }
             
             if (!(Test-SqlSa -SqlInstance $destServer -SqlCredential $DestinationSqlCredential)) {
-                Stop-Function -Message "Not a sysadmin on $destinstance. Quitting."
-                return
+                Stop-Function -Message "Not a sysadmin on $destinstance" -Continue
             }
             
             $systemDbs = "master", "model", "msdb"
             
-            foreach ($systemDb in $systemDbs) {
-                $sysdb = $sourceServer.databases[$systemDb]
-                $transfer = New-Object Microsoft.SqlServer.Management.Smo.Transfer $sysdb
-                $transfer.CopyAllObjects = $false
-                $transfer.CopyAllDatabaseTriggers = $true
-                $transfer.CopyAllDefaults = $true
-                $transfer.CopyAllRoles = $true
-                $transfer.CopyAllRules = $true
-                $transfer.CopyAllSchemas = $true
-                $transfer.CopyAllSequences = $true
-                $transfer.CopyAllSqlAssemblies = $true
-                $transfer.CopyAllSynonyms = $true
-                $transfer.CopyAllTables = $true
-                $transfer.CopyAllViews = $true
-                $transfer.CopyAllStoredProcedures = $true
-                $transfer.CopyAllUserDefinedAggregates = $true
-                $transfer.CopyAllUserDefinedDataTypes = $true
-                $transfer.CopyAllUserDefinedTableTypes = $true
-                $transfer.CopyAllUserDefinedTypes = $true
-                $transfer.CopyAllUserDefinedFunctions = $true
-                $transfer.CopyAllUsers = $true
-                $transfer.PreserveDbo = $true
-                $transfer.Options.AllowSystemObjects = $false
-                $transfer.Options.ContinueScriptingOnError = $true
-                $transfer.Options.IncludeDatabaseRoleMemberships = $true
-                $transfer.Options.Indexes = $true
-                $transfer.Options.Permissions = $true
-                $transfer.Options.WithDependencies = $false
-                
-                Write-Message -Level Output -Message "Copying from $systemDb."
-                try {
-                    $sqlQueries = $transfer.ScriptTransfer()
+            if (-not $Classic) {
+                foreach ($systemDb in $systemDbs) {
+                    $smodb = $sourceServer.databases[$systemDb]
+                    $tables = $smodb.Tables | Where-Object IsSystemObject -ne $true
                     
-                    foreach ($sql in $sqlQueries) {
-                        Write-Message -Level Debug -Message $sql
-                        if ($PSCmdlet.ShouldProcess($destServer, $sql)) {
+                    foreach ($table in $tables) {
+                        $copyobject = [pscustomobject]@{
+                            SourceServer = $sourceServer.Name
+                            DestinationServer = $destServer.Name
+                            Name         = $table.Name
+                            Type         = "Table in $systemDb"
+                            Status       = $null
+                            Notes        = $null
+                            DateTime     = [Sqlcollaborative.Dbatools.Utility.DbaDateTime](Get-Date)
+                        }
+                        
+                        $transfer = New-Object Microsoft.SqlServer.Management.Smo.Transfer $smodb
+                        $null = $transfer.CopyAllObjects = $false
+                        $null = $transfer.Options.WithDependencies = $true
+                        $null = $transfer.ObjectList.Add($table)
+                        if ($Force) {
+                            $null = $transfer.DropDestinationObjectsFirst = $true
+                        }
+                        $sql = $transfer.ScriptTransfer()
+                        if ($PSCmdlet.ShouldProcess($destServer, "Adding table $($table.Name) to $systemDb")) {
                             try {
-                                $destServer.Query($sql, $systemDb)
+                                Write-Message -Level Debug -Message "$sql"
+                                $null = $destServer.Query($sql, $systemDb)
+                                $copyobject.Status = "Success"
+                                $copyobject.Notes = "May have also created dependencies"
                             }
                             catch {
-                                # Don't care - long story having to do with duplicate stuff
+                                $copyobject.Status = "Failed"
+                                $copyobject.Notes = $_.Exception.InnerException.InnerException.InnerException.InnerException.Message
                             }
+                            $copyobject | Select-DefaultView -Property DateTime, SourceServer, DestinationServer, Name, Type, Status, Notes -TypeName MigrationObject
+                        }
+                    }
+                    
+                    $userobjects = Get-DbaSqlModule -SqlInstance $destServer -Database $systemDb -NoSystemObjects | Sort-Object Type
+                    Write-Message -Level Verbose -Message "Copying from $systemDb."
+                    foreach ($userobject in $userobjects) {
+                        
+                        $name = $userobject.Name
+                        $db = $userobject.Database
+                        $type = get-sqltypename $userobject.Type
+                        $sql = $userobject.Definition
+                        $schema = $userobject.SchemaName
+                        
+                        $copyobject = [pscustomobject]@{
+                            SourceServer = $sourceServer.Name
+                            DestinationServer = $destServer.Name
+                            Name         = $name
+                            Type         = "$type in $systemDb"
+                            Status       = $null
+                            Notes        = $null
+                            DateTime     = [Sqlcollaborative.Dbatools.Utility.DbaDateTime](Get-Date)
+                        }
+                        Write-Message -Level Debug -Message $sql
+                        if ($PSCmdlet.ShouldProcess($destServer, "Adding $type $name to $systemDb")) {
+                            try {
+                                $null = $destServer.Query($sql, $systemDb)
+                                $copyobject.Status = "Successful"
+                            }
+                            catch {
+                                $msg = $_.Exception.InnerException.InnerException.InnerException.InnerException.Message
+                                if ($msg -match "already an object") {
+                                    $copyobject.Status = "Failed"
+                                    $copyobject.Notes = $_.Exception.InnerException.InnerException.InnerException.InnerException.Message
+                                }
+                                else {
+                                    try {
+                                        $smobject = switch ($userobject.Type) {
+                                            "VIEW" { $smodb.Views.Item($name, $schema) }
+                                            "SQL_TABLE_VALUED_FUNCTION" { $smodb.UserDefinedFunctions.Item($name, $schema) }
+                                            "SQL_STORED_PROCEDURE" { $smodb.StoredProcedures.Item($name, $schema) }
+                                            "RULE" { $smodb.Rules.Item($name, $schema) }
+                                            "SQL_INLINE_TABLE_VALUED_FUNCTION" { $smodb.UserDefinedFunctions.Item($name, $schema) }
+                                            "SQL_TRIGGER" { $smodb.Triggers.Item($name, $schema) }
+                                            "SQL_SCALAR_FUNCTION" { $smodb.UserDefinedFunctions.Item($name, $schema) }
+                                        }
+                                        
+                                        if ($smobject) {
+                                            $transfer = New-Object Microsoft.SqlServer.Management.Smo.Transfer $smodb
+                                            $null = $transfer.CopyAllObjects = $false
+                                            $null = $transfer.Options.WithDependencies = $true
+                                            $null = $transfer.ObjectList.Add($smobject)
+                                            if ($Force) {
+                                                $null = $transfer.DropDestinationObjectsFirst = $true
+                                            }
+                                            $sql = $transfer.ScriptTransfer()
+                                            Write-Message -Level Debug -Message "$sql"
+                                            $null = $smodb.Query($sql)
+                                            $copyobject.Status = "Success"
+                                            $copyobject.Notes = "May have also installed dependencies"
+                                        }
+                                        else {
+                                            $copyobject.Status = "Failed"
+                                            $copyobject.Notes = $msg
+                                        }
+                                    }
+                                    catch {
+                                        $copyobject.Status = "Failed"
+                                        $copyobject.Notes = $msg
+                                    }
+                                }
+                            }
+                            $copyobject | Select-DefaultView -Property DateTime, SourceServer, DestinationServer, Name, Type, Status, Notes -TypeName MigrationObject
                         }
                     }
                 }
-                catch {
-                    # Don't care - long story having to do with duplicate stuff
+            }
+            else {
+                foreach ($systemDb in $systemDbs) {
+                    $sysdb = $sourceServer.databases[$systemDb]
+                    $transfer = New-Object Microsoft.SqlServer.Management.Smo.Transfer $sysdb
+                    $transfer.CopyAllObjects = $false
+                    $transfer.CopyAllDatabaseTriggers = $true
+                    $transfer.CopyAllDefaults = $true
+                    $transfer.CopyAllRoles = $true
+                    $transfer.CopyAllRules = $true
+                    $transfer.CopyAllSchemas = $true
+                    $transfer.CopyAllSequences = $true
+                    $transfer.CopyAllSqlAssemblies = $true
+                    $transfer.CopyAllSynonyms = $true
+                    $transfer.CopyAllTables = $true
+                    $transfer.CopyAllViews = $true
+                    $transfer.CopyAllStoredProcedures = $true
+                    $transfer.CopyAllUserDefinedAggregates = $true
+                    $transfer.CopyAllUserDefinedDataTypes = $true
+                    $transfer.CopyAllUserDefinedTableTypes = $true
+                    $transfer.CopyAllUserDefinedTypes = $true
+                    $transfer.CopyAllUserDefinedFunctions = $true
+                    $transfer.CopyAllUsers = $true
+                    $transfer.PreserveDbo = $true
+                    $transfer.Options.AllowSystemObjects = $false
+                    $transfer.Options.ContinueScriptingOnError = $true
+                    $transfer.Options.IncludeDatabaseRoleMemberships = $true
+                    $transfer.Options.Indexes = $true
+                    $transfer.Options.Permissions = $true
+                    $transfer.Options.WithDependencies = $false
+                    
+                    Write-Message -Level Output -Message "Copying from $systemDb."
+                    try {
+                        $sqlQueries = $transfer.ScriptTransfer()
+                        
+                        foreach ($sql in $sqlQueries) {
+                            Write-Message -Level Debug -Message "$sql"
+                            if ($PSCmdlet.ShouldProcess($destServer, $sql)) {
+                                try {
+                                    $destServer.Query($sql, $systemDb)
+                                }
+                                catch {
+                                    # Don't care - long story having to do with duplicate stuff
+                                }
+                            }
+                        }
+                    }
+                    catch {
+                        # Don't care - long story having to do with duplicate stuff
+                    }
                 }
             }
         }
