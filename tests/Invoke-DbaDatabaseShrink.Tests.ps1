76ca2b2cb1e486dff3b5c494003f412e9ee6763c
jpomfret <jpomfret7@gmail.com>
2018-07-18 15:59:25 -0400

    added UnitTests

--- tests/Invoke-DbaDatabaseShrink.Tests.ps1
+++ tests/Invoke-DbaDatabaseShrink.Tests.ps1
@@ -2,21 +2,6 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
-    Context "Validate parameters" {
-        $paramCount = 13
-        $defaultParamCount = 13
-        [object[]]$params = (Get-ChildItem function:\Invoke-DbaDatabaseShrink).Parameters.Keys
-        $knownParameters = 'SqlInstance', 'SqlCredential','Database','ExcludeDatabase','AllUserDatabases','PercentFreeSpace','ShrinkMethod','StatementTimeout','LogsOnly','FileType','ExcludeIndexStats','ExcludeUpdateUsage','EnableException'
-        It "Should contain our specific parameters" {
-            ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
-        }
-        It "Should only contain $paramCount parameters" {
-            $params.Count - $defaultParamCount | Should Be $paramCount
-        }
-    }
-}
-
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "Verifying Database is shrunk" {
         BeforeAll {
