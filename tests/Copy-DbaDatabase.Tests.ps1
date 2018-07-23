6c62f0c9da5f16fcfbba0e154e2e98df9777c98c
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2018-07-23 17:42:03 -0500

    Fixing history tests - now leaving src db alive

--- tests/Copy-DbaDatabase.Tests.ps1
+++ tests/Copy-DbaDatabase.Tests.ps1
@@ -92,7 +92,7 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     Context "UseLastBackups - read backup history" {
         BeforeAll {
             Get-DbaProcess -SqlInstance $script:instance2, $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-            Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2, $script:instance3 -Database $backuprestoredb
+            Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance3 -Database $backuprestoredb
         }
         
         It "copies a database successfully using backup history" {
@@ -115,7 +115,7 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     Context "UseLastBackups with -Continue" {
         BeforeAll {
             Get-DbaProcess -SqlInstance $script:instance2, $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-            Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2, $script:instance3 -Database $backuprestoredb
+            Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance3 -Database $backuprestoredb
             #Pre-stage the restore
             $null = Get-DbaBackupHistory -SqlInstance $script:instance2 -Database $backuprestoredb -LastFull | Restore-DbaDatabase -SqlInstance $script:instance3 -DatabaseName $backuprestoredb -NoRecovery 3>$null
             #Run diff now
