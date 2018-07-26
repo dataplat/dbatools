cd7a36cea03e082dc11bcb2748bc953a9e260601
Chrissy LeMaire <clemaire@gmail.com>
2018-07-26 02:22:54 -1000

    add force test

--- tests/New-DbaAgentJobStep.Tests.ps1
+++ tests/New-DbaAgentJobStep.Tests.ps1
@@ -4,26 +4,33 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "New Agent Job Step is added properly" {
-
+        AfterAll {
+            Remove-DbaAgentJob -SqlInstance $script:instance2 -Job "dbatoolsci Job One"
+        }
         # Create job to add step to
-        $job = New-DbaAgentJob -SqlInstance $script:instance2 -Job "Job One" -Description "Just another job"
+        $job = New-DbaAgentJob -SqlInstance $script:instance2 -Job "dbatoolsci Job One" -Description "Just another job"
 
         It "Should have the right name and description" {
             $results = New-DbaAgentJobStep -SqlInstance $script:instance2 -Job $job -StepName "Step One"
-            $results.Name | Should Be "Step One"
+            $results.Name | Should -Be "Step One"
         }
 
         It "Should actually for sure exist" {
-            $newresults = Get-DbaAgentJob -SqlInstance $script:instance2 -Job "Job One"
-            $newresults.JobSteps.Name | Should Be "Step One"
+            $newresults = Get-DbaAgentJob -SqlInstance $script:instance2 -Job "dbatoolsci Job One"
+            $newresults.JobSteps.Name | Should -Be "Step One"
         }
 
         It "Should not write over existing job steps" {
-            New-DbaAgentJobStep -SqlInstance $script:instance2 -Job "Job One" -StepName "Step One" -WarningAction SilentlyContinue -WarningVariable warn
-            $warn -match "already exists" | Should Be $true
+            New-DbaAgentJobStep -SqlInstance $script:instance2 -Job "dbatoolsci Job One" -StepName "Step One" -WarningAction SilentlyContinue -WarningVariable warn
+            $warn -match "already exists" | Should -Be $true
         }
-
-        # Cleanup and ignore all output
-        Remove-DbaAgentJob -SqlInstance $script:instance2 -Job "Job One" *> $null
+        
+        It "Force should add jobstep to job and the older job step should be second" {
+            $results = New-DbaAgentJobStep -SqlInstance $script:instance2 -Job "dbatoolsci Job One" -StepName "New Step One" -StepId 1 -Force
+            $results.Name | Should -Be "New Step One"
+            $newresults = Get-DbaAgentJob -SqlInstance $script:instance2 -Job "dbatoolsci Job One"
+            $newresults.JobSteps | Where-Object Id -eq 1 | Select-Object -ExpandProperty Name | Should -Be "New Step One"
+            $newresults.JobSteps | Where-Object Id -eq 2 | Select-Object -ExpandProperty Name | Should -Be "Step One"
+        }
     }
 }
