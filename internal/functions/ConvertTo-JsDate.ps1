5b66bb6c05204cb0a19a6449b1e55c9ff49c319a
Chrissy LeMaire <clemaire@gmail.com>
2018-08-14 22:42:11 +0200

    trailing and spaces

--- internal/functions/ConvertTo-JsDate.ps1
+++ internal/functions/ConvertTo-JsDate.ps1
@@ -4,12 +4,12 @@
             Converts Datetime input to a Java Script date format
 
         .DESCRIPTION
-            This function acceptes date time input and converts to a Java script compatible format. 
+            This function acceptes date time input and converts to a Java script compatible format.
             Java Script date time format:
                 New date (yyyy, MM, dd, HH, mm, ss)
 
             This is internal function part of ConvertTo-DbaTimeline
-        
+
         .PARAMETER InputDate
 
             The InputDate parameter must be a valid datetime type
@@ -17,14 +17,14 @@
         .NOTES
             Tags: Internal
             Author: Marcin Gminski (@marcingminski)
-            
+
             Dependency: None
             Requirements: None
 
             Website: https://dbatools.io
             Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
 -           License: MIT https://opensource.org/licenses/MIT
-        
+
         .LINK
             --internal function, not exposed to end user
 
@@ -38,7 +38,7 @@
     param (
         [Parameter(Mandatory = $true)]
         [datetime]
-        $InputDate        
+        $InputDate
     )
     [string]$out = "new Date($(Get-Date $InputDate -format "yyyy"), $($(Get-Date $InputDate -format "MM")-1), $(Get-Date $InputDate -format "dd"), $(Get-Date $InputDate -format "HH"), $(Get-Date $InputDate -format "mm"), $(Get-Date $InputDate -format "ss"))"
     return $out
