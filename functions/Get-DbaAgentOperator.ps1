630a41625fb47c255ba70b5f4cce3a98dce3bbe6
sqlpadawan <sqlpadawan@gmail.com>
2018-08-17 11:15:47 -0400

    Add operator alerts
    Add alerts related to the operator

--- functions/Get-DbaAgentOperator.ps1
+++ functions/Get-DbaAgentOperator.ps1
@@ -100,11 +100,23 @@ function Get-DbaAgentOperator {
                 $jobs = $server.JobServer.jobs | Where-Object { $_.OperatorToEmail, $_.OperatorToNetSend, $_.OperatorToPage -contains $operat.Name }
                 $lastemail = [dbadatetime]$operat.LastEmailDate
 
+				$operatAlerts = @()
+                $alerts = $server.JobServer.alerts
+				foreach($alert in $alerts){                                  
+                    $dtAlert = $alert.EnumNotifications($operat.Name)                   
+                    if ($dtAlert.Rows.Count -gt 0) {
+                        $operatAlerts += $alert.Name
+                    }
+					$alertlastemail = [dbadatetime]$alert.LastOccurrenceDate
+                }
+				
                 Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name ComputerName -Value $server.ComputerName
                 Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name InstanceName -Value $server.ServiceName
                 Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name SqlInstance -Value $server.DomainInstanceName
                 Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name RelatedJobs -Value $jobs
                 Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name LastEmail -Value $lastemail
+                Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name RelatedAlerts -Value $operatAlerts
+                Add-Member -Force -InputObject $operat -MemberType NoteProperty -Name AlertLastEmail -Value $alertlastemail
                 Select-DefaultView -InputObject $operat -Property $defaults
             }
         }
