8fe9aa5bf17e5311f2085cf9913e1739697e4a57
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-08-21 22:52:35 +0200

    reverting changes

--- internal/functions/Select-DefaultView.ps1
+++ internal/functions/Select-DefaultView.ps1
@@ -1,25 +1,16 @@
 function Select-DefaultView {
-<#
-    .SYNOPSIS
-        Wrapper around Select-DbaObject, used to style output.
-        Do not use this function for new commands, instead use Select-DbaObject instead!
+    <#
+
+    This command enables us to send full on objects to the pipeline without the user seeing it
+
+    See it in action in Get-DbaDbSnapshot and Remove-DbaDbSnapshot
+
+    a lot of this is from boe, thanks boe!
+    https://learn-powershell.net/2013/08/03/quick-hits-set-the-default-property-display-in-powershell-on-custom-objects/
+
+    TypeName creates a new type so that we can use ps1xml to modify the output
+    #>
     
-    .DESCRIPTION
-        Wrapper around Select-DbaObject, used to style output.
-        Do not use this function for new commands, instead use Select-DbaObject instead!
-    
-    .PARAMETER InputObject
-        The object to style
-    
-    .PARAMETER Property
-        The properties to show by default.
-    
-    .PARAMETER ExcludeProperty
-        The properties to NOT show by default
-    
-    .PARAMETER TypeName
-        A new name to assign to the type
-#>  
     [CmdletBinding()]
     param (
         [parameter(ValueFromPipeline = $true)]
@@ -35,46 +26,47 @@ function Select-DefaultView {
         [string]
         $TypeName
     )
-    begin {
-        try {
-            $paramSplat = @{ }
-            
-            if ($ExcludeProperty) {
-                $dataRowDefaults = 'Item', 'RowError', 'RowState', 'Table', 'ItemArray', 'HasErrors'
-                $exclusions = New-Object System.Collections.ArrayList
-                $exclusions.AddRange($dataRowDefaults)
-                $exclusions.AddRange($ExcludeProperty)
-                $paramSplat['ShowExcludeProperty'] = $exclusions.ToArray()
-            }
-            if ($Property) {
-                $paramSplat['ShowProperty'] = $Property
-            }
-            if ($TypeName) {
-                $paramSplat['TypeName'] = $TypeName
-            }
-            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Select-DbaObject', [System.Management.Automation.CommandTypes]::Cmdlet)
-            $scriptCmd = { & $wrappedCmd -KeepInputObject @paramSplat }
-            $steppablePipeline = $scriptCmd.GetSteppablePipeline()
-            $steppablePipeline.Begin($PSCmdlet)
-        }
-        catch {
-            throw
-        }
-    }
     process {
-        try {
-            $steppablePipeline.Process($InputObject)
+        
+        if ($null -eq $InputObject) { return }
+        
+        if ($TypeName) {
+            $InputObject.PSObject.TypeNames.Insert(0, "dbatools.$TypeName")
         }
-        catch {
-            throw
+        
+        if ($ExcludeProperty) {
+            if ($InputObject.GetType().Name.ToString() -eq 'DataRow') {
+                $ExcludeProperty += 'Item', 'RowError', 'RowState', 'Table', 'ItemArray', 'HasErrors'
+            }
+            
+            $props = ($InputObject | Get-Member | Where-Object MemberType -in 'Property', 'NoteProperty', 'AliasProperty' | Where-Object { $_.Name -notin $ExcludeProperty }).Name
+            $defaultset = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$props)
         }
-    }
-    end {
-        try {
-            $steppablePipeline.End()
+        else {
+            # property needs to be string
+            if ("$property" -like "* as *") {
+                $newproperty = @()
+                foreach ($p in $property) {
+                    if ($p -like "* as *") {
+                        $old, $new = $p -isplit " as "
+                        # Do not be tempted to not pipe here
+                        $inputobject | Add-Member -Force -MemberType AliasProperty -Name $new -Value $old -ErrorAction SilentlyContinue
+                        $newproperty += $new
+                    }
+                    else {
+                        $newproperty += $p
+                    }
+                }
+                $property = $newproperty
+            }
+            $defaultset = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$Property)
         }
-        catch {
-            throw
-        }
+        
+        $standardmembers = [System.Management.Automation.PSMemberInfo[]]@($defaultset)
+        
+        # Do not be tempted to not pipe here
+        $inputobject | Add-Member -Force -MemberType MemberSet -Name PSStandardMembers -Value $standardmembers -ErrorAction SilentlyContinue
+        
+        $inputobject
     }
 }
