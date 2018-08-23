c27c37f35522dabeb993de78abd965c7f5f143db
Andrew Wickham <andrew@awickham.com>
2018-08-23 11:43:30 -0400

    Code cleanup/optimization

--- functions/Remove-DbaClientAlias.ps1
+++ functions/Remove-DbaClientAlias.ps1
@@ -1,106 +1,88 @@
 function Remove-DbaClientAlias {
     <#
-    .SYNOPSIS
-    Removes a sql alias for the specified server - mimics cliconfg.exe
+        .SYNOPSIS
+            Removes a sql alias for the specified server - mimics cliconfg.exe
 
-    .DESCRIPTION
-    Removes a SQL Server alias by altering HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client
+        .DESCRIPTION
+            Removes a SQL Server alias by altering HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client
 
-    .PARAMETER ComputerName
-    The target computer where the alias will be created
+        .PARAMETER ComputerName
+            The target computer where the alias will be created
 
-    .PARAMETER Credential
-    Allows you to login to remote computers using alternative credentials
+        .PARAMETER Credential
+            Allows you to login to remote computers using alternative credentials
 
-    .PARAMETER Alias
-    The alias or array of aliases to be deleted
+        .PARAMETER Alias
+            The alias or array of aliases to be deleted
 
-    .PARAMETER EnableException
-    By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-    This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-    Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        .PARAMETER EnableException
+            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-    .NOTES
-    Tags: Alias
+        .NOTES
+            Tags: Alias
 
-    Website: https://dbatools.io
-    Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-    License: MIT https://opensource.org/licenses/MIT
+            Website: https://dbatools.io
+            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+            License: MIT https://opensource.org/licenses/MIT
 
-    .LINK
-    https://dbatools.io/Remove-DbaClientAlias
+        .LINK
+            https://dbatools.io/Remove-DbaClientAlias
 
-    .EXAMPLE
-    Remove-DbaClientAlias -ComputerName workstationx -Alias sqlps
-    Removes the sqlps SQL client alias on workstationx
+        .EXAMPLE
+            Remove-DbaClientAlias -ComputerName workstationx -Alias sqlps
 
-    .EXAMPLE
-    Get-DbaClientAlias | Remove-DbaClientAlias
-    Removes all SQL Server client aliases on the local computer
+            Removes the sqlps SQL client alias on workstationx
 
-#>
-    [CmdletBinding()]
-    Param (
-        [parameter(ValueFromPipelineByPropertyName)]
+        .EXAMPLE
+            Get-DbaClientAlias | Remove-DbaClientAlias
+
+            Removes all SQL Server client aliases on the local computer
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [parameter(ValueFromPipelineByPropertyName = $true)]
         [DbaInstanceParameter[]]$ComputerName = $env:COMPUTERNAME,
         [PSCredential]$Credential,
-        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [Alias('AliasName')]
         [string[]]$Alias,
         [Alias('Silent')]
         [switch]$EnableException
     )
 
-    process {
-        foreach ($computer in $ComputerName) {
-            $null = Test-ElevationRequirement -ComputerName $computer -Continue
+    begin {
+        $scriptblock = {
+            $Alias = $args
 
-            $scriptblock = {
-                $Alias = $args
+            $basekeys = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\MSSQLServer", "HKLM:\SOFTWARE\Microsoft\MSSQLServer"
 
-                $basekeys = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\MSSQLServer", "HKLM:\SOFTWARE\Microsoft\MSSQLServer"
+            foreach ($basekey in $basekeys) {
+                $fullKey = "$basekey\Client\ConnectTo"
+                if ((Test-Path $fullKey) -eq $false) {
+                    Write-Warning "Registry key ($fullKey) does not exist. Quitting."
+                    continue
+                }
 
-                foreach ($basekey in $basekeys) {
-
-                    if ((Test-Path $basekey) -eq $false) {
-                        Write-Warning "Base key ($basekey) does not exist. Quitting."
-                        continue
-                    }
-
-                    $client = "$basekey\Client"
-
-                    if ((Test-Path $client) -eq $false) {
-                        continue
-                    }
-
-                    $connect = "$client\ConnectTo"
-
-                    if ((Test-Path $connect) -eq $false) {
-                        continue
-                    }
-
-                    if ($basekey -like "*WOW64*") {
-                        $architecture = "32-bit"
-                    }
-                    else {
-                        $architecture = "64-bit"
-                    }
-
-                    $all = Get-Item -Path $connect
-                    foreach ($entry in $all) {
-                        foreach ($en in $entry) {
-                            $e = $entry.ToString().Replace('HKEY_LOCAL_MACHINE', 'HKLM:\')
-                            foreach ($a in $Alias) {
-                                if ($en.Property -contains $a) {
-                                    Remove-ItemProperty -Path $e -Name $a
-                                }
-                            }
+                $all = Get-Item -Path $fullKey
+                foreach ($entry in $all) {
+                    $e = $entry.ToString().Replace('HKEY_LOCAL_MACHINE', 'HKLM:\')
+                    foreach ($a in $Alias) {
+                        if ($entry.Property -contains $a) {
+                            Remove-ItemProperty -Path $e -Name $a
                         }
                     }
                 }
             }
+        }
+    }
 
-            if ($PScmdlet.ShouldProcess($computer, "Getting aliases")) {
+    process {
+        foreach ($computer in $ComputerName) {
+            $null = Test-ElevationRequirement -ComputerName $computer -Continue
+
+            if ($PSCmdlet.ShouldProcess("$($Alias -join ', ') on $computer", "Remove aliases")) {
                 try {
                     $null = Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock $scriptblock -ErrorAction Stop -Verbose:$false -ArgumentList $Alias
                     Get-DbaClientAlias -ComputerName $computer
