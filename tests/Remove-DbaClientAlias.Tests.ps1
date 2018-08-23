263289da0912368abfa5d2d5f50478321ad2fe88
Andrew Wickham <andrew@awickham.com>
2018-08-23 10:17:26 -0400

    Added support + tests for accepting multiple aliases to remove

--- tests/Remove-DbaClientAlias.Tests.ps1
+++ tests/Remove-DbaClientAlias.Tests.ps1
@@ -4,12 +4,37 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
-        $null = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias-new -Verbose:$false
+        $null = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias1 -Verbose:$false
+        $null = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias2 -Verbose:$false
+        $null = New-DbaClientAlias -ServerName sql2016 -Alias dbatoolscialias3 -Verbose:$false
     }
-    Context "adds the alias" {
-        $results = Remove-DbaClientAlias -Alias dbatoolscialias-new -Verbose:$false
+    Context "removes the alias" {
+        $results = Remove-DbaClientAlias -Alias dbatoolscialias1 -Verbose:$false
         It "alias is not included in results" {
-            $results.AliasName -notcontains 'dbatoolscialias-new' | Should Be $true
+            $results.AliasName -notcontains 'dbatoolscialias1' | Should Be $true
+        }
+    }
+
+    Context "removes an array of aliases" {
+        $testCases = @(
+            @{'Alias' = 'dbatoolscialias2'},
+            @{'Alias' = 'dbatoolscialias3'}
+        )
+
+        $aliases = Get-DbaClientAlias
+        It "alias <Alias> exists" -TestCases $testCases {
+            param ($Alias)
+
+             $aliases.AliasName -contains $Alias | Should Be $true
+        }
+
+        $null = Remove-DbaClientAlias -Alias @('dbatoolscialias2', 'dbatoolscialias3')
+
+        $aliases = Get-DbaClientAlias
+        It "alias <Alias> was removed" -TestCases $testCases {
+            param ($Alias)
+
+            $aliases.AliasName -notcontains $Alias | Should Be $true
         }
     }
 }
