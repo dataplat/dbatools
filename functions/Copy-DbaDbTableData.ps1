4c55ecee00a81ab444a21c279992f12063159b2b
Chrissy LeMaire <clemaire@gmail.com>
2018-09-08 21:25:48 +0200

    DbaTable -> DbaDbTable, Distributor -> RepDistributor

--- functions/Copy-DbaDbTableData.ps1
+++ functions/Copy-DbaDbTableData.ps1
@@ -342,4 +342,7 @@ function Copy-DbaDbTableData {
             }
         }
     }
+    end {
+        Test-DbaDeprecation -DeprecatedOn "1.0.0" -EnableException:$false -Alias Copy-DbaTableData
+    }
 }
