d8223817865ee563445397d4721c0178ef27f08a
Chrissy LeMaire <clemaire@gmail.com>
2018-09-11 10:06:44 +0200

    updated author

--- functions/Test-DbaPath.ps1
+++ functions/Test-DbaPath.ps1
@@ -25,11 +25,8 @@ function Test-DbaPath {
         .NOTES
             Tags: Path, ServiceAccount
             Author: Chrissy LeMaire (@cl), netnerds.net
-            Requires: Admin access to server (not SQL Services),
-            Remoting must be enabled and accessible if $SqlInstance is not local
-
-            dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-            Copyright (C) 2016 Chrissy LeMaire
+            Website: https://dbatools.io
+            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
             License: MIT https://opensource.org/licenses/MIT
 
         .LINK
--- functions/Test-DbaPowerPlan.ps1
+++ functions/Test-DbaPowerPlan.ps1
@@ -29,10 +29,9 @@ function Test-DbaPowerPlan {
 
         .NOTES
             Tags: PowerPlan
-            Requires: WMI access to servers
-
-            dbatools PowerShell module (https://dbatools.io, clemaire@gmail.com)
-            Copyright (C) 2016 Chrissy LeMaire
+            Author: Chrissy LeMaire (@cl), netnerds.net
+            Website: https://dbatools.io
+            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
             License: MIT https://opensource.org/licenses/MIT
 
         .LINK
