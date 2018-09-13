9a6d518060d30e0502176c83f55237591ed33f0e
Chrissy LeMaire <clemaire@gmail.com>
2018-09-13 23:22:26 +0200

    addressing various join path issues plus bug

--- internal/functions/Join-SomePath.ps1
+++ internal/functions/Join-SomePath.ps1
@@ -1,6 +1,8 @@
-﻿function Join-Path {
+﻿function Join-SomePath {
     <#
     An internal command that does not require the local path to exist
+    
+    Boo, this does not work, but keeping it for future ref.
     #>
     [CmdletBinding()]
     param (
