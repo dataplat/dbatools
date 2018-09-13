0c2ceea052b9f5a4ef9e679fd972a74eef5d7287
Chrissy LeMaire <clemaire@gmail.com>
2018-09-13 22:17:31 +0200

    internal overrride that is safer

---
+++ internal/functions/Join-Path.ps1
@@ -0,0 +1,13 @@
+﻿function Join-Path {
+    <#
+    An internal command that does not require the local path to exist
+    #>
+    [CmdletBinding()]
+    param (
+        [string]$Path,
+        [string]$ChildPath
+    )
+    process {
+        [IO.Path]::Combine($Path, $ChildPath)
+    }
+}
