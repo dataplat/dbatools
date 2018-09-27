d06722c42b521be51112ce4cc64d23e5f6000637
Chrissy LeMaire <clemaire@gmail.com>
2018-09-27 17:12:17 +0200

    trim trailing

--- internal/functions/Invoke-Robocopy.ps1
+++ internal/functions/Invoke-Robocopy.ps1
@@ -23,10 +23,10 @@
 .Parameter PassThru
     Switch. Returns an object with the following properties:
 
-    StdOut - array of strings captured from StandardOutput 
-    StdErr - array of strings captured from StandardError 
+    StdOut - array of strings captured from StandardOutput
+    StdErr - array of strings captured from StandardError
     ExitCode - Enum with Robocopy exit code in human-readable format
-    
+
     By default, this function doesn't generate any output.
 
 .Link
@@ -60,7 +60,7 @@
         [Parameter(ValueFromPipelineByPropertyName = $true)]
         [switch]$PassThru
     )
-    
+
     Begin {
         function Start-ConsoleProcess {
             Param
@@ -72,7 +72,7 @@
                 [Parameter(ValueFromPipeline = $true)]
                 [string[]]$InputObject
             )
-            
+
             End {
                 if ($Input) {
                     # Collect all pipeline input
@@ -82,10 +82,10 @@
                 else {
                     $StdIn = $InputObject
                 }
-                
+
                 try {
                     "Starting process: $FilePath", "Redirect StdIn: $([bool]$StdIn.Count)", "Arguments: $ArgumentList" | Write-Verbose
-                    
+
                     if ($StdIn.Count) {
                         $Output = $StdIn | & $FilePath $ArgumentList 2>&1
                     }
@@ -96,12 +96,12 @@
                 catch {
                     throw $_
                 }
-                
+
                 Write-PSFMessage -Level Verbose -Message 'Finished, processing output'
-                
+
                 $StdOut = New-Object -TypeName System.Collections.Generic.List``1[String]
                 $StdErr = New-Object -TypeName System.Collections.Generic.List``1[String]
-                
+
                 foreach ($item in $Output) {
                     # Data from StdOut will be strings, while StdErr produces
                     # System.Management.Automation.ErrorRecord objects.
@@ -113,7 +113,7 @@
                         $StdOut.Add($item)
                     }
                 }
-                
+
                 Write-Verbose 'Returning result'
                 [pscustomobject]@{
                     ExitCode = $LASTEXITCODE
@@ -122,11 +122,11 @@
                 } | Select-Object -Property StdOut, StdErr, ExitCode
             }
         }
-        
+
         # https://learn-powershell.net/2016/03/07/building-a-enum-that-supports-bit-fields-in-powershell/
         function New-RobocopyHelper {
             $TypeName = 'Robocopy.ExitCode'
-            
+
             # http://stackoverflow.com/questions/16552801/how-do-i-conditionally-add-a-class-with-add-type-typedefinition-if-it-isnt-add
             if (! ([System.Management.Automation.PSTypeName]$TypeName).Type) {
                 try {
@@ -136,7 +136,7 @@
                     $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run) # Only run in memory
                     $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule($TypeName, $false)
                     #endregion Module Builder
-                    
+
                     # https://pshirwin.wordpress.com/2016/03/18/robocopy-exitcodes-the-powershell-way/
                     #region Enum
                     $EnumBuilder = $ModuleBuilder.DefineEnum($TypeName, 'Public', [int32])
@@ -158,10 +158,10 @@
                 }
             }
         }
-        
+
         New-RobocopyHelper
     }
-    
+
     Process {
         foreach ($item in $Destination) {
             # Resolve destination paths, remove trailing backslash, add Retries and combine all arguments into one array
@@ -172,14 +172,14 @@
                     $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($_) -replace '\\+$'
                 }
             ) + $ArgumentList + "/R:$Retry"
-            
+
             # Invoke Robocopy
             $Result = Start-ConsoleProcess -FilePath 'robocopy.exe' -ArgumentList $AllArguments
             $Result.ExitCode = [Robocopy.ExitCode]$Result.ExitCode
-            
+
             # Dump Robocopy log to Verbose stream
             $Result.StdOut | Write-Verbose
-            
+
             # Process Robocopy exit code
             # http://latkin.org/blog/2012/07/08/using-enums-in-powershell/
             if ($Result.ExitCode -band [Robocopy.ExitCode]'FailedCopyAttempts, FatalError') {
@@ -194,7 +194,7 @@
                             $_.Matches.Groups[1].Value
                         }
                     )
-                    
+
                     $ErrorMessage -join [System.Environment]::NewLine | Write-Error
                 }
             }
