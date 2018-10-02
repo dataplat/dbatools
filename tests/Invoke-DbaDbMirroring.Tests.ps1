f27430179c8ff15662d3770dd73d176268ac0729
Chrissy LeMaire <clemaire@gmail.com>
2018-10-02 19:31:20 +0200

    add new test
    (*do Invoke-DbaDbMirroring*)

---
+++ tests/Invoke-DbaDbMirroring.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        Get-DbaProcess -SqlInstance $script:instance2 | Where-Object Program -match dbatools | Stop-DbaProcess -Confirm:$false -WarningAction SilentlyContinue
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $db1 = "dbatoolsci_mirroring"
+        
+        Remove-DbaDbSnapshot -SqlInstance $script:instance2 -Database $db12 -Confirm:$false
+        Get-DbaDatabase -SqlInstance $script:instance2 -Database $db1 | Remove-DbaDatabase -Confirm:$false
+        $server.Query("CREATE DATABASE $db1")
+    }
+    AfterAll {
+        Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2, $script:instance3 -Database $db1 -ErrorAction SilentlyContinue
+    }
+    
+    It "returns success" {
+        $results = Invoke-DbaDbMirroring -Primary $script:instance2 -Mirror $script:instance3 -Database $db1 -NetworkShare C:\temp
+        $results.Status -eq 'Success' | Should Be $true
+    }
+}
