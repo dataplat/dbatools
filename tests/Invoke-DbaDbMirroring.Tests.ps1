512aefc3f383eb40205153bee35eda4d3399cff0
Chrissy LeMaire <clemaire@gmail.com>
2018-10-05 00:33:35 +0200

    enable test
    (do *Invoke-DbaDbMirroring*)

--- tests/Invoke-DbaDbMirroring.Tests.ps1
+++ tests/Invoke-DbaDbMirroring.Tests.ps1
@@ -16,7 +16,7 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
         Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2, $script:instance3 -Database $db1 -ErrorAction SilentlyContinue
     }
     
-    It -Skip "returns success" {
+    It "returns success" {
         $results = Invoke-DbaDbMirroring -Primary $script:instance2 -Mirror $script:instance3 -Database $db1 -NetworkShare C:\temp -Force -Confirm:$false
         $results.Status -eq 'Success' | Should Be $true
     }
