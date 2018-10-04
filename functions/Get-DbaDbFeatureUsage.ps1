7ef4e723e77cf313b105fb5b6687d63942c9a876
Chrissy LeMaire <clemaire@gmail.com>
2018-10-04 10:14:15 +0200

    trim traililng

--- functions/Get-DbaDbFeatureUsage.ps1
+++ functions/Get-DbaDbFeatureUsage.ps1
@@ -2,12 +2,12 @@
     <#
         .SYNOPSIS
             Shows features that are enabled in the database but not supported on the all the editions of SQL Server.
-        
+
         .DESCRIPTION
             Shows features that are enabled in the database but not supported on the all the editions of SQL Server.
-            
+
             This feature must be removed before the database can be migrated to all available editions of SQL Server.
-    
+
         .PARAMETER SqlInstance
             The target SQL Server instance
 
@@ -22,7 +22,7 @@
 
         .PARAMETER InputObject
             A collection of databases (such as returned by Get-DbaDatabase), to be tested.
-    
+
         .PARAMETER EnableException
             By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
             This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
@@ -40,9 +40,9 @@
 
         .EXAMPLE
             C:\> Get-DbaDatabase -SqlInstance sql2008 -Database testdb, db2 | Get-DbaDbFeatureUsage
-            
-            Shows features that are enabled in the testdb and db2 databases but 
-            not supported on the all the editions of SQL Server. 
+
+            Shows features that are enabled in the testdb and db2 databases but
+            not supported on the all the editions of SQL Server.
     #>
     [CmdletBinding()]
     Param (
@@ -55,25 +55,25 @@
         [Microsoft.SqlServer.Management.Smo.Database[]]$InputObject,
         [switch]$EnableException
     )
-    
+
     begin {
         $sql = "SELECT  SERVERPROPERTY('MachineName') AS ComputerName,
             ISNULL(SERVERPROPERTY('InstanceName'), 'MSSQLSERVER') AS InstanceName,
             SERVERPROPERTY('ServerName') AS SqlInstance, DB_NAME() as [Database], feature_id as Id,
             feature_name as Feature FROM sys.dm_db_persisted_sku_features"
     }
-    
+
     process {
         foreach ($instance in $SqlInstance) {
             $InputObject += Get-DbaDatabase -SqlInstance $instance -SqlCredential $SqlCredential -Database $Database -ExcludeDatabase $ExcludeDatabase
         }
         foreach ($db in $InputObject) {
             Write-Message -Level Verbose -Message "Processing $db on $($db.Parent.Name)"
-            
+
             if ($db.IsAccessible -eq $false) {
                 Stop-Function -Message "The database $db is not accessible. Skipping database." -Continue
             }
-            
+
             try {
                 $db.Query($sql)
             }
