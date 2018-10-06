1ae826434470d3813e6e17f9a55f8e6d3ebec40b
Chrissy LeMaire <clemaire@gmail.com>
2018-10-06 23:40:07 +0200

    remove trailing

--- functions/Get-DbaSpDatabase.ps1
+++ functions/Get-DbaSpDatabase.ps1
@@ -1,6 +1,6 @@
 ﻿#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
 function Get-DbaSpDatabase {
-<#        
+<#
     .SYNOPSIS
         Returns databases that are part of a SharePoint Farm.
         
