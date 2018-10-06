f15ec18b2b9c0836a7dfe3ebc6c186bfdc0e4627
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2018-10-06 08:41:51 +0200

    Mirroring command set (#4132)
    adding mirror command set

---
+++ tests/Invoke-DbaDbMirroring.Tests.ps1
@@ -0,0 +1,24 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $null = Get-DbaProcess -SqlInstance $script:instance2 | Where-Object Program -match dbatools | Stop-DbaProcess -Confirm:$false -WarningAction SilentlyContinue
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $db1 = "dbatoolsci_mirroring"
+        
+        Remove-DbaDbMirror -SqlInstance $script:instance2 -Database $db1 -Confirm:$false
+        Remove-DbaDatabase -SqlInstance $script:instance2 -Database $db1 -Confirm:$false
+        $null = Get-DbaDatabase -SqlInstance $script:instance2 -Database $db1 | Remove-DbaDatabase -Confirm:$false
+        $null = $server.Query("CREATE DATABASE $db1")
+     }
+    AfterAll {
+        $null = Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance2, $script:instance3 -Database $db1 -ErrorAction SilentlyContinue
+    }
+    
+    It -Skip "returns success" {
+        $results = Invoke-DbaDbMirroring -Primary $script:instance2 -Mirror $script:instance3 -Database $db1 -Confirm:$false -Force -NetworkShare C:\temp
+        $results.Status | Should -Be 'Success'
+    }
+}
---
+++ tests/Remove-DbaEndpoint.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $endpoint = Get-DbaEndpoint -SqlInstance $script:instance2 | Where-Object EndpointType -eq DatabaseMirroring
+        $create = $endpoint | Export-DbaScript -Passthru
+        $null = $endpoint | Remove-DbaEndpoint
+        $results = New-DbaEndpoint -SqlInstance $script:instance2 -Type DatabaseMirroring -Role Partner -Name Mirroring -EncryptionAlgorithm RC4 -Confirm:$false | Start-DbaEndpoint -Confirm:$false
+    }
+    AfterAll {
+        if ($create) {
+            Invoke-DbaQuery -SqlInstance $script:instance2 -Query "$create"
+        }
+    }
+    
+    It "removes an endpoint" {
+        $results = Get-DbaEndpoint -SqlInstance $script:instance2 | Where-Object EndpointType -eq DatabaseMirroring | Remove-DbaEndpoint
+        $results.Status | Should -Be 'Removed'
+    }
+}
