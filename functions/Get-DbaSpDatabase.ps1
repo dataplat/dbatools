dae2e361a33d51277595773f93745139b5ef3a39
Chrissy LeMaire <clemaire@gmail.com>
2018-10-06 23:22:39 +0200

    fixed help

--- functions/Get-DbaSpDatabase.ps1
+++ functions/Get-DbaSpDatabase.ps1
@@ -1,49 +1,50 @@
 ﻿#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
 function Get-DbaSpDatabase {
- <#
+<#        
     .SYNOPSIS
-            Returns databases that are part of a SharePoint Farm.
-    
+        Returns databases that are part of a SharePoint Farm.
+        
     .DESCRIPTION
-            Returns databases that are part of a SharePoint Farm, as found in the SharePoint Configuration database.
-    
-            By default, this command checks SharePoint_Config. To use an alternate databse, use the ConfigDatabase parameter.
-
+        Returns databases that are part of a SharePoint Farm, as found in the SharePoint Configuration database.
+        
+        By default, this command checks SharePoint_Config. To use an alternate databse, use the ConfigDatabase parameter.
+        
     .PARAMETER SqlInstance
-            The target SQL Server instance
-
+        The target SQL Server instance
+        
     .PARAMETER SqlCredential
-            Login to the target instance using alternate Windows or SQL Login Authentication. Accepts credential objects (Get-Credential).
-
+        Login to the target instance using alternate Windows or SQL Login Authentication. Accepts credential objects (Get-Credential).
+        
     .PARAMETER ConfigDatabase
-            The name of the SharePoint Configuration database. Defaults to SharePoint_Config.
-
+        The name of the SharePoint Configuration database. Defaults to SharePoint_Config.
+        
     .PARAMETER InputObject
-            Allows piping from Get-DbaDatabase.
-
+        Allows piping from Get-DbaDatabase.
+        
     .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        
     .NOTES
-            Author: Chrissy LeMaire (@cl), netnerds.net
-            Website: https://dbatools.io
-            Copyright: (c) 2018 by dbatools, licensed under MIT
--           License: MIT https://opensource.org/licenses/MIT
-
-        .LINK
-            https://dbatools.io/Get-DbaSpDatabase
-
-        .EXAMPLE
-            PS C:\> Get-DbaSpDatabase -SqlInstance sqlcluster
-
-            Returns databases that are part of a SharePoint Farm, as found in SharePoint_Config on sqlcluster
-            
-        .EXAMPLE
-            PS C:\> Get-DbaDatabase -SqlInstance sqlcluster -Database SharePoint_Config_2016 | Get-DbaSpDatabase
-
-            Returns databases that are part of a SharePoint Farm, as found in SharePoint_Config_2016 on sqlcluster
+        Author: Chrissy LeMaire (@cl), netnerds.net
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        -           License: MIT https://opensource.org/licenses/MIT
+        
+    .LINK
+        https://dbatools.io/Get-DbaSpDatabase
+        
+    .EXAMPLE
+        PS C:\> Get-DbaSpDatabase -SqlInstance sqlcluster
+        
+        Returns databases that are part of a SharePoint Farm, as found in SharePoint_Config on sqlcluster
+        
+    .EXAMPLE
+        PS C:\> Get-DbaDatabase -SqlInstance sqlcluster -Database SharePoint_Config_2016 | Get-DbaSpDatabase
+        
+        Returns databases that are part of a SharePoint Farm, as found in SharePoint_Config_2016 on sqlcluster
+        
 #>
     [CmdletBinding()]
     Param (
