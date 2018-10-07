7ed7c0c6ae8e001b84f091090775624738e44051
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-10-07 14:00:04 +0200

    fixed build sequence

--- bin/projects/dbatools/dbatools/Properties/AssemblyInfo.cs
+++ bin/projects/dbatools/dbatools/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.10.0.58")]
-[assembly: AssemblyFileVersion("0.10.0.57")]
+[assembly: AssemblyFileVersion("0.10.0.58")]
