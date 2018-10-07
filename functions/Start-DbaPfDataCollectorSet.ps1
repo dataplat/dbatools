70f0750e476e7a802806b8af6229aa663ccd858d
Shawn Melton <wshawnmelton@gmail.com>
2018-10-07 16:06:25 -0500

    update help, redo some examples in start migration cmd

--- functions/Start-DbaPfDataCollectorSet.ps1
+++ functions/Start-DbaPfDataCollectorSet.ps1
@@ -3,62 +3,63 @@ function Start-DbaPfDataCollectorSet {
 <#
     .SYNOPSIS
         Starts Performance Monitor Data Collector Set.
-        
+
     .DESCRIPTION
         Starts Performance Monitor Data Collector Set.
-        
+
     .PARAMETER ComputerName
         The target computer. Defaults to localhost.
-        
+
     .PARAMETER Credential
         Allows you to login to $ComputerName using alternative credentials. To use:
-        
+
         $cred = Get-Credential, then pass $cred object to the -Credential parameter.
-        
+
     .PARAMETER CollectorSet
         The name of the Collector Set to start.
-        
+
     .PARAMETER NoWait
         If this switch is enabled, the collector is started and the results are returned immediately.
-        
+
     .PARAMETER InputObject
         Accepts the object output by Get-DbaPfDataCollectorSet via the pipeline.
-        
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-        
+
     .NOTES
         Tags: PerfMon
         Author: Chrissy LeMaire (@cl), netnerds.net
+
         Website: https://dbatools.io
         Copyright: (c) 2018 by dbatools, licensed under MIT
         License: MIT https://opensource.org/licenses/MIT
-        
+
     .LINK
         https://dbatools.io/Start-DbaPfDataCollectorSet
-        
+
     .EXAMPLE
-        Start-DbaPfDataCollectorSet
-        
+        PS C:\> Start-DbaPfDataCollectorSet
+
         Attempts to start all ready Collectors on localhost.
-        
+
     .EXAMPLE
-        Start-DbaPfDataCollectorSet -ComputerName sql2017
-        
+        PS C:\> Start-DbaPfDataCollectorSet -ComputerName sql2017
+
         Attempts to start all ready Collectors on localhost.
-        
+
     .EXAMPLE
-        Start-DbaPfDataCollectorSet -ComputerName sql2017, sql2016 -Credential (Get-Credential) -CollectorSet 'System Correlation'
-        
+        PS C:\> Start-DbaPfDataCollectorSet -ComputerName sql2017, sql2016 -Credential (Get-Credential) -CollectorSet 'System Correlation'
+
         Starts the 'System Correlation' Collector on sql2017 and sql2016 using alternative credentials.
-        
+
     .EXAMPLE
-        Get-DbaPfDataCollectorSet -CollectorSet 'System Correlation' | Start-DbaPfDataCollectorSet
-        
+        PS C:\> Get-DbaPfDataCollectorSet -CollectorSet 'System Correlation' | Start-DbaPfDataCollectorSet
+
         Starts the 'System Correlation' Collector.
-        
+
 #>
     [CmdletBinding()]
     param (
@@ -73,7 +74,7 @@ function Start-DbaPfDataCollectorSet {
     )
     begin {
         $wait = $NoWait -eq $false
-        
+
         $setscript = {
             $setname = $args[0]; $wait = $args[1]
             $collectorset = New-Object -ComObject Pla.DataCollectorSet
@@ -87,14 +88,14 @@ function Start-DbaPfDataCollectorSet {
                 $InputObject += Get-DbaPfDataCollectorSet -ComputerName $computer -Credential $Credential -CollectorSet $CollectorSet
             }
         }
-        
+
         if ($InputObject) {
             if (-not $InputObject.DataCollectorSetObject) {
                 Stop-Function -Message "InputObject is not of the right type. Please use Get-DbaPfDataCollectorSet."
                 return
             }
         }
-        
+
         # Check to see if its running first
         foreach ($set in $InputObject) {
             $setname = $set.Name
@@ -114,7 +115,7 @@ function Start-DbaPfDataCollectorSet {
             catch {
                 Stop-Function -Message "Failure starting $setname on $computer." -ErrorRecord $_ -Target $computer -Continue
             }
-            
+
             Get-DbaPfDataCollectorSet -ComputerName $computer -Credential $Credential -CollectorSet $setname
         }
     }
