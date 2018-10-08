93999fe764d3b2107b1c201facef25c781600fb7
Shawn Melton <wshawnmelton@gmail.com>
2018-10-07 22:03:12 -0500

    updating help

--- functions/Get-DbaLocaleSetting.ps1
+++ functions/Get-DbaLocaleSetting.ps1
@@ -2,50 +2,49 @@
 <#
     .SYNOPSIS
         Gets the Locale settings on a computer.
-        
+
     .DESCRIPTION
         Gets the Locale settings on one or more computers.
-        
+
         Requires Local Admin rights on destination computer(s).
-        
+
     .PARAMETER ComputerName
         The SQL Server (or server in general) that you're connecting to. This command handles named instances.
-        
+
     .PARAMETER Credential
         Credential object used to connect to the computer as a different user.
-        
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-        
+
     .NOTES
         Tags: OS
-        Author: Klaas Vandenberghe ( @PowerDBAKlaas )
-        
+        Author: Klaas Vandenberghe (@PowerDBAKlaas)
+
         Website: https://dbatools.io
         Copyright: (c) 2018 by dbatools, licensed under MIT
         License: MIT https://opensource.org/licenses/MIT
-        
+
     .LINK
         https://dbatools.io/Get-DbaLocaleSetting
-        
+
     .EXAMPLE
-        Get-DbaLocaleSetting -ComputerName sqlserver2014a
-        
+        PS C:\> Get-DbaLocaleSetting -ComputerName sqlserver2014a
+
         Gets the Locale settings on computer sqlserver2014a.
-        
+
     .EXAMPLE
-        'sql1','sql2','sql3' | Get-DbaLocaleSetting
-        
+        PS C:\> 'sql1','sql2','sql3' | Get-DbaLocaleSetting
+
         Gets the Locale settings on computers sql1, sql2 and sql3.
-        
+
     .EXAMPLE
-        Get-DbaLocaleSetting -ComputerName sql1,sql2 -SqlCredential $credential | Out-Gridview
-        
+        PS C:\> Get-DbaLocaleSetting -ComputerName sql1,sql2 -SqlCredential $credential | Out-Gridview
+
         Gets the Locale settings on computers sql1 and sql2 using SQL Authentication to authenticate to the servers, and shows them in a grid view.
-        
-        
+
 #>
     [CmdletBinding()]
     param (
