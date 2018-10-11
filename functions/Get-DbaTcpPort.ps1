e343a2465e37d4902070e47195d1c6d45a29602e
Joshua Corrick <joshua.corrick@live.com>
2018-10-11 09:30:48 -0400

    Removed indent in Code

--- functions/Get-DbaTcpPort.ps1
+++ functions/Get-DbaTcpPort.ps1
@@ -66,148 +66,148 @@
         Returns an object with server name, IPAddress (just ipv4), port and static ($true/$false) for every server listed in the Central Management Server on sql2014.
 
 #>
-        [CmdletBinding()]
-        param (
-            [parameter(Mandatory, ValueFromPipeline)]
-            [Alias("ServerInstance", "SqlServer")]
-            [DbaInstanceParameter[]]$SqlInstance,
-            [Alias("Credential")]
-            [PSCredential]$SqlCredential,
-            [switch]$Detailed,
-            [switch]$AllTcpPort,
-            [Alias("Ipv4")]
-            [switch]$ExcludeIpv6,
-            [Alias('Silent')]
-            [switch]$EnableException
-        )
-        begin {
-            Test-DbaDeprecation -DeprecatedOn 1.0.0 -Parameter Detailed
-        }
-        process {
-            foreach ($instance in $SqlInstance) {
-                if ($AllTcpPort -eq $true) {
-                    try {
-                        $scriptblock = {
-                            $instance = $args[0]
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [Alias("ServerInstance", "SqlServer")]
+        [DbaInstanceParameter[]]$SqlInstance,
+        [Alias("Credential")]
+        [PSCredential]$SqlCredential,
+        [switch]$Detailed,
+        [switch]$AllTcpPort,
+        [Alias("Ipv4")]
+        [switch]$ExcludeIpv6,
+        [Alias('Silent')]
+        [switch]$EnableException
+    )
+    begin {
+        Test-DbaDeprecation -DeprecatedOn 1.0.0 -Parameter Detailed
+    }
+    process {
+        foreach ($instance in $SqlInstance) {
+            if ($AllTcpPort -eq $true) {
+                try {
+                    $scriptblock = {
+                        $instance = $args[0]
 
-                            Add-Type -AssemblyName Microsoft.VisualBasic
+                        Add-Type -AssemblyName Microsoft.VisualBasic
 
-                            foreach ($servername in $wmi.ServerInstances) {
-                                $instanceName = $servername.Name
-                                $wmiinstance = $wmi.Services | Where-Object { $_.DisplayName -eq "SQL Server ($instanceName)" }
-                                $vsname = ($wmiinstance.AdvancedProperties | Where-Object { $_ -match 'VSNAME' }).Value
+                        foreach ($servername in $wmi.ServerInstances) {
+                            $instanceName = $servername.Name
+                            $wmiinstance = $wmi.Services | Where-Object { $_.DisplayName -eq "SQL Server ($instanceName)" }
+                            $vsname = ($wmiinstance.AdvancedProperties | Where-Object { $_ -match 'VSNAME' }).Value
 
-                                if ($vsname.length -eq 0) {
-                                    $vsname = "$instance\$instanceName"
+                            if ($vsname.length -eq 0) {
+                                $vsname = "$instance\$instanceName"
+                            }
+
+                            $vsname = $vsname.Replace("\MSSQLSERVER", "")
+
+                            try {
+                                $regroot = ($wmiinstance.AdvancedProperties | Where-Object { $_ -match 'REGROOT' }).Value
+                                $dacport = (Get-ItemProperty "HKLM:\$regroot\MSSQLServer\SuperSocketNetLib\AdminConnection\Tcp").TcpDynamicPorts
+
+                                [PsCustomObject]@{
+                                    ComputerName = $instance
+                                    InstanceName = $instanceName
+                                    SqlInstance  = $vsname
+                                    IPAddress    = "0.0.0.0"
+                                    Port         = $dacport
+                                    Static       = $false
+                                    Type         = "DAC"
                                 }
+                            }
+                            catch {
+                                #Shouldn't have an empty catch block
+                                Write-Message -Level Verbose -Message "it's just not our day"
+                            }
 
-                                $vsname = $vsname.Replace("\MSSQLSERVER", "")
+                            $tcp = $servername.ServerProtocols | Where-Object Name -eq Tcp
+                            $ips = $tcp.IPAddresses
 
-                                try {
-                                    $regroot = ($wmiinstance.AdvancedProperties | Where-Object { $_ -match 'REGROOT' }).Value
-                                    $dacport = (Get-ItemProperty "HKLM:\$regroot\MSSQLServer\SuperSocketNetLib\AdminConnection\Tcp").TcpDynamicPorts
+                            # This is a remote command so do not use Write-message
+                            Write-Verbose "Parsing information for $($ips.count) IP addresses."
+                            foreach ($ip in $ips) {
+                                $props = $ip.IPAddressProperties | Where-Object { $_.Name -eq "TcpPort" -or $_.Name -eq "TcpDynamicPorts" }
 
-                                    [PsCustomObject]@{
-                                        ComputerName = $instance
-                                        InstanceName = $instanceName
-                                        SqlInstance  = $vsname
-                                        IPAddress    = "0.0.0.0"
-                                        Port         = $dacport
-                                        Static       = $false
-                                        Type         = "DAC"
-                                    }
-                                }
-                                catch {
-                                    #Shouldn't have an empty catch block
-                                    Write-Message -Level Verbose -Message "it's just not our day"
-                                }
-
-                                $tcp = $servername.ServerProtocols | Where-Object Name -eq Tcp
-                                $ips = $tcp.IPAddresses
-
-                                # This is a remote command so do not use Write-message
-                                Write-Verbose "Parsing information for $($ips.count) IP addresses."
-                                foreach ($ip in $ips) {
-                                    $props = $ip.IPAddressProperties | Where-Object { $_.Name -eq "TcpPort" -or $_.Name -eq "TcpDynamicPorts" }
-
-                                    foreach ($prop in $props) {
-                                        if ([Microsoft.VisualBasic.Information]::IsNumeric($prop.value)) {
-                                            $port = $prop.value
-                                            if ($prop.name -eq 'TcpPort') {
-                                                $static = $true
-                                            }
-                                            else {
-                                                $static = $false
-                                            }
-                                            break
+                                foreach ($prop in $props) {
+                                    if ([Microsoft.VisualBasic.Information]::IsNumeric($prop.value)) {
+                                        $port = $prop.value
+                                        if ($prop.name -eq 'TcpPort') {
+                                            $static = $true
                                         }
+                                        else {
+                                            $static = $false
+                                        }
+                                        break
                                     }
+                                }
 
-                                    [PsCustomObject]@{
-                                        ComputerName = $instance
-                                        InstanceName = $instanceName
-                                        SqlInstance  = $vsname
-                                        IPAddress    = $ip.IPAddress.IPAddressToString
-                                        Port         = $port
-                                        Static       = $static
-                                        Type         = "Normal"
-                                    }
+                                [PsCustomObject]@{
+                                    ComputerName = $instance
+                                    InstanceName = $instanceName
+                                    SqlInstance  = $vsname
+                                    IPAddress    = $ip.IPAddress.IPAddressToString
+                                    Port         = $port
+                                    Static       = $static
+                                    Type         = "Normal"
                                 }
                             }
                         }
+                    }
 
-                        $computer = $instance.ComputerName
-                        $resolved = Resolve-DbaNetworkName -ComputerName $instance -Verbose:$false
-                        $computername = $resolved.FullComputerName
+                    $computer = $instance.ComputerName
+                    $resolved = Resolve-DbaNetworkName -ComputerName $instance -Verbose:$false
+                    $computername = $resolved.FullComputerName
 
-                        try {
-                            Write-Message -Level Verbose -Message "Trying with ComputerName ($computer)."
-                            $someIps = Invoke-ManagedComputerCommand -ComputerName $computer -ArgumentList $computer -ScriptBlock $scriptblock
-                        }
-                        catch {
-                            Write-Message -Level Verbose -Message "Trying with FullComputerName because ComputerName failed."
-                            $someIps = Invoke-ManagedComputerCommand -ComputerName $computername -ArgumentList $fqdn -ScriptBlock $scriptblock
-                        }
+                    try {
+                        Write-Message -Level Verbose -Message "Trying with ComputerName ($computer)."
+                        $someIps = Invoke-ManagedComputerCommand -ComputerName $computer -ArgumentList $computer -ScriptBlock $scriptblock
                     }
                     catch {
-                        Stop-Function -Message "Could not get all information." -Target $instance -ErrorRecord $_
-                    }
-
-                    $results = $someIps | Sort-Object IPAddress
-
-                    if ($ExcludeIpv6) {
-                        $octet = '(?:0?0?[0-9]|0?[1-9][0-9]|1[0-9]{2}|2[0-5][0-5]|2[0-4][0-9])'
-                        [regex]$ipv4 = "^(?:$octet\.){3}$octet$"
-                        $results = $results | Where-Object { $_.IPAddress -match $ipv4 }
+                        Write-Message -Level Verbose -Message "Trying with FullComputerName because ComputerName failed."
+                        $someIps = Invoke-ManagedComputerCommand -ComputerName $computername -ArgumentList $fqdn -ScriptBlock $scriptblock
                     }
                 }
-                #Default Execution of Get-DbaTcpPort
-                if ($AllTcpPort -eq $false -or ($AllTcpPort -eq $true -and $null -eq $someIps)) {
-                    try {
-                        $server = Connect-SqlInstance -SqlInstance "TCP:$instance" -SqlCredential $SqlCredential -MinimumVersion 9
-                    }
-                    catch {
-                        Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $servername -Continue
-                    }
+                catch {
+                    Stop-Function -Message "Could not get all information." -Target $instance -ErrorRecord $_
+                }
 
-                    # WmiComputer can be unreliable :( Use T-SQL
-                    $sql = "SELECT local_net_address,local_tcp_port FROM sys.dm_exec_connections WHERE session_id = @@SPID"
-                    $port = $server.Query($sql)
+                $results = $someIps | Sort-Object IPAddress
 
-                    $results = [PsCustomObject]@{
-                                ComputerName = $server.ComputerName
-                                InstanceName = $server.ServiceName
-                                SqlInstance  = $server.DomainInstanceName
-                                IPAddress    = $port.local_net_address
-                                Port         = $port.local_tcp_port
-                                Static       = $true
-                                Type         = "Normal"
-                               }
+                if ($ExcludeIpv6) {
+                    $octet = '(?:0?0?[0-9]|0?[1-9][0-9]|1[0-9]{2}|2[0-5][0-5]|2[0-4][0-9])'
+                    [regex]$ipv4 = "^(?:$octet\.){3}$octet$"
+                    $results = $results | Where-Object { $_.IPAddress -match $ipv4 }
                 }
             }
-        }
-        end {
-            #return results
-            Select-DefaultView -InputObject $results -Property ComputerName, InstanceName, SqlInstance, IPAddress, Port
+            #Default Execution of Get-DbaTcpPort
+            if ($AllTcpPort -eq $false -or ($AllTcpPort -eq $true -and $null -eq $someIps)) {
+                try {
+                    $server = Connect-SqlInstance -SqlInstance "TCP:$instance" -SqlCredential $SqlCredential -MinimumVersion 9
+                }
+                catch {
+                    Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $servername -Continue
+                }
+
+                # WmiComputer can be unreliable :( Use T-SQL
+                $sql = "SELECT local_net_address,local_tcp_port FROM sys.dm_exec_connections WHERE session_id = @@SPID"
+                $port = $server.Query($sql)
+
+                $results = [PsCustomObject]@{
+                            ComputerName = $server.ComputerName
+                            InstanceName = $server.ServiceName
+                            SqlInstance  = $server.DomainInstanceName
+                            IPAddress    = $port.local_net_address
+                            Port         = $port.local_tcp_port
+                            Static       = $true
+                            Type         = "Normal"
+                            }
+            }
         }
     }
+    end {
+        #return results
+        Select-DefaultView -InputObject $results -Property ComputerName, InstanceName, SqlInstance, IPAddress, Port
+    }
+}
