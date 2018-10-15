c1aaff1206b4ac7dc818164dc91a4ed1212f6478
Patrick Flynn <sqllensman@outlook.com>
2018-10-16 00:29:42 +1100

    Wait statistics (#4201)
    * Adding Full Coverage of Wait Statistics
    
    * Fixing Code Style
    
    * Additional Formatting Standards
    
    * Addint Tests for new functions
    
    * Fixing Tests and Divide by Zero Issues
    
    * Added extra Ignorable waits
    
    * Removing Position = 0 for SqlInstance Parameter
    
    * Remove unused Parameter
    
    * Fixing Tabs in Get-DbaIoLatency
    
    * exclude running some tests on appveyor

--- tests/Get-DbaIoLatency.Tests.ps1
+++ tests/Get-DbaIoLatency.Tests.ps1
@@ -4,7 +4,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tags "UnitTests" {
     Context "Validate parameters" {
-        $knownParameters = 'SqlInstance', 'SqlCredential', 'Threshold', 'IncludeIgnorable', 'EnableException'
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'EnableException'
         $paramCount = $knownParameters.Count
         $SupportShouldProcess = $false
         if ($SupportShouldProcess) {
@@ -28,37 +28,10 @@ Describe "$CommandName Unit Tests" -Tags "UnitTests" {
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "Command returns proper info" {
-        $results = Get-DbaWaitStatistic -SqlInstance $script:instance2 -Threshold 100
+        $results = Get-DbaIoLatency -SqlInstance $script:instance2
 
         It "returns results" {
             $results.Count -gt 0 | Should Be $true
-        }
-
-        foreach ($result in $results) {
-            It "returns a hyperlink" {
-                $result.URL -match 'sqlskills.com' | Should Be $true
-            }
-        }
-    }
-
-    Context "Command returns proper info when using parameter IncludeIgnorable" {
-        $ignoredWaits = 'REQUEST_FOR_DEADLOCK_SEARCH', 'SLEEP_MASTERDBREADY', 'SLEEP_TASK', 'LAZYWRITER_SLEEP'
-        $results = Get-DbaWaitStatistic -SqlInstance $script:instance2 -Threshold 100 -IncludeIgnorable | Where-Object {
-                $ignoredWaits -contains $_.WaitType
-            }
-
-        It "returns results" {
-            $results | Should -Not -BeNullOrEmpty
-        }
-
-        It "results includes ignorable column" {
-            $results[0].PSObject.Properties.Name.Contains('Ignorable') | Should Be $true
-        }
-
-        foreach ($result in $results) {
-            It "returns a hyperlink" {
-                $result.URL -match 'sqlskills.com' | Should Be $true
-            }
         }
     }
 }
