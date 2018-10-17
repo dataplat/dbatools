81bae33fb1157bf93c1acd76670571ab76112eb0
Chrissy LeMaire <clemaire@gmail.com>
2018-10-17 09:48:59 +0200

    fixed test

--- tests/Remove-DbaAgDatabase.Tests.ps1
+++ tests/Remove-DbaAgDatabase.Tests.ps1
@@ -23,23 +23,24 @@ Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $computername = ($script:instance3).Split("\")[0]
-        $null = Get-DbaProcess -SqlInstance $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-        $server = Connect-DbaInstance -SqlInstance $script:instance3
-        $dbname = "dbatoolsci_removeag_agroupdb"
-        $server.Query("create database $dbname")
-        $agname = "dbatoolsci_agdb"
-        $backup = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
-        # New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false
-        $server.Query("CREATE AVAILABILITY GROUP dbatoolsci_agroup
+        try {
+            $computername = ($script:instance3).Split("\")[0]
+            $null = Get-DbaProcess -SqlInstance $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+            $server = Connect-DbaInstance -SqlInstance $script:instance3
+            $dbname = "dbatoolsci_removeag_agroupdb"
+            $server.Query("create database $dbname")
+            $agname = "dbatoolsci_removeag_agroup"
+            $backup = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
+            # New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false
+            $server.Query("CREATE AVAILABILITY GROUP $agname
                             WITH (DB_FAILOVER = OFF, DTC_SUPPORT = NONE, CLUSTER_TYPE = NONE)
                             FOR DATABASE $dbname REPLICA ON N'$script:instance3'
                             WITH (ENDPOINT_URL = N'TCP://$computername`:5022', FAILOVER_MODE = MANUAL, AVAILABILITY_MODE = SYNCHRONOUS_COMMIT)")
+        } catch {}
     }
     AfterAll {
-        Remove-DbaAgDatabase -SqlInstance $server -Database $dbname -Confirm:$false
         Get-DbaAgDatabase -SqlInstance $server -Database $dbname | Remove-DbaAgDatabase -Confirm:$false
-        Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup dbatoolsci_agroup -Confirm:$false
+        Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
         Remove-DbaEndpoint -SqlInstance $server -Endpoint dbatoolsci_AGEndpoint -Confirm:$false
         Get-DbaDbCertificate -SqlInstance $server -Certificate dbatoolsci_AGCert | Remove-DbaDbCertificate -Confirm:$false
         Get-DbaAgDatabase -SqlInstance $server -Database $dbname | Remove-DbaAgDatabase -Confirm:$false
@@ -48,14 +49,14 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     Context "removes ag db" {
         It "returns removed results" {
             $results = Remove-DbaAgDatabase -SqlInstance $script:instance3 -Database $dbname -Confirm:$false
-            $results.AvailabilityGroup | Should -Be 'dbatoolsci_agroup'
+            $results.AvailabilityGroup | Should -Be $agname
             $results.Database | Should -Be $dbname
             $results.Status | Should -Be 'Removed'
         }
         
         It "really removed the db from the ag" {
-            $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup dbatoolsci_agroup
-            $results.AvailabilityGroup | Should -Be 'dbatoolsci_agroup'
+            $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname
+            $results.AvailabilityGroup | Should -Be $agname
             $results.AvailabilityDatabases.Name | Should -Not -Contain $dbname
         }
     }
