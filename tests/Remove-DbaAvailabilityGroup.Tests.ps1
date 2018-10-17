8405babd0db345591c195c1eba3da5771dcb2c35
Chrissy LeMaire <clemaire@gmail.com>
2018-10-17 10:56:06 +0200

    update test

--- tests/Remove-DbaAvailabilityGroup.Tests.ps1
+++ tests/Remove-DbaAvailabilityGroup.Tests.ps1
@@ -23,16 +23,16 @@ Describe "$commandname Unit Tests" -Tag 'UnitTests' {
 
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $null = New-DbaAvailabilityGroup -Primary $script:instance3 -Name dbatoolsci_removeagroup -ClusterType None -FailoverMode Manual -Confirm:$false
+        $null = New-DbaAvailabilityGroup -Primary $script:instance3 -Name dbatoolsci_removewholegroup -ClusterType None -FailoverMode Manual -Confirm:$false
     }
     Context "removes the newly created ag" {
         It "removes the ag" {
-            $results = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup dbatoolsci_removeagroup -Confirm:$false
+            $results = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup dbatoolsci_removewholegroup -Confirm:$false
             $results.Status | Should -Be 'Removed'
-            $results.AvailabilityGroup | Should -Be 'dbatoolsci_removeagroup'
+            $results.AvailabilityGroup | Should -Be 'dbatoolsci_removewholegroup'
         }
         It "really removed the ag" {
-            $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup dbatoolsci_removeagroup
+            $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup dbatoolsci_removewholegroup
             $results | Should -BeNull
         }
     }
