89495306126a6c47f2b68053a6d5f7db9c7e44d9
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-10-18 12:38:09 -0500

    Adding Actions to New-DbaDacOption

--- tests/New-DbaDacOption.Tests.ps1
+++ tests/New-DbaDacOption.Tests.ps1
@@ -3,10 +3,16 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$commandname Unit Tests" -Tag "UnitTests" {
-    It "Returns dacpac options" {
-        New-DbaDacOption | Should -Not -BeNullOrEmpty
+    It "Returns dacpac export options" {
+        New-DbaDacOption -Action Export | Should -Not -BeNullOrEmpty
     }
-    It "Returns bacpac options" {
-        New-DbaDacOption -Type Bacpac | Should -Not -BeNullOrEmpty
+    It "Returns bacpac export options" {
+        New-DbaDacOption -Action Export -Type Bacpac | Should -Not -BeNullOrEmpty
+    }
+    It "Returns dacpac publish options" {
+        New-DbaDacOption -Action Publish  | Should -Not -BeNullOrEmpty
+    }
+    It "Returns bacpac publish options" {
+        New-DbaDacOption -Action Publish -Type Bacpac | Should -Not -BeNullOrEmpty
     }
 }
