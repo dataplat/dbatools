34f57072b945952257dd6c1e00965a475fa93ae6
Joshua Corrick <joshcorr@users.noreply.github.com>
2018-10-18 02:07:21 -0400

    Fix PSAvoidUsingCmdletAliases (#4237)
    * Replace foreach with ForEach-Object
    
    * Replace select with Select-Object
    
    * Replace Export-SqlSpConfigure for Export-DbaSpConfigure
    
    * Replace Show-DbaDatabaseList with Show-DbaDbList
    
    * Add Basic IntegrationTests

--- tests/Get-DbaDbUdf.Tests.ps1
+++ tests/Get-DbaDbUdf.Tests.ps1
@@ -16,8 +16,65 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll{
+#Test Function adapted from examples at:
+#https://docs.microsoft.com/en-us/sql/t-sql/statements/create-function-transact-sql?view=sql-server-2017#examples
+$CreateTestUDFunction = @"
+CREATE FUNCTION dbo.dbatoolssci_ISOweek (@DATE datetime)
+RETURNS int
+WITH EXECUTE AS CALLER
+AS
+BEGIN
+     DECLARE @ISOweek int;
+     SET @ISOweek= DATEPART(wk,@DATE)+1
+          -DATEPART(wk,CAST(DATEPART(yy,@DATE) as CHAR(4))+'0104');
+--Special cases: Jan 1-3 may belong to the previous year
+     IF (@ISOweek=0)
+          SET @ISOweek=dbo.ISOweek(CAST(DATEPART(yy,@DATE)-1
+               AS CHAR(4))+'12'+ CAST(24+DATEPART(DAY,@DATE) AS CHAR(2)))+1;
+--Special case: Dec 29-31 may belong to the next year
+     IF ((DATEPART(mm,@DATE)=12) AND
+          ((DATEPART(dd,@DATE)-DATEPART(dw,@DATE))>= 28))
+          SET @ISOweek=1;
+     RETURN(@ISOweek);
+END;
+"@
+        Invoke-Sqlcmd2 -ServerInstance $script:instance2 -Query $CreateTestUDFunction -Database master
+    }
+    AfterAll{
+        $DropTestUDFunction = "DROP FUNCTION dbo.dbatoolssci_ISOweek;"
+        Invoke-Sqlcmd2 -ServerInstance $script:instance2 -Query $DropTestUDFunction -Database master
+    }
+
+    Context "Partition Functions are correctly located" {
+        $results1 = Get-DbaDbUdf -SqlInstance $script:instance2 -Database master | Where-object {$_.name -eq 'dbatoolssci_ISOweek'} | Select-Object *
+        $results2 = Get-DbaDbUdf -SqlInstance $script:instance2
+
+        It "Should execute and return results"{
+            $results2 | Should -Not -Be $null
+        }
+
+        It "Should execute against Master and return results"{
+            $results1 | Should -Not -Be $null
+        }
+
+        It "Should have matching name dbo.dbatoolssci_ISOweek" {
+            $results1.name | Should -Be 'dbatoolssci_ISOweek'
+            $results1.schema | Should -Be 'dbo'
+        }
+
+        It "Should have a function type of Scalar " {
+            $results1.FunctionType | Should -Be 'Scalar'
+        }
+
+        It "Should have Parameters of [@Date]" {
+            $results1.Parameters | Should -Be "[@Date]"
+        }
+
+        It "Should not Throw an Error" {
+            {Get-DbaDbUdf -SqlInstance $script:instance2 -ExcludeDatabase master -ExcludeSystemUdf } | Should -not -Throw
+        }
+    }
+}
