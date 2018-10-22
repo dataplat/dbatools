002717295969ea0843acbffd65da6e1969fdeaf2
Mötz Jensen <motz@e-s.dk>
2018-10-22 08:49:35 +0200

    Update functions/Get-DbaSsisExecutionHistory.ps1

--- functions/Get-DbaSsisExecutionHistory.ps1
+++ functions/Get-DbaSsisExecutionHistory.ps1
@@ -62,7 +62,7 @@ function Get-DbaSsisExecutionHistory {
         Shows what would happen if the command were executed and would return the SQL statement that would be executed per instance.
 
 #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param (
         [parameter(Mandatory)]
         [DbaInstanceParameter[]]$SqlInstance,
