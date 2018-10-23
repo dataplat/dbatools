4064df0fc3556c44630a4558a3d0eaa2402fa779
niphlod <niphlod@gmail.com>
2018-10-24 00:01:43 +0200

    Invoke-DbaFormatter, fixes #4206

---
+++ tests/Invoke-DbaFormatter.Tests.ps1
@@ -0,0 +1,62 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        $paramCount = 2
+        $defaultParamCount = 11
+        [object[]]$params = (Get-ChildItem function:\Invoke-DbaFormatter).Parameters.Keys
+        $knownParameters = 'Path','EnableException'
+        It "Should contain our specific parameters" {
+            ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
+        }
+        It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should -Be $paramCount
+        }
+    }
+}
+
+Describe "$CommandName IntegrationTests" -Tag "IntegrationTests" {
+    $content = @'
+function Get-DbaStub {
+        <#
+        .SYNOPSIS
+            is a stub
+
+        .DESCRIPTION
+            Using
+#>
+process {
+        Write-Message -Level Verbose "stub"
+}}
+
+
+'@
+    $wantedContent = @'
+function Get-DbaStub {
+    <#
+        .SYNOPSIS
+            is a stub
+
+        .DESCRIPTION
+            Using
+#>
+    process {
+        Write-Message -Level Verbose "stub"
+    }
+}
+
+'@
+
+    Context "formatting actually works" {
+        $temppath = Join-Path $TestDrive 'somefile.ps1'
+        Set-Content -Value $content -Path $temppath
+        Invoke-DbaFormatter -Path $temppath
+        $newcontent = Get-Content -Path $temppath -Raw
+        It "should format things according to dbatools standards" {
+            $newcontent | Should -Be $wantedContent
+        }
+    }
+
+}
