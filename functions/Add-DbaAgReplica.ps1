9c5685a896db90c334be5804c2ee9889b81b1e40
Chrissy LeMaire <clemaire@gmail.com>
2018-10-23 17:35:32 +0200

    remove whitespace

--- functions/Add-DbaAgReplica.ps1
+++ functions/Add-DbaAgReplica.ps1
@@ -164,7 +164,7 @@ function Add-DbaAgReplica {
                     }
                     catch {
                         $Name = $server.DomainInstanceName
-                    }    
+                    }
                 }
             }
 
