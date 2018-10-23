9d92ab64cfea0be523ba314026d5f73539c45624
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2018-10-23 09:34:00 -0500

    Bacpac handling and full SMO model support in Dac* commands (#4256)
    * Adding support for bacpacs and rewriting everything and using SMO by default
    
    * adding support of bacpacs to EXE parameterSet
    
    * New-DbaDacOption initial commit
    
    * Switching to New-DbaDacOption; renames
    
    * Adding Events registration
    
    * Adding support for Publish comman
    
    * Adding Bacpac import functionality and DacOptions
    
    * Adding Actions to New-DbaDacOption
    
    * New tests for bacpacs and smo-based deployments
    
    * Properly obliterate temp files
    
    * Correctly handling deployment options for bacpacs
    
    * Providing new defaults for publihsing
    
    * ScriptOnly fixes
    
    * ScriptOnly and negative tests
    
    * Moving Xml handling into New-DbaDacOption
    
    * Properly removing leftover files
    
    * Fixing UnitTests tests

---
+++ tests/New-DbaDacOption.Tests.ps1
@@ -0,0 +1,27 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Unit Tests" -Tag "UnitTests" {
+    BeforeAll {
+        $publishprofile = New-DbaDacProfile -SqlInstance $script:instance1 -Database whatever -Path C:\temp
+    }
+    AfterAll {
+        Remove-Item -Confirm:$false -Path $publishprofile.FileName -ErrorAction SilentlyContinue
+    }
+    It "Returns dacpac export options" {
+        New-DbaDacOption -Action Export | Should -Not -BeNullOrEmpty
+    }
+    It "Returns bacpac export options" {
+        New-DbaDacOption -Action Export -Type Bacpac | Should -Not -BeNullOrEmpty
+    }
+    It "Returns dacpac publish options" {
+        New-DbaDacOption -Action Publish  | Should -Not -BeNullOrEmpty
+    }
+    It "Returns dacpac publish options from an xml" {
+        New-DbaDacOption -Action Publish -PublishXml $publishprofile.FileName -EnableException | Should -Not -BeNullOrEmpty
+    }
+    It "Returns bacpac publish options" {
+        New-DbaDacOption -Action Publish -Type Bacpac | Should -Not -BeNullOrEmpty
+    }
+}
