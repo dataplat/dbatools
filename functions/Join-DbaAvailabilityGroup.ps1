c6d629b13e771adaf00700ef4c72e085abfe1f98
Chrissy LeMaire <clemaire@gmail.com>
2018-10-28 01:38:35 +0200

    add cluster support

--- functions/Join-DbaAvailabilityGroup.ps1
+++ functions/Join-DbaAvailabilityGroup.ps1
@@ -16,6 +16,10 @@ function Join-DbaAvailabilityGroup {
     .PARAMETER AvailabilityGroup
         The availability group to join.
 
+    .PARAMETER ClusterType
+        Cluster type of the Availability Group. Only supported in SQL Server 2017 and above.
+        Options include: External, Wsfc or None.
+
     .PARAMETER InputObject
         Enables piped input from Get-DbaAvailabilityGroup.
 
@@ -66,6 +70,8 @@ function Join-DbaAvailabilityGroup {
         [DbaInstanceParameter[]]$SqlInstance,
         [PSCredential]$SqlCredential,
         [string[]]$AvailabilityGroup,
+        [ValidateSet('External', 'Wsfc', 'None')]
+        [string]$ClusterType,
         [parameter(ValueFromPipeline)]
         [Microsoft.SqlServer.Management.Smo.AvailabilityGroup[]]$InputObject,
         [switch]$EnableException
@@ -73,8 +79,11 @@ function Join-DbaAvailabilityGroup {
     process {
         if ($InputObject) {
             $AvailabilityGroup += $InputObject.Name
+            if (-not $ClusterType) {
+                $ClusterType = ($InputObject | Select-Object -First 1).ClusterType
+            }
         }
-
+        
         if (-not $AvailabilityGroup) {
             Stop-Function -Message "No availability group to add"
             return
@@ -90,7 +99,11 @@ function Join-DbaAvailabilityGroup {
             foreach ($ag in $AvailabilityGroup) {
                 if ($Pscmdlet.ShouldProcess($server.Name, "Joining $ag")) {
                     try {
-                        $server.JoinAvailabilityGroup($ag)
+                        if ($ClusterType) {
+                            $server.Query("ALTER AVAILABILITY GROUP $ag JOIN WITH (CLUSTER_TYPE = $ClusterType)")
+                        } else {
+                            $server.JoinAvailabilityGroup($ag)
+                        }
                     } catch {
                         Stop-Function -Message "Failure" -ErrorRecord $_ -Continue
                     }
