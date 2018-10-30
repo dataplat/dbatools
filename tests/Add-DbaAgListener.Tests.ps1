a8193132d62b1a692a74d23fe3d648b45429f63a
Chrissy LeMaire <clemaire@gmail.com>
2018-10-30 16:51:46 +0100

    fixed test

--- tests/Add-DbaAgListener.Tests.ps1
+++ tests/Add-DbaAgListener.Tests.ps1
@@ -8,7 +8,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
             Get commands, Default count = 11
             Commands with SupportShouldProcess = 13
         #>
-        $defaultParamCount = 11
+        $defaultParamCount = 13
         [object[]]$params = (Get-ChildItem function:\Add-DbaAgListener).Parameters.Keys
         $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'IPAddress', 'SubnetMask', 'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
         $paramCount = $knownParameters.Count
