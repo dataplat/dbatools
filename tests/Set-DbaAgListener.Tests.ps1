f87d2768580a0abe5ba315cf071733a981c990e5
Chrissy LeMaire <clemaire@gmail.com>
2018-10-30 16:42:15 +0100

    adds and updates

--- tests/Set-DbaAgListener.Tests.ps1
+++ tests/Set-DbaAgListener.Tests.ps1
@@ -1,4 +1,4 @@
-$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
@@ -9,8 +9,8 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
             Commands with SupportShouldProcess = 13
         #>
         $defaultParamCount = 11
-        [object[]]$params = (Get-ChildItem function:\Add-DbaAgListener).Parameters.Keys
-        $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'IPAddress', 'SubnetMask', 'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
+        [object[]]$params = (Get-ChildItem function:\Set-DbaAgListener).Parameters.Keys
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Listener', 'Port', 'InputObject', 'EnableException'
         $paramCount = $knownParameters.Count
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
