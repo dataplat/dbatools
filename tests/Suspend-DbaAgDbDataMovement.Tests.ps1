ad338d8ffdf3f679ab461cc0de872a4699f68aaa
Chrissy LeMaire <clemaire@gmail.com>
2018-10-30 15:48:29 +0100

    initial commit

---
+++ tests/Suspend-DbaAgDbDataMovement.Tests.ps1
@@ -0,0 +1,48 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        $paramCount = 6
+        <#
+            Get commands, Default count = 11
+            Commands with SupportShouldProcess = 13
+        #>
+        $defaultParamCount = 13
+        [object[]]$params = (Get-ChildItem function:\Suspend-DbaAgDbDataMovement).Parameters.Keys
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Database', 'InputObject', 'EnableException'
+        it "Should contain our specific parameters" {
+            ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+        it "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        }
+    }
+}
+
+Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $null = Get-DbaProcess -SqlInstance $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+        $server = Connect-DbaInstance -SqlInstance $script:instance3
+        $agname = "dbatoolsci_resumeagdb_agroup"
+        $dbname = "dbatoolsci_resumeagdb_agroupdb"
+        $server.Query("create database $dbname")
+        $null = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
+        $null = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase -Type Log
+        $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false -Certificate dbatoolsci_AGCert -UseLastBackups
+        $null = Get-DbaAgDatabase -SqlInstance $script:instance3 -AvailabilityGroup $agname | Resume-DbaAgDbDataMovement -Confirm:$false
+    }
+    AfterAll {
+        $null = Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
+        $null = Remove-DbaDatabase -SqlInstance $server -Database $dbname -Confirm:$false
+    }
+    Context "resumes  data movement" {
+        It "returns resumed results" {
+            $results = Resume-DbaAgDbDataMovement -SqlInstance $script:instance3 -Database $dbname -Confirm:$false
+            $results.AvailabilityGroup | Should -Be $agname
+            $results.Name | Should -Be $dbname
+            $results.SynchronizationState | Should -Be 'NotSynchronized'
+        }
+    }
+} #$script:instance2 for appveyor
