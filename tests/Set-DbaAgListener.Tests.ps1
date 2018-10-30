5fede55193f532f1958af690aa2544d3cc525f48
Chrissy LeMaire <clemaire@gmail.com>
2018-10-30 16:43:08 +0100

    remove integration tests for now
    gotta add back

--- tests/Set-DbaAgListener.Tests.ps1
+++ tests/Set-DbaAgListener.Tests.ps1
@@ -20,19 +20,3 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         }
     }
 }
-
-Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
-    BeforeAll {
-        $agname = "dbatoolsci_ag_newlistener"
-    }
-    AfterAll {
-        $null = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname -Confirm:$false
-    }
-    Context "creates a listener" {
-        It "returns results with proper data" {
-            $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
-            $results = $ag | Add-DbaAgListener -IPAddress 127.0.20.1 -Confirm:$false
-            $results.PortNumber | Should -Be 1433
-        }
-    }
-} #$script:instance2 for appveyor
