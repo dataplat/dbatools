6d5c271cf4e83ad5f8dfc8fc26170d3cac151ffd
Chrissy LeMaire <clemaire@gmail.com>
2018-10-31 16:32:30 +0100

    initial commit of skeleton

---
+++ functions/Sync-DbaAvailabilityGroup.ps1
@@ -0,0 +1,132 @@
+﻿#ValidationTags#Messaging,FlowControl,Pipeline,CodeStyle#
+function Sync-DbaAvailabilityGroup {
+    <#
+    .SYNOPSIS
+        Automates the creation of availaibility groups.
+
+    .DESCRIPTION
+        
+    .PARAMETER Primary
+        The primary SQL Server instance. Server version must be SQL Server version 2012 or higher.
+
+    .PARAMETER PrimarySqlCredential
+        Login to the primary instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+
+    .PARAMETER Secondary
+        The target SQL Server instance or instances. Server version must be SQL Server version 2012 or higher.
+
+    .PARAMETER SecondarySqlCredential
+        Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+
+    .PARAMETER AvailabilityGroup
+        The name of the Availability Group.
+
+    .PARAMETER Exclude
+        Exclude one or more objects to export
+
+        Databases
+        Logins
+        AgentServer
+        Credentials
+        LinkedServers
+        SpConfigure
+        CentralManagementServer
+        DatabaseMail
+        SysDbUserObjects
+        SystemTriggers
+        BackupDevices
+        Audits
+        Endpoints
+        ExtendedEvents
+        PolicyManagement
+        ResourceGovernor
+        ServerAuditSpecifications
+        CustomErrors
+        ServerRoles
+        AvailabilityGroups
+        ReplicationSettings
+
+    .PARAMETER InputObject
+        Enables piping from Get-DbaAvailabilityGroup.
+    
+    .PARAMETER WhatIf
+        Shows what would happen if the command were to run. No actions are actually performed.
+
+    .PARAMETER Confirm
+        Prompts you for confirmation before executing any changing operations within the command.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: HA
+        Author: Chrissy LeMaire (@cl), netnerds.net
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://dbatools.io/Sync-DbaAvailabilityGroup
+
+    .EXAMPLE
+        PS C:\> Sync-DbaAvailabilityGroup -Primary sql2016a -Name SharePoint
+
+        Creates a new availability group on sql2016a named SharePoint
+#>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param (
+        [DbaInstanceParameter]$Primary,
+        [PSCredential]$PrimarySqlCredential,
+        [DbaInstanceParameter[]]$Secondary,
+        [PSCredential]$SecondarySqlCredential,
+        [string]$AvailabilityGroup,
+        [ValidateSet('Databases', 'Logins', 'AgentServer', 'Credentials', 'LinkedServers', 'SpConfigure', 'CentralManagementServer', 'DatabaseMail', 'SysDbUserObjects', 'SystemTriggers', 'BackupDevices', 'Audits', 'Endpoints', 'ExtendedEvents', 'PolicyManagement', 'ResourceGovernor', 'ServerAuditSpecifications', 'CustomErrors', 'ServerRoles', 'AvailabilityGroups', 'ReplicationSettings')]
+        [string[]]$Exclude,
+        [parameter(ValueFromPipeline)]
+        [Microsoft.SqlServer.Management.Smo.AvailabilityGroup[]]$InputObject,
+        [switch]$EnableException
+    )
+    process {
+        $stepCounter = $wait = 0
+        $totalSteps = 10
+        $activity = "Adding new availability group $name"
+        
+        if ($Force -and $Secondary -and (-not $NetworkShare -and -not $UseLastBackups) -and ($SeedingMode -ne 'Automatic')) {
+            Stop-Function -Message "NetworkShare or UseLastBackups is required when Force is used"
+            return
+        }
+        
+        try {
+            $server = Connect-SqlInstance -SqlInstance $Primary -SqlCredential $PrimarySqlCredential
+        } catch {
+            Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $Primary
+            return
+        }
+        
+        if ($SeedingMode -eq 'Automatic' -and $server.VersionMajor -lt 13) {
+            Stop-Function -Message "Automatic seeding mode only supported in SQL Server 2016 and above" -Target $Primary
+            return
+        }
+        
+        Write-ProgressHelper -TotalSteps $totalSteps -Activity $activity -StepNumber ($stepCounter++) -Message "Checking perquisites"
+                
+        if ($Secondary) {
+            $secondaries = @()
+            foreach ($computer in $Secondary) {
+                try {
+                    $secondaries += Connect-SqlInstance -SqlInstance $computer -SqlCredential $SecondarySqlCredential
+                } catch {
+                    Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $Primary
+                    return
+                }
+            }
+        }
+        
+        Write-ProgressHelper -TotalSteps $totalSteps -Activity $activity -StepNumber ($stepCounter++) -Message "Granting permissions on availability group, this may take a moment"
+        
+        # Get results
+        Get-DbaAvailabilityGroup -SqlInstance $Primary -SqlCredential $PrimarySqlCredential -AvailabilityGroup $Name
+    }
+}
