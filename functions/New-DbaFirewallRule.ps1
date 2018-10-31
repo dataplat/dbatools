510c0900b6fd7ee5c4c28c0fe8cf96ae671f250c
Chrissy LeMaire <clemaire@gmail.com>
2018-10-31 17:09:44 +0100

    formatting

--- functions/New-DbaFirewallRule.ps1
+++ functions/New-DbaFirewallRule.ps1
@@ -1,123 +1,123 @@
 ﻿function New-DbaFirewallRule {
     <#
-        .SYNOPSIS
-            Adds new firewall rules to Windows Firewall with Advanced Security for SQL Server components.
+    .SYNOPSIS
+        Adds new firewall rules to Windows Firewall with Advanced Security for SQL Server components.
 
-        .DESCRIPTION
-            Based on https://ryanmangansitblog.com/2013/05/01/powershell-script-for-sql-firewall-rules/
-            Adds new firewall rules to Windows Firewall with Advanced Security application for SQL Server components.
-            Includes the Database Engine, SSAS, SSIS, SSRS, and more, and assigns them to a "SQL" Display Group by default.
-            Unless the user provides specific ports for specific components, the default ports will be used.
-            3rd party Firewalls are not supported.
+    .DESCRIPTION
+        Based on https://ryanmangansitblog.com/2013/05/01/powershell-script-for-sql-firewall-rules/
+        Adds new firewall rules to Windows Firewall with Advanced Security application for SQL Server components.
+        Includes the Database Engine, SSAS, SSIS, SSRS, and more, and assigns them to a "SQL" Display Group by default.
+        Unless the user provides specific ports for specific components, the default ports will be used.
+        3rd party Firewalls are not supported.
 
-        .PARAMETER ComputerName
-            The target SQL Server. Default is localhost ($env:COMPUTERNAME).
+    .PARAMETER ComputerName
+        The target SQL Server. Default is localhost ($env:COMPUTERNAME).
 
-        .PARAMETER Credential
-            Allows you to login to $ComputerName using alternative credentials.
+    .PARAMETER Credential
+        Allows you to login to $ComputerName using alternative credentials.
 
-        .PARAMETER NetworkProfile
-            Specifies the Windows Firewall Network Profile. Default is Domain.
+    .PARAMETER NetworkProfile
+        Specifies the Windows Firewall Network Profile. Default is Domain.
 
-        .PARAMETER DisplayGroup
-            Specifies the Windows Firewall Display Group. Default is SQL.
+    .PARAMETER DisplayGroup
+        Specifies the Windows Firewall Display Group. Default is SQL.
 
-        .PARAMETER PortDbEngine
-            Specifies the TCP port used by the Database Engine. Default is 1433.
+    .PARAMETER PortDbEngine
+        Specifies the TCP port used by the Database Engine. Default is 1433.
 
-        .PARAMETER PortDbEngineDAC
-            Specifies the TCP port used by the Database Engine Dedicated Admin Connection. Default is 1434.
+    .PARAMETER PortDbEngineDAC
+        Specifies the TCP port used by the Database Engine Dedicated Admin Connection. Default is 1434.
 
-        .PARAMETER PortBrowserService
-            Specifies the UDP port used by the Browser Service. Default is 1434.
+    .PARAMETER PortBrowserService
+        Specifies the UDP port used by the Browser Service. Default is 1434.
 
-        .PARAMETER PortServiceBroker
-            Specifies the TCP port used by the Service Broker. Default is 4022.
-            Run the below TSQL query to confirm Service Broker is in use and the running port.
-            SELECT name, protocol_desc, port, state_desc FROM sys.tcp_endpoints WHERE type_desc = 'SERVICE_BROKER'
+    .PARAMETER PortServiceBroker
+        Specifies the TCP port used by the Service Broker. Default is 4022.
+        Run the below TSQL query to confirm Service Broker is in use and the running port.
+        SELECT name, protocol_desc, port, state_desc FROM sys.tcp_endpoints WHERE type_desc = 'SERVICE_BROKER'
 
-        .PARAMETER PortDebuggerRPC
-            Specifies the TCP port used by the TSQL Debugger/Remote Procedure Call. Default is 135.
-            Please see the below documentation for additional considerations not covered by this command:
-            https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access#BKMK_port_135
-            https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access#BKMK_IPsec
+    .PARAMETER PortDebuggerRPC
+        Specifies the TCP port used by the TSQL Debugger/Remote Procedure Call. Default is 135.
+        Please see the below documentation for additional considerations not covered by this command:
+        https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access#BKMK_port_135
+        https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access#BKMK_IPsec
 
-        .PARAMETER PortSSASDefaultInstance
-            Specifies the TCP port used by the SQL Server Analysis Services (SSAS) default instance. Default is 2383.
+    .PARAMETER PortSSASDefaultInstance
+        Specifies the TCP port used by the SQL Server Analysis Services (SSAS) default instance. Default is 2383.
 
-        .PARAMETER PortSSASNamedInstance
-            Specifies the TCP port used by a SQL Server Analysis Services (SSAS) named instance. Default is 2382.
-            NOTE: This is also needed for for "Power Pivot" named instance for SharePoint.
-            Named instances use dynamic port assignments.
-            As the discovery service for Analysis Services, SQL Server Browser service listens on TCP port 2382 and redirects the connection request to the port currently used by Analysis Services.
+    .PARAMETER PortSSASNamedInstance
+        Specifies the TCP port used by a SQL Server Analysis Services (SSAS) named instance. Default is 2382.
+        NOTE: This is also needed for for "Power Pivot" named instance for SharePoint.
+        Named instances use dynamic port assignments.
+        As the discovery service for Analysis Services, SQL Server Browser service listens on TCP port 2382 and redirects the connection request to the port currently used by Analysis Services.
 
-        .PARAMETER PortSSIS
-            Specifies the TCP port used by SQL Server Integration Services (SSIS). Default is 135, and this port cannot be changed. Hidden parameter.
-            The path to your SSIS server's MsDtsSrvr.exe will be detected, and a separate firewall rule for this specific executable is also added (CURRENTLY BROKEN FOR MULTIPLE SSIS INSTANCES).
+    .PARAMETER PortSSIS
+        Specifies the TCP port used by SQL Server Integration Services (SSIS). Default is 135, and this port cannot be changed. Hidden parameter.
+        The path to your SSIS server's MsDtsSrvr.exe will be detected, and a separate firewall rule for this specific executable is also added (CURRENTLY BROKEN FOR MULTIPLE SSIS INSTANCES).
 
-        .PARAMETER PortSSRS
-            Specifies the TCP port used by SQL Server Reporting Services (SSRS). Default is 80.
+    .PARAMETER PortSSRS
+        Specifies the TCP port used by SQL Server Reporting Services (SSRS). Default is 80.
 
-        .PARAMETER PortSSRSSSL
-            Specifies the TCP port used by SQL Server Reporting Services (SSRS) using SSL. Default is 443.
+    .PARAMETER PortSSRSSSL
+        Specifies the TCP port used by SQL Server Reporting Services (SSRS) using SSL. Default is 443.
 
-        .PARAMETER PortSSMSBrowse
-            Specifies the UDP port used by the SQL Server Management Studio (SSMS) Browse button. Default is 1433.
+    .PARAMETER PortSSMSBrowse
+        Specifies the UDP port used by the SQL Server Management Studio (SSMS) Browse button. Default is 1433.
 
-        .PARAMETER Force
-            The force parameter will ignore some errors in the parameters and assume defaults.
+    .PARAMETER Force
+        The force parameter will ignore some errors in the parameters and assume defaults.
 
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-        .PARAMETER WhatIf
-            If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
-        .PARAMETER Confirm
-            If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
 
-        .NOTES
-            Tags: Firewall, Shoe
-            Author: John G Hohengarten (@wsuhoey)
-            Contributors: Constantine Kokkinos (@ck), Jason Squires (@js_0505), Patrick Flynn (@sqllensman)
-            Website: https://dbatools.io
-            Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
-            License: MIT https://opensource.org/licenses/MIT
+    .NOTES
+        Tags: Firewall, Shoe
+        Author: John G Hohengarten (@wsuhoey)
+        Contributors: Constantine Kokkinos (@mobileck), Jason Squires (@js_0505), Patrick Flynn (@sqllensman)
+        Website: https://dbatools.io
+        Copyright: (C) Chrissy LeMaire, clemaire@gmail.com
+        License: MIT https://opensource.org/licenses/MIT
 
-            ###  References ###
-            # https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access
-            # https://docs.microsoft.com/en-us/sql/analysis-services/instances/configure-the-windows-firewall-to-allow-analysis-services-access
-            # https://docs.microsoft.com/en-us/sql/integration-services/service/integration-services-service-ssis-service#configure-the-firewall
-            # https://docs.microsoft.com/en-us/sql/reporting-services/report-server/configure-a-firewall-for-report-server-access
-            # https://technet.microsoft.com/en-us/library/jj554908(v=wps.630).aspx
-            # https://serverfault.com/questions/325544/how-to-create-a-windows-2008-advanced-firewall-rules-group-definition-through-th/789187#789187 # helped with setting DisplayGroup for firewall rule
-            # https://sid-500.com/2017/12/11/configuring-windows-firewall-with-powershell/
-            ### /References ###
+        ###  References ###
+        # https://docs.microsoft.com/en-us/sql/sql-server/install/configure-the-windows-firewall-to-allow-sql-server-access
+        # https://docs.microsoft.com/en-us/sql/analysis-services/instances/configure-the-windows-firewall-to-allow-analysis-services-access
+        # https://docs.microsoft.com/en-us/sql/integration-services/service/integration-services-service-ssis-service#configure-the-firewall
+        # https://docs.microsoft.com/en-us/sql/reporting-services/report-server/configure-a-firewall-for-report-server-access
+        # https://technet.microsoft.com/en-us/library/jj554908(v=wps.630).aspx
+        # https://serverfault.com/questions/325544/how-to-create-a-windows-2008-advanced-firewall-rules-group-definition-through-th/789187#789187 # helped with setting DisplayGroup for firewall rule
+        # https://sid-500.com/2017/12/11/configuring-windows-firewall-with-powershell/
+        ### /References ###
 
-        .LINK
-            https://dbatools.io/New-DbaFirewallRule
+    .LINK
+        https://dbatools.io/New-DbaFirewallRule
 
-        .EXAMPLE
-            New-DbaFirewallRule
+    .EXAMPLE
+        New-DbaFirewallRule
 
-            Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to the localhost (the default since unspecified).
+        Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to the localhost (the default since unspecified).
 
-        .EXAMPLE
-            New-DbaFirewallRule -ComputerName sql01
+    .EXAMPLE
+        New-DbaFirewallRule -ComputerName sql01
 
-            Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to server sql01.
+        Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to server sql01.
 
-        .EXAMPLE
-            New-DbaFirewallRule -ComputerName sql01 -PortDbEngine 5554
+    .EXAMPLE
+        New-DbaFirewallRule -ComputerName sql01 -PortDbEngine 5554
 
-            Adds default ports and rules for SQL server components, including the SSAS, SSIS, SSRS, except for the Database Engine which uses port 5554, to server sql01.
+        Adds default ports and rules for SQL server components, including the SSAS, SSIS, SSRS, except for the Database Engine which uses port 5554, to server sql01.
 
-        .EXAMPLE
-            New-DbaFirewallRule -ComputerName sql01 -DisplayGroup "SQL Rules"
+    .EXAMPLE
+        New-DbaFirewallRule -ComputerName sql01 -DisplayGroup "SQL Rules"
 
-            Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to server sql01, and assigns them to a "SQL Rules" Display Group.
+        Adds default ports and rules for SQL Server components, including the Database Engine, SSAS, SSIS, SSRS, to server sql01, and assigns them to a "SQL Rules" Display Group.
 
     #>
     [CmdletBinding(DefaultParameterSetName = "FirewallRules", SupportsShouldProcess = $true)]
@@ -126,12 +126,9 @@
         [Alias("ServerInstance", "SqlServer", "SqlInstance")]
         [string]$ComputerName = $env:COMPUTERNAME,
         [PSCredential]$Credential,
-        #$Credential = [System.Management.Automation.PSCredential]::Empty, # not sure which, seen in internal/functions/Test-PSRemoting.ps1
         [securestring]$Password,
-
         [string]$NetworkProfile = "Domain",
         [string]$DisplayGroup = "SQL", # From New-NetFirewallRule Help: The DisplayGroup parameter cannot be specified upon object creation using the New-NetFirewallRule cmdlet, but can be modified using dot-notation and the Set-NetFirewallRule cmdlet.
-
         [int]$PortDbEngine = 1433,
         [int]$PortDbEngineDAC = 1434,
         [int]$PortBrowserService = 1434,
@@ -146,9 +143,8 @@
         [int]$PortSSRS = 80,
         [int]$PortSSRSSSL = 443,
         [int]$PortSSMSBrowse = 1433,
-
         [switch]$Force,
-        [Alias('Silent')][switch]$EnableException
+        [switch]$EnableException
 
     )
     
