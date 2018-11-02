f634f0d6e16062c8ccbc58e2d5cf54c0492c6891
Patrick Flynn <sqllensman@outlook.com>
2018-11-02 22:44:01 +1100

    Update Tests to include unit test. Fixing documentation of Parameters

--- tests/Add-DbaCmsRegServer.Tests.ps1
+++ tests/Add-DbaCmsRegServer.Tests.ps1
@@ -2,6 +2,30 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+Describe "$CommandName Unit Tests" -Tags "UnitTests" {
+    Context "Validate parameters" {
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'ServerName', 'Name', 'Description', 'Group', 'InputObject', 'EnableException'
+        $SupportShouldProcess = $true
+        $paramCount = $knownParameters.Count
+        if ($SupportShouldProcess) {
+            $defaultParamCount = 13
+        }
+        else {
+            $defaultParamCount = 11
+        }
+        $command = Get-Command -Name $CommandName
+        [object[]]$params = $command.Parameters.Keys
+
+        It "Should contain our specific parameters" {
+            ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+
+        It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        }
+    }
+}
+
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     Context "Setup" {
         BeforeAll {
