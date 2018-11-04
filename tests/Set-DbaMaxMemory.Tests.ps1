de32834d97e7a80fdda30d696dfe6d69b7d1437f
Patrick Flynn <sqllensman@outlook.com>
2018-11-04 16:26:33 +1100

    Adding Unit tests to existing Tests

--- tests/Set-DbaMaxMemory.Tests.ps1
+++ tests/Set-DbaMaxMemory.Tests.ps1
@@ -2,6 +2,30 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+Describe "$CommandName Unit Tests" -Tags "UnitTests" {
+    Context "Validate parameters" {
+        $knownParameters = 'SqlInstance', 'SqlCredential', 'MaxMB', 'EnableException'
+        $SupportShouldProcess = $true
+        $paramCount = $knownParameters.Count
+        if ($SupportShouldProcess) {
+            $defaultParamCount = 13
+        }
+        else {
+            $defaultParamCount = 11
+        }
+        $command = Get-Command -Name $CommandName
+        [object[]]$params = $command.Parameters.Keys
+
+        It "Should contain our specific parameters" {
+            ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+
+        It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        }
+    }
+}
+
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
         $inst1CurrentSqlMax = (Get-DbaMaxMemory -SqlInstance $script:instance1).SqlMaxMB
@@ -19,15 +43,11 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             }
         }
     }
-}
-
-Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
-    InModuleScope dbatools {
-        Context 'Validate input arguments' {
-            It 'SqlInstance parameter host cannot be found' {
-                Set-DbaMaxMemory -SqlInstance 'ABC' 3> $null | Should be $null
-            }
+    Context 'Validate input arguments' {
+        It 'SqlInstance parameter host cannot be found' {
+            Set-DbaMaxMemory -SqlInstance 'ABC' 3> $null | Should be $null
         }
     }
 }
+
 
