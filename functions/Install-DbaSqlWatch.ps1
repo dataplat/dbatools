d0cfbeacde7573f5cf1170ac142d46e7ee0bcada
Chrissy LeMaire <clemaire@gmail.com>
2018-11-04 21:29:44 +0100

    fixed indent

--- functions/Install-DbaSqlWatch.ps1
+++ functions/Install-DbaSqlWatch.ps1
@@ -1,81 +1,81 @@
 #ValidationTags#CodeStyle,Messaging,FlowControl,Pipeline#
 function Install-DbaSqlWatch {
     <#
-        .SYNOPSIS
-            Installs or updates SqlWatch.
+    .SYNOPSIS
+        Installs or updates SqlWatch.
 
-        .DESCRIPTION
-            Downloads, extracts and installs or updates SqlWatch.
-            https://sqlwatch.io/
+    .DESCRIPTION
+        Downloads, extracts and installs or updates SqlWatch.
+        https://sqlwatch.io/
 
-        .PARAMETER SqlInstance
-            SQL Server name or SMO object representing the SQL Server to connect to.
+    .PARAMETER SqlInstance
+        SQL Server name or SMO object representing the SQL Server to connect to.
 
-        .PARAMETER SqlCredential
-            Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
 
-        .PARAMETER Database
-            Specifies the database to install SqlWatch into.s
+    .PARAMETER Database
+        Specifies the database to install SqlWatch into.s
 
-        .PARAMETER LocalFile
-            Specifies the path to a local file to install SqlWatch from. This *should* be the zipfile as distributed by the maintainers.
-            If this parameter is not specified, the latest version will be downloaded and installed from https://github.com/marcingminski/sqlwatch
+    .PARAMETER LocalFile
+        Specifies the path to a local file to install SqlWatch from. This *should* be the zipfile as distributed by the maintainers.
+        If this parameter is not specified, the latest version will be downloaded and installed from https://github.com/marcingminski/sqlwatch
 
-        .PARAMETER Force
-            If this switch is enabled, SqlWatch will be downloaded from the internet even if previously cached.
+    .PARAMETER Force
+        If this switch is enabled, SqlWatch will be downloaded from the internet even if previously cached.
 
-        .PARAMETER Confirm
-            Prompts to confirm actions
+    .PARAMETER Confirm
+        Prompts to confirm actions
 
-        .PARAMETER WhatIf
-            Shows what would happen if the command were to run. No actions are actually performed.
+    .PARAMETER WhatIf
+        Shows what would happen if the command were to run. No actions are actually performed.
 
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-        .NOTES
-            Tags: SqlWatch
-            Author: Ken K (github.com/koglerk)
-            Website: https://sqlwatch.io
-            Website: https://dbatools.io
-            Copyright: (c) 2018 by dbatools, licensed under MIT
-            License: MIT https://opensource.org/licenses/MIT
+    .NOTES
+        Tags: SqlWatch
+        Author: Ken K (github.com/koglerk)
+        Website: https://sqlwatch.io
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
 
-        .LINK
-            https://dbatools.io/Install-DbaSqlWatch
+    .LINK
+        https://dbatools.io/Install-DbaSqlWatch
 
-        .EXAMPLE
-            Install-DbaSqlWatch -SqlInstance server1 -Database master
+    .EXAMPLE
+        Install-DbaSqlWatch -SqlInstance server1 -Database master
 
-            Logs into server1 with Windows authentication and then installs SqlWatch in the master database.
+        Logs into server1 with Windows authentication and then installs SqlWatch in the master database.
 
-        .EXAMPLE
-            Install-DbaSqlWatch -SqlInstance server1\instance1 -Database DBA
+    .EXAMPLE
+        Install-DbaSqlWatch -SqlInstance server1\instance1 -Database DBA
 
-            Logs into server1\instance1 with Windows authentication and then installs SqlWatch in the DBA database.
+        Logs into server1\instance1 with Windows authentication and then installs SqlWatch in the DBA database.
 
-        .EXAMPLE
-            Install-DbaSqlWatch -SqlInstance server1\instance1 -Database master -SqlCredential $cred
+    .EXAMPLE
+        Install-DbaSqlWatch -SqlInstance server1\instance1 -Database master -SqlCredential $cred
 
-            Logs into server1\instance1 with SQL authentication and then installs SqlWatch in the master database.
+        Logs into server1\instance1 with SQL authentication and then installs SqlWatch in the master database.
 
-        .EXAMPLE
-            Install-DbaSqlWatch -SqlInstance sql2016\standardrtm, sql2016\sqlexpress, sql2014
+    .EXAMPLE
+        Install-DbaSqlWatch -SqlInstance sql2016\standardrtm, sql2016\sqlexpress, sql2014
 
-            Logs into sql2016\standardrtm, sql2016\sqlexpress and sql2014 with Windows authentication and then installs SqlWatch in the master database.
+        Logs into sql2016\standardrtm, sql2016\sqlexpress and sql2014 with Windows authentication and then installs SqlWatch in the master database.
 
-        .EXAMPLE
-            $servers = "sql2016\standardrtm", "sql2016\sqlexpress", "sql2014"
-            $servers | Install-DbaSqlWatch
+    .EXAMPLE
+        $servers = "sql2016\standardrtm", "sql2016\sqlexpress", "sql2014"
+        $servers | Install-DbaSqlWatch
 
-            Logs into sql2016\standardrtm, sql2016\sqlexpress and sql2014 with Windows authentication and then installs SqlWatch in the master database.
+        Logs into sql2016\standardrtm, sql2016\sqlexpress and sql2014 with Windows authentication and then installs SqlWatch in the master database.
 
-        .EXAMPLE
-            Install-DbaSqlWatch -SqlInstance sql2016 -Branch development
+    .EXAMPLE
+        Install-DbaSqlWatch -SqlInstance sql2016 -Branch development
 
-            Installs the dev branch version of SqlWatch in the master database on sql2016 instance.
+        Installs the dev branch version of SqlWatch in the master database on sql2016 instance.
     #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
     param (
