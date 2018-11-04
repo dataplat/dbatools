ebeb4cada1d2c7dc1d9e5037875d0d7e122eb8dc
Patrick Flynn <sqllensman@outlook.com>
2018-11-04 16:26:33 +1100

    Adding Unit tests to existing Tests

--- tests/Test-DbaEndpoint.Tests.ps1
+++ tests/Test-DbaEndpoint.Tests.ps1
@@ -2,6 +2,30 @@ $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+Describe "$CommandName Unit Tests" -Tags "UnitTests" {
+    Context "Validate parameters" {
+        $knownParameters = 'SqlInstance','SqlCredential','Endpoint', 'InputObject', 'EnableException'
+        $SupportShouldProcess = $false
+        $paramCount = $knownParameters.Count
+        if ($SupportShouldProcess) {
+            $defaultParamCount = 13
+        }
+        else {
+            $defaultParamCount = 11
+        }
+        $command = Get-Command -Name $CommandName
+        [object[]]$params = $command.Parameters.Keys
+
+        It "Should contain our specific parameters" {
+            ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+
+        It "Should only contain $paramCount parameters" {
+            $params.Count - $defaultParamCount | Should Be $paramCount
+        }
+    }
+}
+
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     It -Skip "returns success" {
         $results = Test-DbaEndpoint -SqlInstance $script:instance3
