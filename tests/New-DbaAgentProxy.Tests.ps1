2de2ea845423fb9bf611cb4d1e37a50e154be945
Joshua Corrick <joshua.corrick@live.com>
2018-11-04 21:42:02 -0500

    Replace -Credential with -ProxyCredential

--- tests/New-DbaAgentProxy.Tests.ps1
+++ tests/New-DbaAgentProxy.Tests.ps1
@@ -7,7 +7,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         $paramCount = 12
         $defaultParamCount = 13
         [object[]]$params = (Get-ChildItem function:\New-DbaAgentProxy).Parameters.Keys
-        $knownParameters = 'SqlInstance','SqlCredential','Name','Credential','SubSystem','Description','Login','ServerRole','MsdbRole','Disabled','Force','EnableException'
+        $knownParameters = 'SqlInstance','SqlCredential','Name','ProxyCredential','SubSystem','Description','Login','ServerRole','MsdbRole','Disabled','Force','EnableException'
         It "Should contain our specific parameters" {
             ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
         }
@@ -20,7 +20,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 # This is quite light of a test but setting up a proxy requires a lot of setup and I don't have time today
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "does not try to add without" {
-        $results = New-DbaAgentProxy -SqlInstance $script:instance2 -Name STIG -Credential 'dbatoolsci_proxytest' -WarningAction SilentlyContinue -WarningVariable warn
+        $results = New-DbaAgentProxy -SqlInstance $script:instance2 -Name STIG -ProxyCredential 'dbatoolsci_proxytest' -WarningAction SilentlyContinue -WarningVariable warn
         It "does not try to add the proxy without a valid credential" {
             $warn -match 'does not exist' | Should Be $true
         }
