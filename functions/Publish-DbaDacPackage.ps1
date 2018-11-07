d86c91d13b89b6dc40f1827a7b5047f2aa420646
Richie Lee <richie@bzzzt.io>
2018-11-07 22:22:22 +0000

    remove trailing whitespace

--- functions/Publish-DbaDacPackage.ps1
+++ functions/Publish-DbaDacPackage.ps1
@@ -303,7 +303,7 @@ function Publish-DbaDacPackage {
                         $dacServices.ImportBacpac($bacPackage, $dbname, $options, $null)
                     }
                 } catch [Microsoft.SqlServer.Dac.DacServicesException] {
-                    $message = Get-ErrorMessage -Record $_ 
+                    $message = Get-ErrorMessage -Record $_
                     Stop-Function -Message "Deployment failed - $($message)" -ErrorRecord $_ -Continue
                 } finally {
                     Unregister-Event -SourceIdentifier "msg"
