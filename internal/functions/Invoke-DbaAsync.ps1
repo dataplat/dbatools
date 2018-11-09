4d710e53a20c0719c9fc427f3a57abbe08b14b2b
Chrissy LeMaire <clemaire@gmail.com>
2018-11-10 00:56:04 +0100

    fixes #3860

--- internal/functions/Invoke-DbaAsync.ps1
+++ internal/functions/Invoke-DbaAsync.ps1
@@ -174,8 +174,9 @@ function Invoke-DbaAsync {
 
             $ds = New-Object system.Data.DataSet
             $da = New-Object system.Data.SqlClient.SqlDataAdapter($cmd)
-
+            
             if ($MessagesToOutput) {
+                $save = [System.Management.Automation.Runspaces.Runspace]::DefaultRunspace
                 $pool = [RunspaceFactory]::CreateRunspacePool(1, [int]$env:NUMBER_OF_PROCESSORS + 1)
                 $pool.ApartmentState = "MTA"
                 $pool.Open()
@@ -227,6 +228,7 @@ function Invoke-DbaAsync {
                 }
                 $pool.Close()
                 $pool.Dispose()
+                [System.Management.Automation.Runspaces.Runspace]::DefaultRunspace = $save
             } else {
                 #Following EventHandler is used for PRINT and RAISERROR T-SQL statements. Executed when -Verbose parameter specified by caller and no -MessageToOutput
                 if ($PSBoundParameters.Verbose) {
