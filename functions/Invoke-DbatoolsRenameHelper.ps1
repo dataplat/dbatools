985b78dd300e750e22de6b7253c990c10581b6ff
Chrissy LeMaire <clemaire@gmail.com>
2018-11-19 15:05:09 +0100

    formatting

--- functions/Invoke-DbatoolsRenameHelper.ps1
+++ functions/Invoke-DbatoolsRenameHelper.ps1
@@ -56,7 +56,7 @@ function Invoke-DbatoolsRenameHelper {
         Shows what would happen if the command would run. If the command would run and there were matches,
         the resulting changes would be written to disk as Ascii encoded.
 
-          #>
+    #>
     [CmdletBinding(SupportsShouldProcess = $true)]
     param (
         [parameter(Mandatory, ValueFromPipeline)]
