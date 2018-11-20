464ee5393459bfc801985ab8631769d492295fdd
Joshua Corrick <joshua.corrick@live.com>
2018-11-19 22:02:23 -0500

    Add IntegrationTests

--- tests/Get-DbaAgentSchedule.Tests.ps1
+++ tests/Get-DbaAgentSchedule.Tests.ps1
@@ -16,8 +16,35 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll{
+        $schedule = New-DbaAgentSchedule -SqlInstance $script:instance2 -Schedule dbatoolsci_MonthlyTest -FrequencyType Monthly -FrequencyInterval 10 -FrequencyRecurrenceFactor 1 -Force
+    }
+    Afterall{
+        $schedule = Get-DbaAgentSchedule -SqlInstance $script:instance2 -schedule dbatoolsci_MonthlyTest
+        $Schedule.DROP()
+    }
+
+    Context "Gets the list of Schedules" {
+        $results = Get-DbaAgentSchedule -SqlInstance $script:instance2
+        It "Results are not empty" {
+            $results | Should Not Be $Null
+        }
+    }
+    Context "Gets a single Schedule" {
+        $results = Get-DbaAgentSchedule -SqlInstance $script:instance2 -Schedule dbatoolsci_MonthlyTest
+        It "Results are not empty" {
+            $results | Should Not Be $Null
+        }
+        It "Should have the name MonthlyTest" {
+            $results.schedulename | Should Be "dbatoolsci_MonthlyTest"
+        }
+        It "Should have a frequency of 10" {
+            $results.FrequencyInterval | Should Be 10
+        }
+        It "Should be enabled" {
+            $results.isenabled | Should Be $true
+        }
+    }
+}
