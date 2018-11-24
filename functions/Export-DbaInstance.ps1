d7a85ef2559cceff477c6606f9724b0866ca92df
Chrissy LeMaire <clemaire@gmail.com>
2018-11-24 16:31:50 +0100

    remove changes

--- functions/Export-DbaInstance.ps1
+++ functions/Export-DbaInstance.ps1
@@ -164,7 +164,7 @@ function Export-DbaInstance {
             if (-not (Test-Bound -ParameterName Path)) {
                 $timenow = (Get-Date -uformat "%m%d%Y%H%M%S")
                 $mydocs = [Environment]::GetFolderPath('MyDocuments')
-                $path = Resolve-Path -Path "$mydocs\$($server.name.replace('\', '$'))-$timenow"
+                $path = "$mydocs\$($server.name.replace('\', '$'))-$timenow"
             }
 
             if (-not (Test-Path $Path)) {
