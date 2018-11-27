c563075113d105c4305459a8a7939c04b8642d11
Chrissy LeMaire <clemaire@gmail.com>
2018-11-27 22:19:15 +0100

    removed duplicate unit tests

--- tests/Get-DbaLogin.Tests.ps1
+++ tests/Get-DbaLogin.Tests.ps1
@@ -4,60 +4,11 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        $paramCount = 12
-        $defaultParamCount = 10
         [object[]]$params = (Get-ChildItem function:\Get-DbaLogin).Parameters.Keys
         $knownParameters = 'SqlInstance', 'SqlCredential', 'Login', 'IncludeFilter', 'ExcludeLogin', 'ExcludeFilter', 'ExcludeSystemLogin', 'Type', 'HasAccess', 'Locked', 'Disabled', 'EnableException'
+        $paramCount = $knownParameters.Count
         It "Should contain our specific parameters" {
             ( (Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count ) | Should Be $paramCount
-        }
-        It "Should only contain $paramCount parameters" {
-            $params.Count - $defaultParamCount | Should Be $paramCount
-        }
-    }
-}
-
-Describe "$CommandName Unit Tests" -Tag UnitTests, Get-DbaLogin {
-    Context "$Command Name Input" {
-        $Params = (Get-Command Get-DbaLogin).Parameters
-        It "Should have a mandatory parameter SQLInstance" {
-            $Params['SQLInstance'].Attributes.Mandatory | Should be $true
-        }
-        It "Should have a parameter SqlCredential" {
-            $Params['SqlCredential'].Count | Should Be 1
-        }
-        It "Should have a parameter Login" {
-            $Params['Login'].Count | Should Be 1
-        }
-        It "Should have a parameter IncludeFilter" {
-            $Params['IncludeFilter'].Count | Should Be 1
-        }
-        It "Should have a parameter ExcludeLogin" {
-            $Params['ExcludeLogin'].Count | Should Be 1
-        }
-        It "Should have a parameter ExcludeFilter" {
-            $Params['ExcludeFilter'].Count | Should Be 1
-        }
-        It "Should have a parameter ExcludeSystemLogin" {
-            $Params['ExcludeSystemLogin'].Count | Should Be 1
-        }
-        It "Should have a parameter Type" {
-            $Params['Type'].Count | Should Be 1
-        }
-        It "Should have a parameter HasAccess" {
-            $Params['HasAccess'].Count | Should Be 1
-        }
-        It "Should have a parameter Locked" {
-            $Params['Locked'].Count | Should Be 1
-        }
-        It "Should have a parameter Disabled" {
-            $Params['Disabled'].Count | Should Be 1
-        }
-        It "Should have a parameter EnableException" {
-            $Params['EnableException'].Count | Should Be 1
-        }
-        It "Should have a silent alias for parameter EnableException" {
-            $Params['EnableException'].Aliases | Should Be 'Silent'
         }
     }
 }
