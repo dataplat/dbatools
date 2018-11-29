5d6e1dfc40fee8fb9d345d9591750727006fb7ff
Joseph <joey.was@gmail.com>
2018-11-29 01:40:08 -0800

    Updated parameter context help for Anonymise

--- functions/Get-DbaBackupInformation.ps1
+++ functions/Get-DbaBackupInformation.ps1
@@ -42,7 +42,7 @@ function Get-DbaBackupInformation {
         If specified the provided path/directory will be traversed (only applies if not using XpDirTree)
 
     .PARAMETER Anonymise
-        If specified we will output the results with ComputerName, InstanceName, Database, UserName, and Paths hashed out
+        If specified we will output the results with ComputerName, InstanceName, Database, UserName, Paths, and Logical and Physical Names hashed out
         This options is mainly for use if we need you to submit details for fault finding to the dbatools team
 
     .PARAMETER ExportPath
