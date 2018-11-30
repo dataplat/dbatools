8cc4cac7d2a2754d605b45644f3f7a3bd02f4b97
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2018-11-30 03:38:49 -0600

    New function - Update-DbaInstance (#4687)
    * Update-DbaInstance - initial set of commands
    
    * Adding InputObject, ConfigurationName and Authentication
    
    * Adding parameter sets for more diverse usage
    
    * First round of hotfixes
    
    * Better messaging and minor fixes
    
    * Initialize-CredSSP initial version
    
    * Restart-WinRMService initial version
    
    * Hotfixes and initial test cases
    
    * Tests and multiple tweaks
    
    * Adding back local execution and permanent PS configuration name
    
    * Adding more verbosity to installation process
    
    * Fixing remoting and some logic/output stuff
    
    * New parameter - KB, help topics, tests fixes
    
    * Hotfix for KB paramset + no remote execution tests
    
    * Adding tests for KBs and *level versions
    
    * Updating CurrentVersion registry checks for latest SQL Server versions
    
    * More presice version detection
    
    * Fetching exact version now
    
    * Single space ftw
    
    * Only run remote checks when BinRoot is present
    
    * Hotfixing and improving version detection
    
    * Moving Unregister-RemoteSessionConfiguration to Invoke-Program
    
    * Creating distinct session for Configurations instead of closing old ones
    
    * OTSB formatting is failing me
    
    * New tests for each version and lots of mocks
    
    * SqlServerVersion to Version
    
    * SqlServerVersion to MajorVersion
    
    * RepositoryPath -> Path
    
    * Help topic updates
    
    * Adding ErrorAction Stop to catch uncatchable
    
    * Adding Test-ElevationRequirement
    
    * Registry on remote machines in now using proper credentials
    
    * Integration test
    
    * Removing -Latest from Update-DbaInstance
    
    * Removing ParameterSets
    
    * Adding error types
    
    * Accounting for a successful WhatIf
    
    * Formatting
    
    * setting x64 as a default arch now
    
    * removing existing sessions after restart
    
    * re-formatting tabs
    
    * Proper error handling in find-sqlserverupdate
    
    * Adding a dbatoolsConfig to control remoting failback preference
    
    * updated parameter syntax
    
    * Updating description with CredSSP recommendations
    
    * moved validation to begin
    
    updated a couple words, formatted

--- internal/configurations/settings/remoting.ps1
+++ internal/configurations/settings/remoting.ps1
@@ -3,3 +3,6 @@ Set-DbatoolsConfig -FullName 'PSRemoting.Sessions.ExpirationTimeout' -Value (New
 
 # Disables session caching
 Set-DbatoolsConfig -FullName 'PSRemoting.Sessions.Enable' -Value $true -Initialize -Validation bool -Handler { [Sqlcollaborative.Dbatools.Connection.ConnectionHost]::PSSessionCacheEnabled = $args[0] } -Description 'Globally enables session caching for PowerShell remoting'
+
+# Prefer CredSSP failback in Invoke-Program
+Set-DbatoolsConfig -FullName 'PSRemoting.Sessions.UsePSSessionConfiguration' -Value $false -Initialize -Validation bool -Description 'Skip CredSSP connection attempt and use a PSSessionConfiguration workaround when executing Invoke-Program'
