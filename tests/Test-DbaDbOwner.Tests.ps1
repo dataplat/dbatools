497dd6fe7a1401514015c43490e4e36b44b53ea6
Chrissy LeMaire <clemaire@gmail.com>
2018-12-05 17:51:33 +0100

    fixed test for real

--- tests/Test-DbaDbOwner.Tests.ps1
+++ tests/Test-DbaDbOwner.Tests.ps1
@@ -2,26 +2,6 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
-    BeforeAll {
-        Get-DbaProcess -SqlInstance $script:instance1 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-        $dbname = "dbatoolsci_testdbowner"
-        $server = Connect-DbaInstance -SqlInstance $script:instance1
-        $null = $server.Query("Create Database [$dbname]")
-    }
-    AfterAll {
-        Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
-    }
-
-    It "return the correct information including database, currentowner and targetowner" {
-        $whoami = whoami
-        $results = Test-DbaDbOwner -SqlInstance $script:instance1 -Database $dbname
-        $results.Database -eq $dbname
-        $results.CurrentOwner -eq $whoami
-        $results.TargetOwner -eq 'sa'
-    }
-}
-
 Describe "$CommandName Unit Tests" -Tag "UnitTests" {
     Context "Validate parameters" {
         <#
@@ -36,9 +16,6 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
         $knownParameters = 'InputObject', 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'TargetLogin', 'EnableException', 'Detailed'
         It "Should contain our specific parameters" {
             ((Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $knownParameters.Count
-        }
-        It "Should only contain $paramCount parameters" {
-            $params.Count - $defaultParamCount | Should Be $paramCount
         }
     }
     InModuleScope 'dbatools' {
@@ -63,7 +40,9 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
                     } #object
                 } #mock connect-sqlserver
 
-                { Test-DbaDbOwner -SqlInstance 'SQLServerName' } | Should Not throw
+                {
+                    Test-DbaDbOwner -SqlInstance 'SQLServerName'
+                } | Should Not throw
             } #It
             It -Skip "Should not return if no wrong owner for default" {
                 Mock Connect-SQLInstance -MockWith {
@@ -85,7 +64,9 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
                     } #object
                 } #mock connect-sqlserver
 
-                { Test-DbaDbOwner -SqlInstance 'SQLServerName' } | Should Not throw
+                {
+                    Test-DbaDbOwner -SqlInstance 'SQLServerName'
+                } | Should Not throw
             } #It
             It -Skip "Should return wrong owner information for one database with no owner specified" {
                 Mock Connect-SQLInstance -MockWith {
@@ -240,7 +221,8 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
                         ) #logins
                     } #object
                 } #mock connect-sqlserver
-                Mock Stop-Function { }
+                Mock Stop-Function {
+                }
 
                 $null = Test-DbaDbOwner -SqlInstance 'SQLServerName' -TargetLogin 'WrongLogin'
                 $assertMockParams = @{
@@ -292,3 +274,24 @@ Describe "$CommandName Unit Tests" -Tag "UnitTests" {
         } # Context
     } #modulescope
 } #describe
+
+
+Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        Get-DbaProcess -SqlInstance $script:instance1 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+        $dbname = "dbatoolsci_testdbowner"
+        $server = Connect-DbaInstance -SqlInstance $script:instance1
+        $null = $server.Query("Create Database [$dbname]")
+    }
+    AfterAll {
+        Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
+    }
+
+    It "return the correct information including database, currentowner and targetowner" {
+        $whoami = whoami
+        $results = Test-DbaDbOwner -SqlInstance $script:instance1 -Database $dbname
+        $results.Database -eq $dbname
+        $results.CurrentOwner -eq $whoami
+        $results.TargetOwner -eq 'sa'
+    }
+}
