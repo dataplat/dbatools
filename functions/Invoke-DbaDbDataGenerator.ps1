79577f712c63c67ec7fba4e4bf2061cfdf397b1b
Sander Stad <sander@sqlstad.nl>
2018-12-21 15:22:09 +0100

    Initial commit

---
+++ functions/Invoke-DbaDbDataGenerator.ps1
@@ -0,0 +1,193 @@
+function Invoke-DbaDbDataGenerator {
+    <#
+    .SYNOPSIS
+        Invoke-DbaDbDataGenerator generates random data for tables
+
+    .DESCRIPTION
+        Invoke-DbaDbDataMasking is able to generate random data for tables.
+
+    .PARAMETER SqlInstance
+        The target SQL Server instance or instances.
+
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+
+    .PARAMETER Database
+        Databases to process through
+
+    .PARAMETER Table
+        Tables to process. By default all the tables will be processed
+
+    .PARAMETER FilePath
+        Configuration file that contains the which tables and columns need to be masked
+
+    .PARAMETER Locale
+        Set the local to enable certain settings in the masking
+
+    .PARAMETER CharacterString
+        The characters to use in string data. 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' by default
+
+    .PARAMETER ExcludeTable
+        Exclude specific tables even if it's listed in the config file.
+
+    .PARAMETER ExcludeColumn
+        Exclude specific columns even if it's listed in the config file.
+
+    .PARAMETER MaxValue
+        Force a max length of strings instead of relying on datatype maxes. Note if a string datatype has a lower MaxValue, that will be used instead.
+
+        Useful for adhoc updates and testing, otherwise, the config file should be used.
+
+    .PARAMETER ExactLength
+        Mask string values to the same length. So 'Tate' will be replaced with 4 random characters.
+
+    .PARAMETER Force
+        Forcefully execute commands when needed
+
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: DataGeneration, Database
+        Author: Sander Stad (@sqlstad, sqlstad.nl)
+
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://dbatools.io/Invoke-DbaDbDataMasking
+
+    .EXAMPLE
+        Invoke-DbaDbDataMasking -SqlInstance SQLDB2 -Database DB1 -FilePath C:\Temp\sqldb1.db1.tables.json
+
+        Apply the data masking configuration from the file "sqldb1.db1.tables.json" to the db1 database on sqldb2. Prompt for confirmation for each table.
+
+    .EXAMPLE
+        Get-ChildItem -Path C:\Temp\sqldb1.db1.tables.json | Invoke-DbaDbDataMasking -SqlInstance SQLDB2 -Database DB1 -Confirm:$false
+
+        Apply the data masking configuration from the file "sqldb1.db1.tables.json" to the db1 database on sqldb2. Do not prompt for confirmation.
+
+    .EXAMPLE
+        New-DbaDbMaskingConfig -SqlInstance SQLDB1 -Database DB1 -Path C:\Temp\clone -OutVariable file
+        $file | Invoke-DbaDbDataMasking -SqlInstance SQLDB2 -Database DB1 -Confirm:$false
+
+        Create the data masking configuration file "sqldb1.db1.tables.json", then use it to mask the db1 database on sqldb2. Do not prompt for confirmation.
+
+    .EXAMPLE
+        Get-ChildItem -Path C:\Temp\sqldb1.db1.tables.json | Invoke-DbaDbDataMasking -SqlInstance SQLDB2, sqldb3 -Database DB1 -Confirm:$false
+
+        See what would happen if you the data masking configuration from the file "sqldb1.db1.tables.json" to the db1 database on sqldb2 and sqldb3. Do not prompt for confirmation.
+
+    #>
+    [CmdLetBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+    param (
+        [DbaInstanceParameter[]]$SqlInstance,
+        [PSCredential]$SqlCredential,
+        [string[]]$Database,
+        [parameter(Mandatory, ValueFromPipeline)]
+        [Alias('Path', 'FullName')]
+        [object]$FilePath,
+        [string]$Locale = 'en',
+        [string]$CharacterString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+        [string[]]$Table,
+        [string[]]$Column,
+        [string[]]$ExcludeTable,
+        [string[]]$ExcludeColumn,
+        [string]$Query,
+        [int]$MaxValue,
+        [switch]$ExactLength,
+        [switch]$EnableException
+    )
+
+    begin {
+        # Create the faker objects
+        Add-Type -Path (Resolve-Path -Path "$script:PSModuleRoot\bin\datamasking\Bogus.dll")
+        $faker = New-Object Bogus.Faker($Locale)
+    }
+
+    process {
+        if (Test-FunctionInterrupt) {
+            return
+        }
+
+        if ($FilePath.ToString().StartsWith('http')) {
+            $tables = Invoke-RestMethod -Uri $FilePath
+        } else {
+            # Check if the destination is accessible
+            if (-not (Test-Path -Path $FilePath)) {
+                Stop-Function -Message "Could not find masking config file $FilePath" -Target $FilePath
+                return
+            }
+
+            # Get all the items that should be processed
+            try {
+                $tables = Get-Content -Path $FilePath -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+            } catch {
+                Stop-Function -Message "Could not parse masking config file" -ErrorRecord $_ -Target $FilePath
+                return
+            }
+        }
+
+        foreach ($tabletest in $tables.Tables) {
+            if ($Table -and $tabletest.Name -notin $Table) {
+                continue
+            }
+            foreach ($columntest in $tabletest.Columns) {
+                if ($columntest.ColumnType -in 'hierarchyid', 'geography', 'xml', 'geometry' -and $columntest.Name -notin $Column) {
+                    Stop-Function -Message "$($columntest.ColumnType) is not supported, please remove the column $($columntest.Name) from the $($tabletest.Name) table" -Target $tables
+                }
+            }
+        }
+
+        if (Test-FunctionInterrupt) {
+            return
+        }
+
+        foreach ($instance in $SqlInstance) {
+            try {
+                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 9
+            } catch {
+                Stop-Function -Message "Failure" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+            }
+
+            if ($Database) {
+                $dbs = Get-DbaDatabase -SqlInstance $server -Database $Database
+            } else {
+                $dbs = Get-DbaDatabase -SqlInstance $server -Database $tables.Name
+            }
+
+            foreach ($db in $dbs) {
+                $stepcounter = 0
+                foreach ($tableobject in $tables.Tables) {
+                    if ($tableobject.Name -in $ExcludeTable -or ($Table -and $tableobject.Name -notin $Table)) {
+                        Write-Message -Level Verbose -Message "Skipping $($tableobject.Name) because it is explicitly excluded"
+                        continue
+                    }
+
+                    if ($tableobject.Name -notin $db.Tables.Name) {
+                        Stop-Function -Message "Table $($tableobject.Name) is not present in $db" -Target $db -Continue
+                    }
+                    try {
+                        if (-not (Test-Bound -ParameterName Query)) {
+                            $query = "SELECT * FROM [$($tableobject.Schema)].[$($tableobject.Name)]"
+                        }
+
+                        $data = $db.Query($query) | ConvertTo-DbaDataTable
+                    } catch {
+                        Stop-Function -Message "Failure retrieving the data from table $($tableobject.Name)" -Target $Database -ErrorRecord $_ -Continue
+                    }
+
+                    $elapsed = [System.Diagnostics.Stopwatch]::StartNew()
+                    $tablecolumns = $tableobject.Columns
+
+}
