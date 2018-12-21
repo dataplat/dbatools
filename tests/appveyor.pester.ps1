d43461382c16a585b7e888158f4e9128b3114f26
Kirill Kravtsov <kkravtsov@wawanesa.com>
2018-12-21 15:54:23 -0600

    Reducing the amount of logs extracted by only archiving messages

--- tests/appveyor.pester.ps1
+++ tests/appveyor.pester.ps1
@@ -334,7 +334,16 @@ if (-not $Finalize) {
         Update-AppveyorTest -Name $f.Name -Framework NUnit -FileName $f.FullName -Outcome Passed -Duration $PesterRun.Time.TotalMilliseconds
     }
     # Gather support package as an artifact
-    New-DbatoolsSupportPackage -Path $ModuleBase
+    # New-DbatoolsSupportPackage -Path $ModuleBase - turns out to be too heavy
+    try {
+        $msgFile = "$ModuleBase\dbatools_messages.xml"
+        Write-Host -ForegroundColor DarkGreen "Dumping message log into $msgFile"
+        Get-DbatoolsLog | Select-Object FunctionName, Level, TimeStamp, Message | Export-Clixml -Path $msgFile -ErrorAction Stop
+        Compress-Archive -Path $msgFile -DestinationPath "$msgFile.zip" -ErrorAction Stop
+        Remote-Item $msgFile
+    } catch {
+        Write-Host -ForegroundColor Red "Message collection failed: $($_.Exception.Message)"
+    }
 } else {
     # Unsure why we're uploading so I removed it for now
     <#
@@ -354,7 +363,7 @@ if (-not $Finalize) {
     #What failed? How many tests did we run ?
     $results = @(Get-ChildItem -Path "$ModuleBase\PesterResults*.xml" | Import-Clixml)
     #Publish the support package regardless of the outcome
-    Get-ChildItem $ModuleBase\dbatools_support_pack*.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+    Get-ChildItem $ModuleBase\dbatools_messages.xml.zip | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
     #$totalcount = $results | Select-Object -ExpandProperty TotalCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
     $failedcount = $results | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
     if ($failedcount -gt 0) {
