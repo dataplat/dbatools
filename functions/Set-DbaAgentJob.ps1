031b1e12e218b9c13ad4d78b716888ef491c6075
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2019-01-10 10:31:27 -0500

    Fixing Set-DbaAgentJob not being able to set some of the values to 0 (#4928)
    * null ne 0
    
    * Test-Bounding all the nulls

--- functions/Set-DbaAgentJob.ps1
+++ functions/Set-DbaAgentJob.ps1
@@ -379,12 +379,12 @@ function Set-DbaAgentJob {
                 }
             }
 
-            if ($EventLogLevel) {
+            if (Test-Bound -ParameterName EventLogLevel) {
                 Write-Message -Message "Setting job event log level to $EventlogLevel" -Level Verbose
                 $currentjob.EventLogLevel = $EventLogLevel
             }
 
-            if ($EmailLevel) {
+            if (Test-Bound -ParameterName EmailLevel) {
                 # Check if the notifiction needs to be removed
                 if ($EmailLevel -eq 0) {
                     # Remove the operator
@@ -403,7 +403,7 @@ function Set-DbaAgentJob {
                 }
             }
 
-            if ($NetsendLevel) {
+            if (Test-Bound -ParameterName NetsendLevel) {
                 # Check if the notifiction needs to be removed
                 if ($NetsendLevel -eq 0) {
                     # Remove the operator
@@ -422,7 +422,7 @@ function Set-DbaAgentJob {
                 }
             }
 
-            if ($PageLevel) {
+            if (Test-Bound -ParameterName PageLevel) {
                 # Check if the notifiction needs to be removed
                 if ($PageLevel -eq 0) {
                     # Remove the operator
@@ -472,7 +472,7 @@ function Set-DbaAgentJob {
                 }
             }
 
-            if ($DeleteLevel) {
+            if (Test-Bound -ParameterName DeleteLevel) {
                 Write-Message -Message "Setting job delete level to $DeleteLevel" -Level Verbose
                 $currentjob.DeleteLevel = $DeleteLevel
             }
