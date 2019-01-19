5b0549e32b745f3ab404b970fb5086378be68536
Chrissy LeMaire <clemaire@gmail.com>
2019-01-19 13:52:01 +0100

    Skip for now but leave in structure in the event params are added

--- tests/Measure-DbatoolsImport.Tests.ps1
+++ tests/Measure-DbatoolsImport.Tests.ps1
@@ -5,13 +5,14 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = ''
+        [object[]]$knownParameters = $null
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
-        It "Should only contain our specific parameters" {
+        It -Skip "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
+
 <#
     Integration test should appear below and are custom to the command you are writing.
     Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
