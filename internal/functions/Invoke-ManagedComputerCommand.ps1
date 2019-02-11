527648a78e374d10c6eeac0842462315de33d33f
pkotov87 <43175113+pkotov87@users.noreply.github.com>
2019-02-11 13:53:16 +0100

    Update Invoke-ManagedComputerCommand.ps1
    Find right library for $wmi

--- internal/functions/Invoke-ManagedComputerCommand.ps1
+++ internal/functions/Invoke-ManagedComputerCommand.ps1
@@ -48,11 +48,38 @@ function Invoke-ManagedComputerCommand {
 
     [scriptblock]$setupScriptBlock = {
         $ipaddr = $args[$args.GetUpperBound(0)]
-
-        # Just in case we go remote, ensure the assembly is loaded
-        [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
-        $wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $ipaddr
-        $null = $wmi.Initialize()
+        
+        # Just in case we go remote, ensure the assembly is loaded. Wenn SQL Server 2014 and 13.0.0.0__89845dcd8080cc91 -> 
+        #[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement') - Failed!!!
+	    # Find right library
+		$Count = -1
+		$i = 0
+		$t = Get-ChildItem "C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.SqlWmiManagement\*"|Sort Name -Descending
+		$t | Foreach{
+						$t1 = $Null;
+						$t1 = Get-ChildItem $_.FullName;
+						Start-Job -Name $i -ScriptBlock {
+									$ipaddr =  $args[1]
+									$test = Add-Type -Path  $args[0] -PassThru;
+									$m = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr;
+									$m.Initialize()
+							} -ArgumentList $t1.FullName,$ipaddr | Out-Null;
+						$job = Get-Job -Name $i |  Wait-Job
+						$CheckJob = Receive-Job -Job $job -Keep -ErrorAction SilentlyContinue
+						If($CheckJob -eq $true)
+						{
+							$Count = $i
+							Return
+						}
+						$i ++
+		}
+		Get-Job | Remove-Job
+		If($count -ge 0)
+		{
+			$test = Add-Type -Path (Get-ChildItem $t[$count].FullName).FullName -PassThru;
+			$wmi = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr ;
+			$null = $wmi.Initialize()
+		}
     }
 
     $prescriptblock = $setupScriptBlock.ToString()
