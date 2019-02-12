6486609092f2e34f26162cde527a98d3854f3ffb
pkotov87 <43175113+pkotov87@users.noreply.github.com>
2019-02-12 13:27:48 +0100

    Update Invoke-ManagedComputerCommand.ps1
    Just to checking.

--- internal/functions/Invoke-ManagedComputerCommand.ps1
+++ internal/functions/Invoke-ManagedComputerCommand.ps1
@@ -51,42 +51,48 @@ function Invoke-ManagedComputerCommand {
         
         # Just in case we go remote, ensure the assembly is loaded. Wenn SQL Server 2014 and 13.0.0.0__89845dcd8080cc91 -> 
         #[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement') - Failed!!!
-	    # Find right library
-		$Count = -1
-		$i = 0
-		$t = Get-ChildItem "C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.SqlWmiManagement\*"|Sort Name -Descending
-		$t | Foreach{
-						$t1 = $Null;
-						$t1 = Get-ChildItem $_.FullName;
-						$job = Start-Job -Name $i -ScriptBlock {
-									$ipaddr =  $args[1]
-									$test = Add-Type -Path  $args[0] -PassThru;
-									$m = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr;
-									$m.Initialize()
-							} -ArgumentList $t1.FullName,$ipaddr |  Wait-Job;
-						$CheckJob = Receive-Job -Job $job -Keep -ErrorAction SilentlyContinue
-						If($CheckJob -eq $true)
-						{
-							$Count = $i
-							$job | Remove-Job -Force
-							Return
-						}
-						$job | Remove-Job -Force
-						$i ++
-		}
-		Get-Job | Remove-Job
-		If($count -ge 0)
+	# Find right library
+	$Count = -1
+	$i = 0
+	[array]$t = Get-ChildItem "C:\Windows\assembly\GAC_MSIL\Microsoft.SqlServer.SqlWmiManagement" -Recurse -Filter "*.dll"|Sort FullName -Descending
+		
+	if($t.count -gt 0)
+	{
+		Foreach($a in $t)
 		{
-			$test = Add-Type -Path (Get-ChildItem $t[$count].FullName).FullName -PassThru;
-			$wmi = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr ;
-			$null = $wmi.Initialize()
+			$job = Start-Job -Name $i -ScriptBlock {
+				$ipaddr =  $args[1]
+				$test = Add-Type -Path  $args[0] -PassThru;
+				$m = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr;
+				$m.Initialize()
+				} -ArgumentList $a.FullName,$ipaddr |  Wait-Job;
+			$CheckJob = Receive-Job -Job $job -Keep -ErrorAction SilentlyContinue
+			if($CheckJob -eq $true)
+			{
+				$job | Remove-Job -Force
+				$Count = $i
+				Return
+			}
+			$job | Remove-Job -Force
+			$i ++
 		}
-		Else
+			
+		if($Count -ge 0)
+		{
+			$test = Add-Type -Path ($t[$count].FullName) -PassThru;				
+		}
+		else
 		{
 			[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
-        		$wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $ipaddr
-        		$null = $wmi.Initialize()
 		}
+	}
+	else
+	{
+		[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
+	}
+		
+	$wmi = New-Object ('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer') $ipaddr ;
+	$null = $wmi.Initialize()
     }
 
     $prescriptblock = $setupScriptBlock.ToString()
