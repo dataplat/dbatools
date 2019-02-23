bd8952ae9335023327a2caf4c8fb544164e91e62
Shawn Melton <wshawnmelton@gmail.com>
2019-02-23 12:03:56 -0600

    adding SmartTarget tag to those cmds using it (#5109)

--- functions/New-DbaXESmartCsvWriter.ps1
+++ functions/New-DbaXESmartCsvWriter.ps1
@@ -40,7 +40,7 @@ function New-DbaXESmartCsvWriter {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: ExtendedEvent, XE, XEvent
+        Tags: ExtendedEvent, XE, XEvent, SmartTarget
         Author: Chrissy LeMaire (@cl) | SmartTarget by Gianluca Sartori (@spaghettidba)
 
         Website: https://dbatools.io
