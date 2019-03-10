18ea1719fa190f2f0dbabeed9a89ecaef3d27be6
Chrissy LeMaire <clemaire@gmail.com>
2019-03-11 00:08:46 +0100

    fix formatting

--- internal/functions/cfirewall.ps1
+++ internal/functions/cfirewall.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Author  : Serge Nikalaichyk (https://www.linkedin.com/in/nikalaichyk)
 Repo    : https://github.com/SNikalaichyk/Firewall
 Version : 1.0.1
@@ -33,11 +33,11 @@ namespace FwRule
     }
 }
 '@
-    
+
     if (-not ('FwRule.Action' -as [Type])) {
         Add-Type -TypeDefinition $TypeDefinition
     }
-    
+
     <#
     .NOTES
         Each time you change a property of a rule, Windows Firewall commits the rule and verifies it for correctness.
@@ -46,7 +46,7 @@ namespace FwRule
     .LINK
         https://msdn.microsoft.com/en-us/library/windows/desktop/aa365344%28v=vs.85%29.aspx
     #>
-    
+
     if (-not (Get-Variable -Name FwRulePropertyData -Scope Script -ErrorAction Ignore)) {
         New-Variable -Name FwRulePropertyData -Force -Option ReadOnly -Scope Script -Value @(
             [PSCustomObject]@{
@@ -125,7 +125,7 @@ function Get-FirewallRule {
         [Parameter(Mandatory = $false)]
         [String]$Program,
         [Parameter(Mandatory = $false)]
-        [ValidateScript({($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
+        [ValidateScript( {($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
         [String]$Protocol,
         [Parameter(Mandatory = $false)]
         [String[]]$RemoteAddress,
@@ -136,93 +136,93 @@ function Get-FirewallRule {
     )
     begin {
         Initialize-FirewallRuleType
-        
+
         $FwPolicy = New-Object -ComObject HNetCfg.FwPolicy2 -ErrorAction Stop
     }
     process {
         if ($Protocol -in @('*', 'Any', 'All')) {
             $PSBoundParameters.Item('Protocol') = [String]$Protocol = '256'
         }
-        
-        $PSBoundParameters.Keys.Where({
+
+        $PSBoundParameters.Keys.Where( {
                 $_ -in $Script:FwRulePropertyData.ParameterName
             }) |
-        ForEach-Object -Begin {
-            
+            ForEach-Object -Begin {
+
             [String[]]$FilterConditions = @('($_ -ne $null)')
-            
+
         } -Process {
-            
+
             $ParameterName = $_
-            $PropertyData = $Script:FwRulePropertyData.Where({
+            $PropertyData = $Script:FwRulePropertyData.Where( {
                     $_.ParameterName -eq $ParameterName
                 })
-            
+
             if ($ParameterName -in @('LocalAddress', 'RemoteAddress')) {
                 $PSBoundParameters.Item($ParameterName) = (
                     $PSBoundParameters.Item($ParameterName) |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Resolve-FirewallRuleAddress |
-                    Sort-Object
+                        Resolve-FirewallRuleAddress |
+                        Sort-Object
                 ) -join ','
-                
+
                 $FilterConditions += '((($_."{0}" -split "," | Sort-Object) -join ",") -as [{2}]) -eq ($PSBoundParameters.Item("{1}") -as [{2}])' -f
                 $PropertyData.PropertyName,
                 $PropertyData.ParameterName,
                 $PropertyData.PropertyType
-                
+
                 return
             }
-            
+
             if ($ParameterName -in @('LocalPort', 'RemotePort')) {
                 $PSBoundParameters.Item($ParameterName) = (
                     $PSBoundParameters.Item($ParameterName) |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Sort-Object
+                        Sort-Object
                 ) -join ','
             }
-            
+
             if ($ParameterName -eq 'Program') {
                 $PSBoundParameters.Item($ParameterName) = [Environment]::ExpandEnvironmentVariables($PSBoundParameters.Item($ParameterName))
             }
-            
+
             $FilterConditions += '($_."{0}" -as [{2}]) -eq ($PSBoundParameters.Item("{1}") -as [{2}])' -f
             $PropertyData.PropertyName,
             $PropertyData.ParameterName,
             $PropertyData.PropertyType
-            
+
         } -End {
-            
+
             $FilterScript = [ScriptBlock]::Create(($FilterConditions -join ' -and '))
-            
+
         }
-        
+
         $FwPolicy.Rules |
-        Where-Object -FilterScript $FilterScript -PipelineVariable FwRule |
-        ForEach-Object {
-            
+            Where-Object -FilterScript $FilterScript -PipelineVariable FwRule |
+            ForEach-Object {
+
             $Script:FwRulePropertyData |
-            Select-Object -PipelineVariable PropertyData |
-            ForEach-Object -Begin {
-                
+                Select-Object -PipelineVariable PropertyData |
+                ForEach-Object -Begin {
+
                 $OutputObject = [PSCustomObject]@{
                 }
-                
+
             } -Process {
-                
+
                 $OutputObject | Add-Member -NotePropertyname $PropertyData.ParameterName `
-                                           -NotePropertyValue ($FwRule."$($PropertyData.PropertyName)" -as $PropertyData.PropertyType)
-                
+                    -NotePropertyValue ($FwRule."$($PropertyData.PropertyName)" -as $PropertyData.PropertyType)
+
             } -End {
-                
+
                 return $OutputObject
-                
+
             }
-            
+
         }
     }
 }
@@ -246,7 +246,7 @@ function New-FirewallRule {
         [ValidateSet('All', 'Domain', 'Private', 'Public')]
         [String[]]$Profile = 'All',
         [String]$Program = $null,
-        [ValidateScript({($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
+        [ValidateScript( {($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
         [String]$Protocol = 'TCP',
         [String[]]$RemoteAddress = $null,
         [String[]]$RemotePort = $null,
@@ -254,99 +254,99 @@ function New-FirewallRule {
     )
     begin {
         Initialize-FirewallRuleType
-        
+
         $FwPolicy = New-Object -ComObject HNetCfg.FwPolicy2 -ErrorAction Stop
     }
     process {
         if (-not $PSCmdlet.ShouldProcess($Name, 'Create Rule')) {
             return
         }
-        
+
         trap {
             Write-Error -Message $_.Exception.Message
-            
+
             return
         }
-        
-        $MyInvocation.MyCommand.Parameters.Keys.Where({
+
+        $MyInvocation.MyCommand.Parameters.Keys.Where( {
                 $_ -in $Script:FwRulePropertyData.ParameterName
             }) |
-        ForEach-Object -Begin {
-            
+            ForEach-Object -Begin {
+
             $Properties = @{
             }
-            
+
         } -Process {
-            
+
             $ParameterName = $_
             $ParameterValue = Get-Variable -Name $ParameterName -Scope Local -ValueOnly -ErrorAction Ignore
-            
+
             if (-not $ParameterValue -and -not $PSBoundParameters.ContainsKey($ParameterName)) {
                 return
             }
-            
+
             if ($ParameterName -in @('LocalAddress', 'RemoteAddress')) {
                 $ParameterValue = (
                     $ParameterValue |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Resolve-FirewallRuleAddress |
-                    Sort-Object
+                        Resolve-FirewallRuleAddress |
+                        Sort-Object
                 ) -join ','
             }
-            
+
             if ($ParameterName -in @('LocalPort', 'RemotePort')) {
                 $ParameterValue = (
                     $ParameterValue |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Sort-Object
+                        Sort-Object
                 ) -join ','
             }
-            
+
             if ($ParameterName -eq 'Program') {
                 $ParameterValue = [Environment]::ExpandEnvironmentVariables($ParameterValue);
             }
-            
+
             if ($ParameterName -eq 'Protocol' -and $ParameterValue -in @('*', 'Any', 'All')) {
                 $ParameterValue = '256'
             }
-            
-            $PropertyData = $Script:FwRulePropertyData.Where({
+
+            $PropertyData = $Script:FwRulePropertyData.Where( {
                     $_.ParameterName -eq $ParameterName
                 })
             $Properties.Add($PropertyData.PropertyName, $ParameterValue)
-            
+
         }
-        
-        $Script:FwRulePropertyData.Where({
+
+        $Script:FwRulePropertyData.Where( {
                 $_.PropertyName -in $Properties.Keys
             }) |
-        Sort-Object -Property Order |
-        ForEach-Object -Begin {
-            
+            Sort-Object -Property Order |
+            ForEach-Object -Begin {
+
             $FwRule = New-Object -ComObject HNetCfg.FwRule -ErrorAction Stop
-            
+
         } -Process {
-            
+
             $PropertyData = $_
-            
+
             try {
                 $FwRule."$($PropertyData.PropertyName)" = ($Properties.Item($PropertyData.PropertyName) -as $PropertyData.PropertyType)
             } catch {
                 throw "Failed to set property '$($PropertyData.PropertyName)': '$($_.Exception.Message)'."
             }
-            
+
         } -End {
-            
+
             try {
                 $FwPolicy.Rules.Add($FwRule)
             } catch {
                 throw "Failed to add rule '$Name': '$($_.Exception.Message)'."
             }
-            
+
         }
     }
 }
@@ -370,7 +370,7 @@ function Remove-FirewallRule {
         [ValidateSet('All', 'Domain', 'Private', 'Public')]
         [String[]]$Profile,
         [String]$Program,
-        [ValidateScript({($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
+        [ValidateScript( {($_ -in [System.Net.Sockets.ProtocolType].GetEnumNames()) -or ($_ -in @('*', 'Any', 'All'))})]
         [String]$Protocol,
         [String[]]$RemoteAddress,
         [String[]]$RemotePort,
@@ -378,111 +378,111 @@ function Remove-FirewallRule {
     )
     begin {
         Initialize-FirewallRuleType
-        
+
         $FwPolicy = New-Object -ComObject HNetCfg.FwPolicy2 -ErrorAction Stop
     }
     process {
         if (-not $PSCmdlet.ShouldProcess($Name, 'Remove Rule')) {
             return
         }
-        
+
         if ($Protocol -in @('*', 'Any', 'All')) {
             $PSBoundParameters.Item('Protocol') = [String]$Protocol = '256'
         }
-        
-        $PSBoundParameters.Keys.Where({
+
+        $PSBoundParameters.Keys.Where( {
                 $_ -in $Script:FwRulePropertyData.ParameterName
             }) |
-        ForEach-Object -Begin {
-            
+            ForEach-Object -Begin {
+
             [String[]]$FilterConditions = @()
-            
+
         } -Process {
-            
+
             $ParameterName = $_
-            $PropertyData = $Script:FwRulePropertyData.Where({
+            $PropertyData = $Script:FwRulePropertyData.Where( {
                     $_.ParameterName -eq $ParameterName
                 })
-            
+
             if ($ParameterName -in @('LocalAddress', 'RemoteAddress')) {
                 $PSBoundParameters.Item($ParameterName) = (
                     $PSBoundParameters.Item($ParameterName) |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Resolve-FirewallRuleAddress |
-                    Sort-Object
+                        Resolve-FirewallRuleAddress |
+                        Sort-Object
                 ) -join ','
-                
+
                 $FilterConditions += '((($_."{0}" -split "," | Sort-Object) -join ",") -as [{2}]) -eq ($PSBoundParameters.Item("{1}") -as [{2}])' -f
                 $PropertyData.PropertyName,
                 $PropertyData.ParameterName,
                 $PropertyData.PropertyType
-                
+
                 return
             }
-            
+
             if ($ParameterName -in @('LocalPort', 'RemotePort')) {
                 $PSBoundParameters.Item($ParameterName) = (
                     $PSBoundParameters.Item($ParameterName) |
-                    Where-Object {
+                        Where-Object {
                         [String]::IsNullOrEmpty($_) -eq $false
                     } |
-                    Sort-Object
+                        Sort-Object
                 ) -join ','
             }
-            
+
             if ($ParameterName -eq 'Program') {
                 $PSBoundParameters.Item($ParameterName) = [Environment]::ExpandEnvironmentVariables($PSBoundParameters.Item($ParameterName))
             }
-            
+
             $FilterConditions += '($_."{0}" -as [{2}]) -eq ($PSBoundParameters.Item("{1}") -as [{2}])' -f
             $PropertyData.PropertyName,
             $PropertyData.ParameterName,
             $PropertyData.PropertyType
-            
+
         } -End {
-            
+
             $FilterScript = [ScriptBlock]::Create(($FilterConditions -join ' -and '))
-            
+
         }
-        
+
         $MatchingRules = @($FwPolicy.Rules | Where-Object -FilterScript $FilterScript)
-        
-         <#
+
+        <#
         .NOTES
             Due to the 'INetFwRules::Remove' method limitations, rules can be removed only by name.
             Workaround is to rename all the matching rules with a random name before removing them.
         .LINKS
             https://msdn.microsoft.com/en-us/library/windows/desktop/aa365349%28v=vs.85%29.aspx
         #>
-        
+
         if ($MatchingRules.Count -eq 0) {
             Write-Verbose -Message "No matching firewall rules could be found."
         } else {
             $MatchingRules |
-            ForEach-Object -Begin {
-                
+                ForEach-Object -Begin {
+
                 Write-Verbose -Message "Removing all the matching firewall rules."
-                
+
                 [String[]]$RuleNamesToRemove = @()
-                
+
             } -Process {
-                
+
                 $RuleNamesToRemove += $NewRandomName = [Guid]::NewGuid().Guid
-                
+
                 Write-Verbose -Message "Renaming firewall rule '$($_.Name)' to '$NewRandomName'."
-                
+
                 $_.Enabled = $false
                 $_.Description = "Disabled and marked for removal: $(Get-Date -Format s)."
                 $_.Name = $NewRandomName
-                
+
             }
-            
+
             if ($RuleNamesToRemove.Count -ne 0) {
                 foreach ($RuleName in $RuleNamesToRemove) {
                     Write-Verbose -Message "Removing firewall rule '$RuleName'."
-                    
+
                     $FwPolicy.Rules.Remove($RuleName)
                 }
             }
@@ -502,7 +502,7 @@ function Resolve-FirewallRuleAddress {
     begin {
         $IPAddressPattern = '(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])'
         $NetworkPrefixPattern = '(\d{1}|[0-2]{1}\d{1}|3[0-2])'
-        
+
         function Convert-NetworkPrefix {
             <#
             .SYNOPSIS
@@ -518,17 +518,17 @@ function Resolve-FirewallRuleAddress {
             )
             process {
                 $Array = New-Object -TypeName 'System.Int32[]' -ArgumentList 32
-                
+
                 0 .. ($Prefix - 1) | ForEach-Object {
                     $Array.SetValue(1, $_)
                 }
-                
+
                 $SubnetMask = [System.Convert]::ToUInt64((-join $Array), 2) -as [IPAddress]
-                
+
                 Write-Output -InputObject $SubnetMask.IPAddressToString
             }
         }
-        
+
         function Test-IPAddressRange {
             <#
             .SYNOPSIS
@@ -546,15 +546,15 @@ function Resolve-FirewallRuleAddress {
             process {
                 $StartBytes = $Start.GetAddressBytes()
                 $EndBytes = $End.GetAddressBytes()
-                
+
                 [Array]::Reverse($StartBytes)
                 [Array]::Reverse($EndBytes)
-                
+
                 $StartInt = [BitConverter]::ToUInt32($StartBytes, 0)
                 $EndInt = [BitConverter]::ToUInt32($EndBytes, 0)
-                
+
                 $Result = $EndInt -gt $StartInt
-                
+
                 Write-Output -InputObject $Result
             }
         }
@@ -566,15 +566,15 @@ function Resolve-FirewallRuleAddress {
                 $SubnetMask = Convert-NetworkPrefix -Prefix $NetworkPrefix
                 $OutputString = $IPAddress.IPAddressToString, $SubnetMask -join '/'
             }
-            
+
             ('^{0}\/{0}$' -f $IPAddressPattern) {
                 [IPAddress]$IPAddress, [IPAddress]$SubnetMask = $InputString -split '/'
                 $OutputString = $IPAddress.IPAddressToString, $SubnetMask.IPAddressToString -join '/'
             }
-            
+
             ('^{0}\-{0}$' -f $IPAddressPattern) {
                 [IPAddress]$StartIPAddress, [IPAddress]$EndIPAddress = $InputString -split '-'
-                
+
                 if ($StartIPAddress.IPAddressToString -eq $EndIPAddress.IPAddressToString) {
                     $OutputString = $StartIPAddress.IPAddressToString, '255.255.255.255' -join '/'
                 } else {
@@ -585,23 +585,23 @@ function Resolve-FirewallRuleAddress {
                         $EndIPAddress.IPAddressToString,
                         $StartIPAddress.IPAddressToString,
                         $InputString |
-                        Write-Error
-                        
+                            Write-Error
+
                         return
                     }
                 }
             }
-            
+
             ('^{0}$' -f $IPAddressPattern) {
                 [IPAddress]$IPAddress = $InputString
                 $OutputString = $IPAddress.IPAddressToString, '255.255.255.255' -join '/'
             }
-            
+
             default {
                 $OutputString = $InputString
             }
         }
-        
+
         Write-Output -InputObject $OutputString
     }
 }
