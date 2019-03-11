78dffb186c48aece368bf6c44dc434658c295e18
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2019-03-11 03:44:03 +0100

    Fix debug and Path is null issues (#5183)
    * add debug by default and dbatools_enabledebug
    
    * remove missing files
    
    * discard leftovers
    
    * put dac back

--- dbatools.psm1
+++ dbatools.psm1
@@ -136,8 +136,8 @@ $script:multiFileImport = $false
 if ($dbatools_multiFileImport) { $script:multiFileImport = $true }
 if ($dbatoolsSystemSystemNode.MultiFileImport) { $script:multiFileImport = $true }
 if ($dbatoolsSystemUserNode.MultiFileImport) { $script:multiFileImport = $true }
-if (Test-Path -Path "$script:PSModuleRoot\.git") { $script:multiFileImport = $true }
-if (Test-Path -Path "$script:PSModuleRoot/.git") { $script:multiFileImport = $true }
+if ((Test-Path -Path "$script:PSModuleRoot\.git") -or $dbatools_enabledebug) { $script:multiFileImport = $true; $script:serialImport = $true }
+if ((Test-Path -Path "$script:PSModuleRoot/.git") -or $dbatools_enabledebug) { $script:multiFileImport = $true; $script:serialImport = $true }
 #endregion Multi File Import
 
 Write-ImportTime -Text "Validated defines"
