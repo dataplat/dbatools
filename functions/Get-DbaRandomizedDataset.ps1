dfc5e062189fdef70170ba8f53282bfe8277a228
sstad <sstad@waypointanalytical.com>
2019-03-15 10:43:45 +0100

    Added help Added comments

--- functions/Get-DbaRandomizedDataset.ps1
+++ functions/Get-DbaRandomizedDataset.ps1
@@ -1,4 +1,67 @@
 function Get-DbaRandomizedDataset {
+    <#
+    .SYNOPSIS
+        This function will generate a random data set based on a template
+
+    .DESCRIPTION
+        Generates a random value based on a template.
+        The templates standardized in the templates folder and can be used to generate a data set.
+        There is also an optiion to point to a specific template
+
+    .PARAMETER Template
+        The name of the template to use.
+        It will go through the default templates to see if it's present
+
+    .PARAMETER TemplateFile
+        File to use as a template
+
+    .PARAMETER RandomizerSubType
+        Subtype to use.
+
+    .PARAMETER Rows
+        Amount of rows to generate. Th default is 100.
+
+    .PARAMETER Locale
+        Set the local to enable certain settings in the masking. The default is 'en'
+
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: DataGeneration
+        Author: Sander Stad (@sqlstad, sqlstad.nl)
+
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://dbatools.io/Get-DbaRandomizedDataSet
+
+    .EXAMPLE
+        Get-DbaRandomizedDataset -Template Personaldata
+
+        Generate a data set based on the default template PersonalData.
+
+    .EXAMPLE
+        Get-DbaRandomizedDataset -Template Personaldata -Rows 10
+
+        Generate a data set based on the default template PersonalData with 10 rows
+
+    .EXAMPLE
+        Get-DbaRandomizedDataset -TemplateFile C:\Dataset\FinancialData.json
+
+        Generates data set based on a template file in another directory
+
+    #>
     [CmdLetBinding()]
     param(
         [string]$Template,
@@ -41,8 +104,7 @@ function Get-DbaRandomizedDataset {
             return
         }
 
-        $result = @()
-
+        # Generate the rows
         for ($i = 1; $i -le $Rows; $i++) {
             $row = New-Object PSCustomObject
 
@@ -53,10 +115,9 @@ function Get-DbaRandomizedDataset {
 
             }
 
-            $result += $row
+            $row
 
         }
 
-        $result
     }
 }
