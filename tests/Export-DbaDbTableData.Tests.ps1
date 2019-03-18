9a1ddb4229665804b3ff0e39f92b994473590ea5
Chrissy LeMaire <clemaire@gmail.com>
2019-03-18 15:06:15 +0100

    stash

--- tests/Export-DbaDbTableData.Tests.ps1
+++ tests/Export-DbaDbTableData.Tests.ps1
@@ -16,55 +16,31 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
         $db = Get-DbaDatabase -SqlInstance $script:instance1 -Database tempdb
-        $db2 = Get-DbaDatabase -SqlInstance $script:instance2 -Database tempdb
         $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example (id int);
             INSERT dbo.dbatoolsci_example
             SELECT top 10 1
             FROM sys.objects")
-        $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example2 (id int)")
-        $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example3 (id int)")
-        $null = $db.Query("CREATE TABLE dbo.dbatoolsci_example4 (id int);
-            INSERT dbo.dbatoolsci_example4
-            SELECT top 13 1
-            FROM sys.objects")
-        $null = $db2.Query("CREATE TABLE dbo.dbatoolsci_example (id int)")
-        $null = $db2.Query("CREATE TABLE dbo.dbatoolsci_example3 (id int)")
-        $null = $db2.Query("CREATE TABLE dbo.dbatoolsci_example4 (id int);
-            INSERT dbo.dbatoolsci_example4
-            SELECT top 13 2
-            FROM sys.objects")
+         $null = $db.Query("Select * into dbatoolsci_temp from sys.databases")
     }
     AfterAll {
         try {
             $null = $db.Query("DROP TABLE dbo.dbatoolsci_example")
-            $null = $db.Query("DROP TABLE dbo.dbatoolsci_example2")
-            $null = $db.Query("DROP TABLE dbo.dbatoolsci_example3")
-            $null = $db.Query("DROP TABLE dbo.dbatoolsci_example4")
-            $null = $db2.Query("DROP TABLE dbo.dbatoolsci_example3")
-            $null = $db2.Query("DROP TABLE dbo.dbatoolsci_example4")
-            $null = $db2.Query("DROP TABLE dbo.dbatoolsci_example")
-            $null = $db.Query("DROP TABLE tempdb.dbo.dbatoolsci_willexist")
+            $null = $db.Query("DROP TABLE dbo.dbatoolsci_temp")
         } catch {
             $null = 1
         }
     }
 
     It "exports the table data" {
-        $null = Copy-DbaDbTableData -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example -DestinationTable dbatoolsci_example2
-        $table1count = $db.Query("select id from dbo.dbatoolsci_example")
-        $table2count = $db.Query("select id from dbo.dbatoolsci_example2")
-        $table1count.Count | Should -Be $table2count.Count
-    }
-
-    It "supports piping" {
-        $null = Get-DbaDbTable -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example | Copy-DbaDbTableData -DestinationTable dbatoolsci_example2 -Truncate
-        $table1count = $db.Query("select id from dbo.dbatoolsci_example")
-        $table2count = $db.Query("select id from dbo.dbatoolsci_example2")
-        $table1count.Count | Should -Be $table2count.Count
+        $escaped = [regex]::escape('INSERT [dbo].[dbatoolsci_example] ([id]) VALUES (1)')
+        $results = Get-DbaDbTable -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example | Export-DbaDbTableData -Passthru
+        "$results" | Should -match $escaped
+        $results = Get-DbaDbTable -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example | Export-DbaDbTableData -Passthru
+        $results | Should -Match 'INSERT [dbo].[dbatoolsci_example] ([id]) VALUES (1)'
     }
 
     It "supports piping more than one table" {
-        $results = Get-DbaDbTable -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example2, dbatoolsci_example | Copy-DbaDbTableData -DestinationTable dbatoolsci_example3
+        $results = Get-DbaDbTable -SqlInstance $script:instance1 -Database tempdb -Table dbatoolsci_example2, dbatoolsci_example | Export-DbaDbTableData -DestinationTable dbatoolsci_example3
         $results.Count | Should -Be 2
     }
 }
