e1d590eaecc2d47f83882eab1059b516355b9495
Chrissy LeMaire <clemaire@gmail.com>
2019-03-19 12:43:40 +0100

    change things up

--- tests/Test-DbaLastBackup.Tests.ps1
+++ tests/Test-DbaLastBackup.Tests.ps1
@@ -40,15 +40,15 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     }
     Context "Setup restores and backups on the local drive for Test-DbaLastBackup" {
         Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbs | Backup-DbaDatabase -Type Database
-        $server.Query("INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample')")
+        Invoke-DbaQuery -SqlInstance $script:instance1 -Query "INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample')"
         Get-DbaDatabase -SqlInstance $script:instance1 -Database $testlastbackup | Backup-DbaDatabase -Type Differential
-        $server.Query("INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample1')")
+        Invoke-DbaQuery -SqlInstance $script:instance1 -Query "INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample1')"
         Get-DbaDatabase -SqlInstance $script:instance1 -Database $testlastbackup | Backup-DbaDatabase -Type Differential
-        $server.Query("INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample2')")
+        Invoke-DbaQuery -SqlInstance $script:instance1 -Query "INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample2')"
         Get-DbaDatabase -SqlInstance $script:instance1 -Database $testlastbackup | Backup-DbaDatabase -Type Log
-        $server.Query("INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample3')")
+        Invoke-DbaQuery -SqlInstance $script:instance1 -Query "INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample3')"
         Get-DbaDatabase -SqlInstance $script:instance1 -Database $testlastbackup | Backup-DbaDatabase -Type Log
-        $server.Query("INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample4')")
+        Invoke-DbaQuery -SqlInstance $script:instance1 -Query "INSERT INTO [$testlastbackup].[dbo].[Example] values ('sample4')"
     }
 
     Context "Test a single database" {
