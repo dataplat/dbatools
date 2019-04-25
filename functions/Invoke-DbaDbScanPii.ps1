92619895d7887738c2c16fbf444bea3d8d984e11
sanderstad <sanderstad@gmail.com>
2019-04-25 18:10:12 +0200

    Initial commit

---
+++ functions/Invoke-DbaDbScanPii.ps1
@@ -0,0 +1,177 @@
+function Invoke-DbaDbScanPii {
+    <#
+    .SYNOPSIS
+
+
+    .DESCRIPTION
+
+
+    .PARAMETER SqlInstance
+        The target SQL Server instance or instances.
+
+    .PARAMETER SqlCredential
+        Login to the target instance using alternative credentials. Windows and SQL Authentication supported. Accepts credential objects (Get-Credential)
+
+    .PARAMETER Database
+        Databases to process through
+
+    .PARAMETER Table
+        Tables to process. By default all the tables will be processed
+
+    .PARAMETER Column
+        Columns to process. By default all the columns will be processed
+
+    .PARAMETER Country
+        Filter out the patterns and known types for one or more countries
+
+    .PARAMETER CountryCode
+        Filter out the patterns and known types for one or more country code
+
+    .PARAMETER SampleCount
+        Amount of rows to sample to make an assessment. The default is 100
+
+    .PARAMETER Force
+        Forcefully execute commands when needed
+
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+        Tags: DataMasking, Database
+        Author: Sander Stad (@sqlstad, sqlstad.nl)
+
+        Website: https://dbatools.io
+        Copyright: (c) 2018 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://dbatools.io/Invoke-DbaDbScanPii
+
+    .EXAMPLE
+
+
+    #>
+    [CmdLetBinding()]
+    param (
+        [DbaInstanceParameter[]]$SqlInstance,
+        [PSCredential]$SqlCredential,
+        [string[]]$Database,
+        [string[]]$Table,
+        [string[]]$Column,
+        [string[]]$Country,
+        [string[]]$CountryCode,
+        [int]$SampleCount = 100,
+        [switch]$EnableException
+    )
+
+    begin {
+
+
+        try {
+            $patternFile = Resolve-Path -Path "$script:PSModuleRoot\bin\datamasking\pii-patterns.json"
+            $patterns = Get-Content -Path $patternFile -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+        } catch {
+            Stop-Function -Message "Couldn't parse pattern file" -ErrorRecord $_
+            return
+        }
+
+        try {
+            $knownTypesFile = Resolve-Path -Path "$script:PSModuleRoot\bin\datamasking\pii-knowntypes.json"
+            $knownTypes = Get-Content -Path $knownTypesFile -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+        } catch {
+            Stop-Function -Message "Couldn't parse known types file" -ErrorRecord $_
+            return
+        }
+
+        if (-not $SqlInstance) {
+            Stop-Function -Message "Please enter a SQL Server instance" -Category InvalidArgument
+        }
+
+        if (-not $Database) {
+            Stop-Function -Message "Please enter a database" -Category InvalidArgument
+        }
+
+        if ($Country.Count -ge 1) {
+            $patterns = $patterns | Where-Object Country -in $Country
+        }
+
+        if ($CountryCode.Count -ge 1) {
+            $patterns = $patterns | Where-Object CountryCode -in $CountryCode
+        }
+    }
+
+    process {
+        if (Test-FunctionInterrupt) {
+            return
+        }
+
+        $results = @()
+
+        foreach ($instance in $SqlInstance) {
+
+            try {
+                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential -MinimumVersion 9
+            } catch {
+                Stop-Function -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+            }
+
+            foreach ($dbName in $Database) {
+
+                $db = $server.Databases[$($dbName)]
+
+                if ($Table) {
+                    $tables = $db.Tables | Where-Object Name -in $Table
+                } else {
+                    $tables = $db.Tables
+                }
+
+                if ($Column) {
+                    $tables = $tables | Where-Object { $_.Columns.Name -in $Column }
+                }
+
+                foreach ($tableobject in $tables) {
+
+                    # Go through the first check to see if any column is found with a known type
+                    foreach ($knownType in $knownTypes) {
+
+                        foreach ($pattern in $knownTypes.Pattern) {
+
+                            $tableobject.Columns | Where-Object { $_.Name -match $pattern } | ForEach-Object {
+                                if ($_.Name -notin $results.Column) {
+                                    $results += [pscustomobject]@{
+                                        ComputerName   = $db.Parent.ComputerName
+                                        InstanceName   = $db.Parent.ServiceName
+                                        SqlInstance    = $db.Parent.DomainInstanceName
+                                        Database       = $dbName
+                                        Schema         = $tableobject.Schema
+                                        Table          = $tableobject.Name
+                                        Column         = $_.Name
+                                        "PII Name"     = $knownType.Name
+                                        "PII Category" = $knownType.Category
+                                    }
+                                }
+
+                            }
+
+                        }
+
+                    } # End for each known type
+
+                } # End for each table
+
+            } # End for each database
+
+        } # End for each instance
+
+        $results
+
+    } # End process
+}
