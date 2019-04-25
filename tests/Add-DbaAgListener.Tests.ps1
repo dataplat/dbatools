3ee3374a7ac1e68663461a810852a215ef98afa7
Brian Scholer <brian@briantist.com>
2019-04-25 13:54:46 -0400

    Fix tests, add back in changes missing from bad paste :(

--- tests/Add-DbaAgListener.Tests.ps1
+++ tests/Add-DbaAgListener.Tests.ps1
@@ -16,14 +16,18 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $agname = "dbatoolsci_ag_newlistener"
+        $listenerName = 'dbatoolsci_listener'
+        $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
+    }
+    AfterEach {
+        $null = $ag | Remove-DbaAgListener -Listener $listenerName
     }
     AfterAll {
         $null = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname -Confirm:$false
     }
     Context "creates a listener" {
         It "returns results with proper data" {
-            $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
-            $results = $ag | Add-DbaAgListener -IPAddress 127.0.20.1 -Confirm:$false
+            $results = $ag | Add-DbaAgListener -Name $listenerName -IPAddress 127.0.20.1 -Confirm:$false
             $results.PortNumber | Should -Be 1433
         }
     }
