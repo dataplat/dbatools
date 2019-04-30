8923af814cab18c6a48d37642cc21b798a55c854
sanderstad <sanderstad@gmail.com>
2019-04-30 10:39:18 +0200

    Adjusted test to recent changes

--- tests/Invoke-DbaDbPiiScan.Tests.ps1
+++ tests/Invoke-DbaDbPiiScan.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'Table', 'Column', 'Country', 'CountryCode', 'SampleCount', 'ExcludeTable', 'ExcludeColumn', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'Table', 'Column', 'Country', 'CountryCode', 'SampleCount', 'ExcludeTable', 'ExcludeColumn', 'KnownNamesFile', 'PatternsFile', 'ExcludeDefaultKnownNames', 'ExcludeDefaultPatterns', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
@@ -79,7 +79,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 
             $results.Count | Should -Be 7
 
-            $results."PII Name" | Should -Contain "Creditcard Discover"
+            $results."PII-Name" | Should -Contain "Creditcard Discover"
         }
 
     }
