f3e99ec9d098d316989c87f1480f98c71cddd1b1
jpomfret <jpomfret7@gmail.com>
2019-05-04 08:15:15 -0400

    adding some tests

--- tests/Set-DbaDbOwner.Tests.ps1
+++ tests/Set-DbaDbOwner.Tests.ps1
@@ -5,15 +5,61 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance','SqlCredential','Database','ExcludeDatabase','TargetLogin','EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'TargetLogin', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $svr = Connect-DbaInstance -SqlInstance $script:instance1
+        $owner = "dbatoolssci_owner_$(Get-Random)"
+        $ownertwo = "dbatoolssci_owner2_$(Get-Random)"
+        $null = New-DbaLogin -SqlInstance $script:instance1 -Login $owner -Password ('Password1234!' | ConvertTo-SecureString -asPlainText -Force)
+        $null = New-DbaLogin -SqlInstance $script:instance1 -Login $ownertwo -Password ('Password1234!' | ConvertTo-SecureString -asPlainText -Force)
+        $dbname = "dbatoolsci_$(Get-Random)"
+        $dbnametwo = "dbatoolsci_$(Get-Random)"
+        $null = New-DbaDatabase -SqlInstance $script:instance1 -Name $dbname -Owner sa
+        $null = New-DbaDatabase -SqlInstance $script:instance1 -Name $dbnametwo -Owner sa
+    }
+    AfterAll {
+        $null = Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname, $dbnametwo -confirm:$false
+        $null = Remove-DbaLogin -SqlInstance $script:instance1 -Login $owner, $ownertwo -confirm:$false
+    }
+    Context "Should set the database owner" {
+        It "Sets the database owner on a specific database" {
+            $results = Set-DbaDbOwner -SqlInstance $script:instance1 -Database $dbName -TargetLogin $owner
+            $results.Owner | Should Be $owner
+        }
+        It "Check it actually set the owner" {
+            $svr.Databases[$dbname].refresh()
+            $svr.Databases[$dbname].Owner | Should Be $owner
+        }
+    }
+
+    Context "Sets multiple database owners" {
+        $results = Set-DbaDbOwner -SqlInstance $script:instance1 -Database $dbName, $dbnametwo -TargetLogin $ownertwo
+        It "Sets the database owner on multiple databases" {
+            foreach ($r in $results) {
+                $r.owner | Should Be $ownertwo
+            }
+        }
+        It "Set 2 database owners" {
+            $results.Count | Should Be 2
+        }
+    }
+
+    Context "Excludes databases" {
+        $svr.Databases[$dbname].refresh()
+        $results = Set-DbaDbOwner -SqlInstance $script:instance1 -ExcludeDatabase $dbnametwo -TargetLogin $owner
+        It "Excludes specified database" {
+            $results.Database | Should Not Contain $dbnametwo
+        }
+        It "Updates at least one database" {
+            @($results).Count | Should BeGreaterOrEqual 1
+        }
+    }
+}
