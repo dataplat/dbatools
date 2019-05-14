effee2b08d870b906732635af981bff56974fe8d
Patrick Flynn <sqllensman@outlook.com>
2019-05-14 18:42:33 +1000

    Changes to resolve Issue 5443

--- functions/Export-DbaCredential.ps1
+++ functions/Export-DbaCredential.ps1
@@ -29,9 +29,6 @@ function Export-DbaCredential {
     .PARAMETER ExcludePassword
         Exports the SQL credential without any sensitive information.
 
-    .PARAMETER InputObject
-        Allow credentials to be piped in from Get-DbaCredential
-
     .PARAMETER Append
         Append to Path
 
@@ -56,7 +53,6 @@ function Export-DbaCredential {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, ValueFromPipeline)]
         [Alias("ServerInstance", "SqlServer")]
         [DbaInstanceParameter[]]$SqlInstance,
         [string[]]$Identity,
@@ -65,59 +61,121 @@ function Export-DbaCredential {
         [string]$Path,
         [switch]$ExcludePassword,
         [switch]$Append,
+        [Parameter(ValueFromPipeline)]
         [Microsoft.SqlServer.Management.Smo.Credential[]]$InputObject,
         [switch]$EnableException
     )
+    begin {
+        $serverArray = @()
+        $credentialArray = @{}
+        $credentialCollection = New-Object System.Collections.ArrayList
+    }
     process {
-        foreach ($instance in $SqlInstance) {
-            try {
-                $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential -MinimumVersion 9
-                $InputObject += $server.Credentials
-            } catch {
-                Stop-Function -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
-            }
+        if (-not $InputObject -and -not $SqlInstance) {
+            Stop-Function -Message "You must pipe in a Credential or specify a SqlInstance"
+            return
+        }
 
-            if ($Identity) {
-                $InputObject = $InputObject | Where-Object Identity -in $Identity
-            }
-
-            if (!(Test-SqlSa -SqlInstance $instance -SqlCredential $sqlcredential)) {
-                Stop-Function -Message "Not a sysadmin on $instance. Quitting." -Target $instance -Continue
-            }
-
-            Write-Message -Level Verbose -Message "Getting NetBios name for $instance."
-            $sourceNetBios = Resolve-NetBiosName $server
-
-            Write-Message -Level Verbose -Message "Checking if Remote Registry is enabled on $instance."
-            try {
-                Invoke-Command2 -Raw -Credential $Credential -ComputerName $sourceNetBios -ScriptBlock { Get-ItemProperty -Path "HKLM:\SOFTWARE\" } -ErrorAction Stop
-            } catch {
-                Stop-Function -Message "Can't connect to registry on $instance." -Target $sourceNetBios -ErrorRecord $_
-                return
-            }
-
-            if (-not (Test-Bound -ParameterName Path)) {
-                $timenow = (Get-Date -uformat "%m%d%Y%H%M%S")
-                $mydocs = [Environment]::GetFolderPath('MyDocuments')
-                $path = "$mydocs\$($server.name.replace('\', '$'))-$timenow-credential.sql"
-            }
-
-            $sql = @()
-
-            if ($ExcludePassword) {
-                Stop-Function -Message "So sorry, there's no other way around it for now. The password has to be exported in plain text."
-                return
-            } else {
+        if (Test-Bound -ParameterName SqlInstance) {
+            foreach ($instance in $SqlInstance) {
                 try {
-                    $creds = Get-DecryptedObject -SqlInstance $server -Type Credential
+                    $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $sqlcredential -MinimumVersion 9
+                    
+                    $serverCreds = $server.Credentials
+                    if (Test-Bound -ParameterName Identity) {
+                        $serverCreds = $serverCreds | Where-Object Identity -in $Identity
+                    }
+
+                    $InputObject += $serverCreds
+                } catch {
+                    Stop-Function -Message "Error occurred while establishing connection to $instance" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
+                }
+            }
+        } 
+
+        foreach ($input in $InputObject) {
+            $server = $input.Parent
+            $instance = $server.Name
+
+            if ($serverArray -notcontains $instance) {
+                try {
+                    if ($ExcludePassword) {
+                        $serverCreds = $server.Credentials
+                        $creds = New-Object System.Collections.ArrayList
+
+                        foreach ($cred in $server.Credentials) {
+                            $credObject = [PSCustomObject]@{
+                                Name        = '[' + $cred.name + ']'
+                                Identity    = $cred.Id.ToString()
+                                Password    = ''
+                            }
+                            $creds.Add($credObject) | Out-Null
+                        }
+                        $creds | Add-Member -MemberType NoteProperty -Name 'SqlInstance' -Value $instance
+                        $creds | Add-Member -MemberType NoteProperty -Name 'ExcludePassword' -Value $ExcludePassword
+                        $credentialCollection.Add($credObject) | Out-Null
+                    } else {
+                        if (!(Test-SqlSa -SqlInstance $server)) {
+                            Stop-Function -Message "Not a sysadmin on $instance. Quitting." -Target $instance -Continue
+                        }
+        
+                        Write-Message -Level Verbose -Message "Getting NetBios name for $instance."
+                        $sourceNetBios = Resolve-NetBiosName $server
+        
+                        Write-Message -Level Verbose -Message "Checking if Remote Registry is enabled on $instance."
+                        try {
+                            Invoke-Command2 -Raw -Credential $Credential -ComputerName $sourceNetBios -ScriptBlock { Get-ItemProperty -Path "HKLM:\SOFTWARE\" } -ErrorAction Stop
+                        } catch {
+                            Stop-Function -Message "Can't connect to registry on $instance." -Target $sourceNetBios -ErrorRecord $_
+                            return
+                        }
+                        
+                        $creds = Get-DecryptedObject -SqlInstance $server -Type Credential
+                        Write-Message -Level Verbose -Message "Adding Members"
+                        $creds | Add-Member -MemberType NoteProperty -Name 'SqlInstance' -Value $instance
+                        $creds | Add-Member -MemberType NoteProperty -Name 'ExcludePassword' -Value $ExcludePassword
+                        $credentialCollection.Add($creds) | Out-Null
+                    }
                 } catch {
                     Stop-Function -Continue -Message "Failure" -ErrorRecord $_
                 }
-                foreach ($currentCred in $creds) {
+
+                $serverArray += $instance
+
+                $key = $input.Parent.Name + '::[' + $input.Name + ']' 
+                $credentialArray.add( $key, $true )
+            } else {
+                $key = $input.Parent.Name + '::[' + $input.Name + ']' 
+                $credentialArray.add( $key, $true )
+            }
+        }
+    }
+
+    end {
+        $sql = @()
+        foreach ($cred in $credentialCollection) {
+            Write-Message -Level Verbose -Message "Credentials in object = $($cred.Count)"
+            if (-not (Test-Bound -ParameterName Path)) {
+                $time = (Get-Date -Format yyyMMddHHmmss)
+                $mydocs = [Environment]::GetFolderPath('MyDocuments')
+                $serverName = $($cred[0].SqlInstance.replace('\', '$'))
+
+                $path = Join-DbaPath -Path $mydocs "$serverName-$time-credential.sql"
+            }
+
+            foreach ($currentCred in $creds) {
+                $key = $currentCred.SqlInstance + '::' + $currentCred.Name
+                if( $credentialArray.ContainsKey($key) ){
                     $name = $currentCred.Name.Replace("'", "''")
                     $identity = $currentCred.Identity.Replace("'", "''")
-                    $password = $currentCred.Password.Replace("'", "''")
-                    $sql += "CREATE CREDENTIAL $name WITH IDENTITY = N'$identity', SECRET = N'$password'"
+                    if ($currentCred.ExcludePassword) {
+                        $sql += "CREATE CREDENTIAL $name WITH IDENTITY = N'$identity', SECRET = N'<EnterStrongPasswordHere>'" 
+                    } else {
+                        $password = $currentCred.Password.Replace("'", "''")
+                        $sql += "CREATE CREDENTIAL $name WITH IDENTITY = N'$identity', SECRET = N'$password'"
+                    }
+
+                    Write-Message -Level Verbose -Message "Created Script for $name"
                 }
             }
 
@@ -127,14 +185,17 @@ function Export-DbaCredential {
                 } else {
                     Set-Content -Path $path -Value $sql
                 }
-                Get-ChildItem -Path $path
             } catch {
                 Stop-Function -Message "Can't write to $path" -ErrorRecord $_ -Continue
             }
 
-
-            Write-Message -Level Verbose -Message "Attempting to migrate $credentialName"
-            Get-ChildItem -Path $path
+            Write-Message -Level Verbose -Message "Credentials exported to $path"
         }
     }
+
+
+
+
+
+
 }
