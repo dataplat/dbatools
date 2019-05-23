5fc70722d376feccdaa4efa028c4b8a541d99454
Cláudio Silva <claudiosil100@gmail.com>
2019-05-23 22:59:45 +0100

    Integration tests for New-DbaDbUser (do New-DbaDbUser)

--- tests/New-DbaDbUser.Tests.ps1
+++ tests/New-DbaDbUser.Tests.ps1
@@ -5,15 +5,47 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance','SqlCredential','Database','ExcludeDatabase','IncludeSystem','Login','Username','Force','EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'IncludeSystem', 'Login', 'Username', 'Force', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $dbname = "dbatoolscidb_$(Get-Random)"
+        $userName = "dbattolscidb_User1"
+        $userNameWithoutLogin = "dbattolscidb_UserWithoutLogin"
+        $null = New-DbaDatabase -SqlInstance $script:instance1 -Name $dbname
+
+        $password = 'MyV3ry$ecur3P@ssw0rd'
+        $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+        $null = New-DbaLogin -SqlInstance $script:instance1 -Login $userName -Password $securePassword
+    }
+    AfterAll {
+        $null = Invoke-DbaQuery -SqlInstance $script:instance1 -Database $dbname -Query "drop table $tablename"
+        $null = Remove-DbaDatabase -SqlInstance $script:instance1 -Database $dbname -Confirm:$false
+    }
+    Context "Should create the user with login" {
+        It "Creates the user" {
+            (New-DbaDbUser -SqlInstance $script:instance1 -Database $dbname -Login $userName).Name | Should Be $userName
+        }
+        It "Really created it" {
+            (Get-DbaDbUser -SqlInstance $script:instance1 -Database $dbname).Name | Should Be $userName
+        }
+    }
+    Context "Should create the user without login" {
+        It "Creates the user" {
+            (New-DbaDbUser -SqlInstance $script:instance1 -Database $dbname -User $userNameWithoutLogin).Name | Should Be $userNameWithoutLogin
+        }
+        $results = Get-DbaDbUser -SqlInstance $script:instance1 -Database $dbname | Where-Object Name -eq $userNameWithoutLogin
+        It "Really created it" {
+            $results.Name | Should Be $userNameWithoutLogin
+        }
+        It "Don't have Login property" {
+            $results.Login | Should Be ""
+        }
+    }
+}
