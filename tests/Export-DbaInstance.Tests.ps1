e8c0bc5e8fa825b0aba0230c0d225f45ea7593ad
Joshua Corrick <joshua.corrick@live.com>
2019-05-25 01:06:59 -0400

    Add Intigration Test

--- tests/Export-DbaInstance.Tests.ps1
+++ tests/Export-DbaInstance.Tests.ps1
@@ -5,15 +5,26 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance','SqlCredential','Credential','Path','NoRecovery','IncludeDbMasterKey','Exclude','BatchSeparator','ScriptingOption','EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Credential', 'Path', 'NoRecovery', 'IncludeDbMasterKey', 'Exclude', 'BatchSeparator', 'ScriptingOption', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+    Context "Should Export all items from an instance" {
+        $results = Export-DbaInstance -SqlInstance $script:instance2
+        $null = Remove-Item -Path $results.Directory[0] -Force -Recurse
+        It "Should execute with default settings" {
+            $results | Should Not Be Null
+        }
+    }
+    Context "Should exclude some items from an Export" {
+        $results = Export-DbaInstance -SqlInstance $script:instance2 -Exclude Databases, Logins, SysDbUserObjects
+        $null = Remove-Item -Path $results.Directory[0] -Force -Recurse
+        It "Should execute with parameters excluding objects" {
+            $results | Should Not Be Null
+        }
+    }
+}
