56a534bdbeec2422ebba8a0dcaa89fe17071ff1e
Joshua Corrick <joshua.corrick@live.com>
2019-05-25 08:49:07 -0400

    Change results to remove

--- tests/Export-DbaInstance.Tests.ps1
+++ tests/Export-DbaInstance.Tests.ps1
@@ -15,14 +15,14 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     Context "Should Export all items from an instance" {
         $results = Export-DbaInstance -SqlInstance $script:instance2
-        $null = Remove-Item -Path $results.Directory[0] -Force -Recurse
+        #$null = Remove-Item -Path $results -Force -Recurse
         It "Should execute with default settings" {
             $results | Should Not Be Null
         }
     }
     Context "Should exclude some items from an Export" {
         $results = Export-DbaInstance -SqlInstance $script:instance2 -Exclude Databases, Logins, SysDbUserObjects
-        $null = Remove-Item -Path $results.Directory[0] -Force -Recurse
+        #$null = Remove-Item -Path $results -Force -Recurse
         It "Should execute with parameters excluding objects" {
             $results | Should Not Be Null
         }
