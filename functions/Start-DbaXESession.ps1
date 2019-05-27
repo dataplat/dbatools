458e677f93dc61df3b43b4d30aa4b40c16a22179
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2019-05-27 17:01:45 +0000

    Extend StopAt on Start-DbaXeSession fixes #5362 (#5616)
    * maybefix
    
    * fix

--- functions/Start-DbaXESession.ps1
+++ functions/Start-DbaXESession.ps1
@@ -126,10 +126,10 @@ function Start-DbaXESession {
                 $name = "XE Session Stop - $session"
                 if ($Pscmdlet.ShouldProcess("$Server", "Making New XEvent StopJob for $session")) {
                     # Setup the schedule time
-                    $time = $(($StopAt).ToString("HHmmss"))
+                    #$time = $(($StopAt).ToString("HHmmss"))
 
                     # Create the schedule
-                    $schedule = New-DbaAgentSchedule -SqlInstance $server -Schedule $name -FrequencyType Once -StartTime $time -Force
+                    $schedule = New-DbaAgentSchedule -SqlInstance $server -Schedule $name -FrequencyType Once -StartDate $StopAt
 
                     # Create the job and attach the schedule
                     $job = New-DbaAgentJob -SqlInstance $server -Job $name -Schedule $schedule -DeleteLevel Always -Force
