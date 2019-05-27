a75e1c6b7ff608f8aea731e73286ea74b4a8a3d6
Shane O'Neill <shaneoneillis@gmail.com>
2019-05-27 16:59:49 +0100

    Errors when database is not present/incorrect. (#5613)
    * Error out if databases not present.
    
    * should error out when database is incorrect.

--- tests/New-DbaDbUser.Tests.ps1
+++ tests/New-DbaDbUser.Tests.ps1
@@ -4,11 +4,23 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'IncludeSystem', 'Login', 'Username', 'Force', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+        }
+    }
+    Context "Should error out if the database does not exist" {
+        Mock -CommandName Connect-SqlInstance {
+            [PSCustomObject]@{
+                Databases    = [String]::Empty
+                IsAccessible = $false
+            }
+        }
+
+        It -Skip "errors out when the databases does not exist and -EnableException is specified" {
+            { New-DbaDbUser -SqlInstance localhost -Database 'NotAtAllReal' -Username $userName -EnableException } | Should -Throw
         }
     }
 }
