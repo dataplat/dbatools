c6e4cbc1eb637428776fd08eab2f5dc02d97e07e
Cláudio Silva <claudiosil100@gmail.com>
2019-05-28 00:41:25 +0100

    remove debug (-Verbose) (do Remove-DbaDatabaseSafely)

--- tests/Remove-DbaDatabaseSafely.Tests.ps1
+++ tests/Remove-DbaDatabaseSafely.Tests.ps1
@@ -27,7 +27,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $null = Remove-DbaAgentJob -Confirm:$false -SqlInstance $script:instance2 -Job 'Rationalised Database Restore Script for dbatoolsci_safely'
     }
     Context "Command actually works" {
-        $results = Remove-DbaDatabaseSafely -SqlInstance $script:instance2 -Database $db1 -BackupFolder C:\temp -NoDbccCheckDb -Verbose
+        $results = Remove-DbaDatabaseSafely -SqlInstance $script:instance2 -Database $db1 -BackupFolder C:\temp -NoDbccCheckDb
         It "Should have database name of $db1" {
             foreach ($result in $results) {
                 $result.DatabaseName | Should Be $db1
