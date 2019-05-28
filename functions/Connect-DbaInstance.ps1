035be177144884c124de1fc14e27653384bfb91a
Chrissy LeMaire <clemaire@gmail.com>
2019-05-28 21:54:56 +0200

    Caused an error

--- functions/Connect-DbaInstance.ps1
+++ functions/Connect-DbaInstance.ps1
@@ -506,7 +506,7 @@ function Connect-DbaInstance {
                     Add-Member -InputObject $server -NotePropertyName DbaInstanceName -NotePropertyValue $instance.InstanceName -Force
                     Add-Member -InputObject $server -NotePropertyName NetPort -NotePropertyValue $instance.Port -Force
                     # Azure has a really hard time with $server.Databases, which we rely on heavily. Fix that.
-                    $currentdb = $server.Databases | Where Name -eq $Database | Select-Object First 1
+                    $currentdb = $server.Databases | Where-Object Name -eq $Database | Select-Object -First 1
                     Add-Member -InputObject $server -NotePropertyName Databases -NotePropertyValue @{ $currentdb.Name = $currentdb } -Force -Passthru
                     continue
                 } catch {
