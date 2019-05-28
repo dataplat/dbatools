9a23672b90442e9fe14d142876432fc2a15002c9
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2019-05-28 18:24:40 +0000

    Force $server.Databases for AzureSql - fixes #5621 (#5625)
    * fixes #5621
    
    * cleaner

--- functions/Connect-DbaInstance.ps1
+++ functions/Connect-DbaInstance.ps1
@@ -504,7 +504,10 @@ function Connect-DbaInstance {
                     Add-Member -InputObject $server -NotePropertyName IsAzure -NotePropertyValue $true -Force
                     Add-Member -InputObject $server -NotePropertyName ComputerName -NotePropertyValue $instance.ComputerName -Force
                     Add-Member -InputObject $server -NotePropertyName DbaInstanceName -NotePropertyValue $instance.InstanceName -Force
-                    Add-Member -InputObject $server -NotePropertyName NetPort -NotePropertyValue $instance.Port -Force -Passthru
+                    Add-Member -InputObject $server -NotePropertyName NetPort -NotePropertyValue $instance.Port -Force
+                    # Azure has a really hard time with $server.Databases, which we rely on heavily. Fix that.
+                    $currentdb = $server.Databases | Where Name -eq $Database | Select-Object First 1
+                    Add-Member -InputObject $server -NotePropertyName Databases -NotePropertyValue @{ $currentdb.Name = $currentdb } -Force -Passthru
                     continue
                 } catch {
                     Stop-Function -Message "Failure" -ErrorRecord $_ -Continue
