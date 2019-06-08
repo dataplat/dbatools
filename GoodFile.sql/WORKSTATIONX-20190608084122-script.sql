88b6b63874132bab48fda4134c0875e263fa582c
Chrissy LeMaire <clemaire@gmail.com>
2019-06-08 08:43:27 +0200

    might fix #5679

---
+++ GoodFile.sql/WORKSTATIONX-20190608084122-script.sql
@@ -0,0 +1,4 @@
+﻿/*
+	Created by BASE\ctrlb using dbatools Export-DbaScript for objects on WORKSTATIONX at 06/08/2019 08:41:22
+	See https://dbatools.io/Export-DbaScript for more information
+*/
--- functions/Export-DbaScript.ps1
+++ functions/Export-DbaScript.ps1
@@ -232,48 +232,54 @@ function Export-DbaScript {
                         if ($ScriptingOptionsObject) {
                             foreach ($script in $scripter.EnumScript($object)) {
                                 if ($BatchSeparator -ne "") {
-                                    $script = "$script`r`n$BatchSeparator`r`n"
+                                    $scriptpart = "$script`r`n$BatchSeparator`r`n"
                                 }
-                                $script | Out-String
+                                $scriptpart | Out-String
                             }
                         } else {
                             if (Get-Member -Name ScriptCreate -InputObject $object) {
-                                $script = $object.ScriptCreate().GetScript()
+                                $scriptpart = $object.ScriptCreate().GetScript()
                             } else {
-                                $script = $object.Script()
+                                $scriptpart = $object.Script()
                             }
 
                             if ($BatchSeparator -ne "") {
-                                $script = "$script`r`n$BatchSeparator`r`n"
+                                $scriptpart = "$script`r`n$BatchSeparator`r`n"
                             }
-                            $script  | Out-String
+                            $scriptpart  | Out-String
                         }
                     } else {
                         if ($ScriptingOptionsObject) {
                             if ($ScriptingOptionsObject.ScriptBatchTerminator) {
-                                $ScriptingOptionsObject.AppendToFile = $true
-                                $ScriptingOptionsObject.ToFileOnly = $true
-                                $ScriptingOptionsObject.FileName = $FilePath
+                                if (-not $ScriptingOptionsObject.AppendToFile) {
+                                    $ScriptingOptionsObject.AppendToFile = $true
+                                }
+                                if (-not $ScriptingOptionsObject.ToFileOnly) {
+                                    $ScriptingOptionsObject.ToFileOnly = $true
+                                }
+                                if (-not $ScriptingOptionsObject.FileName) {
+                                    $ScriptingOptionsObject.FileName = $FilePath
+                                }
                                 $object.Script($ScriptingOptionsObject)
                             } else {
                                 foreach ($script in $scripter.EnumScript($object)) {
                                     if ($BatchSeparator -ne "") {
-                                        $script = "$script`r`n$BatchSeparator`r`n"
+                                        $scriptpart = "$script`r`n$BatchSeparator`r`n"
                                     }
-                                    $script | Out-File -FilePath $FilePath -Encoding $encoding -Append
+                                    $scriptpart | Out-File -FilePath $FilePath -Encoding $encoding -Append
                                 }
                             }
 
                         } else {
                             if (Get-Member -Name ScriptCreate -InputObject $object) {
-                                $script = $object.ScriptCreate().GetScript()
+                                $scriptpart = $object.ScriptCreate().GetScript()
                             } else {
-                                $script = $object.Script()
+                                $scriptpart = $object.Script()
                             }
                             if ($BatchSeparator -ne "") {
-                                $script = "$script`r`n$BatchSeparator`r`n"
+                                $scriptpart = "$script`r`n$BatchSeparator`r`n"
                             }
-                            $script | Out-File -FilePath $FilePath -Encoding $encoding -Append
+                            $scriptpart | Out-File -FilePath $FilePath -Encoding $encoding -Append
                         }
                     }
 
