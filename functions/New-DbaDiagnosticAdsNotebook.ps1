210f224578e70bb63b38101a9b61d605da0fcb5d
Simone Bizzotto <niphlod@gmail.com>
2019-06-10 23:37:14 +0200

    New-DbaDiagnosticAdsNotebook, ignore shoudlprocess req for new- (#5715)
    * New-DbaDiagnosticAdsNotebook, ignore shoudlprocess req for new-
    
    * New-DbaDiagnosticAdsNottebook, whoopsie (do Diagnostic)

--- functions/New-DbaDiagnosticAdsNotebook.ps1
+++ functions/New-DbaDiagnosticAdsNotebook.ps1
@@ -37,7 +37,7 @@ function New-DbaDiagnosticAdsNotebook {
     .NOTES
         Tags: Notebooks, Azure Data Studio
         Author: Gianluca Sartori (@spaghettidba)
--
+
         Website: https://dbatools.io
         Copyright: (c) 2018 by dbatools, licensed under MIT
         License: MIT https://opensource.org/licenses/MIT
@@ -60,7 +60,7 @@ function New-DbaDiagnosticAdsNotebook {
 
         Creates a new Jupyter Notebook named "myNotebook" based on the version "2017" of diagnostic queries, including database-specific queries
     #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
     param(
         [DbaInstanceParameter]$SqlInstance,
         [PSCredential]$SqlCredential,
