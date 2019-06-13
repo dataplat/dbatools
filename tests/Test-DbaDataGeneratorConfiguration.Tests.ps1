e48287e0fc01c4f8e50cdd4b26333337ce7063de
sanderstad <sanderstad@gmail.com>
2019-06-13 13:07:19 +0200

    Changed the way the array got the results

--- tests/Test-DbaDataGeneratorConfiguration.Tests.ps1
+++ tests/Test-DbaDataGeneratorConfiguration.Tests.ps1
@@ -61,7 +61,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $json | ConvertTo-Json -Depth 5 | Out-File $file.FullName -Force
 
         $findings = @()
-        $findings = Test-DbaDataGeneratorConfiguration -FilePath $file.FullName
+        $findings += Test-DbaDataGeneratorConfiguration -FilePath $file.FullName
 
         $findings.Count | Should -Be 1
     }
