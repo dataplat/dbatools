629fe78608db08a88a4e8863bab389dc10f7318d
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2019-06-18 15:33:10 +0000

    Add commands to psd1 and psm1 and some renames of new files (#5783)
    * renames and updates
    
    * add commands to psd1
    
    * add new commands to psm1
    
    * rename with .Tests

--- tests/Test-DbaDbDataGeneratorConfig.Tests.ps1
+++ tests/Test-DbaDbDataGeneratorConfig.Tests.ps1
@@ -15,7 +15,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $dbname = "dbatools_maskingtest"
+        $dbname = "dbatools_datagentest"
         $query = "CREATE DATABASE [$dbname]"
 
         Invoke-DbaQuery -SqlInstance $script:instance1 -Database master -Query $query
@@ -36,7 +36,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 
         Invoke-DbaQuery -SqlInstance $script:instance1 -Database $dbname -Query $query
 
-        $file = New-DbaDbMaskingConfig -SqlInstance $script:instance1 -Database $dbname -Table Customer -Path "C:\temp\datamasking"
+        $file = New-DbaDbDataGeneratorConfig -SqlInstance $script:instance1 -Database $dbname -Table Customer -Path "C:\temp\datageneration"
 
     }
     AfterAll {
@@ -45,7 +45,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 
     It "gives no errors with a correct json file" {
         $findings = @()
-        $findings += Test-DbaDataMaskingConfiguration -FilePath $file.FullName
+        $findings += Test-DbaDbDataGeneratorConfig -FilePath $file.FullName
 
         $findings.Count | Should -Be 0
     }
@@ -55,13 +55,13 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $json = Get-Content -Path $file.FullName | ConvertFrom-Json
 
         # Break the content by removing a property
-        $json.Tables[0].Columns[7].PSObject.Properties.Remove("SubType")
+        $json.Tables[0].Columns[8].PSObject.Properties.Remove("SubType")
 
         # Write the JSON back to the file
         $json | ConvertTo-Json -Depth 5 | Out-File $file.FullName -Force
 
         $findings = @()
-        $findings += Test-DbaDataMaskingConfiguration -FilePath $file.FullName
+        $findings += Test-DbaDbDataGeneratorConfig -FilePath $file.FullName
 
         $findings.Count | Should -Be 1
     }
