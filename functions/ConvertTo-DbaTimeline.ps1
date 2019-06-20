c3845bffdf08035de912adfe59515ee1d47209a8
flagmanchris <christowers@gmail.com>
2019-06-20 20:41:44 +0100

    ConvertTo-DbaTimeline - update row label
    Remove instance name from label when results from only 1 instance

--- functions/ConvertTo-DbaTimeline.ps1
+++ functions/ConvertTo-DbaTimeline.ps1
@@ -140,22 +140,13 @@ function ConvertTo-DbaTimeline {
             $servers += $InputObject[0].SqlInstance
         }
         <#
-            Initially I wanted the server name to appear dynamically based on whether a single or multiple servers were passed as input
-            However (as usual) this is not so simple as we're in pipe mode which means PS is processing each input at once and
-            it does not know how many parameters it has until it has gone through them all. For example $servers.count will start with 1
-            and will be increasing as the process goes through the pipe, or it will stop t 1 if there's only 1 server passed.
-            The $InputObject[0].* is also not available in the begin block. All this means that we will alway show server name next to job name now.
-            I am not 100% happy with this but not sure how to solve it for now.
-        #>
-
-        <#
             This is where do column mapping.
             Check for types - this will help support if someone assigns a variable then pipes
             AgentJobHistory is a forced type while backuphistory is a legit type
         #>
         if ($InputObject[0].TypeName -eq 'AgentJobHistory') {
             $CallerName = "Get-DbaAgentJobHistory"
-            $data = $InputObject | Select-Object @{ Name = "SqlInstance"; Expression = { $_.SqlInstance } }, @{ Name = "InstanceName"; Expression = { $_.InstanceName } }, @{ Name = "vLabel"; Expression = { "[" + $($_.SqlInstance -replace "\\", "\\\") + "] " + $_.Job -replace "\'", ''} }, @{ Name = "hLabel"; Expression = { $_.Status } }, @{ Name = "Style"; Expression = { $(Convert-DbaTimelineStatusColor($_.Status)) } }, @{ Name = "StartDate"; Expression = { $(ConvertTo-JsDate($_.StartDate)) } }, @{ Name = "EndDate"; Expression = { $(ConvertTo-JsDate($_.EndDate)) } }
+            $data = $InputObject | Select-Object @{ Name = "SqlInstance"; Expression = { $_.SqlInstance } }, @{ Name = "InstanceName"; Expression = { $_.InstanceName } }, @{ Name = "vLabel"; Expression = { "[" + $($_.SqlInstance -replace "\\", "\\\") + "] " + $_.Job -replace "\'", '' } }, @{ Name = "hLabel"; Expression = { $_.Status } }, @{ Name = "Style"; Expression = { $(Convert-DbaTimelineStatusColor($_.Status)) } }, @{ Name = "StartDate"; Expression = { $(ConvertTo-JsDate($_.StartDate)) } }, @{ Name = "EndDate"; Expression = { $(ConvertTo-JsDate($_.EndDate)) } }
         } elseif ($InputObject[0] -is [Sqlcollaborative.Dbatools.Database.BackupHistory]) {
             $CallerName = "Get-DbaBackupHistory"
             $data = $InputObject | Select-Object @{ Name = "SqlInstance"; Expression = { $_.SqlInstance } }, @{ Name = "InstanceName"; Expression = { $_.InstanceName } }, @{ Name = "vLabel"; Expression = { "[" + $($_.SqlInstance -replace "\\", "\\\") + "] " + $_.Database } }, @{ Name = "hLabel"; Expression = { $_.Type } }, @{ Name = "StartDate"; Expression = { $(ConvertTo-JsDate($_.Start)) } }, @{ Name = "EndDate"; Expression = { $(ConvertTo-JsDate($_.End)) } }
@@ -168,6 +159,10 @@ function ConvertTo-DbaTimeline {
     }
     end {
         if (Test-FunctionInterrupt) { return }
+        If ($servers.count -eq 1) {
+            # Remove instance name label for single instance
+            $body = $body -replace ("(?!\[')(\[.*?)\] ", "")
+        }
         $end = @"
 ]);
         var paddingHeight = 20;
