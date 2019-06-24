f8a4f51d0ad0ea4662bac0ad18503c176dda722d
GeorgePalacios <30286771+GeorgePalacios@users.noreply.github.com>
2019-06-24 13:16:56 +0100

    Add-Dba-Ag-Database - Added fix for named instances (#5809)
    * Added fix for named instances
    
    $SecondaryInstance.netname wasn't the correct AG replica name
    
    * formatting
    
    * add linux support

--- functions/Add-DbaAgDatabase.ps1
+++ functions/Add-DbaAgDatabase.ps1
@@ -175,10 +175,20 @@ function Add-DbaAgDatabase {
 
             foreach ($secondaryInstance in $secondaryInstances) {
 
-                $agreplica = Get-DbaAgReplica -SqlInstance $Primary -SqlCredential $SqlCredential -AvailabilityGroup $ag.name -Replica $secondaryInstance.NetName
+                try {
+                    $secondaryInstanceReplicaName = $secondaryInstance.NetName
+                } catch {
+                    $secondaryInstanceReplicaName = $secondaryInstance.ComputerName
+                }
+
+                if ($secondaryInstance.InstanceName) {
+                    $secondaryInstanceReplicaName = $secondaryInstanceReplicaName, $secondaryInstance.InstanceName -join "\"
+                }
+
+                $agreplica = Get-DbaAgReplica -SqlInstance $Primary -SqlCredential $SecondarySqlCredential -AvailabilityGroup $ag.name -Replica $secondaryInstanceReplicaName
 
                 if (-not $agreplica) {
-                    Stop-Function -Continue -Message "Could not connect to instance $($secondaryInstance.Name)"
+                    Stop-Function -Continue -Message "Could not connect get secondary replica $($secondaryInstanceReplicaName) from $($Primary.Name)"
                 }
 
                 if ($SeedingMode -eq "Automatic" -and $secondaryInstance.VersionMajor -le 12) {
