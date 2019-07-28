043fb81b7b27097cfc464599186409d5cd5eaa5a
Tracy Boggiano <Tracy.Boggiano@docusign.com>
2019-07-28 19:45:47 -0400

    Invoke-DbatoolsFormatter for functions.

--- functions/Set-DbaDbQueryStoreOption.ps1
+++ functions/Set-DbaDbQueryStoreOption.ps1
@@ -148,8 +148,7 @@ function Set-DbaDbQueryStoreOption {
             try {
                 $server = Connect-SqlInstance -SqlInstance $instance -SqlCredential $SqlCredential -MinimumVersion 13
 
-            }
-            catch {
+            } catch {
                 Stop-Function -Message "Can't connect to $instance. Moving on." -Category InvalidOperation -InnerErrorRecord $_ -Target $instance -Continue
             }
 
@@ -270,8 +269,7 @@ function Set-DbaDbQueryStoreOption {
                         if ($query -ne "") {
                             $db.Query($query, $db.Name)
                         }
-                    }
-                    catch {
+                    } catch {
                         Stop-Function -Message "Could not modify configuration." -Category InvalidOperation -InnerErrorRecord $_ -Target $db -Continue
                     }
                 }
