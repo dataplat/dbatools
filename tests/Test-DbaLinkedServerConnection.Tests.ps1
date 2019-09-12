cbe2e11fbc3e4fc59fbe1f6d88a7001e24eca117
jpomfret <jpomfret7@gmail.com>
2019-09-12 17:28:55 -0400

    fix for issue #6036

--- tests/Test-DbaLinkedServerConnection.Tests.ps1
+++ tests/Test-DbaLinkedServerConnection.Tests.ps1
@@ -24,10 +24,29 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
         $server = Connect-DbaInstance -SqlInstance $script:instance1 -Database master
         $server.Query("EXEC master.dbo.sp_dropserver @server=N'localhost', @droplogins='droplogins'")
     }
+    Context "Function works" {
+        $results = Test-DbaLinkedServerConnection -SqlInstance $script:instance1 | Where-Object LinkedServerName -eq 'localhost'
+        It "function returns results" {
+            $results | Should Not BeNullOrEmpty
+        }
+        It "linked server name is localhost" {
+            $results.LinkedServerName | Should Be 'localhost'
+        }
+        It "connectivity is true" {
+            $results.Connectivity | Should BeTrue
+        }
+    }
 
-    $results = Test-DbaLinkedServerConnection -SqlInstance $script:instance1 | Where-Object LinkedServerName -eq 'localhost'
-    It "can connect to linked server 'localhost'" {
-        $results.LinkedServerName -eq 'localhost'
-        $results.Connectivity -eq $true
+    Context "Piping to function works" {
+        $pipeResults =  Get-DbaLinkedServer -SqlInstance $script:instance1 | Test-DbaLinkedServerConnection
+        It "piping from Get-DbaLinkedServerConnection returns results" {
+            $pipeResults | Should Not BeNullOrEmpty
+        }
+        It "linked server name is localhost" {
+            $pipeResults.LinkedServerName | Should Be 'localhost'
+        }
+        It "connectivity is true" {
+            $pipeResults.Connectivity | Should BeTrue
+        }
     }
 }
