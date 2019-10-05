5f06217cb92cc35438029a15848bab5fe30eea29
Shawn Melton <wshawnmelton@gmail.com>
2019-10-05 17:35:06 -0500

    adjusting test read-dbatracefile

--- tests/Read-DbaTraceFile.Tests.ps1
+++ tests/Read-DbaTraceFile.Tests.ps1
@@ -4,7 +4,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('WhatIf', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Path', 'Database', 'Login', 'Spid', 'EventClass', 'ObjectType', 'ErrorId', 'EventSequence', 'TextData', 'ApplicationName', 'ObjectName', 'Where', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
@@ -28,8 +28,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 
     Context "Verifying command output" {
         It "returns results" {
-            $results = $script:instance1, $script:instance2 | Get-DbaTrace -Id 1 | Read-DbaTraceFile
-
+            $results = Get-DbaTrace -SqlInstance $script:instance1, $script:instance2 -Id 1 | Read-DbaTraceFile
             $results.DatabaseName.Count | Should -BeGreaterThan 0
         }
 
@@ -42,42 +41,40 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
                     and ApplicationName not like 'SQLAgent%'
                     and ApplicationName not like 'Microsoft SQL Server Management Studio%'"
 
-            # Collect the results into a variable so that the bulk import is supafast
-            $results = $script:instance1, $script:instance2 | Get-DbaTrace -Id 1 | Read-DbaTraceFile -Where $where -WarningAction SilentlyContinue -WarningVariable warn
+            # Collect the results into a variable so that the bulk import is super fast
+            Get-DbaTrace -SqlInstance $script:instance1, $script:instance2 -Id 1 | Read-DbaTraceFile -Where $where -WarningAction SilentlyContinue -WarningVariable warn > $null
             $warn | Should -Be $null
         }
     }
     Context "Verify Parameter Use" {
-
-        It "Should execute using paramters Database, Login, Spid" {
+        It "Should execute using parameters Database, Login, Spid" {
             $results = $script:instance1, $script:instance2 | Get-DbaTrace -Id 1 | Read-DbaTraceFile -Database Master -Login sa -Spid 7 -WarningAction SilentlyContinue -WarningVariable warn
             $warn | Should -Be $null
         }
-        It "Should execute using paramters EventClass, ObjectType, ErrorId" {
+        It "Should execute using parameters EventClass, ObjectType, ErrorId" {
             $results = $script:instance1, $script:instance2 | Get-DbaTrace -Id 1 | Read-DbaTraceFile -EventClass 4 -ObjectType 4 -ErrorId 4 -WarningAction SilentlyContinue -WarningVariable warn
             $warn | Should -Be $null
         }
-        It "Should execute using paramters EventSequence, TextData, ApplicationName, ObjectName" {
+        It "Should execute using parameters EventSequence, TextData, ApplicationName, ObjectName" {
             $results = $script:instance1, $script:instance2 | Get-DbaTrace -Id 1 | Read-DbaTraceFile -EventSequence 4 -TextData "Text" -ApplicationName "Application" -ObjectName "Name" -WarningAction SilentlyContinue -WarningVariable warn
             $warn | Should -Be $null
         }
-
     }
     Context "Verify Failure with Mocks" {
         It "Should Fail Connection to SqlInstance" {
             Mock Connect-SqlInstance { throw } -module dbatools
-            Mock Stop-Function { "Error occurred while establishing connection to NotAnInstance" } -module dbatools
+            Mock Stop-Function { "Error occurred while establishing connection to NotAnInstance" } -Module dbatools
             (Read-DbaTraceFile -SqlInstance "NotAnInstance" ) | Should -BeLike "Error occurred while establishing connection to NotAnInstance"
         }
         It "Should try `$CurrentPath" {
             #This mock forces line 257 to be tested
             Mock Test-Bound { $false } -module dbatools
-            (Read-DbaTraceFile -SqlInstance "$script:Instance2" ) | Should -Not -Be $null
+            (Read-DbaTraceFile -SqlInstance "$script:instance2" ) | Should -Not -Be $null
         }
         It "Should Fail to find the Path" {
             Mock Test-DbaPath { $false } -module dbatools
-            Mock Write-Message { "Path Does Not Exist" } -module dbatools
-            (Read-DbaTraceFile -SqlInstance "$script:Instance2" ) | Should -Be "Path does not exist*"
+            Mock Write-Message { "Path does not exist" } -module dbatools
+            (Read-DbaTraceFile -SqlInstance "$script:instance2" ) | Should -Be "Path does not exist"
         }
     }
 }
