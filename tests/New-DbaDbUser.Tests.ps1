f59c7eed43b04eeb0580420422defdfdd9d1fc29
Richard <richardlhughes@hotmail.com>
2019-10-10 08:39:28 -0500

    revert to use Connect-SqlInstance

--- tests/New-DbaDbUser.Tests.ps1
+++ tests/New-DbaDbUser.Tests.ps1
@@ -12,7 +12,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         }
     }
     Context "Should error out if the database does not exist" {
-        Mock -CommandName Connect-DbaInstance {
+        Mock -CommandName Connect-SqlInstance {
             [PSCustomObject]@{
                 Databases    = [String]::Empty
                 IsAccessible = $false
