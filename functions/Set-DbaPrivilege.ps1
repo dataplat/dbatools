b323f190ce661f3dca5439b427a59b6f1fe1646f
Ben Thul <ben.thul@gmail.com>
2020-02-22 18:36:50 -0800

    Changing parameter check to Test-Bound based on PR feedback.

--- functions/Set-DbaPrivilege.ps1
+++ functions/Set-DbaPrivilege.ps1
@@ -89,7 +89,7 @@ function Convert-UserNameToSID ([string] `$Acc ) {
                         }
 
                         $SQLServiceAccounts = @();
-                        if ([string]::IsNullOrEmpty($User) -eq $false) {
+                        if (Test-Bound 'User') {
                             $SQLServiceAccounts += $User;
                         }
                         else {
