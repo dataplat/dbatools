994fba792559aa855753421882034276db2a40bf
Ben Thul <ben.thul@gmail.com>
2020-03-25 09:12:18 -0700

    Added documentation for new parameters.

--- functions/Get-DbaAgentAlert.ps1
+++ functions/Get-DbaAgentAlert.ps1
@@ -19,6 +19,15 @@ function Get-DbaAgentAlert {
     .PARAMETER Alert
         The name of the alerts to return. If null, will get all alerts from the server.
 
+    .PARAMETER ExcludeAlert
+        The name of the alerts to exclude. If not provided, no alerts will be excluded.
+
+    .PARAMETER AlertPattern
+        A wildcard expression for alerts to return.
+
+    .PARAMETER ExcludeAlertPattern
+        A wildcard expression for alerts to exclude.
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
