adaa333925d61813e4ce6b0314a2a80b55744374
Garry Bargsley <gbargsley@gmail.com>
2020-04-08 05:11:13 -0500

    changed ot more descriptive database name

--- functions/Get-DbaDbBackupHistory.ps1
+++ functions/Get-DbaDbBackupHistory.ps1
@@ -349,7 +349,7 @@ function Get-DbaDbBackupHistory {
                     if ($null -eq (Get-PSCallStack)[1].Command -or '{ScriptBlock}' -eq (Get-PSCallStack)[1].Command) {
                         $forkCheckSql = "
                                 SELECT
-                                    database_name as 'name',
+                                    database_name,
                                     MIN(database_backup_lsn) as 'FirstLsn',
                                     MAX(database_backup_lsn) as 'FinalLsn',
                                     MIN(backup_start_date) as 'MinDate',
@@ -369,7 +369,7 @@ function Get-DbaDbBackupHistory {
                             if (-not $LastFull) {
                                 Write-Message -Message "Found backups from multiple recovery forks for $($db.name) on $($server.name), this may affect your results" -Level Warning
                                 foreach ($result in $results) {
-                                    Write-Message -Message "Between $($result.MinDate)/$($result.FirstLsn) and $($result.MaxDate)/$($result.FinalLsn) $($result.name) was on Recovery Fork GUID $($result.RecFork) ($($result.backupcount) backups)" -Level Warning
+                                    Write-Message -Message "Between $($result.MinDate)/$($result.FirstLsn) and $($result.MaxDate)/$($result.FinalLsn) $($result.database_name) was on Recovery Fork GUID $($result.RecFork) ($($result.backupcount) backups)" -Level Warning
                                 }
                             }
                             if ($null -eq $RecoveryFork) {
@@ -429,7 +429,7 @@ function Get-DbaDbBackupHistory {
                     if ((Get-PSCallStack)[1].Command -notlike ' Get-DbaDbBackupHistory*') {
                         $forkCheckSql = "
                             SELECT
-                                database_name as 'name',
+                                database_name,
                                 MIN(database_backup_lsn) as 'FirstLsn',
                                 MAX(database_backup_lsn) as 'FinalLsn',
                                 MIN(backup_start_date) as 'MinDate',
@@ -448,7 +448,7 @@ function Get-DbaDbBackupHistory {
                             if (-not $LastFull) {
                                 Write-Message -Message "Found backups from multiple recovery forks for $($db.name) on $($server.name), this may affect your results" -Level Warning
                                 foreach ($result in $results) {
-                                    Write-Message -Message "Between $($result.MinDate)/$($result.FirstLsn) and $($result.MaxDate)/$($result.FinalLsn) $($result.name) was on Recovery Fork GUID $($result.RecFork) ($($result.backupcount) backups)"   -Level Warning
+                                    Write-Message -Message "Between $($result.MinDate)/$($result.FirstLsn) and $($result.MaxDate)/$($result.FinalLsn) $($result.database_name) was on Recovery Fork GUID $($result.RecFork) ($($result.backupcount) backups)"   -Level Warning
                                 }
                             }
                         }
