70be5dcb0eb402fc82fd7646779c94288d8470c2
Shawn Melton <wshawnmelton@gmail.com>
2020-05-13 15:26:23 -0500

    adjusting test (do Install-DbaFirstResponderKit)

--- tests/Install-DbaFirstResponderKit.Tests.ps1
+++ tests/Install-DbaFirstResponderKit.Tests.ps1
@@ -4,11 +4,11 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Branch', 'Database', 'LocalFile', 'Force', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
@@ -17,28 +17,28 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "Testing First Responder Kit installer" {
         BeforeAll {
             $database = "dbatoolsci_frk_$(Get-Random)"
-            $server = Connect-DbaInstance -SqlInstance $script:instance2
+            $server = Connect-DbaInstance -SqlInstance lab-sql-01 -SqlCredential $labAdminSqlCred
             $server.Query("CREATE DATABASE $database")
         }
         AfterAll {
             Remove-DbaDatabase -SqlInstance $script:instance2 -Database $database -Confirm:$false
         }
 
-        $results = Install-DbaFirstResponderKit -SqlInstance $script:instance2 -Database $database -Branch master -Force
+        $results = Install-DbaFirstResponderKit -SqlInstance $server -Database $database -Branch master -Force
 
         It "Installs to specified database: $database" {
-            $results[0].Database -eq $database | Should Be $true
+            $results[0].Database -eq $database | Should -Be $true
         }
         It "Shows status of Installed" {
-            $results[0].Status -eq "Installed" | Should Be $true
+            $results[0].Status -eq "Installed" | Should -Be $true
         }
         It "At least installed sp_Blitz and sp_BlitzIndex" {
-            'sp_Blitz', 'sp_BlitzIndex' | Should BeIn $results.Name
+            'sp_Blitz', 'sp_BlitzIndex' | Should -BeIn $results.Name
         }
-        It "has the correct properties" {
+        It "Has the correct properties" {
             $result = $results[0]
             $ExpectedProps = 'SqlInstance,InstanceName,ComputerName,Name,Status,Database'.Split(',')
-            ($result.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
+            ($result.PsObject.Properties.Name | Sort-Object) | Should -Be ($ExpectedProps | Sort-Object)
         }
     }
 }
