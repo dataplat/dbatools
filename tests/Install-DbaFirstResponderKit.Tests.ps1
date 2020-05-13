1ea0f98052323b14663f4a99fbbe370b5690b75d
Shawn Melton <wshawnmelton@gmail.com>
2020-05-13 15:40:51 -0500

    (do Install-DbaFirstResponderKit) Forgot to revert my lab setup :(

--- tests/Install-DbaFirstResponderKit.Tests.ps1
+++ tests/Install-DbaFirstResponderKit.Tests.ps1
@@ -17,28 +17,28 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "Testing First Responder Kit installer" {
         BeforeAll {
             $database = "dbatoolsci_frk_$(Get-Random)"
-            $server = Connect-DbaInstance -SqlInstance lab-sql-01 -SqlCredential $labAdminSqlCred
+            $server = Connect-DbaInstance -SqlInstance $script:instance2
             $server.Query("CREATE DATABASE $database")
-
-            $results = Install-DbaFirstResponderKit -SqlInstance $server -Database $database -Branch master -Force
         }
         AfterAll {
             Remove-DbaDatabase -SqlInstance $script:instance2 -Database $database -Confirm:$false
         }
 
+        $results = Install-DbaFirstResponderKit -SqlInstance $server -Database $database -Branch master -Force
+
         It "Installs to specified database: $database" {
-            $results[0].Database -eq $database | Should -Be $true
+            $results[0].Database -eq $database | Should Be $true
         }
         It "Shows status of Installed" {
-            $results[0].Status -eq "Installed" | Should -Be $true
+            $results[0].Status -eq "Installed" | Should Be $true
         }
         It "At least installed sp_Blitz and sp_BlitzIndex" {
-            'sp_Blitz', 'sp_BlitzIndex' | Should -BeIn $results.Name
+            'sp_Blitz', 'sp_BlitzIndex' | Should BeIn $results.Name
         }
         It "Has the correct properties" {
             $result = $results[0]
             $ExpectedProps = 'SqlInstance,InstanceName,ComputerName,Name,Status,Database'.Split(',')
-            ($result.PsObject.Properties.Name | Sort-Object) | Should -Be ($ExpectedProps | Sort-Object)
+            ($result.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
         }
     }
 }
