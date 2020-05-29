f0c15d133dd42a2f27759575763d371d3298e2dc
MikeyBronowski <mikeybronowski@gmail.com>
2020-05-29 21:49:54 +0200

    Adding BeforeAll and AfterAll to cleanup databases

--- tests/New-DbaDatabase.Tests.ps1
+++ tests/New-DbaDatabase.Tests.ps1
@@ -4,16 +4,25 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'Collation', 'Recoverymodel', 'Owner', 'DataFilePath', 'LogFilePath', 'PrimaryFilesize', 'PrimaryFileGrowth', 'PrimaryFileMaxSize', 'LogSize', 'LogGrowth', 'SecondaryFilesize', 'SecondaryFileGrowth', 'SecondaryFileMaxSize', 'SecondaryFileCount', 'DefaultFileGroup', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
 
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $dbname = 'dbatoolsci_newdb', 'dbatoolsci_newdb1', 'dbatoolsci_newdb2'
+        $null = Get-DbaDatabase -SqlInstance $script:instance2, $script:instance3 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    }
+
+    AfterAll {
+        $null = Get-DbaDatabase -SqlInstance $script:instance2, $script:instance3 -Database $dbname | Remove-DbaDatabase -Confirm:$false
+    }
+
     Context "commands work as expected" {
         $null = Get-DbaProcess -SqlInstance $script:instance2, $script:instance3 | Where-Object Program -match dbatools | Stop-DbaProcess -Confirm:$false
         $results = New-DbaDatabase -SqlInstance $script:instance2
