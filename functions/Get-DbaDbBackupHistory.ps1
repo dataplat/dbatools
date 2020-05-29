490900704af3cc0c1253f108d0d635d95258e41a
MikeyBronowski <mikeybronowski@gmail.com>
2020-05-29 23:20:49 +0200

    Removing DeviceType validateset as it's breaking

--- functions/Get-DbaDbBackupHistory.ps1
+++ functions/Get-DbaDbBackupHistory.ps1
@@ -8,7 +8,7 @@ function Get-DbaDbBackupHistory {
 
         You can even get detailed information (including file path) for latest full, differential and log files.
 
-        Backups taken with the CopyOnly option will NOT be returned, unless the IncludeCopyOnly switch is present or the target includes an Availabity Group listener or a database in an Availability Group
+        Backups taken with the CopyOnly option will NOT be returned, unless the IncludeCopyOnly switch is present or the target includes an Availability Group listener or a database in an Availability Group
 
         If an Availability Group listener is specified as the target, then all nodes in the Group will be queried to return backup history
 
@@ -20,7 +20,7 @@ function Get-DbaDbBackupHistory {
         The target SQL Server instance or instances. This can be a collection and receive pipeline input to allow the function to be executed against multiple SQL Server instances.
 
     .PARAMETER SqlCredential
-        Credential object used to connect to the SQL Server Instance as a different user. This can be a Windows or SQL Server account. Windows users are determined by the existence of a backslash, so if you are intending to use an alternative Windows connection instead of a SQL login, ensure it contains a backslash.
+        Credential object used to connect to the SQL Server instance as a different user. This can be a Windows or SQL Server account. Windows users are determined by the existence of a backslash, so if you are intending to use an alternative Windows connection instead of a SQL login, ensure it contains a backslash.
 
     .PARAMETER Database
         Specifies one or more database(s) to process. If unspecified, all databases will be processed.
@@ -53,7 +53,7 @@ function Get-DbaDbBackupHistory {
         If this switch is enabled, the most recent log backup is returned.
 
     .PARAMETER DeviceType
-        Specifies a filter for backup sets based on DeviceTypes. Valid options are 'Disk','Permanent Disk Device', 'Tape', 'Permanent Tape Device','Pipe','Permanent Pipe Device','Virtual Device', in addition to custom integers for your own DeviceTypes.
+        Specifies a filter for backup sets based on DeviceType. Valid options are 'Disk','Permanent Disk Device', 'Tape', 'Permanent Tape Device','Pipe','Permanent Pipe Device','Virtual Device','URL', in addition to custom integers for your own DeviceType.
 
     .PARAMETER Raw
         If this switch is enabled, one object per backup file is returned. Otherwise, media sets (striped backups across multiple files) will be grouped into a single return object.
@@ -73,7 +73,7 @@ function Get-DbaDbBackupHistory {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .PARAMETER AgCheck
-        Internal parameter used for getting history from AvailabilityGroups. If set, this will disable  Availability Group support
+        Internal parameter used for getting history from AvailabilityGroups. If set, this will disable Availability Group support
 
     .NOTES
         Tags: DisasterRecovery, Backup
@@ -276,7 +276,7 @@ function Get-DbaDbBackupHistory {
             if ($server.VersionMajor -ge 12) {
                 $compressedFlag = $true
                 $encryptedFlag = $true
-                # 2014 introducted encryption
+                # 2014 introduced encryption
                 $backupCols = "
                 backupset.backup_size AS TotalSize,
                 backupset.compressed_backup_size as CompressedBackupSize,
@@ -318,7 +318,7 @@ function Get-DbaDbBackupHistory {
                 $databases = $databases | Where-Object Name -NotIn $ExcludeDatabase
             }
             if (($server.AvailabilityGroups.count -gt 0) -and ($agCheck -ne $True)) {
-                $adbs = $databases | Where-Object Name -In  $server.AvailabilityGroups.AvailabilityDatabases.Name
+                $adbs = $databases | Where-Object Name -In $server.AvailabilityGroups.AvailabilityDatabases.Name
                 $adbs = $adbs | Where-Object Name -NotIn $ProcessedAgDatabases
                 ForEach ($adb in $adbs) {
                     Write-Message -Level Verbose -Message "Fetching history from replicas for db $($adb.name)"
@@ -335,7 +335,7 @@ function Get-DbaDbBackupHistory {
                     }
                     # Results already in the right format, drop straight to output
                     $AgLoopResults
-                    # Remove database from collection as it's now done with
+                    # Remove database from collection as it is now done with
                     $databases = $databases | Where-Object Name -ne $adb.name
                 }
             }
@@ -406,7 +406,7 @@ function Get-DbaDbBackupHistory {
                         }
                     }
                     if ($IncludeCopyOnly -eq $true) {
-                        Write-Message -Level Verbose -Message 'Copy Only chekc'
+                        Write-Message -Level Verbose -Message 'Copy Only check'
                         $allBackups += Get-DbaDbBackupHistory -SqlInstance $server -Database $db.Name -raw:$raw -DeviceType $DeviceType -LastLsn $tlogStartDsn -IncludeCopyOnly:$IncludeCopyOnly -Since:$since -RecoveryFork $RecoveryFork -AgCheck:$Agcheck | Where-Object { $_.Type -eq 'Log' -and [bigint]$_.LastLsn -gt [bigint]$tlogStartDsn -and $_.LastRecoveryForkGuid -eq $fullDb.LastRecoveryForkGuid }
                     }
                     else {
