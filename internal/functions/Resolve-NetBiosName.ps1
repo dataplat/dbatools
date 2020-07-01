b603816c2ef411a7a0044912498c0bc9ec3974fe
yoke88 <yoke-msn@hotmail.com>
2020-07-01 21:07:20 +0800

    Update Resolve-NetBiosName.ps1
    handle null computername

--- internal/functions/Resolve-NetBiosName.ps1
+++ internal/functions/Resolve-NetBiosName.ps1
@@ -12,6 +12,8 @@ Internal function. Takes a best guess at the NetBIOS name of a server.
     $server = Connect-SqlInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
     if($server.ComputerName){
         return $server.ComputerName
+    }elseif($server.ComputerNamePhysicalNetBIOS){
+        return $server.ComputerNamePhysicalNetBIOS
     }else{
         return $server.name
     }
