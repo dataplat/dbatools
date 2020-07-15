59fb329e4b2eb751f977c21ba0dbe709ef510f25
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2020-07-16 00:35:18 +0200

    Install-DbaFirstReponderKit - Address issue with deprecated 2005 scripts, update test, rework folder structure

--- tests/Install-DbaFirstResponderKit.Tests.ps1
+++ tests/Install-DbaFirstResponderKit.Tests.ps1
@@ -4,41 +4,94 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Branch', 'Database', 'LocalFile', 'Force', 'EnableException'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
 }
-
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
-    Context "Testing First Responder Kit installer" {
+    Context "Testing First Responder Kit installer with download" {
         BeforeAll {
             $database = "dbatoolsci_frk_$(Get-Random)"
             $server = Connect-DbaInstance -SqlInstance $script:instance2
             $server.Query("CREATE DATABASE $database")
+
+            $resultsDownload = Install-DbaFirstResponderKit -SqlInstance $script:instance2 -Database $database -Branch main -Force -Verbose:$false
         }
         AfterAll {
             Remove-DbaDatabase -SqlInstance $script:instance2 -Database $database -Confirm:$false
         }
 
-        $results = Install-DbaFirstResponderKit -SqlInstance $script:instance2 -Database $database -Branch main -Force
-
         It "Installs to specified database: $database" {
-            $results[0].Database -eq $database | Should Be $true
+            $resultsDownload[0].Database -eq $database | Should Be $true
         }
         It "Shows status of Installed" {
-            $results[0].Status -eq "Installed" | Should Be $true
+            $resultsDownload[0].Status -eq "Installed" | Should Be $true
         }
         It "At least installed sp_Blitz and sp_BlitzIndex" {
-            'sp_Blitz', 'sp_BlitzIndex' | Should BeIn $results.Name
+            'sp_Blitz', 'sp_BlitzIndex' | Should BeIn $resultsDownload.Name
         }
         It "has the correct properties" {
-            $result = $results[0]
+            $result = $resultsDownload[0]
             $ExpectedProps = 'SqlInstance,InstanceName,ComputerName,Name,Status,Database'.Split(',')
             ($result.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
+        }
+        It "Shows status of Updated" {
+            $resultsDownload = Install-DbaFirstResponderKit -SqlInstance $script:instance2 -Database $database -Verbose:$false
+            $resultsDownload[0].Status -eq 'Updated' | Should -Be $true
+        }
+        It "Shows status of Error" {
+            $folder = Join-Path (Get-DbatoolsConfigValue -FullName Path.DbatoolsData) -Child "SQL-Server-First-Responder-Kit-main"
+            $sqlScript = (Get-ChildItem $folder | Select-Object -First 1).FullName
+            Add-Content $sqlScript (New-Guid).ToString()
+            $result = Install-DbaFirstResponderKit -SqlInstance $script:instance2 -Database $database -Verbose:$false
+            $result[0].Status -eq "Error" | Should -Be $true
+        }
+    }
+    Context "Testing First Responder Kit installer with LocalFile" {
+        BeforeAll {
+            $database = "dbatoolsci_frk_$(Get-Random)"
+            $server = Connect-DbaInstance -SqlInstance $script:instance3
+            $server.Query("CREATE DATABASE $database")
+
+            $outfile = "SQL-Server-First-Responder-Kit-main.zip"
+            Invoke-WebRequest -Uri "https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit/archive/main.zip" -OutFile $outfile
+            if (Test-Path $outfile) {
+                $fullOutfile = (Get-ChildItem $outfile).FullName
+            }
+            $resultsLocalFile = Install-DbaFirstResponderKit -SqlInstance $script:instance3 -Database $database -Branch main -LocalFile $fullOutfile  -Force
+        }
+        AfterAll {
+            Remove-DbaDatabase -SqlInstance $script:instance3 -Database $database -Confirm:$false
+        }
+
+        It "Installs to specified database: $database" {
+            $resultsLocalFile[0].Database -eq $database | Should -Be $true
+        }
+        It "Shows status of Installed" {
+            $resultsLocalFile[0].Status -eq "Installed" | Should -Be $true
+        }
+        It "At least installed sp_Blitz and sp_BlitzIndex" {
+            'sp_Blitz', 'sp_BlitzIndex' | Should BeIn $resultsLocalFile.Name
+        }
+        It "Has the correct properties" {
+            $result = $resultsLocalFile[0]
+            $ExpectedProps = 'SqlInstance,InstanceName,ComputerName,Name,Status,Database'.Split(',')
+            ($result.PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
+        }
+        It "Shows status of Updated" {
+            $resultsLocalFile = Install-DbaFirstResponderKit -SqlInstance $script:instance3 -Database $database
+            $resultsLocalFile[0].Status -eq 'Updated' | Should -Be $true
+        }
+        It "Shows status of Error" {
+            $folder = Join-Path (Get-DbatoolsConfigValue -FullName Path.DbatoolsData) -Child "SQL-Server-First-Responder-Kit-main"
+            $sqlScript = (Get-ChildItem $folder | Select-Object -First 1).FullName
+            Add-Content $sqlScript (New-Guid).ToString()
+            $result = Install-DbaFirstResponderKit -SqlInstance $script:instance3 -Database $database
+            $result[0].Status -eq "Error" | Should -Be $true
         }
     }
 }
