75dcb4a667819fcf63bb5a8b3ca5aca01e1227f2
kkravtsov <kkravtsov@xmatters.com>
2020-08-06 14:50:26 -0700

    fixing the refresh issue

--- functions/Copy-DbaDbViewData.ps1
+++ functions/Copy-DbaDbViewData.ps1
@@ -301,10 +301,8 @@ function Copy-DbaDbViewData {
                         $tempTableName = "$($sqlview.Name)_table"
                         $createquery = "SELECT * INTO tempdb..$tempTableName FROM [$($sqlview.Schema)].[$($sqlview.Name)] WHERE 1=2"
                         Invoke-DbaQuery -SqlInstance $server -Database $Database -Query $createquery -EnableException
-                        if ($sqlview.Table -eq 'tempdb') {
-                            #refreshing table list to make sure get-dbadbtable will find the new table
-                            $server.Databases['tempdb'].Tables.Refresh($true)
-                        }
+                        #refreshing table list to make sure get-dbadbtable will find the new table
+                        $server.Databases['tempdb'].Tables.Refresh($true)
                         $tempTable = Get-DbaDbTable -SqlInstance $server -Database tempdb -Table $tempTableName
                         $tablescript = $tempTable | Export-DbaScript -Passthru | Out-String
                         Invoke-DbaQuery -SqlInstance $server -Database $Database -Query "DROP TABLE tempdb..$tempTableName" -EnableException
