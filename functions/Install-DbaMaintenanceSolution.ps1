f1582f5ad978b8092a25ef344683f7893e34b9d4
Tony Fountain <tafountain@gmail.com>
2020-08-07 14:17:27 -0400

    Fix ordering of scripts to be ran

--- functions/Install-DbaMaintenanceSolution.ps1
+++ functions/Install-DbaMaintenanceSolution.ps1
@@ -232,7 +232,7 @@ function Install-DbaMaintenanceSolution {
 
         function Get-DbaOlaWithParameters($listOfFiles) {
 
-            $fileContents = @{ }
+            [hashtable]$fileContents = [ordered]@{ }
             foreach ($file in $listOfFiles) {
                 $fileContents[$file] = Get-Content -Path $file -Raw
             }
