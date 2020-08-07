396717cf95a7db0df56e7023f8d3c70015780d28
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-08-07 09:04:42 -0500

    Update unit test
    Updating unit test to new standard

--- tests/Get-DbaRandomizedValue.Tests.ps1
+++ tests/Get-DbaRandomizedValue.Tests.ps1
@@ -4,11 +4,10 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
         [object[]]$knownParameters = 'DataType', 'RandomizerType', 'RandomizerSubType', 'Min', 'Max', 'Precision', 'CharacterString', 'Format', 'Separator', 'Symbol', 'Locale', 'Value', 'EnableException'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
 }
