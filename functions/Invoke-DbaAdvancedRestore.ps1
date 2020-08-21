e94584d1e126cefe49f4258374744f5e918ecbb9
stuart moore <stuart@stuart-moore.com>
2020-08-21 11:39:07 +0100

    fix AdvRest as well

--- functions/Invoke-DbaAdvancedRestore.ps1
+++ functions/Invoke-DbaAdvancedRestore.ps1
@@ -59,6 +59,7 @@ function Invoke-DbaAdvancedRestore {
 
     .PARAMETER Continue
         Indicates that the restore is continuing a restore, so target database must be in Recovering or Standby states
+        Using this parameter will set WithReplace to True
 
     .PARAMETER AzureCredential
         AzureCredential required to connect to blob storage holding the backups
