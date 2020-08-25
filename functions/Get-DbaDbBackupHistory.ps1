b36bfeb07371714316cd90c4f2c62f59360d6b64
andreasjordan <anj@ordix.de>
2020-08-25 12:29:45 +0200

    Use last last_recovery_fork_guid with -Last*
    When -LastFull, -LastDiff or -LastLog is used, then only take backups
    from the last used database_guid and last_recovery_fork_guid
    into account
    to prevent to return old backups from old recovery forks
    with higher LSNs.
    See issue #6730

--- functions/Get-DbaDbBackupHistory.ps1
+++ functions/Get-DbaDbBackupHistory.ps1
@@ -564,16 +564,15 @@ function Get-DbaDbBackupHistory {
                         JOIN msdb..backupmediaset AS mediaset ON mediafamily.media_set_id = mediaset.media_set_id
                         JOIN msdb..backupset AS backupset ON backupset.media_set_id = mediaset.media_set_id
                         JOIN (
-                        SELECT DISTINCT database_guid, database_name, backup_finish_date
-                        FROM msdb..backupset
-                        WHERE backupset.database_name = '$($db.Name)') dbguid ON dbguid.database_name =  backupset.database_name AND dbguid.database_guid = backupset.database_guid
-                    JOIN (
-                        SELECT database_name, MAX(backup_finish_date) max_finish_date
-                        FROM msdb..backupset
-                        WHERE backupset.database_name = '$($db.Name)'
-                        GROUP BY database_name) dbguid_support ON dbguid_support.database_name = backupset.database_name AND dbguid.backup_finish_date = dbguid_support.max_finish_date
-                    WHERE backupset.database_name = '$($db.Name)' $whereCopyOnly
-                    AND (type = '$first' OR type = '$second')
+                            SELECT database_guid, last_recovery_fork_guid
+                            FROM (SELECT database_guid, last_recovery_fork_guid, ROW_NUMBER() OVER (ORDER BY backup_finish_date DESC) AS rn
+                                  FROM msdb..backupset
+                                  WHERE database_name = '$($db.Name)'
+                                 ) AS bs
+                            WHERE rn = 1
+                            ) AS last_guids ON last_guids.database_guid = backupset.database_guid AND last_guids.last_recovery_fork_guid = backupset.last_recovery_fork_guid
+                    WHERE (type = '$first' OR type = '$second')
+                    $whereCopyOnly
                     $devTypeFilterWhere
                     $sinceSqlFilter
                     $recoveryForkSqlFilter
