f92b63cd6545198e3655f55bc9728d484f9e3c8a
Tony Fountain <tafountain@gmail.com>
2020-08-26 13:30:35 -0400

    Fix minor issues with tests

--- tests/Start-DbaAgentJob.Tests.ps1
+++ tests/Start-DbaAgentJob.Tests.ps1
@@ -4,11 +4,11 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Job', 'ExcludeJob', 'InputObject', 'AllJobs', 'Wait', 'Parallel', 'WaitPeriod', 'SleepPeriod', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
 }
@@ -27,6 +27,10 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         It "returns a CurrentRunStatus of not Idle and supports pipe" {
             $results = Get-DbaAgentJob -SqlInstance $script:instance2 -Job $jobName | Start-DbaAgentJob
             $results.CurrentRunStatus -ne 'Idle' | Should Be $true
+        }
+
+        It "returns a CurrentRunStatus of not null and supports pipe" {
+            $results = Get-DbaAgentJob -SqlInstance $script:instance2 -Job $jobName | Start-DbaAgentJob
             $results.CurrentRunStatus -ne $null | Should Be $true
         }
 
