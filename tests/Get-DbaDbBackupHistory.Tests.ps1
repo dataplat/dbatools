b323531de90931041da9d1da96e384f3e161882f
Simone Bizzotto <niphlod@gmail.com>
2020-09-01 14:25:39 +0200

    Get-DbaDbBackupHistory - adjust test for stability

--- tests/Get-DbaDbBackupHistory.Tests.ps1
+++ tests/Get-DbaDbBackupHistory.Tests.ps1
@@ -22,10 +22,10 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $random = Get-Random
         $dbname = "dbatoolsci_history_$random"
         $dbnameForked = "dbatoolsci_history_forked_$random"
-        $null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname,$dbnameForked | Remove-DbaDatabase -Confirm:$false
+        $null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname, $dbnameForked | Remove-DbaDatabase -Confirm:$false
         $null = Restore-DbaDatabase -SqlInstance $script:instance1 -Path $script:appveyorlabrepo\singlerestore\singlerestore.bak -DatabaseName $dbname -DestinationFilePrefix $dbname
         $server = Connect-DbaInstance -SqlInstance $script:instance1
-        $server.Databases['master'].ExecuteNonQuery("CREATE DATABASE $dbnameForked")
+        $server.Databases['master'].ExecuteNonQuery("CREATE DATABASE $dbnameForked; ALTER DATABASE $dbnameForked SET AUTO_CLOSE OFF WITH ROLLBACK IMMEDIATE")
         $db = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname
         $db | Backup-DbaDatabase -Type Full -BackupDirectory $DestBackupDir
         $db | Backup-DbaDatabase -Type Differential -BackupDirectory $DestBackupDir
@@ -36,7 +36,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     }
 
     AfterAll {
-        $null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname,$dbnameForked | Remove-DbaDatabase -Confirm:$false
+        $null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname, $dbnameForked | Remove-DbaDatabase -Confirm:$false
     }
 
     Context "Get last history for single database" {
