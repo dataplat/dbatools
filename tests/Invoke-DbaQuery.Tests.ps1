d930ca3088563768c6f6c09c8f9f038c83380f3a
niphlod <niphlod@gmail.com>
2020-09-08 23:52:21 +0200

    put commandname back in

--- tests/Invoke-DbaQuery.Tests.ps1
+++ tests/Invoke-DbaQuery.Tests.ps1
@@ -10,6 +10,7 @@ BeforeAll {
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         It "Should only contain our specific parameters" {
+            $CommandName = (Get-Item $PSCommandPath).Name.Replace(".Tests.ps1", "")
             [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
             [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'Query', 'QueryTimeout', 'File', 'SqlObject', 'As', 'SqlParameters', 'AppendServerInstance', 'MessagesToOutput', 'InputObject', 'ReadOnly', 'EnableException', 'CommandType'
             Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
