2a59b156252a66a8d5a0fcd34ecc1e3e22136597
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2020-09-12 21:59:17 +0200

    [Get-DbaAgBackupHistory] - New command for the availability group functionality of Get-DbaDbBackupHistory (#6799)

--- tests/Get-DbaAgBackupHistory.Tests.ps1
+++ tests/Get-DbaAgBackupHistory.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'IncludeCopyOnly', 'Force', 'Since', 'RecoveryFork', 'Last', 'LastFull', 'LastDiff', 'LastLog', 'DeviceType', 'Raw', 'LastLsn', 'Type', 'EnableException', 'IncludeMirror', 'AgCheck'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Database', 'ExcludeDatabase', 'IncludeCopyOnly', 'Force', 'Since', 'RecoveryFork', 'Last', 'LastFull', 'LastDiff', 'LastLog', 'DeviceType', 'Raw', 'LastLsn', 'Type', 'EnableException', 'IncludeMirror'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
@@ -25,7 +25,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $null = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname, $dbnameForked | Remove-DbaDatabase -Confirm:$false
         $null = Restore-DbaDatabase -SqlInstance $script:instance1 -Path $script:appveyorlabrepo\singlerestore\singlerestore.bak -DatabaseName $dbname -DestinationFilePrefix $dbname
         $server = Connect-DbaInstance -SqlInstance $script:instance1
-        $server.Databases['master'].ExecuteNonQuery("CREATE DATABASE $dbnameForked; ALTER DATABASE $dbnameForked SET AUTO_CLOSE OFF WITH ROLLBACK IMMEDIATE")
+        $server.Databases['master'].ExecuteNonQuery("CREATE DATABASE $dbnameForked")
         $db = Get-DbaDatabase -SqlInstance $script:instance1 -Database $dbname
         $db | Backup-DbaDatabase -Type Full -BackupDirectory $DestBackupDir
         $db | Backup-DbaDatabase -Type Differential -BackupDirectory $DestBackupDir
@@ -40,7 +40,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     }
 
     Context "Get last history for single database" {
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -Database $dbname -Last
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -Database $dbname -Last
         It "Should be 4 backups returned" {
             $results.count | Should Be 4
         }
@@ -56,44 +56,44 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     }
 
     Context "Get last history for all databases" {
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1
         It "Should be more than one database" {
             ($results | Where-Object Database -match "master").Count | Should BeGreaterThan 0
         }
     }
 
     Context "ExcludeDatabase is honored" {
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master'
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master'
         It "Should not report about excluded database master" {
             ($results | Where-Object Database -match "master").Count | Should Be 0
         }
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master' -Type Full
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master' -Type Full
         It "Should not report about excluded database master" {
             ($results | Where-Object Database -match "master").Count | Should Be 0
         }
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master' -LastFull
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -ExcludeDatabase 'master' -LastFull
         It "Should not report about excluded database master" {
             ($results | Where-Object Database -match "master").Count | Should Be 0
         }
     }
 
     Context "LastFull should work with multiple databases" {
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -Database $dbname, master -lastfull
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -Database $dbname, master -lastfull
         It "Should return 2 records" {
             $results.count | Should Be 2
         }
     }
 
     Context "Testing IncludeCopyOnly with LastFull" {
-        $results = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -LastFull -Database $dbname
-        $resultsCo = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -LastFull -IncludeCopyOnly -Database $dbname
+        $results = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -LastFull -Database $dbname
+        $resultsCo = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -LastFull -IncludeCopyOnly -Database $dbname
         It "Should return the CopyOnly Backup" {
             ($resultsCo.BackupSetID -ne $Results.BackupSetID) | Should Be $True
         }
     }
 
     Context "Testing IncludeCopyOnly with Last" {
-        $resultsCo = Get-DbaDbBackupHistory -SqlInstance $script:instance1 -Last -IncludeCopyOnly -Database $dbname
+        $resultsCo = Get-DbaAgBackupHistory -SqlInstance $script:instance1 -Last -IncludeCopyOnly -Database $dbname
         It "Should return just the CopyOnly Full Backup" {
             ($resultsCo | Measure-Object).count | Should Be 1
         }
@@ -109,35 +109,4 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         }
     }
 
-    Context "Testing LastFull regression test for #6730" {
-        # skipping until niph addresses this
-        It -Skip "gathers the last full even in a forked scenario" {
-            $dbname = $dbnameForked
-            $database = $server.Databases[$dbname]
-
-            $database.ExecuteNonQuery("CREATE TABLE dbo.test (x char(1000) default 'x')")
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-            1 .. 100 | ForEach-Object -Process { $database.ExecuteNonQuery("INSERT INTO dbo.test DEFAULT VALUES") }
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-            1 .. 1000 | ForEach-Object -Process { $database.ExecuteNonQuery("INSERT INTO dbo.test DEFAULT VALUES") }
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-            1 .. 1000 | ForEach-Object -Process { $database.ExecuteNonQuery("INSERT INTO dbo.test DEFAULT VALUES") }
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-            1 .. 1000 | ForEach-Object -Process { $database.ExecuteNonQuery("INSERT INTO dbo.test DEFAULT VALUES") }
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-
-            $interResults = Get-DbaDbBackupHistory -SqlInstance $server -Database $dbname | Sort-Object -Property End
-            # create a fork restoring from the second backup sorted by date
-            $null = $interResults[1] | Restore-DbaDatabase -SqlInstance $server -WithReplace
-            $null = Backup-DbaDatabase -SqlInstance $server -Database $dbname -Type Full -BackupDirectory $DestBackupDir
-
-            $allHistory = Get-DbaDbBackupHistory -SqlInstance $server -Database $dbname | Sort-Object -Property End -Descending
-            $lastFull = Get-DbaDbBackupHistory -SqlInstance $server -Database $dbname -LastFull
-
-            $allHistory[0].End | Should -Be $lastFull.End
-            $allHistory[0].LastRecoveryForkGUID | Should -Be $lastFull.LastRecoveryForkGUID
-            $allHistory[0].FirstLsn | Should -Be $lastFull.FirstLsn
-
-        }
-    }
 }
