cd8770142f6c0c415f48fdd7ec42dff87edd2ca2
nmbell <23632110+nmbell@users.noreply.github.com>
2020-09-15 21:53:59 -0700

    Find-DbaOrphanedFile: Update tests
    Update tests to exercise -Path and -Recursive parameter functionality

--- tests/Find-DbaOrphanedFile.Tests.ps1
+++ tests/Find-DbaOrphanedFile.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Path', 'FileType', 'LocalOnly', 'RemoteOnly', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Path', 'FileType', 'LocalOnly', 'RemoteOnly', 'EnableException', 'Recurse'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -26,12 +26,19 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
                 }
                 throw "has failed setup"
             }
+			$guid = (New-Guid).Guid
+			$userdir = New-Item -Path "$HOME\$guid" -ItemType Container
+			New-Item -Path $userdir -ItemType File -Name 'file1.txt' | Out-Null
+			New-Item -Path "$userdir\subdir" -ItemType Container | Out-Null
+			New-Item -Path "$userdir\subdir" -ItemType File -Name 'file2.txt' | Out-Null
         }
         AfterAll {
             Get-DbaDatabase -SqlInstance $script:instance2 -Database $dbname | Remove-DbaDatabase -Confirm:$false
         }
         $null = Detach-DbaDatabase -SqlInstance $script:instance2 -Database $dbname -Force
         $results = Find-DbaOrphanedFile -SqlInstance $script:instance2
+        $userpathresults = Find-DbaOrphanedFile -SqlInstance $script:instance2 -Path $userdir -FileType 'txt'
+        $recurseresults  = Find-DbaOrphanedFile -SqlInstance $script:instance2 -Path $userdir -FileType 'txt' -Recurse
 
         It "Has the correct default properties" {
             $ExpectedStdProps = 'ComputerName,InstanceName,SqlInstance,Filename,RemoteFilename'.Split(',')
@@ -46,11 +53,24 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             $results.Count | Should Be 2
         }
 
+        It "Finds 3 files: including $userdir" {
+            $userpathresults.Count | Should Be 3
+        }
+
+        It "Finds 4 files: recursing $userdir" {
+            $recurseresults.Count | Should Be 4
+        }
+
         $results.FileName | Remove-Item
+		$userdir | Remove-Item -Recurse
 
         $results = Find-DbaOrphanedFile -SqlInstance $script:instance2
+        $userpathresults = Find-DbaOrphanedFile -SqlInstance $script:instance2 -Path $userdir -FileType 'txt'
+        $recurseresults  = Find-DbaOrphanedFile -SqlInstance $script:instance2 -Path $userdir -FileType 'txt' -Recurse
         It "Finds zero files after cleaning up" {
             $results.Count | Should Be 0
+            $userpathresults.Count | Should Be 0
+            $recurseresults.Count | Should Be 0
         }
     }
 }
