41fa93e96d2c38d76abd23f486d39e8264225eca
andreasjordan <anj@ordix.de>
2020-09-19 20:30:13 +0200

    Added parameter and debug messages

--- functions/Set-DbaTcpPort.ps1
+++ functions/Set-DbaTcpPort.ps1
@@ -10,16 +10,19 @@ function Set-DbaTcpPort {
         The target SQL Server instance or instances.
 
     .PARAMETER SqlCredential
-        Credential object used to connect to the SQL Server instance as a different user
+        Credential object used to connect to the SQL Server instance as a different user.
 
     .PARAMETER Credential
-        Credential object used to connect to the Windows server itself as a different user (like SQL Configuration Manager)
+        Credential object used to connect to the Windows server itself as a different user (like SQL Configuration Manager).
 
     .PARAMETER Port
         TCPPort that SQLService should listen on.
 
     .PARAMETER IpAddress
-        Which IpAddress should the portchange , if omitted allip (0.0.0.0) will be changed with the new port number.
+        Which IpAddress should the portchange , if omitted AllIP (0.0.0.0) will be changed with the new port number.
+
+    .PARAMETER Restart
+        The Database Engine begins listening on a new port when restarted. Use this switch to restart the service, so that the new settings become effective.
 
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
@@ -48,6 +51,11 @@ function Set-DbaTcpPort {
         Sets the port number 1433 for all IP Addresses on the default instance on sql2017. Prompts for confirmation.
 
     .EXAMPLE
+        PS C:\> Set-DbaTcpPort -SqlInstance sql2017 -Port 1433 -Restart
+
+        Sets the port number 1433 for all IP Addresses on the default instance on sql2017. Prompts for confirmation. Restarts the service.
+
+    .EXAMPLE
         PS C:\> Set-DbaTcpPort -SqlInstance winserver\sqlexpress -IpAddress 192.168.1.22 -Port 1433 -Confirm:$false
 
         Sets the port number 1433 for IP 192.168.1.22 on the sqlexpress instance on winserver. Does not prompt for confirmation.
@@ -67,6 +75,7 @@ function Set-DbaTcpPort {
         [ValidateRange(1, 65535)]
         [int]$Port,
         [IpAddress[]]$IpAddress,
+        [switch]$Restart,
         [switch]$EnableException
     )
 
@@ -137,12 +146,30 @@ function Set-DbaTcpPort {
                 try {
                     $computerName = $instance.ComputerName
                     $resolved = Resolve-DbaNetworkName -ComputerName $computerName
+                    Write-Message -Level Debug -Message "Trying Invoke-ManagedComputerCommand with ComputerName = '$($resolved.FullComputerName)'"
                     Invoke-ManagedComputerCommand -ComputerName $resolved.FullComputerName -ScriptBlock $scriptblock -ArgumentList $instance.ComputerName, $wmiInstanceName, $port, $IpAddress, $instance.InputObject -Credential $Credential
                 } catch {
                     try {
+                        Write-Message -Level Debug -Message "Fallback: Trying Invoke-ManagedComputerCommand with ComputerName = '$computerName'"
                         Invoke-ManagedComputerCommand -ComputerName $instance.ComputerName -ScriptBlock $scriptblock -ArgumentList $instance.ComputerName, $wmiInstanceName, $port, $IpAddress, $instance.InputObject -Credential $Credential
                     } catch {
                         Stop-Function -Message "Failure setting port to $Port for $wmiInstanceName on $computerName" -Continue
+                    }
+                }
+            }
+
+            if ($Restart) {
+                if ($Pscmdlet.ShouldProcess($computerName, "Restarting service for $wmiInstanceName")) {
+                    try {
+                        Write-Message -Level Debug -Message "Trying Restart-DbaService with ComputerName = '$($resolved.FullComputerName)'"
+                        Restart-DbaService -ComputerName $resolved.FullComputerName -InstanceName $wmiInstanceName -Type Engine -Force -Credential $Credential
+                    } catch {
+                        try {
+                            Write-Message -Level Debug -Message "Fallback: Trying Restart-DbaService with ComputerName = '$computerName'"
+                            Restart-DbaService -ComputerName $computerName -InstanceName $wmiInstanceName -Type Engine -Force -Credential $Credential
+                        } catch {
+                            Stop-Function -Message "Failure restarting service for $wmiInstanceName on $computerName" -Continue
+                        }
                     }
                 }
             }
--- tests/Set-DbaTcpPort.Tests.ps1
+++ tests/Set-DbaTcpPort.Tests.ps1
@@ -4,11 +4,11 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance', 'Credential', 'Port', 'IpAddress', 'EnableException'
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'SqlInstance', 'Credential', 'Port', 'IpAddress', 'Restart', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
