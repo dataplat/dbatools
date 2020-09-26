ea3f3699d2ee9ab24bf69523d5195ef57c42aac4
andreasjordan <anj@ordix.de>
2020-09-26 17:25:35 +0200

    Added Error message

--- functions/Invoke-DbaAdvancedInstall.ps1
+++ functions/Invoke-DbaAdvancedInstall.ps1
@@ -293,9 +293,9 @@ function Invoke-DbaAdvancedInstall {
     if ($Port) {
         $null = Set-DbaTcpPort -SqlInstance "$($ComputerName)\$($InstanceName)" -Credential $Credential -Port $Port -EnableException:$EnableException -Confirm:$false
         try {
-            $null = Restart-DbaService -ComputerName $computerName -InstanceName $InstanceName -Credential $Credential -Type Engine -Force -EnableException:$EnableException -Confirm:$false
+            $null = Restart-DbaService -ComputerName $ComputerName -InstanceName $InstanceName -Credential $Credential -Type Engine -Force -EnableException:$EnableException -Confirm:$false
         } catch {
-            $output.Notes += "Port for $($ComputerName)\$($InstanceName) has been changed, but instance restart failed. Restart of instance is necessary for the new settings to become effective."
+            $output.Notes += "Port for $($ComputerName)\$($InstanceName) has been changed, but instance restart failed ($_). Restart of instance is necessary for the new settings to become effective."
         }
 
     }
