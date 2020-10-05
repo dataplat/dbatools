c62c0a9f44ca706f1682344c07e6b4613b19418f
sqlstad <sanderstad@gmail.com>
2020-10-05 12:13:07 +0200

    Added parameter tests

--- tests/Convert-DbaMaskingValue.Tests.ps1
+++ tests/Convert-DbaMaskingValue.Tests.ps1
@@ -2,7 +2,7 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-<# Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'Value', 'DataType', 'Nullable'
@@ -12,7 +12,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
-} #>
+}
 
 Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 
