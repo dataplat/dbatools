0259ab1ed5d289e25284cda25c1c6f9138e16aa1
Jess Pomfret <jpomfret7@gmail.com>
2020-10-28 13:43:23 +0000

    add integration test (#6943)

--- tests/Get-DbaPbmCondition.Tests.ps1
+++ tests/Get-DbaPbmCondition.Tests.ps1
@@ -17,3 +17,55 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
     for more guidence.
 #>
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    BeforeAll {
+        $conditionName = "dbatoolsCondition_$(get-random)"
+        $conditionQuery = "
+            Declare @condition_id int
+            EXEC msdb.dbo.sp_syspolicy_add_condition @name=N'$conditionName', @description=N'', @facet=N'Database', @expression=N'<Operator>
+            <TypeClass>Bool</TypeClass>
+            <OpType>EQ</OpType>
+            <Count>2</Count>
+            <Attribute>
+                <TypeClass>String</TypeClass>
+                <Name>Name</Name>
+            </Attribute>
+            <Constant>
+                <TypeClass>String</TypeClass>
+                <ObjType>System.String</ObjType>
+                <Value>test</Value>
+            </Constant>
+            </Operator>', @is_name_condition=1, @obj_name=N'test', @condition_id=@condition_id OUTPUT
+            Select @condition_id as conditionId"
+
+        $server = Connect-DbaInstance -SqlInstance $script:instance2
+        $conditionId = $server.Query($conditionQuery) | Select-Object -expand conditionId
+    }
+    AfterAll {
+        $dropQuery = "EXEC msdb.dbo.sp_syspolicy_delete_condition @condition_id=$conditionId"
+        $null = $server.Query($dropQuery)
+    }
+
+    Context "Command returns results" {
+        $results = Get-DbaPbmCondition -SqlInstance $script:instance2
+        It "Should get results" {
+            $results | Should Not Be $null
+        }
+
+        It "Should have name property '$conditionName'" {
+            $results.Name | Should Be $conditionName
+        }
+    }
+
+    Context "Command actually works by condition name" {
+        $results = Get-DbaPbmCondition -SqlInstance $script:instance2 -Condition $conditionName
+        It "Should get results" {
+            $results | Should Not Be $null
+        }
+
+        It "Should have name property '$conditionName'" {
+            $results.Name | Should Be $conditionName
+        }
+    }
+}
