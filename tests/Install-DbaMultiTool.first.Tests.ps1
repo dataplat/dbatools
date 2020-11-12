458e6623541242ee1f0c66154821ec85f4de4ac4
John McCall <John@lowlydba.com>
2020-11-11 20:13:36 -0500

    split test files out per instance type
    do Install-DbaMultiTool

--- tests/Install-DbaMultiTool.first.Tests.ps1
+++ tests/Install-DbaMultiTool.first.Tests.ps1
@@ -1,4 +1,4 @@
-$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".first.Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
@@ -56,7 +56,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         BeforeAll {
             $branch = "master"
             $database = "dbatoolsci_multitool_$(Get-Random)"
-            $server = Connect-DbaInstance -SqlInstance $script:instance3
+            $server = Connect-DbaInstance -SqlInstance $script:instance2
             $server.Query("CREATE DATABASE $database;")
 
             $outfile = "DBA-MultiTool-$branch.zip"
@@ -94,36 +94,6 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             Add-Content $sqlScript (New-Guid).ToString()
             $result = Install-DbaMultiTool -SqlInstance $script:instance3 -Database $database
             $result[0].Status -eq "Error" | Should -Be $true
-        }
-    }
-    Context "Testing DBA MultiTool installer with unsupported SQL Server version" {
-        BeforeAll {
-            $branch = "master"
-            $database = "dbatoolsci_multitool_$(Get-Random)"
-            $server = Connect-DbaInstance -SqlInstance $script:instance1
-            $server.Query("CREATE DATABASE $database;")
-
-            $outfile = "DBA-MultiTool-$branch.zip"
-            Invoke-WebRequest -Uri "https://github.com/LowlyDBA/dba-multitool/archive/$branch.zip" -OutFile $outfile
-            if (Test-Path $outfile) {
-                $fullOutfile = (Get-ChildItem $outfile).FullName
-            }
-            $resultsLocalFile = Install-DbaMultiTool -SqlInstance $script:instance1 -Database $database -Branch $branch -LocalFile $fullOutfile -Force
-        }
-        AfterAll {
-            Remove-DbaDatabase -SqlInstance $script:instance1 -Database $database -Confirm:$false
-        }
-
-        It "Installs to specified database: $database" {
-            $resultsLocalFile[0].Database -eq $database | Should -Be $true
-        }
-        It "Shows status of Skipped" {
-            $resultsLocalFile[0].Status -eq "Skipped" | Should -Be $true
-        }
-        It "Has the correct properties" {
-            $result = $resultsLocalFile[0]
-            $ExpectedProps = 'SqlInstance,InstanceName,ComputerName,Name,Status,Database'.Split(',')
-            ($result.PsObject.Properties.Name | Sort-Object) | Should -Be ($ExpectedProps | Sort-Object)
         }
     }
 }
