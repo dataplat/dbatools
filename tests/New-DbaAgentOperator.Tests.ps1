831998120f906ae87a65265fa2949780226a0631
Tracy Boggiano <tracy@tracyboggiano.com>
2020-11-15 05:24:57 -0500

    Fix tests

--- tests/New-DbaAgentOperator.Tests.ps1
+++ tests/New-DbaAgentOperator.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'OperatorName', 'EmailAddress', 'NetSendAddress', 'PagerAddress', 'PagerDay', 'SaturdayStartTime', 'SaturdayEndTime', 'SundayStartTime', 'SundayEndTime', 'WeekendStartTime', 'WeekendEndTime', 'IsFailsafeOperator', 'FailsafeNotificationMethod', 'WhatIf', 'Force', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'OperatorName', 'EmailAddress', 'NetSendAddress', 'PagerAddress', 'PagerDay', 'SaturdayStartTime', 'SaturdayEndTime', 'SundayStartTime', 'SundayEndTime', 'WeekendStartTime', 'WeekendEndTime', 'IsFailsafeOperator', 'FailsafeNotificationMethod', 'Force', 'EnableException'
         It "Should only contain our specific parameters" {
             Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
@@ -16,7 +16,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "New Agent Operator is added properly" {
 
         It "Should have the right name" {
-            $results = New-DbaAgentOperator -SqlInstance $script:instance2 -Operator DBA -EmailAddress operator@operator.com -PagerDays Everyday
+            $results = New-DbaAgentOperator -SqlInstance $script:instance2 -Operator DBA -EmailAddress operator@operator.com -PagerDay Everyday
             $results.Name | Should Be "DBA"
         }
 
