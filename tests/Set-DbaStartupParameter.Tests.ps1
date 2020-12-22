9296be599e53f21d1355bac8ad59373cb600915f
lancasteradam <68036988+lancasteradam@users.noreply.github.com>
2020-12-22 10:33:54 -0800

    Set-DbaStartupParameter - minor changes for multiple instances and named instances
    - Initialized the $parameterString at the top of the loop since it is appended to.
    - Modified the SqlInstance param that is passed into the Get-DbaStartupParameter.
    - Added integration tests for the two issues that were reported.

--- tests/Set-DbaStartupParameter.Tests.ps1
+++ tests/Set-DbaStartupParameter.Tests.ps1
@@ -4,11 +4,10 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Credential', 'MasterData', 'MasterLog', 'ErrorLog', 'TraceFlag', 'CommandPromptStart', 'MinimalStart', 'MemoryToReserve', 'SingleUser', 'SingleUserDetails', 'NoLoggingToWinEvents', 'StartAsNamedInstance', 'DisableMonitoring', 'IncreasedExtents', 'TraceFlagOverride', 'StartUpConfig', 'Offline', 'Force', 'EnableException'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
 }
@@ -17,3 +16,41 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Read https://github.com/sqlcollaborative/dbatools/blob/development/contributing.md#tests
     for more guidence.
 #>
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+
+    BeforeAll {
+        $defaultInstance = $script:instance1
+        $namedInstance = $script:instance2
+    }
+
+    Context "Bug 7035" {
+        # Adding the -Skip here to ensure the AppVeyor instance isn't modified. This test can be run locally.
+        It -Skip "Ensure the startup params are not duplicated when more than one server is modified in the same invocation" {
+            $result = Set-DbaStartupParameter -SqlInstance $defaultInstance, $namedInstance -TraceFlag 3226 -Confirm:$false
+
+            $resultDefaultInstance = Get-DbaStartupParameter -SqlInstance $defaultInstance
+            $resultDefaultInstance.TraceFlags.Count | Should -Be 1
+            $resultDefaultInstance.TraceFlags[0] | Should -Be 3226
+
+            # The duplication occurs after the first server is processed.
+            $resultNamedInstance = Get-DbaStartupParameter -SqlInstance $namedInstance
+            # Using the defaults to test locally
+            $resultNamedInstance.MasterData.Count | Should -Be 1
+            $resultNamedInstance.MasterLog.Count | Should -Be 1
+            $resultNamedInstance.ErrorLog.Count | Should -Be 1
+
+            $resultNamedInstance.TraceFlags.Count | Should -Be 1
+            $resultNamedInstance.TraceFlags[0] | Should -Be 3226
+        }
+
+        # Adding the -Skip here to ensure the AppVeyor instance isn't modified. This test can be run locally.
+        It -Skip "Ensure the correct instance name is returned" {
+            $result = Set-DbaStartupParameter -SqlInstance $namedInstance -TraceFlag 3226 -Confirm:$false
+
+            $result.SqlInstance | Should -Be $namedInstance
+            $result.TraceFlags.Count | Should -Be 1
+            $result.TraceFlags[0] | Should -Be 3226
+        }
+    }
+}
