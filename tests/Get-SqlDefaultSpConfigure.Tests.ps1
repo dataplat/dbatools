141ca0baf24bdfd356d9d16850ed0a382ae2b7ca
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-03 11:17:35 -0600

    internal function test fix Get-SqlDefaultSpConfigure tests

--- tests/Get-SqlDefaultSpConfigure.Tests.ps1
+++ tests/Get-SqlDefaultSpConfigure.Tests.ps1
@@ -1,35 +1,38 @@
 $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
-. "$PSScriptRoot\src\internal\functions\Get-SqlDefaultSPConfigure.ps1"
+
+# import internal function
+. ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'src\internal\functions', 'constants.ps1'))
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlVersion'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
+        [array]$knownParameters = 'SqlVersion'
+
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
 }
 
 Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
     Context "Try all versions of SQL" {
-        $versionName = @{8  = "2000"
-                        9  = "2005"
-                        10 = "2008/2008R2"
-                        11 = "2012"
-                        12 = "2014"
-                        13 = "2016"
-                        14 = "2017"
-                        15 = "2019"}
+        $versionName = @{8 = "2000"
+            9              = "2005"
+            10             = "2008/2008R2"
+            11             = "2012"
+            12             = "2014"
+            13             = "2016"
+            14             = "2017"
+            15             = "2019"
+        }
 
-        foreach ($version in 8..14){
+        foreach ($version in 8..14) {
             $results = Get-SqlDefaultSPConfigure -SqlVersion $version
 
             It "Should return results for $($versionName.item($version))" {
-                $results | Should  Not BeNullOrEmpty
+                $results | Should Not BeNullOrEmpty
             }
 
             It "Should return 'System.Management.Automation.PSCustomObject' object" {
