904619161f19cc9a0f9a902d78cbcc004b5f97e7
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-15 20:34:09 -0600

    add move-dbadbfile to psm1

--- dbatools.psm1
+++ dbatools.psm1
@@ -797,7 +797,8 @@ $script:noncoresmo = @(
     'Get-DbaRepPublication',
     'Test-DbaRepLatency',
     'Export-DbaRepServerSetting',
-    'Get-DbaRepServer'
+    'Get-DbaRepServer',
+    'Move-DbaDbFile'
 )
 $script:windowsonly = @(
     # solvable filesystem issues or other workarounds
