26b02ba1435c0abd9f3db37874a3424e0e3346ec
Adam Lancaster <68036988+lancasteradam@users.noreply.github.com>
2021-01-18 13:26:08 -0800

    Get-DbaLogin, Set-DbaLogin, Connect-SqlInstance - block use with Azure via AzureUnsupported param (#7087)

--- tests/Set-DbaLogin.Tests.ps1
+++ tests/Set-DbaLogin.Tests.ps1
@@ -58,141 +58,134 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 }
 
 Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
-    Context "Change login" {
+    Context "verify command functions" {
         BeforeAll {
+            $random = Get-Random
+
             # Create the new password
-            $password1 = ConvertTo-SecureString -String "password1" -AsPlainText -Force
-            $password2 = ConvertTo-SecureString -String "password2" -AsPlainText -Force
+            $password1 = ConvertTo-SecureString -String "password1A@" -AsPlainText -Force
+            $password2 = ConvertTo-SecureString -String "password2A@" -AsPlainText -Force
 
             # Create the login
-            New-DbaLogin -SqlInstance $script:instance2 -Login testlogin -Password $password1
+            New-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random", "testlogin2_$random" -Password $password1
+
+            New-DbaDatabase -SqlInstance $script:instance2 -Name "testdb1_$random" -Confirm:$false
+        }
+
+        AfterAll {
+            Remove-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random", "testlogin2_$random" -Confirm:$false -Force
+            Remove-DbaDatabase -SqlInstance $script:instance2 -Database "testdb1_$random" -Confirm:$false
         }
 
         It "Does test login exist" {
-            $logins = Get-DbaLogin -SqlInstance $script:instance2 | Where-Object { $_.Name -eq "testlogin" } | Select-Object Name
-            $logins.Name | Should -Be "testlogin"
+            $logins = Get-DbaLogin -SqlInstance $script:instance2 | Where-Object { $_.Name -eq "testlogin1_$random" } | Select-Object Name
+            $logins.Name | Should -Be "testlogin1_$random"
         }
 
         It "Verifies -NewName doesn't already exist when renaming a login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login 'testLogin' -NewName 'sa' -EnableException
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -NewName 'sa' -EnableException
 
             $result.Notes | Should -Be 'New login name already exists'
         }
 
         It 'Change the password from a SecureString' {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -Password $password2
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Password $password2
 
             $result.PasswordChanged | Should -Be $true
         }
 
         It 'Changes the password from a PSCredential' {
-            $cred = New-Object System.Management.Automation.PSCredential ('testLogin', $password2)
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login 'testLogin' -Password $cred
+            $cred = New-Object System.Management.Automation.PSCredential ("testlogin1_$random", $password2)
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Password $cred
 
             $result.PasswordChanged | Should -Be $true
         }
 
         It "Change the password from piped Login" {
-            $login = Get-DbaLogin -Sqlinstance $script:instance2 -Login testLogin
+            $login = Get-DbaLogin -Sqlinstance $script:instance2 -Login "testlogin1_$random"
 
             $result = $login | Set-DbaLogin -Password $password2
             $result.PasswordChanged | Should -Be $true
         }
 
         It "Change the password from InputObject" {
-            $login = Get-DbaLogin -Sqlinstance $script:instance2 -Login testLogin
+            $login = Get-DbaLogin -Sqlinstance $script:instance2 -Login "testlogin1_$random"
 
             $result = Set-DbaLogin -InputObject $login -Password $password2
             $result.PasswordChanged | Should -Be $true
         }
 
         It "Disable the login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -Disable
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Disable
             $result.IsDisabled | Should -Be $true
         }
 
         It "Enable the login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -Enable
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Enable
             $result.IsDisabled | Should -Be $false
         }
 
         It "Deny access to login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -DenyLogin
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -DenyLogin
 
             $result.DenyLogin | Should -Be $true
         }
 
         It "Grant access to login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -GrantLogin
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -GrantLogin
 
             $result.DenyLogin | Should -Be $false
         }
 
         It "Enforces password policy on login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -PasswordPolicyEnforced
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -PasswordPolicyEnforced
 
             $result.PasswordPolicyEnforced | Should Be $true
         }
 
         It "Catches errors when password can't be changed" {
             # enforce password policy
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login 'testLogin' -PasswordPolicyEnforced -EnableException
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -PasswordPolicyEnforced -EnableException
             $result.PasswordPolicyEnforced | Should -Be $true
 
             # violate policy
             $invalidPassword = ConvertTo-SecureString -String "password1" -AsPlainText -Force
 
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login 'testLogin' -Password $invalidPassword -WarningAction 'SilentlyContinue'
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Password $invalidPassword -WarningAction 'SilentlyContinue'
             $result | Should -Be $null
 
-            { Set-DbaLogin -SqlInstance $script:instance2 -Login 'testLogin' -Password $invalidPassword -EnableException } | Should -Throw
+            { Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -Password $invalidPassword -EnableException } | Should -Throw
         }
 
         It "Disables enforcing password policy on login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -PasswordPolicyEnforced:$false
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -PasswordPolicyEnforced:$false
 
             $result.PasswordPolicyEnforced | Should Be $false
         }
 
         It "Add roles to login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -AddRole serveradmin, processadmin
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -AddRole serveradmin, processadmin
 
             $result.ServerRole | Should -Be "processadmin, serveradmin"
         }
 
         It "Remove roles from login" {
-            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin -RemoveRole serveradmin
+            $result = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -RemoveRole serveradmin
 
             $result.ServerRole | Should -Be "processadmin"
         }
 
-        AfterAll {
-            Remove-DbaLogin -SqlInstance $script:instance2 -Login testlogin,testlogin1,testlogin2 -Confirm:$false -Force
-        }
-    }
-    Context "Can handle multiple Logins" {
-        BeforeAll {
-            # Create the new password
-            $password = ConvertTo-SecureString -String "password1" -AsPlainText -Force
-
-            # Create two logins
-            New-DbaLogin -SqlInstance $script:instance2 -Login testlogin1, testlogin2 -Password $password
-        }
-        $results = Set-DbaLogin -SqlInstance $script:instance2 -Login testlogin1, testlogin2 -DenyLogin
-
-        It "Results include multiple objects" {
+        It "Results include multiple changed objects" {
+            $results = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random", "testlogin2_$random" -DenyLogin
             $results.Count | Should -Be 2
-        }
-        if ($results.Count -eq 2) {
-            It "Applies change to multiple logins" {
-                foreach ($r in $results) {
-                    $r.DenyLogin | Should -Be $true
-                }
+            foreach ($r in $results) {
+                $r.DenyLogin | Should -Be $true
             }
         }
 
-        AfterAll {
-            Remove-DbaLogin -SqlInstance $script:instance2 -Login testlogin1,testlogin2 -Confirm:$false -Force
+        It "DefaultDatabase" {
+            $results = Set-DbaLogin -SqlInstance $script:instance2 -Login "testlogin1_$random" -DefaultDatabase "testdb1_$random"
+            $results.DefaultDatabase | Should -Be "testdb1_$random"
         }
     }
 }
