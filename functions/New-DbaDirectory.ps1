a43724e4da29e20a095070e51a8dc7f84daaaa14
GLoeffler14 <59977920+GLoeffler14@users.noreply.github.com>
2021-02-23 08:29:10 -0500

    Update New-DbaDirectory.ps1

--- functions/New-DbaDirectory.ps1
+++ functions/New-DbaDirectory.ps1
@@ -76,7 +76,7 @@ function New-DbaDirectory {
 
         $Path = $Path.Replace("'", "''")
 
-        $exists = Test-DbaPath -SqlInstance $Server -SqlCredential $SqlCredential -Path $Path
+        $exists = Test-DbaPath -SqlInstance $server -SqlCredential $SqlCredential -Path $Path
 
         if ($exists) {
             Stop-Function -Message "$Path already exists" -Target $server -Continue
