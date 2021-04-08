d031278799edafbc50a6af97ba6234ec18f8526e
Ant Green <antg1985@hotmail.co.uk>
2021-04-08 10:04:06 +0100

    Further tweaks for AppVeyor
    niphlod advised to move the param up one so exception is the last param.
    
    also added the param to the test, my bad.
    
    fingers crossed this is right now

--- functions/Install-DbaMaintenanceSolution.ps1
+++ functions/Install-DbaMaintenanceSolution.ps1
@@ -53,13 +53,13 @@ function Install-DbaMaintenanceSolution {
     .PARAMETER Confirm
         If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
 
+    .PARAMETER InstallParallel
+        If this switch is enabled, the Queue and QueueDatabase tables are created.
+        
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-
-    .PARAMETER InstallParallel
-        If this switch is enabled, the Queue and QueueDatabase tables are created.
 
     .NOTES
         Tags: Community, OlaHallengren
@@ -142,8 +142,9 @@ function Install-DbaMaintenanceSolution {
         [switch]$InstallJobs,
         [string]$LocalFile,
         [switch]$Force,
-        [switch]$EnableException,
-        [switch]$InstallParallel
+        [switch]$InstallParallel,
+        [switch]$EnableException
+
     )
     begin {
         if ($Force) { $ConfirmPreference = 'none' }
