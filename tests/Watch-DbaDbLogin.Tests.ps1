5ce0565875982d044db700b951d83fa1fa4f282a
lancasteradam <68036988+lancasteradam@users.noreply.github.com>
2021-04-19 07:34:12 -0700

    Incorporating feedback

--- tests/Watch-DbaDbLogin.Tests.ps1
+++ tests/Watch-DbaDbLogin.Tests.ps1
@@ -14,7 +14,6 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 
 Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
-        $SkipLocalTest = $true # Change to $false to run locally. The Watch-DbaDbLogin excludes data generated from the local machine, so it's necessary to either test with a target instance that has connections from other machines or comment out the clause "| Where-Object { $_.Host -ne $instance.ComputerName -and ![string]::IsNullOrEmpty($_.Host) }" that used after the call to $instance.Query($sql).
         $random = Get-Random
 
         $tableName1 = 'dbatoolsciwatchdblogin1'
@@ -43,21 +42,21 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     }
     Context "Command actually works" {
 
-        It -Skip:$SkipLocalTest "ServersFromFile" {
+        It "ServersFromFile" {
             Watch-DbaDbLogin -SqlInstance $script:instance1 -Database $databaseName -Table $tableName1 -ServersFromFile $testFile -EnableException
             $result = Get-DbaDbTable -SqlInstance $script:instance1 -Database $databaseName -Table $tableName1 -IncludeSystemDBs
             $result.Name | Should Be $tableName1
             $result.Count | Should BeGreaterThan 0
         }
 
-        It -Skip:$SkipLocalTest "Pipeline of instances" {
+        It "Pipeline of instances" {
             $instance1, $instance2 | Watch-DbaDbLogin -SqlInstance $script:instance1 -Database $databaseName -Table $tableName2 -EnableException
             $result = Get-DbaDbTable -SqlInstance $script:instance1 -Database $databaseName -Table $tableName2 -IncludeSystemDBs
             $result.Name | Should Be $tableName2
             $result.Count | Should BeGreaterThan 0
         }
 
-        It -Skip:$SkipLocalTest "ServersFromCMS" {
+        It "ServersFromCMS" {
             Watch-DbaDbLogin -SqlInstance $script:instance1 -Database $databaseName -Table $tableName3 -SqlCms $script:instance1 -EnableException
             $result = Get-DbaDbTable -SqlInstance $script:instance1 -Database $databaseName -Table $tableName3 -IncludeSystemDBs
             $result.Name | Should Be $tableName3
