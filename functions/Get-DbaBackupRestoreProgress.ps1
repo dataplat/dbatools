5e92ea0224aa4fdf138a40df97768551e88b1b50
Mark Boomaars <mboomaars@gmail.com>
2021-05-07 12:13:50 +0200

    Added Elapsed_Hrs column

--- functions/Get-DbaBackupRestoreProgress.ps1
+++ functions/Get-DbaBackupRestoreProgress.ps1
@@ -61,9 +61,10 @@ function Get-DbaBackupRestoreProgress {
                     SERVERPROPERTY('ServerName') AS SqlInstance,
                     r.session_id AS SessionId,
                     r.command AS Command,
-                    CONVERT (NUMERIC(6, 2), r.percent_complete) AS [CompletedPct],
-                    CONVERT (VARCHAR(20), DATEADD (ms, r.estimated_completion_time, GETDATE ()), 20) AS ETA,
+                    CONVERT (NUMERIC(6, 2), r.percent_complete) AS [PercentComplete],
                     CONVERT (NUMERIC(6, 2), r.total_elapsed_time / 1000.0 / 60.0) AS Elapsed_Min,
+                    CONVERT (NUMERIC(6, 2), r.total_elapsed_time / 1000.0 / 60.0 / 60.0) AS Elapsed_Hrs,
+                    CONVERT (VARCHAR(20), DATEADD (ms, r.estimated_completion_time, GETDATE ()), 20) AS ETA,
                     CONVERT (NUMERIC(6, 2), r.estimated_completion_time / 1000.0 / 60.0) AS ETA_Min,
                     CONVERT (NUMERIC(6, 2), r.estimated_completion_time / 1000.0 / 60.0 / 60.0) AS ETA_Hrs,
                     CONVERT (VARCHAR(100), (SELECT SUBSTRING (text, r.statement_start_offset / 2, CASE WHEN r.statement_end_offset = -1 THEN 1000 ELSE (r.statement_end_offset - r.statement_start_offset) / 2 END) FROM sys.dm_exec_sql_text (sql_handle))) AS Stmt
@@ -81,17 +82,18 @@ function Get-DbaBackupRestoreProgress {
 
             foreach ($row in $server.Query($sql)) {
                 [PSCustomObject]@{
-                    ComputerName = $server.NetName
-                    InstanceName = $server.ServiceName
-                    SqlInstance  = $server.DomainInstanceName
-                    SessionId    = $row.SessionId
-                    Command      = $row.Command
-                    CompletedPct = $row.CompletedPct
-                    ETA          = $row.ETA
-                    Elapsed_Min  = $row.Elapsed_Min
-                    ETA_Min      = $row.ETA_Min
-                    ETA_Hrs      = $row.ETA_Hrs
-                    Stmt         = $row.Stmt
+                    ComputerName    = $server.NetName
+                    InstanceName    = $server.ServiceName
+                    SqlInstance     = $server.DomainInstanceName
+                    SessionId       = $row.SessionId
+                    Command         = $row.Command
+                    PercentComplete = $row.PercentComplete
+                    Elapsed_Min     = $row.Elapsed_Min
+                    Elapsed_Hrs     = $row.Elapsed_Hrs
+                    ETA             = $row.ETA
+                    ETA_Min         = $row.ETA_Min
+                    ETA_Hrs         = $row.ETA_Hrs
+                    Stmt            = $row.Stmt
                 }
             }
         }
