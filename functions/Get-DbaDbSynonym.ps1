084a46576d1f5c6002300d4ebc5bbca44b2217f6
MikeyBronowski <mikeybronowski@gmail.com>
2021-05-14 22:07:24 +0100

    WhatIf/Confirm

--- functions/Get-DbaDbSynonym.ps1
+++ functions/Get-DbaDbSynonym.ps1
@@ -37,6 +37,12 @@ function Get-DbaDbSynonym {
     .PARAMETER InputObject
         Enables piped input from Get-DbaDatabase
 
+    .PARAMETER WhatIf
+        Shows what would happen if the command were to run. No actions are actually performed.
+
+    .PARAMETER Confirm
+        Prompts you for confirmation before executing any changing operations within the command.
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
