d455886206ad038a13d353025f996ae2eca7b029
andreasjordan <anj@ordix.de>
2021-05-27 20:28:59 +0200

    Update test

--- tests/Set-DbaNetworkConfiguration.Tests.ps1
+++ tests/Set-DbaNetworkConfiguration.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'Credential', 'EnableProtokoll', 'DisableProtokoll', 'DynamicPortForIPAll', 'StaticPortForIPAll', 'InputObject', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'Credential', 'EnableProtokoll', 'DisableProtokoll', 'DynamicPortForIPAll', 'StaticPortForIPAll', 'RestartService', 'InputObject', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -23,6 +23,10 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         $results = $netConf | Set-DbaNetworkConfiguration
 
         It "Should Return a Result" {
+            $results.ComputerName | Should -Be $netConf.ComputerName
+        }
+
+        It "Should Return a Change" {
             $results.Changes | Should -Match "Changed NamedPipesEnabled to"
         }
 
@@ -43,6 +47,10 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         }
 
         It "Should Return a Result" {
+            $results.ComputerName | Should -Be $netConf.ComputerName
+        }
+
+        It "Should Return a Change" {
             $results.Changes | Should -Match "Changed NamedPipesEnabled to"
         }
 
