db409068f7b968700e7bc8e95bb2261a3a6986bf
andreasjordan <anj@ordix.de>
2021-05-27 16:14:40 +0200

    added parameter help for WhatIf and Confirm

--- functions/Set-DbaNetworkConfiguration.ps1
+++ functions/Set-DbaNetworkConfiguration.ps1
@@ -19,6 +19,12 @@ function Set-DbaNetworkConfiguration {
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
     .NOTES
         Tags: SQLWMI
         Author: Andreas Jordan (@JordanOrdix), ordix.de
