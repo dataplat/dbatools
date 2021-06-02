3c8cd3cd7677a0edbb7c11990085bd5b465cb6d0
Murilo Machado <murilo.machado@minervafoods.com>
2021-06-02 17:28:07 +0000

    Reverting changes to autogenerated file Improving description of commandtimeout parameter

--- functions/Copy-DbaDbTableData.ps1
+++ functions/Copy-DbaDbTableData.ps1
@@ -95,7 +95,7 @@ function Copy-DbaDbTableData {
         Value in seconds for the BulkCopy operations timeout. The default is 5000 seconds.
 
     .PARAMETER CommandTimeOut
-        Value in seconds for the reader command operations timeout. The default is 0 seconds (no timeout).
+        Gets or sets the wait time (in seconds) before terminating the attempt to execute the reader and bulk copy operation. The default is 0 seconds (no timeout).
 
     .PARAMETER InputObject
         Enables piping of Table objects from Get-DbaDbTable
