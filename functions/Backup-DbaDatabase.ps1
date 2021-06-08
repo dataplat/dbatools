678646425a18257fc9400343e6d83bee4556f85e
Oleg Strutinskii <14327077+Zhunya@users.noreply.github.com>
2021-06-08 22:04:10 +0300

    Do it right way

--- functions/Backup-DbaDatabase.ps1
+++ functions/Backup-DbaDatabase.ps1
@@ -440,7 +440,8 @@ function Backup-DbaDatabase {
                 Write-Message -Level Warning -Message "$failreason"
             }
 
-            $lastfull = ($db.Refresh()).LastBackupDate.Year
+            $db.Refresh()
+            $lastfull = $db.LastBackupDate.Year
 
             if ($Type -notin @("Database", "Full") -and $lastfull -eq 1) {
                 $failreason = "$db does not have an existing full backup, cannot take log or differentialbackup"
