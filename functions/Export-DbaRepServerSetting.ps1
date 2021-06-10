eea4f98811f9de93c79430da85a17cfb8215c196
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2021-06-10 09:52:09 +0200

    Document commands as unsupported (#7429)

--- functions/Export-DbaRepServerSetting.ps1
+++ functions/Export-DbaRepServerSetting.ps1
@@ -6,6 +6,9 @@ function Export-DbaRepServerSetting {
     .DESCRIPTION
         Exports replication server settings to file.
 
+        All replication commands need SSMS 17 installed and are therefore currently not supported.
+        Have a look at this issue to get more information: https://github.com/sqlcollaborative/dbatools/issues/7428
+
     .PARAMETER SqlInstance
         The target SQL Server instance or instances
 
@@ -103,6 +106,7 @@ function Export-DbaRepServerSetting {
             $rmodll = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Rmo")
 
             if ($null -eq $repdll -or $null -eq $rmodll) {
+                Write-Message -Level Warning -Message 'All replication commands need SSMS 17 installed and are therefore currently not supported.'
                 Stop-Function -Message "Could not load replication libraries" -ErrorRecord $_
                 return
             }
