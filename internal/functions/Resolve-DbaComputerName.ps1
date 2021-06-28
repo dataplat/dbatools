d3718f6231346773da1b474838bc1d21686035dd
andreasjordan <anj@ordix.de>
2021-06-28 17:34:17 +0200

    New command Resolve-DbaComputerName

---
+++ internal/functions/Resolve-DbaComputerName.ps1
@@ -0,0 +1,34 @@
+function Resolve-DbaComputerName {
+    <#
+    .SYNOPSIS
+        Resolves the computer based on the ComputerName of the input DbaInstanceParameter.
+    .DESCRIPTION
+        Resolves the computer based on the ComputerName of the input DbaInstanceParameter.
+        Designed to get the FullComputerName needed for Invoke-Command2.
+        Default is to return the FullComputerName, but can also return other properties returned by Resolve-DbaNetworkName.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [DbaInstanceParameter]$ComputerName,
+        [PSCredential]$Credential,
+        [ValidateSet('ComputerName', 'IPAddress', 'DNSHostname', 'DNSDomain', 'Domain', 'DNSHostEntry', 'FQDN', 'FullComputerName')]
+        [string]$Property = 'FullComputerName'
+    )
+
+    try {
+        $resolved = Resolve-DbaNetworkName -ComputerName $ComputerName -Credential $Credential -EnableException
+        $output = $resolved.$Property
+    } catch {
+        Write-Message -Level Debug -Message "First approach to resolve '$ComputerName' failed" -ErrorRecord $_
+        try {
+            $resolved = Resolve-DbaNetworkName -ComputerName $ComputerName -Credential $Credential -Turbo -EnableException
+            $output = $resolved.$Property
+        } catch {
+            Write-Message -Level Debug -Message "Second approach to resolve '$ComputerName' failed" -ErrorRecord $_
+            $output = $ComputerName.ComputerName
+        }
+    }
+    Write-Message -Level Verbose -Message "Resolved '$ComputerName' to '$output'"
+    $output
+}
