cb59c21694d4d33b88133d2c9de963f8c4d2502c
potatoqualitee <clemaire@gmail.com>
2021-06-29 17:35:32 +0000

    update version

--- bin/diagnosticquery/SQLServerDiagnosticQueries_2016SP2.sql
+++ bin/diagnosticquery/SQLServerDiagnosticQueries_2016SP2.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2016 SP2 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: June 22, 2021
+-- Last Modified: June 25, 2021
 -- https://glennsqlperformance.com/
 -- https://sqlserverperformance.wordpress.com/
 -- YouTube: https://bit.ly/2PkoAM1 
