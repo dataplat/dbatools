381c12b42262b2b6df2d4b3aa42f127e55ab3da1
andreasjordan <anj@ordix.de>
2021-07-01 21:29:25 +0200

    fixed test

--- tests/Get-ObjectNameParts.Tests.ps1
+++ tests/Get-ObjectNameParts.Tests.ps1
@@ -7,7 +7,6 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'ObjectName'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
@@ -17,10 +16,10 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
     Context "Test one part names" {
         It "Should return correct parts" {
-            $input = 'table1', '[table2]', '[tab..le3]', '[table]]x4]', '[table5]]]'
+            $objectName = 'table1', '[table2]', '[tab..le3]', '[table]]x4]', '[table5]]]'
             $table = 'table1', 'table2', 'tab..le3', 'table]]x4', 'table5]]'
             for ($i = 0; $i -lt $input.Count; $i++) {
-                $result = Get-ObjectNameParts -ObjectName $input[$i]
+                $result = Get-ObjectNameParts -ObjectName $objectName[$i]
                 $result.Parsed | Should Be $true
                 $result.Database | Should Be $null
                 $result.Schema | Should Be $null
@@ -30,11 +29,11 @@ Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
     }
     Context "Test two part names" {
         It "Should return correct parts" {
-            $input = 'schema1.table1', '[sche..ma2].[table2]', 'schema3.[tab..le3]', '[schema4].[table]]x4]', 'schema5.[table5]]]'
+            $objectName = 'schema1.table1', '[sche..ma2].[table2]', 'schema3.[tab..le3]', '[schema4].[table]]x4]', 'schema5.[table5]]]'
             $table = 'table1', 'table2', 'tab..le3', 'table]]x4', 'table5]]'
             $schema = 'schema1', 'sche..ma2', 'sche ma3', 'schema4', 'schema5'
             for ($i = 0; $i -lt $input.Count; $i++) {
-                $result = Get-ObjectNameParts -ObjectName $input[$i]
+                $result = Get-ObjectNameParts -ObjectName $objectName[$i]
                 $result.Parsed | Should Be $true
                 $result.Database | Should Be $null
                 $result.Schema | Should Be $schema[$i]
@@ -44,12 +43,12 @@ Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
     }
     Context "Test three part names" {
         It "Should return correct parts" {
-            $input = 'database1.schema1.table1', 'database2..table2', 'database3..[tab..le3]', 'db4.[sche..ma4].table4'
+            $objectName = 'database1.schema1.table1', 'database2..table2', 'database3..[tab..le3]', 'db4.[sche..ma4].table4'
             $table = 'table1', 'table2', 'tab..le3', 'table4'
             $schema = 'schema1', $null, $null, 'sche..ma4'
             $database = 'database1', 'database2', 'database3', 'db4'
             for ($i = 0; $i -lt $input.Count; $i++) {
-                $result = Get-ObjectNameParts -ObjectName $input[$i]
+                $result = Get-ObjectNameParts -ObjectName $objectName[$i]
                 $result.Parsed | Should Be $true
                 $result.Database | Should Be $database[$i]
                 $result.Schema | Should Be $schema[$i]
@@ -58,12 +57,12 @@ Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
         }
     }
     Context "Test wrong names" {
-        It "Should not return parts" {
-            $input = 'schema1...table1', 'sche..ma2.table2', 'correct.to.testthetest'
-            for ($i = 0; $i -lt $input.Count; $i++) {
-                $result = Get-ObjectNameParts -ObjectName $input[$i]
-                $result.Parsed | Should Be $false
-            }
+        It "Should not return parts for 'schema1...table1'" {
+            (Get-ObjectNameParts -ObjectName 'schema1...table1').Parsed | Should Be $false
+        }
+
+        It "Should not return parts for 'sche..ma2.table2'" {
+            (Get-ObjectNameParts -ObjectName 'sche..ma2.table2').Parsed | Should Be $false
         }
     }
 }
