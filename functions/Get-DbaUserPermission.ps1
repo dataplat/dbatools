cb29f0d08a81f9470d5f4163d0d2fada36ad6c73
Chrissy LeMaire <clemaire@gmail.com>
2021-07-12 23:01:45 +0200

    Revert "fix query calls"
    This reverts commit 48a1c2980e3f4c8cfd7e3a2e1d7ebd29dbe13a85.

--- functions/Get-DbaUserPermission.ps1
+++ functions/Get-DbaUserPermission.ps1
@@ -234,7 +234,7 @@ function Get-DbaUserPermission {
                     Write-Message -Level Verbose -Message "Building data table for server objects"
 
                     try {
-                        $serverDT = Invoke-DbaQuery -SqlInstance $server -Database $db.Name -Query $serverSQL
+                        $serverDT = $db.Query($serverSQL)
                     } catch {
                         # here to avoid an empty catch
                         $null = 1
@@ -264,7 +264,7 @@ function Get-DbaUserPermission {
 
                 Write-Message -Level Verbose -Message "Building data table for $db objects"
                 try {
-                    $dbDT = Invoke-DbaQuery -SqlInstance $server -Database $db.Name -Query $dbSQL
+                    $dbDT = $db.Query($dbSQL)
                 } catch {
                     # here to avoid an empty catch
                     $null = 1
