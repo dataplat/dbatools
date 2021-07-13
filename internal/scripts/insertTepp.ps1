159fec4c6d1cf2688b1546673281fcc099273803
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2021-07-13 10:17:30 +0200

    New/Set-DbaAgentSchedule - Fix usage of FrequencyInterval for daily interval (#7517)

--- internal/scripts/insertTepp.ps1
+++ internal/scripts/insertTepp.ps1
@@ -81,4 +81,7 @@ Register-DbaTeppArgumentCompleter -Command "Get-DbaProcess", "Stop-DbaProcess" -
 Register-DbaTeppArgumentCompleter -Command "Get-DbaProcess", "Stop-DbaProcess" -Parameter Spid -Name processSpid
 Register-DbaTeppArgumentCompleter -Command "Import-DbaXESessionTemplate", "Get-DbaXESessionTemplate", "Export-DbaXESessionTemplate" -Parameter Template -Name xesessiontemplate
 Register-DbaTeppArgumentCompleter -Command "Import-DbaPfDataCollectorSetTemplate", "Get-DbaPfDataCollectorSetTemplate", "Export-DbaPfDataCollectorSetTemplate"  -Parameter Template -Name perfmontemplate
+Register-DbaTeppArgumentCompleter -Command "New-DbaAgentSchedule" -Parameter FrequencyInterval -Name newagentschedule
+Register-DbaTeppArgumentCompleter -Command "Set-DbaAgentSchedule" -Parameter FrequencyInterval -Name setagentschedule
+
 #endregion Explicit TEPP
