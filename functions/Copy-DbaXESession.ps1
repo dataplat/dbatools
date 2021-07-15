a9096ba48fbe9aaf85f41c6e7a13e6865395916d
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-15 11:15:15 +0200

    add connection to try block (#7561)

--- functions/Copy-DbaXESession.ps1
+++ functions/Copy-DbaXESession.ps1
@@ -100,13 +100,15 @@ function Copy-DbaXESession {
     begin {
         try {
             $sourceServer = Connect-SqlInstance -SqlInstance $Source -SqlCredential $SourceSqlCredential -MinimumVersion 11
+
+            $sourceSqlConn = $sourceServer.ConnectionContext.SqlConnectionObject
+            $sourceSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $sourceSqlConn
+            $sourceStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $sourceSqlStoreConnection
         } catch {
             Stop-Function -Message "Error occurred while establishing connection to $Source" -Category ConnectionError -ErrorRecord $_ -Target $Source
             return
         }
-        $sourceSqlConn = $sourceServer.ConnectionContext.SqlConnectionObject
-        $sourceSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $sourceSqlConn
-        $sourceStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $sourceSqlStoreConnection
+
         $storeSessions = $sourceStore.Sessions | Where-Object { $_.Name -notin 'AlwaysOn_health', 'system_health' }
         if ($XeSession) {
             $storeSessions = $storeSessions | Where-Object Name -In $XeSession
@@ -122,13 +124,12 @@ function Copy-DbaXESession {
         foreach ($destinstance in $Destination) {
             try {
                 $destServer = Connect-SqlInstance -SqlInstance $destinstance -SqlCredential $DestinationSqlCredential -MinimumVersion 11
+                $destSqlConn = $destServer.ConnectionContext.SqlConnectionObject
+                $destSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $destSqlConn
+                $destStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $destSqlStoreConnection
             } catch {
                 Stop-Function -Message "Error occurred while establishing connection to $destinstance" -Category ConnectionError -ErrorRecord $_ -Target $destinstance -Continue
             }
-
-            $destSqlConn = $destServer.ConnectionContext.SqlConnectionObject
-            $destSqlStoreConnection = New-Object Microsoft.SqlServer.Management.Sdk.Sfc.SqlStoreConnection $destSqlConn
-            $destStore = New-Object  Microsoft.SqlServer.Management.XEvent.XEStore $destSqlStoreConnection
 
             Write-Message -Level Verbose -Message "Migrating sessions."
             foreach ($session in $storeSessions) {
