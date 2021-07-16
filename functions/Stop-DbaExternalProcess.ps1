8dda356526d8f760278d45cab4950e435783ff7b
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-16 12:28:52 +0200

    more

--- functions/Stop-DbaExternalProcess.ps1
+++ functions/Stop-DbaExternalProcess.ps1
@@ -16,6 +16,9 @@ function Stop-DbaExternalProcess {
     .PARAMETER Credential
         Allows you to login to $ComputerName using alternative credentials.
 
+    .PARAMETER ProcessId
+        The process ID of the OS process to kill
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
@@ -30,12 +33,17 @@ function Stop-DbaExternalProcess {
         License: MIT https://opensource.org/licenses/MIT
 
     .LINK
-        https://dbatools.io/Get-DbaExternalProcess
+        https://dbatools.io/Set-DbaExternalProcess
 
     .EXAMPLE
-        PS C:\> Get-DbaExternalProcess -ComputerName SERVER01, SERVER02
+        PS C:\> Get-DbaExternalProcess -ComputerName SQL01 | Stop-DbaExternalProcess
 
-        Gets OS processes created by SQL Server on SERVER01 and SERVER02
+        Kills all OS processes created by SQL Server on SQL01
+
+    .EXAMPLE
+        PS C:\> Get-DbaExternalProcess -ComputerName SQL01 | Where-Object Name -eq "cmd.exe" | Stop-DbaExternalProcess
+
+        Kills all cmd.exe processes created by SQL Server on SQL01
 
     #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
---
+++ tests/Stop-DbaExternalProcess.Tests.ps1
@@ -0,0 +1,28 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'ComputerName', 'Credential', 'EnableException'
+        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        It "Should only contain our specific parameters" {
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+        }
+    }
+}
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    Context "Can get a certificate" {
+        BeforeAll {
+            $null = Add-DbaComputerCertificate -Path $script:appveyorlabrepo\certificates\localhost.crt -Confirm:$false
+            $thumbprint = "29C469578D6C6211076A09CEE5C5797EEA0C2713"
+        }
+        AfterAll {
+            Remove-DbaComputerCertificate -Thumbprint $thumbprint -Confirm:$false
+        }
+
+
+    }
+}
