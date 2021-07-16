edcd52eedd2433cf05fb349177d9e75f7cbf99ba
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-16 11:37:39 +0200

    fix test

--- tests/Test-DbaComputerCertificateExpiration.ps1
+++ tests/Test-DbaComputerCertificateExpiration.ps1
@@ -15,15 +15,13 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "tests a certificate" {
-        BeforeAll {
-            $null = Add-DbaComputerCertificate -Path $script:appveyorlabrepo\certificates\localhost.crt -Confirm:$false -ErrorAction SilentlyContinue
-            $thumbprint = "29C469578D6C6211076A09CEE5C5797EEA0C2713"
-        }
         AfterAll {
             Remove-DbaComputerCertificate -Thumbprint $thumbprint -Confirm:$false -ErrorAction SilentlyContinue
         }
 
         It "reports that the certificate is expired" {
+            $null = Add-DbaComputerCertificate -Path $script:appveyorlabrepo\certificates\localhost.crt -Confirm:$false -ErrorAction SilentlyContinue
+            $thumbprint = "29C469578D6C6211076A09CEE5C5797EEA0C2713"
             Test-DbaComputerCertificateExpiration -Thumbprint $thumbprint | Select-Object -ExpandProperty Note | Should -Be "This certificate has expired and is no longer valid"
             $cert.Thumbprint | Should Be $thumbprint
         }
