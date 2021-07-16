6d1e7d225ee35cd881d84877ead0fee9618976c2
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-16 13:08:03 +0200

    need coffee

--- tests/Test-DbaComputerCertificateExpiration.Tests.ps1
+++ tests/Test-DbaComputerCertificateExpiration.Tests.ps1
@@ -22,8 +22,9 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         It "reports that the certificate is expired" {
             $null = Add-DbaComputerCertificate -Path $script:appveyorlabrepo\certificates\localhost.crt -Confirm:$false
             $thumbprint = "29C469578D6C6211076A09CEE5C5797EEA0C2713"
-            Test-DbaComputerCertificateExpiration -Thumbprint $thumbprint | Select-Object -ExpandProperty Note | Should -Be "This certificate has expired and is no longer valid"
-            $cert.Thumbprint | Should Be $thumbprint
+            $results = Test-DbaComputerCertificateExpiration -Thumbprint $thumbprint
+            $results | Select-Object -ExpandProperty Note | Should -Be "This certificate has expired and is no longer valid"
+            $results.Thumbprint | Should Be $thumbprint
         }
     }
 }
