31b5193303f244ce3ac0753e8ce7763400448bb9
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2021-07-21 09:52:23 +0200

    New Commands: New-/Get-/Remove-DbaFirewallRule (#7576)

---
+++ tests/Remove-DbaFirewallRule.Tests.ps1
@@ -0,0 +1,18 @@
+$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tags "UnitTests" {
+    Context "Validate parameters" {
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'SqlInstance', 'Credential', 'Type', 'InputObject', 'EnableException'
+        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        It "Should only contain our specific parameters" {
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+        }
+    }
+}
+
+<#
+The command will be tested together with New-DbaFirewallRule
+#>
