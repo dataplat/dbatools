ca69055348c1aa6e47d956c81047ea204420e2f6
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-23 05:29:31 -0700

    New-Command - Get-DbatoolsError (#7612)

---
+++ functions/Get-DbatoolsError.ps1
@@ -0,0 +1,67 @@
+function Get-DbatoolsError {
+    <#
+    .SYNOPSIS
+        Returns detailed dbatools-related error information
+
+    .DESCRIPTION
+        Returns detailed dbatools-related error information
+
+        By default, it only returns the most recent error ($error[0])
+
+    .PARAMETER First
+        Works like `Select-Object -First 1`
+
+    .PARAMETER Last
+        Works like `Select-Object -Last 1`
+
+    .PARAMETER Skip
+        Works like `Select-Object -Skip 1`
+
+    .PARAMETER All
+        Returns detailed information for all dbatools-related errors
+
+    .NOTES
+        Tags: Module, Support
+        Author: Chrissy LeMaire (@cl)
+
+        Website: https://dbatools.io
+        Copyright: (c) 2021 by dbatools, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://dbatools.io/Get-DbatoolsError
+
+    .EXAMPLE
+        PS C:\> Get-DbatoolsError
+
+        Returns detailed error information for the most recent dbatools error
+
+    .EXAMPLE
+        PS C:\> Get-DbatoolsError -All
+
+        Returns detailed error information for all dbatools-related errors
+
+    .EXAMPLE
+        PS C:\> Get-DbatoolsError -Last 1
+
+        Returns the oldest dbatools-related error in the pipeline
+    #>
+    [CmdletBinding()]
+    param (
+        [int]$First,
+        [int]$Last,
+        [int]$Skip,
+        [switch]$All
+    )
+    process {
+        if (Test-Bound -Not First, Last, Skip, All) {
+            $First = 1
+        }
+
+        if ($All) {
+            $First = $global:error.Count
+        }
+
+        $global:error | Where-Object ScriptStackTrace -match dbatools | Select-Object -First $First -Last $Last -Skip $Skip -Property CategoryInfo, ErrorDetails, Exception, FullyQualifiedErrorId, InvocationInfo, PipelineIterationInfo, PSMessageDetails, ScriptStackTrace, TargetObject
+    }
+}
---
+++ tests/Get-DbatoolsError.Tests.ps1
@@ -0,0 +1,25 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'First', 'Last', 'Skip', 'All'
+        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        It "Should only contain our specific parameters" {
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+        }
+    }
+}
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    Context "Gets an error" {
+        It "returns a dbatools error" {
+            try {
+                $null = Connect-DbaInstance -SqlInstance nothing -ConnectTimeout 1 -ErrorAction Stop
+            } catch {}
+            Get-DbatoolsError | Should -Not -BeNullOrEmpty
+        }
+    }
+}
