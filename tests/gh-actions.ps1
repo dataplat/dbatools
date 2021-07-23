61ec0c7c5177b8efff4f9ad6478a3ec186c3e636
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-23 10:35:37 +0200

    adjust test

--- tests/gh-actions.ps1
+++ tests/gh-actions.ps1
@@ -176,10 +176,11 @@ Describe "Integration Tests" -Tag "IntegrationTests" {
     }
 
     It "connects to Azure" {
+        $PSDefaultParameterValues.Clear()
         $securestring = ConvertTo-SecureString $env:CLIENTSECRET -AsPlainText -Force
         $azurecred = New-Object PSCredential -ArgumentList $env:CLIENTID, $securestring
-        Connect-DbaInstance -SqlInstance dbatooolstest.database.windows.net -SqlCredential $azurecred -Tenant $env:TENANTID | Should -Not throw
-        Connect-DbaInstance -SqlInstance "Server=dbatoolstest.database.windows.net; Authentication=Active Directory Service Principal; Database=test; User Id=$env:CLIENTID; Password=$env:CLIENTSECRET;" | Select -ExpandProperty ComputerName | Should -Be "dbatoolstest.database.windows.net"
+        Connect-DbaInstance -SqlInstance dbatooolstest.database.windows.net -SqlCredential $azurecred -Tenant $env:TENANTID | Select-Object -ExpandProperty ComputerName | Should -Be "dbatoolstest.database.windows.net"
+        Connect-DbaInstance -SqlInstance "Server=dbatoolstest.database.windows.net; Authentication=Active Directory Service Principal; Database=test; User Id=$env:CLIENTID; Password=$env:CLIENTSECRET;" | Select-Object -ExpandProperty ComputerName | Should -Be "dbatoolstest.database.windows.net"
     }
 }
 
