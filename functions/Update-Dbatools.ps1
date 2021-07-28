9d0ab947ea4d9f400a6cc113d370b96dc923302c
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-28 09:59:19 +0200

    Update-Dbatools -- Add -Cleanup parameter
    fixes #7379

--- functions/Update-Dbatools.ps1
+++ functions/Update-Dbatools.ps1
@@ -9,6 +9,9 @@ function Update-Dbatools {
     .PARAMETER Development
         If this switch is enabled, the current development branch will be installed. By default, the latest official release is installed.
 
+    .PARAMETER Cleanup
+        If this switch is enabled, old versions of dbatools will be removed.
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
@@ -22,7 +25,7 @@ function Update-Dbatools {
 
     .NOTES
         Tags: Module
-        Author: Chrissy LeMaire (@cl), netnerds.net
+        Author: Shawn Melton (@wsmelton), http://wsmelton.github.io
 
         Website: https://dbatools.io
         Copyright: (c) 2018 by dbatools, licensed under MIT
@@ -46,6 +49,7 @@ function Update-Dbatools {
     param(
         [Alias("dev", "devbranch")]
         [switch]$Development,
+        [switch]$Cleanup,
         [switch]$EnableException
     )
     $MyModuleBase = [SqlCollaborative.Dbatools.dbaSystem.SystemHost]::ModuleBase
@@ -60,5 +64,14 @@ function Update-Dbatools {
         if ($PSCmdlet.ShouldProcess("release branch", "Updating dbatools")) {
             & $InstallScript;
         }
+    }
+    if ($Cleanup) {
+        Write-Message -Level Verbose -Message "Removing older versions of dbatools"
+        try {
+            $cleanfile = Join-DbaPath -Path $((Get-InstalledModule dbatools).InstalledLocation) -ChildPath "cleanup.ps1" -ErrorAction Stop
+        } catch {
+            $cleanfile = Join-DbaPath -Path $(Split-Path (Get-Module dbatools).Path) -ChildPath "cleanup.ps1"
+        }
+        & $cleanfile
     }
 }
