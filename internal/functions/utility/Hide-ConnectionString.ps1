378c15f22cce98fcbf8a45a76550cb89658b4c23
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-29 02:19:00 +0200

    let function be used by multiple commands

---
+++ internal/functions/utility/Hide-ConnectionString.ps1
@@ -0,0 +1,14 @@
+function Hide-ConnectionString {
+    Param (
+        [string]$ConnectionString
+    )
+    try {
+        $connStringBuilder = New-Object Microsoft.Data.SqlClient.SqlConnectionStringBuilder $ConnectionString
+        if ($connStringBuilder.Password) {
+            $connStringBuilder.Password = ''.Padleft(8, '*')
+        }
+        return $connStringBuilder.ConnectionString
+    } catch {
+        return "Failed to mask the connection string`: $($_.Exception.Message)"
+    }
+}
