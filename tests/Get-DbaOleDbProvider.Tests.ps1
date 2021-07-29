0e1d0a65d6d3182b4ccc97cbc24675872ca5b73e
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-29 14:01:06 +0200

    Create Get-DbaOleDbProvider.Tests.ps1

---
+++ tests/Get-DbaOleDbProvider.Tests.ps1
@@ -0,0 +1,22 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "Get-DbaComputerSystem Unit Tests" -Tag "UnitTests" {
+    Context "Validate parameters" {
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Provider', 'EnableException'
+
+        It "Should only contain our specific parameters" {
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
+        }
+    }
+}
+Describe "$CommandName Integration Test" -Tag "IntegrationTests" {
+    Context "Validate output" {
+        $result = Get-DbaOleDbProvider -SqlInstance $script:instance1
+        It "has some output" {
+            $result | Should -Not -BeNullOrEmpty
+        }
+    }
+}
