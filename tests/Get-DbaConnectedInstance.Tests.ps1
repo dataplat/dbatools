98a8cbe693a7e2dac1b2bbc5f586c83add393f3f
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-07-30 10:01:37 +0200

    Update Get-DbaConnectedInstance.Tests.ps1

--- tests/Get-DbaConnectedInstance.Tests.ps1
+++ tests/Get-DbaConnectedInstance.Tests.ps1
@@ -2,6 +2,15 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
+Describe "$CommandName Unit Tests" -Tag "UnitTests" {
+    Context "Validate parameters" {
+        # fake tests, no parameters to validate
+        It "Should only contain our specific parameters" {
+            $null | Should -BeNullOrEmpty
+        }
+    }
+}
+
 Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $null = Get-DbaDatabase -SqlInstance $script:instance1
