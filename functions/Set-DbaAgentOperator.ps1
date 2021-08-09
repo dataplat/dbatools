98206faa309cafc4be3c25408605de9993fc88c2
unknown <administrator@MYDOMAIN.COM>
2021-08-09 23:07:07 +0100

    Set-DBAAgentOperator Failsafe Operator Handling Correction
    - Modified to set the failsafe notification method on the AlertSystem
      object rather than the FailSafeOperator object
    - Modified the list of possible input values for the
      $FailSafeNotificationMethod input parameter to include 'NotifyAll'
    - Comment and variable spelling corrections

--- functions/Set-DbaAgentOperator.ps1
+++ functions/Set-DbaAgentOperator.ps1
@@ -29,7 +29,7 @@ function Set-DbaAgentOperator {
         The net send address the SQL Agent will use for the operator to net send alerts.
 
     .PARAMETER PagerAddress
-        The pager email address the SQL Agent will use to send alerts to the oeprator.
+        The pager email address the SQL Agent will use to send alerts to the operator.
 
     .PARAMETER PagerDay
         Defines what days the pager portion of the operator will be used. The default is 'Everyday'. Valid parameters
@@ -58,7 +58,7 @@ function Set-DbaAgentOperator {
         If this switch is enabled, this operator will be your failsafe operator and replace the one that existed before.
 
     .PARAMETER FailsafeNotificationMethod
-        Defines the notification method for the failsafe oeprator.  Value must be NotifyMail or NotifyPager.
+        Defines the notification method for the failsafe operator.  Value must be NotifyEmail, Pager or NotifyAll.
         The default is NotifyEmail.
 
     .PARAMETER WhatIf
@@ -221,7 +221,7 @@ function Set-DbaAgentOperator {
             }
         }
 
-        if ($IsFailsafeOperator -and ($FailsafeNotificationMethod -notin ('NotifyMail', 'NotifyPager'))) {
+        if ($IsFailsafeOperator -and ($FailsafeNotificationMethod -notin ('NotifyEmail', 'Pager', 'NotifyAll'))) {
             Stop-Function -Message "You must specify a notification method for the failsafe operator."
             return
         }
@@ -250,7 +250,7 @@ function Set-DbaAgentOperator {
 
         if ($SqlInstance) {
             try {
-                $InputObject += Get-DbaAgentOperator -SqlInstance $SqlIntance -SqlCredential $SqlCredential -Operator $($op.Name) -EnableException
+                $InputObject += Get-DbaAgentOperator -SqlInstance $SqlInstance -SqlCredential $SqlCredential -Operator $($op.Name) -EnableException
             } catch {
                 Stop-Function -Message "Failed" -Category ConnectionError -ErrorRecord $_ -Target $instance -Continue
             }
@@ -336,7 +336,7 @@ function Set-DbaAgentOperator {
                 if ($IsFailsafeOperator) {
                     if ($Pscmdlet.ShouldProcess($server, "Updating FailSafe Operator to $operator")) {
                         $server.JobServer.AlertSystem.FailSafeOperator = $Operator
-                        $server.JobServer.AlertSystem.FailSafeOperator.NotificationMethod = $FailsafeNotificationMethod
+                        $server.JobServer.AlertSystem.NotificationMethod = $FailsafeNotificationMethod
                         $server.JobServer.AlertSystem.Alter()
                     }
                 }
