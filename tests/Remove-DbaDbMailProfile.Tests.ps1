48a091b105bd46ee46fb2f09deb829fcf12ffc89
MikeyBronowski <mikeybronowski@gmail.com>
2021-09-29 15:22:56 +0100

    Updates the repcaced parameter name

--- tests/Remove-DbaDbMailProfile.Tests.ps1
+++ tests/Remove-DbaDbMailProfile.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Profile', 'ExcludeProfile', 'InputObject', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'ExcludeProfile', 'InputObject', 'EnableException'
         It "Should only contain our specific parameters" {
             Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
@@ -29,23 +29,23 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     Context "commands work as expected" {
 
         It "removes a database mail profile" {
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename ) | Should -Not -BeNullOrEmpty
-            Remove-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename -Confirm:$false
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename ) | Should -Not -BeNullOrEmpty
+            Remove-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename -Confirm:$false
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename ) | Should -BeNullOrEmpty
         }
 
         It "supports piping database mail profile" {
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename ) | Should -Not -BeNullOrEmpty
-            Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename | Remove-DbaDbMailProfile -Confirm:$false
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename ) | Should -Not -BeNullOrEmpty
+            Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename | Remove-DbaDbMailProfile -Confirm:$false
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename ) | Should -BeNullOrEmpty
         }
 
         It "removes all database mail profiles but excluded" {
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename2 ) | Should -Not -BeNullOrEmpty
             (Get-DbaDbMailProfile -SqlInstance $server, $server2 -ExcludeProfile $profilename2 ) | Should -Not -BeNullOrEmpty
             Remove-DbaDbMailProfile -SqlInstance $server, $server2 -ExcludeProfile $profilename2 -Confirm:$false
             (Get-DbaDbMailProfile -SqlInstance $server, $server2 -ExcludeProfile $profilename2 ) | Should -BeNullOrEmpty
-            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Profile $profilename2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailProfile -SqlInstance $server, $server2 -Name $profilename2 ) | Should -Not -BeNullOrEmpty
         }
 
         It "removes all database mail profiles" {
