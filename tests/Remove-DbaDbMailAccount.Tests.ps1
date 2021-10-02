5bc9243af63448395bd893d3ea73d6c4dc736a1d
MikeyBronowski <mikeybronowski@gmail.com>
2021-10-02 23:06:09 +0100

    Removes second server from tests

--- tests/Remove-DbaDbMailAccount.Tests.ps1
+++ tests/Remove-DbaDbMailAccount.Tests.ps1
@@ -17,41 +17,40 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     BeforeEach {
 
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-        $server2 = Connect-DbaInstance -SqlInstance $script:instance3
         $accountname = "dbatoolsci_test_$(get-random)"
         $accountname2 = "dbatoolsci_test_$(get-random)"
 
-        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname -EmailAddress admin@ad.local
-        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 -EmailAddress admin@ad.local
+        $null = New-DbaDbMailAccount -SqlInstance $server -Name $accountname -EmailAddress admin@ad.local
+        $null = New-DbaDbMailAccount -SqlInstance $server -Name $accountname2 -EmailAddress admin@ad.local
 
     }
 
     Context "commands work as expected" {
 
         It "removes a database mail account" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -Not -BeNullOrEmpty
-            Remove-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname ) | Should -Not -BeNullOrEmpty
+            Remove-DbaDbMailAccount -SqlInstance $server -Account $accountname -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname ) | Should -BeNullOrEmpty
         }
 
         It "supports piping database mail account" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -Not -BeNullOrEmpty
-            Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname | Remove-DbaDbMailAccount -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname ) | Should -Not -BeNullOrEmpty
+            Get-DbaDbMailAccount -SqlInstance $server -Account $accountname | Remove-DbaDbMailAccount -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname ) | Should -BeNullOrEmpty
         }
 
         It "removes all database mail accounts but excluded" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 ) | Should -Not -BeNullOrEmpty
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 ) | Should -Not -BeNullOrEmpty
-            Remove-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 ) | Should -BeNullOrEmpty
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server -ExcludeAccount $accountname2 ) | Should -Not -BeNullOrEmpty
+            Remove-DbaDbMailAccount -SqlInstance $server -ExcludeAccount $accountname2 -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server -ExcludeAccount $accountname2 ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server -Account $accountname2 ) | Should -Not -BeNullOrEmpty
         }
 
         It "removes all database mail accounts" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 ) | Should -Not -BeNullOrEmpty
-            Remove-DbaDbMailAccount -SqlInstance $server, $server2 -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server ) | Should -Not -BeNullOrEmpty
+            Remove-DbaDbMailAccount -SqlInstance $server -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server ) | Should -BeNullOrEmpty
         }
     }
 }
