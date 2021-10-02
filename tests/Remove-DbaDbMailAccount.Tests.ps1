e59b0b21c711f8f5eb1c6951a00a4c7b13f56788
MikeyBronowski <mikeybronowski@gmail.com>
2021-10-02 01:36:15 +0100

    Parameter name changed

--- tests/Remove-DbaDbMailAccount.Tests.ps1
+++ tests/Remove-DbaDbMailAccount.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'ExcludeAccount', 'InputObject', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Account', 'ExcludeAccount', 'InputObject', 'EnableException'
         It "Should only contain our specific parameters" {
             Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
@@ -21,31 +21,31 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $accountname = "dbatoolsci_test_$(get-random)"
         $accountname2 = "dbatoolsci_test_$(get-random)"
 
-        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname -EmailAddress admin@ad.local
-        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname2 -EmailAddress admin@ad.local
+        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname -EmailAddress admin@ad.local
+        $null = New-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 -EmailAddress admin@ad.local
 
     }
 
     Context "commands work as expected" {
 
         It "removes a database mail account" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname ) | Should -Not -BeNullOrEmpty
-            Remove-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -Not -BeNullOrEmpty
+            Remove-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -BeNullOrEmpty
         }
 
         It "supports piping database mail account" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname ) | Should -Not -BeNullOrEmpty
-            Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname | Remove-DbaDbMailAccount -Confirm:$false
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname ) | Should -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -Not -BeNullOrEmpty
+            Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname | Remove-DbaDbMailAccount -Confirm:$false
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname ) | Should -BeNullOrEmpty
         }
 
         It "removes all database mail accounts but excluded" {
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 ) | Should -Not -BeNullOrEmpty
             (Get-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 ) | Should -Not -BeNullOrEmpty
             Remove-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 -Confirm:$false
             (Get-DbaDbMailAccount -SqlInstance $server, $server2 -ExcludeAccount $accountname2 ) | Should -BeNullOrEmpty
-            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Name $accountname2 ) | Should -Not -BeNullOrEmpty
+            (Get-DbaDbMailAccount -SqlInstance $server, $server2 -Account $accountname2 ) | Should -Not -BeNullOrEmpty
         }
 
         It "removes all database mail accounts" {
