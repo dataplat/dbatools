f5d0e9eb70d51a3d6bfbce7afb03715651c4debd
Mikey Bronowski <MikeyBronowski@gmail.com>
2021-10-06 09:48:24 +0100

    New-DbaDbMailAccount, New-DbaDbMailProfile - renaming parameter name to Account, Profile respectively  (#7835)

--- tests/New-DbaDbMailProfile.Tests.ps1
+++ tests/New-DbaDbMailProfile.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'Description', 'MailAccountName', 'MailAccountPriority', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Profile', 'Description', 'MailAccountName', 'MailAccountPriority', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -41,7 +41,7 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
 
         $splat = @{
             SqlInstance         = $script:instance2
-            Name                = $profilename
+            Profile             = $profilename
             Description         = $description
             MailAccountName     = $mailaccountname
             MailAccountPriority = $mailaccountpriority
