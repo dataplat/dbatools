f927793982ced12970c4bc5a1daf1a40239ac0c6
andreasjordan <anj@ordix.de>
2021-11-17 19:10:32 +0100

    Update test

--- tests/Update-DbaBuildReference.Tests.ps1
+++ tests/Update-DbaBuildReference.Tests.ps1
@@ -6,7 +6,7 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         It "Should only contain our specific parameters" {
             [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
-            [object[]]$knownParameters = 'EnableException'
+            [object[]]$knownParameters = 'LocalFile', 'EnableException'
             Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
         }
     }
