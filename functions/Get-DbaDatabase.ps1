c49ffaf7cff70ff3762176f5f8d7e95e108ea5f2
chightow <charleshightower@gmail.com>
2021-11-22 12:54:56 -0600

    cne

--- functions/Get-DbaDatabase.ps1
+++ functions/Get-DbaDatabase.ps1
@@ -318,7 +318,7 @@ function Get-DbaDatabase {
 
                 $hasCopyOnly = $inputObject | Where-CollationSensitiveObject -Property Name -In -Value $lastCopyOnlyBackups.Database -Collation $server.Collation
                 $inputObject = $inputObject | Where-CollationSensitiveObject -Property Name -Notin -Value $lastFullBackups.Database -Collation $server.Collation
-                $inputObject = $inputObject | Where-Object Name -ne 'tempdb'
+                $inputObject = $inputObject | Where-Object Name -cne 'tempdb'
             }
             if ($NoLogBackup -or $NoLogBackupSince) {
                 if (!$NoLogBackupSince) {
