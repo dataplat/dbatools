74abc6bc3b139709ababb04097d08868034db115
MikeyBronowski <mikeybronowski@gmail.com>
2021-11-30 23:51:15 +0000

    Removes comments

--- functions/Remove-DbaAgentAlertCategory.ps1
+++ functions/Remove-DbaAgentAlertCategory.ps1
@@ -70,7 +70,7 @@ function Remove-DbaAgentAlertCategory {
     param (
         [Parameter(ParameterSetName = 'NonPipeline', Mandatory = $true, Position = 0)]
         [DbaInstanceParameter[]]$SqlInstance,
-        #[Parameter(ParameterSetName = 'NonPipeline')]
+        [Parameter(ParameterSetName = 'NonPipeline')]
         [PSCredential]$SqlCredential,
         [Parameter(ParameterSetName = 'NonPipeline')]
         [string[]]$Category,
