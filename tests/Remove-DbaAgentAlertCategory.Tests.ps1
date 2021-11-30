256a35c912f23e86864c9c4a53225c5dd448ede9
MikeyBronowski <mikeybronowski@gmail.com>
2021-11-30 23:25:10 +0000

    fixes test + adds test

--- tests/Remove-DbaAgentAlertCategory.Tests.ps1
+++ tests/Remove-DbaAgentAlertCategory.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Category', 'Force', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Category', 'InputObject', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -29,11 +29,19 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         }
 
         It "Remove the alert categories" {
-            Remove-DbaAgentAlertCategory -SqlInstance $script:instance2 -Category CategoryTest1, CategoryTest2, Categorytest3
+            Remove-DbaAgentAlertCategory -SqlInstance $script:instance2 -Category CategoryTest1, CategoryTest2, Categorytest3 -Confirm:$false
 
             $newresults = Get-DbaAgentAlertCategory -SqlInstance $script:instance2 -Category CategoryTest1, CategoryTest2, CategoryTest3
 
             $newresults.Count | Should Be 0
+        }
+
+        It "supports piping SQL Agent alert" {
+            $categoryName = "dbatoolsci_test_$(get-random)"
+            $results2 = New-DbaAgentAlertCategory -SqlInstance $script:instance2 -Category $categoryName
+            (Get-DbaAgentAlertCategory -SqlInstance $server -Category $categoryName ) | Should -Not -BeNullOrEmpty
+            Get-DbaAgentAlertCategory -SqlInstance $server -Category $categoryName | Remove-DbaAgentAlertCategory -Confirm:$false
+            (Get-DbaAgentAlertCategory -SqlInstance $server -Category $categoryName ) | Should -BeNullOrEmpty
         }
     }
 }
