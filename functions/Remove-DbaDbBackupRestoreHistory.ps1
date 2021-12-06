cd4d4c916e1f421ecbce3ee2772d59e411b22987
MikeyBronowski <mikeybronowski@gmail.com>
2021-12-06 15:56:42 +0000

    Adds tags.

--- functions/Remove-DbaDbBackupRestoreHistory.ps1
+++ functions/Remove-DbaDbBackupRestoreHistory.ps1
@@ -45,7 +45,7 @@ function Remove-DbaDbBackupRestoreHistory {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: Delete
+        Tags: Delete, Backup, Restore, Database
         Author: IJeb Reitsma
 
         Website: https://dbatools.io
