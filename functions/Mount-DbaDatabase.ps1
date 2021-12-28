6123abfdda7a3d87c85a3046cb055f0dd063187b
wsmelton <11204251+wsmelton@users.noreply.github.com>
2021-12-27 22:36:11 -0600

    Attach and Detach tag update

--- functions/Mount-DbaDatabase.ps1
+++ functions/Mount-DbaDatabase.ps1
@@ -40,7 +40,7 @@ function Mount-DbaDatabase {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: Database
+        Tags: Attach, Database
         Author: Chrissy LeMaire (@cl), netnerds.net
 
         Website: https://dbatools.io
