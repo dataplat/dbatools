04c46b37f4241e51d0faf51ddc6ec6304593ff16
wsmelton <11204251+wsmelton@users.noreply.github.com>
2021-12-28 20:13:00 -0600

    General tag update

--- functions/Add-DbaExtendedProperty.ps1
+++ functions/Add-DbaExtendedProperty.ps1
@@ -71,7 +71,7 @@ function Add-DbaExtendedProperty {
         Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
     .NOTES
-        Tags: extendedproperties
+        Tags: General, ExtendedProperties
         Author: Chrissy LeMaire (@cl), netnerds.net
 
         Website: https://dbatools.io
