c12363d13aad1cf1cf939673127f7f170fc963b7
wsmelton <11204251+wsmelton@users.noreply.github.com>
2021-12-28 19:37:48 -0600

    basic test for Join-DbaPath and Resolve-DbaPath

---
+++ tests/Resolve-DbaPath.Tests.ps1
@@ -0,0 +1,14 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        [array]$params = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($CommandName, 'Function')).Parameters.Keys
+        [object[]]$knownParameters = 'Path', 'Provider', 'SingleItem', 'NewChild'
+
+        It "Should only contain our specific parameters" {
+            Compare-Object -ReferenceObject $knownParameters -DifferenceObject $params | Should -BeNullOrEmpty
+        }
+    }
+}
