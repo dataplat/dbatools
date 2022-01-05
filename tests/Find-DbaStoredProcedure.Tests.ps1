2fe694eaf05114f549f10ea3082cdc29c18bda2f
mattcargile <matthew.a.cargile@gmail.com>
2022-01-05 14:46:01 -0600

    fix: revert previous change. move to keeping the wildcard search on dbatools* to using Should Contain instead. Potential to find other procedures with similar names on the same instance.

--- tests/Find-DbaStoredProcedure.Tests.ps1
+++ tests/Find-DbaStoredProcedure.Tests.ps1
@@ -29,9 +29,9 @@ AS
             $DropProcedure = "DROP PROCEDURE dbo.cp_dbatoolsci_sysadmin;"
             $null = Invoke-DbaQuery -SqlInstance $script:instance2 -Database 'Master' -Query $DropProcedure
         }
-        $results = Find-DbaStoredProcedure -SqlInstance $script:instance2 -Pattern 'cp_dbatoolsci_sysadmin' -IncludeSystemDatabases
+        $results = Find-DbaStoredProcedure -SqlInstance $script:instance2 -Pattern dbatools* -IncludeSystemDatabases
         It "Should find a specific StoredProcedure named cp_dbatoolsci_sysadmin" {
-            $results.Name | Should Be "cp_dbatoolsci_sysadmin"
+            $results.Name | Should Contain "cp_dbatoolsci_sysadmin"
         }
     }
     Context "Command finds Procedures in a User Database" {
@@ -50,10 +50,10 @@ AS
         }
         $results = Find-DbaStoredProcedure -SqlInstance $script:instance2 -Pattern dbatools* -Database 'dbatoolsci_storedproceduredb'
         It "Should find a specific StoredProcedure named sp_dbatoolsci_custom" {
-            $results.Name | Should Be "sp_dbatoolsci_custom"
+            $results.Name | Should Contain "sp_dbatoolsci_custom"
         }
         It "Should find sp_dbatoolsci_custom in dbatoolsci_storedproceduredb" {
-            $results.Database | Should Be "dbatoolsci_storedproceduredb"
+            $results.Database | Should Contain "dbatoolsci_storedproceduredb"
         }
         $results = Find-DbaStoredProcedure -SqlInstance $script:instance2 -Pattern dbatools* -ExcludeDatabase 'dbatoolsci_storedproceduredb'
         It "Should find no results when Excluding dbatoolsci_storedproceduredb" {
