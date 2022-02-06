f72a2d1b0610fc3fe467bac0ab7367b872fc8948
josh <toosuto@gmail.com>
2022-02-06 08:02:49 -0800

    Update functions/Get-DbaUserPermission.ps1
    Co-authored-by: Shawn Melton <11204251+wsmelton@users.noreply.github.com>

--- functions/Get-DbaUserPermission.ps1
+++ functions/Get-DbaUserPermission.ps1
@@ -37,7 +37,6 @@ function Get-DbaUserPermission {
 
     .PARAMETER ExcludeSecurables
         Allow you to exclude object-level permissions from the output, and only return role permission(s).
-        switch will exclude specific object level permissions and simply return roles for users.
 
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
