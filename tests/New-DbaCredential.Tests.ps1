a0ee785d3fc9e74be53f4beac2cb4ad9c0878be3
Cláudio Silva <claudiosil100@gmail.com>
2022-02-21 22:39:42 +0000

    fix tests (do New-DbaCredential)

--- tests/New-DbaCredential.Tests.ps1
+++ tests/New-DbaCredential.Tests.ps1
@@ -6,7 +6,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'Identity', 'SecurePassword', '$NoPassword', 'MappedClassType', 'ProviderName', 'Force', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Name', 'Identity', 'SecurePassword', 'NoPassword', 'MappedClassType', 'ProviderName', 'Force', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -60,7 +60,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
                 Identity = 'Managed Identity'
                 NoPassword = $true
             }
-            $results = New-DbaCredential $credentialParams
+            $results = New-DbaCredential @credentialParams
             $results.Name | Should Be "https://mystorageaccount.blob.core.windows.net/mycontainer"
             $results.Identity | Should Be "Managed Identity"
         }
