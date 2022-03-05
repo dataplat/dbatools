47fb9959095cb68eeeae4f7b1d8228428d4be21a
Adam Lancaster <68036988+lancasteradam@users.noreply.github.com>
2022-03-05 06:40:32 -0800

    Add DatabaseID to the return object for backup commands (#8204)

--- tests/Backup-DbaDbMasterKey.Tests.ps1
+++ tests/Backup-DbaDbMasterKey.Tests.ps1
@@ -4,11 +4,11 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Credential', 'Database', 'ExcludeDatabase', 'SecurePassword', 'Path', 'InputObject', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
@@ -30,6 +30,10 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
         It "backs up the db cert" {
             $results.Database -eq 'tempdb'
             $results.Status -eq "Success"
+        }
+
+        It "Database ID should be returned" {
+            $results.DatabaseID | Should -Be (Get-DbaDatabase -SqlInstance $script:instance1 -Database tempdb).ID
         }
     }
 }
