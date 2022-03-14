8d6316b1e0c6e59313ba2c41760cc2e063523909
andreasjordan <anj@ordix.de>
2022-03-14 09:48:33 +0100

    Only encrypt newnew  databases (do Start-DbaDbEncryption)

--- tests/Start-DbaDbEncryption.Tests.ps1
+++ tests/Start-DbaDbEncryption.Tests.ps1
@@ -32,7 +32,7 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             $passwd = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
             $splat = @{
                 SqlInstance             = $script:instance2
-                AllUserDatabases        = $true
+                Database                = $alldbs.Name
                 MasterKeySecurePassword = $passwd
                 BackupSecurePassword    = $passwd
                 BackupPath              = "C:\temp"
