6cdf96b246e6c3e2e75c4b701aadfa125a2b412b
ACALVETT <andrew@calvett.co.uk>
2022-03-25 19:26:23 +0000

    Cleanup

--- functions/Test-DbaMaxMemory.ps1
+++ functions/Test-DbaMaxMemory.ps1
@@ -83,15 +83,14 @@ function Test-DbaMaxMemory {
                     } else {
                         $serverService = Get-DbaService -ComputerName $instance -EnableException
                     }
-                    
+
                     $instanceCount = ($serverService | Where-Object State -Like Running | Where-Object InstanceName | Where-Object ServiceType -eq 'Engine' | Group-Object InstanceName | Measure-Object Count).Count
 
-                    if ($instanceCount = 0)
-                    {
-                        Write-Message -Level Warning -Message "Couldn't get accurate SQL Server instance count on $instance. Defaulting to 1." -Target $instance
+                    if ($instanceCount -eq 0) {
+                        Write-Message -Level Warning -Message "Couldn't get accurate SQL Server instance count on $instance. Defaulting to 1."
                         $instanceCount = 1
                     }
-                    
+
                     $otherConsumers = $serverService | Where-Object ServiceType -in ('SSAS', 'SSRS', 'SSIS')
                     if ($null -ne $otherConsumers) {
                         Write-Message -Level Warning -Message "The memory calculation maybe inccurate as the following SQL components have also been detected: $($otherConsumers.ServiceType -join(','))"
