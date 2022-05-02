8b77f40972fe828fb56881d0d56490f270e0f293
Chrissy LeMaire <clemaire@gmail.com>
2022-05-02 13:04:30 +0200

    change windows test

--- tests/gh-winactions.ps1
+++ tests/gh-winactions.ps1
@@ -1,21 +1,7 @@
 Describe "Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
-        $password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
-        $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin", $password
-
-        $PSDefaultParameterValues["*:SqlInstance"] = "localhost"
-        $PSDefaultParameterValues["*:Source"] = "localhost"
-        $PSDefaultParameterValues["*:Destination"] = "localhost:14333"
-        $PSDefaultParameterValues["*:Primary"] = "localhost"
-        $PSDefaultParameterValues["*:Mirror"] = "localhost:14333"
-        $PSDefaultParameterValues["*:SqlCredential"] = $cred
-        $PSDefaultParameterValues["*:SourceSqlCredential"] = $cred
-        $PSDefaultParameterValues["*:DestinationSqlCredential"] = $cred
-        $PSDefaultParameterValues["*:PrimarySqlCredential"] = $cred
-        $PSDefaultParameterValues["*:MirrorSqlCredential"] = $cred
-        $PSDefaultParameterValues["*:WitnessSqlCredential"] = $cred
+        $PSDefaultParameterValues["*:SqlInstance"] = "(localdb)\dbatools"
         $PSDefaultParameterValues["*:Confirm"] = $false
-        $PSDefaultParameterValues["*:SharedPath"] = "/shared"
         $PSDefaultParameterValues["*:WarningAction"] = "SilentlyContinue"
         $global:ProgressPreference = "SilentlyContinue"
 
@@ -34,25 +20,11 @@ Describe "Integration Tests" -Tag "IntegrationTests" {
         $extractOptions = New-DbaDacOption -Action Export
         $extractOptions.ExtractAllTableData = $true
         $dacpac = Export-DbaDacPackage -Database $dbname -DacOption $extractOptions
-        $results = $dacpac | Publish-DbaDacPackage -PublishXml $publishprofile.FileName -Database $dbname -SqlInstance localhost:14333 -Confirm:$false
+        $null = Remove-DbaDatabase -Database $db.Name
+        $results = $dacpac | Publish-DbaDacPackage -PublishXml $publishprofile.FileName -Database $dbname -Confirm:$false
         $results.Result | Should -BeLike '*Update complete.*'
-        $ids = Invoke-DbaQuery -Database $dbname -SqlInstance localhost:14333 -Query 'SELECT id FROM dbo.example'
+        $ids = Invoke-DbaQuery -Database $dbname -Query 'SELECT id FROM dbo.example'
         $ids.id | Should -Not -BeNullOrEmpty
-    }
-
-    It "connects to Azure" {
-        $PSDefaultParameterValues.Clear()
-        $securestring = ConvertTo-SecureString $env:CLIENTSECRET -AsPlainText -Force
-        $azurecred = New-Object PSCredential -ArgumentList $env:CLIENTID, $securestring
-        Connect-DbaInstance -SqlInstance dbatoolstest.database.windows.net -SqlCredential $azurecred -Tenant $env:TENANTID | Select-Object -ExpandProperty ComputerName | Should -Be "dbatoolstest.database.windows.net"
-        Connect-DbaInstance -SqlInstance "Server=dbatoolstest.database.windows.net; Authentication=Active Directory Service Principal; Database=test; User Id=$env:CLIENTID; Password=$env:CLIENTSECRET;" | Select-Object -ExpandProperty ComputerName | Should -Be "dbatoolstest.database.windows.net"
-    }
-
-    It "gets a database from Azure" {
-        $PSDefaultParameterValues.Clear()
-        $securestring = ConvertTo-SecureString $env:CLIENTSECRET -AsPlainText -Force
-        $azurecred = New-Object PSCredential -ArgumentList $env:CLIENTID, $securestring
-        $server = Connect-DbaInstance -SqlInstance dbatoolstest.database.windows.net -SqlCredential $azurecred -Tenant $env:TENANTID
-        (Get-DbaDatabase -SqlInstance $server -Database test).Name | Should -Be "test"
+        $null = Remove-DbaDatabase -Database $db.Name
     }
 }
