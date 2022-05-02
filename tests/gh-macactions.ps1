6c9f743235dbe7f9445bdcdc7eee2d23a193dcb9
Chrissy LeMaire <clemaire@gmail.com>
2022-05-02 14:34:44 +0200

    add mac

---
+++ tests/gh-macactions.ps1
@@ -0,0 +1,29 @@
+Describe "Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+        $PSDefaultParameterValues["*:SqlInstance"] = "(localdb)\MSSQLLocalDB"
+        $PSDefaultParameterValues["*:Confirm"] = $false
+        $global:ProgressPreference = "SilentlyContinue"
+
+        Import-Module ./dbatools.psm1 -Force
+    }
+
+    It "publishes a package" {
+        $db = New-DbaDatabase
+        $dbname = $db.Name
+        $null = $db.Query("CREATE TABLE dbo.example (id int, PRIMARY KEY (id));
+            INSERT dbo.example
+            SELECT top 100 object_id
+            FROM sys.objects")
+
+        $publishprofile = New-DbaDacProfile -Database $dbname -Path C:\temp
+        $extractOptions = New-DbaDacOption -Action Export
+        $extractOptions.ExtractAllTableData = $true
+        $dacpac = Export-DbaDacPackage -Database $dbname -DacOption $extractOptions
+        $null = Remove-DbaDatabase -Database $db.Name
+        $results = $dacpac | Publish-DbaDacPackage -PublishXml $publishprofile.FileName -Database $dbname -Confirm:$false
+        $results.Result | Should -BeLike '*Update complete.*'
+        $ids = Invoke-DbaQuery -Database $dbname -Query 'SELECT id FROM dbo.example'
+        $ids.id | Should -Not -BeNullOrEmpty
+        $null = Remove-DbaDatabase -Database $db.Name
+    }
+}
