afe0a07c1778f228b54656901ae5e9b767a38298
Chrissy LeMaire <clemaire@gmail.com>
2022-06-15 14:19:48 +0200

    remove build.ps1

--- build.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module ".\dbatools.psd1"
-
-Find-DbaCommand -Rebuild
-
-Import-Module HelpOut
-Install-Maml -FunctionRoot functions,internal\functions -Module dbatools -Compact -NoVersion -Verbose
-
-Save-DbaDiagnosticQueryScript -Path ".\bin\diagnosticquery"
