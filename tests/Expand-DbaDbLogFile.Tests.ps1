bcbfbda2a268a45b9a04ed6333e8b550a984c743
Adam Lancaster <68036988+lancasteradam@users.noreply.github.com>
2022-06-28 04:28:50 -0700

    Expand-DbaDbLogFile: Add DatabaseID to the result object (#8407)

--- tests/Expand-DbaDbLogFile.Tests.ps1
+++ tests/Expand-DbaDbLogFile.Tests.ps1
@@ -4,41 +4,44 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'TargetLogSize', 'IncrementSize', 'LogFileId', 'ShrinkLogFile', 'ShrinkSize', 'BackupDirectory', 'ExcludeDiskSpaceValidation', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
 
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
-        $server = Connect-DbaInstance -SqlInstance $script:instance1
-        $db1 = "dbatoolsci_expand"
-        $server.Query("CREATE DATABASE $db1")
+        $db1Name = "dbatoolsci_expand"
+        $db1 = New-DbaDatabase -SqlInstance $script:instance1 -Name $db1Name
     }
     AfterAll {
-        Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance1 -Database $db1
+        Remove-DbaDatabase -Confirm:$false -SqlInstance $script:instance1 -Database $db1Name
     }
 
-    $results = Expand-DbaDbLogFile -SqlInstance $script:instance1 -Database $db1 -TargetLogSize 128
+    Context "Ensure command functionality" {
 
-    It -Skip "Should have correct properties" {
-        $ExpectedProps = 'ComputerName,InstanceName,SqlInstance,Database,ID,Name,LogFileCount,InitialSize,CurrentSize,InitialVLFCount,CurrentVLFCount'.Split(',')
-        ($results[0].PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
-    }
+        $results = Expand-DbaDbLogFile -SqlInstance $script:instance1 -Database $db1 -TargetLogSize 128
 
-    It "Should have database name of $db1" {
-        foreach ($result in $results) {
-            $result.InitialSize -gt $result.CurrentSize
+        It -Skip "Should have correct properties" {
+            $ExpectedProps = 'ComputerName,InstanceName,SqlInstance,Database,ID,Name,LogFileCount,InitialSize,CurrentSize,InitialVLFCount,CurrentVLFCount'.Split(',')
+            ($results[0].PsObject.Properties.Name | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
         }
-    }
 
-    It "Should have grown the log file" {
-        foreach ($result in $results) {
-            $result.InitialSize -gt $result.CurrentSize
+        It "Should have database name and ID of $db1" {
+            foreach ($result in $results) {
+                $result.Database | Should -Be $db1Name
+                $result.DatabaseID | Should -Be $db1.ID
+            }
+        }
+
+        It "Should have grown the log file" {
+            foreach ($result in $results) {
+                $result.InitialSize -gt $result.CurrentSize
+            }
         }
     }
 }
