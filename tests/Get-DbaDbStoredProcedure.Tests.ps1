d9dc9aec3cbcb39903863e5a7bd883827f9e1ae3
Adam Lancaster <68036988+lancasteradam@users.noreply.github.com>
2022-07-03 01:50:44 -0700

    Get-DbaDbStoredProcedure: adding -Name and -Schema params and returning DatabaseId. (#8428)

--- tests/Get-DbaDbStoredProcedure.Tests.ps1
+++ tests/Get-DbaDbStoredProcedure.Tests.ps1
@@ -11,7 +11,7 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('WhatIf', 'Confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'ExcludeSystemSp', 'InputObject', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'ExcludeSystemSp', 'Name', 'Schema', 'InputObject', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
@@ -22,15 +22,23 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
 Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     BeforeAll {
         $server = Connect-DbaInstance -SqlInstance $script:instance2
-        $procName = ("dbatools_{0}" -f $(Get-Random))
-        $server.Query("CREATE PROCEDURE $procName AS SELECT 1", 'tempdb')
+        $random = Get-Random
+        $db1Name = "dbatoolsci_$random"
+        $db1 = New-DbaDatabase -SqlInstance $server -Name $db1Name
+        $procName = "proc1"
+        $db1.Query("CREATE PROCEDURE $procName AS SELECT 1")
+
+        $schemaName = "schema1"
+        $procName2 = "proc2"
+        $db1.Query("CREATE SCHEMA $schemaName")
+        $db1.Query("CREATE PROCEDURE $schemaName.$procName2 AS SELECT 1")
     }
     AfterAll {
-        $null = $server.Query("DROP PROCEDURE $procName", 'tempdb')
+        $db1 | Remove-DbaDatabase -Confirm:$false
     }
 
     Context "Command actually works" {
-        $results = Get-DbaDbStoredProcedure -SqlInstance $script:instance2 -Database tempdb
+        $results = Get-DbaDbStoredProcedure -SqlInstance $script:instance2 -Database $db1Name
         It "Should have standard properties" {
             $ExpectedProps = 'ComputerName,InstanceName,SqlInstance'.Split(',')
             ($results[0].PsObject.Properties.Name | Where-Object { $_ -in $ExpectedProps } | Sort-Object) | Should Be ($ExpectedProps | Sort-Object)
@@ -49,19 +57,48 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
             $results.Database | Should -Not -Contain 'master'
         }
         It "Should exclude system procedures" {
-            $results = Get-DbaDbStoredProcedure -SqlInstance $script:instance2 -Database tempdb -ExcludeSystemSp
+            $results = Get-DbaDbStoredProcedure -SqlInstance $script:instance2 -Database $db1Name -ExcludeSystemSp
             $results | Where-Object Name -eq 'sp_helpdb' | Should -BeNullOrEmpty
         }
     }
 
     Context "Piping works" {
         It "Should allow piping from string" {
-            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database tempdb
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database $db1Name
             ($results | Where-Object Name -eq $procName).Name | Should -Not -BeNullOrEmpty
         }
         It "Should allow piping from Get-DbaDatabase" {
-            $results = Get-DbaDatabase -SqlInstance $script:instance2 -Database tempdb | Get-DbaDbStoredProcedure
+            $results = Get-DbaDatabase -SqlInstance $script:instance2 -Database $db1Name | Get-DbaDbStoredProcedure
             ($results | Where-Object Name -eq $procName).Name | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context "Search by name and schema" {
+        It "Search by name" {
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database $db1Name -Name $procName
+            $results.Name | Should -Be $procName
+            $results.DatabaseId | Should -Be $db1.Id
+        }
+        It "Search by 2 part name" {
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database $db1Name -Name "$schemaName.$procName2"
+            $results.Name | Should -Be $procName2
+            $results.Schema | Should -Be $schemaName
+        }
+        It "Search by 3 part name and omit the -Database param" {
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Name "$db1Name.$schemaName.$procName2"
+            $results.Name | Should -Be $procName2
+            $results.Schema | Should -Be $schemaName
+            $results.Database | Should -Be $db1Name
+        }
+        It "Search by name and schema params" {
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database $db1Name -Name $procName2 -Schema $schemaName
+            $results.Name | Should -Be $procName2
+            $results.Schema | Should -Be $schemaName
+        }
+        It "Search by schema name" {
+            $results = $script:instance2 | Get-DbaDbStoredProcedure -Database $db1Name -Schema $schemaName
+            $results.Name | Should -Be $procName2
+            $results.Schema | Should -Be $schemaName
         }
     }
 }
