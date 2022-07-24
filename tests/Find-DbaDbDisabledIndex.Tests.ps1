ef4156ad21893ddd4315f4f6291f7c22eaa4324a
lancasteradam <68036988+lancasteradam@users.noreply.github.com>
2022-07-24 15:48:59 -0700

    Find-DbaDbDisabledIndex: Add DatabaseId to the return object

--- tests/Find-DbaDbDisabledIndex.Tests.ps1
+++ tests/Find-DbaDbDisabledIndex.Tests.ps1
@@ -4,11 +4,11 @@ Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'ExcludeDatabase', 'NoClobber', 'Append', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
@@ -18,29 +18,39 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
         BeforeAll {
             $server = Connect-DbaInstance -SqlInstance $script:instance1
             $random = Get-Random
+            $databaseName1 = "dbatoolsci1_$random"
+            $databaseName2 = "dbatoolsci2_$random"
+            $db1 = New-DbaDatabase -SqlInstance $server -Name $databaseName1
+            $db2 = New-DbaDatabase -SqlInstance $server -Name $databaseName2
             $indexName = "dbatoolsci_index_$random"
             $tableName = "dbatoolsci_table_$random"
             $sql = "create table $tableName (col1 int)
                     create index $indexName on $tableName (col1)
                     ALTER INDEX $indexName ON $tableName DISABLE;"
-            $null = $server.Query($sql, 'tempdb')
+            $null = $db1.Query($sql)
+            $null = $db2.Query($sql)
         }
         AfterAll {
-            $sql = "drop table $tableName;"
-            $null = $server.Query($sql, 'tempdb')
+            $db1, $db2 | Remove-DbaDatabase -Confirm:$false
         }
 
         It "Should find disabled index: $indexName" {
             $results = Find-DbaDbDisabledIndex -SqlInstance $script:instance1
-            $results.IndexName -contains $indexName | Should Be $true
+            ($results | Where-Object { $_.IndexName -eq $indexName }).Count | Should -Be 2
+            ($results | Where-Object { $_.DatabaseName -in $databaseName1, $databaseName2 }).Count | Should -Be 2
+            ($results | Where-Object { $_.DatabaseId -in $db1.Id, $db2.Id }).Count | Should -Be 2
         }
         It "Should find disabled index: $indexName for specific database" {
-            $results = Find-DbaDbDisabledIndex -SqlInstance $script:instance1 -Database tempdb
-            $results.IndexName -contains $indexName | Should Be $true
+            $results = Find-DbaDbDisabledIndex -SqlInstance $script:instance1 -Database $databaseName1
+            $results.IndexName | Should -Be $indexName
+            $results.DatabaseName | Should -Be $databaseName1
+            $results.DatabaseId | Should -Be $db1.Id
         }
         It "Should exclude specific database" {
-            $results = Find-DbaDbDisabledIndex -SqlInstance $script:instance1 -ExcludeDatabase tempdb
-            $results.DatabaseName -contains 'tempdb' | Should Be $false
+            $results = Find-DbaDbDisabledIndex -SqlInstance $script:instance1 -ExcludeDatabase $databaseName1
+            $results.IndexName | Should -Be $indexName
+            $results.DatabaseName | Should -Be $databaseName2
+            $results.DatabaseId | Should -Be $db2.Id
         }
     }
 }
