ecd18c02bfb61b1915cf6cc492cae52d3b19c778
Kirill Kravtsov <30303784+nvarscar@users.noreply.github.com>
2022-07-30 02:01:04 -0700

    Adding -Property parameter to New-DbaDacOption (#8464)

--- tests/New-DbaDacOption.Tests.ps1
+++ tests/New-DbaDacOption.Tests.ps1
@@ -5,7 +5,7 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 Describe "$commandname Unit Tests" -Tag "UnitTests" {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'Type', 'Action', 'PublishXml', 'EnableException'
+        [object[]]$knownParameters = 'Type', 'Action', 'PublishXml', 'Property', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
@@ -26,12 +26,24 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
         New-DbaDacOption -Action Export -Type Bacpac | Should -Not -BeNullOrEmpty
     }
     It "Returns dacpac publish options" {
-        New-DbaDacOption -Action Publish  | Should -Not -BeNullOrEmpty
+        New-DbaDacOption -Action Publish | Should -Not -BeNullOrEmpty
     }
     It "Returns dacpac publish options from an xml" {
         New-DbaDacOption -Action Publish -PublishXml $publishprofile.FileName -EnableException | Should -Not -BeNullOrEmpty
     }
     It "Returns bacpac publish options" {
         New-DbaDacOption -Action Publish -Type Bacpac | Should -Not -BeNullOrEmpty
+    }
+    It "Properly sets a property value when specified" {
+        (New-DbaDacOption -Action Export -Property @{CommandTimeout = 5 }).CommandTimeout | Should -Be 5
+        (New-DbaDacOption -Action Export -Type Bacpac -Property @{CommandTimeout = 5 }).CommandTimeout | Should -Be 5
+        (New-DbaDacOption -Action Publish -Property @{GenerateDeploymentReport = $true }).GenerateDeploymentReport | Should -BeTrue
+        (New-DbaDacOption -Action Publish -Type Bacpac -Property @{CommandTimeout = 5 }).CommandTimeout | Should -Be 5
+        $result = (New-DbaDacOption -Action Publish -Property @{
+                GenerateDeploymentReport = $true; DeployOptions = @{CommandTimeout = 5 }
+            }
+        )
+        $result.GenerateDeploymentReport | Should -BeTrue
+        $result.DeployOptions.CommandTimeout | Should -Be 5
     }
 }
