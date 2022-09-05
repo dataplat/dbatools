6024c935fcf59f9af54e86d33c27c6b8e9a10401
potatoqualitee <clemaire@gmail.com>
2022-09-05 02:29:18 +0000

    update version

--- bin/diagnosticquery/SQLServerDiagnosticQueries_2016.sql
+++ bin/diagnosticquery/SQLServerDiagnosticQueries_2016.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2016 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: August 8, 2022
+-- Last Modified: September 1, 2022
 -- https://glennsqlperformance.com/
 -- https://sqlserverperformance.wordpress.com/
 -- YouTube: https://bit.ly/2PkoAM1 
