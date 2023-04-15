13565f71b5b7acb268f94ba9bf532c572f3e6212
Chrissy LeMaire <clemaire@gmail.com>
2023-04-15 12:13:43 +0200

    Invoke-ManagedComputerCommand - Add explicit SQLWMI import
    Fixes #8833 maybe

--- private/functions/Invoke-ManagedComputerCommand.ps1
+++ private/functions/Invoke-ManagedComputerCommand.ps1
@@ -43,19 +43,38 @@ function Invoke-ManagedComputerCommand {
 
     $resolved = Resolve-DbaNetworkName -ComputerName $computer -Turbo
     $ipaddr = $resolved.IpAddress
-    $ArgumentList += $ipaddr
+
+    $additionalArgumentList = [PSCustomObject]@{
+        ipaddr  = $ipaddr
+        version = 0
+    }
+    $ArgumentList += $additionalArgumentList
 
     [scriptblock]$setupScriptBlock = {
-        $ipaddr = $args[$args.GetUpperBound(0)]
+        $ipaddr = $args[$args.GetUpperBound(0)].ipaddr
+        $version = $args[$args.GetUpperBound(0)].version
 
         $setupVerbose = @( )
         $setupVerbose += "Starting WMI initialization at $ipaddr"
 
+        if ($version -eq 0) {
+            $setupVerbose += "Using latest version"
+            $null  = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
+        } else {
+            $setupVerbose += "Attempting specific version $version"
+            $dll = Get-ChildItem $env:windir\Microsoft.NET\assembly\GAC_MSIL\Microsoft.SqlServer.SqlWmiManagement, $env:windir\assembly\GAC_MSIL\Microsoft.SqlServer.SqlWmiManagement -Recurse -File | Where-Object FullName -like "*$version.0*"
+
+            if ($dll.FullName) {
+                $setupVerbose += "Loading from $($dll.FullName)"
+                $null = Add-Type -Path $dll.FullName
+            } else {
+                $setupVerbose += "Could not find matching version $version"
+            }
+        }
         # Just in case we go remote, ensure the assembly is loaded
-        [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SqlWmiManagement')
+
         $wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $ipaddr
         $result = $wmi.Initialize()
-
         $setupVerbose += "Finished WMI initialization with $result"
     }
 
@@ -65,14 +84,75 @@ function Invoke-ManagedComputerCommand {
     $scriptBlock = [ScriptBlock]::Create("$prescriptblock  $postscriptblock")
     Write-Message -Level Verbose -Message "Connecting to SQL WMI on $computer."
 
+    $parms = @{
+        ScriptBlock  = $ScriptBlock
+        ArgumentList = $ArgumentList
+        Credential   = $Credential
+        ErrorAction  = 'Stop'
+    }
     try {
-        Invoke-Command2 -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -Credential $Credential -ErrorAction Stop
+        Invoke-Command2 @parms
     } catch {
-        Write-Message -Level Verbose -Message "Local connection attempt to $computer failed. Connecting remotely."
-
-        # For surely resolve stuff, and going by default with kerberos, this needs to match FullComputerName
+        Write-Message -Level Verbose -Message "Local connection attempt to $computer failed | $PSItem. Connecting remotely."
         $hostname = $resolved.FullComputerName
 
-        Invoke-Command2 -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList -ComputerName $hostname -Credential $Credential -ErrorAction Stop
+        # For surely resolve stuff, and going by default with kerberos, this needs to match FullComputerName
+
+        try {
+            Invoke-Command2 @parms -ComputerName $hostname
+        } catch {
+            try {
+                $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 16
+                $parms.ArgumentList = $ArgumentList
+                Invoke-Command2 @parms -ComputerName $hostname
+            } catch {
+                # lol I'm not sure how to catch the last error so...
+                try {
+                    $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 15
+                    $parms.ArgumentList = $ArgumentList
+                    Invoke-Command2 @parms -ComputerName $hostname
+                } catch {
+                    try {
+                        $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 14
+                        $parms.ArgumentList = $ArgumentList
+                        Invoke-Command2 @parms -ComputerName $hostname
+                    } catch {
+                        try {
+                            $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 13
+                            $parms.ArgumentList = $ArgumentList
+                            Invoke-Command2 @parms -ComputerName $hostname
+                        } catch {
+                            try {
+                                $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 12
+                                $parms.ArgumentList = $ArgumentList
+                                Invoke-Command2 @parms -ComputerName $hostname
+                            } catch {
+                                try {
+                                    $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 11
+                                    $parms.ArgumentList = $ArgumentList
+                                    Invoke-Command2 @parms -ComputerName $hostname
+                                } catch {
+                                    try {
+                                        $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 10
+                                        $parms.ArgumentList = $ArgumentList
+                                        Invoke-Command2 @parms -ComputerName $hostname
+                                    } catch {
+                                        try {
+                                            $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 9
+                                            $parms.ArgumentList = $ArgumentList
+                                            Invoke-Command2 @parms -ComputerName $hostname
+                                        } catch {
+                                            $ArgumentList[$ArgumentList.GetUpperBound(0)].version = 8
+                                            $parms.ArgumentList = $ArgumentList
+                                            Invoke-Command2 @parms -ComputerName $hostname
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
