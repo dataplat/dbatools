5c2682890cc9f6054682a07e1b6b47b0f09493d7
Jess Pomfret <jesspomfret@datamasterminds.io>
2023-06-01 11:39:06 +0100

    enable piping and tidy up

--- public/Remove-DbaReplArticle.ps1
+++ public/Remove-DbaReplArticle.ps1
@@ -7,7 +7,7 @@ function Remove-DbaReplArticle {
         Removes an article from a publication for the database on the target SQL instances.
 
         Dropping an article from a publication does not remove the object from the publication database or the corresponding object from the subscription database.
-        Use DROP <Object> to remove these objects if necessary. #TODO: add a param for this ClearUpSubObject
+        Use DROP <Object> to remove these objects if necessary. #TODO: add a param for this DropObjectOnSubscriber
 
         Dropping an article invalidates the current snapshot; therefore a new snapshot must be created.
 
@@ -24,7 +24,7 @@ function Remove-DbaReplArticle {
     .PARAMETER Database
         The database on the publisher that contains the article to be removed from replication.
 
-    .PARAMETER PublicationName
+    .PARAMETER Publication
         The name of the replication publication.
 
     .PARAMETER Schema
@@ -35,6 +35,9 @@ function Remove-DbaReplArticle {
 
     .PARAMETER DropObjectOnSubscriber
         If this switch is enabled, the object will be dropped from the subscriber database.
+
+    .PARAMETER InputObject
+        Enables piping from Get-DbaReplArticle
 
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
@@ -49,45 +52,115 @@ function Remove-DbaReplArticle {
 
     .NOTES
         Tags: Replication
-        Author: Jess Pomfret (@jpomfret)
+        Author: Jess Pomfret (@jpomfret), jesspomfret.com
 
         Website: https://dbatools.io
-        Copyright: (c) 2022 by dbatools, licensed under MIT
+        Copyright: (c) 2023 by dbatools, licensed under MIT
         License: MIT https://opensource.org/licenses/MIT
 
-        https://learn.microsoft.com/en-us/sql/relational-databases/replication/publish/delete-an-article?view=sql-server-ver16
+        https://learn.microsoft.com/en-us/sql/relational-databases/replication/publish/delete-an-article
 
     .LINK
         https://dbatools.io/Remove-DbaReplArticle
 
     .EXAMPLE
-        PS C:\> Remove-DbaReplArticle -SqlInstance mssql1 -Database Pubs -PublicationName PubFromPosh -Name 'publishers'
+        PS C:\> Remove-DbaReplArticle -SqlInstance mssql1 -Database Pubs -Publication PubFromPosh -Name 'publishers'
 
         Removes the publishers article from a publication called PubFromPosh on mssql1
 
+    .EXAMPLE
+
+        PS C:\> Get-DbaReplArticle -SqlInstance mssql1 -Database Pubs -Publication TestPub | Remove-DbaReplArticle
+
+        Removes all articles from a publication called TestPub on mssql1
     #>
-    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
+        # [parameter(Mandatory, ParameterSetName = "Default")]
         [DbaInstanceParameter[]]$SqlInstance,
 
         [PSCredential]$SqlCredential,
 
-        [parameter(Mandatory)]
+        #[parameter(Mandatory, ParameterSetName = "Default")]
         [String]$Database,
 
-        [parameter(Mandatory)]
-        [String]$PublicationName,
+        #[parameter(Mandatory, ParameterSetName = "Default")]
+        [String]$Publication,
 
         [String]$Schema = 'dbo',
 
-        [parameter(Mandatory)]
+        #[parameter(Mandatory, ParameterSetName = "Default")]
         [String]$Name,
 
         [Switch]$DropObjectOnSubscriber,
 
+        [Parameter(ValueFromPipeline)]  # , Mandatory, ParameterSetName = "input")]
+        [Microsoft.SqlServer.Replication.Article[]]$InputObject,
+
         [Switch]$EnableException
     )
+
+    begin {
+        $articles = @( )
+    }
+
     process {
+        if (-not $PSBoundParameters.SqlInstance -and -not $PSBoundParameters.InputObject) {
+            Stop-Function -Message "You must specify either SqlInstance or InputObject"
+            return
+        }
+        if ($SqlInstance) {
+            $params = $PSBoundParameters
+            $null = $params.Remove('InputObject')
+            $null = $params.Remove('WhatIf')
+            $null = $params.Remove('Confirm')
+            $articles = Get-DbaReplArticle @params
+        } else {
+            $articles += $InputObject
+        }
+    }
+
+    end {
+        # We have to delete in the end block to prevent "Collection was modified; enumeration operation may not execute." if directly piped from Get-DbaReplArticle.
+        foreach ($art in $articles) {
+            if ($PSCmdlet.ShouldProcess($art.Name, "Removing the article $($art.SourceObjectOwner).$($art.SourceObjectName) from the $($art.PublicationName) publication on $($art.SqlServerName)")) {
+                $output = [pscustomobject]@{
+                    ComputerName = $art.ComputerName
+                    InstanceName = $art.InstanceName
+                    SqlInstance  = $art.SqlInstance
+                    Database     = $art.DatabaseName
+                    ObjectName   = $art.SourceObjectName
+                    ObjectSchema = $art.SourceObjectOwner
+                    Status       = $null
+                    IsRemoved    = $false
+                }
+                try {
+
+                    $pub = Get-DbaReplPublication -SqlInstance $art.SqlInstance -SqlCredential $SqlCredential -Database $art.DatabaseName -Name $art.PublicationName
+
+                    if ($pub.Subscriptions) {
+                        Write-Message -Level Verbose -Message ("There is a subscription so remove article {0} from subscription on {1}" -f $art.Name, $pub.Subscriptions.SubscriberName)
+                        $query = "exec sp_dropsubscription @publication = '{0}', @article= '{1}',@subscriber = '{2}'" -f $art.PublicationName, $art.Name, $pub.Subscriptions.SubscriberName
+                        Invoke-DbaQuery -SqlInstance $art.SqlInstance -SqlCredential $SqlCredential -Database $art.DatabaseName -query $query
+                    }
+                    if (($art.IsExistingObject)) {
+                        $art.Remove()
+                    } else {
+                        Stop-Function -Message "Article doesn't exist in $PublicationName on $instance" -ErrorRecord $_ -Target $instance -Continue
+                    }
+                    $output.Status = "Removed"
+                    $output.IsRemoved = $true
+                } catch {
+                    Stop-Function -Message "Failed to remove the article from publication" -ErrorRecord $_
+                    $output.Status = (Get-ErrorMessage -Record $_)
+                    $output.IsRemoved = $false
+                }
+                $output
+            }
+        }
+    }
+
+    <#
         foreach ($instance in $SqlInstance) {
             try {
                 $replServer = Get-DbaReplServer -SqlInstance $instance -SqlCredential $SqlCredential
@@ -129,14 +202,16 @@ function Remove-DbaReplArticle {
                     }
 
                     if ($DropObjectOnSubscriber) {
-                        #TODO: Drop object on subscriber
+
                     }
                 }
             } catch {
                 Stop-Function -Message "Unable to remove article $ArticleName from $PublicationName on $instance" -ErrorRecord $_ -Target $instance -Continue
             }
         }
-    }
+    }#>
+
+
 }
 
 
