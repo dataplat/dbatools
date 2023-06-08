e20eab1516dcae60c7f11ae2bc54bc262f73c8fa
Jess Pomfret <jesspomfret@datamasterminds.io>
2023-06-08 15:05:48 +0100

    too many dlls

--- tests/InModule.Help.Tests.ps1
+++ tests/InModule.Help.Tests.ps1
@@ -19,8 +19,8 @@ if ($env:appveyor) {
         'Microsoft.SqlServer.Management.XEventDbScoped',
         'Microsoft.SqlServer.Management.XEventDbScopedEnum',
         'Microsoft.SqlServer.Management.XEventEnum',
-        'Microsoft.SqlServer.Replication.dll',
-        'Microsoft.SqlServer.Rmo.dll'
+        'Microsoft.SqlServer.Replication',
+        'Microsoft.SqlServer.Rmo'
     )
 
     foreach ($name in $names) {
