b4f14b25cd01cf061292ee73ddbe2d0bf28d2aad
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2023-06-11 17:10:47 -0500

    Sync-DbaLoginPassword - add help

--- public/Sync-DbaLoginPassword.ps1
+++ public/Sync-DbaLoginPassword.ps1
@@ -1,4 +1,69 @@
 function Sync-DbaLoginPassword {
+    <#
+    .SYNOPSIS
+    Sync the password of a login between instances
+
+    .DESCRIPTION
+    Sync the password of a login(s) between instances using the password hash
+
+    .PARAMETER Source
+    Source SQL Server. You must have sysadmin access and server version must be SQL Server version 2000 or higher.
+
+    .PARAMETER SourceSqlCredential
+    Login to the target instance using alternative credentials. Accepts PowerShell credentials (Get-Credential).
+
+    Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+
+    For MFA support, please use Connect-DbaInstance.
+
+    .PARAMETER Destination
+    Destination SQL Server. You must have sysadmin access and the server must be SQL Server 2000 or higher.
+
+    .PARAMETER DestinationSqlCredential
+    Destination SQL Server. You must have sysadmin access and the server must be SQL Server 2000 or higher.
+
+    Windows Authentication, SQL Server Authentication, Active Directory - Password, and Active Directory - Integrated are all supported.
+
+    For MFA support, please use Connect-DbaInstance.
+
+    .PARAMETER Login
+    The login(s) to process. Options for this list are auto-populated from the server. If unspecified, all logins will be processed.
+
+    .PARAMETER ExcludeLogin
+    The login(s) to exclude. Options for this list are auto-populated from the server.
+
+    .PARAMETER WhatIf
+    If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+    If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+    .PARAMETER EnableException
+    By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+    This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+    Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .NOTES
+    Tags: Migration, Login
+    Author: Shawn Melton, wsmelton.github.io
+
+    Website: https://dbatools.io
+    Copyright: (c) 2018 by dbatools, licensed under MIT
+    License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+    https://dbatools.io/Sync-DbaLoginPassword
+
+    .EXAMPLE
+    Sync-DbaLoginPassword -Source sql201901 -Destination sql201902 -Login TestLogin1
+
+    Synchronize the password hash for login TestLogin1 between each instance. If found matching it will not overwrite.
+
+    .EXAMPLE
+    Sync-DbaLoginPassword -Source sql201901 -Destination sql201902 -Login TestLogin1 -Force
+
+    Synchronize the password hash for login TestLogin1 between each instance. If found matching it will overwrite.
+    #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
     param(
         [Parameter(Mandatory, ValueFromPipeline)]
@@ -61,7 +126,7 @@ function Sync-DbaLoginPassword {
             foreach ($sourceLogin in $allSourceLogins) {
                 $loginName = $sourceLogin.Name
                 $sourceLoginPwdHash = $sourceLoginsPwdHash.$loginName
-                $destLoginName = ($allDestLogins | Where-Object Name -eq $loginName).Name
+                $destLoginName = ($allDestLogins | Where-Object Name -EQ $loginName).Name
                 $destLoginPwdHash = $destLoginsPwdHash.$loginName
 
                 $syncLoginPasswordStatus = [pscustomobject]@{
@@ -104,7 +169,7 @@ function Sync-DbaLoginPassword {
 
                 Write-Message -Level Verbose -Message "Source hash for $loginName [$sourceLoginPwdHash]"
                 Write-Message -Level Verbose -Message "Destination hash for $loginName [$destLoginPwdHash]"
-                if ($sourceLoginPwdHash -eq $destLoginPwdHash) {
+                if ($sourceLoginPwdHash -eq $destLoginPwdHash -and (Test-Bound Force -Not)) {
                     Write-Message -Level Warning -Message "Password hash already matches for login '$loginName'. Skipping"
                     $syncLoginPasswordStatus.Status = 'Skipped'
                     $syncLoginPasswordStatus.Notes = "Password hash already matches for login '$loginName'."
