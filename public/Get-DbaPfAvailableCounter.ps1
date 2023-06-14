a0724725fd95025bb2a3a534b33070def19bd4ed
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2023-06-14 08:11:48 -0500

    Get-DbaPfAvailableCounter - formatting

--- public/Get-DbaPfAvailableCounter.ps1
+++ public/Get-DbaPfAvailableCounter.ps1
@@ -10,12 +10,10 @@ function Get-DbaPfAvailableCounter {
         https://daniel.streefkerkonline.com/2016/02/18/use-powershell-to-list-all-windows-performance-counters-and-their-numeric-ids
 
     .PARAMETER ComputerName
-        The target computer. Defaults to localhost.
+        Target computer(s). If no computer name is specified, the local computer is targeted
 
     .PARAMETER Credential
-        Allows you to login to servers using alternative credentials. To use:
-
-        $scred = Get-Credential, then pass $scred object to the -Credential parameter.
+        Alternate credential object to use for accessing the target computer(s).
 
     .PARAMETER Pattern
         Specify a pattern for filtering.
@@ -60,48 +58,43 @@ function Get-DbaPfAvailableCounter {
         PS C:\> Get-DbaPfAvailableCounter -Pattern *sql* | Add-DbaPfDataCollectorCounter -CollectorSet 'Test Collector Set' -Collector DataCollector01
 
         Adds all counters matching "sql" to the DataCollector01 within the 'Test Collector Set' CollectorSet.
-
     #>
     [CmdletBinding()]
     param (
-        [DbaInstance[]]$ComputerName = $env:ComputerName,
+        [DbaInstanceParameter[]]$ComputerName = $env:ComputerName,
         [PSCredential]$Credential,
         [string]$Pattern,
         [switch]$EnableException
     )
     begin {
         $scriptBlock = {
-            $counters = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Perflib\009' -Name 'counter' | Select-Object -ExpandProperty Counter |
-            Where-Object { $_ -notmatch '[0-90000]' } | Sort-Object | Get-Unique
+            $counters = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Perflib\009' -Name 'counter' | Select-Object -ExpandProperty Counter | Where-Object { $_ -notmatch '[0-90000]' } | Sort-Object | Get-Unique
 
-        foreach ($counter in $counters) {
-            [pscustomobject]@{
-                ComputerName = $env:COMPUTERNAME
-                Name         = $counter
-                Credential   = $args
+            foreach ($counter in $counters) {
+                [pscustomobject]@{
+                    ComputerName = $env:COMPUTERNAME
+                    Name         = $counter
+                    Credential   = $args
+                }
+            }
+        }
+
+        # In case people really want a "like" search, which is slower
+        $Pattern = $Pattern.Replace("*", ".*").Replace("..*", ".*")
+    }
+    process {
+        foreach ($computer in $ComputerName) {
+            try {
+                if ($pattern) {
+                    Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock $scriptBlock -ArgumentList $credential -ErrorAction Stop |
+                        Where-Object Name -Match $pattern | Select-DefaultView -ExcludeProperty Credential
+                } else {
+                    Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock $scriptBlock -ArgumentList $credential -ErrorAction Stop |
+                        Select-DefaultView -ExcludeProperty Credential
+                }
+            } catch {
+                Stop-Function -Message "Failure" -ErrorRecord $_ -Target $computer -Continue
             }
         }
     }
-
-    # In case people really want a "like" search, which is slower
-    $Pattern = $Pattern.Replace("*", ".*").Replace("..*", ".*")
-}
-process {
-
-
-    foreach ($computer in $ComputerName) {
-
-        try {
-            if ($pattern) {
-                Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock $scriptBlock -ArgumentList $credential -ErrorAction Stop |
-                    Where-Object Name -match $pattern | Select-DefaultView -ExcludeProperty Credential
-            } else {
-                Invoke-Command2 -ComputerName $computer -Credential $Credential -ScriptBlock $scriptBlock -ArgumentList $credential -ErrorAction Stop |
-                    Select-DefaultView -ExcludeProperty Credential
-            }
-        } catch {
-            Stop-Function -Message "Failure" -ErrorRecord $_ -Target $computer -Continue
-        }
-    }
-}
 }
