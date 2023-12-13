4630855089aee9a2f29fde91768aa2825ec05aa8
= <=>
2023-12-13 13:24:07 +0100

    Explicitly declare parameters
    using namend parameters and pass these to Invoke-WebRequest

--- private/functions/Invoke-TlsWebRequest.ps1
+++ private/functions/Invoke-TlsWebRequest.ps1
@@ -1,15 +1,56 @@
 function Invoke-TlsWebRequest {
-    Param (
+    param(
         [switch]$UseBasicParsing,
-        [switch]$ProxyUseDefaultCredentials
+        [Uri]$Uri,
+        [Version]$HttpVersion,
+        [Microsoft.PowerShell.Commands.WebRequestSession]$WebSession,
+        [String]$SessionVariable,
+        [switch]$AllowUnencryptedAuthentication,
+        [Microsoft.PowerShell.Commands.WebAuthenticationType]$Authentication,
+        [PSCredential]$Credential,
+        [switch]$UseDefaultCredentials,
+        [String]$CertificateThumbprint,
+        [X509Certificate]$Certificate,
+        [switch]$SkipCertificateCheck,
+        [Microsoft.PowerShell.Commands.WebSslProtocol]$SslProtocol,
+        [SecureString]$Token,
+        [String]$UserAgent,
+        [switch]$DisableKeepAlive,
+        [Int32]$ConnectionTimeoutSeconds,
+        [Int32]$OperationTimeoutSeconds,
+        [Collections.IDictionary]$Headers,
+        [switch]$SkipHeaderValidation,
+        [switch]$AllowInsecureRedirect,
+        [Int32]$MaximumRedirection,
+        [Int32]$MaximumRetryCount,
+        [switch]$PreserveAuthorizationOnRedirect,
+        [Int32]$RetryIntervalSec,
+        [String]$CustomMethod,
+        [Microsoft.PowerShell.Commands.WebRequestMethod]$Method,
+        [switch]$PreserveHttpMethodOnRedirect,
+        [System.Net.Sockets.UnixDomainSocketEndPoint]$UnixSocket,
+        [switch]$NoProxy,
+        [Uri]$Proxy,
+        [PSCredential]$ProxyCredential,
+        [switch]$ProxyUseDefaultCredentials,
+        [Object]$Body,
+        [Collections.IDictionary]$Form,
+        [String]$ContentType,
+        [String]$TransferEncoding,
+        [String]$InFile,
+        [String]$OutFile,
+        [switch]$PassThru,
+        [switch]$Resume,
+        [switch]$SkipHttpErrorCheck
     )
+
     <#
     Internal utility that mimics invoke-webrequest
     but enables all tls available version
     rather than the default, which on a lot
     of standard installations is just TLS 1.0
-
     #>
+
     $currentVersionTls = [Net.ServicePointManager]::SecurityProtocol
     $currentSupportableTls = [Math]::Max($currentVersionTls.value__, [Net.SecurityProtocolType]::Tls.value__)
     $availableTls = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentSupportableTls }
@@ -17,7 +58,7 @@ function Invoke-TlsWebRequest {
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
     }
 
-    Invoke-WebRequest @Args -UseBasicParsing:$UseBasicParsing -ProxyUseDefaultCredentials:$ProxyUseDefaultCredentials
+    Invoke-WebRequest @PSBoundParameters
 
     [Net.ServicePointManager]::SecurityProtocol = $currentVersionTls
 }
