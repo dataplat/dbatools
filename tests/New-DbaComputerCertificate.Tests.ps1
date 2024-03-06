5beeb8b5faf4294dc5841dd6e6fb55bfa328454c
Daniel Merchant <dmerchant@hamiltongroupuk.com>
2024-03-06 09:10:46 +0000

    Check to see if tests run in appveyor

--- tests/New-DbaComputerCertificate.Tests.ps1
+++ tests/New-DbaComputerCertificate.Tests.ps1
@@ -13,48 +13,47 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     }
 }
 
-if (-not $env:appveyor) {
-    Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
-        Context "Can generate a new certificate" {
-            BeforeAll {
-                $cert = New-DbaComputerCertificate -SelfSigned -EnableException
-            }
-            AfterAll {
-                Remove-DbaComputerCertificate -Thumbprint $cert.Thumbprint -Confirm:$false
-            }
-            It "returns the right EnhancedKeyUsageList" {
-                "$($cert.EnhancedKeyUsageList)" -match '1\.3\.6\.1\.5\.5\.7\.3\.1' | Should Be $true
-            }
-            It "returns the right FriendlyName" {
-                "$($cert.FriendlyName)" -match 'SQL Server' | Should Be $true
-            }
-            It "Returns the right default encryption algorithm" {
-                "$(($cert |  select-object  @{n="SignatureAlgorithm";e={$_.SignatureAlgorithm.FriendlyName}})).SignatureAlgorithm)" -match 'sha1RSA' | Should Be $true
-            }
-            It "Returns the right default one year expiry date" {
-                $cert.NotAfter  -match ((Get-Date).Date).AddMonths(12) | Should Be $true
-            }
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    Context "Can generate a new certificate" {
+        BeforeAll {
+            $cert = New-DbaComputerCertificate -SelfSigned -EnableException
+        }
+        AfterAll {
+            Remove-DbaComputerCertificate -Thumbprint $cert.Thumbprint -Confirm:$false
+        }
+        It "returns the right EnhancedKeyUsageList" {
+            "$($cert.EnhancedKeyUsageList)" -match '1\.3\.6\.1\.5\.5\.7\.3\.1' | Should Be $true
+        }
+        It "returns the right FriendlyName" {
+            "$($cert.FriendlyName)" -match 'SQL Server' | Should Be $true
+        }
+        It "Returns the right default encryption algorithm" {
+            "$(($cert |  select-object  @{n="SignatureAlgorithm";e={$_.SignatureAlgorithm.FriendlyName}})).SignatureAlgorithm)" -match 'sha1RSA' | Should Be $true
+        }
+        It "Returns the right default one year expiry date" {
+            $cert.NotAfter  -match ((Get-Date).Date).AddMonths(12) | Should Be $true
         }
     }
 }
 
 
 
-if (-not $env:appveyor) {
-    Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
-        Context "Can generate a new certificate with correct settings" {
-            BeforeAll {
-                $cert = New-DbaComputerCertificate -SelfSigned -EnableException -HashAlgorithm "Sha256" -MonthsValid 60
-            }
-            AfterAll {
-                Remove-DbaComputerCertificate -Thumbprint $cert.Thumbprint -Confirm:$false
-            }
-            It "Returns the right encryption algorithm" {
-                "$(($cert |  select-object  @{n="SignatureAlgorithm";e={$_.SignatureAlgorithm.FriendlyName}})).SignatureAlgorithm)" -match 'sha256RSA' | Should Be $true
-            }
-            It "Returns the right five year (60 month) expiry date" {
-                $cert.NotAfter  -match ((Get-Date).Date).AddMonths(60) | Should Be $true
-            }
+
+
+Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
+    Context "Can generate a new certificate with correct settings" {
+        BeforeAll {
+            $cert = New-DbaComputerCertificate -SelfSigned -EnableException -HashAlgorithm "Sha256" -MonthsValid 60
+        }
+        AfterAll {
+            Remove-DbaComputerCertificate -Thumbprint $cert.Thumbprint -Confirm:$false
+        }
+        It "Returns the right encryption algorithm" {
+            "$(($cert |  select-object  @{n="SignatureAlgorithm";e={$_.SignatureAlgorithm.FriendlyName}})).SignatureAlgorithm)" -match 'sha256RSA' | Should Be $true
+        }
+        It "Returns the right five year (60 month) expiry date" {
+            $cert.NotAfter  -match ((Get-Date).Date).AddMonths(60) | Should Be $true
         }
     }
 }
