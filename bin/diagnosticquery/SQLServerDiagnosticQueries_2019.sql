c2153258c3868a326e1d18d4dcc97ba8e44989ba
Chrissy LeMaire <clemaire@gmail.com>
2024-10-05 19:58:01 +0200

    v2.1.25

--- bin/diagnosticquery/SQLServerDiagnosticQueries_2019.sql
+++ bin/diagnosticquery/SQLServerDiagnosticQueries_2019.sql
@@ -1,7 +1,7 @@
 
 -- SQL Server 2019 Diagnostic Information Queries
 -- Glenn Berry 
--- Last Modified: September 11, 2024
+-- Last Modified: October 2, 2024
 -- https://glennsqlperformance.com/ 
 -- https://sqlserverperformance.wordpress.com/
 -- YouTube: https://bit.ly/2PkoAM1 
