4f1f3528fd587461dac53c78cb8d29655cc7cc69
Chrissy LeMaire <clemaire@gmail.com>
2024-10-15 15:56:31 +0200

    try this  (do Add-DbaAgDatabase)

--- tests/Add-DbaAgListener.Tests.ps1
+++ tests/Add-DbaAgListener.Tests.ps1
@@ -1,34 +1,53 @@
-$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
-. "$PSScriptRoot\constants.ps1"
+param($ModuleName = 'dbatools')
 
-Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+Describe "Add-DbaAgListener Unit Tests" -Tag 'UnitTests' {
+    BeforeAll {
+        # Import the function
+        . (Join-Path -Path $PSScriptRoot -ChildPath '..\functions\Add-DbaAgListener.ps1')
+    }
+
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Name', 'IPAddress', 'SubnetIP', 'SubnetMask', 'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
-        It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
+        BeforeDiscovery {
+            $commandInfo = Get-Command Add-DbaAgListener
+            $parameterInfo = $commandInfo.Parameters
+        }
+
+        It "Should have parameter <_>" -ForEach @(
+            'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Name', 'IPAddress', 'SubnetIP', 'SubnetMask',
+            'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
+        ) {
+            $parameterInfo.ContainsKey($_) | Should -Be $true
+        }
+
+        It "SqlInstance parameter should be mandatory" {
+            $parameterInfo['SqlInstance'].Attributes.Mandatory | Should -Be $true
+        }
+
+        It "Port parameter should have a default value of 1433" {
+            $parameterInfo['Port'].DefaultValue | Should -Be 1433
         }
     }
 }
 
-Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
+Describe "Add-DbaAgListener Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $agname = "dbatoolsci_ag_newlistener"
         $listenerName = 'dbatoolsci_listener'
         $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
     }
+
     AfterEach {
         $null = Remove-DbaAgListener -SqlInstance $script:instance3 -Listener $listenerName -AvailabilityGroup $agname -Confirm:$false
     }
+
     AfterAll {
         $null = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname -Confirm:$false
     }
-    Context "creates a listener" {
-        It "returns results with proper data" {
-            $results = $ag | Add-DbaAgListener -Name $listenerName -IPAddress 127.0.20.1 -Port 14330 -Confirm:$false
-            $results.PortNumber | Should -Be 14330
-        }
+
+    It "creates a listener" {
+        $results = $ag | Add-DbaAgListener -Name $listenerName -IPAddress 127.0.20.1 -Port 14330 -Confirm:$false
+        $results.PortNumber | Should -Be 14330
     }
-} #$script:instance2 for appveyor
+}
+
+#$script:instance2 for appveyor
