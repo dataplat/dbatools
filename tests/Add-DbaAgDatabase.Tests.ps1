ecc4d484142487991504c4d77ad4c5531c8c5387
Chrissy LeMaire <clemaire@gmail.com>
2024-10-15 13:42:41 +0200

    Revert "test updates"
    This reverts commit 9e2bec865409043572f379c73d90127d7a3b7a44.

--- tests/Add-DbaAgDatabase.Tests.ps1
+++ tests/Add-DbaAgDatabase.Tests.ps1
@@ -3,64 +3,38 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
-    BeforeAll {
-        # Mocking Get-Command for parameter validation
-        Mock Get-Command {
-            [PSCustomObject]@{
-                Parameters = @{
-                    SqlInstance          = @{Name = 'SqlInstance'}
-                    SqlCredential        = @{Name = 'SqlCredential'}
-                    AvailabilityGroup    = @{Name = 'AvailabilityGroup'}
-                    Database             = @{Name = 'Database'}
-                    Secondary            = @{Name = 'Secondary'}
-                    SecondarySqlCredential = @{Name = 'SecondarySqlCredential'}
-                    InputObject          = @{Name = 'InputObject'}
-                    SeedingMode          = @{Name = 'SeedingMode'}
-                    SharedPath           = @{Name = 'SharedPath'}
-                    UseLastBackup        = @{Name = 'UseLastBackup'}
-                    AdvancedBackupParams = @{Name = 'AdvancedBackupParams'}
-                    EnableException      = @{Name = 'EnableException'}
-                }
-            }
-        } -ParameterFilter { $Name -eq $CommandName }
-    }
-
     Context "Validate parameters" {
-        It "Should have the correct parameters" {
-            $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Database', 'Secondary', 'SecondarySqlCredential', 'InputObject', 'SeedingMode', 'SharedPath', 'UseLastBackup', 'AdvancedBackupParams', 'EnableException'
-            $command = Get-Command $CommandName
-            $commandParameters = $command.Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-            $commandParameters | Should -Be $knownParameters
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Database', 'Secondary', 'SecondarySqlCredential', 'InputObject', 'SeedingMode', 'SharedPath', 'UseLastBackup', 'AdvancedBackupParams', 'EnableException'
+        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        It "Should only contain our specific parameters" {
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
-}
 
-Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
-    BeforeAll {
-        $server = Connect-DbaInstance -SqlInstance $script:instance3
-        $agname = "dbatoolsci_addagdb_agroup"
-        $dbname = "dbatoolsci_addagdb_agroupdb"
-        $newdbname = "dbatoolsci_addag_agroupdb_2"
-        $null = $server.Query("CREATE DATABASE $dbname")
-        $null = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
-        $null = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false -Certificate dbatoolsci_AGCert
-    }
-
-    AfterAll {
-        $null = Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
-        $null = Remove-DbaDatabase -SqlInstance $server -Database $dbname, $newdbname -Confirm:$false
-    }
-
-    Context "adds ag db and returns proper results" {
-        It "adds the database to the availability group" {
-            $server.Query("CREATE DATABASE $newdbname")
-            $null = Get-DbaDatabase -SqlInstance $script:instance3 -Database $newdbname | Backup-DbaDatabase
+    # Integration Tests
+    Describe "Integration Tests" -Tag "IntegrationTests" {
+        BeforeAll {
+            $null = Get-DbaProcess -SqlInstance $script:instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+            $server = Connect-DbaInstance -SqlInstance $script:instance3
+            $agname = "dbatoolsci_addagdb_agroup"
+            $dbname = "dbatoolsci_addagdb_agroupdb"
+            $newdbname = "dbatoolsci_addag_agroupdb_2"
+            $server.Query("create database $dbname")
+            $backup = Get-DbaDatabase -SqlInstance $script:instance3 -Database $dbname | Backup-DbaDatabase
+            $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false -Certificate dbatoolsci_AGCert
+        }
+        AfterAll {
+            $null = Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
+            $null = Remove-DbaDatabase -SqlInstance $server -Database $dbname, $newdbname -Confirm:$false
+        }
+        It "adds ag db and returns proper results" {
+            $server.Query("create database $newdbname")
+            $backup = Get-DbaDatabase -SqlInstance $script:instance3 -Database $newdbname | Backup-DbaDatabase
             $results = Add-DbaAgDatabase -SqlInstance $script:instance3 -AvailabilityGroup $agname -Database $newdbname -Confirm:$false
             $results.AvailabilityGroup | Should -Be $agname
             $results.Name | Should -Be $newdbname
             $results.IsJoined | Should -Be $true
         }
     }
-}
-
-#$script:instance2 for appveyor
+} #$script:instance2 for appveyor
--- tests/Add-DbaAgListener.Tests.ps1
+++ tests/Add-DbaAgListener.Tests.ps1
@@ -3,34 +3,12 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
-    BeforeAll {
-        # Mocking Get-Command for parameter validation
-        Mock Get-Command {
-            [PSCustomObject]@{
-                Parameters = @{
-                    SqlInstance     = @{Name = 'SqlInstance'}
-                    SqlCredential   = @{Name = 'SqlCredential'}
-                    AvailabilityGroup = @{Name = 'AvailabilityGroup'}
-                    Name            = @{Name = 'Name'}
-                    IPAddress       = @{Name = 'IPAddress'}
-                    SubnetIP        = @{Name = 'SubnetIP'}
-                    SubnetMask      = @{Name = 'SubnetMask'}
-                    Port            = @{Name = 'Port'}
-                    Dhcp            = @{Name = 'Dhcp'}
-                    Passthru        = @{Name = 'Passthru'}
-                    InputObject     = @{Name = 'InputObject'}
-                    EnableException = @{Name = 'EnableException'}
-                }
-            }
-        } -ParameterFilter { $Name -eq $CommandName }
-    }
-
     Context "Validate parameters" {
-        It "Should have the correct parameters" {
-            $knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Name', 'IPAddress', 'SubnetIP', 'SubnetMask', 'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
-            $command = Get-Command $CommandName
-            $commandParameters = $command.Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-            $commandParameters | Should -Be $knownParameters
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'AvailabilityGroup', 'Name', 'IPAddress', 'SubnetIP', 'SubnetMask', 'Port', 'Dhcp', 'Passthru', 'InputObject', 'EnableException'
+        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
+        It "Should only contain our specific parameters" {
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
@@ -39,24 +17,18 @@ Describe "$commandname Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
         $agname = "dbatoolsci_ag_newlistener"
         $listenerName = 'dbatoolsci_listener'
-        $null = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
+        $ag = New-DbaAvailabilityGroup -Primary $script:instance3 -Name $agname -ClusterType None -FailoverMode Manual -Confirm:$false -Certificate dbatoolsci_AGCert
     }
-
     AfterEach {
         $null = Remove-DbaAgListener -SqlInstance $script:instance3 -Listener $listenerName -AvailabilityGroup $agname -Confirm:$false
     }
-
     AfterAll {
         $null = Remove-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname -Confirm:$false
     }
-
     Context "creates a listener" {
         It "returns results with proper data" {
-            $results = Get-DbaAvailabilityGroup -SqlInstance $script:instance3 -AvailabilityGroup $agname | 
-                Add-DbaAgListener -Name $listenerName -IPAddress 127.0.20.1 -Port 14330 -Confirm:$false
+            $results = $ag | Add-DbaAgListener -Name $listenerName -IPAddress 127.0.20.1 -Port 14330 -Confirm:$false
             $results.PortNumber | Should -Be 14330
         }
     }
-}
-
-#$script:instance2 for appveyor
+} #$script:instance2 for appveyor
