d27e2edcb66e878d6575037c2970f7b74e1aaf79
Chrissy LeMaire <clemaire@gmail.com>
2024-10-21 08:23:24 +0200

    updates

--- tests/Measure-DbaDiskSpaceRequirement.Tests.ps1
+++ tests/Measure-DbaDiskSpaceRequirement.Tests.ps1
@@ -4,11 +4,11 @@ $global:TestConfig = Get-TestConfig
 
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
+        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
         [object[]]$knownParameters = 'Source', 'Database', 'SourceSqlCredential', 'Destination', 'DestinationDatabase', 'DestinationSqlCredential', 'Credential', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
         }
     }
 }
@@ -31,16 +31,16 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
             It "Should be sourced from Master" {
                 $r.SourceDatabase | Should -Be $Options.Database
             }
-            It "Should be sourced from the instance $($TestConfig.instance1)" {
-                $r.SourceSqlInstance | Should -Be $server1.SqlInstance
+            It "Should be sourced from the correct instance" {
+                $r.SourceSqlInstance.InstanceName | Should -Be $server1.InstanceName
             }
             It "Should be destined for Dbatoolsci_DestinationDB" {
                 $r.DestinationDatabase | Should -Be $Options.DestinationDatabase
             }
-            It "Should be destined for the instance $($TestConfig.instance2)" {
-                $r.DestinationSqlInstance | Should -Be $server2.SqlInstance
+            It "Should be destined for the correct instance" {
+                $r.DestinationSqlInstance.InstanceName | Should -Be $server2.InstanceName
             }
-            It "Should be have files on source" {
+            It "Should have files on source" {
                 $r.FileLocation | Should Be "Only on Source"
             }
         }
