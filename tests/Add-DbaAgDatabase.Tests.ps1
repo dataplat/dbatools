9eadcd164b8d7fc8e2391a4587d8aa80841a8e79
Chrissy LeMaire <clemaire@gmail.com>
2024-10-25 12:26:46 +0200

    migrate commands to v5

--- tests/Add-DbaAgDatabase.Tests.ps1
+++ tests/Add-DbaAgDatabase.Tests.ps1
@@ -2,53 +2,73 @@
 param($ModuleName = 'dbatools')
 $global:TestConfig = Get-TestConfig
 
-Describe "Add-DbaAgDatabase" {
-    Context "Validate parameters" {
+Describe "Add-DbaAgDatabase" -Tag "UnitTests" {
+    Context "Parameter validation" {
         BeforeAll {
             $command = Get-Command Add-DbaAgDatabase
+            $expectedParameters = $TestConfig.CommonParameters
+
+            $expectedParameters += @(
+                "SqlInstance",
+                "SqlCredential",
+                "AvailabilityGroup",
+                "Database",
+                "Secondary",
+                "SecondarySqlCredential",
+                "InputObject",
+                "SeedingMode",
+                "SharedPath",
+                "UseLastBackup",
+                "AdvancedBackupParams",
+                "EnableException"
+            )
         }
-        $parms = @(
-            "SqlInstance",
-            "SqlCredential",
-            "AvailabilityGroup",
-            "Database",
-            "Secondary",
-            "SecondarySqlCredential",
-            "InputObject",
-            "SeedingMode",
-            "SharedPath",
-            "UseLastBackup",
-            "AdvancedBackupParams",
-            "EnableException",
-            "WhatIf",
-            "Confirm"
-        )
-        It "Has required parameter: <_>" -ForEach $parms {
+
+        It "Should have exactly the expected parameters" {
+            $actualParameters = $command.Parameters.Keys | Where-Object { $PSItem -notin "WhatIf", "Confirm" }
+            Compare-Object -ReferenceObject $expectedParameters -DifferenceObject $actualParameters | Should -BeNullOrEmpty
+        }
+
+        It "Has parameter: <_>" -ForEach $expectedParameters {
             $command | Should -HaveParameter $PSItem
         }
     }
+}
 
-    Context "Integration Tests" -Tag "IntegrationTests" {
+Describe "Add-DbaAgDatabase" -Tag "IntegrationTests" {
+    BeforeAll {
+        $null = Get-DbaProcess -SqlInstance $TestConfig.instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
+        $server = Connect-DbaInstance -SqlInstance $TestConfig.instance3
+        $agname = "dbatoolsci_addagdb_agroup"
+        $dbname = "dbatoolsci_addagdb_agroupdb"
+        $newdbname = "dbatoolsci_addag_agroupdb_2"
+        $server.Query("create database $dbname")
+        $backup = Get-DbaDatabase -SqlInstance $TestConfig.instance3 -Database $dbname | Backup-DbaDatabase
+        $splat = @{
+            Primary       = $TestConfig.instance3
+            Name          = $agname
+            ClusterType   = "None"
+            FailoverMode  = "Manual"
+            Database      = $dbname
+            Confirm       = $false
+            Certificate   = "dbatoolsci_AGCert"
+        }
+        $ag = New-DbaAvailabilityGroup @splat
+    }
+
+    AfterAll {
+        $null = Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
+        $null = Remove-DbaDatabase -SqlInstance $server -Database $dbname, $newdbname -Confirm:$false
+    }
+
+    Context "When adding a database to an availability group" {
         BeforeAll {
-            $null = Get-DbaProcess -SqlInstance $TestConfig.instance3 -Program 'dbatools PowerShell module - dbatools.io' | Stop-DbaProcess -WarningAction SilentlyContinue
-            $server = Connect-DbaInstance -SqlInstance $TestConfig.instance3
-            $agname = "dbatoolsci_addagdb_agroup"
-            $dbname = "dbatoolsci_addagdb_agroupdb"
-            $newdbname = "dbatoolsci_addag_agroupdb_2"
-            $server.Query("create database $dbname")
-            $backup = Get-DbaDatabase -SqlInstance $TestConfig.instance3 -Database $dbname | Backup-DbaDatabase
-            $ag = New-DbaAvailabilityGroup -Primary $TestConfig.instance3 -Name $agname -ClusterType None -FailoverMode Manual -Database $dbname -Confirm:$false -Certificate dbatoolsci_AGCert
-        }
-
-        AfterAll {
-            $null = Remove-DbaAvailabilityGroup -SqlInstance $server -AvailabilityGroup $agname -Confirm:$false
-            $null = Remove-DbaDatabase -SqlInstance $server -Database $dbname, $newdbname -Confirm:$false
-        }
-
-        It "adds ag db and returns proper results" {
             $server.Query("create database $newdbname")
             $backup = Get-DbaDatabase -SqlInstance $TestConfig.instance3 -Database $newdbname | Backup-DbaDatabase
             $results = Add-DbaAgDatabase -SqlInstance $TestConfig.instance3 -AvailabilityGroup $agname -Database $newdbname -Confirm:$false
+        }
+
+        It "Adds the database to the availability group" {
             $results.AvailabilityGroup | Should -Be $agname
             $results.Name | Should -Be $newdbname
             $results.IsJoined | Should -Be $true
--- tests/Add-DbaComputerCertificate.Tests.ps1
+++ tests/Add-DbaComputerCertificate.Tests.ps1
@@ -1,30 +1,53 @@
-$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+#Requires -Module @{ ModuleName="Pester"; ModuleVersion="5.0"}
+param($ModuleName = "dbatools")
 $global:TestConfig = Get-TestConfig
 
-Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
-    Context "Validate parameters" {
-        [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object {$_ -notin ('whatif', 'confirm')}
-        [object[]]$knownParameters = 'ComputerName', 'Credential', 'SecurePassword', 'Certificate', 'Path', 'Store', 'Folder', 'Flag', 'EnableException'
-        $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
-        It "Should only contain our specific parameters" {
-            (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object {$_}) -DifferenceObject $params).Count ) | Should Be 0
+Describe "Add-DbaComputerCertificate" -Tag "UnitTests" {
+    Context "Parameter validation" {
+        BeforeAll {
+            $command = Get-Command Add-DbaComputerCertificate
+            $expectedParameters = $TestConfig.CommonParameters
+
+            $expectedParameters += @(
+                "ComputerName",
+                "Credential",
+                "SecurePassword",
+                "Certificate",
+                "Path",
+                "Store",
+                "Folder",
+                "Flag",
+                "EnableException"
+            )
+        }
+
+        It "Should have exactly the expected parameters" {
+            $actualParameters = $command.Parameters.Keys | Where-Object { $PSItem -notin "WhatIf", "Confirm" }
+            Compare-Object -ReferenceObject $expectedParameters -DifferenceObject $actualParameters | Should -BeNullOrEmpty
+        }
+
+        It "Has parameter: <_>" -ForEach $expectedParameters {
+            $command | Should -HaveParameter $PSItem
         }
     }
 }
 
-Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+Describe "Add-DbaComputerCertificate" -Tag "IntegrationTests" {
     Context "Certificate is added properly" {
-        $results = Add-DbaComputerCertificate -Path "$($TestConfig.appveyorlabrepo)\certificates\localhost.crt" -Confirm:$false
+        BeforeAll {
+            $results = Add-DbaComputerCertificate -Path "$($TestConfig.appveyorlabrepo)\certificates\localhost.crt" -Confirm:$false
+        }
 
         It "Should show the proper thumbprint has been added" {
-            $results.Thumbprint | Should Be "29C469578D6C6211076A09CEE5C5797EEA0C2713"
+            $results.Thumbprint | Should -Be "29C469578D6C6211076A09CEE5C5797EEA0C2713"
         }
 
         It "Should be in LocalMachine\My Cert Store" {
-            $results.PSParentPath | Should Be "Microsoft.PowerShell.Security\Certificate::LocalMachine\My"
+            $results.PSParentPath | Should -Be "Microsoft.PowerShell.Security\Certificate::LocalMachine\My"
         }
 
-        Remove-DbaComputerCertificate -Thumbprint 29C469578D6C6211076A09CEE5C5797EEA0C2713 -Confirm:$false
+        AfterAll {
+            Remove-DbaComputerCertificate -Thumbprint 29C469578D6C6211076A09CEE5C5797EEA0C2713 -Confirm:$false
+        }
     }
 }
