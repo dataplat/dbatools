648a70ea9399cf107cc726547bf6a608876fa3b2
Simone Bizzotto <niphlod@gmail.com>
2025-02-21 19:37:50 +0100

    fix diagnostic suppress

--- public/Invoke-DbaQuery.ps1
+++ public/Invoke-DbaQuery.ps1
@@ -169,7 +169,7 @@ function Invoke-DbaQuery {
         See https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/sql/sqlclient-support-for-high-availability-disaster-recovery#connecting-with-multisubnetfailover
     #>
     [CmdletBinding(DefaultParameterSetName = "Query")]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "Needed to re-auth as different user for ad")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", Justification="Needed to re-auth as different user for ad")]
     param (
         [Parameter(ValueFromPipeline)]
         [Parameter(ParameterSetName = 'Query', Position = 0)]
