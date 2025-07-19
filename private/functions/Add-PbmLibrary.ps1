2077b556a0e23503b0240bfad24d2156630c1e0d
Chrissy LeMaire <clemaire@gmail.com>
2025-07-19 22:02:04 +0200

    Remove unnecessary quotes from Join-DbaPath arguments
    Updated Join-DbaPath calls in Add-PbmLibrary.ps1 to remove single quotes around static string arguments for ChildPath. This improves code consistency and readability.

--- private/functions/Add-PbmLibrary.ps1
+++ private/functions/Add-PbmLibrary.ps1
@@ -3,9 +3,9 @@ function Add-PbmLibrary {
         [switch]$EnableException
     )
     try {
-        $platformlib = Join-DbaPath -Path $script:libraryroot -ChildPath 'lib'
-        $dmfdll = Join-DbaPath -Path $platformlib -ChildPath 'Microsoft.SqlServer.Dmf.dll'
-        $dmfcommon = Join-DbaPath -Path $platformlib -ChildPath 'Microsoft.SqlServer.Dmf.Common.DLL'
+        $platformlib = Join-DbaPath -Path $script:libraryroot -ChildPath lib
+        $dmfdll = Join-DbaPath -Path $platformlib -ChildPath Microsoft.SqlServer.Dmf.dll
+        $dmfcommon = Join-DbaPath -Path $platformlib -ChildPath Microsoft.SqlServer.Dmf.Common.DLL
         Add-Type -Path $dmfcommon -ErrorAction Stop
         Add-Type -Path $dmfdll -ErrorAction Stop
     } catch {
