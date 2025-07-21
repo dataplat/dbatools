d3e82997507a8df9356c64eb5529ff5d6780ea09
andreasjordan <anj@ordix.de>
2025-07-21 19:13:40 +0200

    Install and uninstall in the background with needed restarts

--- tests/Environment/install_local_instances.ps1
+++ tests/Environment/install_local_instances.ps1
@@ -1,5 +1,23 @@
 $ErrorActionPreference = 'Stop'
 
+if ((Get-ScheduledTask).TaskName -notcontains 'RunMeAtStartup') {
+    $scheduledTaskActionParams = @{
+        Execute  = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+        Argument = "-ExecutionPolicy RemoteSigned -NonInteractive -File $($MyInvocation.MyCommand.Path)"
+    }
+    $scheduledTaskParams = @{
+        TaskName = 'RunMeAtStartup'
+        Trigger  = New-ScheduledTaskTrigger -AtStartup
+        User     = 'SYSTEM'
+        Action   = New-ScheduledTaskAction @scheduledTaskActionParams
+    }
+    $null = Register-ScheduledTask @scheduledTaskParams
+    Restart-Computer -Force
+    exit
+}
+
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Starting install"
+
 $repoBase = 'C:\GitHub\dbatools'
 
 Import-Module -Name "$repoBase\dbatools.psm1" -Force
@@ -18,9 +36,10 @@ $instanceParams = @{
     Configuration      = @{
         SqlMaxMemory = '2048'
         NpEnabled    = 1
-    } 
+    }
     AuthenticationMode = 'Mixed'
     SaCredential       = $TestConfig.SqlCred
+    AdminAccount       = "$(hostname)\Administrators"
     EnableException    = $false
 }
 
@@ -28,27 +47,46 @@ foreach ($instance in $sqlInstance) {
     if (Get-DbaService -SqlInstance $instance) {
         continue
     }
+    Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Starting install of $instance"
     $result = Install-DbaInstance @instanceParams -SqlInstance $instance
+    Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Finished install of $instance"
     if ($result.Successful -ne $true) {
-        $result | Format-List *
-        throw 'Installation failed'
+        Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Installation failed"
+        Unregister-ScheduledTask -TaskName RunMeAtStartup -Confirm:$false
+        exit
     }
     if ($result.Notes -match 'restart') {
-        $result.Notes
-        throw 'Installation needs restart'
+        Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Restart needed"
+        Restart-Computer -Force
+        exit
     }
 }
 
-$null = Set-DbaSpConfigure -SqlInstance $sqlInstance -Name IsSqlClrEnabled -Value $true
-$null = Set-DbaSpConfigure -SqlInstance $sqlInstance -Name ClrStrictSecurity -Value $false
+$null = Set-DbaSpConfigure -SqlInstance $sqlInstance -SqlCredential $TestConfig.SqlCred -Name IsSqlClrEnabled -Value $true
+$null = Set-DbaSpConfigure -SqlInstance $sqlInstance -SqlCredential $TestConfig.SqlCred -Name ClrStrictSecurity -Value $false
 
-$null = Set-DbaSpConfigure -SqlInstance $sqlInstance[1, 2] -Name ExtensibleKeyManagementEnabled -Value $true
-Invoke-DbaQuery -SqlInstance $sqlInstance[1, 2] -Query "CREATE CRYPTOGRAPHIC PROVIDER dbatoolsci_AKV FROM FILE = '$($TestConfig.appveyorlabrepo)\keytests\ekm\Microsoft.AzureKeyVaultService.EKM.dll'"
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Step 1"
+
+$null = Set-DbaSpConfigure -SqlInstance $sqlInstance[1, 2] -SqlCredential $TestConfig.SqlCred -Name ExtensibleKeyManagementEnabled -Value $true
+Invoke-DbaQuery -SqlInstance $sqlInstance[1, 2] -SqlCredential $TestConfig.SqlCred -Query "CREATE CRYPTOGRAPHIC PROVIDER dbatoolsci_AKV FROM FILE = '$($TestConfig.appveyorlabrepo)\keytests\ekm\Microsoft.AzureKeyVaultService.EKM.dll'"
+
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Step 2"
+
 $null = Enable-DbaAgHadr -SqlInstance $sqlInstance[1, 2] -Force
 
-Invoke-DbaQuery -SqlInstance $sqlInstance[2] -Query "IF NOT EXISTS (select * from sys.symmetric_keys where name like '%DatabaseMasterKey%') CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<StrongPassword>'"
-Invoke-DbaQuery -SqlInstance $sqlInstance[2] -Query "IF EXISTS ( SELECT * FROM sys.tcp_endpoints WHERE name = 'End_Mirroring') DROP ENDPOINT endpoint_mirroring"
-Invoke-DbaQuery -SqlInstance $sqlInstance[2] -Query "CREATE CERTIFICATE dbatoolsci_AGCert WITH SUBJECT = 'AG Certificate'"
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Step 3"
+
+Invoke-DbaQuery -SqlInstance $sqlInstance[2] -SqlCredential $TestConfig.SqlCred -Query "IF NOT EXISTS (select * from sys.symmetric_keys where name like '%DatabaseMasterKey%') CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<StrongPassword>'"
+Invoke-DbaQuery -SqlInstance $sqlInstance[2] -SqlCredential $TestConfig.SqlCred -Query "IF EXISTS ( SELECT * FROM sys.tcp_endpoints WHERE name = 'End_Mirroring') DROP ENDPOINT endpoint_mirroring"
+Invoke-DbaQuery -SqlInstance $sqlInstance[2] -SqlCredential $TestConfig.SqlCred -Query "CREATE CERTIFICATE dbatoolsci_AGCert WITH SUBJECT = 'AG Certificate'"
 
 $null = Set-DbaNetworkConfiguration -SqlInstance $sqlInstance[1] -StaticPortForIPAll 14333 -RestartService
 $null = Set-DbaNetworkConfiguration -SqlInstance $sqlInstance[2] -StaticPortForIPAll 14334 -RestartService
+
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Finished install"
+
+# Remove the task as we are finished
+Unregister-ScheduledTask -TaskName RunMeAtStartup -Confirm:$false
+
+# Restart the computer one last time to have everything clean
+Restart-Computer -Force
--- tests/Environment/uninstall_local_instances.ps1
+++ tests/Environment/uninstall_local_instances.ps1
@@ -1,5 +1,23 @@
 $ErrorActionPreference = 'Stop'
 
+if ((Get-ScheduledTask).TaskName -notcontains 'RunMeAtStartup') {
+    $scheduledTaskActionParams = @{
+        Execute  = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+        Argument = "-ExecutionPolicy RemoteSigned -NonInteractive -File $($MyInvocation.MyCommand.Path)"
+    }
+    $scheduledTaskParams = @{
+        TaskName = 'RunMeAtStartup'
+        Trigger  = New-ScheduledTaskTrigger -AtStartup
+        User     = 'SYSTEM'
+        Action   = New-ScheduledTaskAction @scheduledTaskActionParams
+    }
+    $null = Register-ScheduledTask @scheduledTaskParams
+    Restart-Computer -Force
+    exit
+}
+
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Starting uninstall"
+
 $repoBase = 'C:\GitHub\dbatools'
 
 Import-Module -Name "$repoBase\dbatools.psm1" -Force
@@ -15,7 +33,7 @@ $null = Stop-DbaService -SqlInstance $sqlInstance -Type Engine -Force -ErrorActi
 $instanceParams = @{
     Version         = 2022
     Path            = '\\fs\Software\SQLServer\ISO'
-    Configuration   = @{ ACTION = 'Uninstall' } 
+    Configuration   = @{ ACTION = 'Uninstall' }
     EnableException = $false
 }
 
@@ -23,15 +41,27 @@ foreach ($instance in $sqlInstance) {
     if (-not (Get-DbaService -SqlInstance $instance)) {
         continue
     }
+    Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Starting uninstall of $instance"
     $result = Install-DbaInstance @instanceParams -SqlInstance $instance
+    Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Finished uninstall of $instance"
     if ($result.Successful -ne $true) {
-        $result | Format-List *
-        throw 'Installation failed'
+        Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Uninstallation failed"
+        Unregister-ScheduledTask -TaskName RunMeAtStartup -Confirm:$false
+        exit
     }
     if ($result.Notes -match 'restart') {
-        $result.Notes
-        throw 'Installation needs restart'
+        Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Restart needed"
+        Restart-Computer -Force
+        exit
     }
 }
 
 Remove-Item -Path 'C:\Program Files\Microsoft SQL Server', 'C:\Temp\*' -Recurse
+
+Add-Content -Path $PSScriptRoot\logs\status.txt -Value "[$([datetime]::Now.ToString('HH:mm:ss'))] Finished uninstall"
+
+# Remove the task as we are finished
+Unregister-ScheduledTask -TaskName RunMeAtStartup -Confirm:$false
+
+# Restart the computer one last time to have everything clean
+Restart-Computer -Force
