d105500ad2f4d2a72cc58b8e65f3eb94b65afb05
Andreas Jordan <anj@ordix.de>
2025-08-10 19:42:17 +0200

    revert last commit and try new idea (do Read-DbaXEFile)

--- tests/Read-DbaXEFile.Tests.ps1
+++ tests/Read-DbaXEFile.Tests.ps1
@@ -18,26 +18,28 @@ Describe "$CommandName Integration Tests" -Tags "IntegrationTests" {
     Context "Verifying command output" {
         BeforeAll {
             # Just to analyse problems on AppVeyor
-            $sessions = Get-DbaXESession -SqlInstance $TestConfig.instance3
+            $sessions = Get-DbaXESession -SqlInstance $TestConfig.instance2
             foreach ($session in $sessions) {
                 Write-Warning -Message "Status and AutoStart of $($session.Name): $($session.Status) / $($session.AutoStart)"
             }
-            $results = Get-DbaXESession -SqlInstance $TestConfig.instance3 -Session system_health | Read-DbaXEFile -Raw
+            $results = Get-DbaXESession -SqlInstance $TestConfig.instance2 -Session system_health | Read-DbaXEFile -Raw
             if (-not $results) {
-                Write-Warning -Message "No results, so restarting service"
-                $null = Restart-DbaService -SqlInstance $TestConfig.instance3 -Type Engine -Force
-                $results = Get-DbaXESession -SqlInstance $TestConfig.instance3 -Session system_health | Read-DbaXEFile -Raw
+                Write-Warning -Message "No results, so trying an invalid login and restarting service"
+                $cred = [PSCredential]::new('invalid', (ConvertTo-SecureString -String 'invalid' -AsPlainText -Force))
+                try { $null = Connect-DbaInstance -SqlInstance $TestConfig.instance2 -SqlCredential $cred } catch { }
+                $null = Restart-DbaService -SqlInstance $TestConfig.instance2 -Type Engine -Force
+                $results = Get-DbaXESession -SqlInstance $TestConfig.instance2 -Session system_health | Read-DbaXEFile -Raw
                 if (-not $results) {
                     Write-Warning -Message "Still no results..."
                 }
             }
         }
         It "returns some results" {
-            $results = Get-DbaXESession -SqlInstance $TestConfig.instance3 -Session system_health | Read-DbaXEFile -Raw
+            $results = Get-DbaXESession -SqlInstance $TestConfig.instance2 -Session system_health | Read-DbaXEFile -Raw
             $results | Should -Not -BeNullOrEmpty
         }
         It "returns some results" {
-            $results = Get-DbaXESession -SqlInstance $TestConfig.instance3 -Session system_health | Read-DbaXEFile
+            $results = Get-DbaXESession -SqlInstance $TestConfig.instance2 -Session system_health | Read-DbaXEFile
             $results | Should -Not -BeNullOrEmpty
         }
     }
