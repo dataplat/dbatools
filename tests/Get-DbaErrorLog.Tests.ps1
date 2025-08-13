db91d7342c51b80b40adb6b936a8065898c23a47
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2025-08-13 11:44:51 +0200

    Update Get-DbaErrorLog.Tests.ps1

--- tests/Get-DbaErrorLog.Tests.ps1
+++ tests/Get-DbaErrorLog.Tests.ps1
@@ -5,9 +5,6 @@ param(
     $PSDefaultParameterValues = $TestConfig.Defaults
 )
 
-Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
-$global:TestConfig = Get-TestConfig
-
 Describe $CommandName -Tag UnitTests {
     Context "Parameter validation" {
         BeforeAll {
@@ -38,7 +35,6 @@ Describe $CommandName -Tag IntegrationTests {
             $PSDefaultParameterValues['*-Dba*:EnableException'] = $true
 
             $sourceFilter = "Logon"
-            $textFilter = "All rights reserved"
             $login = "DaperDan"
             $l = Get-DbaLogin -SqlInstance $TestConfig.instance1 -Login $login
             if ($l) {
@@ -57,11 +53,15 @@ Describe $CommandName -Tag IntegrationTests {
                 Connect-DbaInstance -SqlInstance $TestConfig.instance1 -SqlCredential $sqlCred -ErrorVariable whatever
             } catch {}
 
+            # Use script scope for variables accessed in It blocks
+            $script:sourceFilter = "Logon"
+            $script:textFilter = "All rights reserved"
+
             # Get data for Before/After filter tests
             $after = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 | Select-Object -First 1
             $before = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 | Select-Object -Last 1
-            $afterFilter = $after.LogDate.AddMinutes(+1)
-            $beforeFilter = $before.LogDate.AddMinutes(-1)
+            $script:afterFilter = $after.LogDate.AddMinutes(+1)
+            $script:beforeFilter = $before.LogDate.AddMinutes(-1)
 
             # We want to run all commands outside of the BeforeAll block without EnableException to be able to test for specific warnings.
             $PSDefaultParameterValues.Remove('*-Dba*:EnableException')
@@ -74,43 +74,43 @@ Describe $CommandName -Tag IntegrationTests {
         }
 
         It "Returns filtered results for [Source = <sourceFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Source $sourceFilter
-            $results[0].Source | Should -Be $sourceFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Source $script:sourceFilter
+            $results[0].Source | Should -Be $script:sourceFilter
         }
 
         It "Returns filtered result for [LogNumber = 0] and [Source = <sourceFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 0 -Source $sourceFilter
-            $results[0].Source | Should -Be $sourceFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 0 -Source $script:sourceFilter
+            $results[0].Source | Should -Be $script:sourceFilter
         }
 
         It "Returns filtered results for [Text = <textFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Text $textFilter
-            $results[0].Text | Should -BeLike "*$textFilter*"
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Text $script:textFilter
+            $results[0].Text | Should -BeLike "*$script:textFilter*"
         }
 
         It "Returns filtered result for [LogNumber = 0] and [Text = <textFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 0 -Text $textFilter
-            $results[0].Text | Should -BeLike "*$textFilter*"
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 0 -Text $script:textFilter
+            $results[0].Text | Should -BeLike "*$script:textFilter*"
         }
 
         It "Returns filtered results for [After = <afterFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -After $afterFilter
-            $results[0].LogDate | Should -BeGreaterOrEqual $afterFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -After $script:afterFilter
+            $results[0].LogDate | Should -BeGreaterOrEqual $script:afterFilter
         }
 
         It "Returns filtered results for [LogNumber = 1] and [After = <afterFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 -After $afterFilter
-            $results[0].LogDate | Should -BeGreaterOrEqual $afterFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 -After $script:afterFilter
+            $results[0].LogDate | Should -BeGreaterOrEqual $script:afterFilter
         }
 
         It "Returns filtered result for [Before = <beforeFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Before $beforeFilter
-            $results[-1].LogDate | Should -BeLessOrEqual $beforeFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -Before $script:beforeFilter
+            $results[-1].LogDate | Should -BeLessOrEqual $script:beforeFilter
         }
 
         It "Returns filtered result for [LogNumber = 1] and [Before = <beforeFilter>]" {
-            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 -Before $beforeFilter
-            $results[-1].LogDate | Should -BeLessOrEqual $beforeFilter
+            $results = Get-DbaErrorLog -SqlInstance $TestConfig.instance1 -LogNumber 1 -Before $script:beforeFilter
+            $results[-1].LogDate | Should -BeLessOrEqual $script:beforeFilter
         }
     }
 }
