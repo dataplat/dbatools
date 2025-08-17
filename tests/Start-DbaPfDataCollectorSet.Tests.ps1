f8e61a89730245916855b138239985dfe02539cd
Andreas Jordan <anj@ordix.de>
2025-08-17 13:21:18 +0200

    small fix (do Start-DbaPfDataCollectorSet)

--- tests/Start-DbaPfDataCollectorSet.Tests.ps1
+++ tests/Start-DbaPfDataCollectorSet.Tests.ps1
@@ -9,7 +9,7 @@ Describe $CommandName -Tag UnitTests {
     Context "Parameter validation" {
         It "Should have the expected parameters" {
             $hasParameters = (Get-Command $CommandName).Parameters.Values.Name | Where-Object { $PSItem -notin ("WhatIf", "Confirm") }
-            $expectedParameters = $global:TestConfig.CommonParameters
+            $expectedParameters = $TestConfig.CommonParameters
             $expectedParameters += @(
                 "ComputerName",
                 "Credential",
