e1e836441c363a188e14d804212381d074fb9b5a
Andreas Jordan <anj@ordix.de>
2025-08-18 09:51:50 +0200

    more fixes

--- tests/New-DbaReplCreationScriptOptions.Tests.ps1
+++ tests/New-DbaReplCreationScriptOptions.Tests.ps1
@@ -9,7 +9,7 @@ Describe $CommandName -Tag UnitTests {
     Context "Parameter validation" {
         It "Should have the expected parameters" {
             $hasParameters = (Get-Command $CommandName).Parameters.Values.Name | Where-Object { $PSItem -notin ("WhatIf", "Confirm") }
-            $expectedParameters = $TestConfig.CommonParameters
+            $expectedParameters = @( )  # Command does not use [CmdletBinding()]
             $expectedParameters += @(
                 "Options",
                 "NoDefaults"
