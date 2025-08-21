d6630c33acba9616b332cd77f5a0c4916065bd90
Andreas Jordan <anj@ordix.de>
2025-08-21 15:06:14 +0200

    fixes

--- tests/Backup-DbaDbMasterKey.Tests.ps1
+++ tests/Backup-DbaDbMasterKey.Tests.ps1
@@ -42,20 +42,13 @@ Describe $CommandName -Tag IntegrationTests {
         # We'll create the master key if it doesn't exist, and track files created for cleanup.
 
         # Set variables. They are available in all the It blocks.
-        $testInstance    = $TestConfig.instance1
-        $testDatabase    = "tempdb"
-        $masterKeyPass   = ConvertTo-SecureString -String "GoodPass1234!" -AsPlainText -Force
+        $random = Get-Random
+        $testInstance = $TestConfig.instance1
+        $testDatabase = "dbatoolscli_db_$random"
+        $masterKeyPass = ConvertTo-SecureString -String "GoodPass1234!" -AsPlainText -Force
 
-        # Create the objects.
-        if (-not (Get-DbaDbMasterKey -SqlInstance $testInstance -Database $testDatabase)) {
-            $splatNewKey = @{
-                SqlInstance = $testInstance
-                Database    = $testDatabase
-                Password    = $masterKeyPass
-                Confirm     = $false
-            }
-            $null = New-DbaDbMasterKey @splatNewKey
-        }
+        $null = New-DbaDatabase -SqlInstance $testInstance -Name $testDatabase
+        $null = New-DbaDbMasterKey -SqlInstance $testInstance -Database $testDatabase -Password $masterKeyPass
 
         # We want to run all commands outside of the BeforeAll block without EnableException to be able to test for specific warnings.
         $PSDefaultParameterValues.Remove("*-Dba*:EnableException")
@@ -66,7 +59,7 @@ Describe $CommandName -Tag IntegrationTests {
         $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
 
         # Cleanup all created objects.
-        Get-DbaDbMasterKey -SqlInstance $testInstance -Database $testDatabase | Remove-DbaDbMasterKey -Confirm:$false
+        Remove-DbaDatabase -SqlInstance $testInstance -Database $testDatabase
 
         # Remove the backup directory.
         Remove-Item -Path $backupPath -Recurse -ErrorAction SilentlyContinue
