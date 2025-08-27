ec2e73af748063deb5c11f182c2e07d8ad22cfc5
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2025-08-27 21:13:26 +0200

    Invoke-DbatoolsRenameHelper - fix command and tests (#9822)

--- public/Invoke-DbatoolsRenameHelper.ps1
+++ public/Invoke-DbatoolsRenameHelper.ps1
@@ -303,9 +303,9 @@ function Invoke-DbatoolsRenameHelper {
             $file = $fileobject.FullName
 
             foreach ($name in $paramrenames.GetEnumerator()) {
-                if ((Select-String -Pattern $name.Key -Path $file)) {
+                if ((Select-String -Pattern "\b$($name.Key)\b" -Path $file)) {
                     if ($Pscmdlet.ShouldProcess($file, "Replacing $($name.Key) with $($name.Value)")) {
-                        $content = (Get-Content -Path $file -Raw).Replace($name.Key, $name.Value).Trim()
+                        $content = ((Get-Content -Path $file -Raw) -Replace "\b$($name.Key)\b", $name.Value).Trim()
                         Set-Content -Path $file -Encoding $Encoding -Value $content
                         [PSCustomObject]@{
                             Path         = $file
