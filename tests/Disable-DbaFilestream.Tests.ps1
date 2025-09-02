6cc6da16206b580637a4f88637e18ef3ecf1ad54
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2025-09-02 18:18:39 +0200

    Refactor Filestream related tests (#9828)

--- tests/Disable-DbaFilestream.Tests.ps1
+++ tests/Disable-DbaFilestream.Tests.ps1
@@ -22,47 +22,23 @@ Describe $CommandName -Tag UnitTests {
     }
 }
 
-<#
 Describe $CommandName -Tag IntegrationTests {
     BeforeAll {
         # We want to run all commands in the BeforeAll block with EnableException to ensure that the test fails if the setup fails.
         $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
 
-        # Store the original FileStream level to restore after testing
-        $originalFileStream = Get-DbaFilestream -SqlInstance $TestConfig.instance1
+        $null = Enable-DbaFilestream -SqlInstance $TestConfig.instance1 -FileStreamLevel 1 -Force
 
         # We want to run all commands outside of the BeforeAll block without EnableException to be able to test for specific warnings.
         $PSDefaultParameterValues.Remove("*-Dba*:EnableException")
     }
 
-    AfterAll {
-        # We want to run all commands in the AfterAll block with EnableException to ensure that the test fails if the cleanup fails.
-        $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
-
-        # Restore the original FileStream level
-        Set-DbaFilestream -SqlInstance $TestConfig.instance1 -FileStreamLevel $originalFileStream.InstanceAccessLevel -Force
-
-        $PSDefaultParameterValues.Remove("*-Dba*:EnableException")
-    }
-
     Context "When changing FileStream Level" {
-        BeforeAll {
-            # Move it on one, but keep it less than 4 with modulo division
-            $newLevel = ($originalFileStream.FileStreamStateId + 1) % 3
-            $splatFilestream = @{
-                SqlInstance     = $TestConfig.instance1
-                FileStreamLevel = $newLevel
-                Force           = $true
-                WarningAction   = "SilentlyContinue"
-                ErrorVariable   = "errvar"
-                ErrorAction     = "SilentlyContinue"
-            }
-            $results = Set-DbaFilestream @splatFilestream
-        }
-
         It "Should change the FileStream Level" {
-            $results.InstanceAccessLevel | Should -Be $newLevel
+            $results = Disable-DbaFilestream -SqlInstance $TestConfig.instance1 -Force
+
+            $results.InstanceAccessLevel | Should -Be 0
+            $results.ServiceAccessLevel | Should -Be 0
         }
     }
 }
-#>
