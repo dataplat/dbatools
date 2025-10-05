22590c610f6166c170d14bc6ae7bf03b5486a63c
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2025-10-05 16:32:21 +0200

    Update resource group name in backend config
    Changed the Terraform backend azurerm resource_group_name from 'dbatools-ci-runners' to 'dbatools-ci-images' to reflect the correct resource group for state storage.

--- gh-runners/version.tf
+++ gh-runners/version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   backend "azurerm" {
-    resource_group_name  = "dbatools-ci-runners"
+    resource_group_name  = "dbatools-ci-images"
     storage_account_name = "dbatoolstfstate"
     container_name       = "tfstate"
     key                  = "vmss.tfstate"
