19ed3267af4b6d48b199477ac625f8d99f81f614
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2025-11-09 15:09:02 +0100

    Invoke-DbaDbLogShipping - Fix Azure backup URL to use base URL instead of database-specific path
    (do Invoke-DbaDbLogShipping)

--- public/Invoke-DbaDbLogShipping.ps1
+++ public/Invoke-DbaDbLogShipping.ps1
@@ -1483,21 +1483,19 @@ function Invoke-DbaDbLogShipping {
                                 $Timestamp = Get-Date -format "yyyyMMddHHmmss"
 
                                 if ($UseAzure) {
-                                    # Backup to Azure blob storage
+                                    # Backup to Azure blob storage - use base URL, put database folder in filename
                                     $splatBackup = @{
                                         SqlInstance    = $SourceSqlInstance
                                         SqlCredential  = $SourceSqlCredential
                                         Database       = $($db.Name)
-                                        AzureBaseUrl   = $DatabaseSharedPath
-                                        BackupFileName = "FullBackup_$($db.Name)_PreLogShipping_$Timestamp.bak"
+                                        AzureBaseUrl   = $SharedPath
+                                        BackupFileName = "$($db.Name)/FullBackup_$($db.Name)_PreLogShipping_$Timestamp.bak"
                                         Type           = "Full"
                                     }
 
+                                    # For Azure credentials, explicitly pass if provided, otherwise let Backup-DbaDatabase auto-detect
                                     if ($AzureCredential) {
                                         $splatBackup.AzureCredential = $AzureCredential
-                                    } else {
-                                        # When AzureCredential is not specified, use SAS token credential named after the base URL
-                                        $splatBackup.AzureCredential = "[$SharedPath]"
                                     }
 
                                     $LastBackup = Backup-DbaDatabase @splatBackup
