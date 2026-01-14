892cba43b5323f7123e4a8c6d41ba62ded6868e8
Andreas Jordan <anj@ordix.de>
2026-01-14 16:57:13 +0100

    fix comment and use different instance

--- tests/Test-DbaLinkedServerConnection.Tests.ps1
+++ tests/Test-DbaLinkedServerConnection.Tests.ps1
@@ -22,10 +22,10 @@ Describe $CommandName -Tag UnitTests {
 
 Describe $CommandName -Tag IntegrationTests {
     BeforeAll {
-        $port = (Get-DbaTcpPort -SqlInstance $TestConfig.instance1).Port
+        $port = (Get-DbaTcpPort -SqlInstance $TestConfig.InstanceSingle).Port
         $target = "localhost:$port"
 
-        $server = Connect-DbaInstance -SqlInstance $TestConfig.instance1
+        $server = Connect-DbaInstance -SqlInstance $TestConfig.InstanceSingle
         if ($server.VersionMajor -ge 17) {
             # Starting with SQL Server 2025 (17.x), MSOLEDBSQL uses Microsoft OLE DB Driver version 19, which adds support for TDS 8.0. However, this driver introduces a breaking change. You must now specify the encrypt parameter.
             $server.Query("EXEC master.dbo.sp_addlinkedserver @server=N'$target', @srvproduct=N'', @provider=N'MSOLEDBSQL', @provstr = N'encrypt=optional;TrustServerCertificate=yes'")
@@ -43,14 +43,14 @@ Describe $CommandName -Tag IntegrationTests {
 
     Context "Function works" {
         BeforeAll {
-            $results = Test-DbaLinkedServerConnection -SqlInstance $TestConfig.instance1 | Where-Object LinkedServerName -eq $target
+            $results = Test-DbaLinkedServerConnection -SqlInstance $TestConfig.InstanceSingle | Where-Object LinkedServerName -eq $target
         }
 
         It "function returns results" {
             $results | Should -Not -BeNullOrEmpty
         }
 
-        It "linked server name is localhost" {
+        It "linked server name is correct" {
             $results.LinkedServerName | Should -Be $target
         }
 
@@ -61,14 +61,14 @@ Describe $CommandName -Tag IntegrationTests {
 
     Context "Piping to function works" {
         BeforeAll {
-            $pipeResults = Get-DbaLinkedServer -SqlInstance $TestConfig.instance1 | Test-DbaLinkedServerConnection
+            $pipeResults = Get-DbaLinkedServer -SqlInstance $TestConfig.InstanceSingle | Test-DbaLinkedServerConnection
         }
 
         It "piping from Get-DbaLinkedServerConnection returns results" {
             $pipeResults | Should -Not -BeNullOrEmpty
         }
 
-        It "linked server name is localhost" {
+        It "linked server name is correct" {
             $pipeResults.LinkedServerName | Should -Be $target
         }
 
