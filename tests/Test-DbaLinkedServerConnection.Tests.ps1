bb423ece010ffd9e09ea4ee9a82c852f2c005b5c
Andreas Jordan <anj@ordix.de>
2026-01-14 20:46:11 +0100

    Enable named pipes

--- tests/Test-DbaLinkedServerConnection.Tests.ps1
+++ tests/Test-DbaLinkedServerConnection.Tests.ps1
@@ -22,10 +22,16 @@ Describe $CommandName -Tag UnitTests {
 
 Describe $CommandName -Tag IntegrationTests {
     BeforeAll {
-        $port = (Get-DbaTcpPort -SqlInstance $TestConfig.InstanceSingle).Port
+        if ($env:AppVeyor) {
+            # On appVeyor we need named pipes to connect to localhost
+            # As we don't want enable named pipes for all tests, we enable it just for this test
+            $null = Set-DbaNetworkConfiguration -SqlInstance $TestConfig.InstanceRestart -EnableProtocol NamedPipes -RestartService
+        }
+
+        $port = (Get-DbaTcpPort -SqlInstance $TestConfig.InstanceRestart).Port
         $target = "localhost:$port"
 
-        $server = Connect-DbaInstance -SqlInstance $TestConfig.InstanceSingle
+        $server = Connect-DbaInstance -SqlInstance $TestConfig.InstanceRestart
         if ($server.VersionMajor -ge 17) {
             # Starting with SQL Server 2025 (17.x), MSOLEDBSQL uses Microsoft OLE DB Driver version 19, which adds support for TDS 8.0. However, this driver introduces a breaking change. You must now specify the encrypt parameter.
             $server.Query("EXEC master.dbo.sp_addlinkedserver @server=N'$target', @srvproduct=N'', @provider=N'MSOLEDBSQL', @provstr = N'encrypt=optional;TrustServerCertificate=yes'")
@@ -39,11 +45,16 @@ Describe $CommandName -Tag IntegrationTests {
 
     AfterAll {
         $server.Query("EXEC master.dbo.sp_dropserver @server=N'$target'")
+
+        if ($env:AppVeyor) {
+            $null = Set-DbaNetworkConfiguration -SqlInstance $TestConfig.InstanceRestart -DisableProtocol NamedPipes -RestartService
+        }
+
     }
 
     Context "Function works" {
         BeforeAll {
-            $results = Test-DbaLinkedServerConnection -SqlInstance $TestConfig.InstanceSingle | Where-Object LinkedServerName -eq $target
+            $results = Test-DbaLinkedServerConnection -SqlInstance $TestConfig.InstanceRestart | Where-Object LinkedServerName -eq $target
         }
 
         It "function returns results" {
@@ -62,7 +73,7 @@ Describe $CommandName -Tag IntegrationTests {
 
     Context "Piping to function works" {
         BeforeAll {
-            $pipeResults = Get-DbaLinkedServer -SqlInstance $TestConfig.InstanceSingle | Test-DbaLinkedServerConnection
+            $pipeResults = Get-DbaLinkedServer -SqlInstance $TestConfig.InstanceRestart | Test-DbaLinkedServerConnection
         }
 
         It "piping from Get-DbaLinkedServerConnection returns results" {
