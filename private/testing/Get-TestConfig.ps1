70637bd8cba929ab83cb0fa8e781ece8a73fb52a
Andreas Jordan <anj@ordix.de>
2026-01-16 19:49:34 +0100

    use 2008R2 as base version

--- private/testing/Get-TestConfig.ps1
+++ private/testing/Get-TestConfig.ps1
@@ -50,13 +50,13 @@ function Get-TestConfig {
         # This configuration is used for the automated test on AppVeyor
         $config['DbaToolsCi_Computer'] = "$(hostname)"
 
-        $config['InstanceSingle'] = "$(hostname)\sql2016"
-        $config['InstanceMulti1'] = "$(hostname)\sql2016"
+        $config['InstanceSingle'] = "$(hostname)\sql2008r2sp2"
+        $config['InstanceMulti1'] = "$(hostname)\sql2008r2sp2"
         $config['InstanceMulti2'] = "$(hostname)\sql2017"
-        $config['InstanceCopy1'] = "$(hostname)\sql2016"
+        $config['InstanceCopy1'] = "$(hostname)\sql2008r2sp2"
         $config['InstanceCopy2'] = "$(hostname)\sql2017"
         $config['InstanceHadr'] = "$(hostname)\sql2017"
-        $config['InstanceRestart'] = "$(hostname)\sql2017"
+        $config['InstanceRestart'] = "$(hostname)\sql2008r2sp2"
 
         $config['SQLUserName'] = $null  # placeholders for -SqlCredential testing
         $config['SQLPassword'] = $null
