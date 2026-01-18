f26058575a7b947b08be3433f4d20fb58275d341
Andreas Jordan <anj@ordix.de>
2026-01-18 15:26:15 +0100

    try new image as default

--- tests/appveyor.common.ps1
+++ tests/appveyor.common.ps1
@@ -4,7 +4,7 @@ function Get-TestsForScenario {
     # does this scenario run an 'autodetect' ?
     if ($TestsRunGroups[$Scenario].StartsWith('autodetect_')[0]) {
         # exclude any test specifically tied to a non-autodetect or version specific scenario
-        $TiedFunctions = ($TestsRunGroups.GetEnumerator() | Where-Object { $_.Value -notlike 'autodetect_*' -and $_.Value -notlike '20*' }).Value
+        $TiedFunctions = ($TestsRunGroups.GetEnumerator() | Where-Object { $_.Value -notlike 'autodetect_*' -and $_.Name -notlike '20*' }).Value
         $RemainingTests = $AllTests | Where-Object { ($_.Name -replace '^([^.]+)(.+)?.Tests.ps1', '$1') -notin $TiedFunctions }
         # and now scan for the instance string
         $ScanFor = $TestsRunGroups[$Scenario].Replace('autodetect_', '')
