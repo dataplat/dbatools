28ff15468cf433abec9be09cee3fe7965a6fcbb5
Chrissy LeMaire <clemaire@gmail.com>
2026-07-13 19:53:33 +0200

    Get-DbaQueryExecutionTime: add characterization IntegrationTests (fixture proc in tempdb)
    The test file had no IntegrationTests context at all, so every gate
    integration leg executed zero tests and an empty run is never green.
    
    (do Get-DbaQueryExecutionTime)

--- tests/Get-DbaQueryExecutionTime.Tests.ps1
+++ tests/Get-DbaQueryExecutionTime.Tests.ps1
@@ -25,8 +25,46 @@ Describe $CommandName -Tag UnitTests {
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/dataplat/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe $CommandName -Tag IntegrationTests {
+    BeforeAll {
+        # We want to run all commands in the BeforeAll block with EnableException to ensure that the test fails if the setup fails.
+        $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
+
+        $splatFixture = @{
+            SqlInstance = $TestConfig.InstanceSingle
+            Database    = "tempdb"
+        }
+        $null = Invoke-DbaQuery @splatFixture -Query "CREATE OR ALTER PROCEDURE dbo.dbatoolsci_queryexectime AS SELECT 1 AS A"
+        foreach ($execRun in 1..3) {
+            $null = Invoke-DbaQuery @splatFixture -Query "EXEC dbo.dbatoolsci_queryexectime"
+        }
+
+        # We want to run all commands outside of the BeforeAll block without EnableException to be able to test for specific warnings.
+        $PSDefaultParameterValues.Remove("*-Dba*:EnableException")
+    }
+
+    AfterAll {
+        # We want to run all commands in the AfterAll block with EnableException to ensure that the test fails if the cleanup fails.
+        $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
+
+        $null = Invoke-DbaQuery -SqlInstance $TestConfig.InstanceSingle -Database tempdb -Query "DROP PROCEDURE dbo.dbatoolsci_queryexectime" -ErrorAction SilentlyContinue
+    }
+
+    Context "When retrieving query execution times" {
+        It "Returns the fixture procedure with the expected shape" {
+            $splatQuery = @{
+                SqlInstance     = $TestConfig.InstanceSingle
+                Database        = "tempdb"
+                MinExecs        = 2
+                MinExecMs       = 0
+                MaxResultsPerDb = 5000
+            }
+            $results = @(Get-DbaQueryExecutionTime @splatQuery)
+            $fixture = @($results | Where-Object ProcName -eq "dbatoolsci_queryexectime")
+            $fixture | Should -Not -BeNullOrEmpty
+            $fixture[0].Database | Should -Be "tempdb"
+            $fixture[0].Executions | Should -BeGreaterOrEqual 2
+        }
+    }
+}
