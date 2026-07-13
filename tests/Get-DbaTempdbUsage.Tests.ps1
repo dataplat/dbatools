9db91e39ca769830324764b991837103f1bcd278
Chrissy LeMaire <clemaire@gmail.com>
2026-07-13 20:49:14 +0200

    Get-DbaTempdbUsage: add characterization IntegrationTests (held-open tempdb session fixture)
    (do Get-DbaTempdbUsage)

--- tests/Get-DbaTempdbUsage.Tests.ps1
+++ tests/Get-DbaTempdbUsage.Tests.ps1
@@ -19,8 +19,30 @@ Describe $CommandName -Tag UnitTests {
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/dataplat/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe $CommandName -Tag IntegrationTests {
+    Context "When retrieving tempdb usage" {
+        BeforeAll {
+            # We want to run all commands in the BeforeAll block with EnableException to ensure that the test fails if the setup fails.
+            $PSDefaultParameterValues["*-Dba*:EnableException"] = $true
+
+            # A held-open session with a live temp table gives the DMV join a nonzero
+            # net tempdb allocation to report.
+            $fixtureServer = Connect-DbaInstance -SqlInstance $TestConfig.InstanceSingle -NonPooledConnection
+            $fixtureServer.ConnectionContext.ExecuteNonQuery("CREATE TABLE #dbatoolsci_tempdbusage (Filler CHAR(8000)); INSERT INTO #dbatoolsci_tempdbusage SELECT REPLICATE(CHAR(120), 10);") | Out-Null
+
+            $PSDefaultParameterValues.Remove("*-Dba*:EnableException")
+        }
+
+        AfterAll {
+            $fixtureServer.ConnectionContext.Disconnect()
+        }
+
+        It "Returns the fixture session with tempdb allocations" {
+            $results = @(Get-DbaTempdbUsage -SqlInstance $TestConfig.InstanceSingle)
+            $fixtureRows = @($results | Where-Object Spid -eq $fixtureServer.ConnectionContext.ProcessID)
+            $fixtureRows | Should -Not -BeNullOrEmpty
+            $fixtureRows[0].TotalUserAllocatedKB | Should -BeGreaterThan 0
+        }
+    }
+}
