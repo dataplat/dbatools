e98a2e4933ae690dc9af6c04104b0ee161a57b0d
Chrissy LeMaire <clemaire@gmail.com>
2026-07-14 01:21:14 +0200

    Invoke-DbaPfRelog: add characterization IntegrationTests (logman-captured blg fixture)
    (do Invoke-DbaPfRelog)

--- tests/Invoke-DbaPfRelog.Tests.ps1
+++ tests/Invoke-DbaPfRelog.Tests.ps1
@@ -33,8 +33,34 @@ Describe $CommandName -Tag UnitTests {
         }
     }
 }
-<#
-    Integration test should appear below and are custom to the command you are writing.
-    Read https://github.com/dataplat/dbatools/blob/development/contributing.md#tests
-    for more guidence.
-#>
+
+Describe $CommandName -Tag IntegrationTests {
+    Context "When relogging a captured blg" {
+        BeforeAll {
+            # a real 3-second logman capture gives relog.exe a genuine input file
+            $blgDir = "$($TestConfig.Temp)\$CommandName-$(Get-Random)"
+            $null = New-Item -Path $blgDir -ItemType Directory
+            $counterPath = "\Processor(_Total)\% Processor Time"
+            $null = logman create counter dbatoolsci_w1109 -c $counterPath -si 1 -o "$blgDir\dbatoolsci_w1109" -f bin
+            $null = logman start dbatoolsci_w1109
+            Start-Sleep -Seconds 3
+            $null = logman stop dbatoolsci_w1109
+            $null = logman delete dbatoolsci_w1109
+        }
+
+        AfterAll {
+            $null = logman stop dbatoolsci_w1109 2>&1
+            $null = logman delete dbatoolsci_w1109 2>&1
+            Remove-Item -Path $blgDir -Recurse -ErrorAction SilentlyContinue
+        }
+
+        It "Converts a blg to csv and decorates the output file" {
+            $blg = @(Get-ChildItem -Path $blgDir -Filter *.blg)
+            $blg | Should -Not -BeNullOrEmpty
+            $results = @(Invoke-DbaPfRelog -Path $blg[0].FullName -Type csv -AllowClobber)
+            $results | Should -Not -BeNullOrEmpty
+            $results[0].Extension | Should -Be ".csv"
+            $results[0].RelogFile | Should -Be $true
+        }
+    }
+}
