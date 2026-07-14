edf3d0f1326c0670055c0f1f8c26dde25e7b6d2c
Chrissy LeMaire <clemaire@gmail.com>
2026-07-15 01:09:12 +0200

    Fix Watch-DbaDbLogin warning assertion

--- tests/Watch-DbaDbLogin.Tests.ps1
+++ tests/Watch-DbaDbLogin.Tests.ps1
@@ -81,7 +81,7 @@ Describe $CommandName -Tag IntegrationTests {
             Watch-DbaDbLogin -WarningVariable validationWarnings
 
             $validationWarnings.Count | Should -Be 1
-            $validationWarnings[0].ToString() | Should -BeLike "*[Watch-DbaDbLogin] $expectedMessage"
+            $validationWarnings[0].ToString().EndsWith("[Watch-DbaDbLogin] $expectedMessage") | Should -BeTrue
             $Error[0].FullyQualifiedErrorId | Should -Be "dbatools_Watch-DbaDbLogin"
         }
     }
